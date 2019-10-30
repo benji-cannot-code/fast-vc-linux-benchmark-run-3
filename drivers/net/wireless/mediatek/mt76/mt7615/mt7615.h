48a41d37a4ae3caadb65add66aeade6cd2f4f41f
Felix Fietkau <nbd@nbd.name>
2019-10-30 17:50:57 +0100

    mt76: mt7615: add multiple wiphy support to the rx path
    Program the RMAC CHFREQ registers to properly indicate the band that the
    frames are received on.
    Add some sanity checks to the programmed values, because the firmware
    programs these registers differently
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7615/mt7615.h
+++ drivers/net/wireless/mediatek/mt76/mt7615/mt7615.h
@@ -37,6 +37,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MT_FRAC_SCALE		12
 #define MT_FRAC(val, div)	(((val) << MT_FRAC_SCALE) / (div))
 
+#define MT_CHFREQ_VALID		BIT(7)
+#define MT_CHFREQ_DBDC_IDX	BIT(6)
+#define MT_CHFREQ_SEQ		GENMASK(5, 0)
+
 struct mt7615_vif;
 struct mt7615_sta;
 
@@ -93,8 +97,12 @@ struct mt7615_phy {
 
 	u16 chainmask;
 
+	u8 chfreq_seq;
 	u8 rdd_state;
 	int dfs_state;
+
+	__le32 rx_ampdu_ts;
+	u32 ampdu_ref;
 };
 
 struct mt7615_dev {
@@ -106,9 +114,6 @@ struct mt7615_dev {
 	struct mt7615_phy phy;
 	u32 vif_mask;
 	u32 omac_mask;
-
-	__le32 rx_ampdu_ts;
-	u32 ampdu_ref;
 
 	struct list_head sta_poll_list;
 	spinlock_t sta_poll_lock;
