0e431e113844998d133406c40a5c91b394ab1d6c
Christoph Hellwig <hch@lst.de>
2019-10-30 16:11:47 -0700

    riscv: enter WFI in default_power_off() if SBI does not shutdown
    Provide a new default fallback power off that just sits in a wfi loop
    to save some power.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Anup Patel <anup@brainfault.org>
    Reviewed-by: Atish Patra <atish.patra@wdc.com>
    [paul.walmsley@sifive.com: split the WFI fix apart from the
     nommu-related default_power_off() changes]
    Signed-off-by: Paul Walmsley <paul.walmsley@sifive.com>

--- arch/riscv/kernel/reset.c
+++ arch/riscv/kernel/reset.c
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static void default_power_off(void)
 {
 	sbi_shutdown();
-	while (1);
+	while (1)
+		wait_for_interrupt();
 }
 
 void (*pm_power_off)(void) = default_power_off;
