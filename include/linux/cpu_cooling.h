b55633b26a61e1c159f7be03cd77557f99a0a936
Daniel Lezcano <daniel.lezcano@linaro.org>
2019-10-30 10:10:36 +0100

    thermal: cpu_cooling: Remove pointless dependency on CONFIG_OF
    The option CONFIG_CPU_THERMAL depends on CONFIG_OF in the Kconfig.
    
    It it pointless to check if CONFIG_OF is set in the header file as
    this is always true if CONFIG_CPU_THERMAL is true. Remove it.
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Amit Kucheria <amit.kucheria@linaro.org>
    Link: https://lore.kernel.org/r/20191030091038.678-1-daniel.lezcano@linaro.org

--- include/linux/cpu_cooling.h
+++ include/linux/cpu_cooling.h
@@ -48,7 +48,7 @@ void cpufreq_cooling_unregister(struct thermal_cooling_device *cdev)
 }
 #endif	/* CONFIG_CPU_THERMAL */
 
-#if defined(CONFIG_THERMAL_OF) && defined(CONFIG_CPU_THERMAL)
+#ifdef CONFIG_CPU_THERMAL
 /**
  * of_cpufreq_cooling_register - create cpufreq cooling device based on DT.
  * @policy: cpufreq policy.
@@ -61,6 +61,6 @@ of_cpufreq_cooling_register(struct cpufreq_policy *policy)
 {
 	return NULL;
 }
-#endif /* defined(CONFIG_THERMAL_OF) && defined(CONFIG_CPU_THERMAL) */
+#endif /* CONFIG_CPU_THERMAL */
 
 #endif /* __CPU_COOLING_H__ */
