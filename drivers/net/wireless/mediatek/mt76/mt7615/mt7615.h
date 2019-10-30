28a99e907e5c1e21aa9d07008cf40446654bfb99
Felix Fietkau <nbd@nbd.name>
2019-10-30 14:16:28 +0100

    mt76: mt7615: rework chainmask handling
    Move chainmask to struct mt7615_phy and instead of needlessly making the
    format similar to values for older chips, make it refer to the actual chain
    bits used for the rx/tx path.
    This is important for multiple wiphy support, where for a secondary phy,
    antenna_mask will start at 0, and chainmask will start at the chain offset
    (bit 2)
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7615/mt7615.h
+++ drivers/net/wireless/mediatek/mt76/mt7615/mt7615.h
@@ -91,6 +91,8 @@ struct mt7615_phy {
 	s8 ofdm_sensitivity;
 	s8 cck_sensitivity;
 
+	u16 chainmask;
+
 	u8 rdd_state;
 	int dfs_state;
 };
@@ -102,7 +104,6 @@ struct mt7615_dev {
 	};
 
 	struct mt7615_phy phy;
-	u16 chainmask;
 	u32 vif_mask;
 	u32 omac_mask;
 
