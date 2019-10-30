747c413b3a6cf9952fd7fff5dc7db95fe4470f35
Dan Williams <dan.j.williams@intel.com>
2019-10-30 16:45:53 -0700

    libnvdimm/pmem: Delete include of nd-core.h
    The entire point of nd-core.h is to hide functionality that no leaf
    driver should touch. In fact, the commit that added it had no need to
    include it.
    
    Fixes: 06e8ccdab15f ("acpi: nfit: Add support for detect platform...")
    Cc: Ira Weiny <ira.weiny@intel.com>
    Cc: Dave Jiang <dave.jiang@intel.com>
    Cc: Vishal Verma <vishal.l.verma@intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/nvdimm/pmem.c
+++ drivers/nvdimm/pmem.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "pmem.h"
 #include "pfn.h"
 #include "nd.h"
-#include "nd-core.h"
 
 static struct device *to_dev(struct pmem_device *pmem)
 {
