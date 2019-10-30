3608969227d37d8122676d6a456398de36861e47
Yong Zhao <Yong.Zhao@amd.com>
2019-10-30 18:07:20 -0400

    drm/amdkfd: Adjust function sequences to avoid unnecessary declarations
    This is cleaner.
    
    Signed-off-by: Yong Zhao <Yong.Zhao@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdkfd/kfd_kernel_queue_v9.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_kernel_queue_v9.c
@@ -28,18 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "kfd_pm4_opcodes.h"
 
 static bool initialize_v9(struct kernel_queue *kq, struct kfd_dev *dev,
-			enum kfd_queue_type type, unsigned int queue_size);
-static void uninitialize_v9(struct kernel_queue *kq);
-static void submit_packet_v9(struct kernel_queue *kq);
-
-void kernel_queue_init_v9(struct kernel_queue_ops *ops)
-{
-	ops->initialize = initialize_v9;
-	ops->uninitialize = uninitialize_v9;
-	ops->submit_packet = submit_packet_v9;
-}
-
-static bool initialize_v9(struct kernel_queue *kq, struct kfd_dev *dev,
 			enum kfd_queue_type type, unsigned int queue_size)
 {
 	int retval;
@@ -66,6 +54,13 @@ static void submit_packet_v9(struct kernel_queue *kq)
 	*kq->wptr64_kernel = kq->pending_wptr64;
 	write_kernel_doorbell64(kq->queue->properties.doorbell_ptr,
 				kq->pending_wptr64);
+}
+
+void kernel_queue_init_v9(struct kernel_queue_ops *ops)
+{
+	ops->initialize = initialize_v9;
+	ops->uninitialize = uninitialize_v9;
+	ops->submit_packet = submit_packet_v9;
 }
 
 static int pm_map_process_v9(struct packet_manager *pm,
