03ab1bc092527388ce0f021bd5b9c8714659defd
Sven Eckelmann <sven@narfation.org>
2019-10-30 08:03:49 +0100

    batman-adv: Drop lockdep.h include for soft-interface.c
    The commit ab92d68fc22f ("net: core: add generic lockdep keys") removed
    all lockdep functionality from soft-interface.c but didn't remove the
    include for this functionality.
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

--- net/batman-adv/soft-interface.c
+++ net/batman-adv/soft-interface.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/kref.h>
 #include <linux/list.h>
-#include <linux/lockdep.h>
 #include <linux/netdevice.h>
 #include <linux/netlink.h>
 #include <linux/percpu.h>
