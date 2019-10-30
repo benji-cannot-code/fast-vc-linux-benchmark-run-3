ac456440c63e19051a49b811144a13f1b7d66f84
Linus Walleij <linus.walleij@linaro.org>
2019-10-30 08:36:24 +0100

    spi: zynq-qspi: Drop GPIO header
    This driver does not use any symbols from the legacy GPIO
    header so drop the include.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20191030073624.23974-1-linus.walleij@linaro.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-zynq-qspi.c
+++ drivers/spi/spi-zynq-qspi.c
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/clk.h>
 #include <linux/delay.h>
-#include <linux/gpio.h>
 #include <linux/interrupt.h>
 #include <linux/io.h>
 #include <linux/module.h>
