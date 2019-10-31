5b9dfd6adff072b8e3f1d3ee4cef2129488c15d4
Chenwandun <chenwandun@huawei.com>
2019-10-31 17:43:49 +0800

    drm/dp_mst: fix gcc compile error
    drivers/gpu/drm/drm_dp_mst_topology.c: In function __topology_ref_save:
    drivers/gpu/drm/drm_dp_mst_topology.c:1424:6: error: implicit declaration of function stack_trace_save; did you mean stack_depot_save? [-Werror=implicit-function-declaration]
      n = stack_trace_save(stack_entries, ARRAY_SIZE(stack_entries), 1);
          ^~~~~~~~~~~~~~~~
          stack_depot_save
    drivers/gpu/drm/drm_dp_mst_topology.c: In function __dump_topology_ref_history:
    drivers/gpu/drm/drm_dp_mst_topology.c:1513:3: error: implicit declaration of function stack_trace_snprint; did you mean acpi_trace_point? [-Werror=implicit-function-declaration]
       stack_trace_snprint(buf, PAGE_SIZE, entries, nr_entries, 4);
       ^~~~~~~~~~~~~~~~~~~
       acpi_trace_point
    
    stack_trace_save and stack_trace_snprint are declared in <linux/stacktrace.h>,
    so there is need to include it, and <linux/stackdepot.h> is already included
    by practices, so just replace <linux/stackdepot.h> by <linux/stacktrace.h>.
    
    Signed-off-by: Chenwandun <chenwandun@huawei.com>
    Reviewed-by: Lyude Paul <lyude@redhat.com>
    Acked-by: Daniel Vetter <daniel@ffwll.ch>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/1572515029-42087-1-git-send-email-chenwandun@huawei.com

--- drivers/gpu/drm/drm_dp_mst_topology.c
+++ drivers/gpu/drm/drm_dp_mst_topology.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/seq_file.h>
 
 #if IS_ENABLED(CONFIG_DRM_DEBUG_DP_MST_TOPOLOGY_REFS)
-#include <linux/stackdepot.h>
+#include <linux/stacktrace.h>
 #include <linux/sort.h>
 #include <linux/timekeeping.h>
 #include <linux/math64.h>
