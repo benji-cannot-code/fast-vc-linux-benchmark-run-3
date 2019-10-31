e1c205a245a1851da4ce8cb09f01727a8d16b5fa
Thomas Richter <tmricht@linux.ibm.com>
2019-10-31 10:38:53 +0100

    s390/cpum_sf: Assign error value to err variable
    When starting the CPU Measurement sampling facility using
    qsi() function, this function may return an error value.
    This error value is referenced in the else part of the
    if statement to dump its value in a debug statement.
    Right now this value is always zero because it has not been
    assigned a value.
    
    Signed-off-by: Thomas Richter <tmricht@linux.ibm.com>
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- arch/s390/kernel/perf_cpum_sf.c
+++ arch/s390/kernel/perf_cpum_sf.c
@@ -1008,7 +1008,8 @@ static void cpumsf_pmu_disable(struct pmu *pmu)
 	}
 
 	/* Save state of TEAR and DEAR register contents */
-	if (!qsi(&si)) {
+	err = qsi(&si);
+	if (!err) {
 		/* TEAR/DEAR values are valid only if the sampling facility is
 		 * enabled.  Note that cpumsf_pmu_disable() might be called even
 		 * for a disabled sampling facility because cpumsf_pmu_enable()
