b20b12ed6b55146624ce20ae99e081225ff760b7
Sven Eckelmann <sven@narfation.org>
2019-10-31 17:34:37 +0100

    batman-adv: Use 'fallthrough' pseudo keyword
    The usage of the '/* fall through */' comments in switches are no longer
    marked as non-deprecated variant of implicit fall throughs for switch
    statements. The commit 294f69e662d1 ("compiler_attributes.h: Add
    'fallthrough' pseudo keyword for switch/case use") introduced a replacement
    keyword which should be used instead.
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

--- net/batman-adv/soft-interface.c
+++ net/batman-adv/soft-interface.c
@@ -231,7 +231,7 @@ static netdev_tx_t batadv_interface_tx(struct sk_buff *skb,
 			break;
 		}
 
-		/* fall through */
+		fallthrough;
 	case ETH_P_BATMAN:
 		goto dropped;
 	}
@@ -456,7 +456,7 @@ void batadv_interface_rx(struct net_device *soft_iface,
 		if (vhdr->h_vlan_encapsulated_proto != htons(ETH_P_BATMAN))
 			break;
 
-		/* fall through */
+		fallthrough;
 	case ETH_P_BATMAN:
 		goto dropped;
 	}
