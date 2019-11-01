fcc3d449068e8527501713b82b695bb920ad40a9
Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
2019-11-01 12:41:13 +0200

    drm/i915: Update DRIVER_DATE to 20191101
    Signed-off-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -112,7 +112,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRIVER_NAME		"i915"
 #define DRIVER_DESC		"Intel Graphics"
 #define DRIVER_DATE		"20191101"
-#define DRIVER_TIMESTAMP	1572591869
+#define DRIVER_TIMESTAMP	1572604873
 
 struct drm_i915_gem_object;
 
