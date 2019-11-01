c7d4ff3a60b8858ac4cdfdd661b72cf92f387d47
Yan-Hsuan Chuang <yhchuang@realtek.com>
2019-11-01 13:19:42 +0800

    rtw88: raise LPS threshold to 50, for less power consumption
    The LPS threshold was set to 2, means driver will leave LPS
    mode if there is more than 2 frames TX/RX for every 2 seconds.
    This makes driver enter/leave LPS frequently even if we just
    "ping -i1" to the others.
    
    Apparently we do not want to leave LPS mode if there is only
    some background traffics or web surfing. By experiment, set
    this to 50 is a more reasonable value to lower the over all
    power consumption.
    
    Signed-off-by: Yan-Hsuan Chuang <yhchuang@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtw88/ps.h
+++ drivers/net/wireless/realtek/rtw88/ps.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __RTW_PS_H_
 #define __RTW_PS_H_
 
-#define RTW_LPS_THRESHOLD	2
+#define RTW_LPS_THRESHOLD	50
 
 #define POWER_MODE_ACK		BIT(6)
 #define POWER_MODE_PG		BIT(4)
