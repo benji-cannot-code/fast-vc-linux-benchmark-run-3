4da0066b657245e00f974d1817f556e97aba041f
Ard Biesheuvel <ardb@kernel.org>
2019-11-05 16:01:02 +0200

    crypto: omap-aes-gcm - deal with memory allocation failure
    The OMAP gcm(aes) driver invokes omap_crypto_align_sg() without
    dealing with the errors it may return, resulting in a crash if
    the routine fails in a __get_free_pages(GFP_ATOMIC) call. So
    bail and return the error rather than limping on if one occurs.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Reviewed-by: Tero Kristo <t-kristo@ti.com>
    Tested-by: Tero Kristo <t-kristo@ti.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/omap-aes-gcm.c
+++ drivers/crypto/omap-aes-gcm.c
@@ -121,6 +121,8 @@ static int omap_aes_gcm_copy_buffers(struct omap_aes_dev *dd,
 					   OMAP_CRYPTO_FORCE_SINGLE_ENTRY,
 					   FLAGS_ASSOC_DATA_ST_SHIFT,
 					   &dd->flags);
+		if (ret)
+			return ret;
 	}
 
 	if (cryptlen) {
@@ -133,6 +135,8 @@ static int omap_aes_gcm_copy_buffers(struct omap_aes_dev *dd,
 					   OMAP_CRYPTO_FORCE_SINGLE_ENTRY,
 					   FLAGS_IN_DATA_ST_SHIFT,
 					   &dd->flags);
+		if (ret)
+			return ret;
 	}
 
 	dd->in_sg = dd->in_sgl;
