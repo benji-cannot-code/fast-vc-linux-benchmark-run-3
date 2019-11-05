6e4226c85c1e96af1d8919dd8e6015eba0a5722a
Javier F. Arias <jarias.linux@gmail.com>
2019-11-05 14:13:48 -0500

    staging: rtl8723bs: Fix line over 80 characters
    Fix line over 80 characters by wrapping arguments in function call.
    
    Suggested-by: Julia Lawall <julia.lawall@lip6.fr>.
    Signed-off-by: Javier F. Arias <jarias.linux@gmail.com>
    --
    Changes in V4:
    	- Changed the number of arguments before wrapping to make
    	  the code more readable.
    Changes in V3:
            - Edit the commit message to properly use the Suggested-by
              tag.
    Changes in V2:
            - Edit the commit message to use the Suggested-by tag.
    
     drivers/staging/rtl8723bs/hal/rtl8723b_dm.c | 3 ++-
     1 file changed, 2 insertions(+), 1 deletion(-)
    
    Link: https://lore.kernel.org/r/61967dc169db6d343b9183361cd6c1ad7ad149fd.1572640293.git.jarias.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/rtl8723b_dm.c
+++ drivers/staging/rtl8723bs/hal/rtl8723b_dm.c
@@ -189,7 +189,8 @@ void rtl8723b_HalDmWatchDog(struct adapter *Adapter)
 
 		bBtDisabled = hal_btcoex_IsBtDisabled(Adapter);
 
-		ODM_CmnInfoUpdate(&pHalData->odmpriv, ODM_CMNINFO_BT_ENABLED, ((bBtDisabled == true)?false:true));
+		ODM_CmnInfoUpdate(&pHalData->odmpriv, ODM_CMNINFO_BT_ENABLED,
+				  ((bBtDisabled == true)?false:true));
 
 		ODM_DMWatchdog(&pHalData->odmpriv);
 	}
