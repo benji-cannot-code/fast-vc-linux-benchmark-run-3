d37782e21ec57c86fbf5ab493d604ffafc90276f
Akeem G Abodunrin <akeem.g.abodunrin@intel.com>
2019-11-06 02:05:33 -0800

    ice: Check if VF is disabled for Opcode and other operations
    This patch adds code to check if PF or VF is disabled before honoring
    mailbox message to configure VF - If it is disabled, and opcode is for
    resetting VF, the PF driver simply tell VF that all is set. In addition,
    if reset is ongoing, and Admin intend to configure VF on the host, we can
    poll the VF enabling bit to make sure it is ready before continue - If
    after ~250 milliseconds, VF is not in active state, we can bail out with
    invalid error.
    
    Signed-off-by: Akeem G Abodunrin <akeem.g.abodunrin@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/ice/ice_virtchnl_pf.h
+++ drivers/net/ethernet/intel/ice/ice_virtchnl_pf.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ICE_MAX_POLICY_INTR_PER_VF	33
 #define ICE_MIN_INTR_PER_VF		(ICE_MIN_QS_PER_VF + 1)
 #define ICE_DFLT_INTR_PER_VF		(ICE_DFLT_QS_PER_VF + 1)
+#define ICE_MAX_VF_RESET_WAIT		15
 
 /* Specific VF states */
 enum ice_vf_states {
