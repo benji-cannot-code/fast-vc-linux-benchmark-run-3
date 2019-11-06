5bfccda5e7f2f830009c16f60d6d0b182ee5afaf
Luca Coelho <luciano.coelho@intel.com>
2019-11-06 09:27:51 +0200

    iwlwifi: add support for version 2 of SOC_CONFIGURATION_CMD
    This new command is mostly backwards compatible, with the exception
    that the device_type element was changed into a bitmask.  The device
    type bit remains the same (because we only had 0 and 1 anyway), but
    when using v1 we can't set any other bits, because that would change
    the integer.
    
    Other than that, the struct remains the same and the driver can set
    the device_type bit in both cases, but it can only set the low_latency
    bit if VER_2 is used.
    
    Change-Id: Ib68d4c821ebcce253b42ed0ea15881fb4e3e01da
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/iwl-config.h
+++ drivers/net/wireless/intel/iwlwifi/iwl-config.h
@@ -304,6 +304,7 @@ struct iwl_pwr_tx_backoff {
  * @gen2: 22000 and on transport operation
  * @mq_rx_supported: multi-queue rx support
  * @integrated: discrete or integrated
+ * @low_latency_xtal: use the low latency xtal if supported
  */
 struct iwl_cfg_trans_params {
 	const struct iwl_base_params *base_params;
@@ -316,6 +317,7 @@ struct iwl_cfg_trans_params {
 	    gen2:1,
 	    mq_rx_supported:1,
 	    integrated:1,
+	    low_latency_xtal:1,
 	    bisr_workaround:1;
 };
 
