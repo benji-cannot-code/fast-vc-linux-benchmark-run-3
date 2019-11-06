01f695914ee8f7a1addfdca40f729c6b4a9884e1
Jacob Keller <jacob.e.keller@intel.com>
2019-11-06 02:05:40 -0800

    ice: use more accurate ICE_DBG mask types
    ice_debug_cq is passed a mask which is always ICE_DBG_AQ_CMD. Modify this
    function, removing the mask parameter entirely, and directly use the more
    appropriate ICE_DBG_AQ_DESC and ICE_DBG_AQ_DESC_BUF.
    
    The function is only called from ice_controlq.c, and has no
    other callers outside of that file. Move it and mark it static to avoid
    namespace pollution.
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/ice/ice_common.h
+++ drivers/net/ethernet/intel/ice/ice_common.h
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 enum ice_status ice_nvm_validate_checksum(struct ice_hw *hw);
 
-void
-ice_debug_cq(struct ice_hw *hw, u32 mask, void *desc, void *buf, u16 buf_len);
 enum ice_status ice_init_hw(struct ice_hw *hw);
 void ice_deinit_hw(struct ice_hw *hw);
 enum ice_status
