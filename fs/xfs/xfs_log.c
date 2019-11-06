b5f72c7f1ee9cbb7017bf4229cecdd608fd8b39e
Darrick J. Wong <darrick.wong@oracle.com>
2019-11-06 08:41:20 -0800

    xfs: annotate functions that trip static checker locking checks
    Add some lock annotations to helper functions that seem to have
    unbalanced locking that confuses the static analyzers.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/xfs_log.c
+++ fs/xfs/xfs_log.c
@@ -2809,6 +2809,8 @@ xlog_state_do_iclog_callbacks(
 	struct xlog		*log,
 	struct xlog_in_core	*iclog,
 	bool			aborted)
+		__releases(&log->l_icloglock)
+		__acquires(&log->l_icloglock)
 {
 	spin_unlock(&log->l_icloglock);
 	spin_lock(&iclog->ic_callback_lock);
