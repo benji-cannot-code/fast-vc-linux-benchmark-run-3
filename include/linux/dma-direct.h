8415aca27c2c95276a534f24eb2a974d895eb7eb
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2019-11-07 16:06:44 +0100

    dma-direct: check for overflows on 32 bit DMA addresses
    As seen on the new Raspberry Pi 4 and sta2x11's DMA implementation it is
    possible for a device configured with 32 bit DMA addresses and a partial
    DMA mapping located at the end of the address space to overflow. It
    happens when a higher physical address, not DMAable, is translated to
    it's DMA counterpart.
    
    For example the Raspberry Pi 4, configurable up to 4 GB of memory, has
    an interconnect capable of addressing the lower 1 GB of physical memory
    with a DMA offset of 0xc0000000. It transpires that, any attempt to
    translate physical addresses higher than the first GB will result in an
    overflow which dma_capable() can't detect as it only checks for
    addresses bigger then the maximum allowed DMA address.
    
    Fix this by verifying in dma_capable() if the DMA address range provided
    is at any point lower than the minimum possible DMA address on the bus.
    
    Signed-off-by: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- include/linux/dma-direct.h
+++ include/linux/dma-direct.h
@@ -4,7 +4,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_DMA_DIRECT_H 1
 
 #include <linux/dma-mapping.h>
+#include <linux/memblock.h> /* for min_low_pfn */
 #include <linux/mem_encrypt.h>
+
+static inline dma_addr_t phys_to_dma(struct device *dev, phys_addr_t paddr);
 
 #ifdef CONFIG_ARCH_HAS_PHYS_TO_DMA
 #include <asm/dma-direct.h>
@@ -25,11 +28,16 @@ static inline phys_addr_t __dma_to_phys(struct device *dev, dma_addr_t dev_addr)
 
 static inline bool dma_capable(struct device *dev, dma_addr_t addr, size_t size)
 {
+	dma_addr_t end = addr + size - 1;
+
 	if (!dev->dma_mask)
 		return false;
 
-	return addr + size - 1 <=
-		min_not_zero(*dev->dma_mask, dev->bus_dma_mask);
+	if (!IS_ENABLED(CONFIG_ARCH_DMA_ADDR_T_64BIT) &&
+	    min(addr, end) < phys_to_dma(dev, PFN_PHYS(min_low_pfn)))
+		return false;
+
+	return end <= min_not_zero(*dev->dma_mask, dev->bus_dma_mask);
 }
 #endif /* !CONFIG_ARCH_HAS_PHYS_TO_DMA */
 
