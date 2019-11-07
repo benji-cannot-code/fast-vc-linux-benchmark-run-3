b1f411ff3b6376cde9fc48f68cc79ac6d8d05971
Dan Williams <dan.j.williams@intel.com>
2019-11-06 17:43:43 -0800

    device-dax: Add a driver for "hmem" devices
    Platform firmware like EFI/ACPI may publish "hmem" platform devices.
    Such a device is a performance differentiated memory range likely
    reserved for an application specific use case. The driver gives access
    to 100% of the capacity via a device-dax mmap instance by default.
    
    However, if over-subscription and other kernel memory management is
    desired the resulting dax device can be assigned to the core-mm via the
    kmem driver.
    
    This consumes "hmem" devices the producer of "hmem" devices is saved for
    a follow-on patch so that it can reference the new CONFIG_DEV_DAX_HMEM
    symbol to gate performing the enumeration work.
    
    Reported-by: kbuild test robot <lkp@intel.com>
    Reviewed-by: Dave Hansen <dave.hansen@linux.intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/linux/memregion.h
+++ include/linux/memregion.h
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/errno.h>
 
+struct memregion_info {
+	int target_node;
+};
+
 #ifdef CONFIG_MEMREGION
 int memregion_alloc(gfp_t gfp);
 void memregion_free(int id);
