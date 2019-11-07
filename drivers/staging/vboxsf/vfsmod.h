f022054293b7c34ba0a6e1b2c24a928432b4f751
YueHaibing <yuehaibing@huawei.com>
2019-11-07 01:59:23 +0000

    staging: vboxsf: Remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Link: https://lore.kernel.org/r/20191107015923.100013-1-yuehaibing@huawei.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vboxsf/vfsmod.h
+++ drivers/staging/vboxsf/vfsmod.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/backing-dev.h>
 #include <linux/idr.h>
-#include <linux/version.h>
 #include "shfl_hostintf.h"
 
 #define DIR_BUFFER_SIZE SZ_16K
