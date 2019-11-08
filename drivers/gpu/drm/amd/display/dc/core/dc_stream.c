d649df0bce062469307471f58f2c6a60f4fe00e2
Colin Ian King <colin.king@canonical.com>
2019-11-08 16:29:45 +0000

    drm/amd/display: remove redundant variable status
    Variable status is redundant, it is being initialized with a value
    that is over-written later and this is being returned immediately
    after the assignment.  Clean up the code by removing status and
    just returning the value returned from the call to function
    dc->hwss.dmdata_status_done.
    
    Addresses-Coverity: ("Unused value")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_stream.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_stream.c
@@ -566,7 +566,6 @@ bool dc_stream_get_scanoutpos(const struct dc_stream_state *stream,
 
 bool dc_stream_dmdata_status_done(struct dc *dc, struct dc_stream_state *stream)
 {
-	bool status = true;
 	struct pipe_ctx *pipe = NULL;
 	int i;
 
@@ -582,8 +581,7 @@ bool dc_stream_dmdata_status_done(struct dc *dc, struct dc_stream_state *stream)
 	if (i == MAX_PIPES)
 		return true;
 
-	status = dc->hwss.dmdata_status_done(pipe);
-	return status;
+	return dc->hwss.dmdata_status_done(pipe);
 }
 
 bool dc_stream_set_dynamic_metadata(struct dc *dc,
