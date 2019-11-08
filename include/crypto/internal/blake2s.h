1fc74c4410ebb06ce1b4984fedcd7314c9ea6ea8
Ard Biesheuvel <ardb@kernel.org>
2019-11-08 13:22:30 +0100

    crypto: blake2s - implement generic shash driver
    Wire up our newly added Blake2s implementation via the shash API.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/internal/blake2s.h
+++ include/crypto/internal/blake2s.h
@@ -6,6 +6,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <crypto/blake2s.h>
 
+struct blake2s_tfm_ctx {
+	u8 key[BLAKE2S_KEY_SIZE];
+	unsigned int keylen;
+};
+
 void blake2s_compress_generic(struct blake2s_state *state,const u8 *block,
 			      size_t nblocks, const u32 inc);
 
