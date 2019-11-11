cde3adaa3a1da997399b8745459dc01511970611
Hans de Goede <hdegoede@redhat.com>
2019-11-11 12:38:45 +0100

    staging: rtl8723bs: Add 024c:0525 to the list of SDIO device-ids
    Add 024c:0525 to the list of SDIO device-ids, based on a patch found
    in the Android X86 kernels. According to that patch this device id is
    used on the Alcatel Plus 10 device.
    
    Reported-and-tested-by: youling257 <youling257@gmail.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Cc: stable <stable@vger.kernel.org>
    Link: https://lore.kernel.org/r/20191111113846.24940-1-hdegoede@redhat.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/os_dep/sdio_intf.c
+++ drivers/staging/rtl8723bs/os_dep/sdio_intf.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static const struct sdio_device_id sdio_ids[] =
 {
 	{ SDIO_DEVICE(0x024c, 0x0523), },
+	{ SDIO_DEVICE(0x024c, 0x0525), },
 	{ SDIO_DEVICE(0x024c, 0x0623), },
 	{ SDIO_DEVICE(0x024c, 0x0626), },
 	{ SDIO_DEVICE(0x024c, 0xb723), },
