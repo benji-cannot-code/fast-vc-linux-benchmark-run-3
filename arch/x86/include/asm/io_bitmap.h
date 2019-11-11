0fd2d64e6ea00d977a3571e48eb3e5a68dcde03b
Thomas Gleixner <tglx@linutronix.de>
2019-11-11 23:03:25 +0100

    x86/ioperm: Share I/O bitmap if identical
    The I/O bitmap is duplicated on fork. That's wasting memory and slows down
    fork. There is no point to do so. As long as the bitmap is not modified it
    can be shared between threads and processes.
    
    Add a refcount and just share it on fork. If a task modifies the bitmap
    then it has to do the duplication if and only if it is shared.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Andy Lutomirski <luto@kernel.org>

--- arch/x86/include/asm/io_bitmap.h
+++ arch/x86/include/asm/io_bitmap.h
@@ -3,15 +3,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_X86_IOBITMAP_H
 #define _ASM_X86_IOBITMAP_H
 
+#include <linux/refcount.h>
 #include <asm/processor.h>
 
 struct io_bitmap {
 	u64		sequence;
+	refcount_t	refcnt;
 	/* The maximum number of bytes to copy so all zero bits are covered */
 	unsigned int	max;
 	unsigned long	bitmap[IO_BITMAP_LONGS];
 };
 
+struct task_struct;
+
+void io_bitmap_share(struct task_struct *tsk);
 void io_bitmap_exit(void);
 
 void tss_update_io_bitmap(void);
