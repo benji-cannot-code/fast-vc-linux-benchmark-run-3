4e9479be83448af08f7c33170e76ab7a8690eb25
Vladimir Oltean <olteanv@gmail.com>
2019-11-12 02:11:54 +0200

    net: dsa: sja1105: Implement state machine for TAS with PTP clock source
    Tested using the following bash script and the tc from iproute2-next:
    
    	#!/bin/bash
    
    	set -e -u -o pipefail
    
    	NSEC_PER_SEC="1000000000"
    
    	gatemask() {
    		local tc_list="$1"
    		local mask=0
    
    		for tc in ${tc_list}; do
    			mask=$((${mask} | (1 << ${tc})))
    		done
    
    		printf "%02x" ${mask}
    	}
    
    	if ! systemctl is-active --quiet ptp4l; then
    		echo "Please start the ptp4l service"
    		exit
    	fi
    
    	now=$(phc_ctl /dev/ptp1 get | gawk '/clock time is/ { print $5; }')
    	# Phase-align the base time to the start of the next second.
    	sec=$(echo "${now}" | gawk -F. '{ print $1; }')
    	base_time="$(((${sec} + 1) * ${NSEC_PER_SEC}))"
    
    	tc qdisc add dev swp5 parent root handle 100 taprio \
    		num_tc 8 \
    		map 0 1 2 3 5 6 7 \
    		queues 1@0 1@1 1@2 1@3 1@4 1@5 1@6 1@7 \
    		base-time ${base_time} \
    		sched-entry S $(gatemask 7) 100000 \
    		sched-entry S $(gatemask "0 1 2 3 4 5 6") 400000 \
    		clockid CLOCK_TAI flags 2
    
    The "state machine" is a workqueue invoked after each manipulation
    command on the PTP clock (reset, adjust time, set time, adjust
    frequency) which checks over the state of the time-aware scheduler.
    So it is not monitored periodically, only in reaction to a PTP command
    typically triggered from a userspace daemon (linuxptp). Otherwise there
    is no reason for things to go wrong.
    
    Now that the timecounter/cyclecounter has been replaced with hardware
    operations on the PTP clock, the TAS Kconfig now depends upon PTP and
    the standalone clocksource operating mode has been removed.
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/sja1105/sja1105_tas.h
+++ drivers/net/dsa/sja1105/sja1105_tas.h
@@ -9,8 +9,27 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #if IS_ENABLED(CONFIG_NET_DSA_SJA1105_TAS)
 
+enum sja1105_tas_state {
+	SJA1105_TAS_STATE_DISABLED,
+	SJA1105_TAS_STATE_ENABLED_NOT_RUNNING,
+	SJA1105_TAS_STATE_RUNNING,
+};
+
+enum sja1105_ptp_op {
+	SJA1105_PTP_NONE,
+	SJA1105_PTP_CLOCKSTEP,
+	SJA1105_PTP_ADJUSTFREQ,
+};
+
 struct sja1105_tas_data {
 	struct tc_taprio_qopt_offload *offload[SJA1105_NUM_PORTS];
+	enum sja1105_tas_state state;
+	enum sja1105_ptp_op last_op;
+	struct work_struct tas_work;
+	s64 earliest_base_time;
+	s64 oper_base_time;
+	u64 max_cycle_time;
+	bool enabled;
 };
 
 int sja1105_setup_tc_taprio(struct dsa_switch *ds, int port,
@@ -19,6 +38,10 @@ int sja1105_setup_tc_taprio(struct dsa_switch *ds, int port,
 void sja1105_tas_setup(struct dsa_switch *ds);
 
 void sja1105_tas_teardown(struct dsa_switch *ds);
+
+void sja1105_tas_clockstep(struct dsa_switch *ds);
+
+void sja1105_tas_adjfreq(struct dsa_switch *ds);
 
 #else
 
@@ -36,6 +59,10 @@ static inline int sja1105_setup_tc_taprio(struct dsa_switch *ds, int port,
 static inline void sja1105_tas_setup(struct dsa_switch *ds) { }
 
 static inline void sja1105_tas_teardown(struct dsa_switch *ds) { }
+
+static inline void sja1105_tas_clockstep(struct dsa_switch *ds) { }
+
+static inline void sja1105_tas_adjfreq(struct dsa_switch *ds) { }
 
 #endif /* IS_ENABLED(CONFIG_NET_DSA_SJA1105_TAS) */
 
