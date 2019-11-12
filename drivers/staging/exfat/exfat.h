f2051aa019f5de4e81f1ae2ec2bb52270a975055
Valdis Kletnieks <valdis.kletnieks@vt.edu>
2019-11-11 21:09:49 -0500

    staging: exfat: Clean up return codes - FFS_FORMATERR
    Convert FFS_FORMATERR to -EFSCORRUPTED
    
    Signed-off-by: Valdis Kletnieks <Valdis.Kletnieks@vt.edu>
    Link: https://lore.kernel.org/r/20191112021000.42091-2-Valdis.Kletnieks@vt.edu
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/exfat/exfat.h
+++ drivers/staging/exfat/exfat.h
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #undef DEBUG
 #endif
 
+#define EFSCORRUPTED	EUCLEAN		/* Filesystem is corrupted */
+
 #define DENTRY_SIZE		32	/* dir entry size */
 #define DENTRY_SIZE_BITS	5
 
@@ -210,7 +212,6 @@ static inline u16 get_row_index(u16 i)
 /* return values */
 #define FFS_SUCCESS             0
 #define FFS_MEDIAERR            1
-#define FFS_FORMATERR           2
 #define FFS_MOUNTED             3
 #define FFS_NOTMOUNTED          4
 #define FFS_ALIGNMENTERR        5
