3287bbc858ab64469d6751c63db1abecb49d8e92
Dmitry Safonov <dima@arista.com>
2019-11-12 01:27:10 +0000

    x86/vdso: Provide vdso_data offset on vvar_page
    VDSO support for time namespaces needs to set up a page with the same
    layout as VVAR. That timens page will be placed on position of VVAR page
    inside namespace. That page has vdso_data->seq set to 1 to enforce
    the slow path and vdso_data->clock_mode set to VCLOCK_TIMENS to enforce
    the time namespace handling path.
    
    To prepare the time namespace page the kernel needs to know the vdso_data
    offset.  Provide arch_get_vdso_data() helper for locating vdso_data on VVAR
    page.
    
    Co-developed-by: Andrei Vagin <avagin@openvz.org>
    Signed-off-by: Andrei Vagin <avagin@openvz.org>
    Signed-off-by: Dmitry Safonov <dima@arista.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20191112012724.250792-22-dima@arista.com

--- arch/x86/include/asm/vvar.h
+++ arch/x86/include/asm/vvar.h
@@ -20,10 +20,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_X86_VVAR_H
 #define _ASM_X86_VVAR_H
 
-#if defined(__VVAR_KERNEL_LDS)
-
-/* The kernel linker script defines its own magic to put vvars in the
- * right place.
+#ifdef EMIT_VVAR
+/*
+ * EMIT_VVAR() is used by the kernel linker script to put vvars in the
+ * right place. Also, it's used by kernel code to import offsets values.
  */
 #define DECLARE_VVAR(offset, type, name) \
 	EMIT_VVAR(name, offset)
