ace943961ad83f6ba48ea9b22017a9789a8a4f76
Thomas Hellstrom <thellstrom@vmware.com>
2019-11-13 19:02:42 +0100

    drm/ttm, drm/vmwgfx: Use a configuration option for the TTM dma page pool
    Drivers like vmwgfx may want to test whether the dma page pool is present
    or not. Since it's activated by default by TTM if compiled-in, define a
    hidden configuration option that the driver can test for.
    
    Cc: Christian König <christian.koenig@amd.com>
    Signed-off-by: Thomas Hellstrom <thellstrom@vmware.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/ttm/ttm_page_alloc_dma.c
+++ drivers/gpu/drm/ttm/ttm_page_alloc_dma.c
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *   when freed).
  */
 
-#if defined(CONFIG_SWIOTLB) || defined(CONFIG_INTEL_IOMMU)
 #define pr_fmt(fmt) "[TTM] " fmt
 
 #include <linux/dma-mapping.h>
@@ -1239,5 +1238,3 @@ int ttm_dma_page_alloc_debugfs(struct seq_file *m, void *data)
 	return 0;
 }
 EXPORT_SYMBOL_GPL(ttm_dma_page_alloc_debugfs);
-
-#endif
