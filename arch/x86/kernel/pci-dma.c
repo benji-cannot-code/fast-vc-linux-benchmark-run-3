b6a92d94418e4a686da7b671a6224e5f598a7180
Christoph Hellwig <hch@lst.de>
2019-11-13 08:18:34 +0100

    x86: Remove the calgary IOMMU driver
    The calgary IOMMU was only used on high-end IBM systems in the early
    x86_64 age and has no known users left.  Remove it to avoid having to
    touch it for pending changes to the DMA API.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/20191113071836.21041-2-hch@lst.de

--- arch/x86/kernel/pci-dma.c
+++ arch/x86/kernel/pci-dma.c
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/dma.h>
 #include <asm/iommu.h>
 #include <asm/gart.h>
-#include <asm/calgary.h>
 #include <asm/x86_init.h>
 #include <asm/iommu_table.h>
 
@@ -112,11 +111,6 @@ static __init int iommu_setup(char *p)
 			iommu_set_default_translated(true);
 
 		gart_parse_options(p);
-
-#ifdef CONFIG_CALGARY_IOMMU
-		if (!strncmp(p, "calgary", 7))
-			use_calgary = 1;
-#endif /* CONFIG_CALGARY_IOMMU */
 
 		p += strcspn(p, ",");
 		if (*p == ',')
