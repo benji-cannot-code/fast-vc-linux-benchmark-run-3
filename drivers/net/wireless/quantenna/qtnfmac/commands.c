aaeaccc34e0c6abe4fa2747d151a1aa6b7eac766
Sergey Matyukevich <sergey.matyukevich.os@quantenna.com>
2019-11-13 11:06:47 +0000

    qtnfmac: fix using skb after free
    KASAN reported use-after-free error:
    
    [  995.220767] BUG: KASAN: use-after-free in qtnf_cmd_send_with_reply+0x169/0x3e0 [qtnfmac]
    [  995.221098] Read of size 2 at addr ffff888213d1ded0 by task kworker/1:1/71
    
    The issue in qtnf_cmd_send_with_reply impacts all the commands that do
    not need response other then return code. For such commands, consume_skb
    is used for response skb and right after that return code in response
    skb is accessed.
    
    Signed-off-by: Sergey Matyukevich <sergey.matyukevich.os@quantenna.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/quantenna/qtnfmac/commands.c
+++ drivers/net/wireless/quantenna/qtnfmac/commands.c
@@ -84,6 +84,7 @@ static int qtnf_cmd_send_with_reply(struct qtnf_bus *bus,
 	struct qlink_cmd *cmd;
 	struct qlink_resp *resp = NULL;
 	struct sk_buff *resp_skb = NULL;
+	int resp_res = 0;
 	u16 cmd_id;
 	u8 mac_id;
 	u8 vif_id;
@@ -114,6 +115,7 @@ static int qtnf_cmd_send_with_reply(struct qtnf_bus *bus,
 	}
 
 	resp = (struct qlink_resp *)resp_skb->data;
+	resp_res = le16_to_cpu(resp->result);
 	ret = qtnf_cmd_check_reply_header(resp, cmd_id, mac_id, vif_id,
 					  const_resp_size);
 	if (ret)
@@ -129,8 +131,8 @@ out:
 	else
 		consume_skb(resp_skb);
 
-	if (!ret && resp)
-		return qtnf_cmd_resp_result_decode(le16_to_cpu(resp->result));
+	if (!ret)
+		return qtnf_cmd_resp_result_decode(resp_res);
 
 	pr_warn("VIF%u.%u: cmd 0x%.4X failed: %d\n",
 		mac_id, vif_id, cmd_id, ret);
