0f1bc116abca53c2274af7c210d90bbd8121b980
Julian Wiedmann <jwi@linux.ibm.com>
2019-11-14 11:19:24 +0100

    s390/qeth: don't check drvdata in sysfs code
    Given the way how the sysfs attributes are registered / unregistered,
    the show/store helpers will never be called with a NULL drvdata.
    
    Signed-off-by: Julian Wiedmann <jwi@linux.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/s390/net/qeth_l2_sys.c
+++ drivers/s390/net/qeth_l2_sys.c
@@ -19,9 +19,6 @@ static ssize_t qeth_bridge_port_role_state_show(struct device *dev,
 	int rc = 0;
 	char *word;
 
-	if (!card)
-		return -EINVAL;
-
 	if (qeth_l2_vnicc_is_in_use(card))
 		return sprintf(buf, "n/a (VNIC characteristics)\n");
 
@@ -80,8 +77,6 @@ static ssize_t qeth_bridge_port_role_store(struct device *dev,
 	int rc = 0;
 	enum qeth_sbp_roles role;
 
-	if (!card)
-		return -EINVAL;
 	if (sysfs_streq(buf, "primary"))
 		role = QETH_SBP_ROLE_PRIMARY;
 	else if (sysfs_streq(buf, "secondary"))
@@ -133,9 +128,6 @@ static ssize_t qeth_bridgeport_hostnotification_show(struct device *dev,
 	struct qeth_card *card = dev_get_drvdata(dev);
 	int enabled;
 
-	if (!card)
-		return -EINVAL;
-
 	if (qeth_l2_vnicc_is_in_use(card))
 		return sprintf(buf, "n/a (VNIC characteristics)\n");
 
@@ -150,9 +142,6 @@ static ssize_t qeth_bridgeport_hostnotification_store(struct device *dev,
 	struct qeth_card *card = dev_get_drvdata(dev);
 	bool enable;
 	int rc;
-
-	if (!card)
-		return -EINVAL;
 
 	rc = kstrtobool(buf, &enable);
 	if (rc)
@@ -184,9 +173,6 @@ static ssize_t qeth_bridgeport_reflect_show(struct device *dev,
 	struct qeth_card *card = dev_get_drvdata(dev);
 	char *state;
 
-	if (!card)
-		return -EINVAL;
-
 	if (qeth_l2_vnicc_is_in_use(card))
 		return sprintf(buf, "n/a (VNIC characteristics)\n");
 
@@ -207,9 +193,6 @@ static ssize_t qeth_bridgeport_reflect_store(struct device *dev,
 	struct qeth_card *card = dev_get_drvdata(dev);
 	int enable, primary;
 	int rc = 0;
-
-	if (!card)
-		return -EINVAL;
 
 	if (sysfs_streq(buf, "none")) {
 		enable = 0;
@@ -316,9 +299,6 @@ static ssize_t qeth_vnicc_timeout_show(struct device *dev,
 	u32 timeout;
 	int rc;
 
-	if (!card)
-		return -EINVAL;
-
 	rc = qeth_l2_vnicc_get_timeout(card, &timeout);
 	if (rc == -EBUSY)
 		return sprintf(buf, "n/a (BridgePort)\n");
@@ -335,9 +315,6 @@ static ssize_t qeth_vnicc_timeout_store(struct device *dev,
 	struct qeth_card *card = dev_get_drvdata(dev);
 	u32 timeout;
 	int rc;
-
-	if (!card)
-		return -EINVAL;
 
 	rc = kstrtou32(buf, 10, &timeout);
 	if (rc)
@@ -358,9 +335,6 @@ static ssize_t qeth_vnicc_char_show(struct device *dev,
 	u32 vnicc;
 	int rc;
 
-	if (!card)
-		return -EINVAL;
-
 	vnicc = qeth_l2_vnicc_sysfs_attr_to_char(attr->attr.name);
 	rc = qeth_l2_vnicc_get_state(card, vnicc, &state);
 
@@ -380,9 +354,6 @@ static ssize_t qeth_vnicc_char_store(struct device *dev,
 	bool state;
 	u32 vnicc;
 	int rc;
-
-	if (!card)
-		return -EINVAL;
 
 	if (kstrtobool(buf, &state))
 		return -EINVAL;
