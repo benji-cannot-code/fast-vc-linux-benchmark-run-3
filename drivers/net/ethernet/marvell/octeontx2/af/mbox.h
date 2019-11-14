bdcce765f1547ca285923a3695862183a0c060a5
Geetha sowjanya <gakula@marvell.com>
2019-11-14 10:56:24 +0530

    octeontx2-af: Sync hw mbox with bounce buffer.
    If mailbox client has a bounce buffer or a intermediate buffer where
    mbox messages are framed then copy them from there to HW buffer.
    If 'mbase' and 'hw_mbase' are not same then assume 'mbase' points to
    bounce buffer.
    
    This patch also adds msg_size field to mbox header to copy only valid
    data instead of whole buffer.
    
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/octeontx2/af/mbox.h
+++ drivers/net/ethernet/marvell/octeontx2/af/mbox.h
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define INTR_MASK(pfvfs) ((pfvfs < 64) ? (BIT_ULL(pfvfs) - 1) : (~0ull))
 
-#define MBOX_RSP_TIMEOUT	1000 /* in ms, Time to wait for mbox response */
+#define MBOX_RSP_TIMEOUT	2000 /* Time(ms) to wait for mbox response */
 
 #define MBOX_MSG_ALIGN		16  /* Align mbox msg start to 16bytes */
 
@@ -76,6 +76,7 @@ struct otx2_mbox {
 
 /* Header which preceeds all mbox messages */
 struct mbox_hdr {
+	u64 msg_size;	/* Total msgs size embedded */
 	u16  num_msgs;   /* No of msgs embedded */
 };
 
