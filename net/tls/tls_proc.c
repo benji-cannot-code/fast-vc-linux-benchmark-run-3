b2fe489259b4de17e97bed9dde641c790c6678b4
YueHaibing <yuehaibing@huawei.com>
2019-11-14 15:39:46 +0800

    net/tls: Fix unused function warning
    If PROC_FS is not set, gcc warning this:
    
    net/tls/tls_proc.c:23:12: warning:
     'tls_statistics_seq_show' defined but not used [-Wunused-function]
    
    Use #ifdef to guard this.
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Acked-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tls/tls_proc.c
+++ net/tls/tls_proc.c
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/snmp.h>
 #include <net/tls.h>
 
+#ifdef CONFIG_PROC_FS
 static const struct snmp_mib tls_mib_list[] = {
 	SNMP_MIB_ITEM("TlsCurrTxSw", LINUX_MIB_TLSCURRTXSW),
 	SNMP_MIB_ITEM("TlsCurrRxSw", LINUX_MIB_TLSCURRRXSW),
@@ -33,6 +34,7 @@ static int tls_statistics_seq_show(struct seq_file *seq, void *v)
 
 	return 0;
 }
+#endif
 
 int __net_init tls_proc_init(struct net *net)
 {
