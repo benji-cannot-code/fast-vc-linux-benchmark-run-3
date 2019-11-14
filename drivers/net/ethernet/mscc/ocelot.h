707da77dad65765ba5218b1e4b5c91c08e54a2ff
Vladimir Oltean <vladimir.oltean@nxp.com>
2019-11-14 17:03:28 +0200

    net: mscc: ocelot: publish ocelot_sys.h to include/soc/mscc
    The Felix DSA driver needs to write to SYS_RAM_INIT_RAM_INIT for its own
    chip initialization process.
    
    Also update the MAINTAINERS file such that the headers exported by the
    ocelot driver are under the same maintainers' umbrella as the driver
    itself.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mscc/ocelot.h
+++ drivers/net/ethernet/mscc/ocelot.h
@@ -19,12 +19,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ptp_clock_kernel.h>
 #include <linux/regmap.h>
 
+#include <soc/mscc/ocelot_sys.h>
 #include <soc/mscc/ocelot.h>
 #include "ocelot_ana.h"
 #include "ocelot_dev.h"
 #include "ocelot_qsys.h"
 #include "ocelot_rew.h"
-#include "ocelot_sys.h"
 #include "ocelot_qs.h"
 #include "ocelot_tc.h"
 #include "ocelot_ptp.h"
