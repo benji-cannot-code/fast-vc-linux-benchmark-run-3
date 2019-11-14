a97cabd7c1c227954475769db0d5606b36854b29
Stefano Garzarella <sgarzare@redhat.com>
2019-11-14 10:57:38 +0100

    vsock: remove include/linux/vm_sockets.h file
    This header file now only includes the "uapi/linux/vm_sockets.h".
    We can include directly it when needed.
    
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Reviewed-by: Jorgen Hansen <jhansen@vmware.com>
    Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/af_vsock.h
+++ include/net/af_vsock.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/kernel.h>
 #include <linux/workqueue.h>
-#include <linux/vm_sockets.h>
+#include <uapi/linux/vm_sockets.h>
 
 #include "vsock_addr.h"
 
