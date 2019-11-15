307e7c05c27b24d1b76047a8afe849e01b6260c3
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2019-11-15 11:17:30 +0200

    tracing: Increase SYNTH_FIELDS_MAX for synthetic_events
    Increase the maximum allowed count of synthetic event fields from 16 to 32
    in order to allow for larger-than-usual events.
    
    Link: http://lkml.kernel.org/r/20191115091730.9192-1-dedekind1@gmail.com
    
    Reviewed-by: Tom Zanussi <tom.zanussi@linux.intel.com>
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/trace_events_hist.c
+++ kernel/trace/trace_events_hist.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "trace_dynevent.h"
 
 #define SYNTH_SYSTEM		"synthetic"
-#define SYNTH_FIELDS_MAX	16
+#define SYNTH_FIELDS_MAX	32
 
 #define STR_VAR_LEN_MAX		32 /* must be multiple of sizeof(u64) */
 
