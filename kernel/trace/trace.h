36b0769be857c60805ed75907df89499e2fb2b39
Steven Rostedt (VMware) <rostedt@goodmis.org>
2019-11-14 22:43:58 -0500

    tracing: Add missing "inline" in stub function of latency_fsnotify()
    The latency_fsnotify() stub when the function is not defined, was missing
    the "inline".
    
    Link: https://lore.kernel.org/r/20191115140213.74c5efe7@canb.auug.org.au
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/trace.h
+++ kernel/trace/trace.h
@@ -802,7 +802,7 @@ void latency_fsnotify(struct trace_array *tr);
 
 #else
 
-static void latency_fsnotify(struct trace_array *tr) { }
+static inline void latency_fsnotify(struct trace_array *tr) { }
 
 #endif
 
