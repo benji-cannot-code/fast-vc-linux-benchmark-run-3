ecf6913cacfb8721e2b37d487133e42a168d6a79
Jesper Dangaard Brouer <brouer@redhat.com>
2019-11-16 12:22:43 +0100

    page_pool: add destroy attempts counter and rename tracepoint
    When Jonathan change the page_pool to become responsible to its
    own shutdown via deferred work queue, then the disconnect_cnt
    counter was removed from xdp memory model tracepoint.
    
    This patch change the page_pool_inflight tracepoint name to
    page_pool_release, because it reflects the new responsability
    better.  And it reintroduces a counter that reflect the number of
    times page_pool_release have been tried.
    
    The counter is also used by the code, to only empty the alloc
    cache once.  With a stuck work queue running every second and
    counter being 64-bit, it will overrun in approx 584 billion
    years. For comparison, Earth lifetime expectancy is 7.5 billion
    years, before the Sun will engulf, and destroy, the Earth.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/trace/events/page_pool.h
+++ include/trace/events/page_pool.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <net/page_pool.h>
 
-TRACE_EVENT(page_pool_inflight,
+TRACE_EVENT(page_pool_release,
 
 	TP_PROTO(const struct page_pool *pool,
 		 s32 inflight, u32 hold, u32 release),
@@ -23,6 +23,7 @@ TRACE_EVENT(page_pool_inflight,
 		__field(s32,	inflight)
 		__field(u32,	hold)
 		__field(u32,	release)
+		__field(u64,	cnt)
 	),
 
 	TP_fast_assign(
@@ -30,10 +31,12 @@ TRACE_EVENT(page_pool_inflight,
 		__entry->inflight	= inflight;
 		__entry->hold		= hold;
 		__entry->release	= release;
+		__entry->cnt		= pool->destroy_cnt;
 	),
 
-	TP_printk("page_pool=%p inflight=%d hold=%u release=%u",
-	  __entry->pool, __entry->inflight, __entry->hold, __entry->release)
+	TP_printk("page_pool=%p inflight=%d hold=%u release=%u cnt=%llu",
+		__entry->pool, __entry->inflight, __entry->hold,
+		__entry->release, __entry->cnt)
 );
 
 TRACE_EVENT(page_pool_state_release,
