8ada426633e52e7c40b29cf28907c46000bd99a1
Hans de Goede <hdegoede@redhat.com>
2019-11-18 16:51:28 +0100

    drm/modes: parse_cmdline: Set bpp/refresh_specified after successful parsing
    drm_connector_get_cmdline_mode() calls
    drm_mode_parse_command_line_for_connector() with &connector->cmdline_mode
    as mode argument, so anything which we store in the mode arguments gets
    kept even if we return false.
    
    Avoid storing a possibly false-postive bpp/refresh_specified setting
    in connector->cmdline_mode by moving the setting of these to after
    successful parsing of the bpp/refresh parts of the video= argument.
    
    Acked-by: Maxime Ripard <mripard@kernel.org>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191118155134.30468-7-hdegoede@redhat.com

--- drivers/gpu/drm/drm_modes.c
+++ drivers/gpu/drm/drm_modes.c
@@ -1772,10 +1772,8 @@ bool drm_mode_parse_command_line_for_connector(const char *mode_option,
 
 	/* Try to locate the bpp and refresh specifiers, if any */
 	bpp_ptr = strchr(name, '-');
-	if (bpp_ptr) {
+	if (bpp_ptr)
 		bpp_off = bpp_ptr - name;
-		mode->bpp_specified = true;
-	}
 
 	refresh_ptr = strchr(name, '@');
 	if (refresh_ptr) {
@@ -1783,7 +1781,6 @@ bool drm_mode_parse_command_line_for_connector(const char *mode_option,
 			return false;
 
 		refresh_off = refresh_ptr - name;
-		mode->refresh_specified = true;
 	}
 
 	/* Locate the start of named options */
@@ -1826,6 +1823,8 @@ bool drm_mode_parse_command_line_for_connector(const char *mode_option,
 		ret = drm_mode_parse_cmdline_bpp(bpp_ptr, &bpp_end_ptr, mode);
 		if (ret)
 			return false;
+
+		mode->bpp_specified = true;
 	}
 
 	if (refresh_ptr) {
@@ -1833,6 +1832,8 @@ bool drm_mode_parse_command_line_for_connector(const char *mode_option,
 						     &refresh_end_ptr, mode);
 		if (ret)
 			return false;
+
+		mode->refresh_specified = true;
 	}
 
 	/*
