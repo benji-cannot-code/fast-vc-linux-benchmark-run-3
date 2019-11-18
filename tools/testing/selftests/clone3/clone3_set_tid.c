29008424a612fb0ebe2035b0ac51b01bb3914eef
Andrei Vagin <avagin@gmail.com>
2019-11-17 22:47:48 -0800

    selftests/clone3: flush stdout and stderr before clone3() and _exit()
    Buffers have to be flushed before clone3() to avoid double messages in
    the log.
    
    Fixes: 41585bbeeef9 ("selftests: add tests for clone3() with *set_tid")
    Signed-off-by: Andrei Vagin <avagin@gmail.com>
    Link: https://lore.kernel.org/r/20191118064750.408003-1-avagin@gmail.com
    Signed-off-by: Christian Brauner <christian.brauner@ubuntu.com>

--- tools/testing/selftests/clone3/clone3_set_tid.c
+++ tools/testing/selftests/clone3/clone3_set_tid.c
@@ -31,6 +31,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static int pipe_1[2];
 static int pipe_2[2];
 
+static void child_exit(int ret)
+{
+	fflush(stdout);
+	fflush(stderr);
+	_exit(ret);
+}
+
 static int call_clone3_set_tid(pid_t *set_tid,
 			       size_t set_tid_size,
 			       int flags,
@@ -85,8 +92,8 @@ static int call_clone3_set_tid(pid_t *set_tid,
 		}
 
 		if (set_tid[0] != getpid())
-			_exit(EXIT_FAILURE);
-		_exit(exit_code);
+			child_exit(EXIT_FAILURE);
+		child_exit(exit_code);
 	}
 
 	if (expected_pid == 0 || expected_pid == pid) {
@@ -250,7 +257,7 @@ int main(int argc, char *argv[])
 	pid = fork();
 	if (pid == 0) {
 		ksft_print_msg("Child has PID %d\n", getpid());
-		_exit(EXIT_SUCCESS);
+		child_exit(EXIT_SUCCESS);
 	}
 	if (waitpid(pid, &status, 0) < 0)
 		ksft_exit_fail_msg("Waiting for child %d failed", pid);
@@ -310,7 +317,7 @@ int main(int argc, char *argv[])
 		 */
 		test_clone3_set_tid(set_tid, 3, CLONE_NEWPID, 0, 42, true);
 
-		_exit(ksft_cnt.ksft_pass);
+		child_exit(ksft_cnt.ksft_pass);
 	}
 
 	close(pipe_1[1]);
