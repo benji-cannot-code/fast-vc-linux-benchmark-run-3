6ef1626ccdbcdb41758cfdfba57039c45d812e6f
Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
2019-11-19 17:41:47 +0300

    DRM: ARC: PGU: add ARGB8888 format to supported format list
    As we ignore first 8 bit of 32 bit pixel value we can add ARGB8888
    format as alias of XRGB8888.
    
    Signed-off-by: Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
    Signed-off-by: Alexey Brodkin <abrodkin@synopsys.com>

--- drivers/gpu/drm/arc/arcpgu_crtc.c
+++ drivers/gpu/drm/arc/arcpgu_crtc.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static const u32 arc_pgu_supported_formats[] = {
 	DRM_FORMAT_RGB565,
 	DRM_FORMAT_XRGB8888,
+	DRM_FORMAT_ARGB8888,
 };
 
 static void arc_pgu_set_pxl_fmt(struct drm_crtc *crtc)
