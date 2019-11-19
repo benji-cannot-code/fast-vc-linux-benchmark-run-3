fd06d7c6da6144b8b0f31aab04366051b6221b6c
Dennis Li <Dennis.Li@amd.com>
2019-11-19 14:02:57 +0800

    drm/amdgpu: implement querying ras error count for mmhub9.4
    Get mmhub error counter by accessing EDC_CNT registers.
    
    v2: Add mmhub_v9_4_ prefix for local static variable and function
    
    Signed-off-by: Dennis Li <dennis.li@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Reviewed-by: Tao Zhou <tao.zhou1@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/mmhub_v9_4.h
+++ drivers/gpu/drm/amd/amdgpu/mmhub_v9_4.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MMHUB_V9_4_H__
 #define __MMHUB_V9_4_H__
 
+extern const struct amdgpu_mmhub_funcs mmhub_v9_4_funcs;
+
 u64 mmhub_v9_4_get_fb_location(struct amdgpu_device *adev);
 int mmhub_v9_4_gart_enable(struct amdgpu_device *adev);
 void mmhub_v9_4_gart_disable(struct amdgpu_device *adev);
