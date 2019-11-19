f4aaed6456f3ea67f0515f3b4eb64e2a312c050f
Gwendal Grignou <gwendal@chromium.org>
2019-11-19 13:45:45 +0100

    iio / platform: cros_ec: Add cros-ec-sensorhub driver
    Similar to HID sensor stack, the new driver sits between cros-ec-dev
    and the IIO device drivers:
    
    The EC based IIO device topology would be:
    
    iio:device1 ->
       ...0/0000:00:1f.0/PNP0C09:00/GOOG0004:00/cros-ec-dev.6.auto/
                                                cros-ec-sensorhub.7.auto/
                                                cros-ec-accel.15.auto/
                                                iio:device1
    
    It will be expanded to control EC sensor FIFO.
    
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    [Fix "unknown type name 'uint32_t'" type errors]
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>

---
+++ include/linux/platform_data/cros_ec_sensorhub.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Chrome OS EC MEMS Sensor Hub driver.
+ *
+ * Copyright 2019 Google LLC
+ */
+
+#ifndef __LINUX_PLATFORM_DATA_CROS_EC_SENSORHUB_H
+#define __LINUX_PLATFORM_DATA_CROS_EC_SENSORHUB_H
+
+#include <linux/platform_data/cros_ec_commands.h>
+
+/**
+ * struct cros_ec_sensorhub - Sensor Hub device data.
+ *
+ * @ec: Embedded Controller where the hub is located.
+ */
+struct cros_ec_sensorhub {
+	struct cros_ec_dev *ec;
+};
+
+#endif   /* __LINUX_PLATFORM_DATA_CROS_EC_SENSORHUB_H */
