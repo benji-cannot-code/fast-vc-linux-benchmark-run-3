eb71ffc353dabfec350a39b93e4e92fd8446558c
Lorenzo Bianconi <lorenzo@kernel.org>
2019-11-20 16:54:18 +0200

    net: page_pool: add the possibility to sync DMA memory for device
    Introduce the following parameters in order to add the possibility to sync
    DMA memory for device before putting allocated pages in the page_pool
    caches:
    - PP_FLAG_DMA_SYNC_DEV: if set in page_pool_params flags, all pages that
      the driver gets from page_pool will be DMA-synced-for-device according
      to the length provided by the device driver. Please note DMA-sync-for-CPU
      is still device driver responsibility
    - offset: DMA address offset where the DMA engine starts copying rx data
    - max_len: maximum DMA memory size page_pool is allowed to flush. This
      is currently used in __page_pool_alloc_pages_slow routine when pages
      are allocated from page allocator
    These parameters are supposed to be set by device drivers.
    
    This optimization reduces the length of the DMA-sync-for-device.
    The optimization is valid because pages are initially
    DMA-synced-for-device as defined via max_len. At RX time, the driver
    will perform a DMA-sync-for-CPU on the memory for the packet length.
    What is important is the memory occupied by packet payload, because
    this is the area CPU is allowed to read and modify. As we don't track
    cache-lines written into by the CPU, simply use the packet payload length
    as dma_sync_size at page_pool recycle time. This also take into account
    any tail-extend.
    
    Tested-by: Matteo Croce <mcroce@redhat.com>
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Acked-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/page_pool.h
+++ include/net/page_pool.h
@@ -35,8 +35,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ptr_ring.h>
 #include <linux/dma-direction.h>
 
-#define PP_FLAG_DMA_MAP 1 /* Should page_pool do the DMA map/unmap */
-#define PP_FLAG_ALL	PP_FLAG_DMA_MAP
+#define PP_FLAG_DMA_MAP		BIT(0) /* Should page_pool do the DMA
+					* map/unmap
+					*/
+#define PP_FLAG_DMA_SYNC_DEV	BIT(1) /* If set all pages that the driver gets
+					* from page_pool will be
+					* DMA-synced-for-device according to
+					* the length provided by the device
+					* driver.
+					* Please note DMA-sync-for-CPU is still
+					* device driver responsibility
+					*/
+#define PP_FLAG_ALL		(PP_FLAG_DMA_MAP | PP_FLAG_DMA_SYNC_DEV)
 
 /*
  * Fast allocation side cache array/stack
@@ -66,6 +76,8 @@ struct page_pool_params {
 	int		nid;  /* Numa node id to allocate from pages from */
 	struct device	*dev; /* device, for DMA pre-mapping purposes */
 	enum dma_data_direction dma_dir; /* DMA mapping direction */
+	unsigned int	max_len; /* max DMA sync memory size */
+	unsigned int	offset;  /* DMA addr offset */
 };
 
 struct page_pool {
@@ -152,8 +164,8 @@ static inline void page_pool_use_xdp_mem(struct page_pool *pool,
 #endif
 
 /* Never call this directly, use helpers below */
-void __page_pool_put_page(struct page_pool *pool,
-			  struct page *page, bool allow_direct);
+void __page_pool_put_page(struct page_pool *pool, struct page *page,
+			  unsigned int dma_sync_size, bool allow_direct);
 
 static inline void page_pool_put_page(struct page_pool *pool,
 				      struct page *page, bool allow_direct)
@@ -162,14 +174,14 @@ static inline void page_pool_put_page(struct page_pool *pool,
 	 * allow registering MEM_TYPE_PAGE_POOL, but shield linker.
 	 */
 #ifdef CONFIG_PAGE_POOL
-	__page_pool_put_page(pool, page, allow_direct);
+	__page_pool_put_page(pool, page, -1, allow_direct);
 #endif
 }
 /* Very limited use-cases allow recycle direct */
 static inline void page_pool_recycle_direct(struct page_pool *pool,
 					    struct page *page)
 {
-	__page_pool_put_page(pool, page, true);
+	__page_pool_put_page(pool, page, -1, true);
 }
 
 /* Disconnects a page (from a page_pool).  API users can have a need
