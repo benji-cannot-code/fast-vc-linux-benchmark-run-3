caa8a6d488398ee4faba6b1fb9217ae936e0905c
Bibby Hsieh <bibby.hsieh@mediatek.com>
2019-11-21 09:54:07 +0800

    soc: mediatek: cmdq: define the instruction struct
    Define an instruction structure for gce driver to append command.
    This structure can make the client's code more readability.
    
    Signed-off-by: Bibby Hsieh <bibby.hsieh@mediatek.com>
    Reviewed-by: CK Hu <ck.hu@mediatek.com>
    Signed-off-by: Matthias Brugger <matthias.bgg@gmail.com>

--- include/linux/mailbox/mtk-cmdq-mailbox.h
+++ include/linux/mailbox/mtk-cmdq-mailbox.h
@@ -21,6 +21,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CMDQ_WFE_WAIT			BIT(15)
 #define CMDQ_WFE_WAIT_VALUE		0x1
 
+/*
+ * WFE arg_b
+ * bit 0-11: wait value
+ * bit 15: 1 - wait, 0 - no wait
+ * bit 16-27: update value
+ * bit 31: 1 - update, 0 - no update
+ */
+#define CMDQ_WFE_OPTION			(CMDQ_WFE_UPDATE | CMDQ_WFE_WAIT | \
+					CMDQ_WFE_WAIT_VALUE)
+
 /** cmdq event maximum */
 #define CMDQ_MAX_EVENT			0x3ff
 
