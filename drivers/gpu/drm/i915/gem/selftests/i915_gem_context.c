c340641bf295c221b06df5e74865b98ea3258e28
Chris Wilson <chris@chris-wilson.co.uk>
2019-11-21 23:30:21 +0000

    drm/i915/selftests: Shorten infinite wait for sseu
    Use our more regular igt_flush_test() to bind the wait-for-idle and
    error out instead of waiting around forever on critical failure.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Stuart Summers <stuart.summers@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191121233021.507400-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gem/selftests/i915_gem_context.c
+++ drivers/gpu/drm/i915/gem/selftests/i915_gem_context.c
@@ -1169,8 +1169,7 @@ out:
 		igt_spinner_end(spin);
 
 	if ((flags & TEST_IDLE) && ret == 0) {
-		ret = intel_gt_wait_for_idle(ce->engine->gt,
-					     MAX_SCHEDULE_TIMEOUT);
+		ret = igt_flush_test(ce->engine->i915);
 		if (ret)
 			return ret;
 
