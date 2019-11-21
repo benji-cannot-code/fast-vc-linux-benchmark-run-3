cf9f2ef028f0fd7c79c0c72deb232a9330c24541
Marco Felsch <m.felsch@pengutronix.de>
2019-11-21 10:46:17 +0100

    drm/imx: parallel-display: fix edid memory leak
    The edid memory is only freed if the component.unbind() is called. This
    is okay if the parallel-display was bound but if the bind() fails we
    leak the memory.
    
    Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>
    [p.zabel@pengutronix.de: rebased, dropped now empty unbind()]
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/gpu/drm/imx/parallel-display.c
+++ drivers/gpu/drm/imx/parallel-display.c
@@ -326,7 +326,8 @@ static int imx_pd_bind(struct device *dev, struct device *master, void *data)
 
 	edidp = of_get_property(np, "edid", &imxpd->edid_len);
 	if (edidp)
-		imxpd->edid = kmemdup(edidp, imxpd->edid_len, GFP_KERNEL);
+		imxpd->edid = devm_kmemdup(dev, edidp, imxpd->edid_len,
+					   GFP_KERNEL);
 
 	ret = of_property_read_string(np, "interface-pix-fmt", &fmt);
 	if (!ret) {
@@ -350,17 +351,8 @@ static int imx_pd_bind(struct device *dev, struct device *master, void *data)
 	return 0;
 }
 
-static void imx_pd_unbind(struct device *dev, struct device *master,
-	void *data)
-{
-	struct imx_parallel_display *imxpd = dev_get_drvdata(dev);
-
-	kfree(imxpd->edid);
-}
-
 static const struct component_ops imx_pd_ops = {
 	.bind	= imx_pd_bind,
-	.unbind	= imx_pd_unbind,
 };
 
 static int imx_pd_probe(struct platform_device *pdev)
