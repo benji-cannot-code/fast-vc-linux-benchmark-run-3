f941fd81913df8c5f66e873837680c291fef17b8
David Howells <dhowells@redhat.com>
2019-11-21 09:12:16 +0000

    afs: Use call->_iter not &call->iter in debugging statements
    Use call->_iter not &call->iter in debugging statements as the latter is a
    convenience iter whereas the former represents we're actually doing at the
    moment.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- fs/afs/fsclient.c
+++ fs/afs/fsclient.c
@@ -324,7 +324,7 @@ static int afs_deliver_fs_fetch_data(struct afs_call *call)
 	int ret;
 
 	_enter("{%u,%zu/%llu}",
-	       call->unmarshall, iov_iter_count(&call->iter), req->actual_len);
+	       call->unmarshall, iov_iter_count(call->_iter), req->actual_len);
 
 	switch (call->unmarshall) {
 	case 0:
@@ -371,7 +371,7 @@ static int afs_deliver_fs_fetch_data(struct afs_call *call)
 		/* extract the returned data */
 	case 2:
 		_debug("extract data %zu/%llu",
-		       iov_iter_count(&call->iter), req->remain);
+		       iov_iter_count(call->_iter), req->remain);
 
 		ret = afs_extract_data(call, true);
 		if (ret < 0)
@@ -399,7 +399,7 @@ static int afs_deliver_fs_fetch_data(struct afs_call *call)
 
 	case 3:
 		_debug("extract discard %zu/%llu",
-		       iov_iter_count(&call->iter), req->actual_len - req->len);
+		       iov_iter_count(call->_iter), req->actual_len - req->len);
 
 		ret = afs_extract_data(call, true);
 		if (ret < 0)
@@ -1853,7 +1853,7 @@ static int afs_deliver_fs_get_capabilities(struct afs_call *call)
 	u32 count;
 	int ret;
 
-	_enter("{%u,%zu}", call->unmarshall, iov_iter_count(&call->iter));
+	_enter("{%u,%zu}", call->unmarshall, iov_iter_count(call->_iter));
 
 	switch (call->unmarshall) {
 	case 0:
