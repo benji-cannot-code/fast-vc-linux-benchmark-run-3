d1dc21ccc6f6ee8e093a8e544f43de755eec7354
Thomas Zimmermann <tzimmermann@suse.de>
2019-11-22 11:05:45 +0100

    drm/gma500: Remove struct psb_fbdev
    Gma500's struct psb_fbdev is an, otherwise empty, wrapper around
    struct drm_fb_helper. Remove it.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Signed-off-by: Patrik Jakobsson <patrik.r.jakobsson@gmail.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191122100545.16812-7-tzimmermann@suse.de

--- drivers/gpu/drm/gma500/framebuffer.h
+++ drivers/gpu/drm/gma500/framebuffer.h
@@ -10,13 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _FRAMEBUFFER_H_
 #define _FRAMEBUFFER_H_
 
-#include <drm/drm_fb_helper.h>
-
 #include "psb_drv.h"
-
-struct psb_fbdev {
-	struct drm_fb_helper psb_fb_helper; /* must be first */
-};
 
 extern int gma_connector_clones(struct drm_device *dev, int type_mask);
 
--- drivers/gpu/drm/gma500/psb_drv.c
+++ drivers/gpu/drm/gma500/psb_drv.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <drm/drm.h>
 #include <drm/drm_drv.h>
+#include <drm/drm_fb_helper.h>
 #include <drm/drm_file.h>
 #include <drm/drm_ioctl.h>
 #include <drm/drm_irq.h>
