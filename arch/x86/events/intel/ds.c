748251bc929e8b0e0507e6dea65ee8edf6deccdb
Sean Christopherson <sean.j.christopherson@intel.com>
2019-11-26 08:54:10 -0800

    perf/x86/intel: Explicitly include asm/io.h to use virt_to_phys()
    Through a labyrinthian sequence of includes, usage of virt_to_phys() is
    dependent on the include of asm/io.h in asm/realmode.h via asm/acpi.h.
    Explicitly include asm/io.h to break the dependency on realmode.h so
    that a future patch can remove the realmode.h include from acpi.h
    without breaking the build.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Link: https://lkml.kernel.org/r/20191126165417.22423-6-sean.j.christopherson@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/events/intel/ds.c
+++ arch/x86/events/intel/ds.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/perf_event.h>
 #include <asm/tlbflush.h>
 #include <asm/insn.h>
+#include <asm/io.h>
 
 #include "../perf_event.h"
 
