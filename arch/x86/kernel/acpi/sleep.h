d3fcb885d23033f0779c8b263dd869770e0b6a50
Sean Christopherson <sean.j.christopherson@intel.com>
2019-11-26 08:54:15 -0800

    x86/ACPI/sleep: Remove an unnecessary include of asm/realmode.h
    None of the declarations in x86's acpi/sleep.h are in any way dependent
    on the real mode boot code.  Remove sleep.h's include of asm/realmode.h
    to limit the dependencies on realmode.h to code that actually interacts
    with the boot code.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://lkml.kernel.org/r/20191126165417.22423-11-sean.j.christopherson@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/kernel/acpi/sleep.h
+++ arch/x86/kernel/acpi/sleep.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	Variables and functions used by the code in sleep.c
  */
 
-#include <asm/realmode.h>
+#include <linux/linkage.h>
 
 extern unsigned long saved_video_mode;
 extern long saved_magic;
