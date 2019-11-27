672b362858a8a40413e33a0943879bad7a4d4a67
Chris Wilson <chris@chris-wilson.co.uk>
2019-11-27 09:56:57 +0000

    drm/i915/gt: Manual rc6 entry upon parking
    Now that we rapidly park the GT when the GPU idles, we often find
    ourselves idling faster than the RC6 promotion timer. Thus if we tell
    the GPU to enter RC6 manually as we park, we can do so quicker (by
    around 50ms, half an EI on average) and marginally increase our
    powersaving across all execlists platforms.
    
    v2: Now with a selftest to check we can enter RC6 manually
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Andi Shyti <andi.shyti@intel.com>
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Cc: Imre Deak <imre.deak@intel.com>
    Acked-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Andi Shyti <andi.shyti@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191127095657.3209854-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gt/selftest_rc6.h
+++ drivers/gpu/drm/i915/gt/selftest_rc6.h
@@ -9,5 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SELFTEST_RC6_H
 
 int live_rc6_ctx_wa(void *arg);
+int live_rc6_manual(void *arg);
 
 #endif /* SELFTEST_RC6_H */
