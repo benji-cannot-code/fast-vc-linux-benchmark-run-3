8019e8bb6c9154d3603323f7fe0d7add695b0089
Anilkumar Kolli <akolli@codeaurora.org>
2019-11-27 18:29:53 +0200

    ath11k: update tcl cmd descriptor parameters for STA mode
    It is observed that ath11k STA mode UL throughput is low.
    This is due to packets delivered to FW from TCL instead of TQM.
    TCL AST search fail causes packet delivered to FW, fix this by
    properly configuring the TCL address search type and ast_hash.
    STA UL throughput is improved 10times with 11AC AP.
    
    Signed-off-by: Anilkumar Kolli <akolli@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath11k/hal_tx.h
+++ drivers/net/wireless/ath/ath11k/hal_tx.h
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HAL_TX_ADDRX_EN			1
 #define HAL_TX_ADDRY_EN			2
 
-#define HAL_TX_ADDR_SEARCH_INDEX	0
-#define HAL_TX_ADDR_SEARCH_DEFAULT	1
+#define HAL_TX_ADDR_SEARCH_DEFAULT	0
+#define HAL_TX_ADDR_SEARCH_INDEX	1
 
 struct hal_tx_info {
 	u16 meta_data_flags; /* %HAL_TCL_DATA_CMD_INFO0_META_ */
