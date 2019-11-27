93d7c6a6c42a2ef7c8e550ca906abc2cd8e44316
Gilad Ben-Yossef <gilad@benyossef.com>
2019-11-27 10:49:05 +0200

    crypto: ccree - remove useless define
    The define of CC_DEV_SHA_MAX is not needed since we moved
    to runtime detection of capabilities. Remove it.
    
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ccree/cc_driver.h
+++ drivers/crypto/ccree/cc_driver.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Registers definitions from shared/hw/ree_include */
 #include "cc_host_regs.h"
-#define CC_DEV_SHA_MAX 512
 #include "cc_crypto_ctx.h"
 #include "cc_hw_queue_defs.h"
 #include "cc_sram_mgr.h"
