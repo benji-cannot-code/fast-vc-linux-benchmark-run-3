90071b65ec94d8b011be7d6c2fd2f05042da291c
Nikola Cornij <nikola.cornij@amd.com>
2019-11-28 14:14:56 -0500

    drm/amd/display: Minimize DSC resource re-assignment
    [why]
    Assigning a different DSC resource than the one previosly used is
    currently not handled. This causes black screen on mode change when more
    than one monitor is connected on some ASICs.
    
    [how]
    - Acquire the previously used DSC if available
    - Make sure re-program is triggered if new DSC is used
    
    Acked-by: Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
    Signed-off-by: Nikola Cornij <nikola.cornij@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_resource.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_resource.c
@@ -2667,6 +2667,9 @@ bool pipe_need_reprogram(
 		false == pipe_ctx_old->stream->dpms_off)
 		return true;
 
+	if (pipe_ctx_old->stream_res.dsc != pipe_ctx->stream_res.dsc)
+		return true;
+
 	return false;
 }
 
