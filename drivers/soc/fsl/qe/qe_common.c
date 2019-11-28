4f01bc84597734973945a98444452d6be742f242
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2019-11-28 15:55:41 +0100

    soc: fsl: qe: make cpm_muram_free() return void
    Nobody uses the return value from cpm_muram_free, and functions that
    free resources usually return void. One could imagine a use for a "how
    much have I allocated" a la ksize(), but knowing how much one had
    access to after the fact is useless.
    
    Reviewed-by: Timur Tabi <timur@kernel.org>
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Signed-off-by: Li Yang <leoyang.li@nxp.com>

--- drivers/soc/fsl/qe/qe_common.c
+++ drivers/soc/fsl/qe/qe_common.c
@@ -171,7 +171,7 @@ EXPORT_SYMBOL(cpm_muram_alloc);
  * cpm_muram_free - free a chunk of multi-user ram
  * @offset: The beginning of the chunk as returned by cpm_muram_alloc().
  */
-int cpm_muram_free(s32 offset)
+void cpm_muram_free(s32 offset)
 {
 	unsigned long flags;
 	int size;
@@ -189,7 +189,6 @@ int cpm_muram_free(s32 offset)
 	}
 	gen_pool_free(muram_pool, offset + GENPOOL_OFFSET, size);
 	spin_unlock_irqrestore(&cpm_muram_lock, flags);
-	return size;
 }
 EXPORT_SYMBOL(cpm_muram_free);
 
