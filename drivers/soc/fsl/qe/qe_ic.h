92649fd04beb6846c8588e1a93f1810824095082
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2019-11-28 15:55:07 +0100

    soc: fsl: qe: drop volatile qualifier of struct qe_ic::regs
    The actual io accessors (e.g. in_be32) implicitly add a volatile
    qualifier to their address argument. Remove volatile from the struct
    definition and the qe_ic_(read/write) helpers, in preparation for
    switching from the ppc-specific io accessors to generic ones.
    
    Reviewed-by: Timur Tabi <timur@kernel.org>
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Signed-off-by: Li Yang <leoyang.li@nxp.com>

--- drivers/soc/fsl/qe/qe_ic.h
+++ drivers/soc/fsl/qe/qe_ic.h
@@ -73,7 +73,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct qe_ic {
 	/* Control registers offset */
-	volatile u32 __iomem *regs;
+	u32 __iomem *regs;
 
 	/* The remapper for this QEIC */
 	struct irq_domain *irqhost;
