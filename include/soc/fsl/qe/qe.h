790557f6529a262217702b669f54b8bb490eb91f
Rasmus Villemoes <linux@rasmusvillemoes.dk>
2019-11-28 15:55:32 +0100

    soc/fsl/qe/qe.h: update include path for cpm.h
    asm/cpm.h under arch/powerpc is now just a wrapper for including
    soc/fsl/cpm.h. In order to make the qe.h header usable on other
    architectures, use the latter path directly.
    
    Reviewed-by: Timur Tabi <timur@kernel.org>
    Signed-off-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Signed-off-by: Li Yang <leoyang.li@nxp.com>

--- include/soc/fsl/qe/qe.h
+++ include/soc/fsl/qe/qe.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/errno.h>
 #include <linux/err.h>
-#include <asm/cpm.h>
+#include <soc/fsl/cpm.h>
 #include <soc/fsl/qe/immap_qe.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
