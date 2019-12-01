c98ead11866463afe833d30430271a21e8676f0a
Ben Dooks <ben.dooks@codethink.co.uk>
2019-11-30 17:49:18 -0800

    fs/buffer.c: include internal.h for missing declarations
    The declarations of __block_write_begin_int and guard_bio_eod are needed
    from internal.h so include it to fix the following sparse warnings:
    
      fs/buffer.c:1930:5: warning: symbol '__block_write_begin_int' was not declared. Should it be static?
      fs/buffer.c:2994:6: warning: symbol 'guard_bio_eod' was not declared. Should it be static?
    
    Link: http://lkml.kernel.org/r/20191011170039.16100-1-ben.dooks@codethink.co.uk
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/buffer.c
+++ fs/buffer.c
@@ -50,6 +50,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <trace/events/block.h>
 #include <linux/fscrypt.h>
 
+#include "internal.h"
+
 static int fsync_buffers_list(spinlock_t *lock, struct list_head *list);
 static int submit_bh_wbc(int op, int op_flags, struct buffer_head *bh,
 			 enum rw_hint hint, struct writeback_control *wbc);
