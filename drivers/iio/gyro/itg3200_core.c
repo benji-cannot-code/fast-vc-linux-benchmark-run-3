803e6ad6ae5c0f17df8c58a7745b9e553f73211c
Linus Walleij <linus.walleij@linaro.org>
2019-12-02 09:26:03 +0100

    iio: itg3200: Drop GPIO include
    The driver include <linux/gpio.h> yet does not use any
    of the symbols from the header, so drop the include.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/gyro/itg3200_core.c
+++ drivers/iio/gyro/itg3200_core.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/irq.h>
 #include <linux/i2c.h>
-#include <linux/gpio.h>
 #include <linux/slab.h>
 #include <linux/stat.h>
 #include <linux/module.h>
