bd53323f80df8179d0e850cb00d77a3842dc2ad6
Linus Walleij <linus.walleij@linaro.org>
2019-12-02 09:17:30 +0100

    iio: ak8975: Convert to use GPIO descriptor
    The end-of-conversion (EOC) GPIO line is better to grab using
    a GPIO descriptor. We drop the pdata for this: clients using board
    files can use machine descriptor tables to pass this GPIO from
    static data.
    
    Cc: Stephan Gerhold <stephan@gerhold.net>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- include/linux/iio/magnetometer/ak8975.h
+++ include/linux/iio/magnetometer/ak8975.h
@@ -7,11 +7,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /**
  * struct ak8975_platform_data - AK8975 magnetometer driver platform data
- * @eoc_gpio:    data ready event gpio
  * @orientation: mounting matrix relative to main hardware
  */
 struct ak8975_platform_data {
-	int                     eoc_gpio;
 	struct iio_mount_matrix orientation;
 };
 
