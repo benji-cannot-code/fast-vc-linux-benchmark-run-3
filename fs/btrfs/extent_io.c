51bdf0a1779e76027d21906682bed4b0f2818992
Omar Sandoval <osandov@fb.com>
2019-12-02 17:34:22 -0800

    btrfs: remove redundant i_size check in __extent_writepage_io()
    In __extent_writepage_io(), we check whether
    
      i_size <= page_offset(page).
    
    Note that if i_size < page_offset(page), then
    i_size >> PAGE_SHIFT < page->index.
    
    If i_size == page_offset(page), then
    i_size >> PAGE_SHIFT == page->index && offset_in_page(i_size) == 0.
    
    __extent_writepage() already has a check for these cases that
    returns without calling __extent_writepage_io():
    
      end_index = i_size >> PAGE_SHIFT
      pg_offset = offset_in_page(i_size);
      if (page->index > end_index ||
         (page->index == end_index && !pg_offset)) {
              page->mapping->a_ops->invalidatepage(page, 0, PAGE_SIZE);
              unlock_page(page);
              return 0;
      }
    
    Get rid of the one in __extent_writepage_io(), which was obsoleted in
    211c17f51f46 ("Fix corners in writepage and btrfs_truncate_page").
    
    Signed-off-by: Omar Sandoval <osandov@fb.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -3456,11 +3456,6 @@ static noinline_for_stack int __extent_writepage_io(struct inode *inode,
 	update_nr_written(wbc, nr_written + 1);
 
 	end = page_end;
-	if (i_size <= start) {
-		btrfs_writepage_endio_finish_ordered(page, start, page_end, 1);
-		goto done;
-	}
-
 	blocksize = inode->i_sb->s_blocksize;
 
 	while (cur <= end) {
@@ -3541,7 +3536,6 @@ static noinline_for_stack int __extent_writepage_io(struct inode *inode,
 		pg_offset += iosize;
 		nr++;
 	}
-done:
 	*nr_ret = nr;
 	return ret;
 }
