e33bb378acd9d427fee4fab786a0deaa4cf4f11f
Shile Zhang <shile.zhang@linux.alibaba.com>
2019-12-04 08:46:32 +0800

    scripts/sorttable: Implement build-time ORC unwind table sorting
    The ORC unwinder has two tables: .orc_unwind_ip and .orc_unwind, which
    need to be sorted for binary search. Previously this sorting was done
    during bootup.
    
    Sort them at build time to speed up booting.
    
    Add the ORC tables sorting in a parallel build process to speed up the build.
    
    [ mingo: Rewrote the changelog and fixed some comments. ]
    
    Suggested-by: Andy Lutomirski <luto@amacapital.net>
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Shile Zhang <shile.zhang@linux.alibaba.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Masahiro Yamada <yamada.masahiro@socionext.com>
    Cc: Michal Marek <michal.lkml@markovi.net>
    Cc: linux-kbuild@vger.kernel.org
    Link: https://lkml.kernel.org/r/20191204004633.88660-7-shile.zhang@linux.alibaba.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- scripts/sorttable.c
+++ scripts/sorttable.c
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * sorttable.c: Sort the kernel's table
  *
+ * Added ORC unwind tables sort support and other updates:
+ * Copyright (C) 1999-2019 Alibaba Group Holding Limited. by:
+ * Shile Zhang <shile.zhang@linux.alibaba.com>
+ *
  * Copyright 2011 - 2012 Cavium, Inc.
  *
  * Based on code taken from recortmcount.c which is:
@@ -10,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright 2009 John F. Reiser <jreiser@BitWagon.com>.  All rights reserved.
  *
  * Restructured to fit Linux format, as well as other updates:
- *  Copyright 2010 Steven Rostedt <srostedt@redhat.com>, Red Hat Inc.
+ * Copyright 2010 Steven Rostedt <srostedt@redhat.com>, Red Hat Inc.
  */
 
 /*
