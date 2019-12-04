e01852cef532c862a43ef130ada91af64ad4c4f2
Hongbo Yao <yaohongbo@huawei.com>
2019-12-04 13:45:48 +0800

    drm/msm/dpu: Fix compile warnings
    Using the following command will get compile warnings:
    make W=1 drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.o ARCH=arm64
    
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c: In function
    ‘_dpu_crtc_program_lm_output_roi’:
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c:91:19: warning: variable
    ‘dpu_crtc’ set but not used [-Wunused-but-set-variable]
      struct dpu_crtc *dpu_crtc;
                       ^~~~~~~~
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c: In function
    ‘dpu_crtc_atomic_begin’:
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c:428:35: warning: variable
    ‘smmu_state’ set but not used [-Wunused-but-set-variable]
      struct dpu_crtc_smmu_state_data *smmu_state;
                                       ^~~~~~~~~~
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c: In function
    ‘dpu_crtc_atomic_flush’:
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c:489:25: warning: variable
    ‘event_thread’ set but not used [-Wunused-but-set-variable]
      struct msm_drm_thread *event_thread;
                             ^~~~~~~~~~~~
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c: In function
    ‘dpu_crtc_destroy_state’:
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c:565:19: warning: variable
    ‘dpu_crtc’ set but not used [-Wunused-but-set-variable]
      struct dpu_crtc *dpu_crtc;
                       ^~~~~~~~
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c: In function
    ‘dpu_crtc_duplicate_state’:
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c:664:19: warning: variable
    ‘dpu_crtc’ set but not used [-Wunused-but-set-variable]
      struct dpu_crtc *dpu_crtc;
                       ^~~~~~~~
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c: In function
    ‘dpu_crtc_disable’:
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c:693:26: warning: variable
    ‘priv’ set but not used [-Wunused-but-set-variable]
      struct msm_drm_private *priv;
                              ^~~~
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c:691:27: warning: variable
    ‘mode’ set but not used [-Wunused-but-set-variable]
      struct drm_display_mode *mode;
                               ^~~~
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c: In function ‘dpu_crtc_enable’:
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c:766:26: warning: variable
    ‘priv’ set but not used [-Wunused-but-set-variable]
      struct msm_drm_private *priv;
                              ^~~~
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c: In function ‘dpu_crtc_init’:
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c:1292:18: warning: variable
    ‘kms’ set but not used [-Wunused-but-set-variable]
      struct dpu_kms *kms = NULL;
                      ^~~
    drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c:663: warning: Excess function
    parameter 'Returns' description in 'dpu_crtc_duplicate_state'
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Hongbo Yao <yaohongbo@huawei.com>
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_crtc.c
@@ -89,11 +89,9 @@ static void _dpu_crtc_setup_blend_cfg(struct dpu_crtc_mixer *mixer,
 
 static void _dpu_crtc_program_lm_output_roi(struct drm_crtc *crtc)
 {
-	struct dpu_crtc *dpu_crtc;
 	struct dpu_crtc_state *crtc_state;
 	int lm_idx, lm_horiz_position;
 
-	dpu_crtc = to_dpu_crtc(crtc);
 	crtc_state = to_dpu_crtc_state(crtc->state);
 
 	lm_horiz_position = 0;
@@ -431,7 +429,6 @@ static void dpu_crtc_atomic_begin(struct drm_crtc *crtc,
 	struct drm_encoder *encoder;
 	struct drm_device *dev;
 	unsigned long flags;
-	struct dpu_crtc_smmu_state_data *smmu_state;
 
 	if (!crtc) {
 		DPU_ERROR("invalid crtc\n");
@@ -449,7 +446,6 @@ static void dpu_crtc_atomic_begin(struct drm_crtc *crtc,
 	dpu_crtc = to_dpu_crtc(crtc);
 	cstate = to_dpu_crtc_state(crtc->state);
 	dev = crtc->dev;
-	smmu_state = &dpu_crtc->smmu_state;
 
 	_dpu_crtc_setup_lm_bounds(crtc, crtc->state);
 
@@ -492,7 +488,6 @@ static void dpu_crtc_atomic_flush(struct drm_crtc *crtc,
 	struct drm_device *dev;
 	struct drm_plane *plane;
 	struct msm_drm_private *priv;
-	struct msm_drm_thread *event_thread;
 	unsigned long flags;
 	struct dpu_crtc_state *cstate;
 
@@ -513,8 +508,6 @@ static void dpu_crtc_atomic_flush(struct drm_crtc *crtc,
 		DPU_ERROR("invalid crtc index[%d]\n", crtc->index);
 		return;
 	}
-
-	event_thread = &priv->event_thread[crtc->index];
 
 	if (dpu_crtc->event) {
 		DPU_DEBUG("already received dpu_crtc->event\n");
@@ -568,7 +561,6 @@ static void dpu_crtc_atomic_flush(struct drm_crtc *crtc,
 static void dpu_crtc_destroy_state(struct drm_crtc *crtc,
 		struct drm_crtc_state *state)
 {
-	struct dpu_crtc *dpu_crtc;
 	struct dpu_crtc_state *cstate;
 
 	if (!crtc || !state) {
@@ -576,7 +568,6 @@ static void dpu_crtc_destroy_state(struct drm_crtc *crtc,
 		return;
 	}
 
-	dpu_crtc = to_dpu_crtc(crtc);
 	cstate = to_dpu_crtc_state(state);
 
 	DPU_DEBUG("crtc%d\n", crtc->base.id);
@@ -663,11 +654,9 @@ static void dpu_crtc_reset(struct drm_crtc *crtc)
 /**
  * dpu_crtc_duplicate_state - state duplicate hook
  * @crtc: Pointer to drm crtc structure
- * @Returns: Pointer to new drm_crtc_state structure
  */
 static struct drm_crtc_state *dpu_crtc_duplicate_state(struct drm_crtc *crtc)
 {
-	struct dpu_crtc *dpu_crtc;
 	struct dpu_crtc_state *cstate, *old_cstate;
 
 	if (!crtc || !crtc->state) {
@@ -675,7 +664,6 @@ static struct drm_crtc_state *dpu_crtc_duplicate_state(struct drm_crtc *crtc)
 		return NULL;
 	}
 
-	dpu_crtc = to_dpu_crtc(crtc);
 	old_cstate = to_dpu_crtc_state(crtc->state);
 	cstate = kmemdup(old_cstate, sizeof(*old_cstate), GFP_KERNEL);
 	if (!cstate) {
@@ -694,9 +682,7 @@ static void dpu_crtc_disable(struct drm_crtc *crtc,
 {
 	struct dpu_crtc *dpu_crtc;
 	struct dpu_crtc_state *cstate;
-	struct drm_display_mode *mode;
 	struct drm_encoder *encoder;
-	struct msm_drm_private *priv;
 	unsigned long flags;
 	bool release_bandwidth = false;
 
@@ -706,8 +692,6 @@ static void dpu_crtc_disable(struct drm_crtc *crtc,
 	}
 	dpu_crtc = to_dpu_crtc(crtc);
 	cstate = to_dpu_crtc_state(crtc->state);
-	mode = &cstate->base.adjusted_mode;
-	priv = crtc->dev->dev_private;
 
 	DRM_DEBUG_KMS("crtc%d\n", crtc->base.id);
 
@@ -769,14 +753,12 @@ static void dpu_crtc_enable(struct drm_crtc *crtc,
 {
 	struct dpu_crtc *dpu_crtc;
 	struct drm_encoder *encoder;
-	struct msm_drm_private *priv;
 	bool request_bandwidth;
 
 	if (!crtc) {
 		DPU_ERROR("invalid crtc\n");
 		return;
 	}
-	priv = crtc->dev->dev_private;
 
 	pm_runtime_get_sync(crtc->dev->dev);
 
