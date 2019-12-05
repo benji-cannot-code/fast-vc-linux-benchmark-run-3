ed4d8d39c048c70d031485c7d84d3269cf41b42d
Leonard Crestez <leonard.crestez@nxp.com>
2019-12-05 12:05:07 +0200

    PM / devfreq: Use PM QoS for sysfs min/max_freq
    Switch the handling of min_freq and max_freq from sysfs to use the
    dev_pm_qos_request interface.
    
    Since PM QoS handles frequencies as kHz this change reduces the
    precision of min_freq and max_freq. This shouldn't introduce problems
    because frequencies which are not an integer number of kHz are likely
    not an integer number of Hz either.
    
    Try to ensure compatibility by rounding min values down and rounding
    max values up.
    
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>
    Acked-by: Chanwoo Choi <cw00.choi@samsung.com>
    Reviewed-by: Matthias Kaehlcke <mka@chromium.org>
    Tested-by: Matthias Kaehlcke <mka@chromium.org>
    [cw00.choi: Return -EAGAIN instead of -EINVAL if dev_pm_qos is inactive]
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- include/linux/devfreq.h
+++ include/linux/devfreq.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/notifier.h>
 #include <linux/pm_opp.h>
+#include <linux/pm_qos.h>
 
 #define DEVFREQ_NAME_LEN 16
 
@@ -124,8 +125,8 @@ struct devfreq_dev_profile {
  * @previous_freq:	previously configured frequency value.
  * @data:	Private data of the governor. The devfreq framework does not
  *		touch this.
- * @min_freq:	Limit minimum frequency requested by user (0: none)
- * @max_freq:	Limit maximum frequency requested by user (0: none)
+ * @user_min_freq_req:	PM QoS minimum frequency request from user (via sysfs)
+ * @user_max_freq_req:	PM QoS maximum frequency request from user (via sysfs)
  * @scaling_min_freq:	Limit minimum frequency requested by OPP interface
  * @scaling_max_freq:	Limit maximum frequency requested by OPP interface
  * @stop_polling:	 devfreq polling status of a device.
@@ -164,8 +165,8 @@ struct devfreq {
 
 	void *data; /* private data for governors */
 
-	unsigned long min_freq;
-	unsigned long max_freq;
+	struct dev_pm_qos_request user_min_freq_req;
+	struct dev_pm_qos_request user_max_freq_req;
 	unsigned long scaling_min_freq;
 	unsigned long scaling_max_freq;
 	bool stop_polling;
