4592bc1e75219952352d8d569ff4dd4247085bde
Linus Torvalds <torvalds@linux-foundation.org>
2019-12-06 12:40:35 -0800

    pipe: fix incorrect caching of pipe state over pipe_wait()
    Similarly to commit 8f868d68d335 ("pipe: Fix missing mask update after
    pipe_wait()") this fixes a case where the pipe rewrite ended up caching
    the pipe state incorrectly over a pipe lock drop event.
    
    It wasn't quite as obvious, because you needed to splice data from a
    pipe to a file, which is a fairly unusual operation, but it's completely
    wrong.
    
    Make sure we load the pipe head/tail/size information only after we've
    waited for there to be data in the pipe.
    
    While in that file, also make one of the splice helper functions use the
    canonical arghument order for pipe_empty().  That's syntactic - pipe
    emptiness is just that head and tail are equal, and thus mixing up head
    and tail doesn't really matter.  It's still wrong, though.
    
    Reported-by: David Sterba <dsterba@suse.cz>
    Cc: David Howells <dhowells@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/splice.c
+++ fs/splice.c
@@ -496,7 +496,7 @@ static int splice_from_pipe_feed(struct pipe_inode_info *pipe, struct splice_des
 	unsigned int mask = pipe->ring_size - 1;
 	int ret;
 
-	while (!pipe_empty(tail, head)) {
+	while (!pipe_empty(head, tail)) {
 		struct pipe_buffer *buf = &pipe->bufs[tail & mask];
 
 		sd->len = buf->len;
@@ -712,9 +712,7 @@ iter_file_splice_write(struct pipe_inode_info *pipe, struct file *out,
 	splice_from_pipe_begin(&sd);
 	while (sd.total_len) {
 		struct iov_iter from;
-		unsigned int head = pipe->head;
-		unsigned int tail = pipe->tail;
-		unsigned int mask = pipe->ring_size - 1;
+		unsigned int head, tail, mask;
 		size_t left;
 		int n;
 
@@ -732,6 +730,10 @@ iter_file_splice_write(struct pipe_inode_info *pipe, struct file *out,
 				break;
 			}
 		}
+
+		head = pipe->head;
+		tail = pipe->tail;
+		mask = pipe->ring_size - 1;
 
 		/* build the vector */
 		left = sd.total_len;
