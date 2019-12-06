d988039aa07ec1f7d96a36024052d8abfa44f461
Paul E. McKenney <paulmck@kernel.org>
2019-12-06 15:02:59 -0800

    torture: Allow disabling of boottime CPU-hotplug torture operations
    In theory, RCU-hotplug operations are supposed to work as soon as there
    is more than one CPU online.  However, in practice, in normal production
    there is no way to make them happen until userspace is up and running.
    Besides which, on smaller systems, rcutorture doesn't start doing hotplug
    operations until 30 seconds after the start of boot, which on most
    systems also means the better part of 30 seconds after the end of boot.
    This commit therefore provides a new torture.disable_onoff_at_boot kernel
    boot parameter that suppresses CPU-hotplug torture operations until
    about the time that init is spawned.
    
    Of course, if you know of a need for boottime CPU-hotplug operations,
    then you should avoid passing this argument to any of the torture tests.
    You might also want to look at the splats linked to below.
    
    Link: https://lore.kernel.org/lkml/20191206185208.GA25636@paulmck-ThinkPad-P72/
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/torture.c
+++ kernel/torture.c
@@ -43,6 +43,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Paul E. McKenney <paulmck@linux.ibm.com>");
 
+static bool disable_onoff_at_boot;
+module_param(disable_onoff_at_boot, bool, 0444);
+
 static char *torture_type;
 static int verbose;
 
@@ -230,6 +233,10 @@ torture_onoff(void *arg)
 		VERBOSE_TOROUT_STRING("torture_onoff end holdoff");
 	}
 	while (!torture_must_stop()) {
+		if (disable_onoff_at_boot && !rcu_inkernel_boot_has_ended()) {
+			schedule_timeout_interruptible(HZ / 10);
+			continue;
+		}
 		cpu = (torture_random(&rand) >> 4) % (maxcpu + 1);
 		if (!torture_offline(cpu,
 				     &n_offline_attempts, &n_offline_successes,
