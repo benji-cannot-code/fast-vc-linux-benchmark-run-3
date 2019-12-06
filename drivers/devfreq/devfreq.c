bf5cbda9067afd221c8d7315fc3fd5b72236fa17
Kamil Konieczny <k.konieczny@samsung.com>
2019-12-06 11:11:29 +0100

    PM / devfreq: Move declaration of DEVICE_ATTR_RW(min_freq)
    Declaration of DEVICE_ATTR_RW(min_freq) is placed after function
    max_freq_store. Move it to the correct place after min_freq_show.
    
    Signed-off-by: Kamil Konieczny <k.konieczny@samsung.com>
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -1471,6 +1471,7 @@ static ssize_t min_freq_show(struct device *dev, struct device_attribute *attr,
 
 	return sprintf(buf, "%lu\n", min_freq);
 }
+static DEVICE_ATTR_RW(min_freq);
 
 static ssize_t max_freq_store(struct device *dev, struct device_attribute *attr,
 			      const char *buf, size_t count)
@@ -1511,7 +1512,6 @@ static ssize_t max_freq_store(struct device *dev, struct device_attribute *attr,
 
 	return count;
 }
-static DEVICE_ATTR_RW(min_freq);
 
 static ssize_t max_freq_show(struct device *dev, struct device_attribute *attr,
 			     char *buf)
