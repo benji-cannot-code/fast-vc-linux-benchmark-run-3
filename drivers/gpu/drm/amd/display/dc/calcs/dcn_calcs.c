929c24840afacf659827ece76c690a4471c08e35
Timothy Pearson <tpearson@raptorengineering.com>
2019-12-07 16:47:46 -0600

    amdgpu: Enable initial DCN support on POWER
    DCN requires floating point support to operate.  Add the appropriate
    x86/ppc64 guards and FPU / AltiVec / VSX context switches to DCN.
    
    Note that the current DC20 code doesn't contain all required FPU
    wrappers on x86 or POWER, so this patch is insufficient to fully
    enable DC20 on POWER.
    
    v2: s/X86_64/X86/g to retain previous behavior.
    
    Signed-off-by: Timothy Pearson <tpearson@raptorengineering.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/calcs/dcn_calcs.c
+++ drivers/gpu/drm/amd/display/dc/calcs/dcn_calcs.c
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Copyright 2017 Advanced Micro Devices, Inc.
+ * Copyright 2019 Raptor Engineering, LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
