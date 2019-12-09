b3f984bb88f66c180eac0593b52e0b8594039291
Jyri Sarha <jsarha@ti.com>
2019-12-09 16:42:14 +0200

    drm/tilcdc: Remove obsolete bundled tilcdc tfp410 driver
    Remove obsolete bundled tfp410 driver with its "ti,tilcdc,tfp410"
    devicetree binding. No platform has ever used this driver in the
    mainline kernel and if anybody connects tfp410 to tilcdc he or she
    should use the generic drm tfp410 bridge driver.
    
    Signed-off-by: Jyri Sarha <jsarha@ti.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/6e2db6c328467cc51e8d633ecb0ffa7c5736f2e8.1575901747.git.jsarha@ti.com

--- drivers/gpu/drm/tilcdc/tilcdc_drv.c
+++ drivers/gpu/drm/tilcdc/tilcdc_drv.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "tilcdc_external.h"
 #include "tilcdc_panel.h"
 #include "tilcdc_regs.h"
-#include "tilcdc_tfp410.h"
 
 static LIST_HEAD(module_list);
 
@@ -644,7 +643,6 @@ static struct platform_driver tilcdc_platform_driver = {
 static int __init tilcdc_drm_init(void)
 {
 	DBG("init");
-	tilcdc_tfp410_init();
 	tilcdc_panel_init();
 	return platform_driver_register(&tilcdc_platform_driver);
 }
@@ -654,7 +652,6 @@ static void __exit tilcdc_drm_fini(void)
 	DBG("fini");
 	platform_driver_unregister(&tilcdc_platform_driver);
 	tilcdc_panel_fini();
-	tilcdc_tfp410_fini();
 }
 
 module_init(tilcdc_drm_init);
