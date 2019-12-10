b3be192db32653c1f591466bb6314fee823a4332
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2019-12-10 15:36:59 +0100

    tty: serial: samsung.h: fix up minor comment issues
    checkpatch found some minor issues with comments in samsung.h, so fix
    that up.
    
    Cc: Kukjin Kim <kgene@kernel.org>
    Cc: Krzysztof Kozlowski <krzk@kernel.org>
    Cc: Hyunki Koo <kkoos00@naver.com>
    Cc: HYUN-KI KOO <hyunki00.koo@samsung.com>
    Cc: Shinbeom Choi <sbeom.choi@samsung.com>
    Cc: Jiri Slaby <jslaby@suse.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-samsung-soc@vger.kernel.org
    Cc: linux-serial@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Link: https://lore.kernel.org/r/20191210143706.3928480-3-gregkh@linuxfoundation.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/serial/samsung.h
+++ drivers/tty/serial/samsung.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-// SPDX-License-Identifier: GPL-2.0
+/* SPDX-License-Identifier: GPL-2.0 */
 #ifndef __SAMSUNG_H
 #define __SAMSUNG_H
 
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Ben Dooks, Copyright (c) 2003-2008 Simtec Electronics
  *	http://armlinux.simtec.co.uk/
-*/
+ */
 
 #include <linux/dmaengine.h>
 
