36a563caba22ae9027958ecedda419f706b01cc6
David Howells <dhowells@redhat.com>
2019-12-10 07:31:10 -0500

    NFS: Do some tidying of the parsing code
    Do some tidying of the parsing code, including:
    
     (*) Returning 0/error rather than true/false.
    
     (*) Putting the nfs_fs_context pointer first in some arg lists.
    
     (*) Unwrap some lines that will now fit on one line.
    
     (*) Provide unioned sockaddr/sockaddr_storage fields to avoid casts.
    
     (*) nfs_parse_devname() can paste its return values directly into the
         nfs_fs_context struct as that's where the caller puts them.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- fs/nfs/internal.h
+++ fs/nfs/internal.h
@@ -83,11 +83,11 @@ struct nfs_client_initdata {
  * In-kernel mount arguments
  */
 struct nfs_fs_context {
-	int			flags;
+	unsigned int		flags;		/* NFS{,4}_MOUNT_* flags */
 	unsigned int		rsize, wsize;
 	unsigned int		timeo, retrans;
-	unsigned int		acregmin, acregmax,
-				acdirmin, acdirmax;
+	unsigned int		acregmin, acregmax;
+	unsigned int		acdirmin, acdirmax;
 	unsigned int		namlen;
 	unsigned int		options;
 	unsigned int		bsize;
@@ -103,7 +103,10 @@ struct nfs_fs_context {
 	bool			sloppy;
 
 	struct {
-		struct sockaddr_storage	address;
+		union {
+			struct sockaddr	address;
+			struct sockaddr_storage	_address;
+		};
 		size_t			addrlen;
 		char			*hostname;
 		u32			version;
@@ -112,7 +115,10 @@ struct nfs_fs_context {
 	} mount_server;
 
 	struct {
-		struct sockaddr_storage	address;
+		union {
+			struct sockaddr	address;
+			struct sockaddr_storage	_address;
+		};
 		size_t			addrlen;
 		char			*hostname;
 		char			*export_path;
