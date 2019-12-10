8759ab43661c18b33af76bc12aef990dce9cee0e
Takashi Iwai <tiwai@suse.de>
2019-12-10 07:34:32 +0100

    ALSA: rme9652: Support PCM sync_stop
    The driver invokes snd_pcm_period_elapsed() simply from the interrupt
    handler.  Set card->sync_irq for enabling the missing sync_stop PCM
    operation.
    
    Link: https://lore.kernel.org/r/20191210063454.31603-34-tiwai@suse.de
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/rme9652/hdsp.c
+++ sound/pci/rme9652/hdsp.c
@@ -5234,6 +5234,7 @@ static int snd_hdsp_create(struct snd_card *card,
 	}
 
 	hdsp->irq = pci->irq;
+	card->sync_irq = hdsp->irq;
 	hdsp->precise_ptr = 0;
 	hdsp->use_midi_tasklet = 1;
 	hdsp->dds_value = 0;
