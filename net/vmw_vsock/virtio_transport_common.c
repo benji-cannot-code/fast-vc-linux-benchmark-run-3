a5e59b8a3e6d19fc1e2e9ae3cad2c32f0d1c670d
Stefano Garzarella <sgarzare@redhat.com>
2019-12-10 11:43:02 +0100

    vsock/virtio_transport_common: remove unused virtio header includes
    We can remove virtio header includes, because virtio_transport_common
    doesn't use virtio API, but provides common functions to interface
    virtio/vhost transports with the af_vsock core, and to handle
    the protocol.
    
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/vmw_vsock/virtio_transport_common.c
+++ net/vmw_vsock/virtio_transport_common.c
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched/signal.h>
 #include <linux/ctype.h>
 #include <linux/list.h>
-#include <linux/virtio.h>
-#include <linux/virtio_ids.h>
-#include <linux/virtio_config.h>
 #include <linux/virtio_vsock.h>
 #include <uapi/linux/vsockmon.h>
 
