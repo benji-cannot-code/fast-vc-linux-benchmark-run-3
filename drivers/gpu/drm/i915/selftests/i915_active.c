4b132eb74eba5d637e88b172d7f3f28341546f6d
Chris Wilson <chris@chris-wilson.co.uk>
2019-12-10 11:55:02 +0000

    drm/i915/selftests: Show the i915_active on failure
    Print the i915_active state on selftest failure, with a hope it helps
    illuminate the cause of the failure.
    
    References: https://gitlab.freedesktop.org/drm/intel/issues/765
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Acked-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191210115502.3767070-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/selftests/i915_active.c
+++ drivers/gpu/drm/i915/selftests/i915_active.c
@@ -156,7 +156,11 @@ static int live_active_wait(void *arg)
 
 	i915_active_wait(&active->base);
 	if (!READ_ONCE(active->retired)) {
+		struct drm_printer p = drm_err_printer(__func__);
+
 		pr_err("i915_active not retired after waiting!\n");
+		i915_active_print(&active->base, &p);
+
 		err = -EINVAL;
 	}
 
@@ -185,7 +189,11 @@ static int live_active_retire(void *arg)
 		err = -EIO;
 
 	if (!READ_ONCE(active->retired)) {
+		struct drm_printer p = drm_err_printer(__func__);
+
 		pr_err("i915_active not retired after flushing!\n");
+		i915_active_print(&active->base, &p);
+
 		err = -EINVAL;
 	}
 
