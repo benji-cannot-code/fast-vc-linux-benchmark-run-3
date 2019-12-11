9319880ff0afd30d3129504ca31867ebd0007b43
Colin Ian King <colin.king@canonical.com>
2019-12-11 08:34:43 +0000

    ath11k: fix several spelling mistakes
    There are several spelling mistakes in warning and debug messages,
    fix them.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath11k/debug.c
+++ drivers/net/wireless/ath/ath11k/debug.c
@@ -705,7 +705,7 @@ static ssize_t ath11k_write_extd_rx_stats(struct file *file,
 					       DP_RX_BUFFER_SIZE, &tlv_filter);
 
 	if (ret) {
-		ath11k_warn(ar->ab, "failed to set rx filter for moniter status ring\n");
+		ath11k_warn(ar->ab, "failed to set rx filter for monitor status ring\n");
 		goto exit;
 	}
 
@@ -949,7 +949,7 @@ static ssize_t ath11k_write_pktlog_filter(struct file *file,
 					       HAL_RXDMA_MONITOR_STATUS,
 					       DP_RX_BUFFER_SIZE, &tlv_filter);
 	if (ret) {
-		ath11k_warn(ar->ab, "failed to set rx filter for moniter status ring\n");
+		ath11k_warn(ar->ab, "failed to set rx filter for monitor status ring\n");
 		goto out;
 	}
 
