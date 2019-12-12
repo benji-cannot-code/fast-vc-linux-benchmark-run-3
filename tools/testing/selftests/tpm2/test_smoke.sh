ea0aab0a19c4fa1d181519e488ee55d39bed1896
Tadeusz Struk <tadeusz.struk@intel.com>
2019-12-12 09:48:59 -0800

    tpm: selftest: cleanup after unseal with wrong auth/policy test
    Unseal with wrong auth or wrong policy test affects DA lockout
    and eventually causes the tests to fail with:
    "ProtocolError: TPM_RC_LOCKOUT: rc=0x00000921"
    when the tests run multiple times.
    Send tpm clear command after the test to reset the DA counters.
    
    Signed-off-by: Tadeusz Struk <tadeusz.struk@intel.com>
    Reviewed-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>

--- tools/testing/selftests/tpm2/test_smoke.sh
+++ tools/testing/selftests/tpm2/test_smoke.sh
@@ -4,3 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 python -m unittest -v tpm2_tests.SmokeTest
 python -m unittest -v tpm2_tests.AsyncTest
+
+CLEAR_CMD=$(which tpm2_clear)
+if [ -n $CLEAR_CMD ]; then
+	tpm2_clear -T device
+fi
