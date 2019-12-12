cea1fe7dd48e0ac8576d3a7585383912a6c3419a
Chen Zhou <chenzhou10@huawei.com>
2019-12-12 10:35:56 +0800

    scsi: sym53c8xx: fix typos in comments
    Fix the typo "GPOI" -> "GPIO" in comment.
    
    Link: https://lore.kernel.org/r/20191212023556.72618-1-chenzhou10@huawei.com
    Signed-off-by: Chen Zhou <chenzhou10@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/sym53c8xx_2/sym_nvram.c
+++ drivers/scsi/sym53c8xx_2/sym_nvram.c
@@ -228,7 +228,7 @@ static void sym_display_Tekram_nvram(struct sym_device *np, Tekram_nvram *nvram)
 /*
  *  24C16 EEPROM reading.
  *
- *  GPOI0 - data in/data out
+ *  GPIO0 - data in/data out
  *  GPIO1 - clock
  *  Symbios NVRAM wiring now also used by Tekram.
  */
@@ -525,7 +525,7 @@ static int sym_read_Symbios_nvram(struct sym_device *np, Symbios_nvram *nvram)
 /*
  *  93C46 EEPROM reading.
  *
- *  GPOI0 - data in
+ *  GPIO0 - data in
  *  GPIO1 - data out
  *  GPIO2 - clock
  *  GPIO4 - chip select
