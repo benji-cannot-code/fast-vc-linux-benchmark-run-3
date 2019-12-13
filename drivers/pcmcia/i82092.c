671d96182eb7f29b962c2c79d2cd3e863c4787d6
Simon Geis <simon.geis@fau.de>
2019-12-13 14:53:12 +0100

    PCMCIA/i82092: include <linux/io.h> instead of <asm/io.h>
    Include the more general linux/io.h instead of asm/io.h
    as checkpatch suggests.
    
    Co-developed-by: Lukas Panzer <lukas.panzer@fau.de>
    Signed-off-by: Lukas Panzer <lukas.panzer@fau.de>
    Signed-off-by: Simon Geis <simon.geis@fau.de>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/pcmcia/i82092.c
+++ drivers/pcmcia/i82092.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <pcmcia/ss.h>
 
-#include <asm/io.h>
+#include <linux/io.h>
 
 #include "i82092aa.h"
 #include "i82365.h"
