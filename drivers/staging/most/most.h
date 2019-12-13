d44daf054c1dac30b7cda223a197d6e6536b7b97
Christian Gromm <christian.gromm@microchip.com>
2019-12-13 13:04:16 +0100

    staging: most: rename enum mbo_status_flags
    The data structures of the most.h header file will be exposed
    to the kernel once the file is moved out of the staging area.
    This is why the name is prefixed with the string 'most'.
    
    Reported-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Christian Gromm <christian.gromm@microchip.com>
    Link: https://lore.kernel.org/r/1576238662-16512-4-git-send-email-christian.gromm@microchip.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/most/most.h
+++ drivers/staging/most/most.h
@@ -48,7 +48,7 @@ enum most_channel_data_type {
 	MOST_CH_SYNC = 1 << 5,
 };
 
-enum mbo_status_flags {
+enum most_status_flags {
 	/* MBO was processed successfully (data was send or received )*/
 	MBO_SUCCESS = 0,
 	/* The MBO contains wrong or missing information.  */
@@ -185,7 +185,7 @@ struct mbo {
 	dma_addr_t bus_address;
 	u16 buffer_length;
 	u16 processed_length;
-	enum mbo_status_flags status;
+	enum most_status_flags status;
 	void (*complete)(struct mbo *mbo);
 };
 
