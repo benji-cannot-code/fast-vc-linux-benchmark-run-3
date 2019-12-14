c9c8a9fad275d7c2b99995a0adf524764cb2d4da
Lorenzo Bianconi <lorenzo@kernel.org>
2019-12-14 17:52:54 +0100

    iio: imu: st_lsm6dsx: export max num of slave devices in st_lsm6dsx_shub_settings
    Export max number of slave devices supported by the i2c master
    controller in st_lsm6dsx_shub_settings data structure.
    This is a preliminary patch to support i2c master controller
    on lsm6dsm sensors
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_shub.c
+++ drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_shub.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "st_lsm6dsx.h"
 
-#define ST_LSM6DSX_MAX_SLV_NUM			3
 #define ST_LSM6DSX_SLV_ADDR(n, base)		((base) + (n) * 3)
 #define ST_LSM6DSX_SLV_SUB_ADDR(n, base)	((base) + 1 + (n) * 3)
 #define ST_LSM6DSX_SLV_CONFIG(n, base)		((base) + 2 + (n) * 3)
@@ -771,7 +770,7 @@ int st_lsm6dsx_shub_probe(struct st_lsm6dsx_hw *hw, const char *name)
 		if (err < 0)
 			return err;
 
-		if (++num_ext_dev >= ST_LSM6DSX_MAX_SLV_NUM)
+		if (++num_ext_dev >= hw->settings->shub_settings.num_ext_dev)
 			break;
 		id++;
 	}
