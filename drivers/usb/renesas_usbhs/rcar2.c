4e797aad5e4f91a91df6b03358ec8c287445828c
Linus Walleij <linus.walleij@linaro.org>
2019-12-17 15:12:41 +0100

    usb: renesas_usbhs: Switch to GPIO descriptor
    The Renesas USBHS driver includes a bit of surplus headers
    and uses the old GPIO API so let's switch it to use the
    GPIO descriptor.
    
    I noticed that the enable_gpio inside renesas_usbhs_driver_param
    isn't really referenced anywhere, and it is also the wrong
    type (u32) so let's just delete it and use a local variable
    instead.
    
    Cc: Eugeniu Rosca <erosca@de.adit-jv.com>
    Cc: Veeraiyan Chidambaram <veeraiyan.chidambaram@in.bosch.com>
    Cc: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20191217141241.57639-1-linus.walleij@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/renesas_usbhs/rcar2.c
+++ drivers/usb/renesas_usbhs/rcar2.c
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2019 Renesas Electronics Corporation
  */
 
-#include <linux/gpio.h>
-#include <linux/of_gpio.h>
 #include <linux/phy/phy.h>
 #include "common.h"
 #include "rcar2.h"
