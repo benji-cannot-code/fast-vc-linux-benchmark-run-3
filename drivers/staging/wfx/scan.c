46ab444e5323f24a54576e8bd0760acfa8ec70e5
Jérôme Pouiller <jerome.pouiller@silabs.com>
2019-12-17 16:15:30 +0000

    staging: wfx: drop useless wfx_scan_complete()
    Since wfx_scan_complete() is now only called from
    wfx_scan_complete_cb(), it make sense to merge the both functions.
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>
    Link: https://lore.kernel.org/r/20191217161318.31402-47-Jerome.Pouiller@silabs.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wfx/scan.c
+++ drivers/staging/wfx/scan.c
@@ -229,12 +229,6 @@ fail:
 	schedule_work(&wvif->scan.work);
 }
 
-static void wfx_scan_complete(struct wfx_vif *wvif)
-{
-	up(&wvif->scan.lock);
-	wfx_scan_work(&wvif->scan.work);
-}
-
 void wfx_scan_complete_cb(struct wfx_vif *wvif,
 			  const struct hif_ind_scan_cmpl *arg)
 {
@@ -258,6 +252,7 @@ void wfx_scan_timeout(struct work_struct *work)
 			wvif->scan.curr = wvif->scan.end;
 			hif_stop_scan(wvif);
 		}
-		wfx_scan_complete(wvif);
+		up(&wvif->scan.lock);
+		wfx_scan_work(&wvif->scan.work);
 	}
 }
