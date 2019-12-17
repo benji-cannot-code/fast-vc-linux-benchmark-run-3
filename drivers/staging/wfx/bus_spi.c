f306dd9a3870168592fc287742bd26a18b8118b9
Jérôme Pouiller <jerome.pouiller@silabs.com>
2019-12-17 16:14:51 +0000

    staging: wfx: uniformize naming rule
    In wfx driver, when a function is used as a struct member, its name is
    the name of the member prefixed with "wfx_".
    
    This patch apply this rule to wfx_spi_remove().
    
    Also remove the useless comment above the function.
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>
    Link: https://lore.kernel.org/r/20191217161318.31402-17-Jerome.Pouiller@silabs.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wfx/bus_spi.c
+++ drivers/staging/wfx/bus_spi.c
@@ -224,8 +224,7 @@ static int wfx_spi_probe(struct spi_device *func)
 	return ret;
 }
 
-/* Disconnect Function to be called by SPI stack when device is disconnected */
-static int wfx_spi_disconnect(struct spi_device *func)
+static int wfx_spi_remove(struct spi_device *func)
 {
 	struct wfx_spi_priv *bus = spi_get_drvdata(func);
 
@@ -264,5 +263,5 @@ struct spi_driver wfx_spi_driver = {
 	},
 	.id_table = wfx_spi_id,
 	.probe = wfx_spi_probe,
-	.remove = wfx_spi_disconnect,
+	.remove = wfx_spi_remove,
 };
