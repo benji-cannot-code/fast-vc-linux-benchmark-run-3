ee4bb439230e5456818a5541260566596f2ae70b
Erik Kaneda <erik.kaneda@intel.com>
2019-12-17 11:35:21 -0800

    ACPICA: acpisrc: add unix line ending support for non-windows build
    ACPICA commit 48ef9f7456f0a73d1d6023ea8e79442cdcff757f
    
    Link: https://github.com/acpica/acpica/commit/48ef9f74
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Erik Kaneda <erik.kaneda@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- include/acpi/platform/acenv.h
+++ include/acpi/platform/acenv.h
@@ -129,6 +129,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 
+/*
+ * acpisrc CR\LF support
+ * Unix file line endings do not include the carriage return.
+ * If the acpisrc utility is being built using a microsoft compiler, it means
+ * that it will be running on a windows machine which means that the output is
+ * expected to have CR/LF newlines. If the acpisrc utility is built with
+ * anything else, it will likely run on a system with LF newlines. This flag
+ * tells the acpisrc utility that newlines will be in the LF format.
+ */
+#define ACPI_SRC_OS_LF_ONLY 0
+
 /*! [Begin] no source code translation */
 
 /******************************************************************************
