96e56964130832f0f82d59d15ec9fe1565d58bcb
Alex Deucher <alexander.deucher@amd.com>
2019-12-17 15:39:04 -0500

    drm/amdgpu/display: include delay.h
    For udelay.  This is needed for some platforms.
    
    Reviewed-by: Nicholas Kazlauskas <nicholas.kazluaskas@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/modules/hdcp/hdcp2_execution.c
+++ drivers/gpu/drm/amd/display/modules/hdcp/hdcp2_execution.c
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+#include <linux/delay.h>
+
 #include "hdcp.h"
 
 static inline enum mod_hdcp_status check_receiver_id_list_ready(struct mod_hdcp *hdcp)
