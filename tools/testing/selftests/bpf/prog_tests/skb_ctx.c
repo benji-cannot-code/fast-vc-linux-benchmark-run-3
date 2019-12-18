e60dabb3acb65414fdc31fab465fab4807798c03
Nikita V. Shirokov <tehnerd@tehnerd.com>
2019-12-18 12:57:47 -0800

    bpf: Allow to change skb mark in test_run
    allow to pass skb's mark field into bpf_prog_test_run ctx
    for BPF_PROG_TYPE_SCHED_CLS prog type. that would allow
    to test bpf programs which are doing decision based on this
    field
    
    Signed-off-by: Nikita V. Shirokov <tehnerd@tehnerd.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/skb_ctx.c
+++ tools/testing/selftests/bpf/prog_tests/skb_ctx.c
@@ -14,6 +14,7 @@ void test_skb_ctx(void)
 		.tstamp = 7,
 		.wire_len = 100,
 		.gso_segs = 8,
+		.mark = 9,
 	};
 	struct bpf_prog_test_run_attr tattr = {
 		.data_in = &pkt_v4,
@@ -94,4 +95,8 @@ void test_skb_ctx(void)
 		   "ctx_out_tstamp",
 		   "skb->tstamp == %lld, expected %d\n",
 		   skb.tstamp, 8);
+	CHECK_ATTR(skb.mark != 10,
+		   "ctx_out_mark",
+		   "skb->mark == %u, expected %d\n",
+		   skb.mark, 10);
 }
