86456f7985472488ec626239bd2e113f77275022
Masahiro Yamada <masahiroy@kernel.org>
2019-12-18 23:43:07 +0900

    drivers/component: remove modular code
    drivers/base/Makefile always adds component.o to obj-y, like this:
    
      obj-y                   := component.o core.o bus.o dd.o syscore.o \
    
    Hence it is never compiled as a module. Remove useless modular code.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
    Link: https://lore.kernel.org/r/20191218144307.19243-1-masahiroy@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/component.c
+++ drivers/base/component.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/kref.h>
 #include <linux/list.h>
-#include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/slab.h>
 #include <linux/debugfs.h>
@@ -776,5 +775,3 @@ void component_del(struct device *dev, const struct component_ops *ops)
 	kfree(component);
 }
 EXPORT_SYMBOL_GPL(component_del);
-
-MODULE_LICENSE("GPL v2");
