6069bd8810afc4b904dc884670acc2acc4744821
Eric Biggers <ebiggers@google.com>
2019-12-19 10:56:24 -0800

    fscrypt: include <linux/ioctl.h> in UAPI header
    <linux/fscrypt.h> defines ioctl numbers using the macros like _IOWR()
    which are defined in <linux/ioctl.h>, so <linux/ioctl.h> should be
    included as a prerequisite, like it is in many other kernel headers.
    
    In practice this doesn't really matter since anyone referencing these
    ioctl numbers will almost certainly include <sys/ioctl.h> too in order
    to actually call ioctl().  But we might as well fix this.
    
    Link: https://lore.kernel.org/r/20191219185624.21251-1-ebiggers@kernel.org
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- include/uapi/linux/fscrypt.h
+++ include/uapi/linux/fscrypt.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _UAPI_LINUX_FSCRYPT_H
 #define _UAPI_LINUX_FSCRYPT_H
 
+#include <linux/ioctl.h>
 #include <linux/types.h>
 
 /* Encryption policy flags */
