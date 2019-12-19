d3147e849e479bdd676a4f94f682fa5808db91f9
Gurchetan Singh <gurchetansingh@chromium.org>
2019-12-18 16:57:33 -0800

    drm/virtio: move drm_connector_to_virtio_gpu_output to virtgpu_display
    That's the only file that uses it.
    
    Signed-off-by: Gurchetan Singh <gurchetansingh@chromium.org>
    Link: http://patchwork.freedesktop.org/patch/msgid/20191219005733.18960-6-gurchetansingh@chromium.org
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- drivers/gpu/drm/virtio/virtgpu_drv.h
+++ drivers/gpu/drm/virtio/virtgpu_drv.h
@@ -134,8 +134,6 @@ struct virtio_gpu_output {
 };
 #define drm_crtc_to_virtio_gpu_output(x) \
 	container_of(x, struct virtio_gpu_output, crtc)
-#define drm_connector_to_virtio_gpu_output(x) \
-	container_of(x, struct virtio_gpu_output, conn)
 
 struct virtio_gpu_framebuffer {
 	struct drm_framebuffer base;
