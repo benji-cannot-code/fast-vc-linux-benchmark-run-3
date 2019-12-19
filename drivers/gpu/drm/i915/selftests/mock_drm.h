a4f205f03a62655337693c62e3f003743e3ddde6
Jani Nikula <jani.nikula@intel.com>
2019-12-19 17:56:51 +0200

    drm/i915/selftests: make mock_drm.h self-contained
    Needs i915_drv.h because i915 gets dereferenced.
    
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Masahiro Yamada <yamada.masahiro@socionext.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191219155652.2666-2-jani.nikula@intel.com

--- drivers/gpu/drm/i915/selftests/mock_drm.h
+++ drivers/gpu/drm/i915/selftests/mock_drm.h
@@ -28,7 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <drm/drm_file.h>
 
-struct drm_i915_private;
+#include "i915_drv.h"
+
 struct drm_file;
 struct file;
 
