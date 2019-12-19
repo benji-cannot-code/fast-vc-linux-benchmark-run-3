1d7c7db3be1bb45b97762386fc45258bf290625c
Gurchetan Singh <gurchetansingh@chromium.org>
2019-12-18 16:57:32 -0800

    drm/virtio: move to_virtio_fence inside virtgpu_fence
    That's the only file that uses it.
    
    Signed-off-by: Gurchetan Singh <gurchetansingh@chromium.org>
    Link: http://patchwork.freedesktop.org/patch/msgid/20191219005733.18960-5-gurchetansingh@chromium.org
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- drivers/gpu/drm/virtio/virtgpu_fence.c
+++ drivers/gpu/drm/virtio/virtgpu_fence.c
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "virtgpu_drv.h"
 
+#define to_virtio_fence(x) \
+	container_of(x, struct virtio_gpu_fence, f)
+
 static const char *virtio_get_driver_name(struct dma_fence *f)
 {
 	return "virtio_gpu";
