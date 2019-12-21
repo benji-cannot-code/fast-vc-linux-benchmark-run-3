87a709abc9e91dd28a9f5ebbdcf773fdbdcc19f0
Sean Christopherson <sean.j.christopherson@intel.com>
2019-12-20 20:45:04 -0800

    x86/cpu: Detect VMX features on Intel, Centaur and Zhaoxin CPUs
    Add an entry in struct cpuinfo_x86 to track VMX capabilities and fill
    the capabilities during IA32_FEAT_CTL MSR initialization.
    
    Make the VMX capabilities dependent on IA32_FEAT_CTL and
    X86_FEATURE_NAMES so as to avoid unnecessary overhead on CPUs that can't
    possibly support VMX, or when /proc/cpuinfo is not available.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/20191221044513.21680-11-sean.j.christopherson@intel.com

--- arch/x86/include/asm/vmxfeatures.h
+++ arch/x86/include/asm/vmxfeatures.h
@@ -4,6 +4,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_X86_VMXFEATURES_H
 
 /*
+ * Defines VMX CPU feature bits
+ */
+#define NVMXINTS			3 /* N 32-bit words worth of info */
+
+/*
  * Note: If the comment begins with a quoted string, that string is used
  * in /proc/cpuinfo instead of the macro name.  If the string is "",
  * this feature bit is not displayed in /proc/cpuinfo at all.
