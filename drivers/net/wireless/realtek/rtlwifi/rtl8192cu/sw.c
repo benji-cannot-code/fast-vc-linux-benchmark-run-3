a53dd87f4656531f0c7fddb8b8705886b7dc5ba5
Amadeusz Sławiński <amade@asmblr.net>
2019-12-23 13:37:10 +0100

    rtlwifi: rtl8192cu: Remove sw.h header
    It has one define, which is already defined in include from reg.h.
    All functions are declared in their own headers and included in *.c
    files belonging to them.
    This makes sw.h unnecessary, so we can remove it.
    
    Signed-off-by: Amadeusz Sławiński <amade@asmblr.net>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/realtek/rtlwifi/rtl8192cu/sw.c
+++ drivers/net/wireless/realtek/rtlwifi/rtl8192cu/sw.c
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "mac.h"
 #include "dm.h"
 #include "rf.h"
-#include "sw.h"
 #include "trx.h"
 #include "led.h"
 #include "hw.h"
