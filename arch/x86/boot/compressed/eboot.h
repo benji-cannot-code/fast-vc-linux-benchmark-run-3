85187cc94491e213d8634138026dccf6074fceda
Ard Biesheuvel <ardb@kernel.org>
2019-12-24 16:10:10 +0100

    efi/libstub: Drop explicit 32/64-bit protocol definitions
    Now that we have incorporated the mixed mode protocol definitions
    into the native ones using unions, we no longer need the separate
    32/64 bit struct definitions, with the exception of the EFI system
    table definition and the boot services, runtime services and
    configuration table definitions. So drop the unused ones.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Cc: Arvind Sankar <nivedita@alum.mit.edu>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: James Morse <james.morse@arm.com>
    Cc: Matt Fleming <matt@codeblueprint.co.uk>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-efi@vger.kernel.org
    Link: https://lkml.kernel.org/r/20191224151025.32482-11-ardb@kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/boot/compressed/eboot.h
+++ arch/x86/boot/compressed/eboot.h
@@ -13,18 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DESC_TYPE_CODE_DATA	(1 << 0)
 
-typedef struct {
-	u32 get_mode;
-	u32 set_mode;
-	u32 blt;
-} efi_uga_draw_protocol_32_t;
-
-typedef struct {
-	u64 get_mode;
-	u64 set_mode;
-	u64 blt;
-} efi_uga_draw_protocol_64_t;
-
 typedef union {
 	struct {
 		void *get_mode;
--- drivers/firmware/efi/libstub/random.c
+++ drivers/firmware/efi/libstub/random.c
@@ -12,16 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 typedef union efi_rng_protocol efi_rng_protocol_t;
 
-typedef struct {
-	u32 get_info;
-	u32 get_rng;
-} efi_rng_protocol_32_t;
-
-typedef struct {
-	u64 get_info;
-	u64 get_rng;
-} efi_rng_protocol_64_t;
-
 union efi_rng_protocol {
 	struct {
 		efi_status_t (*get_info)(efi_rng_protocol_t *,
