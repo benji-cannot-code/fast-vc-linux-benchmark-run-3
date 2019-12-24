ef0e453c807eb4518b4f1c18a7ad6eb36ced2452
Xu Wang <vulab@iscas.ac.cn>
2019-12-24 09:37:04 +0000

    ppp: Remove redundant BUG_ON() check in ppp_pernet
    Passing NULL to ppp_pernet causes a crash via BUG_ON.
    Dereferencing net in net_generic() also has the same effect.
    This patch removes the redundant BUG_ON check on the same parameter.
    
    Signed-off-by: Xu Wang <vulab@iscas.ac.cn>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ppp/ppp_generic.c
+++ drivers/net/ppp/ppp_generic.c
@@ -297,8 +297,6 @@ static struct class *ppp_class;
 /* per net-namespace data */
 static inline struct ppp_net *ppp_pernet(struct net *net)
 {
-	BUG_ON(!net);
-
 	return net_generic(net, ppp_net_id);
 }
 
