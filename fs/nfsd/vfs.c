dd1a8175985a5ac4d8a23f100547cd632fb37892
zhengbin <zhengbin13@huawei.com>
2019-12-25 11:19:34 +0800

    nfsd: use true,false for bool variable in vfs.c
    Fixes coccicheck warning:
    
    fs/nfsd/vfs.c:1389:5-13: WARNING: Assignment of 0/1 to bool variable
    fs/nfsd/vfs.c:1398:5-13: WARNING: Assignment of 0/1 to bool variable
    fs/nfsd/vfs.c:1415:2-10: WARNING: Assignment of 0/1 to bool variable
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: zhengbin <zhengbin13@huawei.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/vfs.c
+++ fs/nfsd/vfs.c
@@ -1397,7 +1397,7 @@ do_nfsd_create(struct svc_rqst *rqstp, struct svc_fh *fhp,
 			    && d_inode(dchild)->i_atime.tv_sec == v_atime
 			    && d_inode(dchild)->i_size  == 0 ) {
 				if (created)
-					*created = 1;
+					*created = true;
 				break;
 			}
 			/* fall through */
@@ -1406,7 +1406,7 @@ do_nfsd_create(struct svc_rqst *rqstp, struct svc_fh *fhp,
 			    && d_inode(dchild)->i_atime.tv_sec == v_atime
 			    && d_inode(dchild)->i_size  == 0 ) {
 				if (created)
-					*created = 1;
+					*created = true;
 				goto set_attr;
 			}
 			/* fall through */
@@ -1423,7 +1423,7 @@ do_nfsd_create(struct svc_rqst *rqstp, struct svc_fh *fhp,
 		goto out_nfserr;
 	}
 	if (created)
-		*created = 1;
+		*created = true;
 
 	nfsd_check_ignore_resizing(iap);
 
