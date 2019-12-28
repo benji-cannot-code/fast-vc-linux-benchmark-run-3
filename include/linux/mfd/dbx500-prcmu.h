7665d64a63c6df8c7a3006449be19bd7f834947f
Linus Walleij <linus.walleij@linaro.org>
2019-12-28 23:26:15 +0100

    mfd: dbx500-prcmu: Drop DSI pll clock functions
    The DSI PLLs are handled by the generic clock framework
    since ages, this code is completely unused and misleading.
    Delete it.
    
    Cc: Stephan Gerhold <stephan@gerhold.net>
    Cc: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/dbx500-prcmu.h
+++ include/linux/mfd/dbx500-prcmu.h
@@ -322,16 +322,6 @@ static inline bool prcmu_is_ac_wake_requested(void)
 	return db8500_prcmu_is_ac_wake_requested();
 }
 
-static inline int prcmu_disable_dsipll(void)
-{
-	return db8500_prcmu_disable_dsipll();
-}
-
-static inline int prcmu_enable_dsipll(void)
-{
-	return db8500_prcmu_enable_dsipll();
-}
-
 static inline int prcmu_config_esram0_deep_sleep(u8 state)
 {
 	return db8500_prcmu_config_esram0_deep_sleep(state);
@@ -505,16 +495,6 @@ static inline void prcmu_modem_reset(void) {}
 static inline bool prcmu_is_ac_wake_requested(void)
 {
 	return false;
-}
-
-static inline int prcmu_disable_dsipll(void)
-{
-	return 0;
-}
-
-static inline int prcmu_enable_dsipll(void)
-{
-	return 0;
 }
 
 static inline int prcmu_config_esram0_deep_sleep(u8 state)
