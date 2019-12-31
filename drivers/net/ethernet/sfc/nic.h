989daebd5dc124c3bd707407e90fdd9c8f814caf
Ben Hutchings <ben@decadent.org.uk>
2019-12-31 16:59:08 +0000

    sfc: Remove unnecessary dependencies on I2C
    Only the SFC4000 code, now moved to sfc-falcon, needed I2C.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Acked-by: Edward Cree <ecree@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/sfc/nic.h
+++ drivers/net/ethernet/sfc/nic.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EFX_NIC_H
 
 #include <linux/net_tstamp.h>
-#include <linux/i2c-algo-bit.h>
 #include "net_driver.h"
 #include "efx.h"
 #include "mcdi.h"
