1cc4301226982baaef8b5b7ad44d90b217f0e195
Andrew F. Davis <afd@ti.com>
2020-01-02 16:19:41 -0500

    ARM: OMAP2+: Introduce check for OP-TEE in omap_secure_init()
    This check and associated flag can be used to signal the presence
    of OP-TEE on the platform. This can be used to determine which
    SMC calls to make to perform secure operations.
    
    Signed-off-by: Andrew F. Davis <afd@ti.com>
    Reviewed-by: Lokesh Vutla <lokeshvutla@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/omap-secure.c
+++ arch/arm/mach-omap2/omap-secure.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/io.h>
 #include <linux/memblock.h>
+#include <linux/of.h>
 
 #include <asm/cacheflush.h>
 #include <asm/memblock.h>
@@ -20,6 +21,23 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "omap-secure.h"
 
 static phys_addr_t omap_secure_memblock_base;
+
+bool optee_available;
+
+static void __init omap_optee_init_check(void)
+{
+	struct device_node *np;
+
+	/*
+	 * We only check that the OP-TEE node is present and available. The
+	 * OP-TEE kernel driver is not needed for the type of interaction made
+	 * with OP-TEE here so the driver's status is not checked.
+	 */
+	np = of_find_node_by_path("/firmware/optee");
+	if (np && of_device_is_available(np))
+		optee_available = true;
+	of_node_put(np);
+}
 
 /**
  * omap_sec_dispatcher: Routine to dispatch low power secure
@@ -167,4 +185,5 @@ u32 rx51_secure_rng_call(u32 ptr, u32 count, u32 flag)
 
 void __init omap_secure_init(void)
 {
+	omap_optee_init_check();
 }
