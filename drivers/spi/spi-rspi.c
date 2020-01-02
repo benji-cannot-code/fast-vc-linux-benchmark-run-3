148e5498961628eaf3e61daaf4576b0b8290f896
Geert Uytterhoeven <geert+renesas@glider.be>
2020-01-02 14:38:20 +0100

    spi: rspi: Remove set_config_register() macro
    The set_config_register() macro is used in a single place.
    Make the code easier to read by just removing it.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://lore.kernel.org/r/20200102133822.29346-5-geert+renesas@glider.be
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-rspi.c
+++ drivers/spi/spi-rspi.c
@@ -427,8 +427,6 @@ static int qspi_set_receive_trigger(struct rspi_data *rspi, unsigned int len)
 	return n;
 }
 
-#define set_config_register(spi, n) spi->ops->set_config_register(spi, n)
-
 static void rspi_enable_irq(const struct rspi_data *rspi, u8 enable)
 {
 	rspi_write8(rspi, rspi_read8(rspi, RSPI_SPCR) | enable, RSPI_SPCR);
@@ -941,7 +939,7 @@ static int rspi_prepare_message(struct spi_controller *ctlr,
 	if (spi->mode & SPI_LOOP)
 		rspi->sppcr |= SPPCR_SPLP;
 
-	set_config_register(rspi, 8);
+	rspi->ops->set_config_register(rspi, 8);
 
 	if (msg->spi->mode &
 	    (SPI_TX_DUAL | SPI_TX_QUAD | SPI_RX_DUAL | SPI_RX_QUAD)) {
