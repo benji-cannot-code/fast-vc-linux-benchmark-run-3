dfee67b7672c21413e59f5505c3e6f12a6beefd3
Ard Biesheuvel <ardb@kernel.org>
2020-01-03 12:39:35 +0100

    efi/libstub/x86: Force 'hidden' visibility for extern declarations
    Commit c3710de5065d ("efi/libstub/x86: Drop __efi_early() export and
    efi_config struct") introduced a reference from C code in eboot.c to
    the startup_32 symbol defined in the .S startup code. This results in
    a GOT based reference to startup_32, and since GOT entries carry
    absolute addresses, they need to be fixed up before they can be used.
    
    On modern toolchains (binutils 2.26 or later), this reference is
    relaxed into a R_386_GOTOFF relocation (or the analogous X86_64 one)
    which never uses the absolute address in the entry, and so we get
    away with not fixing up the GOT table before calling the EFI entry
    point. However, GCC 4.6 combined with a binutils of the era (2.24)
    will produce a true GOT indirected reference, resulting in a wrong
    value to be returned for the address of startup_32() if the boot
    code is not running at the address it was linked at.
    
    Fortunately, we can easily override this behavior, and force GCC to
    emit the GOTOFF relocations explicitly, by setting the visibility
    pragma 'hidden'.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Arvind Sankar <nivedita@alum.mit.edu>
    Cc: Matthew Garrett <mjg59@google.com>
    Cc: linux-efi@vger.kernel.org
    Link: https://lkml.kernel.org/r/20200103113953.9571-3-ardb@kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- arch/x86/boot/compressed/eboot.c
+++ arch/x86/boot/compressed/eboot.c
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * ----------------------------------------------------------------------- */
 
+#pragma GCC visibility push(hidden)
+
 #include <linux/efi.h>
 #include <linux/pci.h>
 
