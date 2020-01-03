854634329b2c3e0901f969d16be9e0c9463faffd
Al Cooper <alcooperx@gmail.com>
2020-01-03 13:18:05 -0500

    phy: usb: Add support for new Synopsys USB controller on the 7216
    The 7216 has the new USB XHCI controller from Synopsys. While
    this new controller and the PHY are similar to the STB versions,
    the major differences are:
    
    - Many of the registers and fields in the CTRL block have been
      removed or changed.
    - A new set of Synopsys control registers, BCHP_USB_XHCI_GBL, were
      added.
    - MDIO functionality has been replaced with direct access registers
      in the BCHP_USB_XHCI_GBL block.
    - Power up PHY defaults that had to be changed by MDIO in previous
      chips will now power up with the correct defaults.
    
    A new init module was created for this new Synopsys USB controller.
    A new compatible string was added and the driver will dispatch
    into one of two init modules based on it. A "reg-names" field was
    added so the driver can more easily get optional registers.
    A DT bindings document was also added for this driver.
    
    Signed-off-by: Al Cooper <alcooperx@gmail.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

--- drivers/phy/broadcom/phy-brcm-usb-init.h
+++ drivers/phy/broadcom/phy-brcm-usb-init.h
@@ -44,6 +44,7 @@ struct brcm_usb_init_ops {
 struct  brcm_usb_init_params {
 	void __iomem *ctrl_regs;
 	void __iomem *xhci_ec_regs;
+	void __iomem *xhci_gbl_regs;
 	int ioc;
 	int ipp;
 	int mode;
@@ -56,6 +57,7 @@ struct  brcm_usb_init_params {
 };
 
 void brcm_usb_dvr_init_7445(struct brcm_usb_init_params *params);
+void brcm_usb_dvr_init_7216(struct brcm_usb_init_params *params);
 
 static inline u32 brcm_usb_readl(void __iomem *addr)
 {
