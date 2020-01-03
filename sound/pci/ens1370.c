e41af81ea2d44ecc5d9930b505f0f482c2ae387a
Takashi Iwai <tiwai@suse.de>
2020-01-03 09:16:25 +0100

    ALSA: pci: Constify snd_device_ops definitions
    Now we may declare const for snd_device_ops definitions, so let's do
    it for optimization.
    
    There should be no functional changes by this patch.
    
    Link: https://lore.kernel.org/r/20200103081714.9560-10-tiwai@suse.de
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/ens1370.c
+++ sound/pci/ens1370.c
@@ -2042,7 +2042,7 @@ static int snd_ensoniq_create(struct snd_card *card,
 {
 	struct ensoniq *ensoniq;
 	int err;
-	static struct snd_device_ops ops = {
+	static const struct snd_device_ops ops = {
 		.dev_free =	snd_ensoniq_dev_free,
 	};
 
