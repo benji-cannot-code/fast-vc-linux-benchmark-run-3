15355c5a52715c32ca1ae25f6539994b3b6cbd8d
Mark Brown <broonie@kernel.org>
2020-01-06 19:58:16 +0000

    arm64: asm: Add new-style position independent function annotations
    As part of an effort to make the annotations in assembly code clearer and
    more consistent new macros have been introduced, including replacements
    for ENTRY() and ENDPROC().
    
    On arm64 we have ENDPIPROC(), a custom version of ENDPROC() which is
    used for code that will need to run in position independent environments
    like EFI, it creates an alias for the function with the prefix __pi_ and
    then emits the standard ENDPROC. Add new-style macros to replace this
    which expand to the standard SYM_FUNC_*() and SYM_FUNC_ALIAS_*(),
    resulting in the same object code. These are added in linkage.h for
    consistency with where the generic assembler code has its macros.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    [will: Rename 'WEAK' macro, use ';' instead of ASM_NL, deprecate ENDPIPROC]
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/linkage.h
+++ arch/arm64/include/asm/linkage.h
@@ -5,4 +5,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ALIGN		.align 2
 #define __ALIGN_STR	".align 2"
 
+/*
+ * Annotate a function as position independent, i.e., safe to be called before
+ * the kernel virtual mapping is activated.
+ */
+#define SYM_FUNC_START_PI(x)			\
+		SYM_FUNC_START_ALIAS(__pi_##x);	\
+		SYM_FUNC_START(x)
+
+#define SYM_FUNC_START_WEAK_PI(x)		\
+		SYM_FUNC_START_ALIAS(__pi_##x);	\
+		SYM_FUNC_START_WEAK(x)
+
+#define SYM_FUNC_END_PI(x)			\
+		SYM_FUNC_END(x);		\
+		SYM_FUNC_END_ALIAS(__pi_##x)
+
 #endif
