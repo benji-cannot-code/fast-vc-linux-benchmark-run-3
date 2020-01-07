c132218cf1994b75be05d198b2ec989b2ae73154
Elliot Berman <eberman@codeaurora.org>
2020-01-07 13:04:12 -0800

    firmware: qcom_scm: Remove unused qcom_scm_get_version
    Remove unused qcom_scm_get_version.
    
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Reviewed-by: Vinod Koul <vkoul@kernel.org>
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>
    Tested-by: Brian Masney <masneyb@onstation.org> # arm32
    Tested-by: Stephan Gerhold <stephan@gerhold.net>
    Signed-off-by: Elliot Berman <eberman@codeaurora.org>
    Link: https://lore.kernel.org/r/1578431066-19600-4-git-send-email-eberman@codeaurora.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- include/linux/qcom_scm.h
+++ include/linux/qcom_scm.h
@@ -79,7 +79,6 @@ extern int qcom_scm_assign_mem(phys_addr_t mem_addr, size_t mem_sz,
 			       const struct qcom_scm_vmperm *newvm,
 			       unsigned int dest_cnt);
 extern void qcom_scm_cpu_power_down(u32 flags);
-extern u32 qcom_scm_get_version(void);
 extern int qcom_scm_set_remote_state(u32 state, u32 id);
 extern bool qcom_scm_restore_sec_cfg_available(void);
 extern int qcom_scm_restore_sec_cfg(u32 device_id, u32 spare);
@@ -124,7 +123,6 @@ static inline int qcom_scm_assign_mem(phys_addr_t mem_addr, size_t mem_sz,
 				      const struct qcom_scm_vmperm *newvm,
 				      unsigned int dest_cnt) { return -ENODEV; }
 static inline void qcom_scm_cpu_power_down(u32 flags) {}
-static inline u32 qcom_scm_get_version(void) { return 0; }
 static inline u32
 qcom_scm_set_remote_state(u32 state,u32 id) { return -ENODEV; }
 static inline bool qcom_scm_restore_sec_cfg_available(void) { return false; }
