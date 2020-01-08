6477513fd3293190c094a273eb99c82191e77c73
Alex Maftei (amaftei) <amaftei@solarflare.com>
2020-01-08 16:13:43 +0000

    sfc: move common rx code
    The moved code deals with managing rx buffers and queues.
    A tiny bit of refactoring was required in other files to stitch the
    code together.
    
    Signed-off-by: Alexandru-Mihai Maftei <amaftei@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/sfc/ef10.c
+++ drivers/net/ethernet/sfc/ef10.c
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include "net_driver.h"
+#include "rx_common.h"
 #include "ef10_regs.h"
 #include "io.h"
 #include "mcdi.h"
