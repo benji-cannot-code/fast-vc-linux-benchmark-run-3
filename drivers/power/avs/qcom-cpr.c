62a610644e51a54e02468083137a120176fd90e8
YueHaibing <yuehaibing@huawei.com>
2020-01-08 01:58:49 +0000

    power: avs: qcom-cpr: remove duplicated include from qcom-cpr.c
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Acked-by: Niklas Cassel <nks@flawful.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/power/avs/qcom-cpr.c
+++ drivers/power/avs/qcom-cpr.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/regulator/consumer.h>
 #include <linux/clk.h>
 #include <linux/nvmem-consumer.h>
-#include <linux/bitops.h>
 
 /* Register Offsets for RB-CPR and Bit Definitions */
 
