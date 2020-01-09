40205f9657d757613532536c407a312728ba7b24
Chen Zhou <chenzhou10@huawei.com>
2020-01-09 09:23:03 +0800

    drm/i915/gtt: add missing include file asm/smp.h
    Fix build error:
    drivers/gpu/drm/i915/gt/intel_ggtt.c: In function ggtt_restore_mappings:
    drivers/gpu/drm/i915/gt/intel_ggtt.c:1239:3: error:
    	implicit declaration of function wbinvd_on_all_cpus; did you mean wrmsr_on_cpus? [-Werror=implicit-function-declaration]
       wbinvd_on_all_cpus();
          ^~~~~~~~~~~~~~~~~~
             wrmsr_on_cpus
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Chen Zhou <chenzhou10@huawei.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200109012303.153001-1-chenzhou10@huawei.com

--- drivers/gpu/drm/i915/gt/intel_ggtt.c
+++ drivers/gpu/drm/i915/gt/intel_ggtt.c
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/stop_machine.h>
 
 #include <asm/set_memory.h>
+#include <asm/smp.h>
 
 #include "intel_gt.h"
 #include "i915_drv.h"
