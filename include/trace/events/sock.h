031a2df4fc7c436321c6d0076217dddba48a7df5
Mat Martineau <mathew.j.martineau@linux.intel.com>
2020-01-09 07:59:16 -0800

    tcp: Define IPPROTO_MPTCP
    To open a MPTCP socket with socket(AF_INET, SOCK_STREAM, IPPROTO_MPTCP),
    IPPROTO_MPTCP needs a value that differs from IPPROTO_TCP. The existing
    IPPROTO numbers mostly map directly to IANA-specified protocol numbers.
    MPTCP does not have a protocol number allocated because MPTCP packets
    use the TCP protocol number. Use private number not used OTA.
    
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/trace/events/sock.h
+++ include/trace/events/sock.h
@@ -20,7 +20,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define inet_protocol_names		\
 		EM(IPPROTO_TCP)			\
 		EM(IPPROTO_DCCP)		\
-		EMe(IPPROTO_SCTP)
+		EM(IPPROTO_SCTP)		\
+		EMe(IPPROTO_MPTCP)
 
 #define tcp_state_names			\
 		EM(TCP_ESTABLISHED)		\
