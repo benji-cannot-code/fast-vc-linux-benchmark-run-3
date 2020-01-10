ef0d63031277316a3613d0c26230f829035eb7be
YueHaibing <yuehaibing@huawei.com>
2020-01-10 01:35:17 +0000

    sfc: remove duplicated include from ef10.c
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Acked-by: Edward Cree <ecree@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/sfc/ef10.c
+++ drivers/net/ethernet/sfc/ef10.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "workarounds.h"
 #include "selftest.h"
 #include "ef10_sriov.h"
-#include "rx_common.h"
 #include <linux/in.h>
 #include <linux/jhash.h>
 #include <linux/wait.h>
