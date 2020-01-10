0bf1c3e3d12d2764a8ed5bbb7733c8b63c263137
Michal Wajdeczko <michal.wajdeczko@intel.com>
2020-01-10 22:27:20 +0000

    drm/i915/uc: Add ops to intel_uc
    Instead of spreading multiple conditionals across the uC code
    to find out current mode of uC operation, start using predefined
    set of function pointers that reflect that mode.
    
    Begin with pair of init_hw/fini_hw functions that are responsible
    for uC hardware initialization and cleanup.
    
    v2: drop ops_none, use macro to generate ops helpers
    v3: reuse __uc_check_hw to avoid redundant comment
    v4: forward declare ops struct vs functions
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200110222723.14724-2-michal.wajdeczko@intel.com

--- drivers/gpu/drm/i915/gt/uc/intel_uc.h
+++ drivers/gpu/drm/i915/gt/uc/intel_uc.h
@@ -11,7 +11,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "intel_huc.h"
 #include "i915_params.h"
 
+struct intel_uc;
+
+struct intel_uc_ops {
+	int (*init_hw)(struct intel_uc *uc);
+	void (*fini_hw)(struct intel_uc *uc);
+};
+
 struct intel_uc {
+	struct intel_uc_ops const *ops;
 	struct intel_guc guc;
 	struct intel_huc huc;
 
@@ -26,8 +34,6 @@ void intel_uc_fetch_firmwares(struct intel_uc *uc);
 void intel_uc_cleanup_firmwares(struct intel_uc *uc);
 void intel_uc_sanitize(struct intel_uc *uc);
 void intel_uc_init(struct intel_uc *uc);
-int intel_uc_init_hw(struct intel_uc *uc);
-void intel_uc_fini_hw(struct intel_uc *uc);
 void intel_uc_fini(struct intel_uc *uc);
 void intel_uc_reset_prepare(struct intel_uc *uc);
 void intel_uc_suspend(struct intel_uc *uc);
@@ -64,5 +70,16 @@ static inline bool intel_uc_uses_huc(struct intel_uc *uc)
 {
 	return intel_huc_is_enabled(&uc->huc);
 }
+
+#define intel_uc_ops_function(_NAME, _OPS, _TYPE, _RET) \
+static inline _TYPE intel_uc_##_NAME(struct intel_uc *uc) \
+{ \
+	if (uc->ops->_OPS) \
+		return uc->ops->_OPS(uc); \
+	return _RET; \
+}
+intel_uc_ops_function(init_hw, init_hw, int, 0);
+intel_uc_ops_function(fini_hw, fini_hw, void, );
+#undef intel_uc_ops_function
 
 #endif
