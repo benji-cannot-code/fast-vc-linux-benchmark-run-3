0fded1831e3fcb4dd50ffc731e5aced66223beff
Arnd Bergmann <arnd@arndb.de>
2020-01-12 13:04:45 +0100

    ixp4xx_eth: move platform_data definition
    The platform data is needed to compile the driver as standalone,
    so move it to a global location along with similar files.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- arch/arm/mach-ixp4xx/include/mach/platform.h
+++ arch/arm/mach-ixp4xx/include/mach/platform.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASSEMBLY__
 
 #include <linux/reboot.h>
+#include <linux/platform_data/eth_ixp4xx.h>
 
 #include <asm/types.h>
 
@@ -91,18 +92,6 @@ struct ixp4xx_pata_data {
 	unsigned long	cs1_bits;
 	void __iomem	*cs0;
 	void __iomem	*cs1;
-};
-
-#define IXP4XX_ETH_NPEA		0x00
-#define IXP4XX_ETH_NPEB		0x10
-#define IXP4XX_ETH_NPEC		0x20
-
-/* Information about built-in Ethernet MAC interfaces */
-struct eth_plat_info {
-	u8 phy;		/* MII PHY ID, 0 - 31 */
-	u8 rxq;		/* configurable, currently 0 - 31 only */
-	u8 txreadyq;
-	u8 hwaddr[6];
 };
 
 /*
--- drivers/net/ethernet/xscale/ixp4xx_eth.c
+++ drivers/net/ethernet/xscale/ixp4xx_eth.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/net_tstamp.h>
 #include <linux/of.h>
 #include <linux/phy.h>
+#include <linux/platform_data/eth_ixp4xx.h>
 #include <linux/platform_device.h>
 #include <linux/ptp_classify.h>
 #include <linux/slab.h>
