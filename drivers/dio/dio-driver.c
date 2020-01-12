06ebb748fc5aaab996f42c12d57222ad4980947f
Geert Uytterhoeven <geert@linux-m68k.org>
2020-01-12 17:56:11 +0100

    dio: Make dio_match_device() static
    Unlike its PCI counterpart, dio_match_device() was never used outside
    the DIO bus code.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Link: https://lore.kernel.org/r/20200112165613.20960-2-geert@linux-m68k.org

--- drivers/dio/dio-driver.c
+++ drivers/dio/dio-driver.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  dio_device_id structure or %NULL if there is no match.
  */
 
-const struct dio_device_id *
+static const struct dio_device_id *
 dio_match_device(const struct dio_device_id *ids,
 		   const struct dio_dev *d)
 {
@@ -138,7 +138,6 @@ static int __init dio_driver_init(void)
 
 postcore_initcall(dio_driver_init);
 
-EXPORT_SYMBOL(dio_match_device);
 EXPORT_SYMBOL(dio_register_driver);
 EXPORT_SYMBOL(dio_unregister_driver);
 EXPORT_SYMBOL(dio_bus_type);
