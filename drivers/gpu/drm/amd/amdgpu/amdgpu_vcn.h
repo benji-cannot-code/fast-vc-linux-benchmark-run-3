b8a2612195b454ae61d68f50e8408c3478e9b71e
James Zhu <James.Zhu@amd.com>
2020-01-13 16:40:00 -0500

    drm/amdgpu/vcn: support multiple-instance dpg pause mode
    Add multiple-instance dpg pause mode support for VCN2.5
    
    Signed-off-by: James Zhu <James.Zhu@amd.com>
    Reviewed-by: Leo Liu <leo.liu@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vcn.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vcn.h
@@ -200,7 +200,7 @@ struct amdgpu_vcn {
 
 	unsigned	harvest_config;
 	int (*pause_dpg_mode)(struct amdgpu_device *adev,
-		struct dpg_pause_state *new_state);
+		int inst_idx, struct dpg_pause_state *new_state);
 };
 
 int amdgpu_vcn_sw_init(struct amdgpu_device *adev);
