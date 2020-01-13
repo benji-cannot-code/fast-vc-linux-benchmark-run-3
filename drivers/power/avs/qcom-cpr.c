3889ef8bce2e9ccd90066ed0ea791dd782765137
Niklas Cassel <nks@flawful.org>
2020-01-13 21:02:58 +0100

    power: avs: qcom-cpr: add a printout after the driver has been initialized
    In order to easier inform the user that the driver has been initialized
    successfully, add a printout after the driver has been initialized.
    
    At the same time, remove a dev_dbg() that is now redundant.
    
    Signed-off-by: Niklas Cassel <nks@flawful.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/power/avs/qcom-cpr.c
+++ drivers/power/avs/qcom-cpr.c
@@ -1548,8 +1548,6 @@ static int cpr_pd_attach_dev(struct generic_pm_domain *domain,
 		goto unlock;
 	}
 
-	dev_dbg(drv->dev, "number of OPPs: %d\n", drv->num_corners);
-
 	drv->corners = devm_kcalloc(drv->dev, drv->num_corners,
 				    sizeof(*drv->corners),
 				    GFP_KERNEL);
@@ -1586,6 +1584,9 @@ static int cpr_pd_attach_dev(struct generic_pm_domain *domain,
 		regmap_update_bits(drv->tcsr, acc_desc->enable_reg,
 				   acc_desc->enable_mask,
 				   acc_desc->enable_mask);
+
+	dev_info(drv->dev, "driver initialized with %u OPPs\n",
+		 drv->num_corners);
 
 unlock:
 	mutex_unlock(&drv->lock);
