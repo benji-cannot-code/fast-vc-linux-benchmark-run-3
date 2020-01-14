0f8f215c1ae8936decd77fdca69b8752a3011e8e
Ben Skeggs <bskeggs@redhat.com>
2020-01-15 06:34:22 +1000

    drm/nouveau/flcn/msgq: remove error handling for msg_queue_open(), it can't fail
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/falcon/msgq.c
+++ drivers/gpu/drm/nouveau/nvkm/falcon/msgq.c
@@ -23,13 +23,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include "qmgr.h"
 
-static int
+static void
 msg_queue_open(struct nvkm_msgqueue *priv, struct nvkm_msgqueue_queue *queue)
 {
 	struct nvkm_falcon *falcon = priv->falcon;
 	mutex_lock(&queue->mutex);
 	queue->position = nvkm_falcon_rd32(falcon, queue->tail_reg);
-	return 0;
 }
 
 static void
@@ -92,11 +91,7 @@ msg_queue_read(struct nvkm_msgqueue *priv, struct nvkm_msgqueue_queue *queue,
 	const struct nvkm_subdev *subdev = priv->falcon->owner;
 	int ret;
 
-	ret = msg_queue_open(priv, queue);
-	if (ret) {
-		nvkm_error(subdev, "fail to open queue %d\n", queue->index);
-		return ret;
-	}
+	msg_queue_open(priv, queue);
 
 	if (msg_queue_empty(priv, queue)) {
 		ret = 0;
