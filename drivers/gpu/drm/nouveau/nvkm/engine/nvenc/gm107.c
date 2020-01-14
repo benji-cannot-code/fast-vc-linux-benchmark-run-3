938676dc3c480964f6e6a23d16dd5cb375da06a8
Ben Skeggs <bskeggs@redhat.com>
2020-01-15 06:34:21 +1000

    drm/nouveau/flcn: specify FBIF offset from subdev
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/nvenc/gm107.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/nvenc/gm107.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static const struct nvkm_falcon_func
 gm107_nvenc_flcn = {
+	.fbif = 0x800,
 	.load_imem = nvkm_falcon_v1_load_imem,
 	.load_dmem = nvkm_falcon_v1_load_dmem,
 	.read_dmem = nvkm_falcon_v1_read_dmem,
