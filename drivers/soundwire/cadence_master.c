4401cc1b9d8cc8423740cece381704679c6b748d
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-01-14 17:31:24 -0600

    soundwire: cadence: fix kernel-doc parameter descriptions
    Fix previous update, bad git merge likely. oops.
    
    Fixes: 39737a313085fa ("soundwire: cadence: update kernel-doc parameter descriptions")
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20200114233124.13888-1-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/soundwire/cadence_master.c
+++ drivers/soundwire/cadence_master.c
@@ -820,7 +820,6 @@ EXPORT_SYMBOL(sdw_cdns_thread);
 /**
  * sdw_cdns_exit_reset() - Program reset parameters and start bus operations
  * @cdns: Cadence instance
- * @state: True if we are trying to enable interrupt.
  */
 int sdw_cdns_exit_reset(struct sdw_cdns *cdns)
 {
@@ -847,6 +846,7 @@ EXPORT_SYMBOL(sdw_cdns_exit_reset);
 /**
  * sdw_cdns_enable_interrupt() - Enable SDW interrupts
  * @cdns: Cadence instance
+ * @state: True if we are trying to enable interrupt.
  */
 int sdw_cdns_enable_interrupt(struct sdw_cdns *cdns, bool state)
 {
