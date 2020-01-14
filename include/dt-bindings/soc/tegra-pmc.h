409ae08c66eb09c9c3c5fa18c828e320bc5b56bf
Sowjanya Komatineni <skomatineni@nvidia.com>
2020-01-13 23:24:14 -0800

    dt-bindings: soc: tegra-pmc: Add ID for Tegra PMC 32 kHz blink clock
    Tegra PMC has blink functionality that allows 32 kHz clock out to
    blink pin of the Tegra.
    
    This patch adds id for this blink clock to use for enabling or
    disabling blink output through device tree.
    
    Tested-by: Dmitry Osipenko <digetx@gmail.com>
    Reviewed-by: Dmitry Osipenko <digetx@gmail.com>
    Acked-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Sowjanya Komatineni <skomatineni@nvidia.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- include/dt-bindings/soc/tegra-pmc.h
+++ include/dt-bindings/soc/tegra-pmc.h
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TEGRA_PMC_CLK_OUT_1		0
 #define TEGRA_PMC_CLK_OUT_2		1
 #define TEGRA_PMC_CLK_OUT_3		2
+#define TEGRA_PMC_CLK_BLINK		3
 
-#define TEGRA_PMC_CLK_MAX		3
+#define TEGRA_PMC_CLK_MAX		4
 
 #endif	/* _DT_BINDINGS_SOC_TEGRA_PMC_H */
