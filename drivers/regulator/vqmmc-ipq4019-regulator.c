76d5178d5cd14e98a73376c7bb4c0d3fae3a3db6
Axel Lin <axel.lin@ingics.com>
2020-01-14 14:58:46 +0800

    regulator: vqmmc-ipq4019: Remove ipq4019_regulator_remove
    This driver is using devm_regulator_register() so no need to call
    regulator_unregister() in ipq4019_regulator_remove().
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Link: https://lore.kernel.org/r/20200114065847.31667-1-axel.lin@ingics.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/vqmmc-ipq4019-regulator.c
+++ drivers/regulator/vqmmc-ipq4019-regulator.c
@@ -82,15 +82,6 @@ static int ipq4019_regulator_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static int ipq4019_regulator_remove(struct platform_device *pdev)
-{
-	struct regulator_dev *rdev = platform_get_drvdata(pdev);
-
-	regulator_unregister(rdev);
-
-	return 0;
-}
-
 static const struct of_device_id regulator_ipq4019_of_match[] = {
 	{ .compatible = "qcom,vqmmc-ipq4019-regulator", },
 	{},
@@ -98,7 +89,6 @@ static const struct of_device_id regulator_ipq4019_of_match[] = {
 
 static struct platform_driver ipq4019_regulator_driver = {
 	.probe = ipq4019_regulator_probe,
-	.remove = ipq4019_regulator_remove,
 	.driver = {
 		.name = "vqmmc-ipq4019-regulator",
 		.owner = THIS_MODULE,
