e50a1930451d7ba7dffe3c22db3d84fe047d1b6e
Ben Skeggs <bskeggs@redhat.com>
2020-01-15 06:34:21 +1000

    drm/nouveau/gr/gp108: split from gp107
    ACR LS FW loading is moving out of SECBOOT and into their specific subdevs,
    and the available GP107/GP108 FWs have interface differences.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/gr/gp107.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/gr/gp107.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <nvif/class.h>
 
-static const struct gf100_gr_func
+const struct gf100_gr_func
 gp107_gr = {
 	.oneinit_tiles = gm200_gr_oneinit_tiles,
 	.oneinit_sm_id = gm200_gr_oneinit_sm_id,
