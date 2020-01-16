b9685afae3bcab439e4b9e55fbcf7cebee47bcae
Florian Fainelli <f.fainelli@gmail.com>
2020-01-15 20:48:50 -0800

    net: phy: Maintain MDIO device and bus statistics
    We maintain global statistics for an entire MDIO bus, as well as broken
    down, per MDIO bus address statistics. Given that it is possible for
    MDIO devices such as switches to access MDIO bus addresses for which
    there is not a mdio_device instance created (therefore not a a
    corresponding device directory in sysfs either), we also maintain
    per-address statistics under the statistics folder. The layout looks
    like this:
    
    /sys/class/mdio_bus/../statistics/
    	transfers
    	errrors
    	writes
    	reads
    	transfers_<addr>
    	errors_<addr>
    	writes_<addr>
    	reads_<addr>
    
    When a mdio_device instance is registered, a statistics/ folder is
    created with the tranfers, errors, writes and reads attributes which
    point to the appropriate MDIO bus statistics structure.
    
    Statistics are 64-bit unsigned quantities and maintained through the
    u64_stats_sync.h helper functions.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Tested-by: Andrew Lunn <andrew@lunn.ch>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/timer.h>
 #include <linux/workqueue.h>
 #include <linux/mod_devicetable.h>
+#include <linux/u64_stats_sync.h>
 
 #include <linux/atomic.h>
 
@@ -213,6 +214,15 @@ struct sfp_bus;
 struct sfp_upstream_ops;
 struct sk_buff;
 
+struct mdio_bus_stats {
+	u64_stats_t transfers;
+	u64_stats_t errors;
+	u64_stats_t writes;
+	u64_stats_t reads;
+	/* Must be last, add new statistics above */
+	struct u64_stats_sync syncp;
+};
+
 /*
  * The Bus class for PHYs.  Devices which provide access to
  * PHYs should register using this structure
@@ -225,6 +235,7 @@ struct mii_bus {
 	int (*read)(struct mii_bus *bus, int addr, int regnum);
 	int (*write)(struct mii_bus *bus, int addr, int regnum, u16 val);
 	int (*reset)(struct mii_bus *bus);
+	struct mdio_bus_stats stats[PHY_MAX_ADDR];
 
 	/*
 	 * A lock to ensure that only one thing can read/write
