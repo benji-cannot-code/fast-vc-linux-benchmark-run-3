3e59ac2685d481f3a8e4ebfb93d9890fc3540865
YueHaibing <yuehaibing@huawei.com>
2020-01-16 02:26:57 +0000

    sfc: remove duplicated include from efx.c
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/sfc/efx.c
+++ drivers/net/ethernet/sfc/efx.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "efx_channels.h"
 #include "rx_common.h"
 #include "tx_common.h"
-#include "rx_common.h"
 #include "nic.h"
 #include "io.h"
 #include "selftest.h"
