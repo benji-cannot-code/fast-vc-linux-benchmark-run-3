91739173b74aef1855ca0bc0d499e8c6371bb936
Andrii Nakryiko <andriin@fb.com>
2020-01-15 16:55:49 -0800

    selftests/bpf: Add whitelist/blacklist of test names to test_progs
    Add ability to specify a list of test name substrings for selecting which
    tests to run. So now -t is accepting a comma-separated list of strings,
    similarly to how -n accepts a comma-separated list of test numbers.
    
    Additionally, add ability to blacklist tests by name. Blacklist takes
    precedence over whitelist. Blacklisting is important for cases where it's
    known that some tests can't pass (e.g., due to perf hardware events that are
    not available within VM). This is going to be used for libbpf testing in
    Travis CI in its Github repo.
    
    Example runs with just whitelist and whitelist + blacklist:
    
      $ sudo ./test_progs -tattach,core/existence
      #1 attach_probe:OK
      #6 cgroup_attach_autodetach:OK
      #7 cgroup_attach_multi:OK
      #8 cgroup_attach_override:OK
      #9 core_extern:OK
      #10/44 existence:OK
      #10/45 existence___minimal:OK
      #10/46 existence__err_int_sz:OK
      #10/47 existence__err_int_type:OK
      #10/48 existence__err_int_kind:OK
      #10/49 existence__err_arr_kind:OK
      #10/50 existence__err_arr_value_type:OK
      #10/51 existence__err_struct_type:OK
      #10 core_reloc:OK
      #19 flow_dissector_reattach:OK
      #60 tp_attach_query:OK
      Summary: 8/8 PASSED, 0 SKIPPED, 0 FAILED
    
      $ sudo ./test_progs -tattach,core/existence -bcgroup,flow/arr
      #1 attach_probe:OK
      #9 core_extern:OK
      #10/44 existence:OK
      #10/45 existence___minimal:OK
      #10/46 existence__err_int_sz:OK
      #10/47 existence__err_int_type:OK
      #10/48 existence__err_int_kind:OK
      #10/51 existence__err_struct_type:OK
      #10 core_reloc:OK
      #60 tp_attach_query:OK
      Summary: 4/6 PASSED, 0 SKIPPED, 0 FAILED
    
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Cc: Julia Kartseva <hex@fb.com>
    Link: https://lore.kernel.org/bpf/20200116005549.3644118-1-andriin@fb.com

--- tools/testing/selftests/bpf/test_progs.h
+++ tools/testing/selftests/bpf/test_progs.h
@@ -47,8 +47,14 @@ enum verbosity {
 	VERBOSE_SUPER,
 };
 
+struct str_set {
+	const char **strs;
+	int cnt;
+};
+
 struct test_selector {
-	const char *name;
+	struct str_set whitelist;
+	struct str_set blacklist;
 	bool *num_set;
 	int num_set_len;
 };
