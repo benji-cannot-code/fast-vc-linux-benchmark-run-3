5b71b70bdaa0c01da8c75e2a24298883690d8f2b
Chris Wilson <chris@chris-wilson.co.uk>
2020-01-17 10:21:45 +0000

    drm/i915: Include the debugfs params header for its own definition
    drivers/gpu/drm/i915/i915_debugfs_params.c:228:15: warning: symbol 'i915_debugfs_params' was not declared. Should it be static?
    drivers/gpu/drm/i915/i915_debugfs_params.c:228:16: error: no previous prototype for ‘i915_debugfs_params’ [-Werror=missing-prototypes]
      228 | struct dentry *i915_debugfs_params(struct drm_i915_private *i915)
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Fixes: c43c5a8818d4 ("drm/i915/params: add i915 parameters to debugfs")
    Link: https://patchwork.freedesktop.org/patch/msgid/20200117102145.2948244-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_debugfs_params.c
+++ drivers/gpu/drm/i915/i915_debugfs_params.c
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/kernel.h>
 
+#include "i915_debugfs_params.h"
 #include "i915_drv.h"
 #include "i915_params.h"
 
