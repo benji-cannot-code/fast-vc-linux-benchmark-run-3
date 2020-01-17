eed3f4d6f085fe452b28611cdccbcce53a18ca81
Ajay Singh <ajay.kathat@microchip.com>
2020-01-17 10:31:26 +0000

    staging: wilc1000: added 'wilc_' prefix for 'struct assoc_resp' name
    Use 'wilc_' prefix for 'assoc_resp' struct to have proper naming
    convention.
    
    Signed-off-by: Ajay Singh <ajay.kathat@microchip.com>
    Link: https://lore.kernel.org/r/20200117160157.8706-4-ajay.kathat@microchip.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wilc1000/fw.h
+++ drivers/staging/wilc1000/fw.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define WILC_MAX_NUM_PMKIDS			16
 #define WILC_MAX_NUM_SCANNED_CH			14
 
-struct assoc_resp {
+struct wilc_assoc_resp {
 	__le16 capab_info;
 	__le16 status_code;
 	__le16 aid;
