d13db6639387602da2a56377038ad325edbcff50
Andrii Nakryiko <andriin@fb.com>
2020-01-16 16:41:02 -0800

    libbpf: Revert bpf_helper_defs.h inclusion regression
    Revert bpf_helpers.h's change to include auto-generated bpf_helper_defs.h
    through <> instead of "", which causes it to be searched in include path. This
    can break existing applications that don't have their include path pointing
    directly to where libbpf installs its headers.
    
    There is ongoing work to make all (not just bpf_helper_defs.h) includes more
    consistent across libbpf and its consumers, but this unbreaks user code as is
    right now without any regressions. Selftests still behave sub-optimally
    (taking bpf_helper_defs.h from libbpf's source directory, if it's present
    there), which will be fixed in subsequent patches.
    
    Fixes: 6910d7d3867a ("selftests/bpf: Ensure bpf_helper_defs.h are taken from selftests dir")
    Reported-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200117004103.148068-1-andriin@fb.com

--- tools/lib/bpf/bpf_helpers.h
+++ tools/lib/bpf/bpf_helpers.h
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __BPF_HELPERS__
 #define __BPF_HELPERS__
 
-#include <bpf_helper_defs.h>
+#include "bpf_helper_defs.h"
 
 #define __uint(name, val) int (*name)[val]
 #define __type(name, val) typeof(val) *name
