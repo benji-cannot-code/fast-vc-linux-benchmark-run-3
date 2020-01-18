12f8d4fdeb9825498e15f3948b00606263c8c48a
Alex Marginean <alexandru.marginean@nxp.com>
2020-01-18 14:19:15 +0200

    net: phylink: allow in-band AN for USXGMII
    USXGMII supports passing link information in-band between PHY and MAC PCS,
    add it to the list of protocols that support in-band AN mode.
    
    Being a MAC-PHY protocol that can auto-negotiate link speeds up to 10
    Gbps, we populate the initial supported mask with the entire spectrum of
    link modes up to 10G that PHYLINK supports, and we let the driver reduce
    that mask in its .phylink_validate method.
    
    Signed-off-by: Alex Marginean <alexandru.marginean@nxp.com>
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/phylink.c
+++ drivers/net/phy/phylink.c
@@ -299,6 +299,7 @@ static int phylink_parse_mode(struct phylink *pl, struct fwnode_handle *fwnode)
 			phylink_set(pl->supported, 2500baseX_Full);
 			break;
 
+		case PHY_INTERFACE_MODE_USXGMII:
 		case PHY_INTERFACE_MODE_10GKR:
 		case PHY_INTERFACE_MODE_10GBASER:
 			phylink_set(pl->supported, 10baseT_Half);
