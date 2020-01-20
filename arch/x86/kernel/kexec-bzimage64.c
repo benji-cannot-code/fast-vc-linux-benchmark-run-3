38bfcebe0dd567d0f61563930190ea21e2570676
Ard Biesheuvel <ardb@kernel.org>
2020-01-20 16:15:00 +0100

    efi/x86: Remove runtime table address from kexec EFI setup data
    Since commit 33b85447fa61946b ("efi/x86: Drop two near identical versions
    of efi_runtime_init()"), we no longer map the EFI runtime services table
    before calling SetVirtualAddressMap(), which means we don't need the 1:1
    mapped physical address of this table, and so there is no point in passing
    the address via EFI setup data on kexec boot.
    
    Note that the kexec tools will still look for this address in sysfs, so
    we still need to provide it.
    
    Tested-by: Tony Luck <tony.luck@intel.com> # arch/ia64
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/x86/kernel/kexec-bzimage64.c
+++ arch/x86/kernel/kexec-bzimage64.c
@@ -143,7 +143,6 @@ prepare_add_efi_setup_data(struct boot_params *params,
 	struct efi_setup_data *esd = (void *)sd + sizeof(struct setup_data);
 
 	esd->fw_vendor = efi.fw_vendor;
-	esd->runtime = efi.runtime;
 	esd->tables = efi.config_table;
 	esd->smbios = efi.smbios;
 
