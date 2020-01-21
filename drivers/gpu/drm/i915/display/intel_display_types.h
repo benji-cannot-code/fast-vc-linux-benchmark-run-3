d4501cad1ea1d756f3d1e0476bbfe7ee1c9ce1bb
Jani Nikula <jani.nikula@intel.com>
2020-01-21 13:39:15 +0200

    drm/i915: add display engine uncore helpers
    Add convenience helpers for the most common uncore operations with
    struct drm_i915_private * as context rather than struct intel_uncore *.
    
    The goal is to replace all instances of I915_READ(),
    I915_POSTING_READ(), I915_WRITE(), I915_READ_FW(), and I915_WRITE_FW()
    in display/ with these, to finally be able to get rid of the implicit
    dev_priv local parameter use.
    
    The idea is that any non-u32 reads or writes are special enough that
    they can use the intel_uncore_* functions directly.
    
    v2:
    - rename the file intel_de.h
    - move intel_de_wait_for_* there too
    - also add de fw helpers
    
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200121113915.9813-1-jani.nikula@intel.com

--- drivers/gpu/drm/i915/display/intel_display_types.h
+++ drivers/gpu/drm/i915/display/intel_display_types.h
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/cec-notifier.h>
 
 #include "i915_drv.h"
+#include "intel_de.h"
 
 struct drm_printer;
 
