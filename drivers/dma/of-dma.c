53a7e73b7d4d2bed6c10392a67259221c130554f
Geert Uytterhoeven <geert+renesas@glider.be>
2020-01-21 10:33:11 +0100

    dmaengine: Move dma_get_{,any_}slave_channel() to private dmaengine.h
    The functions dma_get_slave_channel() and dma_get_any_slave_channel()
    are called from DMA engine drivers only.  Hence move their declarations
    from the public header file <linux/dmaengine.h> to the private header
    file drivers/dma/dmaengine.h.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Link: https://lore.kernel.org/r/20200121093311.28639-4-geert+renesas@glider.be
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/of-dma.c
+++ drivers/dma/of-dma.c
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_dma.h>
 
+#include "dmaengine.h"
+
 static LIST_HEAD(of_dma_list);
 static DEFINE_MUTEX(of_dma_lock);
 
