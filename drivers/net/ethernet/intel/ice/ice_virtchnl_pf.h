990846cc7f775a9306372de0d853c23a07ee0fdf
Brett Creeley <brett.creeley@intel.com>
2020-01-22 07:21:27 -0800

    ice: Fix Port VLAN priority bits
    Currently when configuring a port VLAN for a VF we are only shifting the
    QoS bits by 12. This is incorrect. Fix this by getting rid of the ICE
    specific VLAN defines and use the kernel VLAN defines instead.
    
    Also, don't assign a value to vlanprio until the VLAN ID and QoS
    parameters have been validated.
    
    Also, there are many places we do (le16_to_cpu(vsi->info.pvid) &
    VLAN_VID_MASK). Instead do (vf->port_vlan_info & VLAN_VID_MASK) because
    we always save what's stored in vsi->info.pvid to vf->port_vlan_info in
    the CPU's endianness.
    
    Signed-off-by: Brett Creeley <brett.creeley@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/ice/ice_virtchnl_pf.h
+++ drivers/net/ethernet/intel/ice/ice_virtchnl_pf.h
@@ -6,11 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ICE_VIRTCHNL_PF_H_
 #include "ice.h"
 
-#define ICE_MAX_VLANID			4095
-#define ICE_VLAN_PRIORITY_S		12
-#define ICE_VLAN_M			0xFFF
-#define ICE_PRIORITY_M			0x7000
-
 /* Restrict number of MAC Addr and VLAN that non-trusted VF can programmed */
 #define ICE_MAX_VLAN_PER_VF		8
 #define ICE_MAX_MACADDR_PER_VF		12
