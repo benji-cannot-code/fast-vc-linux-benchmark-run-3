f661231da73c0e4d21569ad58288a1fceead4f12
Mohit P. Tahiliani <tahiliani@nitk.edu.in>
2020-01-22 23:52:28 +0530

    pie: rearrange structure members and their initializations
    Rearrange the members of the structure such that closely
    referenced members appear together and/or fit in the same
    cacheline. Also, change the order of their initializations to
    match the order in which they appear in the structure.
    
    Signed-off-by: Mohit P. Tahiliani <tahiliani@nitk.edu.in>
    Signed-off-by: Leslie Monis <lesliemonis@gmail.com>
    Signed-off-by: Gautam Ramakrishnan <gautamramk@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/sch_pie.c
+++ net/sched/sch_pie.c
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* private data for the Qdisc */
 struct pie_sched_data {
-	struct pie_params params;
 	struct pie_vars vars;
+	struct pie_params params;
 	struct pie_stats stats;
 	struct timer_list adapt_timer;
 	struct Qdisc *sch;
