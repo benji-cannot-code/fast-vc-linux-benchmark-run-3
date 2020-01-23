f4704867df8026c0fab1cc0fb1163b1e1461b5be
Benjamin Thiel <b.thiel@posteo.de>
2020-01-23 16:27:54 +0100

    x86/syscalls: Add prototypes for C syscall callbacks
    .. in order to fix a couple of -Wmissing-prototypes warnings.
    
    No functional change.
    
     [ bp: Massage commit message and drop newlines. ]
    
    Signed-off-by: Benjamin Thiel <b.thiel@posteo.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/20200123152754.20149-1-b.thiel@posteo.de

--- arch/x86/entry/common.c
+++ arch/x86/entry/common.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/fpu/api.h>
 #include <asm/nospec-branch.h>
 #include <asm/io_bitmap.h>
+#include <asm/syscall.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/syscalls.h>
