4ad8d238513a191cd29e55117dd2008c96c40a08
Benjamin Thiel <b.thiel@posteo.de>
2020-01-23 18:29:45 +0100

    x86/cpu: Move prototype for get_umwait_control_msr() to a global location
    .. in order to fix a -Wmissing-prototypes warning.
    
    No functional change.
    
    Signed-off-by: Benjamin Thiel <b.thiel@posteo.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Cc: kvm@vger.kernel.org
    Link: https://lkml.kernel.org/r/20200123172945.7235-1-b.thiel@posteo.de

--- arch/x86/kvm/vmx/vmx.h
+++ arch/x86/kvm/vmx/vmx.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern const u32 vmx_msr_index[];
 extern u64 host_efer;
 
-extern u32 get_umwait_control_msr(void);
-
 #define MSR_TYPE_R	1
 #define MSR_TYPE_W	2
 #define MSR_TYPE_RW	3
