9093c1205f272b3f7f7b19ef6a3c434c3a4df837
Christophe Leroy <christophe.leroy@c-s.fr>
2020-01-24 11:54:43 +0000

    powerpc/32s: Prepare prevent_user_access() for user_access_end()
    In preparation of implementing user_access_begin and friends
    on powerpc, the book3s/32 version of prevent_user_access() need
    to be prepared for user_access_end().
    
    user_access_end() doesn't provide the address and size which
    were passed to user_access_begin(), required by prevent_user_access()
    to know which segment to modify.
    
    The list of segments which where unprotected by allow_user_access()
    are available in current->kuap. But we don't want prevent_user_access()
    to read this all the time, especially everytime it is 0 (for instance
    because the access was not a write access).
    
    Implement a special direction named KUAP_CURRENT. In this case only,
    the addr and end are retrieved from current->kuap.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/55bcc1f25d8200892a31f67a0b024ff3b816c3cc.1579866752.git.christophe.leroy@c-s.fr

--- arch/powerpc/include/asm/kup.h
+++ arch/powerpc/include/asm/kup.h
@@ -6,6 +6,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KUAP_READ	1
 #define KUAP_WRITE	2
 #define KUAP_READ_WRITE	(KUAP_READ | KUAP_WRITE)
+/*
+ * For prevent_user_access() only.
+ * Use the current saved situation instead of the to/from/size params.
+ * Used on book3s/32
+ */
+#define KUAP_CURRENT	4
 
 #ifdef CONFIG_PPC64
 #include <asm/book3s/64/kup-radix.h>
@@ -87,6 +93,11 @@ static inline void prevent_read_write_user(void __user *to, const void __user *f
 					   unsigned long size)
 {
 	prevent_user_access(to, from, size, KUAP_READ_WRITE);
+}
+
+static inline void prevent_current_access_user(void)
+{
+	prevent_user_access(NULL, NULL, ~0UL, KUAP_CURRENT);
 }
 
 #endif /* !__ASSEMBLY__ */
