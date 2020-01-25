b1d1b8b3c861c2d0a7e458e80b9e2f6e1930c8a8
Mat Martineau <mathew.j.martineau@linux.intel.com>
2020-01-24 16:04:03 -0800

    mptcp: Fix code formatting
    checkpatch.pl had a few complaints in the last set of MPTCP patches:
    
    ERROR: code indent should use tabs where possible
    +^I         subflow, sk->sk_family, icsk->icsk_af_ops, target, mapped);$
    
    CHECK: Comparison to NULL could be written "!new_ctx"
    +	if (new_ctx == NULL) {
    
    ERROR: "foo * bar" should be "foo *bar"
    +static const struct proto_ops * tcp_proto_ops(struct sock *sk)
    
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void __mptcp_close(struct sock *sk, long timeout);
 
-static const struct proto_ops * tcp_proto_ops(struct sock *sk)
+static const struct proto_ops *tcp_proto_ops(struct sock *sk)
 {
 #if IS_ENABLED(CONFIG_IPV6)
 	if (sk->sk_family == AF_INET6)
