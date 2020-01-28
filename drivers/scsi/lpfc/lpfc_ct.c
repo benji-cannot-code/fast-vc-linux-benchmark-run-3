1548b523290511a41be3f3e99f59ae8365aa3e77
James Smart <jsmart2021@gmail.com>
2020-01-27 16:23:04 -0800

    scsi: lpfc: Fix registration of ELS type support in fdmi
    Adjust FC4 Types in FDMI settings
    
    The driver sets FDMI information registring ELS as a FC4 type.  ELS is a
    fc3 type and should not be registered.
    
    Fix by removing ELS type bit when we register for FDMI Port attributes.
    
    Link: https://lore.kernel.org/r/20200128002312.16346-5-jsmart2021@gmail.com
    Signed-off-by: Dick Kennedy <dick.kennedy@broadcom.com>
    Signed-off-by: James Smart <jsmart2021@gmail.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/lpfc/lpfc_ct.c
+++ drivers/scsi/lpfc/lpfc_ct.c
@@ -2454,7 +2454,6 @@ lpfc_fdmi_port_attr_fc4type(struct lpfc_vport *vport,
 	ae = (struct lpfc_fdmi_attr_entry *)&ad->AttrValue;
 	memset(ae, 0, 32);
 
-	ae->un.AttrTypes[3] = 0x02; /* Type 0x1 - ELS */
 	ae->un.AttrTypes[2] = 0x01; /* Type 0x8 - FCP */
 	ae->un.AttrTypes[7] = 0x01; /* Type 0x20 - CT */
 
@@ -2772,7 +2771,6 @@ lpfc_fdmi_port_attr_active_fc4type(struct lpfc_vport *vport,
 	ae = (struct lpfc_fdmi_attr_entry *)&ad->AttrValue;
 	memset(ae, 0, 32);
 
-	ae->un.AttrTypes[3] = 0x02; /* Type 0x1 - ELS */
 	ae->un.AttrTypes[2] = 0x01; /* Type 0x8 - FCP */
 	ae->un.AttrTypes[7] = 0x01; /* Type 0x20 - CT */
 
