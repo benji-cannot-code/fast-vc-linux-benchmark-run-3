ab0c493759279eb9f29b0d7d67bf767652ffecef
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2020-01-31 11:38:28 +0100

    staging: vc04_services: Get rid of vchiq_arm_vcsuspend()
    It's not used.
    
    Signed-off-by: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
    Link: https://lore.kernel.org/r/20200131103836.14312-13-nsaenzjulienne@suse.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vc04_services/interface/vchiq_arm/vchiq_arm.h
+++ drivers/staging/vc04_services/interface/vchiq_arm/vchiq_arm.h
@@ -90,9 +90,6 @@ extern struct vchiq_state *
 vchiq_get_state(void);
 
 extern enum vchiq_status
-vchiq_arm_vcsuspend(struct vchiq_state *state);
-
-extern enum vchiq_status
 vchiq_arm_vcresume(struct vchiq_state *state);
 
 extern enum vchiq_status
