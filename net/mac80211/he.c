0443895f0f0ef56556f6b5623c644cdc75fccfa3
Ilan Peer <ilan.peer@intel.com>
2020-01-31 13:12:53 +0200

    mac80211: Handle SMPS mode changes only in AP mode
    According to IEEE802.11 specifications the SM power save field
    in the HT capability IE and the HE extended capability IE is valid
    only in (re)association frames and should be ignored otherwise.
    Remove code paths that handled this also for non AP modes.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Link: https://lore.kernel.org/r/20200131111300.891737-17-luca@coelho.fi
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/mac80211/he.c
+++ net/mac80211/he.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * HE handling
  *
  * Copyright(c) 2017 Intel Deutschland GmbH
- * Copyright(c) 2019 Intel Corporation
+ * Copyright(c) 2019 - 2020 Intel Corporation
  */
 
 #include "ieee80211_i.h"
