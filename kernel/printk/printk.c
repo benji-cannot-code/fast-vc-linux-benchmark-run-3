cd81904f479a8214bc41562a4b384cc40e887685
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-02-03 15:31:30 +0200

    console: Introduce ->exit() callback
    Some consoles might require special operations on unregistering.
    For instance, serial console, when registered in the kernel,
    keeps power on for entire time, until it gets unregistered.
    Example of use:
    
    	->setup(console):
    		pm_runtime_get(...);
    
    	->exit(console):
    		pm_runtime_put(...);
    
    For such cases to have a balance we would provide ->exit() callback.
    
    Link: http://lkml.kernel.org/r/20200203133130.11591-7-andriy.shevchenko@linux.intel.com
    To: linux-kernel@vger.kernel.org
    To: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Reviewed-by: Petr Mladek <pmladek@suse.com>
    Signed-off-by: Petr Mladek <pmladek@suse.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2854,6 +2854,9 @@ int unregister_console(struct console *console)
 	console_unlock();
 	console_sysfs_notify();
 
+	if (console->exit)
+		res = console->exit(console);
+
 	return res;
 
 out_disable_unlock:
