d807b747fd622fd64eefb5e8456658e2f60e4035
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2020-02-03 14:03:47 +0100

    Revert "gpiolib: remove set but not used variable 'config'"
    This reverts commit e5e42ad224a040f93bf112e96f82b3a0ed97ffab.
    
    This patch came on top of another patch that introduced a regression.
    Revert it before addressing the culprit.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Tested-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -3048,6 +3048,7 @@ static int gpio_do_set_config(struct gpio_chip *gc, unsigned int offset,
 static int gpio_set_config(struct gpio_chip *gc, unsigned int offset,
 			   enum pin_config_param mode)
 {
+	unsigned long config;
 	unsigned arg;
 
 	switch (mode) {
@@ -3061,6 +3062,7 @@ static int gpio_set_config(struct gpio_chip *gc, unsigned int offset,
 		arg = 0;
 	}
 
+	config = PIN_CONF_PACKED(mode, arg);
 	return gpio_do_set_config(gc, offset, mode);
 }
 
