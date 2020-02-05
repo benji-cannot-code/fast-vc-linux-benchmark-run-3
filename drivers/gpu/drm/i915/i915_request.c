981bbbce90966775488cdf42fcf17f4a2b42bc88
Chris Wilson <chris@chris-wilson.co.uk>
2020-02-05 09:54:40 +0000

    drm/i915: Flush execution tasklets before checking request status
    Rather than flushing the submission tasklets just before we sleep, flush
    before we check the request status. Ideally this gives us a moment to
    process the tasklets after sleeping just before we timeout.
    
    v2: Compromise by pushing the flush prior to the timeout, but after the
    check on completion so that we do not further delay the ready client.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200205095441.1769599-1-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_request.c
+++ drivers/gpu/drm/i915/i915_request.c
@@ -1572,6 +1572,8 @@ long i915_request_wait(struct i915_request *rq,
 			break;
 		}
 
+		intel_engine_flush_submission(rq->engine);
+
 		if (signal_pending_state(state, current)) {
 			timeout = -ERESTARTSYS;
 			break;
@@ -1582,7 +1584,6 @@ long i915_request_wait(struct i915_request *rq,
 			break;
 		}
 
-		intel_engine_flush_submission(rq->engine);
 		timeout = io_schedule_timeout(timeout);
 	}
 	__set_current_state(TASK_RUNNING);
