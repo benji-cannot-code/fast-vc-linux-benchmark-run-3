30873a01968473a8f0eb5baf20ce73acfffca414
Ard Biesheuvel <ardb@kernel.org>
2020-02-10 17:02:32 +0100

    efi/libstub: Use hidden visibility for all source files
    Instead of setting the visibility pragma for a small set of symbol
    declarations that could result in absolute references that we cannot
    support in the stub, declare hidden visibility for all code in the
    EFI stub, which is more robust and future proof.
    
    To ensure that the #pragma is taken into account before any other
    includes are processed, put it in a header file of its own and
    include it via the compiler command line using the -include option.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- drivers/firmware/efi/libstub/arm64-stub.c
+++ drivers/firmware/efi/libstub/arm64-stub.c
@@ -7,17 +7,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Adapted from ARM version by Mark Salter <msalter@redhat.com>
  */
 
-/*
- * To prevent the compiler from emitting GOT-indirected (and thus absolute)
- * references to the section markers, override their visibility as 'hidden'
- */
-#pragma GCC visibility push(hidden)
-#include <asm/sections.h>
-#pragma GCC visibility pop
 
 #include <linux/efi.h>
 #include <asm/efi.h>
 #include <asm/memory.h>
+#include <asm/sections.h>
 #include <asm/sysreg.h>
 
 #include "efistub.h"
