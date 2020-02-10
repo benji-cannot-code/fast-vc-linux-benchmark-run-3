158e6fcf8fe48e4d60c4113a4f4f904b48323a85
Pragat Pandya <pragat.pandya@gmail.com>
2020-02-11 00:05:57 +0530

    staging: exfat: Rename variable 'ModifyTimestamp' to 'modify_timestamp'
    Fix checkpatch warning: Avoid CamelCase
    Change all occurrences of identifier "ModifyTimestamp" to
    "modify_timestamp"
    
    Signed-off-by: Pragat Pandya <pragat.pandya@gmail.com>
    Link: https://lore.kernel.org/r/20200210183558.11836-19-pragat.pandya@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/exfat/exfat.h
+++ drivers/staging/exfat/exfat.h
@@ -270,7 +270,7 @@ struct dir_entry_t {
 	u64 Size;
 	u32 num_subdirs;
 	struct date_time_t create_timestamp;
-	struct date_time_t ModifyTimestamp;
+	struct date_time_t modify_timestamp;
 	struct date_time_t AccessTimestamp;
 };
 
