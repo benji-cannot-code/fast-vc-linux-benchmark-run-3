9b6d560d55d04843c0a342a85362dc34a307201d
Marco Elver <elver@google.com>
2020-02-11 17:04:20 +0100

    compiler.h, seqlock.h: Remove unnecessary kcsan.h includes
    No we longer have to include kcsan.h, since the required KCSAN interface
    for both compiler.h and seqlock.h are now provided by kcsan-checks.h.
    
    Acked-by: John Hubbard <jhubbard@nvidia.com>
    Signed-off-by: Marco Elver <elver@google.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/seqlock.h
+++ include/linux/seqlock.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/preempt.h>
 #include <linux/lockdep.h>
 #include <linux/compiler.h>
-#include <linux/kcsan.h>
+#include <linux/kcsan-checks.h>
 #include <asm/processor.h>
 
 /*
