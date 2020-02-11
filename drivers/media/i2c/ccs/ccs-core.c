b5403c17053f7ccb0c37bbf87233ec2c0c87cf71
Sakari Ailus <sakari.ailus@linux.intel.com>
2020-02-11 22:35:40 +0100

    media: ccs: Remove remaining support for platform data
    No need to support platform data; remove support for conveying hardware
    configuration that way.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/i2c/ccs/ccs-core.c
+++ drivers/media/i2c/ccs/ccs-core.c
@@ -2861,9 +2861,6 @@ static struct ccs_hwconfig *ccs_get_hwconfig(struct device *dev)
 	int i;
 	int rval;
 
-	if (!fwnode)
-		return dev->platform_data;
-
 	ep = fwnode_graph_get_next_endpoint(fwnode, NULL);
 	if (!ep)
 		return NULL;
