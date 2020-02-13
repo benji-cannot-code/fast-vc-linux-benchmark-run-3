a196743e9833b49202d026e4d46a30f98a463c99
Tiezhu Yang <yangtiezhu@loongson.cn>
2020-02-13 12:16:07 +0800

    spi: spidev_test: Use perror() only if errno is not 0
    It is better to use perror() only if errno is not 0, it should use printf()
    when errno is 0, otherwise there exists redudant ": Success".
    
    E.g. without this patch:
    
    $ ./spidev_test -p 1234 --input test.bin
    only one of -p and --input may be selected: Success
    Aborted (core dumped)
    
    With this patch:
    
    $ ./spidev_test -p 1234 --input test.bin
    only one of -p and --input may be selected
    Aborted (core dumped)
    
    Signed-off-by: Tiezhu Yang <yangtiezhu@loongson.cn>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://lore.kernel.org/r/1581567368-8055-3-git-send-email-yangtiezhu@loongson.cn
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- tools/spi/spidev_test.c
+++ tools/spi/spidev_test.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <getopt.h>
 #include <fcntl.h>
 #include <time.h>
@@ -27,7 +28,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void pabort(const char *s)
 {
-	perror(s);
+	if (errno != 0)
+		perror(s);
+	else
+		printf("%s\n", s);
+
 	abort();
 }
 
