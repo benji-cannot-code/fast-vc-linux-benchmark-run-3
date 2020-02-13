9e4973ec190cf866ed164d2444f3f33db41ffc81
Tyler Hicks <code@tyhicks.com>
2020-02-13 21:25:54 +0000

    eCryptfs: Replace deactivated email address
    Replace a recently deactived email address with one that I'll be able to
    personally control and keep alive.
    
    Signed-off-by: Tyler Hicks <code@tyhicks.com>

--- fs/ecryptfs/messaging.c
+++ fs/ecryptfs/messaging.c
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2004-2008 International Business Machines Corp.
  *   Author(s): Michael A. Halcrow <mhalcrow@us.ibm.com>
- *		Tyler Hicks <tyhicks@ou.edu>
+ *		Tyler Hicks <code@tyhicks.com>
  */
 #include <linux/sched.h>
 #include <linux/slab.h>
