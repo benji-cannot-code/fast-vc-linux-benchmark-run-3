9f8892b00131ce1c527e64ce153d1c3f2788a65d
Erik Kaneda <erik.kaneda@intel.com>
2020-02-14 10:47:56 -0800

    ACPICA: ASL-ASL+ converter: make root file a parameter for cv_init_file_tree
    ACPICA commit 5d160cc86cca440eac7055f981e48bc14d4eb9f7
    
    This decouples cv_init_file_tree from acpi_gbl_output_file and allows
    it to be called independently of acpi_os_redirect_output()
    
    Link: https://github.com/acpica/acpica/commit/5d160cc8
    Signed-off-by: Erik Kaneda <erik.kaneda@intel.com>
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpica/acmacros.h
+++ drivers/acpi/acpica/acmacros.h
@@ -478,7 +478,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ASL_CV_PRINT_ONE_COMMENT(a,b,c,d) cv_print_one_comment_type (a,b,c,d);
 #define ASL_CV_PRINT_ONE_COMMENT_LIST(a,b) cv_print_one_comment_list (a,b);
 #define ASL_CV_FILE_HAS_SWITCHED(a)       cv_file_has_switched(a)
-#define ASL_CV_INIT_FILETREE(a)      cv_init_file_tree(a);
+#define ASL_CV_INIT_FILETREE(a,b)      cv_init_file_tree(a,b);
 
 #else
 
@@ -493,7 +493,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ASL_CV_PRINT_ONE_COMMENT(a,b,c,d)
 #define ASL_CV_PRINT_ONE_COMMENT_LIST(a,b)
 #define ASL_CV_FILE_HAS_SWITCHED(a)       0
-#define ASL_CV_INIT_FILETREE(a)
+#define ASL_CV_INIT_FILETREE(a,b)
 
 #endif
 
