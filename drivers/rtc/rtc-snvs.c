2286f1ee442f194d1d7f5c2f4098d1c6b37edad1
Anson Huang <Anson.Huang@nxp.com>
2020-02-16 11:27:45 +0800

    rtc: snvs: Remove unused include of of_device.h
    There is nothing in use from of_device.h, remove it.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>
    Link: https://lore.kernel.org/r/1581823666-16944-1-git-send-email-Anson.Huang@nxp.com
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>

--- drivers/rtc/rtc-snvs.c
+++ drivers/rtc/rtc-snvs.c
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/of.h>
-#include <linux/of_device.h>
 #include <linux/platform_device.h>
 #include <linux/pm_wakeirq.h>
 #include <linux/rtc.h>
