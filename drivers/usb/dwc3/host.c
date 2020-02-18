59751d11cf29666115cfdd7a943f7fa853e09265
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-02-18 17:12:19 +0200

    usb: dwc3: Remove kernel doc annotation where it's not needed
    The main comment in the file mistakenly marked with kernel doc annotation
    which makes the parser unhappy:
    
    .../dwc3/host.c:16: warning: Function parameter or member 'dwc' not described in 'dwc3_host_get_irq'
    
    Drop kernel doc annotation from host.c module.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- drivers/usb/dwc3/host.c
+++ drivers/usb/dwc3/host.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
-/**
+/*
  * host.c - DesignWare USB3 DRD Controller Host Glue
  *
  * Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com
