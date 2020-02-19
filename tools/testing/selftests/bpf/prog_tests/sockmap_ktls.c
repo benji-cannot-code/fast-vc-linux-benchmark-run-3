d45437dd70960622dc1571c2089ab100fde680e7
Alexei Starovoitov <ast@kernel.org>
2020-02-19 12:55:14 -0800

    selftests/bpf: Fix build of sockmap_ktls.c
    The selftests fails to build with:
    tools/testing/selftests/bpf/prog_tests/sockmap_ktls.c: In function ‘test_sockmap_ktls_disconnect_after_delete’:
    tools/testing/selftests/bpf/prog_tests/sockmap_ktls.c:72:37: error: ‘TCP_ULP’ undeclared (first use in this function)
       72 |  err = setsockopt(cli, IPPROTO_TCP, TCP_ULP, "tls", strlen("tls"));
          |                                     ^~~~~~~
    
    Similar to commit that fixes build of sockmap_basic.c on systems with old
    /usr/include fix the build of sockmap_ktls.c
    
    Fixes: d1ba1204f2ee ("selftests/bpf: Test unhashing kTLS socket after removing from map")
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Link: https://lore.kernel.org/bpf/20200219205514.3353788-1-ast@kernel.org

--- tools/testing/selftests/bpf/prog_tests/sockmap_ktls.c
+++ tools/testing/selftests/bpf/prog_tests/sockmap_ktls.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "test_progs.h"
 
 #define MAX_TEST_NAME 80
+#define TCP_ULP 31
 
 static int tcp_server(int family)
 {
