d8d606f1706f187c13fc911dfb15c46fbfa857a4
Christophe Leroy <christophe.leroy@c-s.fr>
2020-02-20 22:51:40 +1100

    powerpc/irq: use IS_ENABLED() in check_stack_overflow()
    Instead of #ifdef, use IS_ENABLED(CONFIG_DEBUG_STACKOVERFLOW).
    This enable GCC to check for code validity even when the option
    is not selected.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200220115141.2707-4-mpe@ellerman.id.au

--- arch/powerpc/kernel/irq.c
+++ arch/powerpc/kernel/irq.c
@@ -600,8 +600,10 @@ u64 arch_irq_stat_cpu(unsigned int cpu)
 
 static inline void check_stack_overflow(void)
 {
-#ifdef CONFIG_DEBUG_STACKOVERFLOW
 	long sp;
+
+	if (!IS_ENABLED(CONFIG_DEBUG_STACKOVERFLOW))
+		return;
 
 	sp = current_stack_pointer & (THREAD_SIZE - 1);
 
@@ -610,7 +612,6 @@ static inline void check_stack_overflow(void)
 		pr_err("do_IRQ: stack overflow: %ld\n", sp);
 		dump_stack();
 	}
-#endif
 }
 
 void __do_irq(struct pt_regs *regs)
