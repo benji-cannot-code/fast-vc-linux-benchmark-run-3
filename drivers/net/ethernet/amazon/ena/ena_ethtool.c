d2ec8f533bad9efe5de82cbc4a5c296271948f01
Colin Ian King <colin.king@canonical.com>
2020-02-21 23:26:53 +0000

    net: ena: ethtool: remove redundant non-zero check on rc
    The non-zero check on rc is redundant as a previous non-zero
    check on rc will always return and the second check is never
    reached, hence it is redundant and can be removed.  Also
    remove a blank line.
    
    Addresses-Coverity: ("Logically dead code")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/amazon/ena/ena_ethtool.c
+++ drivers/net/ethernet/amazon/ena/ena_ethtool.c
@@ -675,7 +675,6 @@ static int ena_get_rxfh(struct net_device *netdev, u32 *indir, u8 *key,
 	 * supports getting/setting the hash function.
 	 */
 	rc = ena_com_get_hash_function(adapter->ena_dev, &ena_func, key);
-
 	if (rc) {
 		if (rc == -EOPNOTSUPP) {
 			key = NULL;
@@ -685,9 +684,6 @@ static int ena_get_rxfh(struct net_device *netdev, u32 *indir, u8 *key,
 
 		return rc;
 	}
-
-	if (rc)
-		return rc;
 
 	switch (ena_func) {
 	case ENA_ADMIN_TOEPLITZ:
