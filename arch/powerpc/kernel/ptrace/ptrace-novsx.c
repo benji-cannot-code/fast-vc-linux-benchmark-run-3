078c3f76f6e522848f829fcffc1df18e23609de9
Al Viro <viro@zeniv.linux.org.uk>
2020-02-22 11:01:44 -0500

    powerpc: switch to ->regset_get()
    Note: compat variant of REGSET_TM_CGPR is almost certainly wrong;
    it claims to be 48*64bit, but just as compat REGSET_GPR it stores
    44*32bit of (truncated) registers + 4 32bit zeros... followed by
    48 more 32bit zeroes.  Might be too late to change - it's a userland
    ABI, after all ;-/
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/powerpc/kernel/ptrace/ptrace-novsx.c
+++ arch/powerpc/kernel/ptrace/ptrace-novsx.c
@@ -20,15 +20,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * };
  */
 int fpr_get(struct task_struct *target, const struct user_regset *regset,
-	    unsigned int pos, unsigned int count, void *kbuf, void __user *ubuf)
+	    struct membuf to)
 {
 	BUILD_BUG_ON(offsetof(struct thread_fp_state, fpscr) !=
 		     offsetof(struct thread_fp_state, fpr[32]));
 
 	flush_fp_to_thread(target);
 
-	return user_regset_copyout(&pos, &count, &kbuf, &ubuf,
-				   &target->thread.fp_state, 0, -1);
+	return membuf_write(&to, &target->thread.fp_state, 33 * sizeof(u64));
 }
 
 /*
