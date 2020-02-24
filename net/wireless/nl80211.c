cf857dd72bca32efa57fe175a51681c2487ec87b
Johannes Berg <johannes.berg@intel.com>
2020-02-24 09:38:15 +0100

    nl80211: explicitly include if_vlan.h
    We use that here, and do seem to get it through some recursive
    include, but better include it explicitly.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Link: https://lore.kernel.org/r/20200224093814.1b9c258fec67.I45ac150d4e11c72eb263abec9f1f0c7add9bef2b@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netlink.h>
 #include <linux/nospec.h>
 #include <linux/etherdevice.h>
+#include <linux/if_vlan.h>
 #include <net/net_namespace.h>
 #include <net/genetlink.h>
 #include <net/cfg80211.h>
