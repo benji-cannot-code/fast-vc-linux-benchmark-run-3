503184739276cd4a73f0bc741d3dede9539c0f2c
Andrew Jones <drjones@redhat.com>
2020-02-24 17:10:49 +0100

    KVM: selftests: Fix unknown ucall command asserts
    The TEST_ASSERT in x86_64/platform_info_test.c would have print 'ucall'
    instead of 'uc.cmd'. Also fix all uc.cmd format types.
    
    Signed-off-by: Andrew Jones <drjones@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- tools/testing/selftests/kvm/x86_64/vmx_dirty_log_test.c
+++ tools/testing/selftests/kvm/x86_64/vmx_dirty_log_test.c
@@ -153,7 +153,7 @@ int main(int argc, char *argv[])
 			done = true;
 			break;
 		default:
-			TEST_ASSERT(false, "Unknown ucall 0x%x.", uc.cmd);
+			TEST_ASSERT(false, "Unknown ucall %lu", uc.cmd);
 		}
 	}
 }
