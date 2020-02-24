cbaad7786f434b29a0c681ad8094db533d774b08
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2020-02-25 00:56:45 +0200

    drm/bridge: lvds-codec: Add to_lvds_codec() function
    Factor out the manual container_of() uses to a common to_lvds_codec()
    macro to shorten lines (and provide better type safety, although that
    won't matter much in this case).
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200224225645.28060-1-laurent.pinchart+renesas@ideasonboard.com

--- drivers/gpu/drm/bridge/lvds-codec.c
+++ drivers/gpu/drm/bridge/lvds-codec.c
@@ -22,10 +22,14 @@ struct lvds_codec {
 	u32 connector_type;
 };
 
+static inline struct lvds_codec *to_lvds_codec(struct drm_bridge *bridge)
+{
+	return container_of(bridge, struct lvds_codec, bridge);
+}
+
 static int lvds_codec_attach(struct drm_bridge *bridge)
 {
-	struct lvds_codec *lvds_codec = container_of(bridge,
-						     struct lvds_codec, bridge);
+	struct lvds_codec *lvds_codec = to_lvds_codec(bridge);
 
 	return drm_bridge_attach(bridge->encoder, lvds_codec->panel_bridge,
 				 bridge);
@@ -33,8 +37,7 @@ static int lvds_codec_attach(struct drm_bridge *bridge)
 
 static void lvds_codec_enable(struct drm_bridge *bridge)
 {
-	struct lvds_codec *lvds_codec = container_of(bridge,
-						     struct lvds_codec, bridge);
+	struct lvds_codec *lvds_codec = to_lvds_codec(bridge);
 
 	if (lvds_codec->powerdown_gpio)
 		gpiod_set_value_cansleep(lvds_codec->powerdown_gpio, 0);
@@ -42,8 +45,7 @@ static void lvds_codec_enable(struct drm_bridge *bridge)
 
 static void lvds_codec_disable(struct drm_bridge *bridge)
 {
-	struct lvds_codec *lvds_codec = container_of(bridge,
-						     struct lvds_codec, bridge);
+	struct lvds_codec *lvds_codec = to_lvds_codec(bridge);
 
 	if (lvds_codec->powerdown_gpio)
 		gpiod_set_value_cansleep(lvds_codec->powerdown_gpio, 1);
