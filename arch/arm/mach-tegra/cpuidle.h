6b3470e99fd80a33287982feda0a1b4fbb78cdc7
Dmitry Osipenko <digetx@gmail.com>
2020-02-25 01:40:53 +0300

    cpuidle: tegra: Squash Tegra30 driver into the common driver
    Tegra20 and Terga30 SoCs have common C1 and CC6 idling states and thus
    share the same code paths, there is no point in having separate drivers
    for a similar hardware. This patch merely moves functionality of the old
    driver into the new, although the CC6 state is kept disabled for now since
    old driver had a rudimentary support for this state (allowing to enter
    into CC6 only when secondary CPUs are put offline), while new driver can
    provide a full-featured support. The new feature will be enabled by
    another patch.
    
    Acked-by: Peter De Schrijver <pdeschrijver@nvidia.com>
    Tested-by: Peter Geis <pgwipeout@gmail.com>
    Tested-by: Jasper Korten <jja2000@gmail.com>
    Tested-by: David Heidelberg <david@ixit.cz>
    Tested-by: Nicolas Chauvet <kwizart@gmail.com>
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- arch/arm/mach-tegra/cpuidle.h
+++ arch/arm/mach-tegra/cpuidle.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __MACH_TEGRA_CPUIDLE_H
 
 #ifdef CONFIG_CPU_IDLE
-int tegra30_cpuidle_init(void);
 int tegra114_cpuidle_init(void);
 void tegra_cpuidle_init(void);
 #else
