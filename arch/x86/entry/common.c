061d2fa018128de22a855a58d851494e7e60215a
Thomas Gleixner <tglx@linutronix.de>
2020-02-25 23:08:05 +0100

    x86/entry: Move irq tracing on syscall entry to C-code
    Now that the C entry points are safe, move the irq flags tracing code into
    the entry helper:
    
        - Invoke lockdep before calling into context tracking
    
        - Use the safe trace_hardirqs_on_prepare() trace function after context
          tracking established state and RCU is watching.
    
    enter_from_user_mode() is also still invoked from the exception/interrupt
    entry code which still contains the ASM irq flags tracing. So this is just
    a redundant and harmless invocation of tracing / lockdep until these are
    removed as well.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200505134340.611961721@linutronix.de

--- arch/x86/entry/common.c
+++ arch/x86/entry/common.c
@@ -41,19 +41,36 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <trace/events/syscalls.h>
 
 #ifdef CONFIG_CONTEXT_TRACKING
-/* Called on entry from user mode with IRQs off. */
+/**
+ * enter_from_user_mode - Establish state when coming from user mode
+ *
+ * Syscall entry disables interrupts, but user mode is traced as interrupts
+ * enabled. Also with NO_HZ_FULL RCU might be idle.
+ *
+ * 1) Tell lockdep that interrupts are disabled
+ * 2) Invoke context tracking if enabled to reactivate RCU
+ * 3) Trace interrupts off state
+ */
 __visible noinstr void enter_from_user_mode(void)
 {
 	enum ctx_state state = ct_state();
 
+	lockdep_hardirqs_off(CALLER_ADDR0);
 	user_exit_irqoff();
 
 	instrumentation_begin();
 	CT_WARN_ON(state != CONTEXT_USER);
+	trace_hardirqs_off_prepare();
 	instrumentation_end();
 }
 #else
-static inline void enter_from_user_mode(void) {}
+static __always_inline void enter_from_user_mode(void)
+{
+	lockdep_hardirqs_off(CALLER_ADDR0);
+	instrumentation_begin();
+	trace_hardirqs_off_prepare();
+	instrumentation_end();
+}
 #endif
 
 static noinstr void exit_to_user_mode(void)
