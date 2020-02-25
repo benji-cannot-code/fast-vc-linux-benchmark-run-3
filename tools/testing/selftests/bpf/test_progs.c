7b721776b3e93103572f4f27277d5bc4f049e8c4
Andrii Nakryiko <andriin@fb.com>
2020-02-24 16:08:47 -0800

    selftests/bpf: Print backtrace on SIGSEGV in test_progs
    Due to various bugs in tests clean up code (usually), if host system is
    misconfigured, it happens that test_progs will just crash in the middle of
    running a test with little to no indication of where and why the crash
    happened. For cases where coredump is not readily available (e.g., inside
    a CI), it's very helpful to have a stack trace, which lead to crash, to be
    printed out. This change adds a signal handler that will capture and print out
    symbolized backtrace:
    
      $ sudo ./test_progs -t mmap
      test_mmap:PASS:skel_open_and_load 0 nsec
      test_mmap:PASS:bss_mmap 0 nsec
      test_mmap:PASS:data_mmap 0 nsec
      Caught signal #11!
      Stack trace:
      ./test_progs(crash_handler+0x18)[0x42a888]
      /lib64/libpthread.so.0(+0xf5d0)[0x7f2aab5175d0]
      ./test_progs(test_mmap+0x3c0)[0x41f0a0]
      ./test_progs(main+0x160)[0x407d10]
      /lib64/libc.so.6(__libc_start_main+0xf5)[0x7f2aab15d3d5]
      ./test_progs[0x407ebc]
      [1]    1988412 segmentation fault (core dumped)  sudo ./test_progs -t mmap
    
    Unfortunately, glibc's symbolization support is unable to symbolize static
    functions, only global ones will be present in stack trace. But it's still a
    step forward without adding extra libraries to get a better symbolization.
    
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Song Liu <songliubraving@fb.com>
    Link: https://lore.kernel.org/bpf/20200225000847.3965188-1-andriin@fb.com

--- tools/testing/selftests/bpf/test_progs.c
+++ tools/testing/selftests/bpf/test_progs.c
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "bpf_rlimit.h"
 #include <argp.h>
 #include <string.h>
+#include <signal.h>
+#include <execinfo.h> /* backtrace */
 
 /* defined in test_progs.h */
 struct test_env env = {};
@@ -618,6 +620,23 @@ int cd_flavor_subdir(const char *exec_name)
 	return chdir(flavor);
 }
 
+#define MAX_BACKTRACE_SZ 128
+void crash_handler(int signum)
+{
+	void *bt[MAX_BACKTRACE_SZ];
+	size_t sz;
+
+	sz = backtrace(bt, ARRAY_SIZE(bt));
+
+	if (env.test)
+		dump_test_log(env.test, true);
+	if (env.stdout)
+		stdio_restore();
+
+	fprintf(stderr, "Caught signal #%d!\nStack trace:\n", signum);
+	backtrace_symbols_fd(bt, sz, STDERR_FILENO);
+}
+
 int main(int argc, char **argv)
 {
 	static const struct argp argp = {
@@ -625,7 +644,13 @@ int main(int argc, char **argv)
 		.parser = parse_arg,
 		.doc = argp_program_doc,
 	};
+	struct sigaction sigact = {
+		.sa_handler = crash_handler,
+		.sa_flags = SA_RESETHAND,
+	};
 	int err, i;
+
+	sigaction(SIGSEGV, &sigact, NULL);
 
 	err = argp_parse(&argp, argc, argv, 0, NULL, &env);
 	if (err)
