61765aa91147edf6426ba7ed235e82a68a2d6244
Paolo Abeni <pabeni@redhat.com>
2020-02-26 10:14:47 +0100

    mptcp: add work queue skeleton
    Will be extended with functionality in followup patches.
    Initial user is moving skbs from subflows receive queue to
    the mptcp-level receive queue.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Reviewed-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -71,6 +71,7 @@ struct mptcp_sock {
 	u32		token;
 	unsigned long	flags;
 	bool		can_ack;
+	struct work_struct work;
 	struct list_head conn_list;
 	struct skb_ext	*cached_ext;	/* for the next sendmsg */
 	struct socket	*subflow; /* outgoing connect/listener/!mp_capable */
