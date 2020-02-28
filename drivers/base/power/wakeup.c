45f504e8135315b12aed4e4c0404e09ffc556f8b
Madhuparna Bhowmik <madhuparnabhowmik10@gmail.com>
2020-02-28 23:17:45 +0530

    drivers: base: power: wakeup.c: Use built-in RCU list checking
    Pass cond argument to list_for_each_entry_rcu() to fix the
    following false positive lockdep warning and other uses of
    list_for_each_entry_rcu() in wakeup.c.
    
    [  331.934648] =============================
    [  331.934650] WARNING: suspicious RCU usage
    [  331.934653] 5.6.0-rc1+ #5 Not tainted
    [  331.934655] -----------------------------
    [  331.934657] drivers/base/power/wakeup.c:408 RCU-list traversed in non-reader section!!
    
    [  333.025156] =============================
    [  333.025161] WARNING: suspicious RCU usage
    [  333.025168] 5.6.0-rc1+ #5 Not tainted
    [  333.025173] -----------------------------
    [  333.025180] drivers/base/power/wakeup.c:424 RCU-list traversed in non-reader section!!
    
    Cc: "Rafael J. Wysocki" <rjw@rjwysocki.net>
    Signed-off-by: Madhuparna Bhowmik <madhuparnabhowmik10@gmail.com>
    Link: https://lore.kernel.org/r/20200228174745.9308-1-madhuparnabhowmik10@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -406,7 +406,8 @@ void device_wakeup_arm_wake_irqs(void)
 	int srcuidx;
 
 	srcuidx = srcu_read_lock(&wakeup_srcu);
-	list_for_each_entry_rcu(ws, &wakeup_sources, entry)
+	list_for_each_entry_rcu(ws, &wakeup_sources, entry,
+				srcu_read_lock_held(&wakeup_srcu))
 		dev_pm_arm_wake_irq(ws->wakeirq);
 	srcu_read_unlock(&wakeup_srcu, srcuidx);
 }
@@ -422,7 +423,8 @@ void device_wakeup_disarm_wake_irqs(void)
 	int srcuidx;
 
 	srcuidx = srcu_read_lock(&wakeup_srcu);
-	list_for_each_entry_rcu(ws, &wakeup_sources, entry)
+	list_for_each_entry_rcu(ws, &wakeup_sources, entry,
+				srcu_read_lock_held(&wakeup_srcu))
 		dev_pm_disarm_wake_irq(ws->wakeirq);
 	srcu_read_unlock(&wakeup_srcu, srcuidx);
 }
@@ -875,7 +877,8 @@ void pm_print_active_wakeup_sources(void)
 	struct wakeup_source *last_activity_ws = NULL;
 
 	srcuidx = srcu_read_lock(&wakeup_srcu);
-	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
+	list_for_each_entry_rcu(ws, &wakeup_sources, entry,
+				srcu_read_lock_held(&wakeup_srcu)) {
 		if (ws->active) {
 			pm_pr_dbg("active wakeup source: %s\n", ws->name);
 			active = 1;
@@ -1026,7 +1029,8 @@ void pm_wakep_autosleep_enabled(bool set)
 	int srcuidx;
 
 	srcuidx = srcu_read_lock(&wakeup_srcu);
-	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
+	list_for_each_entry_rcu(ws, &wakeup_sources, entry,
+				srcu_read_lock_held(&wakeup_srcu)) {
 		spin_lock_irq(&ws->lock);
 		if (ws->autosleep_enabled != set) {
 			ws->autosleep_enabled = set;
@@ -1105,7 +1109,8 @@ static void *wakeup_sources_stats_seq_start(struct seq_file *m,
 	}
 
 	*srcuidx = srcu_read_lock(&wakeup_srcu);
-	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
+	list_for_each_entry_rcu(ws, &wakeup_sources, entry,
+				srcu_read_lock_held(&wakeup_srcu)) {
 		if (n-- <= 0)
 			return ws;
 	}
