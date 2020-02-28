cfe129c02b0443e9bebba5fbeb330c40b1e454fe
Christophe Leroy <christophe.leroy@c-s.fr>
2020-02-28 00:14:40 +0000

    powerpc/ptrace: drop PARAMETER_SAVE_AREA_OFFSET
    PARAMETER_SAVE_AREA_OFFSET is not used, drop it.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@c-s.fr>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/6dac2b49207647f75cbf0e6771a545e691f0fd93.1582848567.git.christophe.leroy@c-s.fr

--- arch/powerpc/kernel/ptrace/ptrace.c
+++ arch/powerpc/kernel/ptrace/ptrace.c
@@ -34,16 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CREATE_TRACE_POINTS
 #include <trace/events/syscalls.h>
 
-/*
- * The parameter save area on the stack is used to store arguments being passed
- * to callee function and is located at fixed offset from stack pointer.
- */
-#ifdef CONFIG_PPC32
-#define PARAMETER_SAVE_AREA_OFFSET	24  /* bytes */
-#else /* CONFIG_PPC32 */
-#define PARAMETER_SAVE_AREA_OFFSET	48  /* bytes */
-#endif
-
 struct pt_regs_offset {
 	const char *name;
 	int offset;
