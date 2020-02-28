3014aab07aa8d9b68353eb7912432c2d959679ae
Ard Biesheuvel <ardb@kernel.org>
2020-02-28 13:14:07 +0100

    efi/arm64: Clean EFI stub exit code from cache instead of avoiding it
    Commit 9f9223778 ("efi/libstub/arm: Make efi_entry() an ordinary PE/COFF
    entrypoint") modified the handover code written in assembler, and for
    maintainability, aligned the logic with the logic used in the 32-bit ARM
    version, which is to avoid cache maintenance on the remaining instructions
    in the subroutine that will be executed with the MMU and caches off, and
    instead, branch into the relocated copy of the kernel image.
    
    However, this assumes that this copy is executable, and this means we
    expect EFI_LOADER_DATA regions to be executable as well, which is not
    a reasonable assumption to make, even if this is true for most UEFI
    implementations today.
    
    So change this back, and add a __clean_dcache_area_poc() call to cover
    the remaining code in the subroutine. While at it, switch the other
    call site over to __clean_dcache_area_poc() as well, and clean up the
    terminology in comments to avoid using 'flush' in the context of cache
    maintenance. Also, let's switch to the new style asm annotations.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: linux-efi@vger.kernel.org
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Heinrich Schuchardt <xypron.glpk@gmx.de>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Link: https://lore.kernel.org/r/20200228121408.9075-6-ardb@kernel.org

--- arch/arm64/kernel/image-vars.h
+++ arch/arm64/kernel/image-vars.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_EFI
 
 __efistub_kernel_size		= _edata - _text;
+__efistub_stext_offset		= stext - _text;
 
 
 /*
@@ -35,7 +36,7 @@ __efistub_strnlen		= __pi_strnlen;
 __efistub_strcmp		= __pi_strcmp;
 __efistub_strncmp		= __pi_strncmp;
 __efistub_strrchr		= __pi_strrchr;
-__efistub___flush_dcache_area	= __pi___flush_dcache_area;
+__efistub___clean_dcache_area_poc = __pi___clean_dcache_area_poc;
 
 #ifdef CONFIG_KASAN
 __efistub___memcpy		= __pi_memcpy;
@@ -44,7 +45,6 @@ __efistub___memset		= __pi_memset;
 #endif
 
 __efistub__text			= _text;
-__efistub_stext			= stext;
 __efistub__end			= _end;
 __efistub__edata		= _edata;
 __efistub_screen_info		= screen_info;
