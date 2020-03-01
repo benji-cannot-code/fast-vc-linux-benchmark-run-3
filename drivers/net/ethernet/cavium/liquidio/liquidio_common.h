16664bcc352678d90f779055dde5ae4e3c5c3c9a
Leon Romanovsky <leonro@mellanox.com>
2020-03-01 16:44:38 +0200

    net/liquidio: Delete non-working LIQUIDIO_PACKAGE check
    Size of LIQUIDIO_PACKAGE is 0 and it means that checks of package
    version never worked, delete dead code.
    
    Fixes: 3258124534f6 ("liquidio: Consolidate common functionality")
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/cavium/liquidio/liquidio_common.h
+++ drivers/net/ethernet/cavium/liquidio/liquidio_common.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "octeon_config.h"
 
-#define LIQUIDIO_PACKAGE ""
 #define LIQUIDIO_BASE_MAJOR_VERSION 1
 #define LIQUIDIO_BASE_MINOR_VERSION 7
 #define LIQUIDIO_BASE_MICRO_VERSION 2
