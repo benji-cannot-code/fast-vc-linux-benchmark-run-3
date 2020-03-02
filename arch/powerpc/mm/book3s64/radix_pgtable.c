5ada5d223cff90c6c5c21f58d4677b9edb08d565
Nicholas Piggin <npiggin@gmail.com>
2020-03-02 11:04:10 +1000

    powerpc/64s/radix: Fix CONFIG_SMP=n build
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200302010410.2957362-1-npiggin@gmail.com

--- arch/powerpc/mm/book3s64/radix_pgtable.c
+++ arch/powerpc/mm/book3s64/radix_pgtable.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/firmware.h>
 #include <asm/powernv.h>
 #include <asm/sections.h>
+#include <asm/smp.h>
 #include <asm/trace.h>
 #include <asm/uaccess.h>
 #include <asm/ultravisor.h>
