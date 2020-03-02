06ba424ad0a23e99926f6f2a96a5d7c8fe95555c
Anson Huang <Anson.Huang@nxp.com>
2020-03-02 10:51:25 +0800

    thermal: imx_sc_thermal: Align imx sc thermal msg structs to 4
    The i.MX SCU API strongly assumes that messages are composed out
    of 4-bytes words but some of our message structs have odd sizeofs,
    use __packed __aligned(4) to avoid potential oopses.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/1583117485-30922-1-git-send-email-Anson.Huang@nxp.com

--- drivers/thermal/imx_sc_thermal.c
+++ drivers/thermal/imx_sc_thermal.c
@@ -28,12 +28,12 @@ struct imx_sc_sensor {
 struct req_get_temp {
 	u16 resource_id;
 	u8 type;
-} __packed;
+} __packed __aligned(4);
 
 struct resp_get_temp {
 	u16 celsius;
 	u8 tenths;
-} __packed;
+} __packed __aligned(4);
 
 struct imx_sc_msg_misc_get_temp {
 	struct imx_sc_rpc_msg hdr;
@@ -41,7 +41,7 @@ struct imx_sc_msg_misc_get_temp {
 		struct req_get_temp req;
 		struct resp_get_temp resp;
 	} data;
-};
+} __packed __aligned(4);
 
 static int imx_sc_thermal_get_temp(void *data, int *temp)
 {
