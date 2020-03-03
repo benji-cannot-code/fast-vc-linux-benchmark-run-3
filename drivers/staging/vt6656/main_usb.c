f4db8e9d71f1e90d33151a392460f85e2263b78d
Malcolm Priestley <tvboxspy@gmail.com>
2020-03-03 20:41:05 +0000

    staging: vt6656: Move vnt_rx_data to usbpipe.c
    vnt_rx_data is a USB function and vnt_submit_rx_urb_complete is
    the only caller therefore removing dpc.c/h files and becoming
    static.
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Link: https://lore.kernel.org/r/d3937276-5a47-7184-e263-84d9c9cb5c7a@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/main_usb.c
+++ drivers/staging/vt6656/main_usb.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "power.h"
 #include "wcmd.h"
 #include "rxtx.h"
-#include "dpc.h"
 #include "rf.h"
 #include "firmware.h"
 #include "usbpipe.h"
