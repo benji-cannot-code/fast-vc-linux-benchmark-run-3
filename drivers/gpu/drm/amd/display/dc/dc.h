d715a2ef80a5dbb453a1f22507ba669ad8d3c26d
Martin Tsai <martin.tsai@amd.com>
2020-03-03 10:04:08 +0800

    drm/amd/display: differentiate vsc sdp colorimetry use criteria between MST and SST
    [Why]
    We should check MST BU support capability on output port before building
    vsc info packet.
    
    [How]
    Add a new definition for port and sink capability check.
    
    Signed-off-by: Martin Tsai <martin.tsai@amd.com>
    Reviewed-by: Wenjing Liu <Wenjing.Liu@amd.com>
    Acked-by: Rodrigo Siqueira <Rodrigo.Siqueira@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dc.h
+++ drivers/gpu/drm/amd/display/dc/dc.h
@@ -1048,6 +1048,8 @@ struct dc_sink {
 	struct dc_sink_dsc_caps dsc_caps;
 	struct dc_sink_fec_caps fec_caps;
 
+	bool is_vsc_sdp_colorimetry_supported;
+
 	/* private to DC core */
 	struct dc_link *link;
 	struct dc_context *ctx;
