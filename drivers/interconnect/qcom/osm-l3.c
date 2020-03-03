8bcbb8b1bae7596bf91a5d9f52a9a9b0651a6dfc
Sibi Sankar <sibis@codeaurora.org>
2020-03-03 19:02:52 +0200

    interconnect: qcom: Add OSM L3 support on SC7180
    Add Operating State Manager (OSM) L3 interconnect provider support on
    SC7180 SoCs.
    
    Signed-off-by: Sibi Sankar <sibis@codeaurora.org>
    Reviewed-by: Evan Green <evgreen@chromium.org>
    Link: https://lore.kernel.org/r/20200227105632.15041-6-sibis@codeaurora.org
    Signed-off-by: Georgi Djakov <georgi.djakov@linaro.org>

--- drivers/interconnect/qcom/osm-l3.c
+++ drivers/interconnect/qcom/osm-l3.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <dt-bindings/interconnect/qcom,osm-l3.h>
 
+#include "sc7180.h"
 #include "sdm845.h"
 
 #define LUT_MAX_ENTRIES			40U
@@ -81,6 +82,19 @@ static struct qcom_icc_node *sdm845_osm_l3_nodes[] = {
 const static struct qcom_icc_desc sdm845_icc_osm_l3 = {
 	.nodes = sdm845_osm_l3_nodes,
 	.num_nodes = ARRAY_SIZE(sdm845_osm_l3_nodes),
+};
+
+DEFINE_QNODE(sc7180_osm_apps_l3, SC7180_MASTER_OSM_L3_APPS, 16, SC7180_SLAVE_OSM_L3);
+DEFINE_QNODE(sc7180_osm_l3, SC7180_SLAVE_OSM_L3, 16);
+
+static struct qcom_icc_node *sc7180_osm_l3_nodes[] = {
+	[MASTER_OSM_L3_APPS] = &sc7180_osm_apps_l3,
+	[SLAVE_OSM_L3] = &sc7180_osm_l3,
+};
+
+const static struct qcom_icc_desc sc7180_icc_osm_l3 = {
+	.nodes = sc7180_osm_l3_nodes,
+	.num_nodes = ARRAY_SIZE(sc7180_osm_l3_nodes),
 };
 
 static int qcom_icc_set(struct icc_node *src, struct icc_node *dst)
@@ -243,6 +257,7 @@ err:
 }
 
 static const struct of_device_id osm_l3_of_match[] = {
+	{ .compatible = "qcom,sc7180-osm-l3", .data = &sc7180_icc_osm_l3 },
 	{ .compatible = "qcom,sdm845-osm-l3", .data = &sdm845_icc_osm_l3 },
 	{ }
 };
