5b4bbfd375f2e86c31fe89e1ead95729733d66ba
Parav Pandit <parav@mellanox.com>
2020-03-03 08:12:43 -0600

    net/mlx5e: Use devlink virtual flavour for VF devlink port
    Use newly introduce 'virtual' port flavour for devlink
    port of PCI VF devlink device in non-representors mode.
    
    While at it, remove recently introduced empty lines at end of the file.
    
    Reviewed-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: Parav Pandit <parav@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlx5/core/en/devlink.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en/devlink.h
@@ -8,9 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/devlink.h>
 #include "en.h"
 
-int mlx5e_devlink_phy_port_register(struct net_device *dev);
-void mlx5e_devlink_phy_port_unregister(struct mlx5e_priv *priv);
-struct devlink_port *mlx5e_get_devlink_phy_port(struct net_device *dev);
+int mlx5e_devlink_port_register(struct net_device *dev);
+void mlx5e_devlink_port_unregister(struct mlx5e_priv *priv);
+struct devlink_port *mlx5e_get_devlink_port(struct net_device *dev);
 
 #endif
-
