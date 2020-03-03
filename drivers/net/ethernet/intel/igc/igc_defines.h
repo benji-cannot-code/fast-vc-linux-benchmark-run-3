f2bc54896eda2593c16c7cbbfd9eb5275e956fdb
Sasha Neftin <sasha.neftin@intel.com>
2020-03-03 20:26:10 +0200

    igc: Remove copper fiber switch control
    i225 device support copper mode only
    PHY signal detect indication for copper fiber switch
    not applicable to i225 part
    
    Signed-off-by: Sasha Neftin <sasha.neftin@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/igc/igc_defines.h
+++ drivers/net/ethernet/intel/igc/igc_defines.h
@@ -92,8 +92,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IGC_CTRL_RFCE		0x08000000  /* Receive Flow Control enable */
 #define IGC_CTRL_TFCE		0x10000000  /* Transmit flow control enable */
 
-#define IGC_CONNSW_AUTOSENSE_EN	0x1
-
 /* As per the EAS the maximum supported size is 9.5KB (9728 bytes) */
 #define MAX_JUMBO_FRAME_SIZE	0x2600
 
