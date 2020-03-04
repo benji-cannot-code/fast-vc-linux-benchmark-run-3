a671d2cdce226c9778be26e3053d101ee06ce1c5
Nikolay Borisov <nborisov@suse.com>
2020-03-04 18:12:45 +0200

    vfs: Remove duplicated d_mountpoint check in __is_local_mountpoint
    This function acts as an out-of-line helper for is_local_mountpoint
    is only called after the latter verifies the dentry is not a mountpoint.
    There's no semantic changes and the resulting object code is smaller:
    
    add/remove: 0/0 grow/shrink: 0/1 up/down: 0/-26 (-26)
    Function                                     old     new   delta
    __is_local_mountpoint                        147     121     -26
    Total: Before=34161, After=34135, chg -0.08%
    
    Signed-off-by: Nikolay Borisov <nborisov@suse.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/namespace.c
+++ fs/namespace.c
@@ -670,9 +670,6 @@ bool __is_local_mountpoint(struct dentry *dentry)
 	struct mount *mnt;
 	bool is_covered = false;
 
-	if (!d_mountpoint(dentry))
-		goto out;
-
 	down_read(&namespace_sem);
 	list_for_each_entry(mnt, &ns->list, mnt_list) {
 		is_covered = (mnt->mnt_mountpoint == dentry);
@@ -680,7 +677,7 @@ bool __is_local_mountpoint(struct dentry *dentry)
 			break;
 	}
 	up_read(&namespace_sem);
-out:
+
 	return is_covered;
 }
 
