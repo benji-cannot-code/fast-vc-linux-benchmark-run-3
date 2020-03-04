a8fb847e632d16af092f9fffb31b13caf2e23571
Marc Zyngier <maz@kernel.org>
2020-03-04 20:33:19 +0000

    irqchip/gic-v4.1: Plumb set_vcpu_affinity SGI callbacks
    Just like for vLPIs, there is some configuration information that cannot
    be directly communicated through the normal irqchip API, and we have to
    use our good old friend set_vcpu_affinity as a side-band communication
    mechanism.
    
    This is used to configure group and priority for a given vSGI.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Reviewed-by: Zenghui Yu <yuzenghui@huawei.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Link: https://lore.kernel.org/r/20200304203330.4967-13-maz@kernel.org

--- include/linux/irqchip/arm-gic-v4.h
+++ include/linux/irqchip/arm-gic-v4.h
@@ -104,6 +104,7 @@ enum its_vcpu_info_cmd_type {
 	SCHEDULE_VPE,
 	DESCHEDULE_VPE,
 	INVALL_VPE,
+	PROP_UPDATE_VSGI,
 };
 
 struct its_cmd_info {
@@ -115,6 +116,10 @@ struct its_cmd_info {
 		struct {
 			bool		g0en;
 			bool		g1en;
+		};
+		struct {
+			u8		priority;
+			bool		group;
 		};
 	};
 };
