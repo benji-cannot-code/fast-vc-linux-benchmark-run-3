ca8cd888cb52a06c2158de13233f3be0be95808e
Cezary Rojewski <cezary.rojewski@intel.com>
2020-03-05 15:53:14 +0100

    ASoC: Intel: Skylake: Await purge request ack on CNL
    Each purge request is sent by driver after master core is powered up and
    unresetted but before it is unstalled. On unstall, ROM begins processing
    the request and initializing environment for FW load. Host should await
    ROM's ack before moving forward. Without doing so, ROM init poll may
    start too early and false timeouts can occur.
    
    Fixes: cb6a55284629 ("ASoC: Intel: cnl: Add sst library functions for cnl platform")
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20200305145314.32579-8-cezary.rojewski@intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/skylake/bxt-sst.c
+++ sound/soc/intel/skylake/bxt-sst.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "skl.h"
 
 #define BXT_BASEFW_TIMEOUT	3000
-#define BXT_INIT_TIMEOUT	300
 #define BXT_ROM_INIT_TIMEOUT	70
 #define BXT_IPC_PURGE_FW	0x01004000
 
