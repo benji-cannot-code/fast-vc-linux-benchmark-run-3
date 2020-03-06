900a0b916ccb866edf84441ee4886ff26ad89d94
YueHaibing <yuehaibing@huawei.com>
2020-03-06 11:17:05 +0800

    selftests/timens: Remove duplicated include <time.h>
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Reviewed-by: Dmitry Safonov <dima@arista.com>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>

--- tools/testing/selftests/timens/timer.c
+++ tools/testing/selftests/timens/timer.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdio.h>
 #include <stdint.h>
 #include <signal.h>
-#include <time.h>
 
 #include "log.h"
 #include "timens.h"
