2ab6e3854d65738c0b230d31b52fcef45a68d5f2
Rohit Maheshwari <rohitm@chelsio.com>
2020-03-07 20:06:07 +0530

    chcr: Handle first or middle part of record
    This patch contains handling of first part or middle part of the record.
    When we get a middle record, we will fetch few already sent bytes to
    make packet start 16 byte aligned.
    And if the packet has only the header part, we don't need to send it for
    packet encryption, send that packet as a plaintext.
    
    v1->v2:
    - un-necessary updating left variable.
    
    v3->v4:
    - replaced kfree_skb with dev_kfree_skb_any.
    
    Signed-off-by: Rohit Maheshwari <rohitm@chelsio.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/crypto/chelsio/chcr_common.h
+++ drivers/crypto/chelsio/chcr_common.h
@@ -11,10 +11,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CHCR_KEYCTX_MAC_KEY_SIZE_128       0
 #define CHCR_KEYCTX_CIPHER_KEY_SIZE_128    0
 #define CHCR_SCMD_CIPHER_MODE_AES_GCM      2
+#define CHCR_SCMD_CIPHER_MODE_AES_CTR      3
 #define CHCR_CPL_TX_SEC_PDU_LEN_64BIT      2
 #define CHCR_SCMD_SEQ_NO_CTRL_64BIT        3
 #define CHCR_SCMD_PROTO_VERSION_TLS        0
+#define CHCR_SCMD_PROTO_VERSION_GENERIC    4
 #define CHCR_SCMD_AUTH_MODE_GHASH          4
+#define AES_BLOCK_LEN                      16
 
 enum chcr_state {
 	CHCR_INIT = 0,
