d2f2636ad70e0f00730395e45235c6f51d93bce4
Sherry Zong <sherry.zong@unisoc.com>
2020-03-09 16:18:44 +0800

    power: reset: sc27xx: Power off the external subsystems' connection
    When powering off the whole system, we should power off some external
    subsystems' connection firstly, otherwise some external subsystems
    will hold some power and result in powering down abnormally.
    
    Signed-off-by: Sherry Zong <sherry.zong@unisoc.com>
    Signed-off-by: Baolin Wang <baolin.wang7@gmail.com>
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

--- drivers/power/reset/sc27xx-poweroff.c
+++ drivers/power/reset/sc27xx-poweroff.c
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define SC27XX_PWR_PD_HW	0xc2c
 #define SC27XX_PWR_OFF_EN	BIT(0)
+#define SC27XX_SLP_CTRL		0xdf0
+#define SC27XX_LDO_XTL_EN	BIT(3)
 
 static struct regmap *regmap;
 
@@ -41,6 +43,9 @@ static struct syscore_ops poweroff_syscore_ops = {
 
 static void sc27xx_poweroff_do_poweroff(void)
 {
+	/* Disable the external subsys connection's power firstly */
+	regmap_write(regmap, SC27XX_SLP_CTRL, SC27XX_LDO_XTL_EN);
+
 	regmap_write(regmap, SC27XX_PWR_PD_HW, SC27XX_PWR_OFF_EN);
 }
 
