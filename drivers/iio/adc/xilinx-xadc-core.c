8002385c566c8b2e07d5f1e584205306f27c6e7a
Lars-Peter Clausen <lars@metafoo.de>
2020-03-09 11:28:47 +0100

    iio: xilinx-xadc: Fix typo in author's name
    It appears the author of the xilinx-xadc driver can't even spell his own
    name correctly. Fix that.
    
    Reported-by: Lars Möllendorf <lars.moellendorf@plating.de>
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/adc/xilinx-xadc-core.c
+++ drivers/iio/adc/xilinx-xadc-core.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Xilinx XADC driver
  *
  * Copyright 2013-2014 Analog Devices Inc.
- *  Author: Lars-Peter Clauen <lars@metafoo.de>
+ *  Author: Lars-Peter Clausen <lars@metafoo.de>
  *
  * Documentation for the parts can be found at:
  *  - XADC hardmacro: Xilinx UG480
