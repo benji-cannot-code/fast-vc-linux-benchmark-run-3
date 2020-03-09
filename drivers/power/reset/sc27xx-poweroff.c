c9eefd2788f09b420f0bb5134eb11b5c534c48e9
Baolin Wang <baolin.wang7@gmail.com>
2020-03-09 16:18:46 +0800

    power: reset: sc27xx: Allow the SC27XX poweroff driver building into a module
    Change the config to 'tristate' and use module_platform_driver() to
    allow the SC27XX poweroff driver building into a module, as well as
    adding some mudule information.
    
    Signed-off-by: Baolin Wang <baolin.wang7@gmail.com>
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

--- drivers/power/reset/sc27xx-poweroff.c
+++ drivers/power/reset/sc27xx-poweroff.c
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/cpu.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/pm.h>
 #include <linux/regmap.h>
@@ -72,4 +73,8 @@ static struct platform_driver sc27xx_poweroff_driver = {
 		.name = "sc27xx-poweroff",
 	},
 };
-builtin_platform_driver(sc27xx_poweroff_driver);
+module_platform_driver(sc27xx_poweroff_driver);
+
+MODULE_DESCRIPTION("Power off driver for SC27XX PMIC Device");
+MODULE_AUTHOR("Baolin Wang <baolin.wang@unisoc.com>");
+MODULE_LICENSE("GPL v2");
