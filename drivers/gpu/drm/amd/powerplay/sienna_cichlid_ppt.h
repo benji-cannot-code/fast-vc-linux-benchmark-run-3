5cdc0ba681697525c251661b242adc6191cf0a43
Likun Gao <Likun.Gao@amd.com>
2020-03-10 12:22:34 +0800

    drm/amd/powerplay: support to print pcie levels for sienna_cichlid
    Support to print PCIE clk levels for sienna_cichlid.
    
    Signed-off-by: Likun Gao <Likun.Gao@amd.com>
    Reviewed-by: Kenneth Feng <kenneth.feng@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/sienna_cichlid_ppt.h
+++ drivers/gpu/drm/amd/powerplay/sienna_cichlid_ppt.h
@@ -26,4 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern void sienna_cichlid_set_ppt_funcs(struct smu_context *smu);
 
+#define smnPCIE_LC_SPEED_CNTL                   0x11140290
+#define smnPCIE_LC_LINK_WIDTH_CNTL              0x11140288
+
 #endif
