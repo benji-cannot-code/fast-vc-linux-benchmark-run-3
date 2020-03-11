7241804f7ab542ce99e5cc4b0678d33412794493
Takashi Iwai <tiwai@suse.de>
2020-03-11 09:47:11 +0100

    b43legacy: Use scnprintf() for avoiding potential buffer overflow
    Since snprintf() returns the would-be-output size instead of the
    actual output size, the succeeding calls may go beyond the given
    buffer limit.  Fix it by replacing with scnprintf().
    
    Cc: Larry Finger <Larry.Finger@lwfinger.net>
    Cc: b43-dev@lists.infradead.org
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/broadcom/b43legacy/debugfs.c
+++ drivers/net/wireless/broadcom/b43legacy/debugfs.c
@@ -55,7 +55,7 @@ struct b43legacy_dfs_file * fops_to_dfs_file(struct b43legacy_wldev *dev,
 #define fappend(fmt, x...)	\
 	do {							\
 		if (bufsize - count)				\
-			count += snprintf(buf + count,		\
+			count += scnprintf(buf + count,		\
 					  bufsize - count,	\
 					  fmt , ##x);		\
 		else						\
