d92678070562b25a29e0411b453e97ae85a2b36f
Jules Irenge <jbi.octave@gmail.com>
2020-03-11 01:09:08 +0000

    ALSA: firewire-tascam: Add missing annotation for tscm_hwdep_read_locked()
    Sparse reports a warning at tscm_hwdep_read_locked()
    
    warning: context imbalance in tscm_hwdep_read_locked() - unexpected unlock
    
    The root cause is the missing annotation at tscm_hwdep_read_locked()
    Add the missing __releases(&tscm->lock) annotation
    
    Signed-off-by: Jules Irenge <jbi.octave@gmail.com>
    Acked-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Link: https://lore.kernel.org/r/20200311010908.42366-9-jbi.octave@gmail.com
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/tascam/tascam-hwdep.c
+++ sound/firewire/tascam/tascam-hwdep.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static long tscm_hwdep_read_locked(struct snd_tscm *tscm, char __user *buf,
 				   long count, loff_t *offset)
+	__releases(&tscm->lock)
 {
 	struct snd_firewire_event_lock_status event = {
 		.type = SNDRV_FIREWIRE_EVENT_LOCK_STATUS,
