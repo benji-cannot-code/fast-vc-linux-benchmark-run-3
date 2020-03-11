b58ddf539418bfa5c2cc0b355e3e3a874bbcb046
Lukas Wunner <lukas@wunner.de>
2020-03-11 12:59:01 +0100

    netfilter: Rename ingress hook include file
    Prepare for addition of a netfilter egress hook by renaming
    <linux/netfilter_ingress.h> to <linux/netfilter_netdev.h>.
    
    The egress hook also necessitates a refactoring of the include file,
    but that is done in a separate commit to ease reviewing.
    
    No functional change intended.
    
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/core/dev.c
+++ net/core/dev.c
@@ -136,7 +136,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_macvlan.h>
 #include <linux/errqueue.h>
 #include <linux/hrtimer.h>
-#include <linux/netfilter_ingress.h>
+#include <linux/netfilter_netdev.h>
 #include <linux/crash_dump.h>
 #include <linux/sctp.h>
 #include <net/udp_tunnel.h>
