772bbf2dabbe77321adde7cb76df42569bf4405b
Yan-Hsuan Chuang <yhchuang@realtek.com>
2020-03-12 16:08:51 +0800

    rtw88: pci: define a mask for TX/RX BD indexes
    Add a macro TRX_BD_IDX_MASK for access the TX/RX BD indexes.
    
    The hardware has only 12 bits for TX/RX BD indexes, we should not
    initialize a TX/RX ring or access the TX/RX BD index with a length
    that is larger than TRX_BD_IDX_MASK.
    
    Signed-off-by: Yan-Hsuan Chuang <yhchuang@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200312080852.16684-5-yhchuang@realtek.com

--- drivers/net/wireless/realtek/rtw88/pci.h
+++ drivers/net/wireless/realtek/rtw88/pci.h
@@ -53,6 +53,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RTK_PCI_TXBD_DESA_HI0Q	0x340
 #define RTK_PCI_RXBD_DESA_MPDUQ	0x338
 
+#define TRX_BD_IDX_MASK		GENMASK(11, 0)
+
 /* BCNQ is specialized for rsvd page, does not need to specify a number */
 #define RTK_PCI_TXBD_NUM_H2CQ	0x1328
 #define RTK_PCI_TXBD_NUM_MGMTQ	0x380
