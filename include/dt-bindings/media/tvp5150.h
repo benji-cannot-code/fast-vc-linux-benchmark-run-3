9f95ca996727dbcf823e123191e613199a02aea5
Javier Martinez Canillas <javierm@redhat.com>
2020-03-12 11:31:44 +0100

    media: partial revert of "[media] tvp5150: add HW input connectors support"
    Commit f7b4b54e6364 ("[media] tvp5150: add HW input connectors support")
    added input signals support for the tvp5150, but the approach was found
    to be incorrect so the corresponding DT binding commit 82c2ffeb217a
    ("[media] tvp5150: document input connectors DT bindings") was reverted.
    
    This left the driver with an undocumented (and wrong) DT parsing logic,
    so lets get rid of this code as well until the input connectors support
    is implemented properly.
    
    It's a partial revert due other patches added on top of mentioned commit
    not allowing the commit to be reverted cleanly anymore. But all the code
    related to the DT parsing logic and input entities creation are removed.
    
    Suggested-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Javier Martinez Canillas <javierm@redhat.com>
    Acked-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    [m.felsch@pengutronix.de: rm TVP5150_INPUT_NUM define]
    Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    Acked-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- include/dt-bindings/media/tvp5150.h
+++ include/dt-bindings/media/tvp5150.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TVP5150_COMPOSITE1 1
 #define TVP5150_SVIDEO     2
 
-#define TVP5150_INPUT_NUM  3
-
 /* TVP5150 HW outputs */
 #define TVP5150_NORMAL       0
 #define TVP5150_BLACK_SCREEN 1
