7046501d83b342aa0d1736a73d204f18f44604db
Brian Gerst <brgerst@gmail.com>
2020-03-13 15:51:44 -0400

    x86: Remove unneeded includes
    Clean up includes of and in <asm/syscalls.h>
    
    Signed-off-by: Brian Gerst <brgerst@gmail.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/20200313195144.164260-19-brgerst@gmail.com

--- arch/x86/kernel/signal.c
+++ arch/x86/kernel/signal.c
@@ -43,8 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif /* CONFIG_X86_64 */
 
 #include <asm/syscall.h>
-#include <asm/syscalls.h>
-
 #include <asm/sigframe.h>
 #include <asm/signal.h>
 
