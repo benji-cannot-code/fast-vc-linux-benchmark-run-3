252989688984fc0281a410e5daf4d1ba5902d8f8
Chris Wilson <chris@chris-wilson.co.uk>
2020-03-16 11:38:46 +0000

    drm/i915/gt: Allocate i915_fence_reg array
    Since the number of fence regs can vary dramactically between platforms,
    allocate the array on demand so we don't waste as much space.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200316113846.4974-4-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/i915_vma.h
+++ drivers/gpu/drm/i915/i915_vma.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <drm/drm_mm.h>
 
+#include "gt/intel_ggtt_fencing.h"
 #include "gem/i915_gem_object.h"
 
 #include "i915_gem_gtt.h"
