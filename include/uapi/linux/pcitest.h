464fb54224bad534179ca0027c0dfd3e078019c8
Kishon Vijay Abraham I <kishon@ti.com>
2020-03-16 16:54:23 +0530

    tools: PCI: Add 'd' command line option to support DMA
    Add a new command line option 'd' to use DMA for data transfers.
    It should be used with read, write or copy commands.
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Tested-by: Alan Mikhak <alan.mikhak@sifive.com>

--- include/uapi/linux/pcitest.h
+++ include/uapi/linux/pcitest.h
@@ -21,4 +21,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PCITEST_SET_IRQTYPE	_IOW('P', 0x8, int)
 #define PCITEST_GET_IRQTYPE	_IO('P', 0x9)
 
+#define PCITEST_FLAGS_USE_DMA	0x00000001
+
+struct pci_endpoint_test_xfer_param {
+	unsigned long size;
+	unsigned char flags;
+};
+
 #endif /* __UAPI_LINUX_PCITEST_H */
