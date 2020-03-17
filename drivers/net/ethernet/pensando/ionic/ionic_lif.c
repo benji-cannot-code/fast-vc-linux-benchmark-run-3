639dbd09e679df3d8c8d9dcfccb859e479855d8d
Shannon Nelson <snelson@pensando.io>
2020-03-16 20:22:08 -0700

    ionic: remove adminq napi instance
    Remove the adminq's napi struct when tearing down
    the adminq.
    
    Fixes: 1d062b7b6f64 ("ionic: Add basic adminq support")
    Signed-off-by: Shannon Nelson <snelson@pensando.io>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/pensando/ionic/ionic_lif.c
+++ drivers/net/ethernet/pensando/ionic/ionic_lif.c
@@ -2071,6 +2071,7 @@ static void ionic_lif_deinit(struct ionic_lif *lif)
 		ionic_lif_rss_deinit(lif);
 
 	napi_disable(&lif->adminqcq->napi);
+	netif_napi_del(&lif->adminqcq->napi);
 	ionic_lif_qcq_deinit(lif, lif->notifyqcq);
 	ionic_lif_qcq_deinit(lif, lif->adminqcq);
 
