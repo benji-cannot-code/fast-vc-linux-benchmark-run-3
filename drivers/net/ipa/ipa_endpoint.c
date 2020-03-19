0ea32779ef4b16a6d18776e7e8b0aa3b7d706841
YueHaibing <yuehaibing@huawei.com>
2020-03-19 12:12:00 +0000

    net: ipa: Remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Reviewed-by: Alex Elder <elder@linaro.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ipa/ipa_endpoint.c
+++ drivers/net/ipa/ipa_endpoint.c
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/bitfield.h>
 #include <linux/if_rmnet.h>
-#include <linux/version.h>
 #include <linux/dma-direction.h>
 
 #include "gsi.h"
