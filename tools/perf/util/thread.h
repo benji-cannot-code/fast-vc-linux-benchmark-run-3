fea6de18bebab1fe0b13fd6927f5f168147a1da0
Kan Liang <kan.liang@linux.intel.com>
2020-03-19 13:25:10 -0700

    perf thread: Save previous sample for LBR stitching approach
    To retrieve the overwritten LBRs from previous sample for LBR stitching
    approach, perf has to save the previous sample.
    
    Only allocate the struct lbr_stitch once, when LBR stitching approach is
    enabled and kernel supports hw_idx.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Acked-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Alexey Budankov <alexey.budankov@linux.intel.com>
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Pavel Gerasimov <pavel.gerasimov@intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ravi Bangoria <ravi.bangoria@linux.ibm.com>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Vitaly Slobodskoy <vitaly.slobodskoy@intel.com>
    Link: http://lore.kernel.org/lkml/20200319202517.23423-11-kan.liang@linux.intel.com
    [ Use zalloc()/zfree() for thread->lbr_stitch ]
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/thread.h
+++ tools/perf/util/thread.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/refcount.h>
 #include <linux/rbtree.h>
 #include <linux/list.h>
+#include <linux/zalloc.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -14,12 +15,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <strlist.h>
 #include <intlist.h>
 #include "rwsem.h"
+#include "event.h"
 
 struct addr_location;
 struct map;
 struct perf_record_namespaces;
 struct thread_stack;
 struct unwind_libunwind_ops;
+
+struct lbr_stitch {
+	struct perf_sample		prev_sample;
+};
 
 struct thread {
 	union {
@@ -50,6 +56,7 @@ struct thread {
 
 	/* LBR call stack stitch */
 	bool			lbr_stitch_enable;
+	struct lbr_stitch	*lbr_stitch;
 };
 
 struct machine;
@@ -144,6 +151,11 @@ static inline bool thread__is_filtered(struct thread *thread)
 	}
 
 	return false;
+}
+
+static inline void thread__free_stitch_list(struct thread *thread)
+{
+	zfree(&thread->lbr_stitch);
 }
 
 #endif	/* __PERF_THREAD_H */
