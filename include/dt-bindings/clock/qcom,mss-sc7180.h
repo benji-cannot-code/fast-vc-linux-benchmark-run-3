0404cb50a08afc8b4a4418d1bba61e5f2459ced2
Taniya Das <tdas@codeaurora.org>
2020-03-19 11:05:29 +0530

    dt-bindings: clock: Add YAML schemas for the QCOM MSS clock bindings
    The Modem Subsystem clock provider have a bunch of generic properties
    that are needed in a device tree. Add a YAML schemas for those.
    
    Add clock ids for GCC MSS and MSS clocks which are required to bring
    the modem out of reset.
    
    Signed-off-by: Taniya Das <tdas@codeaurora.org>
    Link: https://lkml.kernel.org/r/1584596131-22741-2-git-send-email-tdas@codeaurora.org
    Reviewed-by: Rob Herring <robh@kernel.org>
    Tested-by: Sibi Sankar <sibis@codeaurora.org>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

---
+++ include/dt-bindings/clock/qcom,mss-sc7180.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_MSS_SC7180_H
+#define _DT_BINDINGS_CLK_QCOM_MSS_SC7180_H
+
+#define MSS_AXI_CRYPTO_CLK	0
+#define MSS_AXI_NAV_CLK		1
+
+#endif
