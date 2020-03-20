ad1c22293233d2144fcd0ed1c1810796186c9369
Vincenzo Frascino <vincenzo.frascino@arm.com>
2020-03-20 14:53:30 +0000

    arm: Introduce asm/vdso/clocksource.h
    The vDSO library should only include the necessary headers required for
    a userspace library (UAPI and a minimal set of kernel headers). To make
    this possible it is necessary to isolate from the kernel headers the
    common parts that are strictly necessary to build the library.
    
    Introduce asm/vdso/clocksource.h to contain all the arm64 specific
    functions that are suitable for vDSO inclusion.
    
    This header will be required by a future patch that will generalize
    vdso/clocksource.h.
    
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Russell King <linux@armlinux.org.uk>
    Link: https://lkml.kernel.org/r/20200320145351.32292-6-vincenzo.frascino@arm.com

--- arch/arm/include/asm/clocksource.h
+++ arch/arm/include/asm/clocksource.h
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
 #ifndef _ASM_CLOCKSOURCE_H
 #define _ASM_CLOCKSOURCE_H
 
-#define VDSO_ARCH_CLOCKMODES	\
-	VDSO_CLOCKMODE_ARCHTIMER
+#include <asm/vdso/clocksource.h>
 
-#endif
+#endif /* _ASM_CLOCKSOURCE_H */
