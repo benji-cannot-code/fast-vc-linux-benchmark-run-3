75d831eecfc40f6870bb6d5b9120fdbfa8eb2134
Vincenzo Frascino <vincenzo.frascino@arm.com>
2020-03-20 14:53:46 +0000

    arm64: vdso32: Include common headers in the vdso library
    The vDSO library should only include the necessary headers required for
    a userspace library (UAPI and a minimal set of kernel headers). To make
    this possible it is necessary to isolate from the kernel headers the
    common parts that are strictly necessary to build the library.
    
    Refactor the vdso32 implementation to include common headers.
    
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Link: https://lkml.kernel.org/r/20200320145351.32292-22-vincenzo.frascino@arm.com

--- arch/arm64/include/asm/vdso/compat_gettimeofday.h
+++ arch/arm64/include/asm/vdso/compat_gettimeofday.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASSEMBLY__
 
 #include <asm/unistd.h>
-#include <uapi/linux/time.h>
+#include <asm/errno.h>
 
 #include <asm/vdso/compat_barrier.h>
 
