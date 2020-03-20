bb9d1fad834ab7e439ad3f384695a1aefa5067d6
Vincenzo Frascino <vincenzo.frascino@arm.com>
2020-03-20 14:53:47 +0000

    mips: vdso: Enable mips to use common headers
    Enable mips to use only the common headers in the implementation of
    the vDSO library.
    
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Paul Burton <paulburton@kernel.org>
    Link: https://lkml.kernel.org/r/20200320145351.32292-23-vincenzo.frascino@arm.com

--- arch/mips/include/asm/vdso/gettimeofday.h
+++ arch/mips/include/asm/vdso/gettimeofday.h
@@ -14,12 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 
-#include <linux/compiler.h>
-#include <linux/time.h>
-
 #include <asm/vdso/vdso.h>
 #include <asm/clocksource.h>
-#include <asm/io.h>
 #include <asm/unistd.h>
 #include <asm/vdso.h>
 
