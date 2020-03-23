0b23b967092aefe4b3f63078f74288a4ef82d983
John Clements <john.clements@amd.com>
2020-03-23 17:22:01 +0800

    drm/amdgpu: disabled fru eeprom access
    added asic support checking function to be filled in by supported asic types
    
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: John Clements <john.clements@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_fru_eeprom.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_fru_eeprom.c
@@ -30,6 +30,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define I2C_PRODUCT_INFO_ADDR_SIZE	0x2
 #define I2C_PRODUCT_INFO_OFFSET		0xC0
 
+bool is_fru_eeprom_supported(struct amdgpu_device *adev)
+{
+	/* TODO: Resolve supported ASIC type */
+
+	return false;
+}
+
 int amdgpu_fru_read_eeprom(struct amdgpu_device *adev, uint32_t addrptr,
 			   unsigned char *buff)
 {
@@ -71,6 +78,9 @@ int amdgpu_fru_get_product_info(struct amdgpu_device *adev)
 {
 	unsigned char buff[32];
 	int addrptr = 0, size = 0;
+
+	if (!is_fru_eeprom_supported(adev))
+		return 0;
 
 	/* If algo exists, it means that the i2c_adapter's initialized */
 	if (!adev->pm.smu_i2c.algo) {
