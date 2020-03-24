497857f8c90556414f669a2c2de1f6dfc6382ec2
Petr Vorel <pvorel@suse.cz>
2020-03-24 21:08:49 +0100

    NFS: Don't specify NFS version in "UDP not supported" error
    UDP was originally disabled in 6da1a034362f for NFSv4. Later in
    b24ee6c64ca7 UDP is by default disabled by NFS_DISABLE_UDP_SUPPORT=y for
    all NFS versions. Therefore remove v4 from error message.
    
    Fixes: b24ee6c64ca7 ("NFS: allow deprecation of NFS UDP protocol")
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/fs_context.c
+++ fs/nfs/fs_context.c
@@ -1136,7 +1136,7 @@ out_no_address:
 	return nfs_invalf(fc, "NFS4: mount program didn't pass remote address");
 
 out_invalid_transport_udp:
-	return nfs_invalf(fc, "NFSv4: Unsupported transport protocol udp");
+	return nfs_invalf(fc, "NFS: Unsupported transport protocol udp");
 }
 #endif
 
@@ -1258,7 +1258,7 @@ out_v4_not_compiled:
 	nfs_errorf(fc, "NFS: NFSv4 is not compiled into kernel");
 	return -EPROTONOSUPPORT;
 out_invalid_transport_udp:
-	return nfs_invalf(fc, "NFSv4: Unsupported transport protocol udp");
+	return nfs_invalf(fc, "NFS: Unsupported transport protocol udp");
 out_no_address:
 	return nfs_invalf(fc, "NFS: mount program didn't pass remote address");
 out_mountproto_mismatch:
