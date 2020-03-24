98c01f29f4aad6c82ebe53397dc9f208d4ab9845
Alexandru Ardelean <alexandru.ardelean@analog.com>
2020-03-24 15:46:29 +0200

    include: fpga: adi-axi-common.h: fixup whitespace tab -> space
    The initial version use a tab between '#define' & 'ADI_AXI_REG_VERSION'.
    This changes it to space. The change is purely cosmetic.
    
    Acked-by: Moritz Fischer <mdf@kernel.org>
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- include/linux/fpga/adi-axi-common.h
+++ include/linux/fpga/adi-axi-common.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef ADI_AXI_COMMON_H_
 #define ADI_AXI_COMMON_H_
 
-#define	ADI_AXI_REG_VERSION			0x0000
+#define ADI_AXI_REG_VERSION			0x0000
 
 #define ADI_AXI_PCORE_VER(major, minor, patch)	\
 	(((major) << 16) | ((minor) << 8) | (patch))
