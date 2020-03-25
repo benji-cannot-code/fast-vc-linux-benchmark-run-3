d8c0f2937bad625450ec88d487a4332ad18b87af
Soumyajit Deb <debsoumyajit100@gmail.com>
2020-03-25 09:24:10 +0530

    staging: hp100: Remove extra blank lines
    Remove extra blank lines from the code blocks.
    Reported by checkpatch.pl
    
    Signed-off-by: Soumyajit Deb <debsoumyajit100@gmail.com>
    Link: https://lore.kernel.org/r/20200325035410.23802-1-debsoumyajit100@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/hp/hp100.c
+++ drivers/staging/hp/hp100.c
@@ -221,7 +221,6 @@ module_param(hp100_mode, int, 0);
 static int hp100_probe1(struct net_device *dev, int ioaddr, u_char bus,
 			struct pci_dev *pci_dev);
 
-
 static int hp100_open(struct net_device *dev);
 static int hp100_close(struct net_device *dev);
 static netdev_tx_t hp100_start_xmit(struct sk_buff *skb,
@@ -347,7 +346,6 @@ static __init int hp100_isa_probe1(struct net_device *dev, int ioaddr)
 	return hp100_probe1(dev, ioaddr, HP100_BUS_ISA, NULL);
  err:
 	return -ENODEV;
-
 }
 /*
  * Probe for ISA board.
@@ -872,9 +870,7 @@ static void hp100_hwinit(struct net_device *dev)
 	/* Finally try to log in the Hub if there may be a VG connection. */
 	if ((lp->lan_type == HP100_LAN_100) || (lp->lan_type == HP100_LAN_ERR))
 		hp100_login_to_vg_hub(dev, 0);	/* relogin */
-
 }
-
 
 /*
  * mmuinit - Reinitialise Cascade MMU and MAC settings.
@@ -1128,7 +1124,6 @@ static int hp100_close(struct net_device *dev)
 	return 0;
 }
 
-
 /*
  * Configure the PDL Rx rings and LAN
  */
@@ -1179,7 +1174,6 @@ static void hp100_init_pdls(struct net_device *dev)
 	}
 }
 
-
 /* These functions "format" the entries in the pdl structure   */
 /* They return how much memory the fragments need.            */
 static int hp100_init_rxpdl(struct net_device *dev,
@@ -1209,7 +1203,6 @@ static int hp100_init_rxpdl(struct net_device *dev,
 
 	return roundup(MAX_RX_FRAG * 2 + 2, 4);
 }
-
 
 static int hp100_init_txpdl(struct net_device *dev,
 			    register hp100_ring_t * ringptr,
@@ -1569,7 +1562,6 @@ drop:
 	return NETDEV_TX_OK;
 }
 
-
 /* clean_txring checks if packets have been sent by the card by reading
  * the TX_PDL register from the performance page and comparing it to the
  * number of committed packets. It then frees the skb's of the packets that
@@ -1733,9 +1725,7 @@ static netdev_tx_t hp100_start_xmit(struct sk_buff *skb,
 drop:
 	dev_kfree_skb(skb);
 	return NETDEV_TX_OK;
-
 }
-
 
 /*
  * Receive Function (Non-Busmaster mode)
@@ -1947,7 +1937,6 @@ static void hp100_rx_bm(struct net_device *dev)
 			hp100_outl((u32) lp->rxrtail->pdl_paddr, RX_PDA);
 			lp->rxrtail = lp->rxrtail->next;
 		}
-
 	}
 }
 
@@ -2030,7 +2019,6 @@ static void hp100_clear_stats(struct hp100_private *lp, int ioaddr)
 	hp100_page(PERFORMANCE);
 	spin_unlock_irqrestore(&lp->lock, flags);
 }
-
 
 /*
  *  multicast setup
@@ -2798,7 +2786,6 @@ void hp100_RegisterDump(struct net_device *dev)
 }
 #endif
 
-
 static void cleanup_dev(struct net_device *d)
 {
 	struct hp100_private *p = netdev_priv(d);
@@ -2919,7 +2906,6 @@ static void hp100_pci_remove(struct pci_dev *pdev)
 	pci_disable_device(pdev);
 }
 
-
 static struct pci_driver hp100_pci_driver = {
 	.name		= "hp100",
 	.id_table	= hp100_pci_tbl,
@@ -3016,7 +3002,6 @@ static int __init hp100_module_init(void)
 	hp100_isa_cleanup();
 	goto out;
 }
-
 
 static void __exit hp100_module_exit(void)
 {
