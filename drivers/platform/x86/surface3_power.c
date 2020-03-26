7ef39c29695c968d6eefc3d4c8117d93051c6e1f
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-03-26 14:28:25 +0200

    platform/x86: surface3_power: Add missed headers
    We obviously are users of bits.h and types.h. Add them to the list.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/platform/x86/surface3_power.c
+++ drivers/platform/x86/surface3_power.c
@@ -33,11 +33,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/acpi.h>
+#include <linux/bits.h>
 #include <linux/freezer.h>
 #include <linux/i2c.h>
 #include <linux/kernel.h>
 #include <linux/kthread.h>
 #include <linux/slab.h>
+#include <linux/types.h>
 #include <linux/uuid.h>
 #include <asm/unaligned.h>
 
