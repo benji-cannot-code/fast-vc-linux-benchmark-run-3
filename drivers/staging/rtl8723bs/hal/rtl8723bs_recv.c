f623fb2df8ae94db33fd65cfe36456423d356ca7
Simran Singhal <singhalsimran0@gmail.com>
2020-03-26 18:58:23 +0530

    staging: rtl8723bs: hal: Remove NULL check before kfree
    NULL check before kfree is unnecessary so remove it.
    
    The following Coccinelle script was used to detect this:
    @@ expression E; @@
    - if (E != NULL) { kfree(E); }
    + kfree(E);
    @@ expression E; @@
    - if (E != NULL) { kfree(E); E = NULL; }
    + kfree(E);
    + E = NULL;
    
    Signed-off-by: Simran Singhal <singhalsimran0@gmail.com>
    Link: https://lore.kernel.org/r/20200326132823.GA18625@simran-Inspiron-5558
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/rtl8723bs_recv.c
+++ drivers/staging/rtl8723bs/hal/rtl8723bs_recv.c
@@ -481,10 +481,8 @@ initbuferror:
 		precvpriv->precv_buf = NULL;
 	}
 
-	if (precvpriv->pallocated_recv_buf) {
-		kfree(precvpriv->pallocated_recv_buf);
-		precvpriv->pallocated_recv_buf = NULL;
-	}
+	kfree(precvpriv->pallocated_recv_buf);
+	precvpriv->pallocated_recv_buf = NULL;
 
 exit:
 	return res;
@@ -519,8 +517,6 @@ void rtl8723bs_free_recv_priv(struct adapter *padapter)
 		precvpriv->precv_buf = NULL;
 	}
 
-	if (precvpriv->pallocated_recv_buf) {
-		kfree(precvpriv->pallocated_recv_buf);
-		precvpriv->pallocated_recv_buf = NULL;
-	}
+	kfree(precvpriv->pallocated_recv_buf);
+	precvpriv->pallocated_recv_buf = NULL;
 }
