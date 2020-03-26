0ab0269038f90efb5fbd321ca78529b92acec7d8
Thomas Gleixner <tglx@linutronix.de>
2020-03-26 16:28:52 +0100

    x86/entry/common: Provide idtentry_enter/exit()
    Provide functions which handle the low level entry and exit similar to
    enter/exit from user mode.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Alexandre Chartre <alexandre.chartre@oracle.com>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Acked-by: Andy Lutomirski <luto@kernel.org>
    Link: https://lkml.kernel.org/r/20200505134904.457578656@linutronix.de

--- arch/x86/include/asm/idtentry.h
+++ arch/x86/include/asm/idtentry.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 
+void idtentry_enter(struct pt_regs *regs);
+void idtentry_exit(struct pt_regs *regs);
+
 /**
  * DECLARE_IDTENTRY - Declare functions for simple IDT entry points
  *		      No error code pushed by hardware
