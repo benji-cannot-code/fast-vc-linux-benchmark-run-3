806ae31ff1452d1e7161116f426800965de1d946
Maharaja Kennadyrajan <mkenna@codeaurora.org>
2020-03-26 20:19:15 +0530

    ath11k: add pktlog checksum in trace events to support pktlog
    Pktlog data are different among the chipset & chipset versions.
    As part of enhancing the user space script to decode the pktlog
    trace events generated, it is desirable to know which chipset or
    which chipset version has provided the events and thereby decode
    the pktlogs appropriately.
    
    Pktlog checksum helps to determine the chipset variant which is
    given by the firmware in the struct wmi_ready_event.
    
    Pktlog checksums are computed during the firmware build.
    So, adding that pktlog checksum in the pklog trace events.
    
    Signed-off-by: Maharaja Kennadyrajan <mkenna@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/1585234155-30574-1-git-send-email-mkenna@codeaurora.org

--- drivers/net/wireless/ath/ath11k/dp_rx.c
+++ drivers/net/wireless/ath/ath11k/dp_rx.c
@@ -1492,7 +1492,8 @@ static void ath11k_htt_pktlog(struct ath11k_base *ab, struct sk_buff *skb)
 		return;
 	}
 
-	trace_ath11k_htt_pktlog(ar, data->payload, hdr->size);
+	trace_ath11k_htt_pktlog(ar, data->payload, hdr->size,
+				ar->ab->pktlog_defs_checksum);
 }
 
 static void ath11k_htt_backpressure_event_handler(struct ath11k_base *ab,
