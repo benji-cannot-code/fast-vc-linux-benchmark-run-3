b31f56c1d5796a65cecabcb7603efe18b446891f
David Disseldorp <ddiss@suse.de>
2020-03-27 15:19:54 +0100

    scsi: target: use the stack for XCOPY passthrough cmds
    Reads and writes in the XCOPY loop are synchronous, so needn't be heap
    allocated / freed with each loop.
    
    Link: https://lore.kernel.org/r/20200327141954.955-6-ddiss@suse.de
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: David Disseldorp <ddiss@suse.de>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/target/target_core_xcopy.h
+++ drivers/target/target_core_xcopy.h
@@ -19,8 +19,6 @@ enum xcopy_origin_list {
 	XCOL_DEST_RECV_OP = 0x02,
 };
 
-struct xcopy_pt_cmd;
-
 struct xcopy_op {
 	int op_origin;
 
@@ -36,9 +34,6 @@ struct xcopy_op {
 	unsigned short stdi;
 	unsigned short dtdi;
 	unsigned short nolb;
-
-	struct xcopy_pt_cmd *src_pt_cmd;
-	struct xcopy_pt_cmd *dst_pt_cmd;
 
 	u32 xop_data_bytes;
 	u32 xop_data_nents;
