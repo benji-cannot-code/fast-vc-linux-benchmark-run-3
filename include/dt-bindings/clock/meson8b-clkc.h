30569a0774d3b04ae4e7c56a419f99fdcc1ce577
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2020-03-31 01:45:34 +0200

    clk: meson8b: export the HDMI system clock
    Export the HDMI system clock (used by the HDMI transmitter) so it can be
    used in the dt-bindings.
    
    Signed-off-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>
    Acked-by: Rob Herring <robh@kernel.org>
    Link: https://lore.kernel.org/r/20200330234535.3327513-2-martin.blumenstingl@googlemail.com

--- include/dt-bindings/clock/meson8b-clkc.h
+++ include/dt-bindings/clock/meson8b-clkc.h
@@ -108,6 +108,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CLKID_PERIPH		126
 #define CLKID_AXI		128
 #define CLKID_L2_DRAM		130
+#define CLKID_HDMI_SYS		174
 #define CLKID_VPU		190
 #define CLKID_VDEC_1		196
 #define CLKID_VDEC_HCODEC	199
