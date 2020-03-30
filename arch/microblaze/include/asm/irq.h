6d8a1ece0c1d81c5c53cbd7b70974b859b73f5af
Marc Zyngier <maz@kernel.org>
2020-03-30 10:43:59 +0100

    Revert "irqchip/xilinx: Enable generic irq multi handler"
    This reverts commit a0789993bf8266e62fea6b4613945ba081c71e7d, which
    breaks a number of PPC platforms.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/44b64be7-9240-fd52-af90-e0245220f38b@xilinx.com

--- arch/microblaze/include/asm/irq.h
+++ arch/microblaze/include/asm/irq.h
@@ -15,4 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct pt_regs;
 extern void do_IRQ(struct pt_regs *regs);
 
+/* should be defined in each interrupt controller driver */
+extern unsigned int xintc_get_irq(void);
+
 #endif /* _ASM_MICROBLAZE_IRQ_H */
