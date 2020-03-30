b4cbe2f96b3b890c0b6942a15f831e63e0f63c35
Andrii Nakryiko <andriin@fb.com>
2020-03-29 20:00:01 -0700

    selftests/bpf: Test FD-based cgroup attachment
    Add selftests to exercise FD-based cgroup BPF program attachments and their
    intermixing with legacy cgroup BPF attachments. Auto-detachment and program
    replacement (both unconditional and cmpxchng-like) are tested as well.
    
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200330030001.2312810-5-andriin@fb.com

---
+++ tools/testing/selftests/bpf/progs/test_cgroup_link.c
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (c) 2020 Facebook
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+int calls = 0;
+int alt_calls = 0;
+
+SEC("cgroup_skb/egress1")
+int egress(struct __sk_buff *skb)
+{
+	__sync_fetch_and_add(&calls, 1);
+	return 1;
+}
+
+SEC("cgroup_skb/egress2")
+int egress_alt(struct __sk_buff *skb)
+{
+	__sync_fetch_and_add(&alt_calls, 1);
+	return 1;
+}
+
+char _license[] SEC("license") = "GPL";
+
