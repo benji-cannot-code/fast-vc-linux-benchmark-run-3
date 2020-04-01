41aeb2a3cef4d269b595540d7f549e918ac4190d
Jérôme Pouiller <jerome.pouiller@silabs.com>
2020-04-01 13:04:02 +0200

    staging: wfx: wfx_flush() did not ensure that frames are processed
    wfx_flush() exited once all frames are retrieved from the device.
    However, it did not ensure they were processed by driver before to
    return. Therefore, some frame may be processed after the interface has
    disappear.
    
    Change the place we signal that the queue is empty to fix that.
    
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>
    Link: https://lore.kernel.org/r/20200401110405.80282-30-Jerome.Pouiller@silabs.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wfx/bh.c
+++ drivers/staging/wfx/bh.c
@@ -109,8 +109,6 @@ static int rx_helper(struct wfx_dev *wdev, size_t read_len, int *is_cnf)
 			release_count = 1;
 		WARN(wdev->hif.tx_buffers_used < release_count, "corrupted buffer counter");
 		wdev->hif.tx_buffers_used -= release_count;
-		if (!wdev->hif.tx_buffers_used)
-			wake_up(&wdev->hif.tx_buffers_empty);
 	}
 	_trace_hif_recv(hif, wdev->hif.tx_buffers_used);
 
@@ -124,6 +122,8 @@ static int rx_helper(struct wfx_dev *wdev, size_t read_len, int *is_cnf)
 	skb_put(skb, hif->len);
 	// wfx_handle_rx takes care on SKB livetime
 	wfx_handle_rx(wdev, skb);
+	if (!wdev->hif.tx_buffers_used)
+		wake_up(&wdev->hif.tx_buffers_empty);
 
 	return piggyback;
 
