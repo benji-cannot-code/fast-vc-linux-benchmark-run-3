eabcedbb84e686f7d7e8398267c99a1d7955c7a4
Wanpeng Li <wanpengli@tencent.com>
2020-04-02 16:20:26 +0800

    KVM: X86: Filter out the broadcast dest for IPI fastpath
    Except destination shorthand, a destination value 0xffffffff is used to
    broadcast interrupts, let's also filter out this for single target IPI
    fastpath.
    
    Reviewed-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Signed-off-by: Wanpeng Li <wanpengli@tencent.com>
    Message-Id: <1585815626-28370-1-git-send-email-wanpengli@tencent.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/lapic.c
+++ arch/x86/kvm/lapic.c
@@ -60,9 +60,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAX_APIC_VECTOR			256
 #define APIC_VECTORS_PER_REG		32
 
-#define APIC_BROADCAST			0xFF
-#define X2APIC_BROADCAST		0xFFFFFFFFul
-
 static bool lapic_timer_advance_dynamic __read_mostly;
 #define LAPIC_TIMER_ADVANCE_ADJUST_MIN	100	/* clock cycles */
 #define LAPIC_TIMER_ADVANCE_ADJUST_MAX	10000	/* clock cycles */
