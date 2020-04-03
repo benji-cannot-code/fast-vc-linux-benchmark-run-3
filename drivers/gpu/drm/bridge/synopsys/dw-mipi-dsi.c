e117fb009459508599e7de7a14d6242bcf9b0cf8
Angelo Ribeiro <Angelo.Ribeiro@synopsys.com>
2020-04-03 15:30:36 +0200

    drm/bridge: dw-mipi-dsi.c: remove unused header file
    dw-mipi-dsi does not use any definition from drm_probe_helper.
    
    Coverity output:
    Event unnecessary_header:
    Including .../include/drm/drm_probe_helper.h does not provide any
    needed symbols.
    
    Reviewed-by: Yannick Fertre <yannick.fertre@st.com>
    Cc: Gustavo Pimentel <gustavo.pimentel@synopsys.com>
    Cc: Joao Pinto <jpinto@synopsys.com>
    Cc: Jose Abreu <jose.abreu@synopsys.com>
    Signed-off-by: Angelo Ribeiro <angelo.ribeiro@synopsys.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/171ff1fb3918664a570dc8f2f34b446612505f76.1585832665.git.angelo.ribeiro@synopsys.com

--- drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_modes.h>
 #include <drm/drm_of.h>
 #include <drm/drm_print.h>
-#include <drm/drm_probe_helper.h>
 
 #define HWVER_131			0x31333100	/* IP version 1.31 */
 
