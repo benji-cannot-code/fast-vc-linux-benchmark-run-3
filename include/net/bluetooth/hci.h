4ae90bfa7752bdc3650e9c91dd30147fa70e4eca
Marcel Holtmann <marcel@holtmann.org>
2020-04-03 21:44:07 +0200

    Bluetooth: Add HCI device identifier for VIRTIO devices
    This patch assigns the next free HCI device identifier to Bluetooth
    devices based on VIRTIO devices.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- include/net/bluetooth/hci.h
+++ include/net/bluetooth/hci.h
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HCI_SPI		7
 #define HCI_I2C		8
 #define HCI_SMD		9
+#define HCI_VIRTIO	10
 
 /* HCI controller types */
 #define HCI_PRIMARY	0x00
