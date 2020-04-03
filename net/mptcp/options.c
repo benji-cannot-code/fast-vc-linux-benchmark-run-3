04e3ff4d5e8745a5c6973db86b8ab6d8102446b5
Geliang Tang <geliangtang@gmail.com>
2020-04-03 17:14:08 +0800

    mptcp: add some missing pr_fmt defines
    Some of the mptcp logs didn't print out the format string:
    
    [  185.651493] DSS
    [  185.651494] data_fin=0 dsn64=0 use_map=0 ack64=1 use_ack=1
    [  185.651494] data_ack=13792750332298763796
    [  185.651495] MPTCP: msk=00000000c4b81cfc ssk=000000009743af53 data_avail=0 skb=0000000063dc595d
    [  185.651495] MPTCP: msk=00000000c4b81cfc ssk=000000009743af53 status=0
    [  185.651495] MPTCP: msk ack_seq=9bbc894565aa2f9a subflow ack_seq=9bbc894565aa2f9a
    [  185.651496] MPTCP: msk=00000000c4b81cfc ssk=000000009743af53 data_avail=1 skb=0000000012e809e1
    
    So this patch added these missing pr_fmt defines. Then we can get the same
    format string "MPTCP" in all mptcp logs like this:
    
    [  142.795829] MPTCP: DSS
    [  142.795829] MPTCP: data_fin=0 dsn64=0 use_map=0 ack64=1 use_ack=1
    [  142.795829] MPTCP: data_ack=8089704603109242421
    [  142.795830] MPTCP: msk=00000000133a24e0 ssk=000000002e508c64 data_avail=0 skb=00000000d5f230df
    [  142.795830] MPTCP: msk=00000000133a24e0 ssk=000000002e508c64 status=0
    [  142.795831] MPTCP: msk ack_seq=66790290f1199d9b subflow ack_seq=66790290f1199d9b
    [  142.795831] MPTCP: msk=00000000133a24e0 ssk=000000002e508c64 data_avail=1 skb=00000000de5aca2e
    
    Signed-off-by: Geliang Tang <geliangtang@gmail.com>
    Reviewed-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/mptcp/options.c
+++ net/mptcp/options.c
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (c) 2017 - 2019, Intel Corporation.
  */
 
+#define pr_fmt(fmt) "MPTCP: " fmt
+
 #include <linux/kernel.h>
 #include <net/tcp.h>
 #include <net/mptcp.h>
