af5e34a76fd22b416b8a9a5d3e15f051b7cd9a81
Marcel Holtmann <marcel@holtmann.org>
2020-04-03 21:44:06 +0200

    Bluetooth: Increment management interface revision
    Increment the mgmt revision due to the recently added new commands.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "mgmt_util.h"
 
 #define MGMT_VERSION	1
-#define MGMT_REVISION	16
+#define MGMT_REVISION	17
 
 static const u16 mgmt_commands[] = {
 	MGMT_OP_READ_INDEX_LIST,
