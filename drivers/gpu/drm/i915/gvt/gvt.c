8377c5cf301622a96ca65351b52e47cf489d72a7
Christoph Hellwig <hch@lst.de>
2020-04-04 11:40:58 +0200

    i915/gvt: remove unused xen bits
    No Xen support anywhere here.  Remove a dead declaration and an unused
    include.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20200404094101.672954-4-hch@lst.de

--- drivers/gpu/drm/i915/gvt/gvt.c
+++ drivers/gpu/drm/i915/gvt/gvt.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/types.h>
-#include <xen/xen.h>
 #include <linux/kthread.h>
 
 #include "i915_drv.h"
