d689344f8db7f3db44e988dc26696952acae8524
Jérôme Pouiller <jerome.pouiller@silabs.com>
2020-04-06 13:17:46 +0200

    staging: wfx: drop unused WFX_LINK_ID_GC_TIMEOUT
    The definition WFX_LINK_ID_GC_TIMEOUT is not used anymore since
    commit d6aeba575f27 ("staging: wfx: simplify the link-id allocation")
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>
    Link: https://lore.kernel.org/r/20200406111756.154086-2-Jerome.Pouiller@silabs.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/wfx/data_tx.c
+++ drivers/staging/wfx/data_tx.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hif_tx_mib.h"
 
 #define WFX_INVALID_RATE_ID    15
-#define WFX_LINK_ID_GC_TIMEOUT ((unsigned long)(10 * HZ))
 
 static int wfx_get_hw_rate(struct wfx_dev *wdev,
 			   const struct ieee80211_tx_rate *rate)
