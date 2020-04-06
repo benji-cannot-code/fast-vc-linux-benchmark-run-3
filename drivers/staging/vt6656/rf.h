919f9d5a232d6aeec4c0c05b127bb053ea7b7b71
Malcolm Priestley <tvboxspy@gmail.com>
2020-04-06 20:31:16 +0100

    staging: vt6556: vnt_rf_setpower convert to use ieee80211_channel.
    ieee80211_channel contains all the necessary information to change
    power according to tx mode required.
    
    vnt_rf_setpower is moved and so that vnt_rf_set_txpower the only
    caller becomes static.
    
    Signed-off-by: Malcolm Priestley <tvboxspy@gmail.com>
    Link: https://lore.kernel.org/r/9eab9af8-fde9-1dc6-fced-95c7a36ecc01@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/rf.h
+++ drivers/staging/vt6656/rf.h
@@ -42,8 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	VNT_RF_REG_LEN      0x17 /* 24 bit length */
 
 int vnt_rf_write_embedded(struct vnt_private *priv, u32 data);
-int vnt_rf_setpower(struct vnt_private *priv, u32 rate, u32 channel);
-int vnt_rf_set_txpower(struct vnt_private *priv, u8 power, u32 rate);
+int vnt_rf_setpower(struct vnt_private *priv, struct ieee80211_channel *ch);
 void vnt_rf_rssi_to_dbm(struct vnt_private *priv, u8 rssi, long *dbm);
 int vnt_rf_table_download(struct vnt_private *priv);
 
