ad8796e68b5ab97c281d40b9d33744a2431d7878
Chuck Lever <chuck.lever@oracle.com>
2020-04-07 15:32:14 -0400

    svcrdma: Add common XDR encoders for RDMA and Read segments
    Clean up: De-duplicate some code.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- net/sunrpc/xprtrdma/svc_rdma_sendto.c
+++ net/sunrpc/xprtrdma/svc_rdma_sendto.c
@@ -377,7 +377,6 @@ static ssize_t svc_rdma_encode_write_segment(__be32 *src,
 
 	xdr_decode_rdma_segment(src, &handle, &length, &offset);
 
-	*p++ = cpu_to_be32(handle);
 	if (*remaining < length) {
 		/* segment only partly filled */
 		length = *remaining;
@@ -386,8 +385,7 @@ static ssize_t svc_rdma_encode_write_segment(__be32 *src,
 		/* entire segment was consumed */
 		*remaining -= length;
 	}
-	*p++ = cpu_to_be32(length);
-	xdr_encode_hyper(p, offset);
+	xdr_encode_rdma_segment(p, handle, length, offset);
 
 	trace_svcrdma_encode_wseg(handle, length, offset);
 	return len;
