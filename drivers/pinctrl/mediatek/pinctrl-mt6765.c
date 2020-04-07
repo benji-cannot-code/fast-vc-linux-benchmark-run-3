767c27e6855f4ad7eb568e4fa1082d82957fa0c2
Light Hsieh <light.hsieh@mediatek.com>
2020-04-08 04:08:17 +0800

    pinctrl: mediatek: make MediaTek MT6765 pinctrl ready for buiding loadable module
    This patch make pinctrl-mt6765 ready for building as loadable module.
    
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Signed-off-by: Light Hsieh <light.hsieh@mediatek.com>
    Link: https://lore.kernel.org/r/42100afe6f97d28447c153bea6b54ea556cf95ca.1586289920.git.sean.wang@mediatek.com
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/mediatek/pinctrl-mt6765.c
+++ drivers/pinctrl/mediatek/pinctrl-mt6765.c
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+#include <linux/module.h>
 #include "pinctrl-mtk-mt6765.h"
 #include "pinctrl-paris.h"
 
@@ -1104,3 +1105,6 @@ static int __init mt6765_pinctrl_init(void)
 	return platform_driver_register(&mt6765_pinctrl_driver);
 }
 arch_initcall(mt6765_pinctrl_init);
+
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("MediaTek MT6765 Pinctrl Driver");
