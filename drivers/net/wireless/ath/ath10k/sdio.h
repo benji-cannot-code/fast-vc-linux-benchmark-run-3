a7a7abdbe0bd8a7176e845be4082566ebc1a6606
Wen Gong <wgong@codeaurora.org>
2020-04-07 08:12:34 +0300

    ath10k: change ATH10K_SDIO_BUS_REQUEST_MAX_NUM from 64 to 1024
    sdio bus bandwidth is low, sometimes for high performance TX test,
    it will lack of ath10k_sdio_bus_request, it will print message:
    ath10k_sdio mmc1:0001:1: unable to allocate bus request for async request
    
    change the num from 64 to 1024 will not happen it.
    
    Tested with QCA6174 SDIO with firmware
    WLAN.RMH.4.4.1-00017-QCARMSWP-1.
    
    Signed-off-by: Wen Gong <wgong@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200212080415.31265-3-wgong@codeaurora.org

--- drivers/net/wireless/ath/ath10k/sdio.h
+++ drivers/net/wireless/ath/ath10k/sdio.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	(ATH10K_SDIO_MAX_BUFFER_SIZE - sizeof(struct ath10k_htc_hdr))
 
 #define ATH10K_HIF_MBOX_NUM_MAX                 4
-#define ATH10K_SDIO_BUS_REQUEST_MAX_NUM         64
+#define ATH10K_SDIO_BUS_REQUEST_MAX_NUM         1024
 
 #define ATH10K_SDIO_HIF_COMMUNICATION_TIMEOUT_HZ (100 * HZ)
 
@@ -99,6 +99,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ATH10K_FIFO_TIMEOUT_AND_CHIP_CONTROL_DISABLE_SLEEP_OFF 0xFFFEFFFF
 #define ATH10K_FIFO_TIMEOUT_AND_CHIP_CONTROL_DISABLE_SLEEP_ON 0x10000
 
+/* TODO: remove this and use skb->cb instead, much cleaner approach */
 struct ath10k_sdio_bus_request {
 	struct list_head list;
 
