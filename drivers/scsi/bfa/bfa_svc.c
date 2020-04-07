b015b5e42378ac0ae752a889b94a7d021f43c6d0
Jason Yan <yanaijie@huawei.com>
2020-04-07 11:21:56 +0800

    scsi: bfa: bfa_svc.c: make two functions static
    Fix the following sparse warning:
    
    drivers/scsi/bfa/bfa_svc.c:4288:1: warning: symbol
    'bfa_fcport_ddportenable' was not declared. Should it be static?
    drivers/scsi/bfa/bfa_svc.c:4297:1: warning: symbol
    'bfa_fcport_ddportdisable' was not declared. Should it be static?
    
    Link: https://lore.kernel.org/r/20200407032202.36789-2-yanaijie@huawei.com
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Jason Yan <yanaijie@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/bfa/bfa_svc.c
+++ drivers/scsi/bfa/bfa_svc.c
@@ -4285,7 +4285,7 @@ bfa_fcport_dportdisable(struct bfa_s *bfa)
 	bfa_port_set_dportenabled(&bfa->modules.port, BFA_FALSE);
 }
 
-void
+static void
 bfa_fcport_ddportenable(struct bfa_s *bfa)
 {
 	/*
@@ -4294,7 +4294,7 @@ bfa_fcport_ddportenable(struct bfa_s *bfa)
 	bfa_sm_send_event(BFA_FCPORT_MOD(bfa), BFA_FCPORT_SM_DDPORTENABLE);
 }
 
-void
+static void
 bfa_fcport_ddportdisable(struct bfa_s *bfa)
 {
 	/*
