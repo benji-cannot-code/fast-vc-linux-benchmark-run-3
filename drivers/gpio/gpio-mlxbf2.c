98da7a090c4608bbef4b00996594034d9e7f2312
YueHaibing <yuehaibing@huawei.com>
2020-04-08 07:08:32 +0000

    gpio: mlxbf2: remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Link: https://lore.kernel.org/r/20200408070832.137037-1-yuehaibing@huawei.com
    Acked-by: Asmaa Mnebhi <Asmaa@mellanox.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-mlxbf2.c
+++ drivers/gpio/gpio-mlxbf2.c
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/resource.h>
 #include <linux/spinlock.h>
 #include <linux/types.h>
-#include <linux/version.h>
 
 /*
  * There are 3 YU GPIO blocks:
