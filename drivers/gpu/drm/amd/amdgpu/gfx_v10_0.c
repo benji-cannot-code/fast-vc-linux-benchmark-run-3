dbf842d295964a9ddc3b86035c5e46efb561b027
shaoyunl <shaoyun.liu@amd.com>
2020-04-09 15:53:17 -0400

    drm/amdgpu/sriov : Use kiq to do tlb invalidation for gfx10 on sriov
    On SRIOV run time, driver shouldn't directly access invalidation registers through MMIO.
    Use kiq to submit wait_reg_mem package for the invalidation
    
    Signed-off-by: shaoyunl <shaoyun.liu@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gfx_v10_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v10_0.c
@@ -3481,6 +3481,9 @@ static void gfx_v10_0_check_fw_write_wait(struct amdgpu_device *adev)
 		    (adev->gfx.mec_feature_version >= 27))
 			adev->gfx.cp_fw_write_wait = true;
 		break;
+	case CHIP_SIENNA_CICHLID:
+		adev->gfx.cp_fw_write_wait = true;
+		break;
 	default:
 		break;
 	}
