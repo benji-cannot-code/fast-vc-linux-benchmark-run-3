6113dbe2578eec5a388108c68f77e56549ab085e
Aurabindo Pillai <mail@aurabindo.in>
2020-04-08 21:28:13 -0400

    drm/amd/amdgpu: add prefix for pr_* prints
    amdgpu uses lots of pr_* calls for printing error messages.
    With this prefix, errors shall be more obvious to the end
    use regarding its origin, and may help debugging.
    
    Prefix format:
    
    [xxx.xxxxx] amdgpu: ...
    
    Signed-off-by: Aurabindo Pillai <mail@aurabindo.in>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -29,6 +29,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __AMDGPU_H__
 #define __AMDGPU_H__
 
+#ifdef pr_fmt
+#undef pr_fmt
+#endif
+
+#define pr_fmt(fmt) "amdgpu: " fmt
+
 #include "amdgpu_ctx.h"
 
 #include <linux/atomic.h>
