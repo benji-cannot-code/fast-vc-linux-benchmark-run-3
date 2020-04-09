a6d3cb1afe699fbae7b4490847c7e53f6bb7fa9a
Stephen Boyd <sboyd@kernel.org>
2020-04-08 23:44:14 -0700

    ARM: mmp: Remove legacy clk code
    Remove all the legacy clk code that supports a non-common clk framework
    implementation of 'struct clk' in mach-mmp. This code doesn't look to be
    compiled anymore given that the MMP is fully supported in the
    multi-platform config via ARCH_MULTIPLATFORM as of commit 377524dc4d77
    ("ARM: mmp: move into ARCH_MULTIPLATFORM"). The ARCH_MULTIPLATFORM
    config selects COMMON_CLK and therefore the Makefile rule can never
    actually compile the code in these files.
    
    Cc: Lubomir Rintel <lkundrak@v3.sk>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: <linux-arm-kernel@lists.infradead.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>
    Link: https://lkml.kernel.org/r/20200409064416.83340-9-sboyd@kernel.org
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    [sboyd@kernel.org: Squash in a clock.h include removal found by Stephen
    Rothwell <sfr@canb.auug.org.au>]

--- arch/arm/mach-mmp/pxa168.c
+++ arch/arm/mach-mmp/pxa168.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/system_misc.h>
 
 #include "addr-map.h"
-#include "clock.h"
 #include "common.h"
 #include <linux/soc/mmp/cputype.h>
 #include "devices.h"
