d8cf4e8c975c3c7a0d19832583b727c2f0e0e337
Borislav Petkov <bp@suse.de>
2020-04-11 18:09:27 +0200

    KVM: SVM: Fix build error due to missing release_pages() include
    Fix:
    
      arch/x86/kvm/svm/sev.c: In function ‘sev_pin_memory’:
      arch/x86/kvm/svm/sev.c:360:3: error: implicit declaration of function ‘release_pages’;\
    	  did you mean ‘reclaim_pages’? [-Werror=implicit-function-declaration]
        360 |   release_pages(pages, npinned);
            |   ^~~~~~~~~~~~~
            |   reclaim_pages
    
    because svm.c includes pagemap.h but the carved out sev.c needs it too.
    Triggered by a randconfig build.
    
    Fixes: eaf78265a4ab ("KVM: SVM: Move SEV code to separate file")
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Message-Id: <20200411160927.27954-1-bp@alien8.de>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/highmem.h>
 #include <linux/psp-sev.h>
+#include <linux/pagemap.h>
 #include <linux/swap.h>
 
 #include "x86.h"
