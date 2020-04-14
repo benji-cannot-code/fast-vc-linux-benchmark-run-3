dcedeccf97a4e47cf3c87fa3b887b3da351bbd43
Oscar Carter <oscar.carter@gmx.com>
2020-04-14 17:38:47 +0200

    staging: vt6656: Use define instead of magic number for tx_rate
    Use the define RATE_11M present in the file "device.h" instead of the
    magic number 3. So the code is more clear.
    
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Oscar Carter <oscar.carter@gmx.com>
    Link: https://lore.kernel.org/r/20200414153849.5785-4-oscar.carter@gmx.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/baseband.c
+++ drivers/staging/vt6656/baseband.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/bits.h>
 #include <linux/kernel.h>
+#include "device.h"
 #include "mac.h"
 #include "baseband.h"
 #include "rf.h"
@@ -222,7 +223,7 @@ unsigned int vnt_get_frame_time(u8 preamble_type, u8 pkt_type,
 
 	rate = (unsigned int)vnt_frame_time[tx_rate];
 
-	if (tx_rate <= 3) {
+	if (tx_rate <= RATE_11M) {
 		if (preamble_type == PREAMBLE_SHORT)
 			preamble = 96;
 		else
