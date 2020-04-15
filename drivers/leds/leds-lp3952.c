9a42ab5024571cb93970f071f44b1f991e7fcdd0
Linus Walleij <linus.walleij@linaro.org>
2020-04-15 14:35:08 +0200

    leds: lp3952: Include the right header
    This driver is using the GPIO descriptor API properly
    as it should be but is including the legacy GPIO header
    <linux/gpio.h>. Fix it by including <linux/gpio/consumer.h>
    instead.
    
    Cc: Tony Makkiel <tony.makkiel@daqri.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Pavel Machek <pavel@ucw.cz>

--- drivers/leds/leds-lp3952.c
+++ drivers/leds/leds-lp3952.c
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/delay.h>
-#include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/i2c.h>
 #include <linux/io.h>
 #include <linux/kernel.h>
