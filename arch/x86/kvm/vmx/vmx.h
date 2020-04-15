3ef79b0a58636b0b7e6775d36b1ecf6cd4602d0d
Sean Christopherson <sean.j.christopherson@intel.com>
2020-04-15 13:34:51 -0700

    KVM: nVMX: Reset register cache (available and dirty masks) on VMCS switch
    Reset the per-vCPU available and dirty register masks when switching
    between vmcs01 and vmcs02, as the masks track state relative to the
    current VMCS.  The stale masks don't cause problems in the current code
    base because the registers are either unconditionally written on nested
    transitions or, in the case of segment registers, have an additional
    tracker that is manually reset.
    
    Note, by dropping (previously implicitly, now explicitly) the dirty mask
    when switching the active VMCS, KVM is technically losing writes to the
    associated fields.  But, the only regs that can be dirtied (RIP, RSP and
    PDPTRs) are unconditionally written on nested transitions, e.g. explicit
    writeback is a waste of cycles, and a WARN_ON would be rather pointless.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Message-Id: <20200415203454.8296-3-sean.j.christopherson@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/vmx/vmx.h
+++ arch/x86/kvm/vmx/vmx.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/intel_pt.h>
 
 #include "capabilities.h"
+#include "kvm_cache_regs.h"
 #include "ops.h"
 #include "vmcs.h"
 
@@ -446,6 +447,16 @@ BUILD_CONTROLS_SHADOW(secondary_exec, SECONDARY_VM_EXEC_CONTROL)
 static inline void vmx_segment_cache_clear(struct vcpu_vmx *vmx)
 {
 	vmx->segment_cache.bitmask = 0;
+}
+
+static inline void vmx_register_cache_reset(struct kvm_vcpu *vcpu)
+{
+	vcpu->arch.regs_avail = ~((1 << VCPU_REGS_RIP) | (1 << VCPU_REGS_RSP)
+				  | (1 << VCPU_EXREG_RFLAGS)
+				  | (1 << VCPU_EXREG_PDPTR)
+				  | (1 << VCPU_EXREG_SEGMENTS)
+				  | (1 << VCPU_EXREG_CR3));
+	vcpu->arch.regs_dirty = 0;
 }
 
 static inline u32 vmx_vmentry_ctrl(void)
