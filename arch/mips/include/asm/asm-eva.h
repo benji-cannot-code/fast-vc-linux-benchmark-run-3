8eb0d6a41b013476df87e0c5f422be4c10e1d019
bibo mao <maobibo@loongson.cn>
2020-04-15 17:56:43 +0800

    MIPS: Fix typo for user_ld macro definition
    There is typo for macro user_ld if __ASSEMBLY__ is declared, this
    patch fixes this issue.
    
    Signed-off-by: bibo mao <maobibo@loongson.cn>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/include/asm/asm-eva.h
+++ arch/mips/include/asm/asm-eva.h
@@ -181,7 +181,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define user_ld(reg, addr)		kernel_lw(reg, addr)
 #else
 #define user_sd(reg, addr)		kernel_sd(reg, addr)
-#define user_ld(reg, addr)		kernel_sd(reg, addr)
+#define user_ld(reg, addr)		kernel_ld(reg, addr)
 #endif /* CONFIG_32BIT */
 
 #endif /* CONFIG_EVA */
