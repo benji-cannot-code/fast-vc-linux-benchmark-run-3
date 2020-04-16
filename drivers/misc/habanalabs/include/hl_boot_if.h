ea2d32f580d273975be0a37aa83769aa2d8fe34d
Ofir Bitton <obitton@habana.ai>
2020-04-16 13:47:15 +0300

    habanalabs: load CPU device boot loader from host
    Load CPU device boot loader during driver boot time in order to avoid flash
    write for every boot loader update.
    
    To preserve backward-compatibility, skip the device boot load if the device
    doesn't request it.
    
    Signed-off-by: Ofir Bitton <obitton@habana.ai>
    Reviewed-by: Oded Gabbay <oded.gabbay@gmail.com>
    Signed-off-by: Oded Gabbay <oded.gabbay@gmail.com>

--- drivers/misc/habanalabs/include/hl_boot_if.h
+++ drivers/misc/habanalabs/include/hl_boot_if.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LKD_HARD_RESET_MAGIC		0xED7BD694
 #define HL_POWER9_HOST_MAGIC		0x1DA30009
 
+#define BOOT_FIT_SRAM_OFFSET		0x200000
+
 /*
  * CPU error bits in BOOT_ERROR registers
  *
@@ -78,6 +80,7 @@ enum cpu_boot_status {
 	CPU_BOOT_STATUS_BMC_WAITING_SKIPPED, /* deprecated - will be removed */
 	/* Last boot loader progress status, ready to receive commands */
 	CPU_BOOT_STATUS_READY_TO_BOOT = 15,
+	CPU_BOOT_STATUS_WAITING_FOR_BOOT_FIT = 16,
 };
 
 enum kmd_msg {
