589d526ddb4ba2faadc24d270617110fae7d0d82
Jason Yan <yanaijie@huawei.com>
2020-04-17 15:33:10 +0800

    staging: rtl8723bs: remove some variables in hal_btcoex.c
    Fix the following gcc warning:
    
    drivers/staging/rtl8723bs/hal/hal_btcoex.c:45:26: warning: ‘ioStaString’
    defined but not used [-Wunused-const-variable=]
     static const char *const ioStaString[] = {
                              ^~~~~~~~~~~
    drivers/staging/rtl8723bs/hal/hal_btcoex.c:38:26: warning:
    ‘h2cStaString’ defined but not used [-Wunused-const-variable=]
     static const char *const h2cStaString[] = {
                              ^~~~~~~~~~~~
    drivers/staging/rtl8723bs/hal/hal_btcoex.c:33:26: warning:
    ‘BtLinkRoleString’ defined but not used [-Wunused-const-variable=]
     static const char *const BtLinkRoleString[] = {
                              ^~~~~~~~~~~~~~~~
    drivers/staging/rtl8723bs/hal/hal_btcoex.c:23:26: warning:
    ‘BtSpecString’ defined but not used [-Wunused-const-variable=]
     static const char *const BtSpecString[] = {
                              ^~~~~~~~~~~~
    drivers/staging/rtl8723bs/hal/hal_btcoex.c:15:26: warning:
    ‘BtProfileString’ defined but not used [-Wunused-const-variable=]
     static const char *const BtProfileString[] = {
                              ^~~~~~~~~~~~~~~
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Jason Yan <yanaijie@huawei.com>
    Link: https://lore.kernel.org/r/20200417073310.40893-1-yanaijie@huawei.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/hal_btcoex.c
+++ drivers/staging/rtl8723bs/hal/hal_btcoex.c
@@ -13,49 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <Mp_Precomp.h>
 
 /* 		Global variables */
-static const char *const BtProfileString[] = {
-	"NONE",
-	"A2DP",
-	"PAN",
-	"HID",
-	"SCO",
-};
-
-static const char *const BtSpecString[] = {
-	"1.0b",
-	"1.1",
-	"1.2",
-	"2.0+EDR",
-	"2.1+EDR",
-	"3.0+HS",
-	"4.0",
-};
-
-static const char *const BtLinkRoleString[] = {
-	"Master",
-	"Slave",
-};
-
-static const char *const h2cStaString[] = {
-	"successful",
-	"h2c busy",
-	"rf off",
-	"fw not read",
-};
-
-static const char *const ioStaString[] = {
-	"success",
-	"can not IO",
-	"rf off",
-	"fw not read",
-	"wait io timeout",
-	"invalid len",
-	"idle Q empty",
-	"insert waitQ fail",
-	"unknown fail",
-	"wrong level",
-	"h2c stopped",
-};
 
 BTC_COEXIST GLBtCoexist;
 static u8 GLBtcWiFiInScanState;
