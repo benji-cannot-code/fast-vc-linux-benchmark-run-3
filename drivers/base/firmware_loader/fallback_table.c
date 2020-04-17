2ff0601ee88090c07b280d3b072167a1a3a47e05
Christoph Hellwig <hch@lst.de>
2020-04-17 08:41:42 +0200

    firmware_loader: remove unused exports
    Neither fw_fallback_config nor firmware_config_table are used by modules.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20200417064146.1086644-3-hch@lst.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/firmware_loader/fallback_table.c
+++ drivers/base/firmware_loader/fallback_table.c
@@ -22,7 +22,6 @@ struct firmware_fallback_config fw_fallback_config = {
 	.loading_timeout = 60,
 	.old_timeout = 60,
 };
-EXPORT_SYMBOL_GPL(fw_fallback_config);
 
 #ifdef CONFIG_SYSCTL
 struct ctl_table firmware_config_table[] = {
@@ -46,5 +45,4 @@ struct ctl_table firmware_config_table[] = {
 	},
 	{ }
 };
-EXPORT_SYMBOL_GPL(firmware_config_table);
 #endif
