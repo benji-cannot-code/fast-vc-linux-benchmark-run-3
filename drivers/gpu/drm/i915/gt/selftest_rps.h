f13c18557b9052ad989bdbc8f6ed8ee77b15accd
Chris Wilson <chris@chris-wilson.co.uk>
2020-04-17 16:20:18 +0100

    drm/i915/selftests: Check power consumption at min/max frequencies
    A basic premise of RPS is that at lower frequencies, not only do we run
    slower, but we save power compared to higher frequencies. For example,
    when idle, we set the minimum frequency just in case there is some
    residual current. Since the power curve should be a physical
    relationship, if we find no power saving it's likely that we've broken
    our frequency handling, so test!
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Cc: Andi Shyti <andi.shyti@intel.com>
    Reviewed-by: Andi Shyti <andi.shyti@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200417152018.13079-2-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gt/selftest_rps.h
+++ drivers/gpu/drm/i915/gt/selftest_rps.h
@@ -8,5 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SELFTEST_RPS_H
 
 int live_rps_interrupt(void *arg);
+int live_rps_power(void *arg);
 
 #endif /* SELFTEST_RPS_H */
