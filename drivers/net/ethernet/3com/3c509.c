2f05086818b46cde2195a9c0c0e266cb7a387653
Leon Romanovsky <leonro@mellanox.com>
2020-04-19 17:18:47 +0300

    drivers: Remove inclusion of vermagic header
    Get rid of linux/vermagic.h includes, so that MODULE_ARCH_VERMAGIC from
    the arch header arch/x86/include/asm/module.h won't be redefined.
    
      In file included from ./include/linux/module.h:30,
                       from drivers/net/ethernet/3com/3c515.c:56:
      ./arch/x86/include/asm/module.h:73: warning: "MODULE_ARCH_VERMAGIC"
    redefined
         73 | # define MODULE_ARCH_VERMAGIC MODULE_PROC_FAMILY
            |
      In file included from drivers/net/ethernet/3com/3c515.c:25:
      ./include/linux/vermagic.h:28: note: this is the location of the
    previous definition
         28 | #define MODULE_ARCH_VERMAGIC ""
            |
    
    Fixes: 6bba2e89a88c ("net/3com: Delete driver and module versions from 3com drivers")
    Co-developed-by: Borislav Petkov <bp@suse.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Acked-by: Shannon Nelson <snelson@pensando.io> # ionic
    Acked-by: Sebastian Reichel <sre@kernel.org> # power
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/3com/3c509.c
+++ drivers/net/ethernet/3com/3c509.c
@@ -86,7 +86,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/eisa.h>
 #include <linux/bitops.h>
-#include <linux/vermagic.h>
 
 #include <linux/uaccess.h>
 #include <asm/io.h>
