0c92185c109bf402ce12e17931ff88aec9f9a530
Ping-Ke Shih <pkshih@realtek.com>
2020-04-20 13:50:47 +0800

    rtw88: 8723d: Add basic chip capabilities
    RTL8723DE is an 11n 1x1 2.4G single band chip with the
    following capabilities:
    
     - TX/RX BD size: 16/8
     - TX/RX desc size: 40/24
     - physical/logical/protected efuse size: 512/512/96
     - TX gain index factor: 1
     - max TX power index: 0x3F
     - band: 2G
     - HT: support
     - VHT: Not support
    
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Yan-Hsuan Chuang <yhchuang@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200420055054.14592-2-yhchuang@realtek.com

---
+++ drivers/net/wireless/realtek/rtw88/rtw8723d_table.h
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
+/* Copyright(c) 2018-2019  Realtek Corporation
+ */
+
+#ifndef __RTW8723D_TABLE_H__
+#define __RTW8723D_TABLE_H__
+
+#endif
