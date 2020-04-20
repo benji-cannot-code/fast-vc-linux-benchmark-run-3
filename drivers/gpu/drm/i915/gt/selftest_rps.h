48d38baf32da43af8b7d8815e758fdc448fc195d
Chris Wilson <chris@chris-wilson.co.uk>
2020-04-20 18:27:39 +0100

    drm/i915/selftests: Exercise dynamic reclocking with RPS
    After having testing all the RPS controls individually, we need to take
    a step back and check how our RPS worker integrates them to perform
    dynamic GPU reclocking. So do that by submitting a spinner and wait and
    see what happens.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200420172739.11620-6-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gt/selftest_rps.h
+++ drivers/gpu/drm/i915/gt/selftest_rps.h
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int live_rps_control(void *arg);
 int live_rps_frequency_cs(void *arg);
 int live_rps_frequency_srm(void *arg);
-int live_rps_interrupt(void *arg);
 int live_rps_power(void *arg);
+int live_rps_interrupt(void *arg);
+int live_rps_dynamic(void *arg);
 
 #endif /* SELFTEST_RPS_H */
