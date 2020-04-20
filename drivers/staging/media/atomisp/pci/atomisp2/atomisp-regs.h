a6570a4d9e609ff16978b7d662bc50c00471ddad
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-04-20 12:33:51 +0200

    media: atomisp: pci/atomisp2/*.h remove #ifdef ISP2401
    Those ifs can easily be removed without breaking the code.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/staging/media/atomisp/pci/atomisp2/atomisp-regs.h
+++ drivers/staging/media/atomisp/pci/atomisp2/atomisp-regs.h
@@ -154,12 +154,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CCK_FUSE_REG_0			0x08
 #define CCK_FUSE_HPLL_FREQ_MASK		0x03
 
-#if defined(ISP2401)
-#define ISP_FREQ_MAX	ISP_FREQ_320MHZ
-#else
-#define ISP_FREQ_MAX	ISP_FREQ_400MHZ
-#endif
-
 /* ISP2401 CSI2+ receiver delay settings */
 #define CSI2_PORT_A_BASE					0xC0000
 #define CSI2_PORT_B_BASE					0xC2000
