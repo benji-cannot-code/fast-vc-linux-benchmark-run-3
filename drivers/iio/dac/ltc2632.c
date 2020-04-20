3a89d1f5d09db2f594cfd2fe60f8c3714353749a
Chris Ruehl <chris.ruehl@gtsys.com.hk>
2020-04-20 12:26:06 +0800

    iio: documentation ltc2632_chip_info add num_channels
    The documentation for ltc_2632_chip_info missed the desciption for the
    num_channels. This trivial patch adds it.
    
    Signed-off-by: Chris Ruehl <chris.ruehl@gtsys.com.hk>
    
    Fixes: 9f15a4a0adc9 ("iio: dac: ltc2632: add support for LTC2636 family")
    Acked-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/dac/ltc2632.c
+++ drivers/iio/dac/ltc2632.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /**
  * struct ltc2632_chip_info - chip specific information
  * @channels:		channel spec for the DAC
+ * @num_channels:	DAC channel count of the chip
  * @vref_mv:		internal reference voltage
  */
 struct ltc2632_chip_info {
