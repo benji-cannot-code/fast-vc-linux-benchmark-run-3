246eb71875251a0920e7f0ca120c6c95e4acf35a
Jason Yan <yanaijie@huawei.com>
2020-04-21 11:39:45 +0800

    usb/early: remove unused including <linux/version.h>
    Fix the following versioncheck warning:
    
    drivers/usb/early/xhci-dbc.c:21:1: unused including <linux/version.h>
    
    Signed-off-by: Jason Yan <yanaijie@huawei.com>
    Link: https://lore.kernel.org/r/20200421033945.27703-1-yanaijie@huawei.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/early/xhci-dbc.c
+++ drivers/usb/early/xhci-dbc.c
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/fixmap.h>
 #include <linux/bcd.h>
 #include <linux/export.h>
-#include <linux/version.h>
 #include <linux/module.h>
 #include <linux/delay.h>
 #include <linux/kthread.h>
