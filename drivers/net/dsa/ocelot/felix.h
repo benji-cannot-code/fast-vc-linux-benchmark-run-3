badcdc27305f571ecc975bdd5fe7388e31dd79f0
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2020-04-21 21:13:47 +0300

    net: dsa: felix: allow flooding for all traffic classes
    Right now it can be seen that the VSC9959 (Felix) switch will not flood
    frames if they have a VLAN tag with a PCP of 1-7 (nonzero).
    
    It turns out that Felix is quite different from its cousin, Ocelot, in
    that frame flooding can be allowed/denied per traffic class. Where
    Ocelot has 1 instance of the ANA_FLOODING register, Felix has 8.
    
    The approach that this driver is going to take is "thanks, but no
    thanks". We have no use case of limiting the flooding domain based on
    traffic class, so we just want to allow packets to be flooded, no matter
    what traffic class they have.
    
    So we copy the line of code from ocelot.c which does the one-shot
    initialization of the flooding PGIDs, and we add it to felix.c as well -
    except replicated 8 times.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/ocelot/felix.h
+++ drivers/net/dsa/ocelot/felix.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _MSCC_FELIX_H
 
 #define ocelot_to_felix(o)		container_of((o), struct felix, ocelot)
+#define FELIX_NUM_TC			8
 
 /* Platform-specific information */
 struct felix_info {
