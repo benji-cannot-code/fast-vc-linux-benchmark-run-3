c6b7ae72aaf94b69de57b485cb76dce968d1a53b
Hsin-Hsiung Wang <hsin-hsiung.wang@mediatek.com>
2020-04-21 11:00:10 +0800

    mfd: Add support for the MediaTek MT6358 PMIC
    This adds support for the MediaTek MT6358 PMIC. This is a
    multifunction device with the following sub modules:
    
    - Regulator
    - RTC
    - Codec
    - Interrupt
    
    It is interfaced to the host controller using SPI interface
    by a proprietary hardware called PMIC wrapper or pwrap.
    MT6358 MFD is a child device of the pwrap.
    
    Signed-off-by: Hsin-Hsiung Wang <hsin-hsiung.wang@mediatek.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- include/linux/mfd/mt6397/core.h
+++ include/linux/mfd/mt6397/core.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 enum chip_id {
 	MT6323_CHIP_ID = 0x23,
+	MT6358_CHIP_ID = 0x58,
 	MT6391_CHIP_ID = 0x91,
 	MT6397_CHIP_ID = 0x97,
 };
@@ -66,8 +67,10 @@ struct mt6397_chip {
 	u16 int_con[2];
 	u16 int_status[2];
 	u16 chip_id;
+	void *irq_data;
 };
 
+int mt6358_irq_init(struct mt6397_chip *chip);
 int mt6397_irq_init(struct mt6397_chip *chip);
 
 #endif /* __MFD_MT6397_CORE_H__ */
