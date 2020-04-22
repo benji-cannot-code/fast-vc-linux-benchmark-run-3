00a73642b4d0d06c89095640d8c6293cb60fa840
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-04-22 14:20:14 +0200

    media: atomisp: atomisp_csi2.c: remove useless ifdefs
    The ifdefs there are meaningless. Just remove them for good.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/staging/media/atomisp/pci/atomisp2/atomisp_csi2.c
+++ drivers/staging/media/atomisp/pci/atomisp2/atomisp_csi2.c
@@ -89,12 +89,7 @@ int atomisp_csi2_set_ffmt(struct v4l2_subdev *sd,
 			  struct v4l2_mbus_framefmt *ffmt)
 {
 	struct atomisp_mipi_csi2_device *csi2 = v4l2_get_subdevdata(sd);
-	struct v4l2_mbus_framefmt *actual_ffmt =
-#ifndef ISP2401
-	    __csi2_get_format(csi2, cfg, which, pad);
-#else
-	    __csi2_get_format(csi2, cfg, which, pad);
-#endif
+	struct v4l2_mbus_framefmt *actual_ffmt = __csi2_get_format(csi2, cfg, which, pad);
 
 	if (pad == CSI2_PAD_SINK) {
 		const struct atomisp_in_fmt_conv *ic;
@@ -120,12 +115,7 @@ int atomisp_csi2_set_ffmt(struct v4l2_subdev *sd,
 	}
 
 	/* FIXME: DPCM decompression */
-	*actual_ffmt = *ffmt =
-#ifndef ISP2401
-			   *__csi2_get_format(csi2, cfg, which, CSI2_PAD_SINK);
-#else
-			   *__csi2_get_format(csi2, cfg, which, CSI2_PAD_SINK);
-#endif
+	*actual_ffmt = *ffmt = *__csi2_get_format(csi2, cfg, which, CSI2_PAD_SINK);
 
 	return 0;
 }
@@ -182,9 +172,6 @@ static const struct v4l2_subdev_ops csi2_ops = {
 	.pad = &csi2_pad_ops,
 };
 
-#ifndef ISP2401
-
-#endif
 /*
  * csi2_link_setup - Setup CSI2 connections.
  * @entity : Pointer to media entity structure
@@ -418,9 +405,6 @@ void atomisp_mipi_csi2_cleanup(struct atomisp_device *isp)
 {
 }
 
-#ifndef ISP2401
-
-#endif
 int atomisp_mipi_csi2_init(struct atomisp_device *isp)
 {
 	struct atomisp_mipi_csi2_device *csi2_port;
