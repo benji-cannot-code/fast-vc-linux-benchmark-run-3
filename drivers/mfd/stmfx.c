1056d56ef512116adf5155d322603023aab340ae
Amelie Delaunay <amelie.delaunay@st.com>
2020-04-22 11:08:33 +0200

    mfd: stmfx: Disable IRQ in suspend to avoid spurious interrupt
    When STMFX supply is stopped, spurious interrupt can occur. To avoid that,
    disable the interrupt in suspend before disabling the regulator and
    re-enable it at the end of resume.
    
    Fixes: 06252ade9156 ("mfd: Add ST Multi-Function eXpander (STMFX) core driver")
    Signed-off-by: Amelie Delaunay <amelie.delaunay@st.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/stmfx.c
+++ drivers/mfd/stmfx.c
@@ -297,6 +297,8 @@ static int stmfx_irq_init(struct i2c_client *client)
 	if (ret)
 		goto irq_exit;
 
+	stmfx->irq = client->irq;
+
 	return 0;
 
 irq_exit:
@@ -487,6 +489,8 @@ static int stmfx_suspend(struct device *dev)
 	if (ret)
 		return ret;
 
+	disable_irq(stmfx->irq);
+
 	if (stmfx->vdd)
 		return regulator_disable(stmfx->vdd);
 
@@ -529,6 +533,8 @@ static int stmfx_resume(struct device *dev)
 			       &stmfx->irq_src, sizeof(stmfx->irq_src));
 	if (ret)
 		return ret;
+
+	enable_irq(stmfx->irq);
 
 	return 0;
 }
