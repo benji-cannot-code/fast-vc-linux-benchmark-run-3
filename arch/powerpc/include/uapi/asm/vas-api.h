b34544eae76e01b14fe04e485064a5e1cd3ae321
Stephen Rothwell <sfr@canb.auug.org.au>
2020-04-22 15:41:29 +1000

    powerpc/vas: Include linux/types.h in uapi/asm/vas-api.h
    allyesconfig fails with:
      ./usr/include/asm/vas-api.h:15:2: error: unknown type name '__u32'
         15 |  __u32 version;
            |  ^~~~~
      ./usr/include/asm/vas-api.h:16:2: error: unknown type name '__s16'
         16 |  __s16 vas_id; /* specific instance of vas or -1 for default */
            |  ^~~~~
      ./usr/include/asm/vas-api.h:17:2: error: unknown type name '__u16'
         17 |  __u16 reserved1;
            |  ^~~~~
      ./usr/include/asm/vas-api.h:18:2: error: unknown type name '__u64'
         18 |  __u64 flags; /* Future use */
            |  ^~~~~
      ./usr/include/asm/vas-api.h:19:2: error: unknown type name '__u64'
         19 |  __u64 reserved2[6];
            |  ^~~~~
    
    uapi headers should be self contained, so add an include of
    linux/types.h.
    
    Fixes: 45f25a79fe50 ("powerpc/vas: Define VAS_TX_WIN_OPEN ioctl API")
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Haren Myneni <haren@linux.ibm.com>
    [mpe: Flesh out change log from linux-next error report]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200422154129.11f988fd@canb.auug.org.au

--- arch/powerpc/include/uapi/asm/vas-api.h
+++ arch/powerpc/include/uapi/asm/vas-api.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _UAPI_MISC_VAS_H
 #define _UAPI_MISC_VAS_H
 
+#include <linux/types.h>
+
 #include <asm/ioctl.h>
 
 #define VAS_MAGIC	'v'
