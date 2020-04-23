2067c161e723060acfc423f630553f87c7750d98
Bjorn Andersson <bjorn.andersson@linaro.org>
2020-04-22 17:37:34 -0700

    soc: qcom: glink_ssr: Internalize ssr_notifiers
    Rather than carrying a special purpose blocking notifier for glink_ssr
    in remoteproc's qcom_common.c, move it into glink_ssr so allow wider
    reuse of the common one.
    
    The rpmsg glink header file is used in preparation for the next patch.
    
    Acked-by: Chris Lew <clew@codeaurora.org>
    Acked-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>
    Link: https://lore.kernel.org/r/20200423003736.2027371-3-bjorn.andersson@linaro.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/remoteproc/qcom_common.c
+++ drivers/remoteproc/qcom_common.c
@@ -43,6 +43,13 @@ static void glink_subdev_stop(struct rproc_subdev *subdev, bool crashed)
 	glink->edge = NULL;
 }
 
+static void glink_subdev_unprepare(struct rproc_subdev *subdev)
+{
+	struct qcom_rproc_glink *glink = to_glink_subdev(subdev);
+
+	qcom_glink_ssr_notify(glink->ssr_name);
+}
+
 /**
  * qcom_add_glink_subdev() - try to add a GLINK subdevice to rproc
  * @rproc:	rproc handle to parent the subdevice
@@ -65,6 +72,7 @@ void qcom_add_glink_subdev(struct rproc *rproc, struct qcom_rproc_glink *glink,
 	glink->dev = dev;
 	glink->subdev.start = glink_subdev_start;
 	glink->subdev.stop = glink_subdev_stop;
+	glink->subdev.unprepare = glink_subdev_unprepare;
 
 	rproc_add_subdev(rproc, &glink->subdev);
 }
