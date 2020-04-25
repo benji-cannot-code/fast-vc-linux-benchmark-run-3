40a4870420aeb5e6c3db79be9bd59879a7a28e61
Michael Straube <straube.linux@gmail.com>
2020-04-25 11:28:22 +0200

    staging: rtl8188eu: remove some superfluous comments
    Remove some obviously superflous comments.
    
    Signed-off-by: Michael Straube <straube.linux@gmail.com>
    Link: https://lore.kernel.org/r/20200425092822.19925-1-straube.linux@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/hal/rtl8188e_dm.c
+++ drivers/staging/rtl8188eu/hal/rtl8188e_dm.c
@@ -30,9 +30,6 @@ static void dm_InitGPIOSetting(struct adapter *Adapter)
 	usb_write8(Adapter, REG_GPIO_MUXCFG, tmp1byte);
 }
 
-/*  */
-/*  functions */
-/*  */
 static void Init_ODM_ComInfo_88E(struct adapter *Adapter)
 {
 	struct hal_data_8188e *hal_data = Adapter->HalData;
