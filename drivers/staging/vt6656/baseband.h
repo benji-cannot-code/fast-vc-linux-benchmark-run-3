74b78013622e443c959a6805bec3beb467abe934
Oscar Carter <oscar.carter@gmx.com>
2020-04-25 15:42:56 +0200

    staging: vt6656: Check the return value of vnt_control_out_* calls
    Check the return value of vnt_control_out_* function calls. When
    necessary modify the function prototype to be able to return the new
    checked error code.
    
    It's safe to modify all the function prototypes without fix the call
    because the only change is the return value from void to int. If before
    the call didn't check the return value, now neither.
    
    Signed-off-by: Oscar Carter <oscar.carter@gmx.com>
    Link: https://lore.kernel.org/r/20200425134257.4502-2-oscar.carter@gmx.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vt6656/baseband.h
+++ drivers/staging/vt6656/baseband.h
@@ -68,11 +68,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TOP_RATE_1M         0x00100000
 
 int vnt_set_short_slot_time(struct vnt_private *priv);
-void vnt_set_vga_gain_offset(struct vnt_private *priv, u8 data);
+int vnt_set_vga_gain_offset(struct vnt_private *priv, u8 data);
 int vnt_set_antenna_mode(struct vnt_private *priv, u8 antenna_mode);
 int vnt_vt3184_init(struct vnt_private *priv);
 int vnt_set_deep_sleep(struct vnt_private *priv);
 int vnt_exit_deep_sleep(struct vnt_private *priv);
-void vnt_update_pre_ed_threshold(struct vnt_private *priv, int scanning);
+int vnt_update_pre_ed_threshold(struct vnt_private *priv, int scanning);
 
 #endif /* __BASEBAND_H__ */
