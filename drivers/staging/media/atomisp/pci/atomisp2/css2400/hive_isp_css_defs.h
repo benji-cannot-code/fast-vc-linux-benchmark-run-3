648ff17390db679bd3eb5b38bb281e3cc9c36533
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-04-28 23:25:12 +0200

    media: atomisp: hive_isp_css_defs.h: keep just one copy of it
    While those headers are different, the different fields
    aren't used at the driver. So, remove those different
    unused fields, rename one define and use just one header
    for all 3 different versions of the ISP.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/staging/media/atomisp/pci/atomisp2/css2400/hive_isp_css_defs.h
+++ drivers/staging/media/atomisp/pci/atomisp2/css2400/hive_isp_css_defs.h
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _hive_isp_css_defs_h__
 #define _hive_isp_css_defs_h__
 
-#define HIVE_ISP_CSS_IS_2400B0_SYSTEM
-
 #define HIVE_ISP_CTRL_DATA_WIDTH     32
 #define HIVE_ISP_CTRL_ADDRESS_WIDTH  32
 #define HIVE_ISP_CTRL_MAX_BURST_SIZE  1
@@ -87,7 +85,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HIVE_GP_REGS_SWITCH_GDC2_IDX                           18
 #define HIVE_GP_REGS_SRST_IDX                                  19
 #define HIVE_GP_REGS_SLV_REG_SRST_IDX                          20
-#define HIVE_GP_REGS_VISA_REG_IDX                              21
 
 /* Bit numbers of the soft reset register */
 #define HIVE_GP_REGS_SRST_ISYS_CBUS                             0
@@ -223,7 +220,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HIVE_GP_TIMER_SP_B_STRMON_IRQ                          16
 #define HIVE_GP_TIMER_ISP_STRMON_IRQ                           17
 #define HIVE_GP_TIMER_MOD_STRMON_IRQ                           18
-#define HIVE_GP_TIMER_ISP_PMEM_ERROR_IRQ                       19
 #define HIVE_GP_TIMER_ISP_BAMEM_ERROR_IRQ                      20
 #define HIVE_GP_TIMER_ISP_DMEM_ERROR_IRQ                       21
 #define HIVE_GP_TIMER_SP_ICACHE_MEM_ERROR_IRQ                  22
