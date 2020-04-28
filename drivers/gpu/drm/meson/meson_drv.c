ccd50a34231acebec2a6a5567ec249a1a3dced1d
Neil Armstrong <narmstrong@baylibre.com>
2020-04-28 11:21:47 +0200

    drm/meson: add mode selection limits against specific SoC revisions
    The Amlogic S805X/Y uses the same die as the S905X, but with more
    limited graphics capabilities.
    
    This adds a soc version detection adding specific limitations on the HDMI
    mode selections.
    
    Here, we limit to HDMI 1.2a max HDMI PHY clock frequency.
    
    Changes sinces v1:
    - Moved frequency check in the vclk code, and also checks DMT modes
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Acked-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    [narmstrong: fixed commit message with HDMI 1.2a instead of HDMI 1.3a]
    Link: https://patchwork.freedesktop.org/patch/msgid/20200428092147.13698-1-narmstrong@baylibre.com

--- drivers/gpu/drm/meson/meson_drv.c
+++ drivers/gpu/drm/meson/meson_drv.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/component.h>
 #include <linux/module.h>
 #include <linux/of_graph.h>
+#include <linux/sys_soc.h>
 #include <linux/platform_device.h>
 #include <linux/soc/amlogic/meson-canvas.h>
 
@@ -184,6 +185,24 @@ static void meson_remove_framebuffers(void)
 	kfree(ap);
 }
 
+struct meson_drm_soc_attr {
+	struct meson_drm_soc_limits limits;
+	const struct soc_device_attribute *attrs;
+};
+
+static const struct meson_drm_soc_attr meson_drm_soc_attrs[] = {
+	/* S805X/S805Y HDMI PLL won't lock for HDMI PHY freq > 1,65GHz */
+	{
+		.limits = {
+			.max_hdmi_phy_freq = 1650000,
+		},
+		.attrs = (const struct soc_device_attribute []) {
+			{ .soc_id = "GXL (S805*)", },
+			{ /* sentinel */ },
+		}
+	},
+};
+
 static int meson_drv_bind_master(struct device *dev, bool has_components)
 {
 	struct platform_device *pdev = to_platform_device(dev);
@@ -192,7 +211,7 @@ static int meson_drv_bind_master(struct device *dev, bool has_components)
 	struct drm_device *drm;
 	struct resource *res;
 	void __iomem *regs;
-	int ret;
+	int ret, i;
 
 	/* Checks if an output connector is available */
 	if (!meson_vpu_has_available_connectors(dev)) {
@@ -281,6 +300,14 @@ static int meson_drv_bind_master(struct device *dev, bool has_components)
 	ret = drm_vblank_init(drm, 1);
 	if (ret)
 		goto free_drm;
+
+	/* Assign limits per soc revision/package */
+	for (i = 0 ; i < ARRAY_SIZE(meson_drm_soc_attrs) ; ++i) {
+		if (soc_device_match(meson_drm_soc_attrs[i].attrs)) {
+			priv->limits = &meson_drm_soc_attrs[i].limits;
+			break;
+		}
+	}
 
 	/* Remove early framebuffers (ie. simplefb) */
 	meson_remove_framebuffers();
