9b3204ced8a3c480265ec4a101c8b67e228de122
Tudor Ambarus <tudor.ambarus@microchip.com>
2020-04-28 08:47:43 +0000

    mtd: spi-nor: Fix SPI NOR acronym
    The correct terminology is serial NOR flash or SPI NOR.
    s/SPI-NOR/SPI NOR and s/spi-nor/SPI NOR across the subsystem.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Reviewed-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>

--- drivers/mtd/spi-nor/controllers/hisi-sfc.c
+++ drivers/mtd/spi-nor/controllers/hisi-sfc.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * HiSilicon FMC SPI-NOR flash controller driver
+ * HiSilicon FMC SPI NOR flash controller driver
  *
  * Copyright (c) 2015-2016 HiSilicon Technologies Co., Ltd.
  */
