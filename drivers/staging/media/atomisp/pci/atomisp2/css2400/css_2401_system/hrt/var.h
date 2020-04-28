e933cae7feabefdef5193da5bc64fe5514ab5ef1
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-04-28 18:26:45 +0200

    media: atomisp: get rid of several typedef-style defines
    Those vars aren't used anymore at this driver. Get rid of
    them.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/staging/media/atomisp/pci/atomisp2/css2400/css_2401_system/hrt/var.h
+++ drivers/staging/media/atomisp/pci/atomisp2/css2400/css_2401_system/hrt/var.h
@@ -21,26 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "system_api.h"
 #include "hive_types.h"
 
-#define hrt_int_type_of_char   char
-#define hrt_int_type_of_uchar  unsigned char
-#define hrt_int_type_of_short  short
-#define hrt_int_type_of_ushort unsigned short
-#define hrt_int_type_of_int    int
-#define hrt_int_type_of_uint   unsigned int
-#define hrt_int_type_of_long   long
-#define hrt_int_type_of_ulong  unsigned long
-#define hrt_int_type_of_ptr    unsigned int
-
-#define hrt_host_type_of_char   char
-#define hrt_host_type_of_uchar  unsigned char
-#define hrt_host_type_of_short  short
-#define hrt_host_type_of_ushort unsigned short
-#define hrt_host_type_of_int    int
-#define hrt_host_type_of_uint   unsigned int
-#define hrt_host_type_of_long   long
-#define hrt_host_type_of_ulong  unsigned long
-#define hrt_host_type_of_ptr    void*
-
 #define HRT_TYPE_BYTES(cell, type) (HRT_TYPE_BITS(cell, type) / 8)
 #define HRT_HOST_TYPE(cell_type)   HRTCAT(hrt_host_type_of_, cell_type)
 #define HRT_INT_TYPE(type)         HRTCAT(hrt_int_type_of_, type)
