2f3be33f26d22b730af4535ffec1ac481add4209
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-04-28 09:30:26 +0200

    media: atomisp: make util.c work with ISP2401 runtime detection
    Don't hide those small functions behind ifdefs.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/staging/media/atomisp/pci/atomisp2/css2400/camera/util/interface/ia_css_util.h
+++ drivers/staging/media/atomisp/pci/atomisp2/css2400/camera/util/interface/ia_css_util.h
@@ -75,7 +75,7 @@ enum ia_css_err ia_css_util_check_res(
     unsigned int width,
     unsigned int height);
 
-#ifdef ISP2401
+/* ISP2401 */
 /* @brief compare resolutions (less or equal)
  *
  * @param[in] a resolution
@@ -88,6 +88,7 @@ bool ia_css_util_res_leq(
     struct ia_css_resolution a,
     struct ia_css_resolution b);
 
+/* ISP2401 */
 /**
  * @brief Check if resolution is zero
  *
@@ -98,6 +99,7 @@ bool ia_css_util_res_leq(
 bool ia_css_util_resolution_is_zero(
     const struct ia_css_resolution resolution);
 
+/* ISP2401 */
 /**
  * @brief Check if resolution is even
  *
@@ -108,7 +110,6 @@ bool ia_css_util_resolution_is_zero(
 bool ia_css_util_resolution_is_even(
     const struct ia_css_resolution resolution);
 
-#endif
 /* @brief check width and height
  *
  * @param[in] stream_format
