810aeb103c6cd943d9bc9312bb118e31311c4fde
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2020-04-29 19:23:41 +0300

    perf tools: Simplify checking if SMT is active.
    SMT now could be disabled via "/sys/devices/system/cpu/smt/control".
    
    Status is shown in "/sys/devices/system/cpu/smt/active" simply as "0" / "1".
    
    If this knob isn't here then fallback to checking topology as before.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Dmitry Monakhov <dmtrmonakhov@yandex-team.ru>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Kan Liang <kan.liang@linux.intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lore.kernel.org/lkml/158817741394.748034.9273604089138009552.stgit@buzz
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/smt.c
+++ tools/perf/util/smt.c
@@ -16,6 +16,9 @@ int smt_on(void)
 	if (cached)
 		return cached_result;
 
+	if (sysfs__read_int("devices/system/cpu/smt/active", &cached_result) > 0)
+		goto done;
+
 	ncpu = sysconf(_SC_NPROCESSORS_CONF);
 	for (cpu = 0; cpu < ncpu; cpu++) {
 		unsigned long long siblings;
@@ -43,6 +46,7 @@ int smt_on(void)
 	}
 	if (!cached) {
 		cached_result = 0;
+done:
 		cached = true;
 	}
 	return cached_result;
