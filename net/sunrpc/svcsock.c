f7eba8b370b529663647a6b2b32e353a0eade34f
Chuck Lever <chuck.lever@oracle.com>
2020-05-01 10:40:47 -0400

    SUNRPC: Remove "#include <trace/events/skb.h>"
    Clean up: Commit 850cbaddb52d ("udp: use it's own memory accounting
    schema") removed the last skb-related tracepoint from svcsock.c, so
    it is no longer necessary to include trace/events/skb.h.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- net/sunrpc/svcsock.c
+++ net/sunrpc/svcsock.c
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/tcp_states.h>
 #include <linux/uaccess.h>
 #include <asm/ioctls.h>
-#include <trace/events/skb.h>
 
 #include <linux/sunrpc/types.h>
 #include <linux/sunrpc/clnt.h>
