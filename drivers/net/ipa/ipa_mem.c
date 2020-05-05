df8a017e925fc17bc81258c08b96c15f7db6ff14
YueHaibing <yuehaibing@huawei.com>
2020-05-05 10:03:07 +0000

    net: ipa: remove duplicated include from ipa_mem.c
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ipa/ipa_mem.c
+++ drivers/net/ipa/ipa_mem.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ipa_data.h"
 #include "ipa_cmd.h"
 #include "ipa_mem.h"
-#include "ipa_data.h"
 #include "ipa_table.h"
 #include "gsi_trans.h"
 
