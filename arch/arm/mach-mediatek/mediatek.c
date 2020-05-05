4ee295c75bb2e498b7761c8cdd715495f1bbac62
Geert Uytterhoeven <geert+renesas@glider.be>
2020-05-05 17:45:35 +0200

    ARM: mediatek: Replace <linux/clk-provider.h> by <linux/of_clk.h>
    The Mediatek platform code is not a clock provider, and just needs to
    call of_clk_init().
    
    Hence it can include <linux/of_clk.h> instead of <linux/clk-provider.h>.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Stephen Boyd <sboyd@kernel.org>
    Link: https://lore.kernel.org/r/20200505154536.4099-3-geert+renesas@glider.be
    Signed-off-by: Matthias Brugger <matthias.bgg@gmail.com>

--- arch/arm/mach-mediatek/mediatek.c
+++ arch/arm/mach-mediatek/mediatek.c
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <asm/mach/arch.h>
 #include <linux/of.h>
-#include <linux/clk-provider.h>
+#include <linux/of_clk.h>
 #include <linux/clocksource.h>
 
 
