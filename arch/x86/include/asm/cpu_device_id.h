1148d45cf88e3fe47363985fc75fa4dae854a6d9
Borislav Petkov <bp@suse.de>
2020-05-05 19:25:08 +0200

    x86/cpu: Add a X86_MATCH_INTEL_FAM6_MODEL_STEPPINGS() macro
    ... to match Intel family 6 CPUs with steppings.
    
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Mark Gross <mgross@linux.intel.com>
    Link: https://lkml.kernel.org/r/20200506071516.25445-3-bp@alien8.de

--- arch/x86/include/asm/cpu_device_id.h
+++ arch/x86/include/asm/cpu_device_id.h
@@ -161,6 +161,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define X86_MATCH_INTEL_FAM6_MODEL(model, data)				\
 	X86_MATCH_VENDOR_FAM_MODEL(INTEL, 6, INTEL_FAM6_##model, data)
 
+#define X86_MATCH_INTEL_FAM6_MODEL_STEPPINGS(model, steppings, data)	\
+	X86_MATCH_VENDOR_FAM_MODEL_STEPPINGS_FEATURE(INTEL, 6, INTEL_FAM6_##model, \
+						     steppings, X86_FEATURE_ANY, data)
+
 /*
  * Match specific microcode revisions.
  *
