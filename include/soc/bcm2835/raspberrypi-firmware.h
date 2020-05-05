f23eccece380448edb995c5a055d67f487f7ce1f
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2020-05-05 18:13:15 +0200

    firmware: raspberrypi: Introduce vl805 init routine
    The Raspberry Pi 4 gets its USB functionality from VL805, a PCIe chip
    that implements xHCI. After a PCI reset, VL805's firmware may either be
    loaded directly from an EEPROM or, if not present, by the SoC's
    co-processor, VideoCore. RPi4's VideoCore OS contains both the non public
    firmware load logic and the VL805 firmware blob. The function this patch
    introduces triggers the aforementioned process.
    
    Link: https://lore.kernel.org/r/20200505161318.26200-3-nsaenzjulienne@suse.de
    Tested-by: Stefan Wahren <stefan.wahren@i2se.com>
    Tested-by: Stefan Wahren <stefan.wahren@i2se.com>
    Signed-off-by: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Reviewed-by: Rob Herring <robh@kernel.org>

--- include/soc/bcm2835/raspberrypi-firmware.h
+++ include/soc/bcm2835/raspberrypi-firmware.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_device.h>
 
 struct rpi_firmware;
+struct pci_dev;
 
 enum rpi_firmware_property_status {
 	RPI_FIRMWARE_STATUS_REQUEST = 0,
@@ -142,6 +143,7 @@ int rpi_firmware_property(struct rpi_firmware *fw,
 int rpi_firmware_property_list(struct rpi_firmware *fw,
 			       void *data, size_t tag_size);
 struct rpi_firmware *rpi_firmware_get(struct device_node *firmware_node);
+int rpi_firmware_init_vl805(struct pci_dev *pdev);
 #else
 static inline int rpi_firmware_property(struct rpi_firmware *fw, u32 tag,
 					void *data, size_t len)
@@ -158,6 +160,11 @@ static inline int rpi_firmware_property_list(struct rpi_firmware *fw,
 static inline struct rpi_firmware *rpi_firmware_get(struct device_node *firmware_node)
 {
 	return NULL;
+}
+
+static inline int rpi_firmware_init_vl805(struct pci_dev *pdev)
+{
+	return 0;
 }
 #endif
 
