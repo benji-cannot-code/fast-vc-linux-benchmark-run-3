81f41ac471fa174aaf6fa2b2b9c1dd120821f131
Francois Buergisser <fbuergisser@chromium.org>
2020-05-06 07:01:37 +0200

    media: mtk-mdp: Remove states for format checks
    The mtk-mdp driver uses states to check if the formats have been set
    on the capture and output when turning the streaming on, setting
    controls or setting the selection rectangles.
    Those states are reset when 0 buffers are requested like when checking
    capabilities.
    This patch removes all format checks and set one by default as queues in
    V4L2 are expected to always have a format set.
    
    https://linuxtv.org/downloads/v4l-dvb-apis/uapi/v4l/vidioc-streamon.html
    https://linuxtv.org/downloads/v4l-dvb-apis/uapi/v4l/vidioc-g-ctrl.html
    https://linuxtv.org/downloads/v4l-dvb-apis/uapi/v4l/vidioc-g-selection.html
    
    Signed-off-by: Francois Buergisser <fbuergisser@chromium.org>
    Signed-off-by: Eizan Miyamoto <eizan@chromium.org>
    Reviewed-by: Enric Balletbo I Serra <enric.balletbo@collabora.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/platform/mtk-mdp/mtk_mdp_core.h
+++ drivers/media/platform/mtk-mdp/mtk_mdp_core.h
@@ -29,8 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MTK_MDP_FMT_FLAG_CAPTURE	BIT(1)
 
 #define MTK_MDP_VPU_INIT		BIT(0)
-#define MTK_MDP_SRC_FMT			BIT(1)
-#define MTK_MDP_DST_FMT			BIT(2)
 #define MTK_MDP_CTX_ERROR		BIT(5)
 
 /**
