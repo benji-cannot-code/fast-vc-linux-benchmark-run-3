a9ffb3976e632aad127ce14296576442c3bdf8ce
Darrick J. Wong <darrick.wong@oracle.com>
2020-05-06 12:07:25 -0700

    xfs: refactor xlog_item_is_intent now that we're done converting
    Now that we've finished converting all types of log intent items to
    provide an ->iop_recover function, we can convert the "is this an intent
    item?" predicate to look for a non-null iop_recover pointer.
    
    Move the predicate closer to the functions that use it.
    
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Chandan Babu R <chandanrlinux@gmail.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- fs/xfs/xfs_log_recover.c
+++ fs/xfs/xfs_log_recover.c
@@ -2547,20 +2547,6 @@ xlog_recover_process_data(
 	return 0;
 }
 
-/* Is this log item a deferred action intent? */
-static inline bool xlog_item_is_intent(struct xfs_log_item *lip)
-{
-	switch (lip->li_type) {
-	case XFS_LI_EFI:
-	case XFS_LI_RUI:
-	case XFS_LI_CUI:
-	case XFS_LI_BUI:
-		return true;
-	default:
-		return false;
-	}
-}
-
 /* Take all the collected deferred ops and finish them in order. */
 static int
 xlog_finish_defer_ops(
@@ -2593,6 +2579,12 @@ xlog_finish_defer_ops(
 	xfs_defer_move(tp, parent_tp);
 
 	return xfs_trans_commit(tp);
+}
+
+/* Is this log item a deferred action intent? */
+static inline bool xlog_item_is_intent(struct xfs_log_item *lip)
+{
+	return lip->li_ops->iop_recover != NULL;
 }
 
 /*
