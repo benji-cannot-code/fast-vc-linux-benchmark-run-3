4e5efa74895fb92c8872a98cc95f92096b7e969a
Geert Uytterhoeven <geert+renesas@glider.be>
2020-05-06 14:03:04 +0200

    media: i2c: imx219: Drop <linux/clk-provider.h> and <linux/clkdev.h>
    The IMX219 camera driver is not a clock provider, but merely a clock
    consumer, and thus does not need to include <linux/clk-provider.h> and
    <linux/clkdev.h>.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/i2c/imx219.c
+++ drivers/media/i2c/imx219.c
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/clk.h>
-#include <linux/clk-provider.h>
-#include <linux/clkdev.h>
 #include <linux/delay.h>
 #include <linux/gpio/consumer.h>
 #include <linux/i2c.h>
