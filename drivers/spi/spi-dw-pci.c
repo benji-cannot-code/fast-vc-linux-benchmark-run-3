60d3c5c9add572d7bbed8ab5b94af63c659a8aa1
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-05-06 18:30:20 +0300

    spi: dw: Move interrupt.h to spi-dw.h who is user of it
    The actual user of interrupt.h is spi-dw.h and not bus drivers.
    Move header there.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: https://lore.kernel.org/r/20200506153025.21441-3-andriy.shevchenko@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-dw-pci.c
+++ drivers/spi/spi-dw-pci.c
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (c) 2009, 2014 Intel Corporation.
  */
 
-#include <linux/interrupt.h>
 #include <linux/pci.h>
 #include <linux/pm_runtime.h>
 #include <linux/slab.h>
--- drivers/spi/spi-dw.h
+++ drivers/spi/spi-dw.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef DW_SPI_HEADER_H
 #define DW_SPI_HEADER_H
 
+#include <linux/interrupt.h>
 #include <linux/io.h>
 #include <linux/scatterlist.h>
 
