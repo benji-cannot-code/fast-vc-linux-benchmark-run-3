8e8a2dd1af908f15d7f2f9ac9dfdede43f4d24f4
Mark Brown <broonie@kernel.org>
2020-05-06 20:51:32 +0100

    arm64: bpf: Annotate JITed code for BTI
    In order to extend the protection offered by BTI to all code executing in
    kernel mode we need to annotate JITed BPF code appropriately for BTI. To
    do this we need to add a landing pad to the start of each BPF function and
    also immediately after the function prologue if we are emitting a function
    which can be tail called. Jumps within BPF functions are all to immediate
    offsets and therefore do not require landing pads.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/20200506195138.22086-6-broonie@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/net/bpf_jit.h
+++ arch/arm64/net/bpf_jit.h
@@ -190,4 +190,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Rn & Rm; set condition flags */
 #define A64_TST(sf, Rn, Rm) A64_ANDS(sf, A64_ZR, Rn, Rm)
 
+/* HINTs */
+#define A64_HINT(x) aarch64_insn_gen_hint(x)
+
+/* BTI */
+#define A64_BTI_C  A64_HINT(AARCH64_INSN_HINT_BTIC)
+#define A64_BTI_J  A64_HINT(AARCH64_INSN_HINT_BTIJ)
+#define A64_BTI_JC A64_HINT(AARCH64_INSN_HINT_BTIJC)
+
 #endif /* _BPF_JIT_H */
