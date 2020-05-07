54b3e6107e12e98736170efb8ce55ee4440a699f
Tony Lindgren <tony@atomide.com>
2020-05-07 09:59:31 -0700

    ARM: OMAP2+: Add omap_init_time_of()
    This allows us to move the SoCs to probe system timers one SoC
    at at time. As arch/arm/mach-omap2/timer.c will be eventually gone,
    let's just add omap_init_time_of() to board-generic.c directly.
    
    Cc: Grygorii Strashko <grygorii.strashko@ti.com>
    Cc: Keerthy <j-keerthy@ti.com>
    Cc: Lokesh Vutla <lokeshvutla@ti.com>
    Cc: Rob Herring <robh@kernel.org>
    Cc: Tero Kristo <t-kristo@ti.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/board-generic.c
+++ arch/arm/mach-omap2/board-generic.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_irq.h>
 #include <linux/of_platform.h>
 #include <linux/irqdomain.h>
+#include <linux/clocksource.h>
 
 #include <asm/setup.h>
 #include <asm/mach/arch.h>
@@ -30,6 +31,13 @@ static void __init __maybe_unused omap_generic_init(void)
 {
 	pdata_quirks_init(omap_dt_match_table);
 	omap_soc_device_init();
+}
+
+/* Clocks are needed early, see drivers/clocksource for the rest */
+void __init __maybe_unused omap_init_time_of(void)
+{
+	omap_clk_init();
+	timer_probe();
 }
 
 #ifdef CONFIG_SOC_OMAP2420
