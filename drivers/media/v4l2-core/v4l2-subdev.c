5606c81d1a037a26689bef580c38e0232285b1bc
Hans Verkuil <hans.verkuil@cisco.com>
2020-05-07 17:12:52 +0200

    media: v4l2-subdev: add VIDIOC_SUBDEV_QUERYCAP ioctl
    While normal video/radio/vbi/swradio nodes have a proper QUERYCAP ioctl
    that apps can call to determine that it is indeed a V4L2 device, there
    is currently no equivalent for v4l-subdev nodes. Adding this ioctl will
    solve that, and it will allow utilities like v4l2-compliance to be used
    with these devices as well.
    
    SUBDEV_QUERYCAP currently returns the version and capabilities of the
    subdevice. Define a capability flag to report if the subdevice is
    registered in read-only mode.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Jacopo Mondi <jacopo@jmondi.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/v4l2-core/v4l2-subdev.c
+++ drivers/media/v4l2-core/v4l2-subdev.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/videodev2.h>
 #include <linux/export.h>
+#include <linux/version.h>
 
 #include <media/v4l2-ctrls.h>
 #include <media/v4l2-device.h>
@@ -345,6 +346,16 @@ static long subdev_do_ioctl(struct file *file, unsigned int cmd, void *arg)
 	int rval;
 
 	switch (cmd) {
+	case VIDIOC_SUBDEV_QUERYCAP: {
+		struct v4l2_subdev_capability *cap = arg;
+
+		memset(cap->reserved, 0, sizeof(cap->reserved));
+		cap->version = LINUX_VERSION_CODE;
+		cap->capabilities = ro_subdev ? V4L2_SUBDEV_CAP_RO_SUBDEV : 0;
+
+		return 0;
+	}
+
 	case VIDIOC_QUERYCTRL:
 		/*
 		 * TODO: this really should be folded into v4l2_queryctrl (this
