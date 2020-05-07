9f42b88afa3e671fcf5bd6d268b0487a5490a98c
Douglas Anderson <dianders@chromium.org>
2020-05-07 16:11:46 -0700

    kdb: Cleanup math with KDB_CMD_HISTORY_COUNT
    From code inspection the math in handle_ctrl_cmd() looks super sketchy
    because it subjects -1 from cmdptr and then does a "%
    KDB_CMD_HISTORY_COUNT".  It turns out that this code works because
    "cmdptr" is unsigned and KDB_CMD_HISTORY_COUNT is a nice power of 2.
    Let's make this a little less sketchy.
    
    This patch should be a no-op.
    
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Link: https://lore.kernel.org/r/20200507161125.1.I2cce9ac66e141230c3644b8174b6c15d4e769232@changeid
    Reviewed-by: Sumit Garg <sumit.garg@linaro.org>
    Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>

--- kernel/debug/kdb/kdb_main.c
+++ kernel/debug/kdb/kdb_main.c
@@ -1109,7 +1109,8 @@ static int handle_ctrl_cmd(char *cmd)
 	switch (*cmd) {
 	case CTRL_P:
 		if (cmdptr != cmd_tail)
-			cmdptr = (cmdptr-1) % KDB_CMD_HISTORY_COUNT;
+			cmdptr = (cmdptr + KDB_CMD_HISTORY_COUNT - 1) %
+				 KDB_CMD_HISTORY_COUNT;
 		strscpy(cmd_cur, cmd_hist[cmdptr], CMD_BUFLEN);
 		return 1;
 	case CTRL_N:
