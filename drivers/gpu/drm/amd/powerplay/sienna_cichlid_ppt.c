136da5986416fa9340d34b1a846741f17c82f639
Kenneth Feng <kenneth.feng@amd.com>
2020-05-07 14:33:06 +0800

    drm/amd/powerplay: show gfxclk=0 in gfxoff state
    The instant retrieved gfxclk value should be 0 in gfxoff state.
    This can be fetched with gfxoff enabled.
    
    Signed-off-by: Kenneth Feng <kenneth.feng@amd.com>
    Reviewed-by: Likun Gao <Likun.Gao@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/sienna_cichlid_ppt.c
+++ drivers/gpu/drm/amd/powerplay/sienna_cichlid_ppt.c
@@ -710,9 +710,6 @@ static int sienna_cichlid_print_clk_levels(struct smu_context *smu,
 	uint32_t mark_index = 0;
 	uint32_t gen_speed, lane_width;
 
-	if ((clk_type == SMU_GFXCLK) || (clk_type == SMU_SCLK))
-		amdgpu_gfx_off_ctrl(adev, false);
-
 	switch (clk_type) {
 	case SMU_GFXCLK:
 	case SMU_SCLK:
@@ -727,6 +724,10 @@ static int sienna_cichlid_print_clk_levels(struct smu_context *smu,
 
 		/* 10KHz -> MHz */
 		cur_value = cur_value / 100;
+
+		/* no need to disable gfxoff when retrieving the current gfxclk */
+		if ((clk_type == SMU_GFXCLK) || (clk_type == SMU_SCLK))
+			amdgpu_gfx_off_ctrl(adev, false);
 
 		ret = smu_get_dpm_level_count(smu, clk_type, &count);
 		if (ret)
