1dec98b0e623bad3b4e1580887e7ce7472ba5439
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-05-08 13:36:28 +0900

    ALSA: firewire-lib: add reference to domain structure from stream structure
    In current implementation, AMDTP domain structure and AMDTP stream
    structure has one way of reference from the former to the latter. For
    future extension, bidirectional reference is needed.
    
    This commit adds a member into stream structure to refer to domain
    structure to which the stream belongs.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Link: https://lore.kernel.org/r/20200508043635.349339-4-o-takashi@sakamocchi.jp
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/amdtp-stream.h
+++ sound/firewire/amdtp-stream.h
@@ -109,6 +109,8 @@ typedef unsigned int (*amdtp_stream_process_ctx_payloads_t)(
 						const struct pkt_desc *desc,
 						unsigned int packets,
 						struct snd_pcm_substream *pcm);
+
+struct amdtp_domain;
 struct amdtp_stream {
 	struct fw_unit *unit;
 	enum cip_flags flags;
@@ -181,6 +183,7 @@ struct amdtp_stream {
 	int channel;
 	int speed;
 	struct list_head list;
+	struct amdtp_domain *domain;
 };
 
 int amdtp_stream_init(struct amdtp_stream *s, struct fw_unit *unit,
