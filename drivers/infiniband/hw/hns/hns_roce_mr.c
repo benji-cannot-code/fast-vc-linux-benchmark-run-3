8063771859525f9201810bf57b2d09c355ddde0c
Lang Cheng <chenglang@huawei.com>
2020-05-08 17:45:56 +0800

    RDMA/hns: Store mr len information into mr obj
    The length information should be stored in the struct ib_mr object,
    otherwise the length value of a valid mr object would always be 0.
    
    Link: https://lore.kernel.org/r/1588931159-56875-7-git-send-email-liweihang@huawei.com
    Signed-off-by: Lang Cheng <chenglang@huawei.com>
    Signed-off-by: Weihang Li <liweihang@huawei.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- drivers/infiniband/hw/hns/hns_roce_mr.c
+++ drivers/infiniband/hw/hns/hns_roce_mr.c
@@ -286,6 +286,7 @@ struct ib_mr *hns_roce_reg_user_mr(struct ib_pd *pd, u64 start, u64 length,
 		goto err_alloc_pbl;
 
 	mr->ibmr.rkey = mr->ibmr.lkey = mr->key;
+	mr->ibmr.length = length;
 
 	return &mr->ibmr;
 
@@ -452,6 +453,7 @@ struct ib_mr *hns_roce_alloc_mr(struct ib_pd *pd, enum ib_mr_type mr_type,
 		goto err_pbl;
 
 	mr->ibmr.rkey = mr->ibmr.lkey = mr->key;
+	mr->ibmr.length = length;
 
 	return &mr->ibmr;
 
