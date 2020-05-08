579842013b851b4a37e5e39144f4530e50573de7
Ben Chuang <ben.chuang@genesyslogic.com.tw>
2020-05-08 14:41:54 +0800

    mmc: sdhci-pci-gli: Add Genesys Logic GL9763E support
    GL9763E supports High Speed SDR, High Speed DDR, HS200, HS400, Enhanced
    Strobe in HS400 mode, 1/4/8 bits data bus and 3.3/1.8V.
    
    Signed-off-by: Ben Chuang <ben.chuang@genesyslogic.com.tw>
    Link: https://lore.kernel.org/r/20200508064154.13473-1-benchuanggli@gmail.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sdhci-pci.h
+++ drivers/mmc/host/sdhci-pci.h
@@ -73,6 +73,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define PCI_DEVICE_ID_GLI_9755		0x9755
 #define PCI_DEVICE_ID_GLI_9750		0x9750
+#define PCI_DEVICE_ID_GLI_9763E		0xe763
 
 /*
  * PCI device class and mask
@@ -196,5 +197,6 @@ extern const struct sdhci_pci_fixes sdhci_snps;
 extern const struct sdhci_pci_fixes sdhci_o2;
 extern const struct sdhci_pci_fixes sdhci_gl9750;
 extern const struct sdhci_pci_fixes sdhci_gl9755;
+extern const struct sdhci_pci_fixes sdhci_gl9763e;
 
 #endif /* __SDHCI_PCI_H */
