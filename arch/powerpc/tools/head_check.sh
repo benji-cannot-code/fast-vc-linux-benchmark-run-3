089592e7ea186e6a6d9986359a1f1eaeb88c0c0d
Geoff Levand <geoff@infradead.org>
2020-05-09 18:58:31 +0000

    powerpc/head_check: Automatic verbosity
    To aid debugging build problems turn on shell tracing for the
    head_check script when the build is verbose.
    
    Signed-off-by: Geoff Levand <geoff@infradead.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/1ae1aed811ba6760af2e46d331285dd6a4de5b80.1589049250.git.geoff@infradead.org

--- arch/powerpc/tools/head_check.sh
+++ arch/powerpc/tools/head_check.sh
@@ -32,8 +32,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # level entry code (boot, interrupt vectors, etc) until r2 is set up. This
 # could cause the kernel to die in early boot.
 
-# Turn this on if you want more debug output:
-# set -x
+# Allow for verbose output
+if [ "$V" = "1" ]; then
+	set -x
+fi
 
 if [ $# -lt 2 ]; then
 	echo "$0 [path to nm] [path to vmlinux]" 1>&2
