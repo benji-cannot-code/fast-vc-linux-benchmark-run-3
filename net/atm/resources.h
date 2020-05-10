2b9d8f22fcd0b71f1759cc050e5b5ce01d413528
Al Viro <viro@zeniv.linux.org.uk>
2020-05-10 17:20:49 -0400

    atm: separate ATM_GETNAMES handling from the rest of atm_dev_ioctl()
    atm_dev_ioctl() does copyin in two different ways - one for
    ATM_GETNAMES, another for everything else.  Start with separating
    the former into a new helper (atm_getnames()).  The next step
    will be to lift the copyin into the callers.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- net/atm/resources.h
+++ net/atm/resources.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern struct list_head atm_devs;
 extern struct mutex atm_dev_mutex;
 
+int atm_getnames(void __user *arg, int compat);
 int atm_dev_ioctl(unsigned int cmd, void __user *arg, int compat);
 
 
