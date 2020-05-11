b048c720246f1e6d23547f4d93eed7d48c56b527
Amit Kucheria <amit.kucheria@linaro.org>
2020-05-11 17:54:56 +0530

    thermal/drivers/clock_cooling: Include export.h
    It is preferrable to include export.h when you are using EXPORT_SYMBOL
    family of macros.
    
    Signed-off-by: Amit Kucheria <amit.kucheria@linaro.org>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/25f16415ab7b7587a052f1bce4133da318d58192.1589199124.git.amit.kucheria@linaro.org

--- drivers/thermal/clock_cooling.c
+++ drivers/thermal/clock_cooling.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpufreq.h>
 #include <linux/device.h>
 #include <linux/err.h>
+#include <linux/export.h>
 #include <linux/idr.h>
 #include <linux/mutex.h>
 #include <linux/pm_opp.h>
