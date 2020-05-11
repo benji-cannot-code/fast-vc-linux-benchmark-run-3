73be05890f400aa512a6db938c02b76da07a3e5f
Kefeng Wang <wangkefeng.wang@huawei.com>
2020-05-11 10:20:01 +0800

    riscv: mmiowb: Fix implicit declaration of function 'smp_processor_id'
    In file included from ./../include/linux/compiler_types.h:68,
                     from <command-line>:
    ../include/asm-generic/mmiowb.h: In function ‘mmiowb_set_pending’:
    ../include/asm-generic/percpu.h:34:38: error: implicit declaration of function ‘smp_processor_id’; did you mean ‘raw_smp_processor_id’? [-Werror=implicit-function-declaration]
     #define my_cpu_offset per_cpu_offset(smp_processor_id())
                                          ^~~~~~~~~~~~~~~~
    ../include/linux/compiler-gcc.h:58:26: note: in definition of macro ‘RELOC_HIDE’
      (typeof(ptr)) (__ptr + (off));     \
                              ^~~
    ../include/linux/percpu-defs.h:249:2: note: in expansion of macro ‘SHIFT_PERCPU_PTR’
      SHIFT_PERCPU_PTR(ptr, my_cpu_offset);    \
      ^~~~~~~~~~~~~~~~
    ../include/asm-generic/percpu.h:34:23: note: in expansion of macro ‘per_cpu_offset’
     #define my_cpu_offset per_cpu_offset(smp_processor_id())
                           ^~~~~~~~~~~~~~
    ../include/linux/percpu-defs.h:249:24: note: in expansion of macro ‘my_cpu_offset’
      SHIFT_PERCPU_PTR(ptr, my_cpu_offset);    \
                            ^~~~~~~~~~~~~
    ../include/asm-generic/mmiowb.h:30:26: note: in expansion of macro ‘this_cpu_ptr’
     #define __mmiowb_state() this_cpu_ptr(&__mmiowb_state)
                              ^~~~~~~~~~~~
    ../include/asm-generic/mmiowb.h:37:28: note: in expansion of macro ‘__mmiowb_state’
      struct mmiowb_state *ms = __mmiowb_state();
                                ^~~~~~~~~~~~~~
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>

--- arch/riscv/include/asm/mmiowb.h
+++ arch/riscv/include/asm/mmiowb.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define mmiowb()	__asm__ __volatile__ ("fence o,w" : : : "memory");
 
+#include <linux/smp.h>
 #include <asm-generic/mmiowb.h>
 
 #endif	/* _ASM_RISCV_MMIOWB_H */
