f095fa3a95d9f4adbd5a8360f600574cf30ffe83
Amit Kucheria <amit.kucheria@linaro.org>
2020-05-11 17:54:54 +0530

    thermal/drivers/thermal_hwmon: Include export.h
    It is preferable to include export.h when you are using EXPORT_SYMBOL
    family of macros.
    
    Signed-off-by: Amit Kucheria <amit.kucheria@linaro.org>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/f542962494a8441fdc8e550a11d0e535b92362a0.1589199124.git.amit.kucheria@linaro.org

--- drivers/thermal/thermal_hwmon.c
+++ drivers/thermal/thermal_hwmon.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  Copyright (C) 2013 Eduardo Valentin <eduardo.valentin@ti.com>
  */
 #include <linux/err.h>
+#include <linux/export.h>
 #include <linux/hwmon.h>
 #include <linux/slab.h>
 #include <linux/thermal.h>
