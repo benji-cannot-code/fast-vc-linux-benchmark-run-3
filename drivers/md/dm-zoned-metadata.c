eba89d33693cb4062080587a92b12b36c212dfae
Hannes Reinecke <hare@suse.de>
2020-05-11 10:24:29 +0200

    dm zoned: ignore metadata zone in dmz_alloc_zone()
    When looking up zones in dmz_alloc_zone() we need to ignore
    metadata zones so as not to accidentally overwrite metadata.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>
    Reviewed-by: Damien Le Moal <damien.lemoal@wdc.com>
    Reviewed-by: Bob Liu <bob.liu@oracle.com>
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>

--- drivers/md/dm-zoned-metadata.c
+++ drivers/md/dm-zoned-metadata.c
@@ -1908,7 +1908,13 @@ again:
 		zone = NULL;
 		goto again;
 	}
+	if (dmz_is_meta(zone)) {
+		struct dmz_dev *dev = dmz_zone_to_dev(zmd, zone);
 
+		dmz_dev_warn(dev, "Zone %u has metadata", zone->id);
+		zone = NULL;
+		goto again;
+	}
 	return zone;
 }
 
