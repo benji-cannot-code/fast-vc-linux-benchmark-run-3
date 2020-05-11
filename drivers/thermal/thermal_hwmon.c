f5ba3f47ad9c275ded176c6cf01773836f200776
Amit Kucheria <amit.kucheria@linaro.org>
2020-05-11 17:54:53 +0530

    thermal/drivers/thermal_hwmon: Sort headers alphabetically
    Sort headers to make it easier to read and find duplicate headers.
    
    Signed-off-by: Amit Kucheria <amit.kucheria@linaro.org>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/29b64f1fe81e674c753c8f8309c310acd782ebea.1589199124.git.amit.kucheria@linaro.org

--- drivers/thermal/thermal_hwmon.c
+++ drivers/thermal/thermal_hwmon.c
@@ -11,10 +11,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  Copyright (C) 2013 Texas Instruments
  *  Copyright (C) 2013 Eduardo Valentin <eduardo.valentin@ti.com>
  */
-#include <linux/hwmon.h>
-#include <linux/thermal.h>
-#include <linux/slab.h>
 #include <linux/err.h>
+#include <linux/hwmon.h>
+#include <linux/slab.h>
+#include <linux/thermal.h>
+
 #include "thermal_hwmon.h"
 
 /* hwmon sys I/F */
