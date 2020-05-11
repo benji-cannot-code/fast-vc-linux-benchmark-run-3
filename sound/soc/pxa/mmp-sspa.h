f458f92b269120943b81c586641d4e352907a357
Lubomir Rintel <lkundrak@v3.sk>
2020-05-11 23:01:32 +0200

    ASoC: mmp-sspa: Set appropriate bus format for given bit width
    The values set by set_dai_fmt() and hw_params() seem to be tailored only
    for 32-bit formats. Negotiate the correct ones in hw_params() callback
    instead.
    
    This was essentially copied from the OLPC kernel driver and tested to
    fix wrong audio output for non-32bit formats. The documentation is not
    available.
    
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>
    Link: https://lore.kernel.org/r/20200511210134.1224532-10-lkundrak@v3.sk
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/pxa/mmp-sspa.h
+++ sound/soc/pxa/mmp-sspa.h
@@ -64,7 +64,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	SSPA_SP_FFLUSH		(1 << 2)	/* FIFO Flush */
 #define	SSPA_SP_S_RST		(1 << 1)	/* Active High Reset Signal */
 #define	SSPA_SP_S_EN		(1 << 0)	/* Serial Clock Domain Enable */
+#define	SSPA_SP_FWID_MASK	(0x3f << 20)
 #define	SSPA_SP_FWID(x)		((x) << 20)	/* Frame-Sync Width */
+#define	SSPA_TXSP_FPER_MASK	(0x3f << 4)
 #define	SSPA_TXSP_FPER(x)	((x) << 4)	/* Frame-Sync Active */
 
 /* sspa clock sources */
