bd1484e600470efc2320a10b4af8082e283ce45b
Masahiro Yamada <masahiroy@kernel.org>
2020-05-11 13:21:49 +0900

    kbuild: make module name conflict fatal error
    I think all the warnings have been fixed by now. Make it a fatal error.
    
    Check it before modpost because we need to stop building *.ko files.
    Also, pass modules.order via a script parameter.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>

--- scripts/modules-check.sh
+++ scripts/modules-check.sh
@@ -4,14 +4,24 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 set -e
 
+if [ $# != 1 ]; then
+	echo "Usage: $0 <modules.order>" >& 2
+	exit 1
+fi
+
+exit_code=0
+
 # Check uniqueness of module names
 check_same_name_modules()
 {
-	for m in $(sed 's:.*/::' modules.order | sort | uniq -d)
+	for m in $(sed 's:.*/::' $1 | sort | uniq -d)
 	do
-		echo "warning: same module names found:" >&2
+		echo "error: the following would cause module name conflict:" >&2
 		sed -n "/\/$m/s:^:  :p" modules.order >&2
+		exit_code=1
 	done
 }
 
-check_same_name_modules
+check_same_name_modules "$1"
+
+exit $exit_code
