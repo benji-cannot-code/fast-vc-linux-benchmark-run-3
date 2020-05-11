13cde8dd94a66175d61c69687040e70b779e897b
Amit Kucheria <amit.kucheria@linaro.org>
2020-05-11 17:54:50 +0530

    thermal/core: Replace module.h with export.h
    Thermal core cannot be modular, remove the unnecessary module.h include
    and replace with export.h to handle EXPORT_SYMBOL family of macros.
    
    Signed-off-by: Amit Kucheria <amit.kucheria@linaro.org>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/33af23406dcdb0c62dae1e6401446b997ccb449f.1589199124.git.amit.kucheria@linaro.org

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#include <linux/module.h>
 #include <linux/device.h>
 #include <linux/err.h>
+#include <linux/export.h>
 #include <linux/slab.h>
 #include <linux/kdev_t.h>
 #include <linux/idr.h>
