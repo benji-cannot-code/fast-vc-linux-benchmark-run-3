d9d5266888f2413d425b45591aecdd82c251bfae
Adrian Hunter <adrian.hunter@intel.com>
2020-05-12 15:19:19 +0300

    perf tools: Add support for PERF_RECORD_KSYMBOL_TYPE_OOL
    PERF_RECORD_KSYMBOL_TYPE_OOL marks an executable page. Create a map
    backed only by memory, which will be populated as necessary by text poke
    events.
    
    Committer notes:
    
    From the patch:
    
    OOL stands for "Out of line" code such as kprobe-replaced instructions
    or optimized kprobes or ftrace trampolines.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Leo Yan <leo.yan@linaro.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Mathieu Poirier <mathieu.poirier@linaro.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Cc: x86@kernel.org
    Link: http://lore.kernel.org/lkml/20200512121922.8997-13-adrian.hunter@intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/symbol.c
+++ tools/perf/util/symbol.c
@@ -1566,6 +1566,7 @@ static bool dso__is_compatible_symtab_type(struct dso *dso, bool kmod,
 
 	case DSO_BINARY_TYPE__BPF_PROG_INFO:
 	case DSO_BINARY_TYPE__BPF_IMAGE:
+	case DSO_BINARY_TYPE__OOL:
 	case DSO_BINARY_TYPE__NOT_FOUND:
 	default:
 		return false;
