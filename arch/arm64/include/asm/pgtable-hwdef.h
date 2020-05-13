65763c4f8cf2e11469034b7584e8bb2ac0f85293
Bhupesh Sharma <bhsharma@redhat.com>
2020-05-14 00:22:37 +0530

    arm64/crash_core: Export TCR_EL1.T1SZ in vmcoreinfo
    TCR_EL1.TxSZ, which controls the VA space size, is configured by a
    single kernel image to support either 48-bit or 52-bit VA space.
    
    If the ARMv8.2-LVA optional feature is present and we are running
    with a 64KB page size, then it is possible to use 52-bits of address
    space for both userspace and kernel addresses. However, any kernel
    binary that supports 52-bit must also be able to fall back to 48-bit
    at early boot time if the hardware feature is not present.
    
    Since TCR_EL1.T1SZ indicates the size of the memory region addressed by
    TTBR1_EL1, export the same in vmcoreinfo. User-space utilities like
    makedumpfile and crash-utility need to read this value from vmcoreinfo
    for determining if a virtual address lies in the linear map range.
    
    While at it also add documentation for TCR_EL1.T1SZ variable being
    added to vmcoreinfo.
    
    It indicates the size offset of the memory region addressed by
    TTBR1_EL1.
    
    Signed-off-by: Bhupesh Sharma <bhsharma@redhat.com>
    Tested-by: John Donnelly <john.p.donnelly@oracle.com>
    Tested-by: Kamlakant Patel <kamlakantp@marvell.com>
    Tested-by: Amit Daniel Kachhap <amit.kachhap@arm.com>
    Reviewed-by: James Morse <james.morse@arm.com>
    Reviewed-by: Amit Daniel Kachhap <amit.kachhap@arm.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Steve Capper <steve.capper@arm.com>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Dave Anderson <anderson@redhat.com>
    Cc: Kazuhito Hagio <k-hagio@ab.jp.nec.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-kernel@vger.kernel.org
    Cc: kexec@lists.infradead.org
    Link: https://lore.kernel.org/r/1589395957-24628-3-git-send-email-bhsharma@redhat.com
    [catalin.marinas@arm.com: removed vabits_actual from the commit log]
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/pgtable-hwdef.h
+++ arch/arm64/include/asm/pgtable-hwdef.h
@@ -217,6 +217,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TCR_TxSZ(x)		(TCR_T0SZ(x) | TCR_T1SZ(x))
 #define TCR_TxSZ_WIDTH		6
 #define TCR_T0SZ_MASK		(((UL(1) << TCR_TxSZ_WIDTH) - 1) << TCR_T0SZ_OFFSET)
+#define TCR_T1SZ_MASK		(((UL(1) << TCR_TxSZ_WIDTH) - 1) << TCR_T1SZ_OFFSET)
 
 #define TCR_EPD0_SHIFT		7
 #define TCR_EPD0_MASK		(UL(1) << TCR_EPD0_SHIFT)
