765499f5a63f160b91e939da604775533b1e49fd
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2020-05-14 18:59:38 +0200

    net: phy: mdio-moxart: remove unneeded include
    mdio-moxart doesn't use regulators in the driver code. We can remove
    the regulator include.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/mdio-moxart.c
+++ drivers/net/phy/mdio-moxart.c
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_mdio.h>
 #include <linux/phy.h>
 #include <linux/platform_device.h>
-#include <linux/regulator/consumer.h>
 
 #define REG_PHY_CTRL            0
 #define REG_PHY_WRITE_DATA      4
