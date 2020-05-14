d60fbf876fa2d3186289913be2a85f86b9a04b34
Sergey Senozhatsky <senozhatsky@chromium.org>
2020-05-14 18:01:45 +0200

    media: videobuf2: handle V4L2_FLAG_MEMORY_NON_CONSISTENT flag
    This patch lets user-space to request a non-consistent memory
    allocation during CREATE_BUFS and REQBUFS ioctl calls.
    
    = CREATE_BUFS
    
      struct v4l2_create_buffers has seven 4-byte reserved areas,
      so reserved[0] is renamed to ->flags. The struct, thus, now
      has six reserved 4-byte regions.
    
    = CREATE_BUFS32
    
      struct v4l2_create_buffers32 has seven 4-byte reserved areas,
      so reserved[0] is renamed to ->flags. The struct, thus, now
      has six reserved 4-byte regions.
    
    = REQBUFS
    
     We use one bit of a ->reserved[1] member of struct v4l2_requestbuffers,
     which is now renamed to ->flags. Unlike v4l2_create_buffers, struct
     v4l2_requestbuffers does not have enough reserved room. Therefore for
     backward compatibility  ->reserved and ->flags were put into anonymous
     union.
    
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -2039,9 +2039,6 @@ static int v4l_reqbufs(const struct v4l2_ioctl_ops *ops,
 
 	if (ret)
 		return ret;
-
-	CLEAR_AFTER_FIELD(p, capabilities);
-
 	return ops->vidioc_reqbufs(file, fh, p);
 }
 
@@ -2081,7 +2078,7 @@ static int v4l_create_bufs(const struct v4l2_ioctl_ops *ops,
 	if (ret)
 		return ret;
 
-	CLEAR_AFTER_FIELD(create, capabilities);
+	CLEAR_AFTER_FIELD(create, flags);
 
 	v4l_sanitize_format(&create->format);
 
