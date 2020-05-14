37388070c0a5b1941b14e0ebcfe079e62d5514a9
Jesper Dangaard Brouer <brouer@redhat.com>
2020-05-14 12:50:13 +0200

    ena: Add XDP frame size to amazon NIC driver
    Frame size ENA_PAGE_SIZE is limited to 16K on systems with larger
    PAGE_SIZE than 16K. Change ENA_XDP_MAX_MTU to also take into account
    the reserved tailroom.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Sameeh Jubran <sameehj@amazon.com>
    Cc: Arthur Kiyanovski <akiyano@amazon.com>
    Link: https://lore.kernel.org/bpf/158945341384.97035.907403694833419456.stgit@firesoul

--- drivers/net/ethernet/amazon/ena/ena_netdev.h
+++ drivers/net/ethernet/amazon/ena/ena_netdev.h
@@ -152,8 +152,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * The buffer size we share with the device is defined to be ENA_PAGE_SIZE
  */
 
-#define ENA_XDP_MAX_MTU (ENA_PAGE_SIZE - ETH_HLEN - ETH_FCS_LEN - \
-				VLAN_HLEN - XDP_PACKET_HEADROOM)
+#define ENA_XDP_MAX_MTU (ENA_PAGE_SIZE - ETH_HLEN - ETH_FCS_LEN -	\
+			 VLAN_HLEN - XDP_PACKET_HEADROOM -		\
+			 SKB_DATA_ALIGN(sizeof(struct skb_shared_info)))
 
 #define ENA_IS_XDP_INDEX(adapter, index) (((index) >= (adapter)->xdp_first_ring) && \
 	((index) < (adapter)->xdp_first_ring + (adapter)->xdp_num_queues))
