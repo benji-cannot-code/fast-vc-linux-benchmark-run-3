9695923924ccd52c64a786ddfd10164fd0a1356f
Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>
2020-05-14 10:48:51 +0300

    drm/i915: Add TGL+ SAGV support
    Starting from TGL we need to have a separate wm0
    values for SAGV and non-SAGV which affects
    how calculations are done.
    
    v2: Remove long lines
    v3: Removed COLOR_PLANE enum references
    v4, v5, v6: Fixed rebase conflict
    v7: - Removed skl_plane_wm_level accessor from skl_allocate_pipe_ddb(Ville)
        - Removed sagv_uv_wm0(Ville)
        - can_sagv->use_sagv_wm(Ville)
    
    v8: - Moved tgl_crtc_can_enable_sagv function up(Ville)
        - Changed comment regarding pipe_wm usage(Ville)
        - Call intel_can_enable_sagv and tgl_compute_sagv_wm only
          for Gen12(Ville)
        - Some sagv debugs removed(Ville)
        - skl_print_wm_changes improvements(Ville)
        - Do assignment instead of memcpy in
          skl_pipe_wm_get_hw_state(Ville)
    
    v9: - Removed can_sagv variable(Ville)
        - Removed spurious line(Ville)
        - Changed u32 to unsigned int as agreed(Ville)
        - Assign sagv only for gen12 in
          skl_pipe_wm_get_hw_state(Ville)
    
    Signed-off-by: Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>
    [vsyrjala: Remove the dead 'return false' from intel_crtc_can_enable_sagv()]
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200514074853.9508-2-stanislav.lisovskiy@intel.com

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -14026,7 +14026,9 @@ static void verify_wm_state(struct intel_crtc *crtc,
 		/* Watermarks */
 		for (level = 0; level <= max_level; level++) {
 			if (skl_wm_level_equals(&hw_plane_wm->wm[level],
-						&sw_plane_wm->wm[level]))
+						&sw_plane_wm->wm[level]) ||
+			    (level == 0 && skl_wm_level_equals(&hw_plane_wm->wm[level],
+							       &sw_plane_wm->sagv_wm0)))
 				continue;
 
 			drm_err(&dev_priv->drm,
@@ -14081,7 +14083,9 @@ static void verify_wm_state(struct intel_crtc *crtc,
 		/* Watermarks */
 		for (level = 0; level <= max_level; level++) {
 			if (skl_wm_level_equals(&hw_plane_wm->wm[level],
-						&sw_plane_wm->wm[level]))
+						&sw_plane_wm->wm[level]) ||
+			    (level == 0 && skl_wm_level_equals(&hw_plane_wm->wm[level],
+							       &sw_plane_wm->sagv_wm0)))
 				continue;
 
 			drm_err(&dev_priv->drm,
