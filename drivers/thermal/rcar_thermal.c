7acce597352efab163577e313585a35785a6b912
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2020-05-14 17:25:05 +0200

    thermal: rcar_thermal: Clean up rcar_thermal_update_temp()
    Moving the ctemp variable out of the private data structure made it
    possible to clean up rcar_thermal_update_temp(). Initialize the local
    ctemp to the error code to return if the reading fails and just return
    it at the end of the function.
    
    It's OK to change the datatype of old, new and ctemp to int as all
    values are ANDed with CTEMP (0x3f) before being stored. While at it
    change the datatype of the loop variable 'i' to to unsigned int.
    
    Suggested-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/20200514152505.1927634-1-niklas.soderlund+renesas@ragnatech.se

--- drivers/thermal/rcar_thermal.c
+++ drivers/thermal/rcar_thermal.c
@@ -199,8 +199,8 @@ static void _rcar_thermal_bset(struct rcar_thermal_priv *priv, u32 reg,
 static int rcar_thermal_update_temp(struct rcar_thermal_priv *priv)
 {
 	struct device *dev = rcar_priv_to_dev(priv);
-	int i;
-	u32 ctemp, old, new;
+	int old, new, ctemp = -EINVAL;
+	unsigned int i;
 
 	mutex_lock(&priv->lock);
 
@@ -210,7 +210,6 @@ static int rcar_thermal_update_temp(struct rcar_thermal_priv *priv)
 	 */
 	rcar_thermal_bset(priv, THSCR, CPCTL, CPCTL);
 
-	ctemp = 0;
 	old = ~0;
 	for (i = 0; i < 128; i++) {
 		/*
@@ -228,7 +227,7 @@ static int rcar_thermal_update_temp(struct rcar_thermal_priv *priv)
 		old = new;
 	}
 
-	if (!ctemp) {
+	if (ctemp < 0) {
 		dev_err(dev, "thermal sensor was broken\n");
 		goto err_out_unlock;
 	}
@@ -249,7 +248,7 @@ static int rcar_thermal_update_temp(struct rcar_thermal_priv *priv)
 err_out_unlock:
 	mutex_unlock(&priv->lock);
 
-	return ctemp ? ctemp : -EINVAL;
+	return ctemp;
 }
 
 static int rcar_thermal_get_current_temp(struct rcar_thermal_priv *priv,
