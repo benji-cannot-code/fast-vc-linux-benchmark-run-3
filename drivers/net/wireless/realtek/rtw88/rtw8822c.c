ac4efd03f98d9562f15aae8e3399be04d21f0f0d
Zong-Zhe Yang <kevin_yang@realtek.com>
2020-05-15 13:23:23 +0800

    rtw88: extract: make 8822c an individual kernel module
    Make objects about 8822c functions and 8822c tables,
    i.e. rtw8822c.o and rtw8822c_table.o, an individual
    kernel module called rtw88_8822c.ko.
    
    For 8822c pcie chip, i.e. 8822CE chip, add a chip
    entry point module called rtw88_8822ce.ko which
    will depend on rtw88_8822c.ko and rtwpci.ko.
    
    Signed-off-by: Zong-Zhe Yang <kevin_yang@realtek.com>
    Signed-off-by: Yan-Hsuan Chuang <yhchuang@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200515052327.31874-4-yhchuang@realtek.com

--- drivers/net/wireless/realtek/rtw88/rtw8822c.c
+++ drivers/net/wireless/realtek/rtw88/rtw8822c.c
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Copyright(c) 2018-2019  Realtek Corporation
  */
 
+#include <linux/module.h>
 #include "main.h"
 #include "coex.h"
 #include "fw.h"
@@ -4378,3 +4379,7 @@ EXPORT_SYMBOL(rtw8822c_hw_spec);
 
 MODULE_FIRMWARE("rtw88/rtw8822c_fw.bin");
 MODULE_FIRMWARE("rtw88/rtw8822c_wow_fw.bin");
+
+MODULE_AUTHOR("Realtek Corporation");
+MODULE_DESCRIPTION("Realtek 802.11ac wireless 8822c driver");
+MODULE_LICENSE("Dual BSD/GPL");
