d346ae5ba73a1050be0b467c067e71490292a12e
Thomas Zimmermann <tzimmermann@suse.de>
2020-05-15 10:32:19 +0200

    drm/mgag200: Remove HW cursor
    The HW cursor of Matrox G200 cards only supports a 16-color palette
    format. Univeral planes require at least ARGB or a similar component-
    based format, so remove the HW cursor.
    
    Alternatively, the driver could dither a cursor image from ARGB to
    16 colors. But this does not produce pleasent-looking results in
    general, so it's useless for modern compositors.
    
    Without HW support, compositors will use software rendering.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Tested-by: John Donnelly <John.p.donnelly@oracle.com>
    Acked-by: Sam Ravnborg <sam@ravnborg.org>
    Acked-by: Emil Velikov <emil.velikov@collabora.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200515083233.32036-2-tzimmermann@suse.de

--- drivers/gpu/drm/mgag200/mgag200_main.c
+++ drivers/gpu/drm/mgag200/mgag200_main.c
@@ -136,10 +136,6 @@ int mgag200_driver_load(struct drm_device *dev, unsigned long flags)
 		goto err_mgag200_mm_fini;
 	}
 
-	ret = mgag200_cursor_init(mdev);
-	if (ret)
-		drm_err(dev, "Could not initialize cursors. Not doing hardware cursors.\n");
-
 	return 0;
 
 err_mgag200_mm_fini:
@@ -155,7 +151,6 @@ void mgag200_driver_unload(struct drm_device *dev)
 
 	if (mdev == NULL)
 		return;
-	mgag200_cursor_fini(mdev);
 	mgag200_mm_fini(mdev);
 	dev->dev_private = NULL;
 }
