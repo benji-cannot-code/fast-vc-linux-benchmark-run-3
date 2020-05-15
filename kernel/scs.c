576acf65f9e11d8d6ddb8250145cbb15a3bbc45f
Will Deacon <will@kernel.org>
2020-05-15 16:15:46 +0100

    scs: Remove references to asm/scs.h from core code
    asm/scs.h is no longer needed by the core code, so remove a redundant
    header inclusion and update the stale Kconfig text.
    
    Tested-by: Sami Tolvanen <samitolvanen@google.com>
    Reviewed-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Will Deacon <will@kernel.org>

--- kernel/scs.c
+++ kernel/scs.c
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/scs.h>
 #include <linux/slab.h>
 #include <linux/vmstat.h>
-#include <asm/scs.h>
 
 static struct kmem_cache *scs_cache;
 
