e8f53dc92ea0bcdd365cad9645857f04011bd740
Lu Baolu <baolu.lu@linux.intel.com>
2020-05-16 14:20:57 +0800

    iommu/vt-d: Disable non-recoverable fault processing before unbind
    When a PASID is used for SVA by the device, it's possible that the PASID
    entry is cleared before the device flushes all ongoing DMA requests. The
    IOMMU should tolerate and ignore the non-recoverable faults caused by the
    untranslated requests from this device.
    
    For example, when an exception happens, the process terminates before the
    device driver stops DMA and call IOMMU driver to unbind PASID. The flow
    of process exist is as follows:
    
    do_exit() {
         exit_mm() {
                 mm_put();
                 exit_mmap() {
                         intel_invalidate_range() //mmu notifier
                         tlb_finish_mmu()
                         mmu_notifier_release(mm) {
                                 intel_iommu_release() {
    [2]                                  intel_iommu_teardown_pasid();
                                         intel_iommu_flush_tlbs();
                                 }
                         }
                         unmap_vmas();
                         free_pgtables();
                 };
         }
         exit_files(tsk) {
                 close_files() {
                         dsa_close();
    [1]                  dsa_stop_dma();
                         intel_svm_unbind_pasid();
                 }
         }
    }
    
    Care must be taken on VT-d to avoid unrecoverable faults between the time
    window of [1] and [2]. [Process exist flow was contributed by Jacob Pan.]
    
    Intel VT-d provides such function through the FPD bit of the PASID entry.
    This sets FPD bit when PASID entry is changing from present to nonpresent
    in the mm notifier and will clear it when the pasid is unbound.
    
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Link: https://lore.kernel.org/r/20200516062101.29541-15-baolu.lu@linux.intel.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- drivers/iommu/intel-svm.c
+++ drivers/iommu/intel-svm.c
@@ -208,7 +208,8 @@ static void intel_mm_release(struct mmu_notifier *mn, struct mm_struct *mm)
 	 */
 	rcu_read_lock();
 	list_for_each_entry_rcu(sdev, &svm->devs, list) {
-		intel_pasid_tear_down_entry(svm->iommu, sdev->dev, svm->pasid);
+		intel_pasid_tear_down_entry(svm->iommu, sdev->dev,
+					    svm->pasid, true);
 		intel_flush_svm_range_dev(svm, sdev, 0, -1, 0);
 	}
 	rcu_read_unlock();
@@ -397,7 +398,8 @@ int intel_svm_unbind_gpasid(struct device *dev, int pasid)
 			sdev->users--;
 		if (!sdev->users) {
 			list_del_rcu(&sdev->list);
-			intel_pasid_tear_down_entry(iommu, dev, svm->pasid);
+			intel_pasid_tear_down_entry(iommu, dev,
+						    svm->pasid, false);
 			intel_flush_svm_range_dev(svm, sdev, 0, -1, 0);
 			/* TODO: Drain in flight PRQ for the PASID since it
 			 * may get reused soon, we don't want to
@@ -640,7 +642,8 @@ int intel_svm_unbind_mm(struct device *dev, int pasid)
 			 * to use. We have a *shared* PASID table, because it's
 			 * large and has to be physically contiguous. So it's
 			 * hard to be as defensive as we might like. */
-			intel_pasid_tear_down_entry(iommu, dev, svm->pasid);
+			intel_pasid_tear_down_entry(iommu, dev,
+						    svm->pasid, false);
 			intel_flush_svm_range_dev(svm, sdev, 0, -1, 0);
 			kfree_rcu(sdev, rcu);
 
