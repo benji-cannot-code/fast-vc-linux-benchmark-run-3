f646857e7856ee3e4832073b724bae0d780d8d60
Takashi Iwai <tiwai@suse.de>
2020-05-16 08:28:54 +0200

    ALSA: hda: Unexport some local helper functions
    snd_hdac_bus_queue_event() and snd_hdac_bus_exec_verb() are used only
    internally in HD-audio core.  Let's drop the exports and move the
    declarations into local.h.
    
    Link: https://lore.kernel.org/r/20200516062854.22141-3-tiwai@suse.de
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/hda/hdac_controller.c
+++ sound/hda/hdac_controller.c
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/core.h>
 #include <sound/hdaudio.h>
 #include <sound/hda_register.h>
+#include "local.h"
 
 /* clear CORB read pointer properly */
 static void azx_clear_corbrp(struct hdac_bus *bus)
