6d2c79e0c4a840198b72c936b562c32a8b799eef
Vijendar Mukunda <Vijendar.Mukunda@amd.com>
2020-05-19 01:17:02 +0800

    ASoC: amd: create platform devices for Renoir
    Create platform devices for generic dmic codec driver
    and machine driver.
    These platform devices required for creation of sound card.
    
    Signed-off-by: Vijendar Mukunda <Vijendar.Mukunda@amd.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20200518171704.24999-13-Vijendar.Mukunda@amd.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/amd/renoir/rn_acp3x.h
+++ sound/soc/amd/renoir/rn_acp3x.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "rn_chip_offset_byte.h"
 
+#define ACP_DEVS		3
 #define ACP_PHY_BASE_ADDRESS 0x1240000
 #define	ACP_REG_START	0x1240000
 #define	ACP_REG_END	0x1250200
