516a66053e3a8a824e2c364dccb767eda2609cc1
Christophe Leroy <christophe.leroy@csgroup.eu>
2020-05-19 05:49:13 +0000

    powerpc/8xx: Add function to set pinned TLBs
    Pinned TLBs cannot be modified when the MMU is enabled.
    
    Create a function to rewrite the pinned TLB entries with MMU off.
    
    To set pinned TLB, we have to turn off MMU, disable pinning,
    do a TLB flush (Either with tlbie and tlbia) then reprogam
    the TLB entries, enable pinning and turn on MMU.
    
    If using tlbie, it cleared entries in both instruction and data
    TLB regardless whether pinning is disabled or not.
    If using tlbia, it clears all entries of the TLB which has
    disabled pinning.
    
    To make it easy, just clear all entries in both TLBs, and
    reprogram them.
    
    The function takes two arguments, the top of the memory to
    consider and whether data is RO under _sinittext.
    When DEBUG_PAGEALLOC is set, the top is the end of kernel rodata.
    Otherwise, that's the top of physical RAM.
    
    Everything below _sinittext is set RX, over _sinittext that's RW.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/c17806014bb1c06513ad1e1d510faea31984b177.1589866984.git.christophe.leroy@csgroup.eu

--- arch/powerpc/include/asm/nohash/32/mmu-8xx.h
+++ arch/powerpc/include/asm/nohash/32/mmu-8xx.h
@@ -194,6 +194,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/mmdebug.h>
 
+void mmu_pin_tlb(unsigned long top, bool readonly);
+
 typedef struct {
 	unsigned int id;
 	unsigned int active;
