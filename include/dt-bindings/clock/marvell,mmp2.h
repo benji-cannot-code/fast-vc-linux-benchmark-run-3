83247134f070b40f49db341f93861bddf1b58a1d
Lubomir Rintel <lkundrak@v3.sk>
2020-05-20 00:41:41 +0200

    dt-bindings: marvell,mmp2: Add clock id for the I2S clocks
    There are two of these on a MMP2.
    
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>
    Acked-by: Rob Herring <robh@kernel.org>
    Link: https://lkml.kernel.org/r/20200519224151.2074597-4-lkundrak@v3.sk
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- include/dt-bindings/clock/marvell,mmp2.h
+++ include/dt-bindings/clock/marvell,mmp2.h
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MMP3_CLK_PLL1_P			28
 #define MMP3_CLK_PLL2_P			29
 #define MMP3_CLK_PLL3			30
+#define MMP2_CLK_I2S0			31
+#define MMP2_CLK_I2S1			32
 
 /* apb periphrals */
 #define MMP2_CLK_TWSI0			60
