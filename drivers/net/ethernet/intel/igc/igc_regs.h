961f5a382cc19a593d44e629f9425937a67e4c77
Sasha Neftin <sasha.neftin@intel.com>
2020-05-19 17:36:34 +0300

    igc: Remove symbol error counter
    Accordance to the i225 datasheet symbol error counter does not
    applicable to the i225 device.
    This patch comes to clean up this counter.
    
    Signed-off-by: Sasha Neftin <sasha.neftin@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/igc/igc_regs.h
+++ drivers/net/ethernet/intel/igc/igc_regs.h
@@ -128,7 +128,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Statistics Register Descriptions */
 #define IGC_CRCERRS	0x04000  /* CRC Error Count - R/clr */
 #define IGC_ALGNERRC	0x04004  /* Alignment Error Count - R/clr */
-#define IGC_SYMERRS	0x04008  /* Symbol Error Count - R/clr */
 #define IGC_RXERRC	0x0400C  /* Receive Error Count - R/clr */
 #define IGC_MPC		0x04010  /* Missed Packet Count - R/clr */
 #define IGC_SCC		0x04014  /* Single Collision Count - R/clr */
