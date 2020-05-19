c75704354bfc63b06f7daea59708438c280ec9fc
Lubomir Rintel <lkundrak@v3.sk>
2020-05-20 00:41:42 +0200

    dt-bindings: marvell,mmp2: Add clock id for the Audio clock
    This clocks the Audio block.
    
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>
    Acked-by: Rob Herring <robh@kernel.org>
    Link: https://lkml.kernel.org/r/20200519224151.2074597-5-lkundrak@v3.sk
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- include/dt-bindings/clock/marvell,mmp2.h
+++ include/dt-bindings/clock/marvell,mmp2.h
@@ -90,6 +90,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MMP3_CLK_GPU_3D			MMP2_CLK_GPU_3D
 #define MMP3_CLK_GPU_2D			125
 #define MMP3_CLK_SDH4			126
+#define MMP2_CLK_AUDIO			127
 
 #define MMP2_NR_CLKS			200
 #endif
