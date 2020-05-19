e67df44289916abeb3a86d6ada0538f5ac6cd27f
Christoph Hellwig <hch@lst.de>
2020-05-19 19:21:07 +0100

    firmware: arm_sdei: remove unused interfaces
    The export symbols to register/unregister and enable/disable events
    aren't used upstream, remove them.
    
    [ dropped the parts of Christoph's patch that made the API static too ]
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: James Morse <james.morse@arm.com>
    Link: https://lore.kernel.org/linux-arm-kernel/20200504164224.2842960-1-hch@lst.de/
    Link: https://lore.kernel.org/r/20200519182108.13693-3-james.morse@arm.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- drivers/firmware/arm_sdei.c
+++ drivers/firmware/arm_sdei.c
@@ -430,7 +430,6 @@ int sdei_event_enable(u32 event_num)
 
 	return err;
 }
-EXPORT_SYMBOL(sdei_event_enable);
 
 static int sdei_api_event_disable(u32 event_num)
 {
@@ -472,7 +471,6 @@ int sdei_event_disable(u32 event_num)
 
 	return err;
 }
-EXPORT_SYMBOL(sdei_event_disable);
 
 static int sdei_api_event_unregister(u32 event_num)
 {
@@ -534,7 +532,6 @@ int sdei_event_unregister(u32 event_num)
 
 	return err;
 }
-EXPORT_SYMBOL(sdei_event_unregister);
 
 /*
  * unregister events, but don't destroy them as they are re-registered by
@@ -644,7 +641,6 @@ int sdei_event_register(u32 event_num, sdei_event_callback *cb, void *arg)
 
 	return err;
 }
-EXPORT_SYMBOL(sdei_event_register);
 
 static int sdei_reregister_event_llocked(struct sdei_event *event)
 {
