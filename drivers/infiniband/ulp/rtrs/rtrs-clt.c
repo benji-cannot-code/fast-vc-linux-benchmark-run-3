0593313a5652a4b88038040bc82cb0245bfb3e2a
Gustavo A. R. Silva <gustavoars@kernel.org>
2020-05-19 11:36:12 -0500

    RDMA/rtrs: client: Fix function return on success
    Remove the if-statement and return the value contained in _err_,
    unconditionally.
    
    Link: https://lore.kernel.org/r/20200519163612.GA6043@embeddedor
    Addresses-Coverity-ID: 1493753 ("Identical code for different branches")
    Fixes: 6a98d71daea1 ("RDMA/rtrs: client: main functionality")
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

--- drivers/infiniband/ulp/rtrs/rtrs-clt.c
+++ drivers/infiniband/ulp/rtrs/rtrs-clt.c
@@ -1591,9 +1591,6 @@ static int create_con_cq_qp(struct rtrs_clt_con *con)
 	 * In case of error we do not bother to clean previous allocations,
 	 * since destroy_con_cq_qp() must be called.
 	 */
-
-	if (err)
-		return err;
 	return err;
 }
 
