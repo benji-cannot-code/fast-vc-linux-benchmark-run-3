bdf615e017c5b3199c382c56a3ab7e7f2bdcfe1e
Björn Töpel <bjorn.topel@intel.com>
2020-05-20 21:20:52 +0200

    xsk: Move defines only used by AF_XDP internals to xsk.h
    Move the XSK_NEXT_PG_CONTIG_{MASK,SHIFT}, and
    XDP_UMEM_USES_NEED_WAKEUP defines from xdp_sock.h to the AF_XDP
    internal xsk.h file. Also, start using the BIT{,_ULL} macro instead of
    explicit shifts.
    
    Signed-off-by: Björn Töpel <bjorn.topel@intel.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200520192103.355233-5-bjorn.topel@gmail.com

--- net/xdp/xsk_queue.h
+++ net/xdp/xsk_queue.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_xdp.h>
 #include <net/xdp_sock.h>
 
+#include "xsk.h"
+
 struct xdp_ring {
 	u32 producer ____cacheline_aligned_in_smp;
 	u32 consumer ____cacheline_aligned_in_smp;
