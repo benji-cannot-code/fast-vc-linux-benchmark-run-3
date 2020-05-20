621469c6821370c2e18445009fe6c918b6a81b03
Chuck Lever <chuck.lever@oracle.com>
2020-05-20 17:30:24 -0400

    SUNRPC: Restructure svc_udp_recvfrom()
    Clean up. At this point, we are not ready yet to support bio_vecs in
    the UDP transport implementation. However, we can clean up
    svc_udp_recvfrom() to match the tracing and straight-lining recently
    changes made in svc_tcp_recvfrom().
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- include/trace/events/sunrpc.h
+++ include/trace/events/sunrpc.h
@@ -1502,6 +1502,8 @@ DECLARE_EVENT_CLASS(svcsock_class,
 			TP_ARGS(xprt, result))
 
 DEFINE_SVCSOCK_EVENT(udp_send);
+DEFINE_SVCSOCK_EVENT(udp_recv);
+DEFINE_SVCSOCK_EVENT(udp_recv_err);
 DEFINE_SVCSOCK_EVENT(tcp_send);
 DEFINE_SVCSOCK_EVENT(tcp_recv);
 DEFINE_SVCSOCK_EVENT(tcp_recv_eagain);
