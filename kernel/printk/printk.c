00a6adbaf394397ccef75e39a5014127708fb80a
Petr Mladek <pmladek@suse.com>
2020-05-20 12:22:33 +0200

    printk/kdb: Redirect printk messages into kdb in any context
    kdb has to get messages on consoles even when the system is stopped.
    It uses kdb_printf() internally and calls console drivers on its own.
    
    It uses a hack to reuse an existing code. It sets "kdb_trap_printk"
    global variable to redirect even the normal printk() into the
    kdb_printf() variant.
    
    The variable "kdb_trap_printk" is checked in printk_default() and
    it is ignored when printk is redirected to printk_safe in NMI context.
    Solve this by moving the check into printk_func().
    
    It is obvious that it is not fully safe. But it does not make things
    worse. The console drivers are already called in this context by
    db_printf() direct calls.
    
    Reported-by: Sumit Garg <sumit.garg@linaro.org>
    Tested-by: Sumit Garg <sumit.garg@linaro.org>
    Reviewed-by: Daniel Thompson <daniel.thompson@linaro.org>
    Acked-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Signed-off-by: Petr Mladek <pmladek@suse.com>
    Link: https://lore.kernel.org/r/20200520102233.GC3464@linux-b0ei

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/memblock.h>
 #include <linux/syscalls.h>
 #include <linux/crash_core.h>
-#include <linux/kdb.h>
 #include <linux/ratelimit.h>
 #include <linux/kmsg_dump.h>
 #include <linux/syslog.h>
@@ -2048,18 +2047,7 @@ EXPORT_SYMBOL(vprintk);
 
 int vprintk_default(const char *fmt, va_list args)
 {
-	int r;
-
-#ifdef CONFIG_KGDB_KDB
-	/* Allow to pass printk() to kdb but avoid a recursion. */
-	if (unlikely(kdb_trap_printk && kdb_printf_cpu < 0)) {
-		r = vkdb_printf(KDB_MSGSRC_PRINTK, fmt, args);
-		return r;
-	}
-#endif
-	r = vprintk_emit(0, LOGLEVEL_DEFAULT, NULL, 0, fmt, args);
-
-	return r;
+	return vprintk_emit(0, LOGLEVEL_DEFAULT, NULL, 0, fmt, args);
 }
 EXPORT_SYMBOL_GPL(vprintk_default);
 
