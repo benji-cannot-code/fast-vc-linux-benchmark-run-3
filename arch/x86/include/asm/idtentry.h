77cbb67918c6ed172a2dbed65f762bff21508a58
Thomas Gleixner <tglx@linutronix.de>
2020-05-21 22:05:17 +0200

    x86/entry: Provide idtentry_entry/exit_cond_rcu()
    After a lengthy discussion [1] it turned out that RCU does not need a full
    rcu_irq_enter/exit() when RCU is already watching. All it needs if
    NOHZ_FULL is active is to check whether the tick needs to be restarted.
    
    This allows to avoid a separate variant for the pagefault handler which
    cannot invoke rcu_irq_enter() on a kernel pagefault which might sleep.
    
    The cond_rcu argument is only temporary and will be removed once the
    existing users of idtentry_enter/exit() have been cleaned up. After that
    the code can be significantly simplified.
    
    [ mingo: Simplified the control flow ]
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: "Paul E. McKenney" <paulmck@kernel.org>
    Acked-by: Andy Lutomirski <luto@kernel.org>
    Link: [1] https://lkml.kernel.org/r/20200515235125.628629605@linutronix.de
    Link: https://lore.kernel.org/r/20200521202117.181397835@linutronix.de

--- arch/x86/include/asm/idtentry.h
+++ arch/x86/include/asm/idtentry.h
@@ -8,8 +8,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 
-void idtentry_enter(struct pt_regs *regs);
-void idtentry_exit(struct pt_regs *regs);
+bool idtentry_enter_cond_rcu(struct pt_regs *regs, bool cond_rcu);
+void idtentry_exit_cond_rcu(struct pt_regs *regs, bool rcu_exit);
+
+static __always_inline void idtentry_enter(struct pt_regs *regs)
+{
+	idtentry_enter_cond_rcu(regs, false);
+}
+
+static __always_inline void idtentry_exit(struct pt_regs *regs)
+{
+	idtentry_exit_cond_rcu(regs, true);
+}
 
 /**
  * DECLARE_IDTENTRY - Declare functions for simple IDT entry points
