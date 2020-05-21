771c5783297e6e2fb988d74e2e704971dbc81e73
Sumeet Pawnikar <sumeet.r.pawnikar@intel.com>
2020-05-21 12:14:26 +0530

    powercap: RAPL: remove unused local MSR define
    Remove unused PLATFORM_POWER_LIMIT MSR local definition from file
    intel_rapl_common.c. This was missed while splitting old RAPL code
    intel_rapl.c file into two new files intel_rapl_msr.c and
    intel_rapl_common.c as per the commit 3382388d7148
    ("intel_rapl: abstract RAPL common code"). Currently, this #define
    entry is being used only in intel_rapl_msr.c file and local definition
    present in this file.
    
    Signed-off-by: Sumeet Pawnikar <sumeet.r.pawnikar@intel.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/powercap/intel_rapl_common.c
+++ drivers/powercap/intel_rapl_common.c
@@ -27,9 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cpu_device_id.h>
 #include <asm/intel-family.h>
 
-/* Local defines */
-#define MSR_PLATFORM_POWER_LIMIT	0x0000065C
-
 /* bitmasks for RAPL MSRs, used by primitive access functions */
 #define ENERGY_STATUS_MASK      0xffffffff
 
