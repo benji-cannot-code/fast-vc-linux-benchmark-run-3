ea459f8db046394a7147b762916f457610c1fefb
Jordan Crouse <jcrouse@codeaurora.org>
2020-05-22 16:03:16 -0600

    drm/msm: Update the MMU helper function APIs
    Instead of using a bare unsigned type for the length value for map/unmap
    functions pass in a size_t to more correctly match up with the underlying
    APIs.
    
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Tested-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/msm_mmu.h
+++ drivers/gpu/drm/msm/msm_mmu.h
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct msm_mmu_funcs {
 	void (*detach)(struct msm_mmu *mmu);
 	int (*map)(struct msm_mmu *mmu, uint64_t iova, struct sg_table *sgt,
-			unsigned len, int prot);
-	int (*unmap)(struct msm_mmu *mmu, uint64_t iova, unsigned len);
+			size_t len, int prot);
+	int (*unmap)(struct msm_mmu *mmu, uint64_t iova, size_t len);
 	void (*destroy)(struct msm_mmu *mmu);
 };
 
