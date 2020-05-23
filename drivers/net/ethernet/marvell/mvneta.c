df0d710eda52cdf6f78d9dd2bfb752b839e28cbb
Sven Auhagen <sven.auhagen@voleatech.de>
2020-05-23 12:14:08 +0200

    mvneta: MVNETA_SKB_HEADROOM set last 3 bits to zero
    For XDP the MVNETA_SKB_HEADROOM is used as an offset for
    the received data.
    The MVNETA manual states that the last 3 bits assumed to be 0.
    
    This is currently the case but lets make it explicit in the definition
    to prevent future problems.
    
    Signed-off-by: Sven Auhagen <sven.auhagen@voleatech.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/mvneta.c
+++ drivers/net/ethernet/marvell/mvneta.c
@@ -325,7 +325,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	      ETH_HLEN + ETH_FCS_LEN,			     \
 	      cache_line_size())
 
-#define MVNETA_SKB_HEADROOM	max(XDP_PACKET_HEADROOM, NET_SKB_PAD)
+/* Driver assumes that the last 3 bits are 0 */
+#define MVNETA_SKB_HEADROOM	(max(XDP_PACKET_HEADROOM, NET_SKB_PAD) & ~0x7)
 #define MVNETA_SKB_PAD	(SKB_DATA_ALIGN(sizeof(struct skb_shared_info) + \
 			 MVNETA_SKB_HEADROOM))
 #define MVNETA_SKB_SIZE(len)	(SKB_DATA_ALIGN(len) + MVNETA_SKB_PAD)
