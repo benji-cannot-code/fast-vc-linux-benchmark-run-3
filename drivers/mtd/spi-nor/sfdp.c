1c3c8d333e7f0c4e8b0f587743c9ae8eab8044e5
Pratyush Yadav <p.yadav@ti.com>
2020-05-25 14:45:32 +0530

    mtd: spi-nor: sfdp: prepare BFPT parsing for JESD216 rev D
    JESD216 rev D makes BFPT 20 DWORDs. Update the BFPT size define to
    reflect that.
    
    The check for rev A or later compared the BFPT header length with the
    maximum BFPT length, BFPT_DWORD_MAX. Since BFPT_DWORD_MAX was 16, and so
    was the BFPT length for both rev A and B, this check worked fine. But
    now, since BFPT_DWORD_MAX is 20, it means this check will also stop BFPT
    parsing for rev A or B, since their length is 16.
    
    So, instead check for BFPT_DWORD_MAX_JESD216 to stop BFPT parsing for
    the first JESD216 version, and check for BFPT_DWORD_MAX_JESD216B for the
    next two versions.
    
    Signed-off-by: Pratyush Yadav <p.yadav@ti.com>
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- drivers/mtd/spi-nor/sfdp.c
+++ drivers/mtd/spi-nor/sfdp.c
@@ -550,7 +550,7 @@ static int spi_nor_parse_bfpt(struct spi_nor *nor,
 				  SNOR_ERASE_TYPE_MASK;
 
 	/* Stop here if not JESD216 rev A or later. */
-	if (bfpt_header->length < BFPT_DWORD_MAX)
+	if (bfpt_header->length == BFPT_DWORD_MAX_JESD216)
 		return spi_nor_post_bfpt_fixups(nor, bfpt_header, &bfpt,
 						params);
 
@@ -605,6 +605,11 @@ static int spi_nor_parse_bfpt(struct spi_nor *nor,
 	default:
 		return -EINVAL;
 	}
+
+	/* Stop here if not JESD216 rev C or later. */
+	if (bfpt_header->length == BFPT_DWORD_MAX_JESD216B)
+		return spi_nor_post_bfpt_fixups(nor, bfpt_header, &bfpt,
+						params);
 
 	return spi_nor_post_bfpt_fixups(nor, bfpt_header, &bfpt, params);
 }
