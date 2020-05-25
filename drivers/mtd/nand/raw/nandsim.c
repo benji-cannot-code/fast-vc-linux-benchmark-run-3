258d638de0669f3ab165f05684cabc8208712392
Miquel Raynal <miquel.raynal@bootlin.com>
2020-05-25 10:58:47 +0200

    mtd: rawnand: nandsim: Free erase_block_wear on error
    Free erase_block_wear on error, which is allocated by
    ns_setup_wear_reporting().
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Link: https://lore.kernel.org/linux-mtd/20200525085851.17682-14-miquel.raynal@bootlin.com

--- drivers/mtd/nand/raw/nandsim.c
+++ drivers/mtd/nand/raw/nandsim.c
@@ -2384,7 +2384,7 @@ static int __init ns_init_module(void)
 
 	ret = ns_init(nsmtd);
 	if (ret)
-		goto err_exit;
+		goto free_ebw;
 
 	ret = nand_create_bbt(chip);
 	if (ret)
@@ -2410,6 +2410,8 @@ unregister_mtd:
 err_exit:
 free_ns_object:
 	ns_free(ns);
+free_ebw:
+	kfree(erase_block_wear);
 	nand_cleanup(chip);
 error:
 	kfree(ns);
