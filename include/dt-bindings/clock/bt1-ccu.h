161c68da596281ef7a1b9a318f863dcd769a47b8
Serge Semin <Sergey.Semin@baikalelectronics.ru>
2020-05-27 01:20:53 +0300

    dt-bindings: clk: Add Baikal-T1 CCU PLLs binding
    Baikal-T1 Clocks Control Unit is responsible for transformation of a
    signal coming from an external oscillator into clocks of various
    frequencies to propagate them then to the corresponding clocks
    consumers (either individual IP-blocks or clock domains). In order
    to create a set of high-frequency clocks the external signal is
    firstly handled by the embedded into CCU PLLs. So the corresponding
    dts-node is just a normal clock-provider node with standard set of
    properties. Note as being part of the Baikal-T1 System Controller its
    DT node is supposed to be a child the system controller node.
    
    Signed-off-by: Serge Semin <Sergey.Semin@baikalelectronics.ru>
    Cc: Alexey Malahov <Alexey.Malahov@baikalelectronics.ru>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: linux-mips@vger.kernel.org
    Link: https://lore.kernel.org/r/20200526222056.18072-2-Sergey.Semin@baikalelectronics.ru
    Reviewed-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

---
+++ include/dt-bindings/clock/bt1-ccu.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2020 BAIKAL ELECTRONICS, JSC
+ *
+ * Baikal-T1 CCU clock indices
+ */
+#ifndef __DT_BINDINGS_CLOCK_BT1_CCU_H
+#define __DT_BINDINGS_CLOCK_BT1_CCU_H
+
+#define CCU_CPU_PLL			0
+#define CCU_SATA_PLL			1
+#define CCU_DDR_PLL			2
+#define CCU_PCIE_PLL			3
+#define CCU_ETH_PLL			4
+
+#endif /* __DT_BINDINGS_CLOCK_BT1_CCU_H */
