d837261a7f4a569f01016abb067008ea50aa6674
Tariq Toukan <tariqt@mellanox.com>
2020-05-26 13:58:09 +0300

    net/mlx5e: kTLS, Improve TLS feature modularity
    Better separate the code into c/h files, so that kTLS internals
    are exposed to the corresponding non-accel flow as follows:
    - Necessary datapath functions are exposed via ktls_txrx.h.
    - Necessary caps and configuration functions are exposed via ktls.h,
      which became very small.
    
    In addition, kTLS internal code sharing is done via ktls_utils.h,
    which is not exposed to any non-accel file.
    
    Add explicit WQE structures for the TLS static and progress
    params, breaking the union of the static with UMR, and the progress
    with PSV.
    
    Generalize the API as a preparation for TLS RX offload support.
    
    Move kTLS TX-specific code to the proper file.
    Remove the inline tag for function in C files, let the compiler decide.
    Use kzalloc/kfree for the priv_tx context.
    
    Signed-off-by: Tariq Toukan <tariqt@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    Reviewed-by: Maxim Mikityanskiy <maximmi@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_accel/tls_rxtx.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en_accel/tls_rxtx.h
@@ -35,15 +35,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MLX5E_TLS_RXTX_H__
 #define __MLX5E_TLS_RXTX_H__
 
+#include "en_accel/ktls_txrx.h"
+
 #ifdef CONFIG_MLX5_EN_TLS
 
 #include <linux/skbuff.h>
 #include "en.h"
 #include "en/txrx.h"
 
-struct mlx5e_accel_tx_tls_state {
-	u32 tls_tisn;
-};
+u16 mlx5e_tls_get_stop_room(struct mlx5e_txqsq *sq);
 
 bool mlx5e_tls_handle_tx_skb(struct net_device *netdev, struct mlx5e_txqsq *sq,
 			     struct sk_buff *skb, struct mlx5e_accel_tx_tls_state *state);
@@ -52,6 +52,13 @@ void mlx5e_tls_handle_tx_wqe(struct mlx5e_txqsq *sq, struct mlx5_wqe_ctrl_seg *c
 
 void mlx5e_tls_handle_rx_skb(struct net_device *netdev, struct sk_buff *skb,
 			     u32 *cqe_bcnt);
+
+#else
+
+static inline u16 mlx5e_tls_get_stop_room(struct mlx5e_txqsq *sq)
+{
+	return 0;
+}
 
 #endif /* CONFIG_MLX5_EN_TLS */
 
