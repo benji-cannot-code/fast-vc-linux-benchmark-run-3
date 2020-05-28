eeb78a86be71b2cb5ae46e4ed49e03a439f9f169
Serge Semin <Sergey.Semin@baikalelectronics.ru>
2020-05-28 12:33:21 +0300

    i2c: designware: Add Baikal-T1 System I2C support
    Baikal-T1 System Controller is equipped with a dedicated I2C Controller
    which functionality is based on the DW APB I2C IP-core, the only
    difference in a way it' registers are accessed. There are three access
    register provided in the System Controller registers map, which indirectly
    address the normal DW APB I2C registers space. So in order to have the
    Baikal-T1 System I2C Controller supported by the common DW APB I2C driver
    we created a dedicated Dw I2C controller model quirk, which retrieves the
    syscon regmap from the parental dt node and creates a new regmap based on
    it.
    
    Signed-off-by: Serge Semin <Sergey.Semin@baikalelectronics.ru>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Wolfram Sang <wsa@kernel.org>

--- drivers/i2c/busses/i2c-designware-core.h
+++ drivers/i2c/busses/i2c-designware-core.h
@@ -184,6 +184,7 @@ struct reset_control;
  * struct dw_i2c_dev - private i2c-designware data
  * @dev: driver model device node
  * @map: IO registers map
+ * @sysmap: System controller registers map
  * @base: IO registers pointer
  * @ext: Extended IO registers pointer
  * @cmd_complete: tx completion indicator
@@ -236,6 +237,7 @@ struct reset_control;
 struct dw_i2c_dev {
 	struct device		*dev;
 	struct regmap		*map;
+	struct regmap		*sysmap;
 	void __iomem		*base;
 	void __iomem		*ext;
 	struct completion	cmd_complete;
@@ -291,6 +293,7 @@ struct dw_i2c_dev {
 #define ACCESS_NO_IRQ_SUSPEND	0x00000002
 
 #define MODEL_MSCC_OCELOT	0x00000100
+#define MODEL_BAIKAL_BT1	0x00000200
 #define MODEL_MASK		0x00000f00
 
 int i2c_dw_init_regmap(struct dw_i2c_dev *dev);
