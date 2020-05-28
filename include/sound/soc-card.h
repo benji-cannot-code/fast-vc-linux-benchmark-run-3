6e3c8a158bdda0792e12fdd03b6ba6a395cb3a9b
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2020-05-28 10:47:56 +0900

    ASoC: soc-card: move snd_soc_card_get_kcontrol() to soc-card
    Card related function should be implemented at soc-card now.
    This patch moves it.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Link: https://lore.kernel.org/r/87a71s25kj.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/sound/soc-card.h
+++ include/sound/soc-card.h
@@ -9,4 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __SOC_CARD_H
 #define __SOC_CARD_H
 
+struct snd_kcontrol *snd_soc_card_get_kcontrol(struct snd_soc_card *soc_card,
+					       const char *name);
+
 #endif /* __SOC_CARD_H */
