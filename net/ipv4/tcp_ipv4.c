0d8b185914db375af99f195a697e281100918605
Eric Dumazet <edumazet@google.com>
2020-05-27 17:34:58 -0700

    tcp: ipv6: support RFC 6069 (TCP-LD)
    Make tcp_ld_RTO_revert() helper available to IPv6, and
    implement RFC 6069 :
    
    Quoting this RFC :
    
    3. Connectivity Disruption Indication
    
       For Internet Protocol version 6 (IPv6) [RFC2460], the counterpart of
       the ICMP destination unreachable message of code 0 (net unreachable)
       and of code 1 (host unreachable) is the ICMPv6 destination
       unreachable message of code 0 (no route to destination) [RFC4443].
       As with IPv4, a router should generate an ICMPv6 destination
       unreachable message of code 0 in response to a packet that cannot be
       delivered to its destination address because it lacks a matching
       entry in its routing table.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Yuchung Cheng <ycheng@google.com>
    Acked-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -405,7 +405,7 @@ void tcp_req_err(struct sock *sk, u32 seq, bool abort)
 EXPORT_SYMBOL(tcp_req_err);
 
 /* TCP-LD (RFC 6069) logic */
-static void tcp_ld_RTO_revert(struct sock *sk, u32 seq)
+void tcp_ld_RTO_revert(struct sock *sk, u32 seq)
 {
 	struct inet_connection_sock *icsk = inet_csk(sk);
 	struct tcp_sock *tp = tcp_sk(sk);
@@ -442,6 +442,7 @@ static void tcp_ld_RTO_revert(struct sock *sk, u32 seq)
 		tcp_retransmit_timer(sk);
 	}
 }
+EXPORT_SYMBOL(tcp_ld_RTO_revert);
 
 /*
  * This routine is called by the ICMP module when it gets some
