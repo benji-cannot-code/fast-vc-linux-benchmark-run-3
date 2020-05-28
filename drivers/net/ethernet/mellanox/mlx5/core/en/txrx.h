1c94ad43bb64e4c9e6f0389bb7e6c64fc4fa7837
Tariq Toukan <tariqt@mellanox.com>
2020-05-28 10:13:00 +0300

    net/mlx5e: kTLS, Add kTLS RX HW offload support
    Implement driver support for the kTLS RX HW offload feature.
    Resync support is added in a downstream patch.
    
    New offload contexts post their static/progress params WQEs
    over the per-channel async ICOSQ, protected under a spin-lock.
    The Channel/RQ is selected according to the socket's rxq index.
    
    Feature is OFF by default. Can be turned on by:
    $ ethtool -K <if> tls-hw-rx-offload on
    
    A new TLS-RX workqueue is used to allow asynchronous addition of
    steering rules, out of the NAPI context.
    It will be also used in a downstream patch in the resync procedure.
    
    Signed-off-by: Tariq Toukan <tariqt@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en/txrx.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en/txrx.h
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 enum mlx5e_icosq_wqe_type {
 	MLX5E_ICOSQ_WQE_NOP,
 	MLX5E_ICOSQ_WQE_UMR_RX,
+#ifdef CONFIG_MLX5_EN_TLS
+	MLX5E_ICOSQ_WQE_UMR_TLS,
+	MLX5E_ICOSQ_WQE_SET_PSV_TLS,
+#endif
 };
 
 static inline bool
@@ -115,8 +119,15 @@ struct mlx5e_icosq_wqe_info {
 		struct {
 			struct mlx5e_rq *rq;
 		} umr;
+#ifdef CONFIG_MLX5_EN_TLS
+		struct {
+			struct mlx5e_ktls_offload_context_rx *priv_rx;
+		} tls_set_params;
+#endif
 	};
 };
+
+void mlx5e_free_icosq_descs(struct mlx5e_icosq *sq);
 
 static inline u16 mlx5e_icosq_get_next_pi(struct mlx5e_icosq *sq, u16 size)
 {
