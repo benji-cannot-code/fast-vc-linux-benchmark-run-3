b1a6913b76fddc47198f996bf3853e1ab52be5ce
Peter Zijlstra <peterz@infradead.org>
2020-05-29 22:41:05 +0200

    s390: Break cyclic percpu include
    In order to use <asm/percpu.h> in irqflags.h, we need to make sure
    asm/percpu.h does not itself depend on irqflags.h
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lkml.kernel.org/r/20200623083721.396143816@infradead.org

--- arch/s390/include/asm/smp.h
+++ arch/s390/include/asm/smp.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/sigp.h>
 #include <asm/lowcore.h>
+#include <asm/processor.h>
 
 #define raw_smp_processor_id()	(S390_lowcore.cpu_nr)
 
