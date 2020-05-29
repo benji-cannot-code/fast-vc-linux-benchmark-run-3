3d04d8697378b0189307d7d2d425d0fc5e1cb8c3
Paul E. McKenney <paulmck@kernel.org>
2020-05-28 19:33:47 -0700

    rcu-tasks: Add #include of rcupdate_trace.h to update.c
    Although this is in some strict sense unnecessary, it is good to allow
    the compiler to compare the function declaration with its definition.
    This commit therefore adds a #include of linux/rcupdate_trace.h to
    kernel/rcu/update.c.
    
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- kernel/rcu/update.c
+++ kernel/rcu/update.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kprobes.h>
 #include <linux/slab.h>
 #include <linux/irq_work.h>
+#include <linux/rcupdate_trace.h>
 
 #define CREATE_TRACE_POINTS
 
