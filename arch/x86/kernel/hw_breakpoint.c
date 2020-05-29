12dbaf9066c6e70e97424eddf6ee4c6cf79af89d
Lai Jiangshan <laijs@linux.alibaba.com>
2020-05-29 23:27:32 +0200

    x86/hw_breakpoint: Prevent data breakpoints on user_pcid_flush_mask
    The per-CPU user_pcid_flush_mask is used in the low level entry code. A
    data breakpoint can cause #DB recursion. 
    
    Protect the full cpu_tlbstate structure for simplicity.
    
    Signed-off-by: Lai Jiangshan <laijs@linux.alibaba.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/20200526014221.2119-5-laijs@linux.alibaba.com
    Link: https://lkml.kernel.org/r/20200529213320.955117574@infradead.org

--- arch/x86/kernel/hw_breakpoint.c
+++ arch/x86/kernel/hw_breakpoint.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/debugreg.h>
 #include <asm/user.h>
 #include <asm/desc.h>
+#include <asm/tlbflush.h>
 
 /* Per cpu debug control register value */
 DEFINE_PER_CPU(unsigned long, cpu_dr7);
@@ -264,6 +265,16 @@ static inline bool within_cpu_entry(unsigned long addr, unsigned long end)
 		if (within_area(addr, end,
 				(unsigned long)&per_cpu(cpu_tss_rw, cpu),
 				sizeof(struct tss_struct)))
+			return true;
+
+		/*
+		 * cpu_tlbstate.user_pcid_flush_mask is used for CPU entry.
+		 * If a data breakpoint on it, it will cause an unwanted #DB.
+		 * Protect the full cpu_tlbstate structure to be sure.
+		 */
+		if (within_area(addr, end,
+				(unsigned long)&per_cpu(cpu_tlbstate, cpu),
+				sizeof(struct tlb_state)))
 			return true;
 	}
 
