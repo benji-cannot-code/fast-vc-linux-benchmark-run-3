426010960011ac4607f1ee06b61508eb9dbe126c
Dan Carpenter <dan.carpenter@oracle.com>
2020-05-29 13:02:07 +0300

    net: phy: marvell: unlock after phy_select_page() failure
    We need to call phy_restore_page() even if phy_select_page() fails.
    Otherwise we are holding the phy_lock_mdio_bus() lock.  This requirement
    is documented at the start of the phy_select_page() function.
    
    Fixes: a618e86da91d ("net : phy: marvell: Speedup TDR data retrieval by only changing page once")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/marvell.c
+++ drivers/net/phy/marvell.c
@@ -1868,7 +1868,7 @@ static int marvell_vct5_amplitude_graph(struct phy_device *phydev)
 	 */
 	page = phy_select_page(phydev, MII_MARVELL_VCT5_PAGE);
 	if (page < 0)
-		return page;
+		goto restore_page;
 
 	for (distance = priv->first;
 	     distance <= priv->last;
