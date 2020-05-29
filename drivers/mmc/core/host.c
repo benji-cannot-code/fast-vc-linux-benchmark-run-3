8a9fa2267197e6b573b01d26b2c1495c960dc4d8
Ulf Hansson <ulf.hansson@linaro.org>
2020-05-29 12:23:41 +0200

    mmc: core: Always allow the card detect uevent to be consumed
    The approach to allow userspace ~5s to consume the uevent, which is
    triggered when a new card is inserted/initialized, currently requires the
    mmc host to support system wakeup.
    
    This is unnecessary limiting, especially for an mmc host that relies on a
    GPIO IRQ for card detect. More precisely, the mmc host may not support
    system wakeup for its corresponding struct device, while the GPIO IRQ still
    could be configured as a wakeup IRQ via enable_irq_wake().
    
    To support all various cases, let's simply drop the need for the wakeup
    support. Instead let's always register a wakeup source and activate it for
    all card detect IRQs by calling __pm_wakeup_event().
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Link: https://lore.kernel.org/r/20200529102341.12529-1-ulf.hansson@linaro.org

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_gpio.h>
 #include <linux/pagemap.h>
+#include <linux/pm_wakeup.h>
 #include <linux/export.h>
 #include <linux/leds.h>
 #include <linux/slab.h>
@@ -37,6 +38,7 @@ static DEFINE_IDA(mmc_host_ida);
 static void mmc_host_classdev_release(struct device *dev)
 {
 	struct mmc_host *host = cls_dev_to_mmc_host(dev);
+	wakeup_source_unregister(host->ws);
 	ida_simple_remove(&mmc_host_ida, host->index);
 	kfree(host);
 }
@@ -401,6 +403,7 @@ struct mmc_host *mmc_alloc_host(int extra, struct device *dev)
 	host->index = err;
 
 	dev_set_name(&host->class_dev, "mmc%d", host->index);
+	host->ws = wakeup_source_register(NULL, dev_name(&host->class_dev));
 
 	host->parent = dev;
 	host->class_dev.parent = dev;
