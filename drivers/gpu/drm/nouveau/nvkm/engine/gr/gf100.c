28a54bd04a9ca75dd3af1c1c5fd1b932a9b4cc3b
Ben Skeggs <bskeggs@redhat.com>
2020-05-30 09:34:08 +1000

    drm/nouveau/gr/gf100-: make some functions static
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/gr/gf100.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/gr/gf100.c
@@ -742,7 +742,7 @@ gf100_gr_fecs_ctrl_ctxsw(struct gf100_gr *gr, u32 mthd)
 	return -ETIMEDOUT;
 }
 
-int
+static int
 gf100_gr_fecs_start_ctxsw(struct nvkm_gr *base)
 {
 	struct gf100_gr *gr = gf100_gr(base);
@@ -757,7 +757,7 @@ gf100_gr_fecs_start_ctxsw(struct nvkm_gr *base)
 	return ret;
 }
 
-int
+static int
 gf100_gr_fecs_stop_ctxsw(struct nvkm_gr *base)
 {
 	struct gf100_gr *gr = gf100_gr(base);
@@ -2033,7 +2033,7 @@ gf100_gr_fini(struct nvkm_gr *base, bool suspend)
 	return 0;
 }
 
-void *
+static void *
 gf100_gr_dtor(struct nvkm_gr *base)
 {
 	struct gf100_gr *gr = gf100_gr(base);
