ed6b4224df83ab3fc7a563052f3a0820448b5c2d
Peter Zijlstra <peterz@infradead.org>
2020-05-29 22:40:58 +0200

    sparc64: Fix asm/percpu.h build error
    In order to break a header dependency between lockdep and task_struct,
    I need per-cpu stuff from lockdep.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: David S. Miller <davem@davemloft.net>
    Link: https://lkml.kernel.org/r/20200623083721.277992771@infradead.org

--- arch/sparc/include/asm/percpu_64.h
+++ arch/sparc/include/asm/percpu_64.h
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/compiler.h>
 
+#ifndef BUILD_VDSO
 register unsigned long __local_per_cpu_offset asm("g5");
+#endif
 
 #ifdef CONFIG_SMP
 
