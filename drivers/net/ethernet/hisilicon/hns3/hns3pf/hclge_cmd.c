e70d6f2bfc20c2a908b4a0d5d626d0d465cf769b
Huazhong Tan <tanhuazhong@huawei.com>
2020-05-30 09:08:28 +0800

    net: hns3: remove an unused macro hclge_is_csq
    Macro hclge_is_csq defined in hcgle_cmd.c has not been used,
    so remove it.
    
    Signed-off-by: Huazhong Tan <tanhuazhong@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_cmd.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_cmd.c
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hnae3.h"
 #include "hclge_main.h"
 
-#define hclge_is_csq(ring) ((ring)->flag & HCLGE_TYPE_CSQ)
-
 #define cmq_ring_to_dev(ring)   (&(ring)->dev->pdev->dev)
 
 static int hclge_ring_space(struct hclge_cmq_ring *ring)
