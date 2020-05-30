be26a4be6f7c4df1587d2dba3ae976a3c2f40402
Ioana Ciornei <ioana.ciornei@nxp.com>
2020-05-31 00:08:13 +0300

    dpaa2-eth: Add PFC support through DCB ops
    Add support in dpaa2-eth for PFC (Priority Flow Control)
    through the DCB ops.
    
    Instruct the hardware to respond to received PFC frames.
    Current firmware doesn't allow us to selectively enable PFC
    on the Rx side for some priorities only, so we will react to
    all incoming PFC frames (and stop transmitting on the traffic
    classes specified in the frame).
    
    Also, configure the hardware to generate PFC frames based on Rx
    congestion notifications. When a certain number of frames accumulate in
    the ingress queues corresponding to a traffic class, priority flow
    control frames are generated for that TC.
    
    The number of PFC traffic classes available can be queried through
    lldptool. Also, which of those traffic classes have PFC enabled is also
    controlled through the same dcbnl_rtnl_ops callbacks.
    
    Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/dpaa2/dpaa2-eth.h
+++ drivers/net/ethernet/freescale/dpaa2/dpaa2-eth.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DPAA2_ETH_H
 #define __DPAA2_ETH_H
 
+#include <linux/dcbnl.h>
 #include <linux/netdevice.h>
 #include <linux/if_vlan.h>
 #include <linux/fsl/mc.h>
@@ -65,6 +66,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define DPAA2_ETH_CG_TAILDROP_THRESH(priv)				\
 	(1024 * dpaa2_eth_queue_count(priv) / dpaa2_eth_tc_count(priv))
+
+/* Congestion group notification threshold: when this many frames accumulate
+ * on the Rx queues belonging to the same TC, the MAC is instructed to send
+ * PFC frames for that TC.
+ * When number of pending frames drops below exit threshold transmission of
+ * PFC frames is stopped.
+ */
+#define DPAA2_ETH_CN_THRESH_ENTRY(priv) \
+	(DPAA2_ETH_CG_TAILDROP_THRESH(priv) / 2)
+#define DPAA2_ETH_CN_THRESH_EXIT(priv) \
+	(DPAA2_ETH_CN_THRESH_ENTRY(priv) * 3 / 4)
 
 /* Maximum number of buffers that can be acquired/released through a single
  * QBMan command
@@ -437,6 +449,10 @@ struct dpaa2_eth_priv {
 	struct dpaa2_eth_cls_rule *cls_rules;
 	u8 rx_cls_enabled;
 	u8 vlan_cls_enabled;
+#ifdef CONFIG_FSL_DPAA2_ETH_DCB
+	u8 dcbx_mode;
+	struct ieee_pfc pfc;
+#endif
 	struct bpf_prog *xdp_prog;
 #ifdef CONFIG_DEBUG_FS
 	struct dpaa2_debugfs dbg;
@@ -568,5 +584,7 @@ int dpaa2_eth_set_cls(struct net_device *net_dev, u64 key);
 int dpaa2_eth_cls_key_size(u64 key);
 int dpaa2_eth_cls_fld_off(int prot, int field);
 void dpaa2_eth_cls_trim_rule(void *key_mem, u64 fields);
+
+extern const struct dcbnl_rtnl_ops dpaa2_eth_dcbnl_ops;
 
 #endif	/* __DPAA2_H */
