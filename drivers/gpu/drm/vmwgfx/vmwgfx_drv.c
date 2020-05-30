939784bc6639b1d2c004eb75dee80b052520d4d9
Emil Velikov <emil.l.velikov@gmail.com>
2020-05-30 13:46:39 +0100

    drm: vmwgfx: remove drm_driver::master_set() return type
    The function always returns zero (success). Ideally we'll remove it all
    together - although that's requires a little more work.
    
    For now, we can drop the return type and simplify the drm core code
    surrounding it.
    
    v2: remove redundant assignment (Sam)
    
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: VMware Graphics <linux-graphics-maintainer@vmware.com>
    Cc: Roland Scheidegger <sroland@vmware.com>
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Reviewed-by: Sam Ravnborg <sam@ravnborg.org>
    Reviewed-by: Roland Scheidegger <sroland@vmware.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200530124640.4176323-1-emil.l.velikov@gmail.com

--- drivers/gpu/drm/vmwgfx/vmwgfx_drv.c
+++ drivers/gpu/drm/vmwgfx/vmwgfx_drv.c
@@ -1130,9 +1130,9 @@ static long vmw_compat_ioctl(struct file *filp, unsigned int cmd,
 }
 #endif
 
-static int vmw_master_set(struct drm_device *dev,
-			  struct drm_file *file_priv,
-			  bool from_open)
+static void vmw_master_set(struct drm_device *dev,
+			   struct drm_file *file_priv,
+			   bool from_open)
 {
 	/*
 	 * Inform a new master that the layout may have changed while
@@ -1140,8 +1140,6 @@ static int vmw_master_set(struct drm_device *dev,
 	 */
 	if (!from_open)
 		drm_sysfs_hotplug_event(dev);
-
-	return 0;
 }
 
 static void vmw_master_drop(struct drm_device *dev,
