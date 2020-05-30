2d4eb66627290b52e1016c51fd4517f5216f69f9
WANG Xuerui <git@xen0n.name>
2020-05-30 15:32:42 +0800

    MIPS: Expose Loongson CPUCFG availability via HWCAP
    The point is to allow userspace to probe for CPUCFG without possibly
    triggering invalid instructions. In addition to that, future Loongson
    feature bits could all be stuffed into CPUCFG bit fields (or "leaves"
    in x86-speak) if Loongson does not make mistakes, so ELF HWCAP bits are
    conserved.
    
    Userspace can determine native CPUCFG availability by checking the LCSRP
    (Loongson CSR Present) bit in CPUCFG output after seeing CPUCFG bit in
    HWCAP. Native CPUCFG always sets the LCSRP bit, as CPUCFG is part of the
    Loongson CSR ASE, while the emulation intentionally leaves this bit
    clear.
    
    The other existing Loongson-specific HWCAP bits are, to my best
    knowledge, unused, as
    
    (1) they are fairly recent additions,
    (2) Loongson never back-ported the patch into their kernel fork, and
    (3) Loongson's existing installed base rarely upgrade, if ever;
    
    However, they are still considered userspace ABI, hence unfortunately
    unremovable. But hopefully at least we could stop adding new Loongson
    HWCAP bits in the future.
    
    Cc: Paul Burton <paulburton@kernel.org>
    Cc: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Cc: Huacai Chen <chenhc@lemote.com>
    Signed-off-by: WANG Xuerui <git@xen0n.name>
    Reviewed-by: Huacai Chen <chenhc@lemote.com>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/loongson64/cpucfg-emul.c
+++ arch/mips/loongson64/cpucfg-emul.c
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <asm/cpu.h>
 #include <asm/cpu-info.h>
+#include <asm/elf.h>
 
 #include <loongson_regs.h>
 #include <cpucfg-emul.h>
@@ -129,7 +130,7 @@ void loongson3_cpucfg_synthesize_data(struct cpuinfo_mips *c)
 
 	/* CPUs with CPUCFG support don't need to synthesize anything. */
 	if (cpu_has_cfg())
-		return;
+		goto have_cpucfg_now;
 
 	c->loongson3_cpucfg_data[0] = 0;
 	c->loongson3_cpucfg_data[1] = 0;
@@ -218,4 +219,10 @@ void loongson3_cpucfg_synthesize_data(struct cpuinfo_mips *c)
 	patch_cpucfg_sel1(c);
 	patch_cpucfg_sel2(c);
 	patch_cpucfg_sel3(c);
+
+have_cpucfg_now:
+	/* We have usable CPUCFG now, emulated or not.
+	 * Announce CPUCFG availability to userspace via hwcap.
+	 */
+	elf_hwcap |= HWCAP_LOONGSON_CPUCFG;
 }
