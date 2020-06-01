4b8ae4be9391ee8c00215f816e7d48b720064be8
Kees Cook <keescook@chromium.org>
2020-06-01 12:34:44 -0700

    selftests/seccomp: Expand benchmark to per-filter measurements
    It's useful to see how much (at a minimum) each filter adds to the
    syscall overhead. Add additional calculations.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- tools/testing/selftests/seccomp/seccomp_bpf.c
+++ tools/testing/selftests/seccomp/seccomp_bpf.c
@@ -3841,7 +3841,6 @@ TEST(user_notification_filter_empty_threaded)
 
 /*
  * TODO:
- * - add microbenchmarks
  * - expand NNP testing
  * - better arch-specific TRACE and TRAP handlers.
  * - endianness checking when appropriate
@@ -3849,7 +3848,6 @@ TEST(user_notification_filter_empty_threaded)
  * - arch value testing (x86 modes especially)
  * - verify that FILTER_FLAG_LOG filters generate log messages
  * - verify that RET_LOG generates log messages
- * - ...
  */
 
 TEST_HARNESS_MAIN
