6ef67172a661039c5217dbb56b43a65fbdf6086e
Baolin Wang <baolin.wang@linux.alibaba.com>
2020-06-01 14:05:52 +0800

    hwspinlock: sprd: Remove redundant header files
    Remove redundant header files.
    
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://lore.kernel.org/r/1590991552-93643-1-git-send-email-baolin.wang@linux.alibaba.com
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/hwspinlock/sprd_hwspinlock.c
+++ drivers/hwspinlock/sprd_hwspinlock.c
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2017 Spreadtrum  - http://www.spreadtrum.com
  */
 
-#include <linux/bitops.h>
 #include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/device.h>
@@ -16,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
-#include <linux/slab.h>
 
 #include "hwspinlock_internal.h"
 
