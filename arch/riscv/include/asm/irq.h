f7e4a9395e798936b471d61d36125fde2778e969
Anup Patel <anup.patel@wdc.com>
2020-06-01 14:45:38 +0530

    RISC-V: self-contained IPI handling routine
    Currently, the IPI handling routine riscv_software_interrupt() does
    not take any argument and also does not perform irq_enter()/irq_exit().
    
    This patch makes IPI handling routine more self-contained by:
    1. Passing "pt_regs *" argument
    2. Explicitly doing irq_enter()/irq_exit()
    3. Explicitly save/restore "pt_regs *" using set_irq_regs()
    
    With above changes, IPI handling routine does not depend on caller
    function to perform irq_enter()/irq_exit() and save/restore of
    "pt_regs *" hence its more self-contained. This also enables us
    to call IPI handling routine from IRQCHIP drivers.
    
    Signed-off-by: Anup Patel <anup.patel@wdc.com>
    Reviewed-by: Atish Patra <atish.patra@wdc.com>
    Reviewed-by: Palmer Dabbelt <palmerdabbelt@google.com>
    Acked-by: Palmer Dabbelt <palmerdabbelt@google.com>
    Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>

--- arch/riscv/include/asm/irq.h
+++ arch/riscv/include/asm/irq.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NR_IRQS         0
 
 void riscv_timer_interrupt(void);
-void riscv_software_interrupt(void);
 
 #include <asm-generic/irq.h>
 
