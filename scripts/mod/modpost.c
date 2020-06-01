c77cfd6a85c7834f14a94dea50597af26b48ebc7
Masahiro Yamada <masahiroy@kernel.org>
2020-06-01 14:57:28 +0900

    modpost: set have_vmlinux in new_module()
    Set have_vmlinux flag in a single place.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -193,6 +193,9 @@ static struct module *new_module(const char *modname)
 	mod->next = modules;
 	modules = mod;
 
+	if (mod->is_vmlinux)
+		have_vmlinux = 1;
+
 	return mod;
 }
 
@@ -2013,9 +2016,6 @@ static void read_symbols(const char *modname)
 
 	mod = new_module(modname);
 
-	if (mod->is_vmlinux)
-		have_vmlinux = 1;
-
 	if (!mod->is_vmlinux) {
 		license = get_modinfo(&info, "license");
 		if (!license)
@@ -2471,8 +2471,6 @@ static void read_dump(const char *fname)
 		mod = find_module(modname);
 		if (!mod) {
 			mod = new_module(modname);
-			if (mod->is_vmlinux)
-				have_vmlinux = 1;
 			mod->from_dump = 1;
 		}
 		s = sym_add_exported(symname, mod, export_no(export));
