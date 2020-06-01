1d3d41d38734082652c7bb360996d658461a7449
Boris Burkov <boris@bur.io>
2020-06-01 13:12:05 -0700

    blk-cgroup: show global disk stats in root cgroup io.stat
    In order to improve consistency and usability in cgroup stat accounting,
    we would like to support the root cgroup's io.stat.
    
    Since the root cgroup has processes doing io even if the system has no
    explicitly created cgroups, we need to be careful to avoid overhead in
    that case.  For that reason, the rstat algorithms don't handle the root
    cgroup, so just turning the file on wouldn't give correct statistics.
    
    To get around this, we simulate flushing the iostat struct by filling it
    out directly from global disk stats. The result is a root cgroup io.stat
    file consistent with both /proc/diskstats and io.stat.
    
    Note that in order to collect the disk stats, we needed to iterate over
    devices. To facilitate that, we had to change the linkage of a disk_type
    to external so that it can be used from blk-cgroup.c to iterate over
    disks.
    
    Suggested-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Boris Burkov <boris@bur.io>
    Acked-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/linux/genhd.h
+++ include/linux/genhd.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define disk_to_dev(disk)	(&(disk)->part0.__dev)
 #define part_to_dev(part)	(&((part)->__dev))
 
+extern const struct device_type disk_type;
 extern struct device_type part_type;
 extern struct class block_class;
 
