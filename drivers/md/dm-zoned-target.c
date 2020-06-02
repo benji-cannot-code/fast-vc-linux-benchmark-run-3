735767cb44fcfd1c1a8857d337d67f25037462e7
Hannes Reinecke <hare@suse.de>
2020-06-02 13:09:54 +0200

    dm zoned: select reclaim zone based on device index
    per-device reclaim should select zones on that device only.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>
    Reviewed-by: Damien Le Moal <damien.lemoal@wdc.com>
    Signed-off-by: Mike Snitzer <snitzer@redhat.com>

--- drivers/md/dm-zoned-target.c
+++ drivers/md/dm-zoned-target.c
@@ -739,6 +739,7 @@ static int dmz_get_zoned_device(struct dm_target *ti, char *path,
 		dev = &dmz->dev[idx];
 	}
 	dev->bdev = bdev;
+	dev->dev_idx = idx;
 	(void)bdevname(dev->bdev, dev->name);
 
 	dev->capacity = i_size_read(bdev->bd_inode) >> SECTOR_SHIFT;
