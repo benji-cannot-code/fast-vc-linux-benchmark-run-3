c2a0463030d09eb71c73ed7ca18f7ba7f8d04820
Sibi Sankar <sibis@codeaurora.org>
2020-06-02 22:02:57 +0530

    remoteproc: qcom_q6v5_mss: Remove redundant running state
    Remove the redundant running state, as an equivalent is maintained in
    the common q6v5 resource handling helpers.
    
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Reviewed-by: Evan Green <evgreen@chromium.org>
    Signed-off-by: Sibi Sankar <sibis@codeaurora.org>
    Link: https://lore.kernel.org/r/20200602163257.26978-2-sibis@codeaurora.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/remoteproc/qcom_q6v5_mss.c
+++ drivers/remoteproc/qcom_q6v5_mss.c
@@ -182,8 +182,6 @@ struct q6v5 {
 	int active_reg_count;
 	int proxy_reg_count;
 
-	bool running;
-
 	bool dump_mba_loaded;
 	size_t current_dump_size;
 	size_t total_dump_size;
@@ -1334,7 +1332,6 @@ static int q6v5_start(struct rproc *rproc)
 
 	/* Reset Dump Segment Mask */
 	qproc->current_dump_size = 0;
-	qproc->running = true;
 
 	return 0;
 
@@ -1349,8 +1346,6 @@ static int q6v5_stop(struct rproc *rproc)
 {
 	struct q6v5 *qproc = (struct q6v5 *)rproc->priv;
 	int ret;
-
-	qproc->running = false;
 
 	ret = qcom_q6v5_request_stop(&qproc->q6v5);
 	if (ret == -ETIMEDOUT)
