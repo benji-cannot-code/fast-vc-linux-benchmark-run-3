b14b7d234e6ed618553c44f24aae9823ee7fa835
Alaa Hleihel <alaa@mellanox.com>
2020-06-02 12:09:21 +0300

    net/mlx5e: Move including net/arp.h from en_rep.c to rep/neigh.c
    After the cited commit, the header net/arp.h is no longer used in en_rep.c.
    So, move it to the new file rep/neigh.c that uses it now.
    
    Signed-off-by: Alaa Hleihel <alaa@mellanox.com>
    Reviewed-by: Vlad Buslov <vladbu@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_rep.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_rep.c
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/switchdev.h>
 #include <net/pkt_cls.h>
 #include <net/act_api.h>
-#include <net/arp.h>
 #include <net/devlink.h>
 #include <net/ipv6_stubs.h>
 
