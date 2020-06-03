09228e97b5df91d399961d12ccc28be90086385c
Pali Rohár <pali@kernel.org>
2020-06-03 10:22:27 +0200

    mwifiex: Fix firmware filename for sd8997 chipset
    Firmware for sd8997 chipset is distributed by Marvell package and also as
    part of the linux-firmware repository in filename sdsd8997_combo_v4.bin.
    
    This patch fixes mwifiex driver to load correct firmware file for sd8997.
    
    Fixes: 6d85ef00d9dfe ("mwifiex: add support for 8997 chipset")
    Signed-off-by: Pali Rohár <pali@kernel.org>
    Acked-by: Ganapathi Bhat <ganapathi.bhat@nxp.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/net/wireless/marvell/mwifiex/sdio.h
+++ drivers/net/wireless/marvell/mwifiex/sdio.h
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SD8801_DEFAULT_FW_NAME "mrvl/sd8801_uapsta.bin"
 #define SD8977_DEFAULT_FW_NAME "mrvl/sdsd8977_combo_v2.bin"
 #define SD8987_DEFAULT_FW_NAME "mrvl/sd8987_uapsta.bin"
-#define SD8997_DEFAULT_FW_NAME "mrvl/sd8997_uapsta.bin"
+#define SD8997_DEFAULT_FW_NAME "mrvl/sdsd8997_combo_v4.bin"
 
 #define BLOCK_MODE	1
 #define BYTE_MODE	0
