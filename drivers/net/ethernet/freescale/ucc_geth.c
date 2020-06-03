d20ab931daa0308c9aca896232dfc8bf9e96e03c
Valentin Longchamp <valentin@longchamp.me>
2020-06-03 23:28:23 +0200

    net: ethernet: freescale: remove unneeded include for ucc_geth
    net/sch_generic.h does not need to be included, remove it.
    
    Signed-off-by: Valentin Longchamp <valentin@longchamp.me>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/freescale/ucc_geth.c
+++ drivers/net/ethernet/freescale/ucc_geth.c
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <soc/fsl/qe/ucc.h>
 #include <soc/fsl/qe/ucc_fast.h>
 #include <asm/machdep.h>
-#include <net/sch_generic.h>
 
 #include "ucc_geth.h"
 
