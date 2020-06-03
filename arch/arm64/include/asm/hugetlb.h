8895da4b11b64d6da6100f96b24022ddb3f72c73
Anshuman Khandual <anshuman.khandual@arm.com>
2020-06-03 16:00:56 -0700

    arm64/mm: drop __HAVE_ARCH_HUGE_PTEP_GET
    Patch series "mm/hugetlb: Add some new generic fallbacks", v3.
    
    This series adds the following new generic fallbacks.  Before that it
    drops __HAVE_ARCH_HUGE_PTEP_GET from arm64 platform.
    
    1. is_hugepage_only_range()
    2. arch_clear_hugepage_flags()
    
    After this arm (32 bit) remains the sole platform defining it's own
    huge_ptep_get() via __HAVE_ARCH_HUGE_PTEP_GET.
    
    This patch (of 3):
    
    Platform specific huge_ptep_get() is required only when fetching the huge
    PTE involves more than just dereferencing the page table pointer.  This is
    not the case on arm64 platform.  Hence huge_ptep_pte() can be dropped
    along with it's __HAVE_ARCH_HUGE_PTEP_GET subscription.  Before that, it
    updates the generic huge_ptep_get() with READ_ONCE() which will prevent
    known page table issues with THP on arm64.
    
    Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Christian Borntraeger <borntraeger@de.ibm.com>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: Helge Deller <deller@gmx.de>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: "James E.J. Bottomley" <James.Bottomley@HansenPartnership.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Palmer Dabbelt <palmer@dabbelt.com>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Paul Walmsley <paul.walmsley@sifive.com>
    Cc: Rich Felker <dalias@libc.org>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Thomas Bogendoerfer <tsbogend@alpha.franken.de>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Vasily Gorbik <gor@linux.ibm.com>
    Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
    Link: http://lkml.kernel.org/r/1588907271-11920-1-git-send-email-anshuman.khandual@arm.com
    Link: http://lkml.kernel.org/r//1506527369-19535-1-git-send-email-will.deacon@arm.com/
    Link: http://lkml.kernel.org/r/1588907271-11920-2-git-send-email-anshuman.khandual@arm.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/arm64/include/asm/hugetlb.h
+++ arch/arm64/include/asm/hugetlb.h
@@ -18,12 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern bool arch_hugetlb_migration_supported(struct hstate *h);
 #endif
 
-#define __HAVE_ARCH_HUGE_PTEP_GET
-static inline pte_t huge_ptep_get(pte_t *ptep)
-{
-	return READ_ONCE(*ptep);
-}
-
 static inline int is_hugepage_only_range(struct mm_struct *mm,
 					 unsigned long addr, unsigned long len)
 {
