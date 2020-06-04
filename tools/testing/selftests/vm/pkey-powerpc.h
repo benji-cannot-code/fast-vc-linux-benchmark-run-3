320c7565a3cacab7500bf94819c1c9451e85ca17
Sandipan Das <sandipan@linux.ibm.com>
2020-06-04 16:52:50 -0700

    selftests: vm: pkeys: use the correct page size on powerpc
    Both 4K and 64K pages are supported on powerpc.  Parts of the selftest
    code perform alignment computations based on the PAGE_SIZE macro which is
    currently hardcoded to 64K for powerpc.  This causes some test failures on
    kernels configured with 4K page size.
    
    In some cases, we need to enforce function alignment on page size.  Since
    this can only be done at build time, 64K is used as the alignment factor
    as that also ensures 4K alignment.
    
    Signed-off-by: Sandipan Das <sandipan@linux.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Dave Hansen <dave.hansen@intel.com>
    Cc: "Desnes A. Nunes do Rosario" <desnesn@linux.vnet.ibm.com>
    Cc: Florian Weimer <fweimer@redhat.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Ram Pai <linuxram@us.ibm.com>
    Cc: Thiago Jung Bauermann <bauerman@linux.ibm.com>
    Cc: "Aneesh Kumar K.V" <aneesh.kumar@linux.ibm.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Michal Suchanek <msuchanek@suse.de>
    Cc: Shuah Khan <shuah@kernel.org>
    Link: http://lkml.kernel.org/r/5dcdfbf3353acdc90f315172e800b49f5ca21299.1585646528.git.sandipan@linux.ibm.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- tools/testing/selftests/vm/pkey-powerpc.h
+++ tools/testing/selftests/vm/pkey-powerpc.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 					     pkey-31 and exec-only key */
 #define PKEY_BITS_PER_PKEY	2
 #define HPAGE_SIZE		(1UL << 24)
-#define PAGE_SIZE		(1UL << 16)
+#define PAGE_SIZE		sysconf(_SC_PAGESIZE)
 
 static inline u32 pkey_bit_position(int pkey)
 {
