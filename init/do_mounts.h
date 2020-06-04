33b309de827258d0bd19940506d8e270050f2aaf
Christoph Hellwig <hch@lst.de>
2020-06-04 10:23:14 +0200

    initrd: remove support for multiple floppies
    Remove the special handling for multiple floppies in the initrd code.
    No one should be using floppies for booting these days. (famous last
    words..)
    
    Includes a spelling fix from Colin Ian King <colin.king@canonical.com>.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>

--- init/do_mounts.h
+++ init/do_mounts.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/major.h>
 #include <linux/root_dev.h>
 
-void  change_floppy(char *fmt, ...);
 void  mount_block_root(char *name, int flags);
 void  mount_root(void);
 extern int root_mountflags;
