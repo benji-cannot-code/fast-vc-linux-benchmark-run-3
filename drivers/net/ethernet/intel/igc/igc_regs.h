1841ceb6ef56f8d33aeb3b34fc3b5f9771ca3dcc
Sasha Neftin <sasha.neftin@intel.com>
2020-06-04 14:25:16 +0300

    igc: Add LPI counters
    Add EEE TX LPI and EEE RX LPI counters. A EEE TX LPI event
    occurs when the transmitter enters EEE (IEEE 802.3az) LPI
    state. A EEE RX LPI event occurs when the receiver detect
    link partner entry into EEE(IEEE 802.3az) LPI state.
    
    Signed-off-by: Sasha Neftin <sasha.neftin@intel.com>
    Tested-by: Aaron Brown <aaron.f.brown@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>

--- drivers/net/ethernet/intel/igc/igc_regs.h
+++ drivers/net/ethernet/intel/igc/igc_regs.h
@@ -189,6 +189,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IGC_ICTXQEC	0x04118  /* Interrupt Cause Tx Queue Empty Count */
 #define IGC_ICTXQMTC	0x0411C  /* Interrupt Cause Tx Queue Min Thresh Count */
 #define IGC_RPTHC	0x04104  /* Rx Packets To Host */
+#define IGC_TLPIC	0x04148  /* EEE Tx LPI Count */
+#define IGC_RLPIC	0x0414C  /* EEE Rx LPI Count */
 #define IGC_HGPTC	0x04118  /* Host Good Packets Tx Count */
 #define IGC_RXDMTC	0x04120  /* Rx Descriptor Minimum Threshold Count */
 #define IGC_HGORCL	0x04128  /* Host Good Octets Received Count Low */
