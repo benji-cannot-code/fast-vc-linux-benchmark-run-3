4cd57471416719d713547c12f0bc0c995250d788
Evan Quan <evan.quan@amd.com>
2020-06-08 16:41:16 +0800

    drm/amd/powerplay: simplify the code around retrieving power limit
    Use the cached max/current power limit for other cases except
    .late_init.
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/navi10_ppt.c
+++ drivers/gpu/drm/amd/powerplay/navi10_ppt.c
@@ -1923,9 +1923,7 @@ static uint32_t navi10_get_pptable_power_limit(struct smu_context *smu)
 	return pptable->SocketPowerLimitAc[PPT_THROTTLER_PPT0];
 }
 
-static int navi10_get_power_limit(struct smu_context *smu,
-				  uint32_t *limit,
-				  bool cap)
+static int navi10_get_power_limit(struct smu_context *smu)
 {
 	struct smu_11_0_powerplay_table *powerplay_table =
 		(struct smu_11_0_powerplay_table *)smu->smu_table.power_play_table;
@@ -1952,8 +1950,6 @@ static int navi10_get_power_limit(struct smu_context *smu,
 		power_limit /= 100;
 	}
 	smu->max_power_limit = power_limit;
-
-	*limit = (cap ? smu->max_power_limit : smu->current_power_limit);
 
 	return 0;
 }
