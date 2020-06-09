accf906c786844f067e3c61a9ad4d59785db3255
Joerg Roedel <jroedel@suse.de>
2020-06-09 15:03:03 +0200

    iommu/vt-d: Move Intel IOMMU driver into subdirectory
    Move all files related to the Intel IOMMU driver into its own
    subdirectory.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Reviewed-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Link: https://lore.kernel.org/r/20200609130303.26974-3-joro@8bytes.org

--- drivers/iommu/intel/dmar.c
+++ drivers/iommu/intel/dmar.c
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/irq_remapping.h>
 #include <asm/iommu_table.h>
 
-#include "irq_remapping.h"
+#include "../irq_remapping.h"
 
 typedef int (*dmar_res_handler_t)(struct acpi_dmar_header *, void *);
 struct dmar_res_callback {
