75d85cdca40ef541d752132de6ed163d19f65a91
Joerg Roedel <jroedel@suse.de>
2020-06-09 15:03:02 +0200

    iommu/amd: Move AMD IOMMU driver into subdirectory
    Move all files related to the AMD IOMMU driver into its own
    subdirectory.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Reviewed-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Reviewed-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Link: https://lore.kernel.org/r/20200609130303.26974-2-joro@8bytes.org

--- drivers/iommu/amd/init.c
+++ drivers/iommu/amd/init.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/crash_dump.h>
 
 #include "amd_iommu.h"
-#include "irq_remapping.h"
+#include "../irq_remapping.h"
 
 /*
  * definitions for the ACPI scanning code
--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/dma.h>
 
 #include "amd_iommu.h"
-#include "irq_remapping.h"
+#include "../irq_remapping.h"
 
 #define CMD_SET_TYPE(cmd, t) ((cmd)->data[1] |= ((t) << 28))
 
