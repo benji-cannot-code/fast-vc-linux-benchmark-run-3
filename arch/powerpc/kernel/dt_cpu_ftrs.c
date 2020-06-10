fe4a403f449b556a6e6abfd92f6016db5c0b14cc
Murilo Opsfelder Araujo <muriloo@linux.ibm.com>
2020-06-10 18:51:12 -0300

    powerpc/dt_cpu_ftrs: Remove unused macro ISA_V2_07B
    Macro ISA_V2_07B is defined but not used anywhere else in the code.
    
    Signed-off-by: Murilo Opsfelder Araujo <muriloo@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200610215114.167544-2-muriloo@linux.ibm.com

--- arch/powerpc/kernel/dt_cpu_ftrs.c
+++ arch/powerpc/kernel/dt_cpu_ftrs.c
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 /* Device-tree visible constants follow */
-#define ISA_V2_07B      2070
 #define ISA_V3_0B       3000
 #define ISA_V3_1        3100
 
