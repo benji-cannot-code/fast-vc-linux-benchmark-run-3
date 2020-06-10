de4f56151ba81fcb92446e6a71f90a2ebb383d64
Herbert Xu <herbert@gondor.apana.org.au>
2020-06-10 21:05:13 +1000

    x86/microcode: Do not select FW_LOADER
    The x86 microcode support works just fine without FW_LOADER. In fact,
    these days most people load microcode early during boot so FW_LOADER
    never gets into the picture anyway.
    
    As almost everyone on x86 needs to enable MICROCODE, this by extension
    means that FW_LOADER is always built into the kernel even if nothing
    uses it. The FW_LOADER system is about two thousand lines long and
    contains user-space facing interfaces that could potentially provide an
    entry point into the kernel (or beyond).
    
    Remove the unnecessary select of FW_LOADER by MICROCODE. People who need
    the FW_LOADER capability can still enable it.
    
     [ bp: Massage a bit. ]
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/20200610042911.GA20058@gondor.apana.org.au

--- arch/x86/kernel/cpu/microcode/core.c
+++ arch/x86/kernel/cpu/microcode/core.c
@@ -146,7 +146,6 @@ extern struct builtin_fw __end_builtin_fw[];
 
 bool get_builtin_firmware(struct cpio_data *cd, const char *name)
 {
-#ifdef CONFIG_FW_LOADER
 	struct builtin_fw *b_fw;
 
 	for (b_fw = __start_builtin_fw; b_fw != __end_builtin_fw; b_fw++) {
@@ -156,7 +155,6 @@ bool get_builtin_firmware(struct cpio_data *cd, const char *name)
 			return true;
 		}
 	}
-#endif
 	return false;
 }
 
