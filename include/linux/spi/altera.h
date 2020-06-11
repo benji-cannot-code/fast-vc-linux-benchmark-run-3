20c593f99f67e82a3ec5d2129aad8934a29c57ef
Xu Yilun <yilun.xu@intel.com>
2020-06-11 11:25:08 +0800

    spi: altera: add platform data for slave information.
    This patch introduces platform data for slave information, it allows
    spi-altera to add new spi devices once master registration is done.
    
    Signed-off-by: Wu Hao <hao.wu@intel.com>
    Signed-off-by: Xu Yilun <yilun.xu@intel.com>
    Signed-off-by: Matthew Gerlach <matthew.gerlach@linux.intel.com>
    Signed-off-by: Russ Weight <russell.h.weight@intel.com>
    Reviewed-by: Tom Rix <trix@redhat.com>
    Link: https://lore.kernel.org/r/1591845911-10197-4-git-send-email-yilun.xu@intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/linux/spi/altera.h
+++ include/linux/spi/altera.h
@@ -15,11 +15,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @mode_bits:		Mode bits of SPI master.
  * @num_chipselect:	Number of chipselects.
  * @bits_per_word_mask:	bitmask of supported bits_per_word for transfers.
+ * @num_devices:	Number of devices that shall be added when the driver
+ *			is probed.
+ * @devices:		The devices to add.
  */
 struct altera_spi_platform_data {
 	u16				mode_bits;
 	u16				num_chipselect;
 	u32				bits_per_word_mask;
+	u16				num_devices;
+	struct spi_board_info		*devices;
 };
 
 #endif /* __LINUX_SPI_ALTERA_H */
