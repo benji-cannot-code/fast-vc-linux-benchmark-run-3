76c1a664453b351fd17df58851a84461f085eaf6
Nathan Lynch <nathanl@linux.ibm.com>
2020-06-12 00:12:31 -0500

    powerpc/numa: remove start/stop_topology_update()
    These APIs have become no-ops, so remove them and all call sites.
    
    Signed-off-by: Nathan Lynch <nathanl@linux.ibm.com>
    Reviewed-by: Srikar Dronamraju <srikar@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200612051238.1007764-12-nathanl@linux.ibm.com

--- arch/powerpc/mm/numa.c
+++ arch/powerpc/mm/numa.c
@@ -1158,8 +1158,6 @@ static long vphn_get_associativity(unsigned long cpu,
 			, rc);
 		break;
 	}
-
-	stop_topology_update();
 out:
 	return rc;
 }
@@ -1213,22 +1211,6 @@ int arch_update_cpu_topology(void)
 	return numa_update_cpu_topology(true);
 }
 
-/*
- * Start polling for associativity changes.
- */
-int start_topology_update(void)
-{
-	return 0;
-}
-
-/*
- * Disable polling for VPHN associativity changes.
- */
-int stop_topology_update(void)
-{
-	return 0;
-}
-
 int prrn_is_enabled(void)
 {
 	return 0;
@@ -1236,8 +1218,6 @@ int prrn_is_enabled(void)
 
 static int topology_update_init(void)
 {
-	start_topology_update();
-
 	topology_inited = 1;
 	return 0;
 }
--- arch/powerpc/platforms/pseries/mobility.c
+++ arch/powerpc/platforms/pseries/mobility.c
@@ -389,8 +389,6 @@ static ssize_t migration_store(struct class *class,
 	if (rc)
 		return rc;
 
-	stop_topology_update();
-
 	do {
 		rc = rtas_ibm_suspend_me(streamid);
 		if (rc == -EAGAIN)
@@ -401,8 +399,6 @@ static ssize_t migration_store(struct class *class,
 		return rc;
 
 	post_mobility_fixup();
-
-	start_topology_update();
 
 	return count;
 }
