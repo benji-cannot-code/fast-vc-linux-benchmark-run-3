f460f7f42388554f132e926360f3e9d5f3bb809c
Nathan Lynch <nathanl@linux.ibm.com>
2020-06-12 00:12:34 -0500

    powerpc/numa: remove arch_update_cpu_topology
    Since arch_update_cpu_topology() doesn't do anything on powerpc now,
    remove it and associated dead code.
    
    Signed-off-by: Nathan Lynch <nathanl@linux.ibm.com>
    Reviewed-by: Srikar Dronamraju <srikar@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200612051238.1007764-15-nathanl@linux.ibm.com

--- arch/powerpc/include/asm/topology.h
+++ arch/powerpc/include/asm/topology.h
@@ -44,7 +44,6 @@ extern void __init dump_numa_cpu_topology(void);
 
 extern int sysfs_add_device_to_node(struct device *dev, int nid);
 extern void sysfs_remove_device_from_node(struct device *dev, int nid);
-extern int numa_update_cpu_topology(bool cpus_locked);
 
 static inline void update_numa_cpu_lookup_table(unsigned int cpu, int node)
 {
@@ -77,11 +76,6 @@ static inline int sysfs_add_device_to_node(struct device *dev, int nid)
 static inline void sysfs_remove_device_from_node(struct device *dev,
 						int nid)
 {
-}
-
-static inline int numa_update_cpu_topology(bool cpus_locked)
-{
-	return 0;
 }
 
 static inline void update_numa_cpu_lookup_table(unsigned int cpu, int node) {}
