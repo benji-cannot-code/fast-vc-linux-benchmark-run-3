f6a92097724b0a16c7a27dd5134ef57b2c4b7a09
Nathan Lynch <nathanl@linux.ibm.com>
2020-06-12 00:12:21 -0500

    powerpc/pseries: remove cede offline state for CPUs
    This effectively reverts commit 3aa565f53c39 ("powerpc/pseries: Add
    hooks to put the CPU into an appropriate offline state"), which added
    an offline mode for CPUs which uses the H_CEDE hcall instead of the
    architected stop-self RTAS function in order to facilitate "folding"
    of dedicated mode processors on PowerVM platforms to achieve energy
    savings. This has been the default offline mode since its
    introduction.
    
    There's nothing about stop-self that would prevent the hypervisor from
    achieving the energy savings available via H_CEDE, so the original
    premise of this change appears to be flawed.
    
    I also have encountered the claim that the transition to and from
    ceded state is much faster than stop-self/start-cpu. Certainly we
    would not want to use stop-self as an *idle* mode. That is what H_CEDE
    is for. However, this difference is insignificant in the context of
    Linux CPU hotplug, where the latency of an offline or online operation
    on current systems is on the order of 100ms, mainly attributable to
    all the various subsystems' cpuhp callbacks.
    
    The cede offline mode also prevents accurate accounting, as discussed
    before:
    https://lore.kernel.org/linuxppc-dev/1571740391-3251-1-git-send-email-ego@linux.vnet.ibm.com/
    
    Unconditionally use stop-self to offline processor threads. This is
    the architected method for offlining CPUs on PAPR systems.
    
    The "cede_offline" boot parameter is rendered obsolete.
    
    Removing this code enables the removal of the partition suspend code
    which temporarily onlines all present CPUs.
    
    Fixes: 3aa565f53c39 ("powerpc/pseries: Add hooks to put the CPU into an appropriate offline state")
    Signed-off-by: Nathan Lynch <nathanl@linux.ibm.com>
    Reviewed-by: Gautham R. Shenoy <ego@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200612051238.1007764-2-nathanl@linux.ibm.com

--- arch/powerpc/platforms/pseries/pmem.c
+++ arch/powerpc/platforms/pseries/pmem.c
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/topology.h>
 
 #include "pseries.h"
-#include "offline_states.h"
 
 static struct device_node *pmem_node;
 
