d70cd550a3f375b711de82104545e75a5b11f00e
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2020-06-14 23:12:21 +0900

    ALSA: firewire-motu: wait for notification when changing clock configuration for protocol v3
    It costs expensive to change clock configuration for models of protocol
    version 3. In current implementation, speculative strategy is used to
    finish the operation; just waiting for 4 seconds. As long as I investigate,
    when accepting and changing clock status actually, the device sends
    notification with mask.
    
    This commit uses wise way to wait for the notification after changing
    sampling clock rate during 4 seconds.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Link: https://lore.kernel.org/r/20200614141221.53527-1-o-takashi@sakamocchi.jp
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/firewire/motu/motu-protocol-v3.c
+++ sound/firewire/motu/motu-protocol-v3.c
@@ -25,6 +25,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  V3_NO_ADAT_OPT_OUT_IFACE_A	0x00040000
 #define  V3_NO_ADAT_OPT_OUT_IFACE_B	0x00400000
 
+#define V3_MSG_FLAG_CLK_CHANGED		0x00000002
+#define V3_CLK_WAIT_MSEC		4000
+
 int snd_motu_protocol_v3_get_clock_rate(struct snd_motu *motu,
 					unsigned int *rate)
 {
@@ -80,9 +83,16 @@ int snd_motu_protocol_v3_set_clock_rate(struct snd_motu *motu,
 		return err;
 
 	if (need_to_wait) {
-		/* Cost expensive. */
-		if (msleep_interruptible(4000) > 0)
-			return -EINTR;
+		int result;
+
+		motu->msg = 0;
+		result = wait_event_interruptible_timeout(motu->hwdep_wait,
+					motu->msg & V3_MSG_FLAG_CLK_CHANGED,
+					msecs_to_jiffies(V3_CLK_WAIT_MSEC));
+		if (result < 0)
+			return result;
+		if (result == 0)
+			return -ETIMEDOUT;
 	}
 
 	return 0;
