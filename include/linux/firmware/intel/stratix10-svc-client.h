da7b3eedad7cea04f47dd853c21a1e7abf62168b
Richard Gong <richard.gong@intel.com>
2020-06-15 09:29:05 -0500

    firmware: stratix10-svc: correct reconfig flag and timeout values
    Correct the incorrect flag value for COMMAND_RECONFIG_FLAG_PARTIAL and
    increase FPGA reconfig timeout values so that Intel service layer and
    FPGA manager drivers can work with all versions of firmware.
    
    Signed-off-by: Richard Gong <richard.gong@intel.com>
    Link: https://lore.kernel.org/r/1592231348-31334-2-git-send-email-richard.gong@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/firmware/intel/stratix10-svc-client.h
+++ include/linux/firmware/intel/stratix10-svc-client.h
@@ -55,18 +55,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Flag bit for COMMAND_RECONFIG
  *
  * COMMAND_RECONFIG_FLAG_PARTIAL:
- * Set to FPGA configuration type (full or partial), the default
- * is full reconfig.
+ * Set to FPGA configuration type (full or partial).
  */
-#define COMMAND_RECONFIG_FLAG_PARTIAL	0
+#define COMMAND_RECONFIG_FLAG_PARTIAL	1
 
 /**
  * Timeout settings for service clients:
  * timeout value used in Stratix10 FPGA manager driver.
  * timeout value used in RSU driver
  */
-#define SVC_RECONFIG_REQUEST_TIMEOUT_MS         100
-#define SVC_RECONFIG_BUFFER_TIMEOUT_MS          240
+#define SVC_RECONFIG_REQUEST_TIMEOUT_MS         300
+#define SVC_RECONFIG_BUFFER_TIMEOUT_MS          720
 #define SVC_RSU_REQUEST_TIMEOUT_MS              300
 
 struct stratix10_svc_chan;
