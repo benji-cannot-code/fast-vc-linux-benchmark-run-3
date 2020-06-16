e257d5fb4d83da98679f8592e947201b95fdc36b
Amelie Delaunay <amelie.delaunay@st.com>
2020-06-16 16:07:15 +0200

    usb: dwc2: override PHY input signals with usb role switch support
    This patch adds support for usb role switch to dwc2, by using overriding
    control of the PHY voltage valid and ID input signals.
    
    iddig signal (ID) can be overridden:
    - when setting GUSBCFG_FORCEHOSTMODE, iddig input pin is overridden with 1;
    - when setting GUSBCFG_FORCEDEVMODE, iddig input pin is overridden with 0.
    
    avalid/bvalid/vbusvalid signals can be overridden respectively with:
    - GOTGCTL_AVALOEN + GOTGCTL_AVALOVAL
    - GOTGCTL_BVALOEN + GOTGCTL_BVALOVAL
    - GOTGCTL_VBVALEN + GOTGCTL_VBVALOVAL
    
    It is possible to determine valid sessions thanks to usb role switch:
    - if USB_ROLE_NONE then !avalid && !bvalid && !vbusvalid
    - if USB_ROLE_DEVICE then !avalid && bvalid && vbusvalid
    - if USB_ROLE_HOST then avalid && !bvalid && vbusvalid
    
    Acked-by: Minas Harutyunyan <hminas@synopsys.com>
    Signed-off-by: Amelie Delaunay <amelie.delaunay@st.com>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- drivers/usb/dwc2/platform.c
+++ drivers/usb/dwc2/platform.c
@@ -318,6 +318,8 @@ static int dwc2_driver_remove(struct platform_device *dev)
 	if (hsotg->params.activate_stm_id_vb_detection)
 		regulator_disable(hsotg->usb33d);
 
+	dwc2_drd_exit(hsotg);
+
 	if (hsotg->ll_hw_enabled)
 		dwc2_lowlevel_hw_disable(hsotg);
 
@@ -534,6 +536,13 @@ static int dwc2_driver_probe(struct platform_device *dev)
 		dwc2_writel(hsotg, ggpio, GGPIO);
 	}
 
+	retval = dwc2_drd_init(hsotg);
+	if (retval) {
+		if (retval != -EPROBE_DEFER)
+			dev_err(hsotg->dev, "failed to initialize dual-role\n");
+		goto error_init;
+	}
+
 	if (hsotg->dr_mode != USB_DR_MODE_HOST) {
 		retval = dwc2_gadget_init(hsotg);
 		if (retval)
@@ -606,6 +615,8 @@ static int __maybe_unused dwc2_suspend(struct device *dev)
 
 	if (is_device_mode)
 		dwc2_hsotg_suspend(dwc2);
+
+	dwc2_drd_suspend(dwc2);
 
 	if (dwc2->params.activate_stm_id_vb_detection) {
 		unsigned long flags;
@@ -686,6 +697,8 @@ static int __maybe_unused dwc2_resume(struct device *dev)
 
 	/* Need to restore FORCEDEVMODE/FORCEHOSTMODE */
 	dwc2_force_dr_mode(dwc2);
+
+	dwc2_drd_resume(dwc2);
 
 	if (dwc2_is_device_mode(dwc2))
 		ret = dwc2_hsotg_resume(dwc2);
