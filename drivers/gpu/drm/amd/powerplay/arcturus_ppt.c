35b2cf7b52db9109a8243630d5bb590d731f07cc
Kevin Wang <kevin1.wang@amd.com>
2020-06-16 13:50:28 +0800

    drm/amd/smu: unify pptable_func{} callback interface
    the pptable_func callback sets should be has unify interface,
    so use "smu" as the pptable_func interface first parameter.
    
    fix interfaces:
    1. i2c_eeprom_init
    2. i2c_eeprom_fini
    
    Signed-off-by: Kevin Wang <kevin1.wang@amd.com>
    Reviewed-by: Huang Rui <ray.huang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/arcturus_ppt.c
+++ drivers/gpu/drm/amd/powerplay/arcturus_ppt.c
@@ -2339,7 +2339,7 @@ static bool arcturus_i2c_adapter_is_added(struct i2c_adapter *control)
 	return control->dev.parent == &adev->pdev->dev;
 }
 
-static int arcturus_i2c_eeprom_control_init(struct i2c_adapter *control)
+static int arcturus_i2c_eeprom_control_init(struct smu_context *smu, struct i2c_adapter *control)
 {
 	struct amdgpu_device *adev = to_amdgpu_device(control);
 	int res;
@@ -2361,7 +2361,7 @@ static int arcturus_i2c_eeprom_control_init(struct i2c_adapter *control)
 	return res;
 }
 
-static void arcturus_i2c_eeprom_control_fini(struct i2c_adapter *control)
+static void arcturus_i2c_eeprom_control_fini(struct smu_context *smu, struct i2c_adapter *control)
 {
 	if (!arcturus_i2c_adapter_is_added(control))
 		return;
