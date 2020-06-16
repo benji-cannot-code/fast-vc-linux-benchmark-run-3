5c3735eea34afe20df6e27f2995cfe4a60cd216b
Ben Skeggs <bskeggs@redhat.com>
2020-06-16 15:15:58 +1000

    drm/nouveau/pmu/gm20x: don't pretend we support loading with our custom FW
    It technically loads, and runs, but is ultimately pointless outside of
    a very narrow window (fanless systems where one wants to attempt using
    the, broken for a lot of gm20x, memory reclocking code).
    
    It's also potentially dangerous to override the VBIOS-provided "Pre-OS"
    PMU, which would be responsible for fan control otherwise.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/pmu/gm200.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/pmu/gm200.c
@@ -25,20 +25,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "priv.h"
 
 static const struct nvkm_pmu_func
-gp100_pmu = {
+gm200_pmu = {
 	.flcn = &gt215_pmu_flcn,
 	.enabled = gf100_pmu_enabled,
 	.reset = gf100_pmu_reset,
 };
 
 static const struct nvkm_pmu_fwif
-gp100_pmu_fwif[] = {
-	{ -1, gf100_pmu_nofw, &gp100_pmu },
+gm200_pmu_fwif[] = {
+	{ -1, gf100_pmu_nofw, &gm200_pmu },
 	{}
 };
 
 int
-gp100_pmu_new(struct nvkm_device *device, int index, struct nvkm_pmu **ppmu)
+gm200_pmu_new(struct nvkm_device *device, int index, struct nvkm_pmu **ppmu)
 {
-	return nvkm_pmu_new_(gp100_pmu_fwif, device, index, ppmu);
+	return nvkm_pmu_new_(gm200_pmu_fwif, device, index, ppmu);
 }
