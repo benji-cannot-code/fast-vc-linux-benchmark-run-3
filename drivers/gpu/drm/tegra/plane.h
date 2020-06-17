43d23e899964afb58b1201be69cbf6a685abab06
Dmitry Osipenko <digetx@gmail.com>
2020-06-18 02:40:38 +0300

    drm/tegra: plane: Rename bottom_up to reflect_y
    This makes the naming consistent with the DRM core.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/plane.h
+++ drivers/gpu/drm/tegra/plane.h
@@ -47,7 +47,7 @@ struct tegra_plane_state {
 	u32 format;
 	u32 swap;
 
-	bool bottom_up;
+	bool reflect_y;
 
 	/* used for legacy blending support only */
 	struct tegra_plane_legacy_blending_state blending[2];
