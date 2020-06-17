1daca3671aa68bba0f29ee1db8f59e592187c95a
Dmitry Osipenko <digetx@gmail.com>
2020-06-18 02:40:40 +0300

    drm/tegra: plane: Support 180° rotation
    Combining horizontal and vertical reflections gives us 180 degrees of
    rotation. Both reflection modes are already supported, and thus, we just
    need to mark the 180 rotation mode as supported. The 180 rotation mode is
    needed for devices like Nexus 7 tablet, which have display panel mounted
    upside-down.
    
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/dc.c
+++ drivers/gpu/drm/tegra/dc.c
@@ -817,6 +817,7 @@ static struct drm_plane *tegra_primary_plane_create(struct drm_device *drm,
 	err = drm_plane_create_rotation_property(&plane->base,
 						 DRM_MODE_ROTATE_0,
 						 DRM_MODE_ROTATE_0 |
+						 DRM_MODE_ROTATE_180 |
 						 DRM_MODE_REFLECT_X |
 						 DRM_MODE_REFLECT_Y);
 	if (err < 0)
@@ -1106,6 +1107,7 @@ static struct drm_plane *tegra_dc_overlay_plane_create(struct drm_device *drm,
 	err = drm_plane_create_rotation_property(&plane->base,
 						 DRM_MODE_ROTATE_0,
 						 DRM_MODE_ROTATE_0 |
+						 DRM_MODE_ROTATE_180 |
 						 DRM_MODE_REFLECT_X |
 						 DRM_MODE_REFLECT_Y);
 	if (err < 0)
