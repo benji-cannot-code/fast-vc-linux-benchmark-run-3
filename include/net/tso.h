45827924b28afc9b348530129f643e2739531190
Eric Dumazet <edumazet@google.com>
2020-06-17 20:53:23 -0700

    net: tso: shrink struct tso_t
    size field can be an int, no need for size_t
    
    Removes a 32bit hole on 64bit kernels.
    
    And align fields for better readability.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/tso.h
+++ include/net/tso.h
@@ -8,12 +8,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TSO_HEADER_SIZE		256
 
 struct tso_t {
-	int next_frag_idx;
-	void *data;
-	size_t size;
-	u16 ip_id;
-	bool ipv6;
-	u32 tcp_seq;
+	int	next_frag_idx;
+	int	size;
+	void	*data;
+	u16	ip_id;
+	bool	ipv6;
+	u32	tcp_seq;
 };
 
 int tso_count_descs(struct sk_buff *skb);
