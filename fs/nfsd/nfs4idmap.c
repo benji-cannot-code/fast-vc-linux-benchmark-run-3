ba2510b9aabe620f79f5f1f809fdd134d9ff2f21
Xu Wang <vulab@iscas.ac.cn>
2020-06-18 01:56:13 +0000

    nfsd: Use seq_putc() in two functions
    A single character (line break) should be put into a sequence.
    Thus use the corresponding function "seq_putc()".
    
    Signed-off-by: Xu Wang <vulab@iscas.ac.cn>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- fs/nfsd/nfs4idmap.c
+++ fs/nfsd/nfs4idmap.c
@@ -169,7 +169,7 @@ idtoname_show(struct seq_file *m, struct cache_detail *cd, struct cache_head *h)
 			ent->id);
 	if (test_bit(CACHE_VALID, &h->flags))
 		seq_printf(m, " %s", ent->name);
-	seq_printf(m, "\n");
+	seq_putc(m, '\n');
 	return 0;
 }
 
@@ -347,7 +347,7 @@ nametoid_show(struct seq_file *m, struct cache_detail *cd, struct cache_head *h)
 			ent->name);
 	if (test_bit(CACHE_VALID, &h->flags))
 		seq_printf(m, " %u", ent->id);
-	seq_printf(m, "\n");
+	seq_putc(m, '\n');
 	return 0;
 }
 
