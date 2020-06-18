a6c1bb49a003bd3f9c302063b44232ae1f376eea
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2020-06-18 11:42:54 +0200

    USB: rename USB OTG hub configuration option
    The USB OTG code has the ability to disable external hubs, but the
    configuration option for it is oddly named.  Rename it to be more
    obvious as to what it does.
    
    Cc: Thomas Bogendoerfer <tsbogend@alpha.franken.de>
    Cc: Bin Liu <b-liu@ti.com>
    Cc: Paul Cercueil <paul@crapouillou.net>
    Cc: Alan Stern <stern@rowland.harvard.edu>
    Cc: Eugeniu Rosca <erosca@de.adit-jv.com>
    Cc: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Cc: David Heinzelmann <heinzelmann.david@gmail.com>
    Cc: "Lee, Chiasheng" <chiasheng.lee@intel.com>
    Cc: Keiya Nobuta <nobuta.keiya@fujitsu.com>
    Cc: Hardik Gajjar <hgajjar@de.adit-jv.com>
    Link: https://lore.kernel.org/r/20200618094300.1887727-3-gregkh@linuxfoundation.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1835,7 +1835,7 @@ static int hub_probe(struct usb_interface *intf, const struct usb_device_id *id)
 		return -E2BIG;
 	}
 
-#ifdef	CONFIG_USB_OTG_BLACKLIST_HUB
+#ifdef	CONFIG_USB_OTG_DISABLE_EXTERNAL_HUB
 	if (hdev->parent) {
 		dev_warn(&intf->dev, "ignoring external hub\n");
 		return -ENODEV;
