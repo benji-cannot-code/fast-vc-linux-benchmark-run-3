b5c77730c3533892d71182aca65ffd288d130f5f
James Zhu <James.Zhu@amd.com>
2020-06-17 22:25:40 -0400

    drm/amdgpu/jpeg2.0: fix no previous prototype for functions
    Include header file jpeg_v2_0.h.
    
    Signed-off-by: James Zhu <James.Zhu@amd.com>
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/jpeg_v2_0.c
+++ drivers/gpu/drm/amd/amdgpu/jpeg_v2_0.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "amdgpu_pm.h"
 #include "soc15.h"
 #include "soc15d.h"
+#include "jpeg_v2_0.h"
 
 #include "vcn/vcn_2_0_0_offset.h"
 #include "vcn/vcn_2_0_0_sh_mask.h"
