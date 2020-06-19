175e25ea13b3dd7ea5dcb99f1f6010098c3b9d8d
Ben Skeggs <bskeggs@redhat.com>
2020-06-20 08:44:11 +1000

    drm/nouveau/kms/nv50-: convert wndw csc_clr() to new push macros
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>
    Reviewed-by: Lyude Paul <lyude@redhat.com>

--- drivers/gpu/drm/nouveau/dispnv50/wndwc37e.c
+++ drivers/gpu/drm/nouveau/dispnv50/wndwc37e.c
@@ -30,9 +30,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <nvif/clc37e.h>
 #include <nvif/pushc37b.h>
 
-static void
+static int
 wndwc37e_csc_clr(struct nv50_wndw *wndw)
 {
+	return 0;
 }
 
 static int
