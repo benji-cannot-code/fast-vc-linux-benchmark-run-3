780d3a2454b03f2b38a89d9e0fd7fd2d830e4b0f
Andrey Lebedev <andrey@lebedev.lt>
2020-06-19 10:58:59 +0300

    drm/lima: Expose job_hang_limit module parameter
    Some pp or gp jobs can be successfully repeated even after they time outs.
    Introduce lima module parameter to specify number of times a job can hang
    before being dropped.
    
    Signed-off-by: Andrey Lebedev <andrey@lebedev.lt>
    Signed-off-by: Qiang Yu <yuq825@gmail.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200619075900.3030696-1-andrey.lebedev@gmail.com

--- drivers/gpu/drm/lima/lima_drv.h
+++ drivers/gpu/drm/lima/lima_drv.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern int lima_sched_timeout_ms;
 extern uint lima_heap_init_nr_pages;
 extern uint lima_max_error_tasks;
+extern uint lima_job_hang_limit;
 
 struct lima_vm;
 struct lima_bo;
