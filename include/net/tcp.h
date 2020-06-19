c01612bd92e399940a85535da5d2d28d5df9bae9
Eric Dumazet <edumazet@google.com>
2020-06-19 12:12:34 -0700

    tcp: remove indirect calls for icsk->icsk_af_ops->queue_xmit
    Mitigate RETPOLINE costs in __tcp_transmit_skb()
    by using INDIRECT_CALL_INET() wrapper.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/skbuff.h>
 #include <linux/kref.h>
 #include <linux/ktime.h>
+#include <linux/indirect_call_wrapper.h>
 
 #include <net/inet_connection_sock.h>
 #include <net/inet_timewait_sock.h>
