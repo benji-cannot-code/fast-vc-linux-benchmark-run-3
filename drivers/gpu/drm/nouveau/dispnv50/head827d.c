7d0fd86994ec4b46457c675258de06860d0e88f2
Ben Skeggs <bskeggs@redhat.com>
2020-06-21 09:17:11 +1000

    drm/nouveau/kms/nv50-: convert core head_curs_clr() to new push macros
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>
    Reviewed-by: Lyude Paul <lyude@redhat.com>

--- drivers/gpu/drm/nouveau/dispnv50/head827d.c
+++ drivers/gpu/drm/nouveau/dispnv50/head827d.c
@@ -25,18 +25,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <nvif/push507c.h>
 
-static void
+static int
 head827d_curs_clr(struct nv50_head *head)
 {
-	struct nv50_dmac *core = &nv50_disp(head->base.base.dev)->core->chan;
-	u32 *push;
-	if ((push = evo_wait(core, 4))) {
-		evo_mthd(push, 0x0880 + head->base.index * 0x400, 1);
-		evo_data(push, 0x05000000);
-		evo_mthd(push, 0x089c + head->base.index * 0x400, 1);
-		evo_data(push, 0x00000000);
-		evo_kick(push, core);
-	}
+	struct nvif_push *push = nv50_disp(head->base.base.dev)->core->chan.push;
+	const int i = head->base.index;
+	int ret;
+
+	if ((ret = PUSH_WAIT(push, 4)))
+		return ret;
+
+	PUSH_NVSQ(push, NV827D, 0x0880 + (i * 0x400), 0x05000000);
+	PUSH_NVSQ(push, NV827D, 0x089c + (i * 0x400), 0x00000000);
+	return 0;
 }
 
 static int
