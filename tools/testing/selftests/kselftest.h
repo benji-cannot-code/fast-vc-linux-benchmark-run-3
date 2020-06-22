58ab962e1d51acf55604a8f9964dc2bfa2ff680c
Kees Cook <keescook@chromium.org>
2020-06-22 11:16:48 -0700

    selftests/harness: Switch to TAP output
    Using the kselftest_harness.h would result in non-TAP test reporting,
    which didn't make much sense given that all the requirements for using
    the low-level API were met. Switch to using ksft_*() helpers while
    retaining as much of a human-readability as possible.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>

--- tools/testing/selftests/kselftest.h
+++ tools/testing/selftests/kselftest.h
@@ -1,8 +1,9 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * kselftest.h:	kselftest framework return codes to include from
- *		selftests.
+ * kselftest.h:	low-level kselftest framework to include from
+ *		selftest programs. When possible, please use
+ *		kselftest_harness.h instead.
  *
  * Copyright (c) 2014 Shuah Khan <shuahkh@osg.samsung.com>
  * Copyright (c) 2014 Samsung Electronics Co., Ltd.
