88ec125af209ff34335a7e14169e3afb15b25774
Arnaldo Carvalho de Melo <acme@redhat.com>
2020-06-23 10:09:03 -0300

    perf expr: Add missing headers noticed when building with NO_LIBBPF=1
    These will break the build as soon as we stop disabling all warnings
    when building flex and bison generated files, so add them before we do
    that to keep the tree bisectable.
    
    Noticed when building on centos:7 with NO_LIBBPF=1:
    
      util/expr.c: In function 'key_equal':
      util/expr.c:29:2: error: implicit declaration of function 'strcmp' [-Werror=implicit-function-declaration]
        return !strcmp((const char *)key1, (const char *)key2);
        ^
      util/expr.c: In function 'expr__add_id':
      util/expr.c:40:3: error: implicit declaration of function 'malloc' [-Werror=implicit-function-declaration]
         val_ptr = malloc(sizeof(double));
         ^
      util/expr.c:40:13: error: incompatible implicit declaration of built-in function 'malloc' [-Werror]
         val_ptr = malloc(sizeof(double));
                   ^
      util/expr.c:42:12: error: 'ENOMEM' undeclared (first use in this function)
          return -ENOMEM;
                  ^
    Signed-off-by: Ian Rogers <irogers@google.com>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Jin Yao <yao.jin@linux.intel.com>
    Cc: John Garry <john.garry@huawei.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/expr.c
+++ tools/perf/util/expr.c
@@ -2,6 +2,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
 #include <stdbool.h>
 #include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
 #include "expr.h"
 #include "expr-bison.h"
 #include "expr-flex.h"
