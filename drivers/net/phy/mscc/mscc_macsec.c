390b71ce22ef1b608a8f66197c8299ef0209895a
Antoine Tenart <antoine.tenart@bootlin.com>
2020-06-23 16:30:08 +0200

    net: phy: mscc: fix copyright and author information in MACsec
    All headers in the MSCC PHY driver have been copied and pasted from the
    original mscc.c file. However the information is not necessarily
    correct, as in the MACsec support. Fix this.
    
    Signed-off-by: Antoine Tenart <antoine.tenart@bootlin.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/mscc/mscc_macsec.c
+++ drivers/net/phy/mscc/mscc_macsec.c
@@ -1,11 +1,11 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: (GPL-2.0 OR MIT)
 /*
- * Driver for Microsemi VSC85xx PHYs
+ * Driver for Microsemi VSC85xx PHYs - MACsec support
  *
- * Author: Nagaraju Lakkaraju
+ * Author: Antoine Tenart
  * License: Dual MIT/GPL
- * Copyright (c) 2016 Microsemi Corporation
+ * Copyright (c) 2020 Microsemi Corporation
  */
 
 #include <linux/phy.h>
