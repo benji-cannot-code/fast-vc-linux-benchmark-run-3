56d4f17251b73d0966beb19c59c615036c567ad2
Pratyush Yadav <p.yadav@ti.com>
2020-06-24 00:00:15 +0530

    spi: spi-mem: allow specifying a command's extension
    In xSPI mode, flashes expect 2-byte opcodes. The second byte is called
    the "command extension". There can be 3 types of extensions in xSPI:
    repeat, invert, and hex. When the extension type is "repeat", the same
    opcode is sent twice. When it is "invert", the second byte is the
    inverse of the opcode. When it is "hex" an additional opcode byte based
    is sent with the command whose value can be anything.
    
    So, make opcode a 16-bit value and add a 'nbytes', similar to how
    multiple address widths are handled.
    
    Some places use sizeof(op->cmd.opcode). Replace them with op->cmd.nbytes
    
    The spi-mxic and spi-zynq-qspi drivers directly use op->cmd.opcode as a
    buffer. Now that opcode is a 2-byte field, this can result in different
    behaviour depending on if the machine is little endian or big endian.
    Extract the opcode in a local 1-byte variable and use that as the buffer
    instead. Both these drivers would reject multi-byte opcodes in their
    supports_op() hook anyway, so we only need to worry about single-byte
    opcodes for now.
    
    The above two changes are put in this commit to keep the series
    bisectable.
    
    Signed-off-by: Pratyush Yadav <p.yadav@ti.com>
    Reviewed-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Link: https://lore.kernel.org/r/20200623183030.26591-3-p.yadav@ti.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/linux/spi/spi-mem.h
+++ include/linux/spi/spi-mem.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	{							\
 		.buswidth = __buswidth,				\
 		.opcode = __opcode,				\
+		.nbytes = 1,					\
 	}
 
 #define SPI_MEM_OP_ADDR(__nbytes, __val, __buswidth)		\
@@ -70,6 +71,8 @@ enum spi_mem_data_dir {
 
 /**
  * struct spi_mem_op - describes a SPI memory operation
+ * @cmd.nbytes: number of opcode bytes (only 1 or 2 are valid). The opcode is
+ *		sent MSB-first.
  * @cmd.buswidth: number of IO lines used to transmit the command
  * @cmd.opcode: operation opcode
  * @cmd.dtr: whether the command opcode should be sent in DTR mode or not
@@ -95,9 +98,10 @@ enum spi_mem_data_dir {
  */
 struct spi_mem_op {
 	struct {
+		u8 nbytes;
 		u8 buswidth;
 		u8 dtr : 1;
-		u8 opcode;
+		u16 opcode;
 	} cmd;
 
 	struct {
