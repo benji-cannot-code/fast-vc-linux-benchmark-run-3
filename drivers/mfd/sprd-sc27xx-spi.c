cd0537938b0b9c20268dff37f9b23183f3876d9c
Lee Jones <lee.jones@linaro.org>
2020-06-23 10:08:41 +0100

    mfd: sprd-sc27xx-spi: Fix symbol 'sprd_pmic_detect_charger_type' was not declared warning
    Sparse reports:
    
     drivers/mfd/sprd-sc27xx-spi.c:59:23: warning: symbol 'sprd_pmic_detect_charger_type' was not declared. Should it be static?
    
    ... due to a missing header file.
    
    Cc: Orson Zhai <orsonzhai@gmail.com>
    Cc: Chunyan Zhang <zhang.lyra@gmail.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Reviewed-by: Baolin Wang <baolin.wang7@gmail.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/sprd-sc27xx-spi.c
+++ drivers/mfd/sprd-sc27xx-spi.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mfd/core.h>
+#include <linux/mfd/sc27xx-pmic.h>
 #include <linux/of_device.h>
 #include <linux/of_platform.h>
 #include <linux/regmap.h>
