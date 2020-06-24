80d41dbb15f190be9a767a4a36885f112aba1fb8
YueHaibing <yuehaibing@huawei.com>
2020-06-24 07:18:21 +0000

    lan743x: Remove duplicated include from lan743x_main.c
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/microchip/lan743x_main.c
+++ drivers/net/ethernet/microchip/lan743x_main.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/rtnetlink.h>
 #include <linux/iopoll.h>
 #include <linux/crc16.h>
-#include <linux/of_net.h>
 #include "lan743x_main.h"
 #include "lan743x_ethtool.h"
 
