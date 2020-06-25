5236908774939c8f6bc60198eec0d1332a46249e
David Brazdil <dbrazdil@google.com>
2020-06-25 14:14:17 +0100

    KVM: arm64: Duplicate hyp/timer-sr.c for VHE/nVHE
    timer-sr.c contains a HVC handler for setting CNTVOFF_EL2 and two helper
    functions for controlling access to physical counter. The former is used by
    both VHE/nVHE and is duplicated, the latter are used only by nVHE and moved
    to nvhe/timer-sr.c.
    
    Signed-off-by: David Brazdil <dbrazdil@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20200625131420.71444-13-dbrazdil@google.com

--- arch/arm64/kvm/hyp/nvhe/timer-sr.c
+++ arch/arm64/kvm/hyp/nvhe/timer-sr.c
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/kvm_hyp.h>
 
-void __hyp_text __kvm_timer_set_cntvoff(u64 cntvoff)
+void __kvm_timer_set_cntvoff(u64 cntvoff)
 {
 	write_sysreg(cntvoff, cntvoff_el2);
 }
