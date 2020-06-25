e7a110ef6a89d38ac14314aeb1f6b09211cb918a
Andrew Scull <ascull@google.com>
2020-06-25 14:14:12 +0100

    KVM: arm64: Move hyp-init.S to nVHE
    hyp-init.S contains the identity mapped initialisation code for the
    non-VHE code that runs at EL2. It is only used for non-VHE.
    
    Adjust code that calls into this to use the prefixed symbol name.
    
    Signed-off-by: Andrew Scull <ascull@google.com>
    Signed-off-by: David Brazdil <dbrazdil@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20200625131420.71444-8-dbrazdil@google.com

--- arch/arm64/include/asm/kvm_asm.h
+++ arch/arm64/include/asm/kvm_asm.h
@@ -79,10 +79,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct kvm;
 struct kvm_vcpu;
 
-extern char __kvm_hyp_init[];
-extern char __kvm_hyp_init_end[];
-
+DECLARE_KVM_NVHE_SYM(__kvm_hyp_init);
 DECLARE_KVM_HYP_SYM(__kvm_hyp_vector);
+#define __kvm_hyp_init		CHOOSE_NVHE_SYM(__kvm_hyp_init)
 #define __kvm_hyp_vector	CHOOSE_HYP_SYM(__kvm_hyp_vector)
 
 #ifdef CONFIG_KVM_INDIRECT_VECTORS
