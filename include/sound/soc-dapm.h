4a10d2394973e3d6e4f6e9d3179e893b1b784aef
Tzung-Bi Shih <tzungbi@google.com>
2020-06-25 23:35:42 +0800

    ASoC: dapm: declare missing structure prototypes
    To fix compilation warnings:
    
    - struct 'snd_soc_pcm_runtime' declared inside parameter list will not
      be visible outside of this definition or declaration
    - struct 'soc_enum' declared inside parameter list will not be visible
      outside of this definition or declaration
    
    Declares the missing structure prototypes.
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@google.com>
    Link: https://lore.kernel.org/r/20200625153543.85039-3-tzungbi@google.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/sound/soc-dapm.h
+++ include/sound/soc-dapm.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/asoc.h>
 
 struct device;
+struct snd_soc_pcm_runtime;
+struct soc_enum;
 
 /* widget has no PM register bit */
 #define SND_SOC_NOPM	-1
