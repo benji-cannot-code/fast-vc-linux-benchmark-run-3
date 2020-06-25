3a539bc3aa9a92f9e91992515afce70238309eac
Linus Walleij <linus.walleij@linaro.org>
2020-06-25 22:21:49 +0200

    spi: lantiq-ssc: Convert to use GPIO descriptors
    This switches the Lantiq SSC driver over to use GPIO descriptor
    handling in the core.
    
    The driver was already utilizing the core to look up and request
    GPIOs from the device tree so this is a pretty small change
    just switching it over to use descriptors directly instead.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Cc: Hauke Mehrtens <hauke@hauke-m.de>
    Link: https://lore.kernel.org/r/20200625202149.209276-1-linus.walleij@linaro.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-lantiq-ssc.c
+++ drivers/spi/spi-lantiq-ssc.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/completion.h>
 #include <linux/spinlock.h>
 #include <linux/err.h>
-#include <linux/gpio.h>
 #include <linux/pm_runtime.h>
 #include <linux/spi/spi.h>
 
@@ -392,7 +391,7 @@ static int lantiq_ssc_setup(struct spi_device *spidev)
 	u32 gpocon;
 
 	/* GPIOs are used for CS */
-	if (gpio_is_valid(spidev->cs_gpio))
+	if (spidev->cs_gpiod)
 		return 0;
 
 	dev_dbg(spi->dev, "using internal chipselect %u\n", cs);
@@ -889,6 +888,7 @@ static int lantiq_ssc_probe(struct platform_device *pdev)
 
 	master->dev.of_node = pdev->dev.of_node;
 	master->num_chipselect = num_cs;
+	master->use_gpio_descriptors = true;
 	master->setup = lantiq_ssc_setup;
 	master->set_cs = lantiq_ssc_set_cs;
 	master->handle_err = lantiq_ssc_handle_err;
