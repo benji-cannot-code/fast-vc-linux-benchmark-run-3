038fd99de85fd5ae7f2665cdeaeb5caecd0ae1ed
Mark Starovoytov <mstarovoitov@marvell.com>
2020-06-26 21:40:35 +0300

    net: atlantic: fix typo in aq_ring_tx_clean
    This patch fixes a typo in aq_ring_tx_clean.
    stats is a union, so the typo doesn't cause any issues, but it's a typo
    nonetheless.
    
    Signed-off-by: Mark Starovoytov <mstarovoitov@marvell.com>
    Signed-off-by: Igor Russkikh <irusskikh@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/aquantia/atlantic/aq_ring.c
+++ drivers/net/ethernet/aquantia/atlantic/aq_ring.c
@@ -1,8 +1,9 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0-only
-/*
- * aQuantia Corporation Network Driver
- * Copyright (C) 2014-2019 aQuantia Corporation. All rights reserved
+/* Atlantic Network Driver
+ *
+ * Copyright (C) 2014-2019 aQuantia Corporation
+ * Copyright (C) 2019-2020 Marvell International Ltd.
  */
 
 /* File aq_ring.c: Definition of functions for Rx/Tx rings. */
@@ -280,7 +281,7 @@ bool aq_ring_tx_clean(struct aq_ring_s *self)
 		}
 
 		if (unlikely(buff->is_eop)) {
-			++self->stats.rx.packets;
+			++self->stats.tx.packets;
 			self->stats.tx.bytes += buff->skb->len;
 
 			dev_kfree_skb_any(buff->skb);
