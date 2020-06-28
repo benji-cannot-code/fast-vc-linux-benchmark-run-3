048af29263143d28488e656ed2bd326c206c0e91
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2020-06-28 13:36:54 +0100

    iio:adc:ingenic: drop of_match_ptr protection and include mod_devicetable.h
    Whilst it's unlikely this device will ever be instantiated using
    ACPI, there is little advantage not assigning the of_device_id table in all
    cases and it avoids providing an example that might be coppied into new
    drivers.
    
    Also include mod_devicetable.h whilst here as of_device_id is defined
    in there so including the header is best practice.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Cc: Artur Rojek <contact@artur-rojek.eu>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>

--- drivers/iio/adc/ingenic-adc.c
+++ drivers/iio/adc/ingenic-adc.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/iopoll.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/mod_devicetable.h>
 #include <linux/mutex.h>
 #include <linux/platform_device.h>
 
@@ -498,7 +499,6 @@ static int ingenic_adc_probe(struct platform_device *pdev)
 	return ret;
 }
 
-#ifdef CONFIG_OF
 static const struct of_device_id ingenic_adc_of_match[] = {
 	{ .compatible = "ingenic,jz4725b-adc", .data = &jz4725b_adc_soc_data, },
 	{ .compatible = "ingenic,jz4740-adc", .data = &jz4740_adc_soc_data, },
@@ -506,12 +506,11 @@ static const struct of_device_id ingenic_adc_of_match[] = {
 	{ },
 };
 MODULE_DEVICE_TABLE(of, ingenic_adc_of_match);
-#endif
 
 static struct platform_driver ingenic_adc_driver = {
 	.driver = {
 		.name = "ingenic-adc",
-		.of_match_table = of_match_ptr(ingenic_adc_of_match),
+		.of_match_table = ingenic_adc_of_match,
 	},
 	.probe = ingenic_adc_probe,
 };
