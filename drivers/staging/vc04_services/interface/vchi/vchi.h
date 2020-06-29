ae7d103d4b8cd67547fac259c0c38f61b0564d1b
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2020-06-29 17:09:33 +0200

    staging: vchi: Get rid of struct vchiq_instance forward declaration
    It's already forward declared in vchiq_if.h. So no need to insist
    
    Signed-off-by: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
    Link: https://lore.kernel.org/r/20200629150945.10720-36-nsaenzjulienne@suse.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vc04_services/interface/vchi/vchi.h
+++ drivers/staging/vc04_services/interface/vchi/vchi.h
@@ -6,13 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VCHI_H_
 
 /******************************************************************************
- * Global defs
- *****************************************************************************/
-
-// Opaque handle for a VCHIQ instance
-struct vchiq_instance;
-
-/******************************************************************************
  * Global funcs - implementation is specific to which side you are on
  * (local / remote)
  *****************************************************************************/
