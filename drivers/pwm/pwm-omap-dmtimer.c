30cb703eb6952016b2b41944524c0cf85ea85cdc
Lee Jones <lee.jones@linaro.org>
2020-06-29 13:47:52 +0100

    pwm: omap-dmtimer: Repair pwm_omap_dmtimer_chip's broken kerneldoc header
    Argument descriptions must be prepended with a '@' to be understood
    by the kerneldoc tooling/parsers/validators.
    
    Fixes the following W=1 warning:
    
      drivers/pwm/pwm-omap-dmtimer.c:70: warning: Function parameter or member 'dm_timer_pdev' not described in 'pwm_omap_dmtimer_chip'
    
    Cc: Tony Lindgren <tony@atomide.com>
    Cc: Joachim Eastwood <manabian@gmail.com>
    Cc: NeilBrown <neilb@suse.de>
    Cc: Grant Erickson <marathon96@gmail.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- drivers/pwm/pwm-omap-dmtimer.c
+++ drivers/pwm/pwm-omap-dmtimer.c
@@ -59,7 +59,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * @mutex:		Mutex to protect pwm apply state
  * @dm_timer:		Pointer to omap dm timer.
  * @pdata:		Pointer to omap dm timer ops.
- * dm_timer_pdev:	Pointer to omap dm timer platform device
+ * @dm_timer_pdev:	Pointer to omap dm timer platform device
  */
 struct pwm_omap_dmtimer_chip {
 	struct pwm_chip chip;
