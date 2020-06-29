401df09fa4ee531cce82fa9f968c7f260f6c7cef
Alexander Lobakin <alobakin@marvell.com>
2020-06-29 14:05:07 +0300

    net: qed: correct existing SPDX tags
    QLogic QED drivers source code is dual licensed under
    GPL-2.0/BSD-3-Clause.
    Correct already existing but wrong SPDX tags to match the actual
    license.
    
    Signed-off-by: Alexander Lobakin <alobakin@marvell.com>
    Signed-off-by: Igor Russkikh <irusskikh@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/qed/qed_fcoe_if.h
+++ include/linux/qed/qed_fcoe_if.h
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* SPDX-License-Identifier: GPL-2.0 */
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-3-Clause) */
+
 #ifndef _QED_FCOE_IF_H
 #define _QED_FCOE_IF_H
 #include <linux/types.h>
