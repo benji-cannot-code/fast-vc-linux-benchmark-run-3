e7d1fd181290e3f5509fccae4c2f1cb46364d12e
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2020-06-29 17:09:15 +0200

    staging: vc04_services: Get rid of vchi_cfg.h
    Nothing in it is being used.
    
    Signed-off-by: Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
    Link: https://lore.kernel.org/r/20200629150945.10720-18-nsaenzjulienne@suse.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vc04_services/interface/vchi/vchi.h
+++ drivers/staging/vc04_services/interface/vchi/vchi.h
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef VCHI_H_
 #define VCHI_H_
 
-#include "vchi_cfg.h"
 #include "vchi_common.h"
 
 /******************************************************************************
