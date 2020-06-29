6ecc1bb461d94c4634f73a463e70bf5087d3b706
Dave Chinner <dchinner@redhat.com>
2020-06-29 14:49:20 -0700

    xfs: remove xfs_inobp_check()
    This debug code is called on every xfs_iflush() call, which then
    checks every inode in the buffer for non-zero unlinked list field.
    Hence it checks every inode in the cluster buffer every time a
    single inode on that cluster it flushed. This is resulting in:
    
    -   38.91%     5.33%  [kernel]  [k] xfs_iflush
       - 17.70% xfs_iflush
          - 9.93% xfs_inobp_check
               4.36% xfs_buf_offset
    
    10% of the CPU time spent flushing inodes is repeatedly checking
    unlinked fields in the buffer. We don't need to do this.
    
    The other place we call xfs_inobp_check() is
    xfs_iunlink_update_dinode(), and this is after we've done this
    assert for the agino we are about to write into that inode:
    
    	ASSERT(xfs_verify_agino_or_null(mp, agno, next_agino));
    
    which means we've already checked that the agino we are about to
    write is not 0 on debug kernels. The inode buffer verifiers do
    everything else we need, so let's just remove this debug code.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/libxfs/xfs_inode_buf.c
+++ fs/xfs/libxfs/xfs_inode_buf.c
@@ -22,30 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/iversion.h>
 
 /*
- * Check that none of the inode's in the buffer have a next
- * unlinked field of 0.
- */
-#if defined(DEBUG)
-void
-xfs_inobp_check(
-	xfs_mount_t	*mp,
-	xfs_buf_t	*bp)
-{
-	int		i;
-	xfs_dinode_t	*dip;
-
-	for (i = 0; i < M_IGEO(mp)->inodes_per_cluster; i++) {
-		dip = xfs_buf_offset(bp, i * mp->m_sb.sb_inodesize);
-		if (!dip->di_next_unlinked)  {
-			xfs_alert(mp,
-	"Detected bogus zero next_unlinked field in inode %d buffer 0x%llx.",
-				i, (long long)bp->b_bn);
-		}
-	}
-}
-#endif
-
-/*
  * If we are doing readahead on an inode buffer, we might be in log recovery
  * reading an inode allocation buffer that hasn't yet been replayed, and hence
  * has not had the inode cores stamped into it. Hence for readahead, the buffer
