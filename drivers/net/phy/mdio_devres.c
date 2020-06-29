b8d8974657e1c50f95349746793b1fd2a8c8ab00
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2020-06-29 14:03:42 +0200

    phy: mdio: add kerneldoc for __devm_mdiobus_register()
    This function is not documented. Add a short kerneldoc description.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/phy/mdio_devres.c
+++ drivers/net/phy/mdio_devres.c
@@ -3,6 +3,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/phy.h>
 
+/**
+ * __devm_mdiobus_register - Resource-managed variant of mdiobus_register()
+ * @bus:	MII bus structure to register
+ * @owner:	Owning module
+ *
+ * Returns 0 on success, negative error number on failure.
+ */
 int __devm_mdiobus_register(struct mii_bus *bus, struct module *owner)
 {
 	int ret;
