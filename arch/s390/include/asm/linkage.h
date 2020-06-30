4ae97d525f17e024dac595928eee3f785c7e9557
Ilya Leoshkevich <iii@linux.ibm.com>
2020-06-30 20:52:03 +0200

    s390/kernel: expand exception table logic to allow new handling options
    This is a s390 port of commit 548acf19234d ("x86/mm: Expand the
    exception table logic to allow new handling options"), which is needed
    for implementing BPF_PROBE_MEM on s390.
    
    The new handler field is made 64-bit in order to allow pointing from
    dynamically allocated entries to handlers in kernel text. Unlike on x86,
    NULL is used instead of ex_handler_default. This is because exception
    tables are used by boot/text_dma.S, and it would be a pain to preserve
    ex_handler_default.
    
    The new infrastructure is ignored in early_pgm_check_handler, since
    there is no pt_regs.
    
    Signed-off-by: Ilya Leoshkevich <iii@linux.ibm.com>
    Reviewed-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- arch/s390/include/asm/linkage.h
+++ arch/s390/include/asm/linkage.h
@@ -15,9 +15,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define __EX_TABLE(_section, _fault, _target)				\
 	stringify_in_c(.section	_section,"a";)				\
-	stringify_in_c(.align	4;)					\
+	stringify_in_c(.align	8;)					\
 	stringify_in_c(.long	(_fault) - .;)				\
 	stringify_in_c(.long	(_target) - .;)				\
+	stringify_in_c(.quad	0;)					\
 	stringify_in_c(.previous)
 
 #define EX_TABLE(_fault, _target)					\
