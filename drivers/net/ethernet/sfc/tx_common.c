34a135c4aa1d4b0ebf421ad4146480c26900cbf5
Edward Cree <ecree@solarflare.com>
2020-06-30 13:12:49 +0100

    sfc: remove duplicate declaration of efx_enqueue_skb_tso()
    Define it in nic_common.h, even though the ef100 driver will have a
     different implementation backing it (actually a WARN_ON_ONCE as it
     should never get called by ef100.  But it needs to still exist because
     common TX path code references it).
    
    Signed-off-by: Edward Cree <ecree@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/sfc/tx_common.c
+++ drivers/net/ethernet/sfc/tx_common.c
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "net_driver.h"
 #include "efx.h"
-#include "nic.h"
+#include "nic_common.h"
 #include "tx_common.h"
 
 static unsigned int efx_tx_cb_page_count(struct efx_tx_queue *tx_queue)
