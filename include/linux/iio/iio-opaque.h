6b818aeb4c53c9bcdb5b392742e3bd38061adc06
Alexandru Ardelean <alexandru.ardelean@analog.com>
2020-06-30 07:57:03 +0300

    iio: core: wrap IIO device into an iio_dev_opaque object
    There are plenty of bad designs we want to discourage or not have to review
    manually usually about accessing private (marked as [INTERN]) fields of
    'struct iio_dev'.
    
    Sometimes users copy drivers that are not always the best examples.
    
    A better idea is to hide those fields into the framework.
    For 'struct iio_dev' this is a 'struct iio_dev_opaque' which wraps a public
    'struct iio_dev' object.
    
    In the next series, some fields will be moved to this new struct, each with
    it's own rework.
    
    This rework will not be complete-able for a while, as many fields need some
    drivers to be reworked in order to finalize them (e.g. 'indio_dev->mlock').
    
    But some fields can already be moved, and in time, all of them may get
    there (in the 'struct iio_dev_opaque' object).
    
    Since a lot of drivers also call 'iio_priv()', in order to preserve
    fast-paths (where this matters), the public iio_dev object will have a
    'priv' field that will have the pointer to the private information already
    computed. The reference returned by this field should be guaranteed to be
    cacheline aligned.
    
    The opaque parts will be moved into the 'include/linux/iio/iio-opaque.h'
    header. Should the hidden information be required for some debugging or
    some special needs, it can be made available via this header.
    Otherwise, only the IIO core files should include this file.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

---
+++ include/linux/iio/iio-opaque.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef _INDUSTRIAL_IO_OPAQUE_H_
+#define _INDUSTRIAL_IO_OPAQUE_H_
+
+/**
+ * struct iio_dev_opaque - industrial I/O device opaque information
+ * @indio_dev:			public industrial I/O device information
+ */
+struct iio_dev_opaque {
+	struct iio_dev			indio_dev;
+};
+
+#define to_iio_dev_opaque(indio_dev)		\
+	container_of(indio_dev, struct iio_dev_opaque, indio_dev)
+
+#endif
