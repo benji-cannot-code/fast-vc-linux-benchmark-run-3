95e128b402ec5f4100f41aa70f574d046331f9e0
Will Deacon <will@kernel.org>
2020-06-30 13:55:59 +0100

    arm64: alternatives: Split up alternative.h
    asm/alternative.h contains both the macros needed to use alternatives,
    as well the type definitions and function prototypes for applying them.
    
    Split the header in two, so that alternatives can be used from core
    header files such as linux/compiler.h without the risk of circular
    includes
    
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/asm-uaccess.h
+++ arch/arm64/include/asm/asm-uaccess.h
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_ASM_UACCESS_H
 #define __ASM_ASM_UACCESS_H
 
-#include <asm/alternative.h>
+#include <asm/alternative-macros.h>
 #include <asm/kernel-pgtable.h>
 #include <asm/mmu.h>
 #include <asm/sysreg.h>
