f4becc734c7e5394d411741dd910c3165f7eb9c6
Lee Jones <lee.jones@linaro.org>
2020-07-01 09:30:57 +0100

    misc: mic: host: mic_x100: Move declaration of mic_x100_intr_init[] into c-file
    mic_x100_intr_init[] is only ever used in mic_x100.c, so instead
    of listing to the compiler complain about unused static arrays
    simply move it into the c-file for direct consumption.  This way
    it can stay 'static'.
    
    Fixes the following W=1 kernel build warning:
    
     In file included from drivers/misc/mic/host/mic_main.c:17:
     drivers/misc/mic/host/mic_x100.h:70:18: warning: ‘mic_x100_intr_init’ defined but not used [-Wunused-const-variable=]
     70 | static const u16 mic_x100_intr_init[] = {
     | ^~~~~~~~~~~~~~~~~~
    
    Cc: Sudeep Dutt <sudeep.dutt@intel.com>
    Cc: Ashutosh Dixit <ashutosh.dixit@intel.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Link: https://lore.kernel.org/r/20200701083118.45744-10-lee.jones@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/mic/host/mic_x100.c
+++ drivers/misc/mic/host/mic_x100.c
@@ -18,6 +18,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "mic_x100.h"
 #include "mic_smpt.h"
 
+static const u16 mic_x100_intr_init[] = {
+		MIC_X100_DOORBELL_IDX_START,
+		MIC_X100_DMA_IDX_START,
+		MIC_X100_ERR_IDX_START,
+		MIC_X100_NUM_DOORBELL,
+		MIC_X100_NUM_DMA,
+		MIC_X100_NUM_ERR,
+};
+
 /**
  * mic_x100_write_spad - write to the scratchpad register
  * @mdev: pointer to mic_device instance
