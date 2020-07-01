c1a47f09b35f34dec8e0f6a89acfa3673fa3342e
Steven Rostedt (VMware) <rostedt@goodmis.org>
2020-07-01 15:29:06 -0400

    ktest.pl: Turn off buffering to the log file
    The log file should be up to date to whatever is happening in ktest.
    Disable buffering to the LOG output file handle.
    
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- tools/testing/ktest/ktest.pl
+++ tools/testing/ktest/ktest.pl
@@ -12,6 +12,7 @@ use File::Path qw(mkpath);
 use File::Copy qw(cp);
 use FileHandle;
 use FindBin;
+use IO::Handle;
 
 my $VERSION = "0.2";
 
@@ -4092,6 +4093,7 @@ if (defined($opt{"LOG_FILE"})) {
 	unlink $opt{"LOG_FILE"};
     }
     open(LOG, ">> $opt{LOG_FILE}") or die "Can't write to $opt{LOG_FILE}";
+    LOG->autoflush(1);
 }
 
 doprint "\n\nSTARTING AUTOMATED TESTS\n\n";
