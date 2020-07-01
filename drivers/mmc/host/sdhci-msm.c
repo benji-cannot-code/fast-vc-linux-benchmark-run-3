b1692a161fb5408b960be7ae1a697f62c707267f
Lee Jones <lee.jones@linaro.org>
2020-07-01 13:47:00 +0100

    mmc: sdhci-msm: Demote faux kerneldoc header down to basic comment block
    __sdhci_msm_set_clock()'s function header is the only one in
    kerneldoc format.  Which seems odd as it's not part of an
    external API and isn't referenced anywhere else.  Seeing as
    there has also been no attempt to describe the expected
    function arguments either, we're going to assume that this is
    not actually a bona fide kerneldoc use-case.
    
    Fixes the following W=1 kernel build warnings:
    
     drivers/mmc/host/sdhci-msm.c:1595: warning: Function parameter or member 'host' not described in '__sdhci_msm_set_clock'
     drivers/mmc/host/sdhci-msm.c:1595: warning: Function parameter or member 'clock' not described in '__sdhci_msm_set_clock'
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Andy Gross <agross@kernel.org>
    Cc: Bjorn Andersson <bjorn.andersson@linaro.org>
    Cc: linux-arm-msm@vger.kernel.org
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Link: https://lore.kernel.org/r/20200701124702.908713-14-lee.jones@linaro.org
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -1703,7 +1703,7 @@ static unsigned int sdhci_msm_get_min_clock(struct sdhci_host *host)
 	return SDHCI_MSM_MIN_CLOCK;
 }
 
-/**
+/*
  * __sdhci_msm_set_clock - sdhci_msm clock control.
  *
  * Description:
