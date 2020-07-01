abb4e9fab23d9c2dc873c09bc68982891d414447
Steven Rostedt (VMware) <rostedt@goodmis.org>
2020-07-01 10:32:28 -0400

    ktest.pl: Define PRE_TEST_DIE to kill the test if the PRE_TEST fails
    Currently, if a PRE_TEST is defined and ran, but fails, there's nothing
    currently available to make the test fail too. Add a PRE_TEST_DIE option that
    when set, if a PRE_TEST is defined and fails, the test will die too.
    
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- tools/testing/ktest/ktest.pl
+++ tools/testing/ktest/ktest.pl
@@ -99,6 +99,7 @@ my $final_post_ktest;
 my $pre_ktest;
 my $post_ktest;
 my $pre_test;
+my $pre_test_die;
 my $post_test;
 my $pre_build;
 my $post_build;
@@ -274,6 +275,7 @@ my %option_map = (
     "PRE_KTEST"			=> \$pre_ktest,
     "POST_KTEST"		=> \$post_ktest,
     "PRE_TEST"			=> \$pre_test,
+    "PRE_TEST_DIE"		=> \$pre_test_die,
     "POST_TEST"			=> \$post_test,
     "BUILD_TYPE"		=> \$build_type,
     "BUILD_OPTIONS"		=> \$build_options,
@@ -4348,7 +4350,11 @@ for (my $i = 1; $i <= $opt{"NUM_TESTS"}; $i++) {
     doprint "RUNNING TEST $i of $opt{NUM_TESTS}$name with option $test_type $run_type$installme\n\n";
 
     if (defined($pre_test)) {
-	run_command $pre_test;
+	my $ret = run_command $pre_test;
+	if (!$ret && defined($pre_test_die) &&
+	    $pre_test_die) {
+	    dodie "failed to pre_test\n";
+	}
     }
 
     unlink $dmesg;
