e903780b1694ace8bb9943ce90c82a8b4b6b1028
Leon Romanovsky <leonro@mellanox.com>
2020-07-02 11:18:06 +0300

    RDMA/mlx5: Separate counters from main.c
    There are number of counters types supported in mlx5_ib: HW counters,
    congestion counters, Q-counters and flow counters. Almost all supporting
    code was placed in main.c that made almost impossible to maintain the code
    anymore. Let's create separate code namespace for the counters to easy
    future generalization effort.
    
    Link: https://lore.kernel.org/r/20200702081809.423482-4-leon@kernel.org
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/infiniband/hw/mlx5/qp.c
+++ drivers/infiniband/hw/mlx5/qp.c
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mlx5/fs.h>
 #include "mlx5_ib.h"
 #include "ib_rep.h"
+#include "counters.h"
 #include "cmd.h"
 #include "qp.h"
 #include "wr.h"
