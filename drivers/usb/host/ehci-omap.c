91fda00832351ef12982b8e9b1f5593e5ec09e46
Linus Walleij <linus.walleij@linaro.org>
2020-07-03 15:10:59 +0200

    usb: ehci-omap: Drop surplus include
    The EHCI OMAP driver includes <linux/gpio.h> but does not use
    any symbols from this file, so drop it.
    
    Cc: Roger Quadros <rogerq@ti.com>
    Cc: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Link: https://lore.kernel.org/r/20200703131059.515436-1-linus.walleij@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/ehci-omap.c
+++ drivers/usb/host/ehci-omap.c
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/usb/ulpi.h>
 #include <linux/pm_runtime.h>
-#include <linux/gpio.h>
 #include <linux/clk.h>
 #include <linux/usb.h>
 #include <linux/usb/hcd.h>
