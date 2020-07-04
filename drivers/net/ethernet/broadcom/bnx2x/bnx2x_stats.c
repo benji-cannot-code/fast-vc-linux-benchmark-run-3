8351218ec17e2132f0835275ea90b9c238ef8523
Sudarsana Reddy Kalluru <skalluru@marvell.com>
2020-07-04 10:13:43 +0530

    bnx2x: Add support for idlechk tests.
    This patch populates a database of idlechk tests (registers and
    predicates) and performs the idlechk using this data.
    
    Signed-off-by: Sudarsana Reddy Kalluru <skalluru@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bnx2x/bnx2x_stats.c
+++ drivers/net/ethernet/broadcom/bnx2x/bnx2x_stats.c
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "bnx2x_cmn.h"
 #include "bnx2x_sriov.h"
 
+extern const u32 dmae_reg_go_c[];
+
 /* Statistics */
 
 /*
