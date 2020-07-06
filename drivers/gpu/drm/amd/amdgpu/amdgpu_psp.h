1f1c1efb71eee1ba1c9876e54df626017d776d6c
John Clements <john.clements@amd.com>
2020-07-06 17:08:33 +0800

    drm/amdgpu: updated ta ucode loading
    add support for loading ucode with ta_firmware_header_v2_0
    
    Reviewed-by: Bhawanpreet Lakha <Bhawanpreet.Lakha@amd.com>
    Signed-off-by: John Clements <john.clements@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_psp.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_psp.h
@@ -372,4 +372,6 @@ int psp_init_asd_microcode(struct psp_context *psp,
 			   const char *chip_name);
 int psp_init_sos_microcode(struct psp_context *psp,
 			   const char *chip_name);
+int psp_init_ta_microcode(struct psp_context *psp,
+			  const char *chip_name);
 #endif
