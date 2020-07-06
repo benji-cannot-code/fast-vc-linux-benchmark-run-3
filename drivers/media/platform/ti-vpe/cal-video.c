7ebd40ce5221556c00ea1b3dcd4b3f439be55f68
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2020-07-06 20:36:44 +0200

    media: ti-vpe: cal: Move CAL I/O accessors to cal.h
    To prepare for the split of the camerarx code to a separate file, move
    the CAL I/O accessors to cal.h. This requires renaming the accessors
    with a cal_prefix, as the current names are too generic and prone to
    namespace clashes.
    
    The reg_read() and read_write() macros, that cover both CAL and CAMERARX
    register access, are split in two groups of inline functions, one for
    CAL access and one for CAMERARX access.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Benoit Parrot <bparrot@ti.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/platform/ti-vpe/cal-video.c
+++ drivers/media/platform/ti-vpe/cal-video.c
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	Laurent Pinchart <laurent.pinchart@ideasonboard.com>
  */
 
-#include <linux/bitfield.h>
 #include <linux/delay.h>
 #include <linux/ioctl.h>
 #include <linux/pm_runtime.h>
