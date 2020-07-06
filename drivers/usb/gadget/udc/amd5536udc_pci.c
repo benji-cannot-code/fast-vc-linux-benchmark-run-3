f5c7a12b26899be5519f56e7bff9f81edce9ac1b
Lee Jones <lee.jones@linaro.org>
2020-07-06 14:33:13 +0100

    usb: gadget: udc: amd5536udc_pci: Remove unused variable 'mod_desc'
    Fixes the following W=1 kernel build warning(s):
    
     drivers/usb/gadget/udc/amd5536udc_pci.c:52:19: warning: ‘mod_desc’ defined but not used [-Wunused-const-variable=]
    
    Cc: Felipe Balbi <balbi@kernel.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Link: https://lore.kernel.org/r/20200706133341.476881-5-lee.jones@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/udc/amd5536udc_pci.c
+++ drivers/usb/gadget/udc/amd5536udc_pci.c
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static struct udc *udc;
 
 /* description */
-static const char mod_desc[] = UDC_MOD_DESCRIPTION;
 static const char name[] = "amd5536udc-pci";
 
 /* Reset all pci context */
