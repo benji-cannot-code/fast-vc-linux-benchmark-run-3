74055ea9e79e8c0a2617c42fdb144c01dab3f4de
Lee Jones <lee.jones@linaro.org>
2020-07-06 14:33:27 +0100

    usb: host: fotg210-hcd: Remove unused variable 'hcc_params'
    The result is actually read into fotg210->caps->hcc_params.  No
    need to popuate an unused varible with the unchecked return value
    from fotg210_readl().
    
    Fixes the following W=1 kernel build warning(s):
    
     drivers/usb/host/fotg210-hcd.c: In function ‘fotg210_run’:
     drivers/usb/host/fotg210-hcd.c:5013:6: warning: variable ‘hcc_params’ set but not used [-Wunused-but-set-variable]
     5013 | u32 hcc_params;
     | ^~~~~~~~~~
    
    Cc: Yuan-Hsin Chen <yhchen@faraday-tech.com>
    Cc: Feng-Hsin Chiang <john453@faraday-tech.com>
    Cc: Po-Yu Chuang <ratbert.chuang@gmail.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Link: https://lore.kernel.org/r/20200706133341.476881-19-lee.jones@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/fotg210-hcd.c
+++ drivers/usb/host/fotg210-hcd.c
@@ -5011,7 +5011,6 @@ static int fotg210_run(struct usb_hcd *hcd)
 {
 	struct fotg210_hcd *fotg210 = hcd_to_fotg210(hcd);
 	u32 temp;
-	u32 hcc_params;
 
 	hcd->uses_new_polling = 1;
 
@@ -5034,7 +5033,7 @@ static int fotg210_run(struct usb_hcd *hcd)
 	 * Scsi_Host.highmem_io, and so forth.  It's readonly to all
 	 * host side drivers though.
 	 */
-	hcc_params = fotg210_readl(fotg210, &fotg210->caps->hcc_params);
+	fotg210_readl(fotg210, &fotg210->caps->hcc_params);
 
 	/*
 	 * Philips, Intel, and maybe others need CMD_RUN before the
