17068913bc87c8f16fc2fc9ee5ef2f932365ea2f
Evan Quan <evan.quan@amd.com>
2020-07-07 14:06:40 +0800

    drm/amd/powerplay: move ppfeature mask setting to smu_cmn.c
    Considering they are shared by all ASICs. And we are moving
    to centralize all feature enablement/support checking and
    setting APIs in smu_cmn.c.
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/smu_internal.h
+++ drivers/gpu/drm/amd/powerplay/smu_internal.h
@@ -93,5 +93,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define smu_get_unique_id(smu)						smu_ppt_funcs(get_unique_id, 0, smu)
 #define smu_log_thermal_throttling(smu)					smu_ppt_funcs(log_thermal_throttling_event, 0, smu)
 #define smu_get_asic_power_limits(smu)					smu_ppt_funcs(get_power_limit, 0, smu)
+#define smu_get_pp_feature_mask(smu, buf)				smu_ppt_funcs(get_pp_feature_mask, 0, smu, buf)
+#define smu_set_pp_feature_mask(smu, new_mask)				smu_ppt_funcs(set_pp_feature_mask, 0, smu, new_mask)
 
 #endif
