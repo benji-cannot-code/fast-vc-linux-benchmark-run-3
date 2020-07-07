0646ea249829d99e25233953085c1ebfb06ab438
Jesper Dangaard Brouer <brouer@redhat.com>
2020-07-07 09:12:19 +0200

    selftests/bpf: test_progs use another shell exit on non-actions
    This is a follow up adjustment to commit 6c92bd5cd465 ("selftests/bpf:
    Test_progs indicate to shell on non-actions"), that returns shell exit
    indication EXIT_FAILURE (value 1) when user selects a non-existing test.
    
    The problem with using EXIT_FAILURE is that a shell script cannot tell
    the difference between a non-existing test and the test failing.
    
    This patch uses value 2 as shell exit indication.
    (Aside note unrecognized option parameters use value 64).
    
    Fixes: 6c92bd5cd465 ("selftests/bpf: Test_progs indicate to shell on non-actions")
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/159410593992.1093222.90072558386094370.stgit@firesoul

--- tools/testing/selftests/bpf/test_progs.c
+++ tools/testing/selftests/bpf/test_progs.c
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <string.h>
 #include <execinfo.h> /* backtrace */
 
+#define EXIT_NO_TEST		2
+
 /* defined in test_progs.h */
 struct test_env env = {};
 
@@ -741,7 +743,7 @@ out:
 	close(env.saved_netns_fd);
 
 	if (env.succ_cnt + env.fail_cnt + env.skip_cnt == 0)
-		return EXIT_FAILURE;
+		return EXIT_NO_TEST;
 
 	return env.fail_cnt ? EXIT_FAILURE : EXIT_SUCCESS;
 }
