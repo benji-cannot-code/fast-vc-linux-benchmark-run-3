99f9b174572f10c511e7c4ed296c54bae8df1e46
Stanimir Varbanov <stanimir.varbanov@linaro.org>
2020-07-07 19:48:54 +0200

    media: venus: Make debug infrastructure more flexible
    Here we introduce debug prefixes for dev_dbg groups by level of
    importance - Venus{Low,Med,High,FW} Enabling the particular level
    will be done by dynamic debug.
    
    For example to enable debug messages with low level:
    echo 'format "VenusLow" +p' > debugfs/dynamic_debug/control
    
    If you want to enable all levels:
    echo 'format "Venus" +p' > debugfs/dynamic_debug/control
    
    All the features which dynamic debugging provide are preserved.
    
    Signed-off-by: Stanimir Varbanov <stanimir.varbanov@linaro.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/platform/qcom/venus/core.h
+++ drivers/media/platform/qcom/venus/core.h
@@ -16,6 +16,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dbgfs.h"
 #include "hfi.h"
 
+#define VDBGL	"VenusLow : "
+#define VDBGM	"VenusMed : "
+#define VDBGH	"VenusHigh: "
+#define VDBGFW	"VenusFW  : "
+
 #define VIDC_CLKS_NUM_MAX		4
 #define VIDC_VCODEC_CLKS_NUM_MAX	2
 #define VIDC_PMDOMAINS_NUM_MAX		3
