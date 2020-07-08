56213b9c5d428c0ae2988cc72f6aeb721eb1903e
Alexander A. Klimov <grandmaster@al2klimov.de>
2020-07-08 21:44:00 +0200

    SPI SUBSYSTEM: Replace HTTP links with HTTPS ones
    Rationale:
    Reduces attack surface on kernel devs opening the links for MITM
    as HTTPS traffic is much harder to manipulate.
    
    Deterministic algorithm:
    For each file:
      If not .svg:
        For each line:
          If doesn't contain `\bxmlns\b`:
            For each link, `\bhttp://[^# \t\r\n]*(?:\w|/)`:
    	  If neither `\bgnu\.org/license`, nor `\bmozilla\.org/MPL\b`:
                If both the HTTP and HTTPS versions
                return 200 OK and serve the same content:
                  Replace HTTP with HTTPS.
    
    Signed-off-by: Alexander A. Klimov <grandmaster@al2klimov.de>
    Link: https://lore.kernel.org/r/20200708194400.22213-1-grandmaster@al2klimov.de
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-ep93xx.c
+++ drivers/spi/spi-ep93xx.c
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * For more information about the SPI controller see documentation on Cirrus
  * Logic web site:
- *     http://www.cirrus.com/en/pubs/manual/EP93xx_Users_Guide_UM1.pdf
+ *     https://www.cirrus.com/en/pubs/manual/EP93xx_Users_Guide_UM1.pdf
  */
 
 #include <linux/io.h>
