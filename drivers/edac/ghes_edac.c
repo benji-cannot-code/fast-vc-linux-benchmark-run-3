a219d4651116e64264c1b73608ff1c60dd3ce56b
Alexander A. Klimov <grandmaster@al2klimov.de>
2020-07-08 13:35:46 +0200

    EDAC: Replace HTTP links with HTTPS ones
    Rationale:
    Reduces attack surface on kernel devs opening the links for MITM
    as HTTPS traffic is much harder to manipulate.
    
      Deterministic algorithm:
      For each file:
        If not .svg:
          For each line:
            If doesn't contain `\bxmlns\b`:
              For each link, `\bhttp://[^# \t\r\n]*(?:\w|/)`:
      	  If neither `\bgnu\.org/license`, nor `\bmozilla\.org/MPL\b`:
                  If both the HTTP and HTTPS versions
                  return 200 OK and serve the same content:
                    Replace HTTP with HTTPS.
    
     [ bp: Merge all EDAC patches into a single one. ]
    
    Signed-off-by: Alexander A. Klimov <grandmaster@al2klimov.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Acked-by: Tero Kristo <t-kristo@ti.com> # ti_edac
    Link: https://lkml.kernel.org/r/20200708113546.14135-1-grandmaster@al2klimov.de

--- drivers/edac/ghes_edac.c
+++ drivers/edac/ghes_edac.c
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (c) 2013 by Mauro Carvalho Chehab
  *
- * Red Hat Inc. http://www.redhat.com
+ * Red Hat Inc. https://www.redhat.com
  */
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
