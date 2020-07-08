e2c0d0df50decd515ec37c158358700213682943
Johan Hovold <johan@kernel.org>
2020-07-08 14:49:58 +0200

    USB: serial: drop unnecessary sysrq include
    There's no need to include sysrq.h in the subsystem header.
    
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Johan Hovold <johan@kernel.org>

--- include/linux/usb/serial.h
+++ include/linux/usb/serial.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kref.h>
 #include <linux/mutex.h>
 #include <linux/serial.h>
-#include <linux/sysrq.h>
 #include <linux/kfifo.h>
 
 /* The maximum number of ports one device can grab at once */
