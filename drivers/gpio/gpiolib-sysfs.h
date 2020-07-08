97d2fd40151370260751f7a14cb4fa8f7704fe4c
Kent Gibson <warthog618@gmail.com>
2020-07-08 12:15:44 +0800

    gpiolib: move gpiolib-sysfs function declarations into their own header
    Move gpiolib-sysfs function declarations into their own header.
    
    These functions are in gpiolib-sysfs.c, and are only required by gpiolib.c,
    and so should be in a module header, not gpiolib.h.
    
    This brings gpiolib-sysfs into line with gpiolib-cdev, and is another step
    towards removing the sysfs inferface.
    
    Signed-off-by: Kent Gibson <warthog618@gmail.com>
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

---
+++ drivers/gpio/gpiolib-sysfs.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef GPIOLIB_SYSFS_H
+#define GPIOLIB_SYSFS_H
+
+#ifdef CONFIG_GPIO_SYSFS
+
+int gpiochip_sysfs_register(struct gpio_device *gdev);
+void gpiochip_sysfs_unregister(struct gpio_device *gdev);
+
+#else
+
+static inline int gpiochip_sysfs_register(struct gpio_device *gdev)
+{
+	return 0;
+}
+
+static inline void gpiochip_sysfs_unregister(struct gpio_device *gdev)
+{
+}
+
+#endif /* CONFIG_GPIO_SYSFS */
+
+#endif /* GPIOLIB_SYSFS_H */
--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "gpiolib-of.h"
 #include "gpiolib-acpi.h"
 #include "gpiolib-cdev.h"
+#include "gpiolib-sysfs.h"
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/gpio.h>
