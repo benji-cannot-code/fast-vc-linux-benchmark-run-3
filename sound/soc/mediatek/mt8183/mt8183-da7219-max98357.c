f89384567edd5ac3a49ee39aa170cb697fe541db
Tzung-Bi Shih <tzungbi@google.com>
2020-07-09 20:24:41 +0800

    ASoC: mediatek: mt8183-da7219: sort header inclusions in alphabetical
    Sorts header inclusions in alphabetical.
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@google.com>
    Link: https://lore.kernel.org/r/20200709122445.1584497-2-tzungbi@google.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/mediatek/mt8183/mt8183-da7219-max98357.c
+++ sound/soc/mediatek/mt8183/mt8183-da7219-max98357.c
@@ -14,9 +14,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
 
-#include "mt8183-afe-common.h"
 #include "../../codecs/da7219-aad.h"
 #include "../../codecs/da7219.h"
+#include "mt8183-afe-common.h"
 
 struct mt8183_da7219_max98357_priv {
 	struct snd_soc_jack headset_jack;
