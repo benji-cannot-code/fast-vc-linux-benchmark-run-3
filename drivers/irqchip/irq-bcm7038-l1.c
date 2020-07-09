155491df55aa652ea24171ccf4541e57ce8239eb
Florian Fainelli <f.fainelli@gmail.com>
2020-07-09 16:41:41 -0700

    irqchip/irq-bcm7038-l1: Allow building on ARM 32-bit
    We need to have a definition for cpu_logical_map[] which on ARM
    platforms is provided by asm/smp_plat.h. This header is not
    automatically included from linux/smp.h and untangling it is a bit
    difficult.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20200709234141.4901-1-f.fainelli@gmail.com

--- drivers/irqchip/irq-bcm7038-l1.c
+++ drivers/irqchip/irq-bcm7038-l1.c
@@ -29,6 +29,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irqchip.h>
 #include <linux/irqchip/chained_irq.h>
 #include <linux/syscore_ops.h>
+#ifdef CONFIG_ARM
+#include <asm/smp_plat.h>
+#endif
 
 #define IRQS_PER_WORD		32
 #define REG_BYTES_PER_IRQ_WORD	(sizeof(u32) * 4)
