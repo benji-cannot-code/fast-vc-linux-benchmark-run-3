71200be1bb71141195bbead383c060009812192a
Gustavo A. R. Silva <gustavoars@kernel.org>
2020-07-08 20:03:59 -0500

    ASoC: codecs: Use fallthrough pseudo-keyword
    Replace the existing /* fall through */ comments and its variants with
    the new pseudo-keyword macro fallthrough[1].
    
    [1] https://www.kernel.org/doc/html/latest/process/deprecated.html?highlight=fallthrough#implicit-switch-case-fall-through
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Link: https://lore.kernel.org/r/20200709010359.GA18971@embeddedor
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt5640.c
+++ sound/soc/codecs/rt5640.c
@@ -1663,7 +1663,7 @@ static int get_sdp_info(struct snd_soc_component *component, int dai_id)
 			break;
 		case RT5640_IF_113:
 			ret |= RT5640_U_IF1;
-			/* fall through */
+			fallthrough;
 		case RT5640_IF_312:
 		case RT5640_IF_213:
 			ret |= RT5640_U_IF2;
@@ -1679,7 +1679,7 @@ static int get_sdp_info(struct snd_soc_component *component, int dai_id)
 			break;
 		case RT5640_IF_223:
 			ret |= RT5640_U_IF1;
-			/* fall through */
+			fallthrough;
 		case RT5640_IF_123:
 		case RT5640_IF_321:
 			ret |= RT5640_U_IF2;
