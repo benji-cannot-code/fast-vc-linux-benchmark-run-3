09c26650810ff2ae5ad42d116a1c80596913a93f
Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
2020-07-09 08:59:25 +0530

    powerpc/book3s64/pkeys: Fixup bit numbering
    This number the pkey bit such that it is easy to follow. PKEY_BIT0 is
    the lower order bit. This makes further changes easy to follow.
    
    No functional change in this patch other than linux page table for
    hash translation now maps pkeys differently.
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200709032946.881753-3-aneesh.kumar@linux.ibm.com

--- arch/powerpc/include/asm/book3s/64/hash-4k.h
+++ arch/powerpc/include/asm/book3s/64/hash-4k.h
@@ -58,11 +58,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define H_PMD_FRAG_NR	(PAGE_SIZE >> H_PMD_FRAG_SIZE_SHIFT)
 
 /* memory key bits, only 8 keys supported */
-#define H_PTE_PKEY_BIT0	0
-#define H_PTE_PKEY_BIT1	0
+#define H_PTE_PKEY_BIT4	0
+#define H_PTE_PKEY_BIT3	0
 #define H_PTE_PKEY_BIT2	_RPAGE_RSV3
-#define H_PTE_PKEY_BIT3	_RPAGE_RSV4
-#define H_PTE_PKEY_BIT4	_RPAGE_RSV5
+#define H_PTE_PKEY_BIT1	_RPAGE_RSV4
+#define H_PTE_PKEY_BIT0	_RPAGE_RSV5
+
 
 /*
  * On all 4K setups, remap_4k_pfn() equates to remap_pfn_range()
