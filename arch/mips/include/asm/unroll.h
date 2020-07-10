c608d67327cf2b593597a7451ab5164e89f22e7d
Nathan Chancellor <natechancellor@gmail.com>
2020-07-10 15:34:41 -0700

    mips: Remove compiler check in unroll macro
    CONFIG_CC_IS_GCC is undefined when Clang is used, which breaks the build
    (see our Travis link below).
    
    Clang 8 was chosen as a minimum version for this check because there
    were some improvements around __builtin_constant_p in that release. In
    reality, MIPS was not even buildable until clang 9 so that check was not
    technically necessary. Just remove all compiler checks and just assume
    that we have a working compiler.
    
    Fixes: d4e60453266b ("Restore gcc check in mips asm/unroll.h")
    Link: https://travis-ci.com/github/ClangBuiltLinux/continuous-integration/jobs/359642821
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/mips/include/asm/unroll.h
+++ arch/mips/include/asm/unroll.h
@@ -26,9 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	 * generate reasonable code for the switch statement,	\
 	 * so we skip the sanity check for those compilers.	\
 	 */							\
-	BUILD_BUG_ON((CONFIG_CC_IS_GCC ||			\
-		      CONFIG_CLANG_VERSION >= 80000) &&		\
-		     !__builtin_constant_p(times));		\
+	BUILD_BUG_ON(!__builtin_constant_p(times));		\
 								\
 	switch (times) {					\
 	case 32: fn(__VA_ARGS__); /* fall through */		\
