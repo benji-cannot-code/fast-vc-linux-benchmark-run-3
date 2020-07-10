7d192b270b4cb4eb169d21894e8a98eb9f5e0cd8
Willem de Bruijn <willemb@google.com>
2020-07-10 09:29:02 -0400

    icmp: support rfc 4884
    Add setsockopt SOL_IP/IP_RECVERR_4884 to return the offset to an
    extension struct if present.
    
    ICMP messages may include an extension structure after the original
    datagram. RFC 4884 standardized this behavior. It stores the offset
    in words to the extension header in u8 icmphdr.un.reserved[1].
    
    The field is valid only for ICMP types destination unreachable, time
    exceeded and parameter problem, if length is at least 128 bytes and
    entire packet does not exceed 576 bytes.
    
    Return the offset to the start of the extension struct when reading an
    ICMP error from the error queue, if it matches the above constraints.
    
    Do not return the raw u8 field. Return the offset from the start of
    the user buffer, in bytes. The kernel does not return the network and
    transport headers, so subtract those.
    
    Also validate the headers. Return the offset regardless of validation,
    as an invalid extension must still not be misinterpreted as part of
    the original datagram. Note that !invalid does not imply valid. If
    the extension version does not match, no validation can take place,
    for instance.
    
    For backward compatibility, make this optional, set by setsockopt
    SOL_IP/IP_RECVERR_RFC4884. For API example and feature test, see
    github.com/wdebruij/kerneltools/blob/master/tests/recv_icmp_v2.c
    
    For forward compatibility, reserve only setsockopt value 1, leaving
    other bits for additional icmp extensions.
    
    Changes
      v1->v2:
      - convert word offset to byte offset from start of user buffer
        - return in ee_data as u8 may be insufficient
      - define extension struct and object header structs
      - return len only if constraints met
      - if returning len, also validate
    
    Signed-off-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/uapi/linux/errqueue.h
+++ include/uapi/linux/errqueue.h
@@ -6,6 +6,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/time_types.h>
 
+/* RFC 4884: return offset to extension struct + validation */
+struct sock_ee_data_rfc4884 {
+	__u16	len;
+	__u8	flags;
+	__u8	reserved;
+};
+
 struct sock_extended_err {
 	__u32	ee_errno;	
 	__u8	ee_origin;
@@ -13,7 +20,10 @@ struct sock_extended_err {
 	__u8	ee_code;
 	__u8	ee_pad;
 	__u32   ee_info;
-	__u32   ee_data;
+	union	{
+		__u32   ee_data;
+		struct sock_ee_data_rfc4884 ee_rfc4884;
+	};
 };
 
 #define SO_EE_ORIGIN_NONE	0
@@ -31,6 +41,8 @@ struct sock_extended_err {
 
 #define SO_EE_CODE_TXTIME_INVALID_PARAM	1
 #define SO_EE_CODE_TXTIME_MISSED	2
+
+#define SO_EE_RFC4884_FLAG_INVALID	1
 
 /**
  *	struct scm_timestamping - timestamps exposed through cmsg
