47b8e28da6262ac0c8e2859981ce9ab994f3723a
Will Deacon <will@kernel.org>
2020-07-10 14:49:40 +0100

    asm/rwonce: Don't pull <asm/barrier.h> into 'asm-generic/rwonce.h'
    Now that 'smp_read_barrier_depends()' has gone the way of the Norwegian
    Blue, drop the inclusion of <asm/barrier.h> in 'asm-generic/rwonce.h'.
    
    This requires fixups to some architecture vdso headers which were
    previously relying on 'asm/barrier.h' coming in via 'linux/compiler.h'.
    
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Will Deacon <will@kernel.org>

--- include/asm-generic/rwonce.h
+++ include/asm-generic/rwonce.h
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kasan-checks.h>
 #include <linux/kcsan-checks.h>
 
-#include <asm/barrier.h>
-
 /*
  * Yes, this permits 64-bit accesses on 32-bit architectures. These will
  * actually be atomic in some cases (namely Armv7 + LPAE), but for others we
