79b3fd54d7dfaef082b88f2aebdb102e0688cd22
Al Viro <viro@zeniv.linux.org.uk>
2020-07-10 20:07:10 -0400

    skb_copy_and_csum_bits(): don't bother with the last argument
    it's always 0
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- net/ipv4/ip_output.c
+++ net/ipv4/ip_output.c
@@ -1127,7 +1127,7 @@ alloc_new_skb:
 			if (fraggap) {
 				skb->csum = skb_copy_and_csum_bits(
 					skb_prev, maxfraglen,
-					data + transhdrlen, fraggap, 0);
+					data + transhdrlen, fraggap);
 				skb_prev->csum = csum_sub(skb_prev->csum,
 							  skb->csum);
 				data += fraggap;
@@ -1412,7 +1412,7 @@ ssize_t	ip_append_page(struct sock *sk, struct flowi4 *fl4, struct page *page,
 				skb->csum = skb_copy_and_csum_bits(skb_prev,
 								   maxfraglen,
 						    skb_transport_header(skb),
-								   fraggap, 0);
+								   fraggap);
 				skb_prev->csum = csum_sub(skb_prev->csum,
 							  skb->csum);
 				pskb_trim_unique(skb_prev, maxfraglen);
