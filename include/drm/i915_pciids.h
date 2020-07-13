00b887dc6b1af355a86a293f9bf891ecb70ce50f
Abdiel Janulgue <abdiel.janulgue@linux.intel.com>
2020-07-13 11:23:18 -0700

    drm/i915/dg1: Add DG1 PCI IDs
    Add the PCI ID for DG1, but keep it out of the table we use to register
    the driver. At this point we can't consider the driver ready to bind to
    the device since we basically miss support for everything. When more
    support is merged we can enable it to work partially for example as a
    display-only driver.
    
    v2: remove DG1 from the pci table and reword commit message (Lucas)
    
    Bspec: 44463
    
    Cc: Matthew Auld <matthew.auld@intel.com>
    Cc: James Ausmus <james.ausmus@intel.com>
    Cc: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Signed-off-by: Abdiel Janulgue <abdiel.janulgue@linux.intel.com>
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Reviewed-by: José Roberto de Souza <jose.souza@intel.com> # v1
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200713182321.12390-3-lucas.demarchi@intel.com

--- include/drm/i915_pciids.h
+++ include/drm/i915_pciids.h
@@ -619,4 +619,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	INTEL_VGA_DEVICE(0x4C90, info), \
 	INTEL_VGA_DEVICE(0x4C9A, info)
 
+/* DG1 */
+#define INTEL_DG1_IDS(info) \
+	INTEL_VGA_DEVICE(0x4905, info)
+
 #endif /* _I915_PCIIDS_H */
