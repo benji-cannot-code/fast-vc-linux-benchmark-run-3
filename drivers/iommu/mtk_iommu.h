8bb7e882826ce587cc38fee43e8e52396d0d9755
Joerg Roedel <jroedel@suse.de>
2020-07-13 12:16:48 +0200

    iommu/mediatek: Include liunx/dma-mapping.h
    This fixes a compile error when cross-compiling the driver
    on x86-32.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Link: https://lore.kernel.org/r/20200713101648.32056-1-joro@8bytes.org

--- drivers/iommu/mtk_iommu.h
+++ drivers/iommu/mtk_iommu.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/iommu.h>
 #include <linux/list.h>
 #include <linux/spinlock.h>
+#include <linux/dma-mapping.h>
 #include <soc/mediatek/smi.h>
 
 #define MTK_LARB_COM_MAX	8
