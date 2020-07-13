a9d1d943eb34afe73d4e3369e091c705a0c86cf5
Pawel Laszczak <pawell@cadence.com>
2020-07-13 12:05:46 +0200

    usb: cdns3: core: removed cdns3_get_current_role_driver function
    Function is not used in driver so it can be removed.
    
    Signed-off-by: Pawel Laszczak <pawell@cadence.com>
    Reviewed-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- drivers/usb/cdns3/core.c
+++ drivers/usb/cdns3/core.c
@@ -28,13 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static int cdns3_idle_init(struct cdns3 *cdns);
 
-static inline
-struct cdns3_role_driver *cdns3_get_current_role_driver(struct cdns3 *cdns)
-{
-	WARN_ON(!cdns->roles[cdns->role]);
-	return cdns->roles[cdns->role];
-}
-
 static int cdns3_role_start(struct cdns3 *cdns, enum usb_role role)
 {
 	int ret;
