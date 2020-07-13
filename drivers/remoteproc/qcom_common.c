7b4febe52a0dd91424224ac2c22f96797bc9b8ef
Kefeng Wang <wangkefeng.wang@huawei.com>
2020-07-13 10:00:03 +0800

    remoteproc: qcom: Add missing slab.h
    drivers/remoteproc/qcom_common.c: In function 'qcom_ssr_get_subsys':
    drivers/remoteproc/qcom_common.c:210:9: error: implicit declaration of function 'kzalloc'; did you mean 'vzalloc'?
    [-Werror=implicit-function-declaration]
      info = kzalloc(sizeof(*info), GFP_KERNEL);
             ^~~~~~~
             vzalloc
    
    kzalloc() is declared in linux/slab.h, add include to fix build issue.
    
    Tested-by: Alex Elder <elder@linaro.org>
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Link: https://lore.kernel.org/r/20200713020003.134039-1-wangkefeng.wang@huawei.com
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/remoteproc/qcom_common.c
+++ drivers/remoteproc/qcom_common.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/remoteproc/qcom_rproc.h>
 #include <linux/rpmsg/qcom_glink.h>
 #include <linux/rpmsg/qcom_smd.h>
+#include <linux/slab.h>
 #include <linux/soc/qcom/mdt_loader.h>
 
 #include "remoteproc_internal.h"
