b35a041eb17c64ff2d049be3844a0b572438e0a8
Baidyanath Kundu <kundubaidya99@gmail.com>
2020-07-13 20:06:53 +0530

    Staging: vc04_services: Fix unsigned int warnings
    This patch fixes the checkpatch.pl warning:
    
    WARNING: Prefer 'unsigned int' to bare use of 'unsigned'
    
    Signed-off-by: Baidyanath Kundu <kundubaidya99@gmail.com>
    Link: https://lore.kernel.org/r/20200713143653.32332-1-kundubaidya99@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vc04_services/interface/vchiq_arm/vchiq_core.c
+++ drivers/staging/vc04_services/interface/vchiq_arm/vchiq_core.c
@@ -2276,7 +2276,7 @@ fail_free_handler_thread:
 	return VCHIQ_ERROR;
 }
 
-void vchiq_msg_queue_push(unsigned handle, struct vchiq_header *header)
+void vchiq_msg_queue_push(unsigned int handle, struct vchiq_header *header)
 {
 	struct vchiq_service *service = find_service_by_handle(handle);
 	int pos;
@@ -2294,7 +2294,7 @@ void vchiq_msg_queue_push(unsigned handle, struct vchiq_header *header)
 }
 EXPORT_SYMBOL(vchiq_msg_queue_push);
 
-struct vchiq_header *vchiq_msg_hold(unsigned handle)
+struct vchiq_header *vchiq_msg_hold(unsigned int handle)
 {
 	struct vchiq_service *service = find_service_by_handle(handle);
 	struct vchiq_header *header;
@@ -3215,7 +3215,7 @@ error_exit:
 	return status;
 }
 
-int vchiq_queue_kernel_message(unsigned handle, void *data, unsigned size)
+int vchiq_queue_kernel_message(unsigned int handle, void *data, unsigned int size)
 {
 	enum vchiq_status status;
 
