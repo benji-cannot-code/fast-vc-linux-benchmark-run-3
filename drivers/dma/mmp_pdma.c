cf01f9c50f0365ea30f8721603ff11843158dfe4
Lee Jones <lee.jones@linaro.org>
2020-07-14 12:15:33 +0100

    dmaengine: mmp_pdma: Demote obvious misuse of kerneldoc to standard comment blocks
    No attempt has been made to document any of the demoted functions here.
    
    Fixes the following W=1 kernel build warning(s):
    
     drivers/dma/mmp_pdma.c:298: warning: Function parameter or member 'chan' not described in 'start_pending_queue'
     drivers/dma/mmp_pdma.c:393: warning: Function parameter or member 'dchan' not described in 'mmp_pdma_alloc_chan_resources'
     drivers/dma/mmp_pdma.c:862: warning: Function parameter or member 'dchan' not described in 'mmp_pdma_issue_pending'
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Link: https://lore.kernel.org/r/20200714111546.1755231-5-lee.jones@linaro.org
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/mmp_pdma.c
+++ drivers/dma/mmp_pdma.c
@@ -291,7 +291,7 @@ static void mmp_pdma_free_phy(struct mmp_pdma_chan *pchan)
 	spin_unlock_irqrestore(&pdev->phy_lock, flags);
 }
 
-/**
+/*
  * start_pending_queue - transfer any pending transactions
  * pending list ==> running list
  */
@@ -382,7 +382,7 @@ mmp_pdma_alloc_descriptor(struct mmp_pdma_chan *chan)
 	return desc;
 }
 
-/**
+/*
  * mmp_pdma_alloc_chan_resources - Allocate resources for DMA channel.
  *
  * This function will create a dma pool for descriptor allocation.
@@ -855,7 +855,7 @@ static enum dma_status mmp_pdma_tx_status(struct dma_chan *dchan,
 	return ret;
 }
 
-/**
+/*
  * mmp_pdma_issue_pending - Issue the DMA start command
  * pending list ==> running list
  */
