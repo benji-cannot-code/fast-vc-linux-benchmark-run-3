9f2dc18eaca26fe2d6958c5ab7992cadad212c46
Mathieu Poirier <mathieu.poirier@linaro.org>
2020-07-14 14:04:38 -0600

    remoteproc: stm32: Remove memory translation from DT parsing
    Other than one has to be done after the other, there is no correlation
    between memory translation and DT parsing.  As such move function
    stm32_rproc_of_memory_translations() to stm32_rproc_probe() so that
    stm32_rproc_parse_dt() can be extended to look for attach bindings
    in a clean way.
    
    Acked-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Reviewed-by: Loic Pallardy <loic.pallardy@st.com>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Link: https://lore.kernel.org/r/20200714200445.1427257-5-mathieu.poirier@linaro.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/remoteproc/stm32_rproc.c
+++ drivers/remoteproc/stm32_rproc.c
@@ -608,7 +608,7 @@ static int stm32_rproc_parse_dt(struct platform_device *pdev,
 
 	*auto_boot = of_property_read_bool(np, "st,auto-boot");
 
-	return stm32_rproc_of_memory_translations(pdev, ddata);
+	return 0;
 }
 
 static int stm32_rproc_probe(struct platform_device *pdev)
@@ -632,6 +632,10 @@ static int stm32_rproc_probe(struct platform_device *pdev)
 	rproc_coredump_set_elf_info(rproc, ELFCLASS32, EM_NONE);
 
 	ret = stm32_rproc_parse_dt(pdev, ddata, &rproc->auto_boot);
+	if (ret)
+		goto free_rproc;
+
+	ret = stm32_rproc_of_memory_translations(pdev, ddata);
 	if (ret)
 		goto free_rproc;
 
