7891ff84eeb09f32691c97057d60940875159c90
Wei Yongjun <weiyongjun1@huawei.com>
2020-07-14 22:24:24 +0800

    cpuidle/pseries: Make symbol 'pseries_idle_driver' static
    The sparse tool complains as follows:
    
    drivers/cpuidle/cpuidle-pseries.c:25:23: warning:
     symbol 'pseries_idle_driver' was not declared. Should it be static?
    
    'pseries_idle_driver' is not used outside of this file, so marks
    it static.
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Wei Yongjun <weiyongjun1@huawei.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200714142424.66648-1-weiyongjun1@huawei.com

--- drivers/cpuidle/cpuidle-pseries.c
+++ drivers/cpuidle/cpuidle-pseries.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/idle.h>
 #include <asm/plpar_wrappers.h>
 
-struct cpuidle_driver pseries_idle_driver = {
+static struct cpuidle_driver pseries_idle_driver = {
 	.name             = "pseries_idle",
 	.owner            = THIS_MODULE,
 };
