3c7e3e8a12b91ebef50df3a74a7babf3408866c7
Christoph Hellwig <hch@lst.de>
2020-07-14 14:18:52 +0200

    sh: don't include <asm/io_trapped.h> in <asm/io.h>
    No need to expose the details of trapped I/O to drivers.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Rich Felker <dalias@libc.org>

--- arch/sh/mm/ioremap.c
+++ arch/sh/mm/ioremap.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/pci.h>
 #include <linux/io.h>
+#include <asm/io_trapped.h>
 #include <asm/page.h>
 #include <asm/pgalloc.h>
 #include <asm/addrspace.h>
