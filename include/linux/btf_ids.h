8cf35f4473e277526d79129b74d21b68b3925bfa
Jiri Olsa <jolsa@kernel.org>
2020-07-14 12:25:34 +0200

    bpf: Fix cross build for CONFIG_DEBUG_INFO_BTF option
    Stephen and 0-DAY CI Kernel Test Service reported broken cross build
    for arm (arm-linux-gnueabi-gcc (GCC) 9.3.0), with following output:
    
       /tmp/ccMS5uth.s: Assembler messages:
       /tmp/ccMS5uth.s:69: Error: unrecognized symbol type ""
       /tmp/ccMS5uth.s:82: Error: unrecognized symbol type ""
    
    Having '@object' for .type  diretive is  wrong because '@' is comment
    character for some architectures. Using STT_OBJECT instead that should
    work everywhere.
    
    Also using HOST* variables to build resolve_btfids so it's properly
    build in crossbuilds (stolen from objtool's Makefile).
    
    Reported-by: kernel test robot <lkp@intel.com>
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Tested-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://lore.kernel.org/bpf/20200714102534.299280-2-jolsa@kernel.org

--- include/linux/btf_ids.h
+++ include/linux/btf_ids.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 asm(							\
 ".pushsection " BTF_IDS_SECTION ",\"a\";       \n"	\
 ".local " #symbol " ;                          \n"	\
-".type  " #symbol ", @object;                  \n"	\
+".type  " #symbol ", STT_OBJECT;               \n"	\
 ".size  " #symbol ", 4;                        \n"	\
 #symbol ":                                     \n"	\
 ".zero 4                                       \n"	\
