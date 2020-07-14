475dd0724c3fd6f5f4365127b6558b29f4344fae
Vadim Pasternak <vadimp@mellanox.com>
2020-07-14 15:01:58 +0300

    platform_data/mlxreg: Add support for complex attributes
    Add new field 'regnum' to the structure 'mlxreg_core_data' to specify
    the number of registers occupied by multi-register attribute.
    
    Signed-off-by: Vadim Pasternak <vadimp@mellanox.com>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- include/linux/platform_data/mlxreg.h
+++ include/linux/platform_data/mlxreg.h
@@ -81,6 +81,7 @@ struct mlxreg_hotplug_device {
  * @hpdev - hotplug device data;
  * @health_cntr: dynamic device health indication counter;
  * @attached: true if device has been attached after good health indication;
+ * @regnum: number of registers occupied by multi-register attribute;
  */
 struct mlxreg_core_data {
 	char label[MLXREG_CORE_LABEL_MAX_SIZE];
@@ -93,6 +94,7 @@ struct mlxreg_core_data {
 	struct mlxreg_hotplug_device hpdev;
 	u8 health_cntr;
 	bool attached;
+	u8 regnum;
 };
 
 /**
