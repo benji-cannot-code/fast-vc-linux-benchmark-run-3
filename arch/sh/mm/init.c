f60075feaaaec00a757de6c76dab22dbe11be3a1
Christoph Hellwig <hch@lst.de>
2020-07-14 14:18:50 +0200

    sh: move ioremap_fixed details out of <asm/io.h>
    ioremap_fixed is an internal implementation detail and should not be
    exposed to drivers.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Rich Felker <dalias@libc.org>

--- arch/sh/mm/init.c
+++ arch/sh/mm/init.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/setup.h>
 #include <asm/cache.h>
 #include <linux/sizes.h>
+#include "ioremap.h"
 
 pgd_t swapper_pg_dir[PTRS_PER_PGD];
 
