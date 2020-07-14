9db280e70b9beab975db62803cd392e8d2519596
Brendan Higgins <brendanhiggins@google.com>
2020-07-14 13:41:30 -0700

    kunit: tool: fix improper treatment of file location
    Commit 01397e822af4 ("kunit: Fix TabError, remove defconfig code and
    handle when there is no kunitconfig") and commit 45ba7a893ad8 ("kunit:
    kunit_tool: Separate out config/build/exec/parse") introduced two
    closely related issues which built off of each other: they excessively
    created the build directory when not present and modified a constant
    (constants in Python only exist by convention).
    
    Together these issues broken a number of unit tests for KUnit tool, so
    fix them.
    
    Fixed up commit log to fic checkpatch commit description style error.
    Shuah Khan <skhan@linuxfoundation.org>
    
    Fixes: 01397e822af4 ("kunit: Fix TabError, remove defconfig code and handle when there is no kunitconfig")
    Fixes: 45ba7a893ad8 ("kunit: kunit_tool: Separate out config/build/exec/parse")
    Signed-off-by: Brendan Higgins <brendanhiggins@google.com>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>

--- tools/testing/kunit/kunit.py
+++ tools/testing/kunit/kunit.py
@@ -239,12 +239,6 @@ def main(argv, linux=None):
 	if cli_args.subcommand == 'run':
 		if not os.path.exists(cli_args.build_dir):
 			os.mkdir(cli_args.build_dir)
-		kunit_kernel.kunitconfig_path = os.path.join(
-			cli_args.build_dir,
-			kunit_kernel.kunitconfig_path)
-
-		if not os.path.exists(kunit_kernel.kunitconfig_path):
-			create_default_kunitconfig()
 
 		if not linux:
 			linux = kunit_kernel.LinuxSourceTree()
@@ -262,12 +256,6 @@ def main(argv, linux=None):
 		if cli_args.build_dir:
 			if not os.path.exists(cli_args.build_dir):
 				os.mkdir(cli_args.build_dir)
-			kunit_kernel.kunitconfig_path = os.path.join(
-				cli_args.build_dir,
-				kunit_kernel.kunitconfig_path)
-
-		if not os.path.exists(kunit_kernel.kunitconfig_path):
-			create_default_kunitconfig()
 
 		if not linux:
 			linux = kunit_kernel.LinuxSourceTree()
@@ -284,12 +272,6 @@ def main(argv, linux=None):
 		if cli_args.build_dir:
 			if not os.path.exists(cli_args.build_dir):
 				os.mkdir(cli_args.build_dir)
-			kunit_kernel.kunitconfig_path = os.path.join(
-				cli_args.build_dir,
-				kunit_kernel.kunitconfig_path)
-
-		if not os.path.exists(kunit_kernel.kunitconfig_path):
-			create_default_kunitconfig()
 
 		if not linux:
 			linux = kunit_kernel.LinuxSourceTree()
@@ -308,12 +290,6 @@ def main(argv, linux=None):
 		if cli_args.build_dir:
 			if not os.path.exists(cli_args.build_dir):
 				os.mkdir(cli_args.build_dir)
-			kunit_kernel.kunitconfig_path = os.path.join(
-				cli_args.build_dir,
-				kunit_kernel.kunitconfig_path)
-
-		if not os.path.exists(kunit_kernel.kunitconfig_path):
-			create_default_kunitconfig()
 
 		if not linux:
 			linux = kunit_kernel.LinuxSourceTree()
