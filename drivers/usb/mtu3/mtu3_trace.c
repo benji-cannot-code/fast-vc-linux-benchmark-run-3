c389b224a2efee83d3e138b4e984a99e504b065a
Lee Jones <lee.jones@linaro.org>
2020-07-15 10:32:05 +0100

    usb: mtu3: mtu3_trace: Supply missing mtu3_debug.h include file
    If the header file containing a function's prototype isn't included by
    the sourcefile containing the associated function, the build system
    complains of missing prototypes.
    
    Fixes the following W=1 kernel build warning(s):
    
     drivers/usb/mtu3/mtu3_trace.c:13:6: warning: no previous prototype for ‘mtu3_dbg_trace’ [-Wmissing-prototypes]
     13 | void mtu3_dbg_trace(struct device *dev, const char *fmt, ...)
     | ^~~~~~~~~~~~~~
    
    Cc: Chunfeng Yun <chunfeng.yun@mediatek.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-mediatek@lists.infradead.org
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Link: https://lore.kernel.org/r/20200715093209.3165641-5-lee.jones@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/mtu3/mtu3_trace.c
+++ drivers/usb/mtu3/mtu3_trace.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #define CREATE_TRACE_POINTS
+#include "mtu3_debug.h"
 #include "mtu3_trace.h"
 
 void mtu3_dbg_trace(struct device *dev, const char *fmt, ...)
