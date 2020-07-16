55801be8c47275854cf99b2c676c790c466c8597
Matt Roper <matthew.d.roper@intel.com>
2020-07-16 15:05:50 -0700

    drm/i915/rkl: Handle HTI
    If HTI (also sometimes called HDPORT) is enabled at startup, it may be
    using some of the PHYs and DPLLs making them unavailable for general
    usage.  Let's read out the HDPORT_STATE register and avoid making use of
    resources that HTI is already using.
    
    v2:
     - Fix minor checkpatch warnings
    
    v3:
     - Just readout HDPORT_STATE register once during init and then parse it
       later as needed.
     - Add a 'has_hti' device info flag to track whether we should readout
       HDPORT_STATE or not.  We can skip the platform/flag tests later since
       the hti_state in dev_priv will remain 0 for platforms it does not
       apply to.
     - Move PLL masking into icl_get_combo_phy_dpll() since at the moment
       RKL is the only platform that has HTI.  (Jose)
    
    Bspec: 49189
    Bspec: 53707
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    Cc: José Roberto de Souza <jose.souza@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200716220551.2730644-5-matthew.d.roper@intel.com
    Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "display/intel_ddi.h"
 #include "display/intel_dp.h"
 #include "display/intel_dp_mst.h"
+#include "display/intel_dpll_mgr.h"
 #include "display/intel_dsi.h"
 #include "display/intel_dvo.h"
 #include "display/intel_gmbus.h"
@@ -17903,6 +17904,13 @@ int intel_modeset_init(struct drm_i915_private *i915)
 
 	if (i915->max_cdclk_freq == 0)
 		intel_update_max_cdclk(i915);
+
+	/*
+	 * If the platform has HTI, we need to find out whether it has reserved
+	 * any display resources before we create our display outputs.
+	 */
+	if (INTEL_INFO(i915)->display.has_hti)
+		i915->hti_state = intel_de_read(i915, HDPORT_STATE);
 
 	/* Just disable it once at startup */
 	intel_vga_disable(i915);
