f52c16a2e1a818589adb7fdc489f54245c974d89
Alexei Podtelezhnikov <apodtele@gmail.com>
2020-07-16 20:20:55 +0300

    drm/i915: Reclassify SKL 0x1923 and 0x1927 as ULT
    Reclassify 0x1923, 0x1927 according to specifications. Of note,
    the second to last digit seems to correspond to GT#.
    
    Signed-off-by: Alexei Podtelezhnikov <apodtele@gmail.com>
    [vsyrjala: Split separate changes into separate patches,
               Sort the IDs]
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200716172106.2656-4-ville.syrjala@linux.intel.com
    Reviewed-by: Anusha Srivatsa <anusha.srivatsa@intel.com>

--- include/drm/i915_pciids.h
+++ include/drm/i915_pciids.h
@@ -358,12 +358,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	INTEL_VGA_DEVICE(0x191D, info)  /* WKS GT2 */
 
 #define INTEL_SKL_ULT_GT3_IDS(info) \
-	INTEL_VGA_DEVICE(0x1926, info) /* ULT GT3 */
+	INTEL_VGA_DEVICE(0x1923, info), /* ULT GT3 */ \
+	INTEL_VGA_DEVICE(0x1926, info), /* ULT GT3 */ \
+	INTEL_VGA_DEVICE(0x1927, info)  /* ULT GT3 */
 
 #define INTEL_SKL_GT3_IDS(info) \
 	INTEL_SKL_ULT_GT3_IDS(info), \
-	INTEL_VGA_DEVICE(0x1923, info), /* ULT GT3 */ \
-	INTEL_VGA_DEVICE(0x1927, info), /* ULT GT3 */ \
 	INTEL_VGA_DEVICE(0x192A, info), /* SRV GT3 */ \
 	INTEL_VGA_DEVICE(0x192B, info), /* Halo GT3 */ \
 	INTEL_VGA_DEVICE(0x192D, info)  /* SRV GT3 */
