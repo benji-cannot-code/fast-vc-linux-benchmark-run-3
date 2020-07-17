b539bb63f08dbb5644fc49de232087f7d1d738b8
Daniel Lezcano <daniel.lezcano@linaro.org>
2020-07-17 18:42:16 +0200

    thermal: netlink: Improve the initcall ordering
    The initcalls like to play joke. In our case, the thermal-netlink
    initcall is called after the thermal-core initcall but this one sends
    a notification before the former is initialized. No issue was spotted,
    but it could lead to a memory corruption, so instead of relying on the
    core_initcall for the thermal-netlink, let's initialize directly from
    the thermal-core init routine, so we have full control of the init
    ordering.
    
    Reported-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Reviewed-by: Amit Kucheria <amit.kucheria@linaro.org>
    Link: https://lore.kernel.org/r/20200717164217.18819-1-daniel.lezcano@linaro.org

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1641,6 +1641,10 @@ static int __init thermal_init(void)
 {
 	int result;
 
+	result = thermal_netlink_init();
+	if (result)
+		goto error;
+
 	mutex_init(&poweroff_lock);
 	result = thermal_register_governors();
 	if (result)
--- drivers/thermal/thermal_netlink.h
+++ drivers/thermal/thermal_netlink.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Netlink notification function */
 #ifdef CONFIG_THERMAL_NETLINK
+int __init thermal_netlink_init(void);
 int thermal_notify_tz_create(int tz_id, const char *name);
 int thermal_notify_tz_delete(int tz_id);
 int thermal_notify_tz_enable(int tz_id);
@@ -24,6 +25,11 @@ int thermal_notify_cdev_delete(int cdev_id);
 int thermal_notify_tz_gov_change(int tz_id, const char *name);
 int thermal_genl_sampling_temp(int id, int temp);
 #else
+static inline int thermal_netlink_init(void)
+{
+	return 0;
+}
+
 static inline int thermal_notify_tz_create(int tz_id, const char *name)
 {
 	return 0;
