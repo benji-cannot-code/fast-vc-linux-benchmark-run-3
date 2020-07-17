0872c06701219965b0391706a55528be1cb878bf
Sami Tolvanen <samitolvanen@google.com>
2020-07-17 12:04:27 -0700

    objtool: Don't autodetect vmlinux.o
    With LTO, we run objtool on vmlinux.o, but don't want noinstr
    validation. This change requires --vmlinux to be passed to objtool
    explicitly.
    
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>

--- tools/objtool/builtin-check.c
+++ tools/objtool/builtin-check.c
@@ -42,7 +42,7 @@ const struct option check_options[] = {
 
 int cmd_check(int argc, const char **argv)
 {
-	const char *objname, *s;
+	const char *objname;
 	struct objtool_file *file;
 	int ret;
 
@@ -52,10 +52,6 @@ int cmd_check(int argc, const char **argv)
 		usage_with_options(check_usage, check_options);
 
 	objname = argv[0];
-
-	s = strstr(objname, "vmlinux.o");
-	if (s && !s[9])
-		vmlinux = true;
 
 	file = objtool_open_read(objname);
 	if (!file)
