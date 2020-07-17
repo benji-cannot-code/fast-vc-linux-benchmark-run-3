14ea960403c027cfb0c4088d2b62dc76733b6182
Takashi Iwai <tiwai@suse.de>
2020-07-17 08:41:30 +0200

    Revert "ALSA: pcm: Use SG-buffer only when direct DMA is available"
    This reverts commit 3ad796cbc36a ("ALSA: pcm: Use SG-buffer only when
    direct DMA is available") also the modification commit 467fd0e82b62
    ("ALSA: pcm: Fix build error on m68k and others").
    
    Poking the DMA internal helper is a layer violation, so we should
    avoid that.  Meanwhile the actual bug has been addressed by the
    Kconfig fix in commit dbed452a078d ("dma-pool: decouple DMA_REMAP from
    DMA_COHERENT_POOL"), so we can live without this hack.
    
    Link: https://lore.kernel.org/r/20200717064130.22957-1-tiwai@suse.de
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/pcm_memory.c
+++ sound/core/pcm_memory.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/moduleparam.h>
 #include <linux/vmalloc.h>
 #include <linux/export.h>
-#include <linux/dma-mapping.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/info.h>
@@ -41,19 +40,6 @@ static int do_alloc_pages(struct snd_card *card, int type, struct device *dev,
 	if (max_alloc_per_card &&
 	    card->total_pcm_alloc_bytes + size > max_alloc_per_card)
 		return -ENOMEM;
-
-
-#ifdef CONFIG_SND_DMA_SGBUF
-	if ((type == SNDRV_DMA_TYPE_DEV_SG || type == SNDRV_DMA_TYPE_DEV_UC_SG) &&
-	    !dma_is_direct(get_dma_ops(dev))) {
-		/* mutate to continuous page allocation */
-		dev_dbg(dev, "Use continuous page allocator\n");
-		if (type == SNDRV_DMA_TYPE_DEV_SG)
-			type = SNDRV_DMA_TYPE_DEV;
-		else
-			type = SNDRV_DMA_TYPE_DEV_UC;
-	}
-#endif /* CONFIG_SND_DMA_SGBUF */
 
 	err = snd_dma_alloc_pages(type, dev, size, dmab);
 	if (!err) {
