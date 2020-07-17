88448a509bb8bc9e0eb13b9f516609b3437ac9d8
Lee Jones <lee.jones@linaro.org>
2020-07-17 14:54:19 +0100

    spi: spi-zynq-qspi: Add description for 2 missing attributes/parameters
    Fixes the following W=1 kernel build warning(s):
    
     drivers/spi/spi-zynq-qspi.c:143: warning: Function parameter or member 'dev' not described in 'zynq_qspi'
     drivers/spi/spi-zynq-qspi.c:334: warning: Function parameter or member 'spi' not described in 'zynq_qspi_config_op'
     drivers/spi/spi-zynq-qspi.c:334: warning: Excess function parameter 'qspi' description in 'zynq_qspi_config_op'
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Reviewed-by: Michal Simek <michal.simek@xilinx.com>
    Cc: Sureshkumar Relli <nagasure@xilinx.com>
    Link: https://lore.kernel.org/r/20200717135424.2442271-10-lee.jones@linaro.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-zynq-qspi.c
+++ drivers/spi/spi-zynq-qspi.c
@@ -120,6 +120,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /**
  * struct zynq_qspi - Defines qspi driver instance
+ * @dev:		Pointer to the this device's information
  * @regs:		Virtual address of the QSPI controller registers
  * @refclk:		Pointer to the peripheral clock
  * @pclk:		Pointer to the APB clock
@@ -317,7 +318,7 @@ static void zynq_qspi_chipselect(struct spi_device *spi, bool assert)
 /**
  * zynq_qspi_config_op - Configure QSPI controller for specified transfer
  * @xqspi:	Pointer to the zynq_qspi structure
- * @qspi:	Pointer to the spi_device structure
+ * @spi:	Pointer to the spi_device structure
  *
  * Sets the operational mode of QSPI controller for the next QSPI transfer and
  * sets the requested clock frequency.
