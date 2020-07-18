84d99d6056340d66a3ca4a153f23ee3c549564ae
Krishna Reddy <vdumpa@nvidia.com>
2020-07-18 12:34:53 -0700

    iommu/arm-smmu: move TLB timeout and spin count macros
    Move TLB timeout and spin count macros to header file to
    allow using the same from vendor specific implementations.
    
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-by: Nicolin Chen <nicoleotsuka@gmail.com>
    Reviewed-by: Pritesh Raithatha <praithatha@nvidia.com>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>
    Reviewed-by: Thierry Reding <thierry.reding@gmail.com>
    Link: https://lore.kernel.org/r/20200718193457.30046-2-vdumpa@nvidia.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -53,9 +53,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define QCOM_DUMMY_VAL -1
 
-#define TLB_LOOP_TIMEOUT		1000000	/* 1s! */
-#define TLB_SPIN_COUNT			10
-
 #define MSI_IOVA_BASE			0x8000000
 #define MSI_IOVA_LENGTH			0x100000
 
