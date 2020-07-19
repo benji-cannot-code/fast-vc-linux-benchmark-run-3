3d8e3320e79b8b8efb3bfa5c87be0609ec57d65f
Ben Skeggs <bskeggs@redhat.com>
2020-07-20 07:56:31 +1000

    drm/nouveau/kms/nv50-: use NVIDIA's headers for core crc_set_src()
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>
    Reviewed-by: Lyude Paul <lyude@redhat.com>

--- drivers/gpu/drm/nouveau/dispnv50/crcc37d.c
+++ drivers/gpu/drm/nouveau/dispnv50/crcc37d.c
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <nvif/push507c.h>
 
+#include <nvhw/class/clc37d.h>
+
 #define CRCC37D_MAX_ENTRIES 2047
 
 struct crcc37d_notifier {
@@ -40,21 +42,23 @@ crcc37d_set_src(struct nv50_head *head, int or,
 {
 	struct nvif_push *push = nv50_disp(head->base.base.dev)->core->chan.push;
 	const int i = head->base.index;
-	u32 crc_args;
+	u32 crc_args = NVVAL(NVC37D, HEAD_SET_CRC_CONTROL, CONTROLLING_CHANNEL, wndw) |
+		       NVDEF(NVC37D, HEAD_SET_CRC_CONTROL, EXPECT_BUFFER_COLLAPSE, FALSE) |
+		       NVDEF(NVC37D, HEAD_SET_CRC_CONTROL, SECONDARY_CRC, NONE) |
+		       NVDEF(NVC37D, HEAD_SET_CRC_CONTROL, CRC_DURING_SNOOZE, DISABLE);
 	int ret;
 
 	switch (source) {
 	case NV50_CRC_SOURCE_TYPE_SOR:
-		crc_args = (0x00000050 + or) << 12;
+		crc_args |= NVDEF(NVC37D, HEAD_SET_CRC_CONTROL, PRIMARY_CRC, SOR(or));
 		break;
 	case NV50_CRC_SOURCE_TYPE_PIOR:
-		crc_args = (0x00000060 + or) << 12;
+		crc_args |= NVDEF(NVC37D, HEAD_SET_CRC_CONTROL, PRIMARY_CRC, PIOR(or));
 		break;
 	case NV50_CRC_SOURCE_TYPE_SF:
-		crc_args = 0x00000030 << 12;
+		crc_args |= NVDEF(NVC37D, HEAD_SET_CRC_CONTROL, PRIMARY_CRC, SF);
 		break;
 	default:
-		crc_args = 0;
 		break;
 	}
 
@@ -62,11 +66,11 @@ crcc37d_set_src(struct nv50_head *head, int or,
 		return ret;
 
 	if (source) {
-		PUSH_NVSQ(push, NVC37D, 0x2180 + (i * 0x400), ctx->ntfy.handle);
-		PUSH_NVSQ(push, NVC37D, 0x2184 + (i * 0x400), crc_args | wndw);
+		PUSH_MTHD(push, NVC37D, HEAD_SET_CONTEXT_DMA_CRC(i), ctx->ntfy.handle);
+		PUSH_MTHD(push, NVC37D, HEAD_SET_CRC_CONTROL(i), crc_args);
 	} else {
-		PUSH_NVSQ(push, NVC37D, 0x2184 + (i * 0x400), 0);
-		PUSH_NVSQ(push, NVC37D, 0x2180 + (i * 0x400), 0);
+		PUSH_MTHD(push, NVC37D, HEAD_SET_CRC_CONTROL(i), 0);
+		PUSH_MTHD(push, NVC37D, HEAD_SET_CONTEXT_DMA_CRC(i), 0);
 	}
 
 	return 0;
