f2f45e86bc05bf7c45909861d497010282e8333d
Ignat Korchagin <ignat@cloudflare.com>
2020-07-19 22:02:21 +0100

    um: Some fixes to build UML with musl
    musl toolchain and headers are a bit more strict. These fixes enable building
    UML with musl as well as seem not to break on glibc.
    
    Signed-off-by: Ignat Korchagin <ignat@cloudflare.com>
    Tested-by: Brendan Higgins <brendanhiggins@google.com>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/x86/um/user-offsets.c
+++ arch/x86/um/user-offsets.c
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdio.h>
 #include <stddef.h>
 #include <signal.h>
-#include <sys/poll.h>
+#include <poll.h>
 #include <sys/mman.h>
 #include <sys/user.h>
 #define __FRAME_OFFSETS
