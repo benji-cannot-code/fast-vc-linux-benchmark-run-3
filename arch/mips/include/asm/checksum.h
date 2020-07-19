ba7a194c3288ffc1f161cb7c20e5f61e2e2b230b
Al Viro <viro@zeniv.linux.org.uk>
2020-07-19 14:01:58 -0400

    mips: __csum_partial_copy_kernel() has no users left
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/mips/include/asm/checksum.h
+++ arch/mips/include/asm/checksum.h
@@ -35,9 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 __wsum csum_partial(const void *buff, int len, __wsum sum);
 
-__wsum __csum_partial_copy_kernel(const void *src, void *dst,
-				  int len, __wsum sum, int *err_ptr);
-
 __wsum __csum_partial_copy_from_user(const void *src, void *dst,
 				     int len, __wsum sum, int *err_ptr);
 __wsum __csum_partial_copy_to_user(const void *src, void *dst,
