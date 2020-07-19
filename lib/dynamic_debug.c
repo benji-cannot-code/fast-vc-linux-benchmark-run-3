b616ffe015ed8e28c1a009748c2146118120488f
Jim Cromie <jim.cromie@gmail.com>
2020-07-19 17:10:57 -0600

    dyndbg: shorten our logging prefix, drop __func__
    For log-message output, reduce column space consumed by current
    pr_fmt by dropping __func__ and shortening "dynamic_debug" to
    "dyndbg".  This improves readability on narrow consoles, and better
    matches other kernel boot info messages.
    
    Acked-by: <jbaron@akamai.com>
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    Link: https://lore.kernel.org/r/20200719231058.1586423-18-jim.cromie@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2013 Du, Changbin <changbin.du@gmail.com>
  */
 
-#define pr_fmt(fmt) KBUILD_MODNAME ":%s: " fmt, __func__
+#define pr_fmt(fmt) "dyndbg: " fmt
 
 #include <linux/kernel.h>
 #include <linux/module.h>
