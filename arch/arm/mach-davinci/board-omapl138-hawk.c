b73e25b98c26842425fccca125cdd4d00e1e7254
Alexander A. Klimov <grandmaster@al2klimov.de>
2020-07-19 12:20:20 +0200

    ARM: davinci: Replace HTTP links with HTTPS ones
    Rationale:
    Reduces attack surface on kernel devs opening the links for MITM
    as HTTPS traffic is much harder to manipulate.
    
    Deterministic algorithm:
    For each file:
      If not .svg:
        For each line:
          If doesn't contain `\bxmlns\b`:
            For each link, `\bhttp://[^# \t\r\n]*(?:\w|/)`:
    	  If neither `\bgnu\.org/license`, nor `\bmozilla\.org/MPL\b`:
                If both the HTTP and HTTPS versions
                return 200 OK and serve the same content:
                  Replace HTTP with HTTPS.
    
    Signed-off-by: Alexander A. Klimov <grandmaster@al2klimov.de>
    [nsekhar@ti.com: drop obsolete hawkboard.org URL completeley
    		 fixup subject line prefix]
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- arch/arm/mach-davinci/board-omapl138-hawk.c
+++ arch/arm/mach-davinci/board-omapl138-hawk.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Initial code: Syed Mohammed Khasim
  *
- * Copyright (C) 2009 Texas Instruments Incorporated - http://www.ti.com
+ * Copyright (C) 2009 Texas Instruments Incorporated - https://www.ti.com
  *
  * This file is licensed under the terms of the GNU General Public License
  * version 2. This program is licensed "as is" without any warranty of
