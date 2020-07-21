74028b6a4f5297c88fe641219cd58e114136a98b
Changfeng <Changfeng.Zhu@amd.com>
2020-07-21 10:38:19 +0800

    Revert "drm/amd/powerplay: drop unnecessary message support check"
    The below 3 messages are not supported on Renoir
    SMU_MSG_PrepareMp1ForShutdown
    SMU_MSG_PrepareMp1ForUnload
    SMU_MSG_PrepareMp1ForReset
    
    It needs to revert patch:
    drm/amd/powerplay: drop unnecessary message support check
    to avoid set mp1 state fail during gpu reset on renoir.
    
    Signed-off-by: changfeng <Changfeng.Zhu@amd.com>
    Reviewed-by: Kenneth Feng <kenneth.feng@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/powerplay/smu_cmn.h
+++ drivers/gpu/drm/amd/powerplay/smu_cmn.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "amdgpu_smu.h"
 
-#if defined(SWSMU_CODE_LAYER_L2) || defined(SWSMU_CODE_LAYER_L3) || defined(SWSMU_CODE_LAYER_L4)
+#if defined(SWSMU_CODE_LAYER_L1) || defined(SWSMU_CODE_LAYER_L2) || defined(SWSMU_CODE_LAYER_L3) || defined(SWSMU_CODE_LAYER_L4)
 int smu_cmn_send_smc_msg_with_param(struct smu_context *smu,
 				    enum smu_message_type msg,
 				    uint32_t param,
