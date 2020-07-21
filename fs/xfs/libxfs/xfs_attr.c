68f031ab59263f59c3c3226acb698c7339b4faaf
Allison Collins <allison.henderson@oracle.com>
2020-07-20 21:47:26 -0700

    xfs: Pull up xfs_attr_rmtval_invalidate
    This patch pulls xfs_attr_rmtval_invalidate out of
    xfs_attr_rmtval_remove and into the calling functions.  Eventually
    __xfs_attr_rmtval_remove will replace xfs_attr_rmtval_remove when we
    introduce delayed attributes.  These functions are exepcted to return
    -EAGAIN when they need a new transaction.  Because the invalidate does
    not need a new transaction, we need to separate it from the rest of the
    function that does.  This will enable __xfs_attr_rmtval_remove to
    smoothly replace xfs_attr_rmtval_remove later.
    
    Signed-off-by: Allison Collins <allison.henderson@oracle.com>
    Reviewed-by: Brian Foster <bfoster@redhat.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Acked-by: Dave Chinner <dchinner@redhat.com>

--- fs/xfs/libxfs/xfs_attr.c
+++ fs/xfs/libxfs/xfs_attr.c
@@ -671,6 +671,10 @@ xfs_attr_leaf_addname(
 		args->rmtblkcnt = args->rmtblkcnt2;
 		args->rmtvaluelen = args->rmtvaluelen2;
 		if (args->rmtblkno) {
+			error = xfs_attr_rmtval_invalidate(args);
+			if (error)
+				return error;
+
 			error = xfs_attr_rmtval_remove(args);
 			if (error)
 				return error;
@@ -1024,6 +1028,10 @@ restart:
 		args->rmtblkcnt = args->rmtblkcnt2;
 		args->rmtvaluelen = args->rmtvaluelen2;
 		if (args->rmtblkno) {
+			error = xfs_attr_rmtval_invalidate(args);
+			if (error)
+				return error;
+
 			error = xfs_attr_rmtval_remove(args);
 			if (error)
 				return error;
@@ -1147,6 +1155,10 @@ xfs_attr_node_removename(
 		error = xfs_trans_roll_inode(&args->trans, args->dp);
 		if (error)
 			goto out;
+
+		error = xfs_attr_rmtval_invalidate(args);
+		if (error)
+			return error;
 
 		error = xfs_attr_rmtval_remove(args);
 		if (error)
