62a55eb1b884bb546cf99c3e013532938e985378
Randy Dunlap <rdunlap@infradead.org>
2020-07-21 14:00:22 -0700

    parisc: elf.h: delete a duplicated word
    Drop the repeated word "the".
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Cc: "James E.J. Bottomley" <James.Bottomley@HansenPartnership.com>
    Cc: Helge Deller <deller@gmx.de>
    Cc: linux-parisc@vger.kernel.org
    Signed-off-by: Helge Deller <deller@gmx.de>

--- arch/parisc/include/asm/elf.h
+++ arch/parisc/include/asm/elf.h
@@ -153,7 +153,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* The following are PA function descriptors 
  *
  * addr:	the absolute address of the function
- * gp:		either the data pointer (r27) for non-PIC code or the
+ * gp:		either the data pointer (r27) for non-PIC code or
  *		the PLT pointer (r19) for PIC code */
 
 /* Format for the Elf32 Function descriptor */
