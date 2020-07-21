6c14a1100d4fd7d5175bbb980649f46e416f46d9
Luke Nelson <lukenels@cs.washington.edu>
2020-07-20 19:52:38 -0700

    bpf, riscv: Modify JIT ctx to support compressed instructions
    This patch makes the necessary changes to struct rv_jit_context and to
    bpf_int_jit_compile to support compressed riscv (RVC) instructions in
    the BPF JIT.
    
    It changes the JIT image to be u16 instead of u32, since RVC instructions
    are 2 bytes as opposed to 4.
    
    It also changes ctx->offset and ctx->ninsns to refer to 2-byte
    instructions rather than 4-byte ones. The riscv PC is required to be
    16-bit aligned with or without RVC, so this is sufficient to refer to
    any valid riscv offset.
    
    The code for computing jump offsets in bytes is updated accordingly,
    and factored into a new "ninsns_rvoff" function to simplify the code.
    
    Signed-off-by: Luke Nelson <luke.r.nels@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200721025241.8077-2-luke.r.nels@gmail.com

--- arch/riscv/net/bpf_jit.h
+++ arch/riscv/net/bpf_jit.h
@@ -14,6 +14,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/filter.h>
 #include <asm/cacheflush.h>
 
+static inline bool rvc_enabled(void)
+{
+	return IS_ENABLED(CONFIG_RISCV_ISA_C);
+}
+
 enum {
 	RV_REG_ZERO =	0,	/* The constant value 0 */
 	RV_REG_RA =	1,	/* Return address */
@@ -51,13 +56,19 @@ enum {
 
 struct rv_jit_context {
 	struct bpf_prog *prog;
-	u32 *insns;		/* RV insns */
+	u16 *insns;		/* RV insns */
 	int ninsns;
 	int epilogue_offset;
 	int *offset;		/* BPF to RV */
 	unsigned long flags;
 	int stack_size;
 };
+
+/* Convert from ninsns to bytes. */
+static inline int ninsns_rvoff(int ninsns)
+{
+	return ninsns << 1;
+}
 
 struct rv_jit_data {
 	struct bpf_binary_header *header;
@@ -75,8 +86,22 @@ static inline void bpf_flush_icache(void *start, void *end)
 	flush_icache_range((unsigned long)start, (unsigned long)end);
 }
 
+/* Emit a 4-byte riscv instruction. */
 static inline void emit(const u32 insn, struct rv_jit_context *ctx)
 {
+	if (ctx->insns) {
+		ctx->insns[ctx->ninsns] = insn;
+		ctx->insns[ctx->ninsns + 1] = (insn >> 16);
+	}
+
+	ctx->ninsns += 2;
+}
+
+/* Emit a 2-byte riscv compressed instruction. */
+static inline void emitc(const u16 insn, struct rv_jit_context *ctx)
+{
+	BUILD_BUG_ON(!rvc_enabled());
+
 	if (ctx->insns)
 		ctx->insns[ctx->ninsns] = insn;
 
@@ -87,7 +112,7 @@ static inline int epilogue_offset(struct rv_jit_context *ctx)
 {
 	int to = ctx->epilogue_offset, from = ctx->ninsns;
 
-	return (to - from) << 2;
+	return ninsns_rvoff(to - from);
 }
 
 /* Return -1 or inverted cond. */
@@ -150,7 +175,7 @@ static inline int rv_offset(int insn, int off, struct rv_jit_context *ctx)
 	off++; /* BPF branch is from PC+1, RV is from PC */
 	from = (insn > 0) ? ctx->offset[insn - 1] : 0;
 	to = (insn + off > 0) ? ctx->offset[insn + off - 1] : 0;
-	return (to - from) << 2;
+	return ninsns_rvoff(to - from);
 }
 
 /* Instruction formats. */
