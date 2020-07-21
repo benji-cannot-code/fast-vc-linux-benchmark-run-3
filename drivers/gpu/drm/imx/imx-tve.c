5490d0c7bb34d21f90237350483fbd83f4342b4f
Philipp Zabel <p.zabel@pengutronix.de>
2020-07-21 16:16:58 +0200

    drm/imx: imx-tve: use regmap fast_io spinlock
    Replace the custom spinlock with the fast_io spinlock provided by
    regmap.
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/gpu/drm/imx/imx-tve.c
+++ drivers/gpu/drm/imx/imx-tve.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
 #include <linux/regulator/consumer.h>
-#include <linux/spinlock.h>
 #include <linux/videodev2.h>
 
 #include <video/imx-ipu-v3.h>
@@ -105,7 +104,6 @@ struct imx_tve {
 	struct drm_connector connector;
 	struct drm_encoder encoder;
 	struct device *dev;
-	spinlock_t lock;	/* register lock */
 	bool enabled;
 	int mode;
 	int di_hsync_pin;
@@ -128,22 +126,6 @@ static inline struct imx_tve *con_to_tve(struct drm_connector *c)
 static inline struct imx_tve *enc_to_tve(struct drm_encoder *e)
 {
 	return container_of(e, struct imx_tve, encoder);
-}
-
-static void tve_lock(void *__tve)
-__acquires(&tve->lock)
-{
-	struct imx_tve *tve = __tve;
-
-	spin_lock(&tve->lock);
-}
-
-static void tve_unlock(void *__tve)
-__releases(&tve->lock)
-{
-	struct imx_tve *tve = __tve;
-
-	spin_unlock(&tve->lock);
 }
 
 static void tve_enable(struct imx_tve *tve)
@@ -501,8 +483,7 @@ static struct regmap_config tve_regmap_config = {
 
 	.readable_reg = imx_tve_readable_reg,
 
-	.lock = tve_lock,
-	.unlock = tve_unlock,
+	.fast_io = true,
 
 	.max_register = 0xdc,
 };
@@ -545,7 +526,6 @@ static int imx_tve_bind(struct device *dev, struct device *master, void *data)
 	memset(tve, 0, sizeof(*tve));
 
 	tve->dev = dev;
-	spin_lock_init(&tve->lock);
 
 	ddc_node = of_parse_phandle(np, "ddc-i2c-bus", 0);
 	if (ddc_node) {
