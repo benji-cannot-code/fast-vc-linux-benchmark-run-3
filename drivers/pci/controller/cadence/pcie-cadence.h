0894487877004dc1324fb739f5e9520d31de6d9e
Alan Douglas <adouglas@cadence.com>
2020-07-22 16:33:12 +0530

    PCI: cadence: Add MSI-X support to Endpoint driver
    Implement ->set_msix() and ->get_msix() callback functions in order
    to configure MSIX capability in the PCIe endpoint controller.
    
    Add cdns_pcie_ep_send_msix_irq() to send MSIX interrupts to Host.
    cdns_pcie_ep_send_msix_irq() gets the MSIX table address (virtual
    address) from "struct cdns_pcie_epf" that gets initialized in
    ->set_bar() call back function.
    
    [kishon@ti.com: Re-implement MSIX support in accordance with the
     re-designed core MSI-X interfaces]
    Link: https://lore.kernel.org/r/20200722110317.4744-11-kishon@ti.com
    Signed-off-by: Alan Douglas <adouglas@cadence.com>
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>

--- drivers/pci/controller/cadence/pcie-cadence.h
+++ drivers/pci/controller/cadence/pcie-cadence.h
@@ -114,6 +114,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CDNS_PCIE_EP_FUNC_BASE(fn)	(((fn) << 12) & GENMASK(19, 12))
 
 #define CDNS_PCIE_EP_FUNC_MSI_CAP_OFFSET	0x90
+#define CDNS_PCIE_EP_FUNC_MSIX_CAP_OFFSET	0xb0
 
 /*
  * Root Port Registers (PCI configuration space for the root port function)
@@ -304,6 +305,14 @@ struct cdns_pcie_rc {
 };
 
 /**
+ * struct cdns_pcie_epf - Structure to hold info about endpoint function
+ * @epf_bar: reference to the pci_epf_bar for the six Base Address Registers
+ */
+struct cdns_pcie_epf {
+	struct pci_epf_bar *epf_bar[PCI_STD_NUM_BARS];
+};
+
+/**
  * struct cdns_pcie_ep - private data for this PCIe endpoint controller driver
  * @pcie: Cadence PCIe controller
  * @max_regions: maximum number of regions supported by hardware
@@ -322,6 +331,7 @@ struct cdns_pcie_rc {
  * @lock: spin lock to disable interrupts while modifying PCIe controller
  *        registers fields (RMW) accessible by both remote RC and EP to
  *        minimize time between read and write
+ * @epf: Structure to hold info about endpoint function
  */
 struct cdns_pcie_ep {
 	struct cdns_pcie	pcie;
@@ -335,6 +345,7 @@ struct cdns_pcie_ep {
 	u8			irq_pending;
 	/* protect writing to PCI_STATUS while raising legacy interrupts */
 	spinlock_t		lock;
+	struct cdns_pcie_epf	*epf;
 };
 
 
