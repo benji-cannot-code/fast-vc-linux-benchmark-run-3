156dbd12b8bb0e37d8eb2595235e2499dd420a82
Drew Fustini <drew@beagleboard.org>
2020-07-22 14:27:52 +0200

    pinctrl: core: print gpio in pins debugfs file
    If there is a gpio range mapping for the pin, then print out the gpio
    chip and line index for the pin in the debugfs 'pins' file with the
    format: "[line-index]:[gpio-label]"
    
    Here is example output on the BeagleBoard.org PocketBeagle (AM3358):
    /sys/kernel/debug/pinctrl/44e10800.pinmux-pinctrl-single/pins
    
    pin 25 (PIN25) 25:gpio-32-63 44e10864 00000037 pinctrl-single
    pin 26 (PIN26) 26:gpio-32-63 44e10868 00000037 pinctrl-single
    pin 27 (PIN27) 27:gpio-32-63 44e1086c 00000037 pinctrl-single
    pin 28 (PIN28) 0:? 44e10870 00000036 pinctrl-single
    pin 29 (PIN29) 0:? 44e10874 00000006 pinctrl-single
    pin 30 (PIN30) 28:gpio-32-63 44e10878 00000027 pinctrl-single
    pin 31 (PIN31) 29:gpio-32-63 44e1087c 00000037 pinctrl-single
    pin 32 (PIN32) 30:gpio-32-63 44e10880 00000037 pinctrl-single
    pin 33 (PIN33) 31:gpio-32-63 44e10884 00000037 pinctrl-single
    pin 34 (PIN34) 0:gpio-64-95 44e10888 00000037 pinctrl-single
    pin 35 (PIN35) 1:gpio-64-95 44e1088c 00000037 pinctrl-single
    
    Suggested-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Suggested-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Drew Fustini <drew@beagleboard.org>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Link: https://lore.kernel.org/r/20200722122751.266440-1-drew@beagleboard.org
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/core.c
+++ drivers/pinctrl/core.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pinctrl/machine.h>
 
 #ifdef CONFIG_GPIOLIB
+#include "../gpio/gpiolib.h"
 #include <asm-generic/gpio.h>
 #endif
 
@@ -1602,6 +1603,9 @@ static int pinctrl_pins_show(struct seq_file *s, void *what)
 	struct pinctrl_dev *pctldev = s->private;
 	const struct pinctrl_ops *ops = pctldev->desc->pctlops;
 	unsigned i, pin;
+	struct pinctrl_gpio_range *range;
+	unsigned int gpio_num;
+	struct gpio_chip *chip;
 
 	seq_printf(s, "registered pins: %d\n", pctldev->desc->npins);
 
@@ -1618,6 +1622,23 @@ static int pinctrl_pins_show(struct seq_file *s, void *what)
 			continue;
 
 		seq_printf(s, "pin %d (%s) ", pin, desc->name);
+
+#ifdef CONFIG_GPIOLIB
+		gpio_num = 0;
+		list_for_each_entry(range, &pctldev->gpio_ranges, node) {
+			if ((pin >= range->pin_base) &&
+			    (pin < (range->pin_base + range->npins))) {
+				gpio_num = range->base + (pin - range->pin_base);
+				break;
+			}
+		}
+		chip = gpio_to_chip(gpio_num);
+		if (chip && chip->gpiodev && chip->gpiodev->base)
+			seq_printf(s, "%u:%s ", gpio_num -
+				chip->gpiodev->base, chip->label);
+		else
+			seq_puts(s, "0:? ");
+#endif
 
 		/* Driver-specific info per pin */
 		if (ops->pin_dbg_show)
