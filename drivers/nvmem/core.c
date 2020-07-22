05f1c95aa039ebf50fcacb2e35b9556f1dcd4fc0
Douglas Anderson <dianders@chromium.org>
2020-07-22 11:06:54 +0100

    nvmem: Enforce nvmem stride in the sysfs interface
    The 'struct nvmem_config' has a stride attribute that specifies the
    needed alignment for accesses into the nvmem.  This is used in
    nvmem_cell_info_to_nvmem_cell() but not in the sysfs read/write
    functions.  If the alignment is important in one place it's important
    everywhere, so let's add enforcement.
    
    For now we'll consider it totally invalid to access with the wrong
    alignment.  We could relax this in the read case where we could just
    read some extra bytes and throw them away.  Relaxing it in the write
    case seems harder (and less safe?) since we'd have to read some data
    first and then write it back.  To keep it symmetric we'll just
    disallow it in both cases.
    
    Reported-by: Ravi Kumar Bokka <rbokka@codeaurora.org>
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Ravi Kumar Bokka <rbokka@codeaurora.org>
    Tested-by: Ravi Kumar Bokka <rbokka@codeaurora.org>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Link: https://lore.kernel.org/r/20200722100705.7772-4-srinivas.kandagatla@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/nvmem/core.c
+++ drivers/nvmem/core.c
@@ -136,6 +136,9 @@ static ssize_t bin_attr_nvmem_read(struct file *filp, struct kobject *kobj,
 	if (pos >= nvmem->size)
 		return 0;
 
+	if (!IS_ALIGNED(pos, nvmem->stride))
+		return -EINVAL;
+
 	if (count < nvmem->word_size)
 		return -EINVAL;
 
@@ -172,6 +175,9 @@ static ssize_t bin_attr_nvmem_write(struct file *filp, struct kobject *kobj,
 	/* Stop the user from writing */
 	if (pos >= nvmem->size)
 		return -EFBIG;
+
+	if (!IS_ALIGNED(pos, nvmem->stride))
+		return -EINVAL;
 
 	if (count < nvmem->word_size)
 		return -EINVAL;
