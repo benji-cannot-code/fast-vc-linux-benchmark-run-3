92a6c04bde6313d204c9255387497c7a6b274d47
Thomas Gleixner <tglx@linutronix.de>
2020-07-23 00:00:06 +0200

    x86/entry: Cleanup idtentry_entry/exit_user
    Cleanup the temporary defines and use irqentry_ instead of idtentry_.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Link: https://lkml.kernel.org/r/20200722220520.602603691@linutronix.de

--- arch/x86/include/asm/idtentry.h
+++ arch/x86/include/asm/idtentry.h
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/irq_stack.h>
 
-/* Temporary define */
-#define idtentry_enter_user	irqentry_enter_from_user_mode
-#define idtentry_exit_user	irqentry_exit_to_user_mode
-
 typedef struct idtentry_state {
 	bool exit_rcu;
 } idtentry_state_t;
