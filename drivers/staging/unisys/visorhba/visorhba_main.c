b060d99c9ae334eacb745995f44ebd3a1367c5a6
Colin Ian King <colin.king@canonical.com>
2020-07-23 16:54:15 +0100

    staging: unisys: visorhba: remove redundant initialization of variables scsicmd_id and rc
    The variables scsicmd_id and rc is being initialized with a value
    that is never read and are being updated later with a new value.
    The initializations are redundant and can be removed.
    
    Addresses-Coverity: ("Unused value")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Link: https://lore.kernel.org/r/20200723155415.994036-1-colin.king@canonical.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/unisys/visorhba/visorhba_main.c
+++ drivers/staging/unisys/visorhba/visorhba_main.c
@@ -306,7 +306,7 @@ static int forward_taskmgmt_command(enum task_mgmt_types tasktype,
 		(struct visorhba_devdata *)scsidev->host->hostdata;
 	int notifyresult = 0xffff;
 	wait_queue_head_t notifyevent;
-	int scsicmd_id = 0;
+	int scsicmd_id;
 
 	if (devdata->serverdown || devdata->serverchangingstate)
 		return FAILED;
@@ -1187,7 +1187,7 @@ static struct visor_driver visorhba_driver = {
  */
 static int visorhba_init(void)
 {
-	int rc = -ENOMEM;
+	int rc;
 
 	visorhba_debugfs_dir = debugfs_create_dir("visorhba", NULL);
 	if (!visorhba_debugfs_dir)
