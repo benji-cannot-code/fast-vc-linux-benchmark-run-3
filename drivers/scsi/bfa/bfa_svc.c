050fe3a9072b17e615aca02e24900e28caa54bb8
Lee Jones <lee.jones@linaro.org>
2020-07-23 13:24:41 +0100

    scsi: bfa: Demote seemingly unintentional kerneldoc header
    This is the only use of kerneldoc in the source file and no descriptions
    are provided.
    
    Also demote standard comment.
    
    Fixes the following W=1 kernel build warning(s):
    
     drivers/scsi/bfa/bfa_svc.c:3847: warning: Function parameter or member 'bfa' not described in 'bfa_fcport_get_cfg_topology'
    
    Link: https://lore.kernel.org/r/20200723122446.1329773-36-lee.jones@linaro.org
    Cc: Anil Gurumurthy <anil.gurumurthy@qlogic.com>
    Cc: Sudarsana Kalluru <sudarsana.kalluru@qlogic.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/scsi/bfa/bfa_svc.c
+++ drivers/scsi/bfa/bfa_svc.c
@@ -2719,7 +2719,7 @@ bfa_fcport_sm_ddport(struct bfa_fcport_s *fcport,
 	case BFA_FCPORT_SM_DPORTDISABLE:
 	case BFA_FCPORT_SM_ENABLE:
 	case BFA_FCPORT_SM_START:
-		/**
+		/*
 		 * Ignore event for a port that is ddport
 		 */
 		break;
@@ -3840,7 +3840,7 @@ bfa_fcport_get_topology(struct bfa_s *bfa)
 	return fcport->topology;
 }
 
-/**
+/*
  * Get config topology.
  */
 enum bfa_port_topology
