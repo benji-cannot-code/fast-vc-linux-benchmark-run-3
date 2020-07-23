7c4a906233d2a4362c16aab35e49b4742ff11647
Michael Ellerman <mpe@ellerman.id.au>
2020-07-24 09:02:26 +1000

    powerpc/test_emulate_sstep: Fix build error
    ppc64_book3e_allmodconfig fails with:
    
      arch/powerpc/lib/test_emulate_step.c: In function 'test_pld':
      arch/powerpc/lib/test_emulate_step.c:113:7: error: implicit declaration of function 'cpu_has_feature'
        113 |  if (!cpu_has_feature(CPU_FTR_ARCH_31)) {
            |       ^~~~~~~~~~~~~~~
    
    Add an include of cpu_has_feature.h to fix it.
    
    Fixes: b6b54b42722a ("powerpc/sstep: Add tests for prefixed integer load/stores")
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200724004109.1461709-1-mpe@ellerman.id.au

--- arch/powerpc/lib/test_emulate_step.c
+++ arch/powerpc/lib/test_emulate_step.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define pr_fmt(fmt) "emulate_step_test: " fmt
 
 #include <linux/ptrace.h>
+#include <asm/cpu_has_feature.h>
 #include <asm/sstep.h>
 #include <asm/ppc-opcode.h>
 #include <asm/code-patching.h>
