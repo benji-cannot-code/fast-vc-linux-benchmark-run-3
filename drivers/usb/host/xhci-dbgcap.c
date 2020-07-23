1659955df4c93bfe11daeea378664d5d10261ef3
Mathias Nyman <mathias.nyman@linux.intel.com>
2020-07-23 17:45:16 +0300

    xhci: dbc: Change to pass dbc pointer to xhci_do_dbc_stop()
    Pass the dbc pointer instead of xhci_hcd pointer in order to
    decouple xhci and dbc.
    
    xhci_do_dbc_stop() only used xhci to get the dbc pointer.
    Pass the dbc pointer instead as a parameter
    
    No functional changes
    This change helps decoupling xhci and DbC
    
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Link: https://lore.kernel.org/r/20200723144530.9992-14-mathias.nyman@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/xhci-dbgcap.c
+++ drivers/usb/host/xhci-dbgcap.c
@@ -501,10 +501,8 @@ static int xhci_do_dbc_start(struct xhci_hcd *xhci)
 	return 0;
 }
 
-static int xhci_do_dbc_stop(struct xhci_hcd *xhci)
+static int xhci_do_dbc_stop(struct xhci_dbc *dbc)
 {
-	struct xhci_dbc		*dbc = xhci->dbc;
-
 	if (dbc->state == DS_DISABLED)
 		return -1;
 
@@ -551,7 +549,7 @@ static void xhci_dbc_stop(struct xhci_hcd *xhci)
 		xhci_dbc_tty_unregister_device(xhci);
 
 	spin_lock_irqsave(&dbc->lock, flags);
-	ret = xhci_do_dbc_stop(xhci);
+	ret = xhci_do_dbc_stop(dbc);
 	spin_unlock_irqrestore(&dbc->lock, flags);
 
 	if (!ret) {
