9424a9a7171b498ae14d8f689176592c5d08701c
Krzysztof Kozlowski <krzk@kernel.org>
2020-07-24 09:40:12 +0200

    memory: omap-gpmc: Include <linux/sizes.h> for SZ_16M
    The driver uses SZ_16M which is defined in include/linux/sizes.h.  On
    ARM it was pulled by other headers but its inclusion is necessary for
    compile testing on other architectures.
    
    This fixes build error when compile testing on i386 architecture:
    
        drivers/memory/omap-gpmc.c: In function ‘gpmc_cs_remap’:
        drivers/memory/omap-gpmc.c:961:12: error: ‘SZ_16M’ undeclared (first use in this function)
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- drivers/memory/omap-gpmc.c
+++ drivers/memory/omap-gpmc.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_platform.h>
 #include <linux/omap-gpmc.h>
 #include <linux/pm_runtime.h>
+#include <linux/sizes.h>
 
 #include <linux/platform_data/mtd-nand-omap2.h>
 
