6c123f86cd596ebf9f32489825ab9cbcd8f45ad2
YiFei Zhu <zhuyifei@google.com>
2020-07-23 23:47:44 -0500

    selftests/bpf: Test CGROUP_STORAGE behavior on shared egress + ingress
    This mirrors the original egress-only test. The cgroup_storage is
    now extended to have two packet counters, one for egress and one
    for ingress. We also extend to have two egress programs to test
    that egress will always share with other egress origrams in the
    same cgroup. The behavior of the counters are exactly the same as
    the original egress-only test.
    
    The test is split into two, one "isolated" test that when the key
    type is struct bpf_cgroup_storage_key, which contains the attach
    type, programs of different attach types will see different
    storages. The other, "shared" test that when the key type is u64,
    programs of different attach types will see the same storage if
    they are attached to the same cgroup.
    
    Signed-off-by: YiFei Zhu <zhuyifei@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/c756f5f1521227b8e6e90a453299dda722d7324d.1595565795.git.zhuyifei@google.com

--- tools/testing/selftests/bpf/progs/cg_storage_multi_isolated.c
+++ tools/testing/selftests/bpf/progs/cg_storage_multi_isolated.c
@@ -21,8 +21,20 @@ struct {
 
 __u32 invocations = 0;
 
-SEC("cgroup_skb/egress")
-int egress(struct __sk_buff *skb)
+SEC("cgroup_skb/egress/1")
+int egress1(struct __sk_buff *skb)
+{
+	struct cgroup_value *ptr_cg_storage =
+		bpf_get_local_storage(&cgroup_storage, 0);
+
+	__sync_fetch_and_add(&ptr_cg_storage->egress_pkts, 1);
+	__sync_fetch_and_add(&invocations, 1);
+
+	return 1;
+}
+
+SEC("cgroup_skb/egress/2")
+int egress2(struct __sk_buff *skb)
 {
 	struct cgroup_value *ptr_cg_storage =
 		bpf_get_local_storage(&cgroup_storage, 0);
