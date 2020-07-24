16c7816a71068bca8b5fe0b57216e0c53791d3a1
Cudzilo, Szymon T <szymon.t.cudzilo@intel.com>
2020-07-23 17:22:01 -0700

    ice: Add AdminQ commands for FW update
    Add structures, identifiers, and helper functions for several AdminQ
    commands related to performing a firmware update for the ice hardware.
    These will be used in future code for implementing the devlink
    .flash_update handler.
    
    Signed-off-by: Cudzilo, Szymon T <szymon.t.cudzilo@intel.com>
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/intel/ice/ice_common.h
+++ drivers/net/ethernet/intel/ice/ice_common.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ice_switch.h"
 #include <linux/avf/virtchnl.h>
 
-enum ice_status ice_nvm_validate_checksum(struct ice_hw *hw);
-
 enum ice_status ice_init_hw(struct ice_hw *hw);
 void ice_deinit_hw(struct ice_hw *hw);
 enum ice_status ice_check_reset(struct ice_hw *hw);
