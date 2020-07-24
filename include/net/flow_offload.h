00cb8d6075d4a7e51494d0bc57e95f59e5a05a9f
Herbert Xu <herbert@gondor.apana.org.au>
2020-07-24 10:50:22 +1000

    flow_offload: Move rhashtable inclusion to the source file
    I noticed that touching linux/rhashtable.h causes lib/vsprintf.c to
    be rebuilt.  This dependency came through a bogus inclusion in the
    file net/flow_offload.h.  This patch moves it to the right place.
    
    This patch also removes a lingering rhashtable inclusion in cls_api
    created by the same commit.
    
    Fixes: 4e481908c51b ("flow_offload: move tc indirect block to...")
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/flow_offload.h
+++ include/net/flow_offload.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/list.h>
 #include <linux/netlink.h>
 #include <net/flow_dissector.h>
-#include <linux/rhashtable.h>
 
 struct flow_match {
 	struct flow_dissector	*dissector;
