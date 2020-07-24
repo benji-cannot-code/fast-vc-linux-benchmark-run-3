e4da5c7c9e3d6cf8e794c1609f7e9a77214a2e1b
Nicholas Piggin <npiggin@gmail.com>
2020-07-24 23:14:21 +1000

    powerpc/pseries: Implement paravirt qspinlocks for SPLPAR
    This implements the generic paravirt qspinlocks using H_PROD and
    H_CONFER to kick and wait.
    
    This uses an un-directed yield to any CPU rather than the directed
    yield to a pre-empted lock holder that paravirtualised simple
    spinlocks use, that requires no kick hcall. This is something that
    could be investigated and improved in future.
    
    Performance results can be found in the commit which added queued
    spinlocks.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200724131423.1362108-5-npiggin@gmail.com

--- arch/powerpc/include/asm/spinlock.h
+++ arch/powerpc/include/asm/spinlock.h
@@ -11,5 +11,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/simple_spinlock.h>
 #endif
 
+#ifndef CONFIG_PARAVIRT_SPINLOCKS
+static inline void pv_spinlocks_init(void) { }
+#endif
+
 #endif /* __KERNEL__ */
 #endif /* __ASM_SPINLOCK_H */
