15164075af947a7d68a8c0be6f49121790118868
Coly Li <colyli@suse.de>
2020-07-25 20:00:35 +0800

    bcache: add bucket_size_hi into struct cache_sb_disk for large bucket
    The large bucket feature is to extend bucket_size from 16bit to 32bit.
    
    When create cache device on zoned device (e.g. zoned NVMe SSD), making
    a single bucket cover one or more zones of the zoned device is the
    simplest way to support zoned device as cache by bcache.
    
    But current maximum bucket size is 16MB and a typical zone size of zoned
    device is 256MB, this is the major motiviation to extend bucket size to
    a larger bit width.
    
    This patch is the basic and first change to support large bucket size,
    the major changes it makes are,
    - Add BCH_FEATURE_INCOMPAT_LARGE_BUCKET for the large bucket feature,
      INCOMPAT means it introduces incompatible on-disk format change.
    - Add BCH_FEATURE_INCOMPAT_FUNCS(large_bucket, LARGE_BUCKET) routines.
    - Adds __le16 bucket_size_hi into struct cache_sb_disk at offset 0x8d0
      for the on-disk super block format.
    - For the in-memory super block struct cache_sb, member bucket_size is
      extended from __u16 to __32.
    - Add get_bucket_size() to combine the bucket_size and bucket_size_hi
      from struct cache_sb_disk into an unsigned int value.
    
    Since we already have large bucket size helpers meta_bucket_pages(),
    meta_bucket_bytes() and alloc_meta_bucket_pages(), they make sure when
    bucket size > 8MB, the memory allocation for bcache meta data bucket
    won't fail no matter how large the bucket size extended. So these meta
    data buckets are handled properly when the bucket size width increase
    from 16bit to 32bit, we don't need to worry about them.
    
    Signed-off-by: Coly Li <colyli@suse.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/md/bcache/features.h
+++ drivers/md/bcache/features.h
@@ -12,9 +12,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BCH_FEATURE_INCOMPAT		2
 #define BCH_FEATURE_TYPE_MASK		0x03
 
+/* Feature set definition */
+/* Incompat feature set */
+#define BCH_FEATURE_INCOMPAT_LARGE_BUCKET	0x0001 /* 32bit bucket size */
+
 #define BCH_FEATURE_COMPAT_SUUP		0
 #define BCH_FEATURE_RO_COMPAT_SUUP	0
-#define BCH_FEATURE_INCOMPAT_SUUP	0
+#define BCH_FEATURE_INCOMPAT_SUUP	BCH_FEATURE_INCOMPAT_LARGE_BUCKET
 
 #define BCH_HAS_COMPAT_FEATURE(sb, mask) \
 		((sb)->feature_compat & (mask))
@@ -22,8 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		((sb)->feature_ro_compat & (mask))
 #define BCH_HAS_INCOMPAT_FEATURE(sb, mask) \
 		((sb)->feature_incompat & (mask))
-
-/* Feature set definition */
 
 #define BCH_FEATURE_COMPAT_FUNCS(name, flagname) \
 static inline int bch_has_feature_##name(struct cache_sb *sb) \
@@ -76,4 +78,5 @@ static inline void bch_clear_feature_##name(struct cache_sb *sb) \
 		~BCH##_FEATURE_INCOMPAT_##flagname; \
 }
 
+BCH_FEATURE_INCOMPAT_FUNCS(large_bucket, LARGE_BUCKET);
 #endif
