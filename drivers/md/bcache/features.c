7aefddd8ea01d238786d57e10d8f9651664134ca
Coly Li <colyli@suse.de>
2020-07-25 20:00:36 +0800

    bcache: add sysfs file to display feature sets information of cache set
    The following three sysfs files are created to display according feature
    set information of bcache:
    	/sys/fs/bcache/<cache set UUID>/internal/feature_compat
    	/sys/fs/bcache/<cache set UUID>/internal/feature_ro_compat
    	/sys/fs/bcache/<cache set UUID>/internal/feature_incompat
    is added by this patch, to display feature sets information of the cache
    set.
    
    Now only an incompat feature 'large_bucket' added in bcache, the sysfs
    file content is:
            [large_bucket]
    string large_bucket means the running bcache drive supports incompat
    feature 'large_bucket', the wrapping [] means the 'large_bucket' feature
    is currently enabled on this cache set.
    
    This patch is ready to display compat and ro_compat features, in future
    once bcache code implements such feature sets, the according feature
    strings will be displayed in their sysfs files too.
    
    Signed-off-by: Coly Li <colyli@suse.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/md/bcache/features.c
+++ drivers/md/bcache/features.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <linux/bcache.h>
 #include "bcache.h"
+#include "features.h"
 
 struct feature {
 	int		compat;
@@ -21,3 +22,55 @@ static struct feature feature_list[] = {
 		"large_bucket"},
 	{0, 0, 0 },
 };
+
+#define compose_feature_string(type)				\
+({									\
+	struct feature *f;						\
+	bool first = true;						\
+									\
+	for (f = &feature_list[0]; f->compat != 0; f++) {		\
+		if (f->compat != BCH_FEATURE_ ## type)			\
+			continue;					\
+		if (BCH_HAS_ ## type ## _FEATURE(&c->sb, f->mask)) {	\
+			if (first) {					\
+				out += snprintf(out, buf + size - out,	\
+						"[");	\
+			} else {					\
+				out += snprintf(out, buf + size - out,	\
+						" [");			\
+			}						\
+		} else if (!first) {					\
+			out += snprintf(out, buf + size - out, " ");	\
+		}							\
+									\
+		out += snprintf(out, buf + size - out, "%s", f->string);\
+									\
+		if (BCH_HAS_ ## type ## _FEATURE(&c->sb, f->mask))	\
+			out += snprintf(out, buf + size - out, "]");	\
+									\
+		first = false;						\
+	}								\
+	if (!first)							\
+		out += snprintf(out, buf + size - out, "\n");		\
+})
+
+int bch_print_cache_set_feature_compat(struct cache_set *c, char *buf, int size)
+{
+	char *out = buf;
+	compose_feature_string(COMPAT);
+	return out - buf;
+}
+
+int bch_print_cache_set_feature_ro_compat(struct cache_set *c, char *buf, int size)
+{
+	char *out = buf;
+	compose_feature_string(RO_COMPAT);
+	return out - buf;
+}
+
+int bch_print_cache_set_feature_incompat(struct cache_set *c, char *buf, int size)
+{
+	char *out = buf;
+	compose_feature_string(INCOMPAT);
+	return out - buf;
+}
