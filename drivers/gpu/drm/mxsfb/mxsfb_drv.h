32730bd965e76852d67df01f9730edd73156999a
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2020-07-27 05:06:53 +0300

    drm: mxsfb: Remove unnecessary spaces after tab
    This is a cosmetic change only, no code change is included.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>
    Reviewed-by: Stefan Agner <stefan@agner.ch>
    Signed-off-by: Stefan Agner <stefan@agner.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200727020654.8231-22-laurent.pinchart@ideasonboard.com

--- drivers/gpu/drm/mxsfb/mxsfb_drv.h
+++ drivers/gpu/drm/mxsfb/mxsfb_drv.h
@@ -17,11 +17,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct clk;
 
 struct mxsfb_devdata {
-	unsigned int	 transfer_count;
-	unsigned int	 cur_buf;
-	unsigned int	 next_buf;
-	unsigned int	 hs_wdth_mask;
-	unsigned int	 hs_wdth_shift;
+	unsigned int	transfer_count;
+	unsigned int	cur_buf;
+	unsigned int	next_buf;
+	unsigned int	hs_wdth_mask;
+	unsigned int	hs_wdth_shift;
 };
 
 struct mxsfb_drm_private {
