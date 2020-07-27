bb2450be5bc4ea1e7cc8089aef7e3eb6585e7249
Peter Chen <peter.chen@nxp.com>
2020-07-27 17:53:37 +0800

    usb: cdns3: host: disable BEI support
    The Cadence xHCI doesn't support BEI well, it causes the disconnection
    of ISOC devices can't be detected, so we disable it.
    
    Reviewed-by: Jun Li <jun.li@nxp.com>
    Signed-off-by: Peter Chen <peter.chen@nxp.com>

--- drivers/usb/cdns3/host.c
+++ drivers/usb/cdns3/host.c
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define LPM_2_STB_SWITCH_EN	BIT(25)
 
 static const struct xhci_plat_priv xhci_plat_cdns3_xhci = {
-	.quirks = XHCI_SKIP_PHY_INIT,
+	.quirks = XHCI_SKIP_PHY_INIT | XHCI_AVOID_BEI,
 	.suspend_quirk = xhci_cdns3_suspend_quirk,
 };
 
