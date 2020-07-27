70d22f367dc464d9e514ca432ca87ee197246609
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2020-07-27 05:06:33 +0300

    drm: mxsfb: Remove fbdev leftovers
    Commit 8e93f1028d74 ("drm/mxsfb: Use drm_fbdev_generic_setup()")
    replaced fbdev handling with drm_fbdev_generic_setup() but left
    inclusion of the drm/drm_fb_cma_helper.h header. Remove it.
    
    Fixes: 8e93f1028d74 ("drm/mxsfb: Use drm_fbdev_generic_setup()")
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Stefan Agner <stefan@agner.ch>
    Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>
    Signed-off-by: Stefan Agner <stefan@agner.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200727020654.8231-2-laurent.pinchart@ideasonboard.com

--- drivers/gpu/drm/mxsfb/mxsfb_drv.c
+++ drivers/gpu/drm/mxsfb/mxsfb_drv.c
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_crtc.h>
 #include <drm/drm_drv.h>
-#include <drm/drm_fb_cma_helper.h>
 #include <drm/drm_fb_helper.h>
 #include <drm/drm_gem_cma_helper.h>
 #include <drm/drm_gem_framebuffer_helper.h>
