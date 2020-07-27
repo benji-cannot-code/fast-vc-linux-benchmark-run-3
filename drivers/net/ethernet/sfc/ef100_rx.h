11c5bb9a0bdacc70d7b719a40d0964554c423d81
Edward Cree <ecree@solarflare.com>
2020-07-27 12:55:55 +0100

    sfc: skeleton EF100 PF driver
    No TX or RX path, no MCDI, not even an ifup/down handler.
    Besides stubs, the bulk of the patch deals with reading the Xilinx
     extended PCIe capability, which tells us where to find our BAR.
    
    Though in the same module, EF100 has its own struct pci_driver,
     which is named sfc_ef100.
    
    A small number of additional nic_type methods are added; those in the
     TX (tx_enqueue) and RX (rx_packet) paths are called through indirect
     call wrappers to minimise the performance impact.
    
    Signed-off-by: Edward Cree <ecree@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ drivers/net/ethernet/sfc/ef100_rx.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0-only */
+/****************************************************************************
+ * Driver for Solarflare network controllers and boards
+ * Copyright 2019 Solarflare Communications Inc.
+ * Copyright 2019-2020 Xilinx Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, incorporated herein by reference.
+ */
+
+#ifndef EFX_EF100_RX_H
+#define EFX_EF100_RX_H
+
+#include "net_driver.h"
+
+void __ef100_rx_packet(struct efx_channel *channel);
+
+#endif
