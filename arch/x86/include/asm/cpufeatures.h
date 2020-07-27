56386e575df9cb6c344e46d874e42f75468ef9c3
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2020-07-26 21:31:29 -0700

    x86/cpufeatures: Add enumeration for SERIALIZE instruction
    The Intel architecture defines a set of Serializing Instructions (a
    detailed definition can be found in Vol.3 Section 8.3 of the Intel "main"
    manual, SDM). However, these instructions do more than what is required,
    have side effects and/or may be rather invasive. Furthermore, some of
    these instructions are only available in kernel mode or may cause VMExits.
    Thus, software using these instructions only to serialize execution (as
    defined in the manual) must handle the undesired side effects.
    
    As indicated in the name, SERIALIZE is a new Intel architecture
    Serializing Instruction. Crucially, it does not have any of the mentioned
    side effects. Also, it does not cause VMExit and can be used in user mode.
    
    This new instruction is currently documented in the latest "extensions"
    manual (ISE). It will appear in the "main" manual in the future.
    
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Acked-by: Dave Hansen <dave.hansen@linux.intel.com>
    Link: https://lore.kernel.org/r/20200727043132.15082-2-ricardo.neri-calderon@linux.intel.com

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -366,6 +366,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define X86_FEATURE_SRBDS_CTRL		(18*32+ 9) /* "" SRBDS mitigation MSR available */
 #define X86_FEATURE_MD_CLEAR		(18*32+10) /* VERW clears CPU buffers */
 #define X86_FEATURE_TSX_FORCE_ABORT	(18*32+13) /* "" TSX_FORCE_ABORT */
+#define X86_FEATURE_SERIALIZE		(18*32+14) /* SERIALIZE instruction */
 #define X86_FEATURE_PCONFIG		(18*32+18) /* Intel PCONFIG */
 #define X86_FEATURE_SPEC_CTRL		(18*32+26) /* "" Speculation Control (IBRS + IBPB) */
 #define X86_FEATURE_INTEL_STIBP		(18*32+27) /* "" Single Thread Indirect Branch Predictors */
