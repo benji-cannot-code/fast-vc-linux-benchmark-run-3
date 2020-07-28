5011dd65afdab954cb89c023f91770ba06404724
Yonghong Song <yhs@fb.com>
2020-07-28 15:18:01 -0700

    selftests/bpf: Test bpf_iter buffer access with negative offset
    Commit afbf21dce668 ("bpf: Support readonly/readwrite buffers
    in verifier") added readonly/readwrite buffer support which
    is currently used by bpf_iter tracing programs. It has
    a bug with incorrect parameter ordering which later fixed
    by Commit f6dfbe31e8fa ("bpf: Fix swapped arguments in calls
    to check_buffer_access").
    
    This patch added a test case with a negative offset access
    which will trigger the error path.
    
    Without Commit f6dfbe31e8fa, running the test case in the patch,
    the error message looks like:
       R1_w=rdwr_buf(id=0,off=0,imm=0) R10=fp0
      ; value_sum += *(__u32 *)(value - 4);
      2: (61) r1 = *(u32 *)(r1 -4)
      R1 invalid (null) buffer access: off=-4, size=4
    
    With the above commit, the error message looks like:
       R1_w=rdwr_buf(id=0,off=0,imm=0) R10=fp0
      ; value_sum += *(__u32 *)(value - 4);
      2: (61) r1 = *(u32 *)(r1 -4)
      R1 invalid rdwr buffer access: off=-4, size=4
    
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Song Liu <songliubraving@fb.com>
    Link: https://lore.kernel.org/bpf/20200728221801.1090406-1-yhs@fb.com

--- tools/testing/selftests/bpf/prog_tests/bpf_iter.c
+++ tools/testing/selftests/bpf/prog_tests/bpf_iter.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "bpf_iter_bpf_percpu_array_map.skel.h"
 #include "bpf_iter_bpf_sk_storage_map.skel.h"
 #include "bpf_iter_test_kern5.skel.h"
+#include "bpf_iter_test_kern6.skel.h"
 
 static int duration;
 
@@ -886,6 +887,16 @@ static void test_rdonly_buf_out_of_bound(void)
 	bpf_iter_test_kern5__destroy(skel);
 }
 
+static void test_buf_neg_offset(void)
+{
+	struct bpf_iter_test_kern6 *skel;
+
+	skel = bpf_iter_test_kern6__open_and_load();
+	if (CHECK(skel, "bpf_iter_test_kern6__open_and_load",
+		  "skeleton open_and_load unexpected success\n"))
+		bpf_iter_test_kern6__destroy(skel);
+}
+
 void test_bpf_iter(void)
 {
 	if (test__start_subtest("btf_id_or_null"))
@@ -934,4 +945,6 @@ void test_bpf_iter(void)
 		test_bpf_sk_storage_map();
 	if (test__start_subtest("rdonly-buf-out-of-bound"))
 		test_rdonly_buf_out_of_bound();
+	if (test__start_subtest("buf-neg-offset"))
+		test_buf_neg_offset();
 }
