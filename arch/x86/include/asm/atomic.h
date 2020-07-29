14ceccc1c29ef6b3c4e182abaea3544660932928
Herbert Xu <herbert@gondor.apana.org.au>
2020-07-29 22:31:05 +1000

    locking/atomic: Move ATOMIC_INIT into linux/types.h
    This patch moves ATOMIC_INIT from asm/atomic.h into linux/types.h.
    This allows users of atomic_t to use ATOMIC_INIT without having to
    include atomic.h as that way may lead to header loops.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Waiman Long <longman@redhat.com>
    Link: https://lkml.kernel.org/r/20200729123105.GB7047@gondor.apana.org.au

--- arch/x86/include/asm/atomic.h
+++ arch/x86/include/asm/atomic.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * resource counting etc..
  */
 
-#define ATOMIC_INIT(i)	{ (i) }
-
 /**
  * arch_atomic_read - read atomic variable
  * @v: pointer of type atomic_t
