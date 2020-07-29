11e39751086b0569ce58dc919e5e97d9fa960eb9
Chris Wilson <chris@chris-wilson.co.uk>
2020-07-29 17:42:19 +0100

    drm/i915/gt: Shrink i915_page_directory's slab bucket
    kmalloc uses power-of-two slab buckets for small allocations (up to a
    few pages). Since i915_page_directory is a page of pointers, plus a
    couple more, this is rounded up to 8K, and we waste nearly 50% of that
    allocation. Long terms this leads to poor memory utilisation, bloating
    the kernel footprint, but the problem is exacerbated by our conservative
    preallocation scheme for binding VMA. As we are required to allocate all
    levels for each vma just in case we need to insert them upon binding,
    this leads to a large multiplication factor for a single page vma. By
    halving the allocation we need for the page directory structure, we
    halve the impact of that factor, bringing workloads that once fitted into
    memory, hopefully back to fitting into memory.
    
    We maintain the split between i915_page_directory and i915_page_table as
    we only need half the allocation for the lowest, most populous, level.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Cc: Matthew Auld <matthew.auld@intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200729164219.5737-3-chris@chris-wilson.co.uk
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>

--- drivers/gpu/drm/i915/gt/gen6_ppgtt.c
+++ drivers/gpu/drm/i915/gt/gen6_ppgtt.c
@@ -262,7 +262,7 @@ static void gen6_ppgtt_free_pd(struct gen6_ppgtt *ppgtt)
 
 	gen6_for_all_pdes(pt, pd, pde)
 		if (pt)
-			free_px(&ppgtt->base.vm, pt);
+			free_pt(&ppgtt->base.vm, pt);
 }
 
 static void gen6_ppgtt_cleanup(struct i915_address_space *vm)
@@ -276,7 +276,8 @@ static void gen6_ppgtt_cleanup(struct i915_address_space *vm)
 
 	mutex_destroy(&ppgtt->flush);
 	mutex_destroy(&ppgtt->pin_mutex);
-	kfree(ppgtt->base.pd);
+
+	free_pd(&ppgtt->base.vm, ppgtt->base.pd);
 }
 
 static int pd_vma_set_pages(struct i915_vma *vma)
@@ -323,7 +324,7 @@ static void pd_vma_unbind(struct i915_address_space *vm, struct i915_vma *vma)
 		if (!pt || atomic_read(&pt->used))
 			continue;
 
-		free_px(&ppgtt->base.vm, pt);
+		free_pt(&ppgtt->base.vm, pt);
 		pd->entry[pde] = NULL;
 	}
 
@@ -448,7 +449,7 @@ struct i915_ppgtt *gen6_ppgtt_create(struct intel_gt *gt)
 	ppgtt->base.vm.alloc_pt_dma = alloc_pt_dma;
 	ppgtt->base.vm.pte_encode = ggtt->vm.pte_encode;
 
-	ppgtt->base.pd = __alloc_pd(sizeof(*ppgtt->base.pd));
+	ppgtt->base.pd = __alloc_pd(I915_PDES);
 	if (!ppgtt->base.pd) {
 		err = -ENOMEM;
 		goto err_free;
@@ -469,7 +470,7 @@ struct i915_ppgtt *gen6_ppgtt_create(struct intel_gt *gt)
 err_scratch:
 	free_scratch(&ppgtt->base.vm);
 err_pd:
-	kfree(ppgtt->base.pd);
+	free_pd(&ppgtt->base.vm, ppgtt->base.pd);
 err_free:
 	mutex_destroy(&ppgtt->pin_mutex);
 	kfree(ppgtt);
