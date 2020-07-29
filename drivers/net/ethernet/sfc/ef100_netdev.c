869fee1e0e2deccce9a633550e4ddd06aae7de54
YueHaibing <yuehaibing@huawei.com>
2020-07-29 02:19:50 +0000

    sfc_ef100: remove duplicated include from ef100_netdev.c
    Remove duplicated include.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Acked-by: Edward Cree <ecree@solarflare.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/sfc/ef100_netdev.c
+++ drivers/net/ethernet/sfc/ef100_netdev.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "tx_common.h"
 #include "ef100_netdev.h"
 #include "ef100_ethtool.h"
-#include "efx_common.h"
 #include "nic_common.h"
 #include "ef100_nic.h"
 #include "ef100_tx.h"
