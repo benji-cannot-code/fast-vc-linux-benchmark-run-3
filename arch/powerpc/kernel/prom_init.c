89c085871f32dff4ee37d7eca7a09170451c55f6
Vladis Dronov <vdronov@redhat.com>
2020-07-29 15:37:41 +0200

    powerpc: fix function annotations to avoid section mismatch warnings with gcc-10
    Certain warnings are emitted for powerpc code when building with a gcc-10
    toolset:
    
        WARNING: modpost: vmlinux.o(.text.unlikely+0x377c): Section mismatch in
        reference from the function remove_pmd_table() to the function
        .meminit.text:split_kernel_mapping()
        The function remove_pmd_table() references
        the function __meminit split_kernel_mapping().
        This is often because remove_pmd_table lacks a __meminit
        annotation or the annotation of split_kernel_mapping is wrong.
    
    Add the appropriate __init and __meminit annotations to make modpost not
    complain. In all the cases there are just a single callsite from another
    __init or __meminit function:
    
    __meminit remove_pagetable() -> remove_pud_table() -> remove_pmd_table()
    __init prom_init() -> setup_secure_guest()
    __init xive_spapr_init() -> xive_spapr_disabled()
    
    Signed-off-by: Vladis Dronov <vdronov@redhat.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200729133741.62789-1-vdronov@redhat.com

--- arch/powerpc/kernel/prom_init.c
+++ arch/powerpc/kernel/prom_init.c
@@ -3271,7 +3271,7 @@ static int enter_secure_mode(unsigned long kbase, unsigned long fdt)
 /*
  * Call the Ultravisor to transfer us to secure memory if we have an ESM blob.
  */
-static void setup_secure_guest(unsigned long kbase, unsigned long fdt)
+static void __init setup_secure_guest(unsigned long kbase, unsigned long fdt)
 {
 	int ret;
 
@@ -3301,7 +3301,7 @@ static void setup_secure_guest(unsigned long kbase, unsigned long fdt)
 	}
 }
 #else
-static void setup_secure_guest(unsigned long kbase, unsigned long fdt)
+static void __init setup_secure_guest(unsigned long kbase, unsigned long fdt)
 {
 }
 #endif /* CONFIG_PPC_SVM */
