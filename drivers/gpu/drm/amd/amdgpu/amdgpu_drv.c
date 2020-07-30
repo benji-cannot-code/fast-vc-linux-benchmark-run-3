a6c7f7dc451248a3793203a064189a36f53b4f2a
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2020-07-30 15:54:59 +0200

    drm/amdgpu: new ids flag for tmz (v2)
    Allows UMD to know if TMZ is supported and enabled.
    
    This commit also bumps KMS_DRIVER_MINOR because if we don't
    UMD can't tell if "ids_flags & AMDGPU_IDS_FLAGS_TMZ == 0" means
    "tmz is not enabled" or "tmz may be enabled but the kernel doesn't
    report it".
    
    v2: use amdgpu_is_tmz() and reworded commit message.
    
    Signed-off-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -89,9 +89,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * - 3.37.0 - L2 is invalidated before SDMA IBs, needed for correctness
  * - 3.38.0 - Add AMDGPU_IB_FLAG_EMIT_MEM_SYNC
  * - 3.39.0 - DMABUF implicit sync does a full pipeline sync
+ * - 3.40.0 - Add AMDGPU_IDS_FLAGS_TMZ
  */
 #define KMS_DRIVER_MAJOR	3
-#define KMS_DRIVER_MINOR	39
+#define KMS_DRIVER_MINOR	40
 #define KMS_DRIVER_PATCHLEVEL	0
 
 int amdgpu_vram_limit = 0;
