5059a5a886c9ede8b32d698cf7b187bea7d1afb2
Ard Biesheuvel <ardb@kernel.org>
2020-07-31 16:07:46 -0700

    x86/boot/compressed: Force hidden visibility for all symbol references
    Eliminate all GOT entries in the decompressor binary, by forcing hidden
    visibility for all symbol references, which informs the compiler that
    such references will be resolved at link time without the need for
    allocating GOT entries.
    
    To ensure that no GOT entries will creep back in, add an assertion to
    the decompressor linker script that will fire if the .got section has
    a non-zero size.
    
    [Arvind: move hidden.h to include/linux instead of making a copy]
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Arvind Sankar <nivedita@alum.mit.edu>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Tested-by: Nick Desaulniers <ndesaulniers@google.com>
    Tested-by: Sedat Dilek <sedat.dilek@gmail.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Acked-by: Arvind Sankar <nivedita@alum.mit.edu>
    Link: https://lore.kernel.org/r/20200731230820.1742553-3-keescook@chromium.org

---
+++ include/linux/hidden.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * When building position independent code with GCC using the -fPIC option,
+ * (or even the -fPIE one on older versions), it will assume that we are
+ * building a dynamic object (either a shared library or an executable) that
+ * may have symbol references that can only be resolved at load time. For a
+ * variety of reasons (ELF symbol preemption, the CoW footprint of the section
+ * that is modified by the loader), this results in all references to symbols
+ * with external linkage to go via entries in the Global Offset Table (GOT),
+ * which carries absolute addresses which need to be fixed up when the
+ * executable image is loaded at an offset which is different from its link
+ * time offset.
+ *
+ * Fortunately, there is a way to inform the compiler that such symbol
+ * references will be satisfied at link time rather than at load time, by
+ * giving them 'hidden' visibility.
+ */
+
+#pragma GCC visibility push(hidden)
