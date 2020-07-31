6b384f17f933ed81b14cde75e6382f1c05a059c5
Randy Dunlap <rdunlap@infradead.org>
2020-07-30 18:42:30 -0700

    block: genhd: delete duplicated words
    Drop the repeated word "to" in multiple places.
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: linux-block@vger.kernel.org
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/genhd.c
+++ block/genhd.c
@@ -1774,7 +1774,7 @@ EXPORT_SYMBOL(__alloc_disk_node);
 
 /**
  * get_disk_and_module - increments the gendisk and gendisk fops module refcount
- * @disk: the struct gendisk to to increment the refcount for
+ * @disk: the struct gendisk to increment the refcount for
  *
  * This increments the refcount for the struct gendisk, and the gendisk's
  * fops module owner.
@@ -1803,7 +1803,7 @@ EXPORT_SYMBOL(get_disk_and_module);
 
 /**
  * put_disk - decrements the gendisk refcount
- * @disk: the struct gendisk to to decrement the refcount for
+ * @disk: the struct gendisk to decrement the refcount for
  *
  * This decrements the refcount for the struct gendisk. When this reaches 0
  * we'll have disk_release() called.
@@ -1820,7 +1820,7 @@ EXPORT_SYMBOL(put_disk);
 
 /**
  * put_disk_and_module - decrements the module and gendisk refcount
- * @disk: the struct gendisk to to decrement the refcount for
+ * @disk: the struct gendisk to decrement the refcount for
  *
  * This is a counterpart of get_disk_and_module() and thus also of
  * get_gendisk().
