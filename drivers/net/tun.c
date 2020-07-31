07a0b0ce7f576d80aebf08497d6d0b5318b3a64d
Jason Wang <jasowang@redhat.com>
2020-07-31 00:17:20 -0400

    tun: add missing rcu annotation in tun_set_ebpf()
    We expecte prog_p to be protected by rcu, so adding the rcu annotation
    to fix the following sparse warning:
    
    drivers/net/tun.c:3003:36: warning: incorrect type in argument 2 (different address spaces)
    drivers/net/tun.c:3003:36:    expected struct tun_prog [noderef] __rcu **prog_p
    drivers/net/tun.c:3003:36:    got struct tun_prog **prog_p
    drivers/net/tun.c:3292:42: warning: incorrect type in argument 2 (different address spaces)
    drivers/net/tun.c:3292:42:    expected struct tun_prog **prog_p
    drivers/net/tun.c:3292:42:    got struct tun_prog [noderef] __rcu **
    drivers/net/tun.c:3296:42: warning: incorrect type in argument 2 (different address spaces)
    drivers/net/tun.c:3296:42:    expected struct tun_prog **prog_p
    drivers/net/tun.c:3296:42:    got struct tun_prog [noderef] __rcu **
    
    Reported-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -2984,7 +2984,7 @@ unlock:
 	return ret;
 }
 
-static int tun_set_ebpf(struct tun_struct *tun, struct tun_prog **prog_p,
+static int tun_set_ebpf(struct tun_struct *tun, struct tun_prog __rcu **prog_p,
 			void __user *data)
 {
 	struct bpf_prog *prog;
