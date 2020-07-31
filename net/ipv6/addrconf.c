e6f0f1575d95e1c9617d2bb400e9637bd0e377e7
Florent Fourcot <florent.fourcot@wifirst.fr>
2020-07-31 15:32:06 +0200

    ipv6/addrconf: call addrconf_ifdown with consistent values
    Second parameter of addrconf_ifdown "how" is used as a boolean
    internally. It does not make sense to call it with something different
    of 0 or 1.
    
    This value is set to 2 in all git history.
    
    Signed-off-by: Florent Fourcot <florent.fourcot@wifirst.fr>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -7190,7 +7190,7 @@ void addrconf_cleanup(void)
 			continue;
 		addrconf_ifdown(dev, 1);
 	}
-	addrconf_ifdown(init_net.loopback_dev, 2);
+	addrconf_ifdown(init_net.loopback_dev, 1);
 
 	/*
 	 *	Check hash table.
