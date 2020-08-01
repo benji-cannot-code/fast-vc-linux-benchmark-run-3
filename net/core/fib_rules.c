5b8fc3c21a6f740ebde78b7d7a5560927be72943
Brian Vazquez <brianvv@google.com>
2020-07-31 20:01:10 -0700

    fib: fix another fib_rules_ops indirect call wrapper problem
    It turns out that on commit 41d707b7332f ("fib: fix fib_rules_ops
    indirect calls wrappers") I forgot to include the case when
    CONFIG_IP_MULTIPLE_TABLES is not set.
    
    Fixes: 41d707b7332f ("fib: fix fib_rules_ops indirect calls wrappers")
    Reported-by: Randy Dunlap <rdunlap@infradead.org>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Brian Vazquez <brianvv@google.com>
    Acked-by: Randy Dunlap <rdunlap@infradead.org> # build-tested
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/fib_rules.c
+++ net/core/fib_rules.c
@@ -18,10 +18,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/indirect_call_wrapper.h>
 
 #ifdef CONFIG_IPV6_MULTIPLE_TABLES
+#ifdef CONFIG_IP_MULTIPLE_TABLES
 #define INDIRECT_CALL_MT(f, f2, f1, ...) \
 	INDIRECT_CALL_INET(f, f2, f1, __VA_ARGS__)
 #else
+#define INDIRECT_CALL_MT(f, f2, f1, ...) INDIRECT_CALL_1(f, f2, __VA_ARGS__)
+#endif
+#elif CONFIG_IP_MULTIPLE_TABLES
 #define INDIRECT_CALL_MT(f, f2, f1, ...) INDIRECT_CALL_1(f, f1, __VA_ARGS__)
+#else
+#define INDIRECT_CALL_MT(f, f2, f1, ...) f(__VA_ARGS__)
 #endif
 
 static const struct fib_kuid_range fib_kuid_range_unset = {
