936801ba4d5ae6d87b3a1fc04adda5c0e81646ba
Mohammed Rushad <mohammedrushad@gmail.com>
2020-08-03 00:39:24 +0530

    staging: rtl8723bs: os_dep: fix brace coding style issue in sdio_intf.c
    This is a patch to the sdio_intf.c file that fixes up an incorrectly
    placed brace found by the checkpatch.pl tool
    
    Signed-off-by: Mohammed Rushad <mohammedrushad@gmail.com>
    Link: https://lore.kernel.org/r/20200802190924.19964-1-mohammedrushad@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/os_dep/sdio_intf.c
+++ drivers/staging/rtl8723bs/os_dep/sdio_intf.c
@@ -16,8 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define dev_to_sdio_func(d)     container_of(d, struct sdio_func, dev)
 #endif
 
-static const struct sdio_device_id sdio_ids[] =
-{
+static const struct sdio_device_id sdio_ids[] = {
 	{ SDIO_DEVICE(0x024c, 0x0523), },
 	{ SDIO_DEVICE(0x024c, 0x0525), },
 	{ SDIO_DEVICE(0x024c, 0x0623), },
