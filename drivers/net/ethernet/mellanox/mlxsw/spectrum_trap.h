ca710304c8315eb53f5e3a31ecf69d143173c5bf
Ido Schimmel <idosch@mellanox.com>
2020-08-03 19:11:35 +0300

    mlxsw: spectrum_trap: Use 'size_t' for array sizes
    Use 'size_t' instead of 'u64' for array sizes, as this this is correct
    type to use for expressions involving sizeof().
    
    Suggested-by: Petr Machata <petrm@mellanox.com>
    Reviewed-by: Petr Machata <petrm@mellanox.com>
    Reviewed-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: Petr Machata <petrm@mellanox.com>
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_trap.h
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_trap.h
@@ -10,13 +10,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct mlxsw_sp_trap {
 	struct mlxsw_sp_trap_policer_item *policer_items_arr;
-	u64 policers_count; /* Number of registered policers */
+	size_t policers_count; /* Number of registered policers */
 
 	struct mlxsw_sp_trap_group_item *group_items_arr;
-	u64 groups_count; /* Number of registered groups */
+	size_t groups_count; /* Number of registered groups */
 
 	struct mlxsw_sp_trap_item *trap_items_arr;
-	u64 traps_count; /* Number of registered traps */
+	size_t traps_count; /* Number of registered traps */
 
 	u16 thin_policer_hw_id;
 
