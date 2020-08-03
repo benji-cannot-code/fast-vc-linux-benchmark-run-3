63423c39ddde23ab310e906ea2dc8921c0d00273
Daniel Campello <campello@chromium.org>
2020-08-03 17:58:09 -0600

    iio: sx9310: Update copyright
    Fixes wrong copyright year.
    
    Signed-off-by: Daniel Campello <campello@chromium.org>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/proximity/sx9310.c
+++ drivers/iio/proximity/sx9310.c
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Based on SX9500 driver and Semtech driver using the input framework
  * <https://my.syncplicity.com/share/teouwsim8niiaud/
  *          linux-driver-SX9310_NoSmartHSensing>.
- * Reworked April 2019 by Evan Green <evgreen@chromium.org>
- * and January 2020 by Daniel Campello <campello@chromium.org>
+ * Reworked in April 2019 by Evan Green <evgreen@chromium.org>
+ * and in January 2020 by Daniel Campello <campello@chromium.org>.
  */
 
 #include <linux/acpi.h>
