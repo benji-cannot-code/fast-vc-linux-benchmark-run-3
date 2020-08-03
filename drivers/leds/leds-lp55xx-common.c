ba7b25e0782f7d7044834f6cc58fcb474e6310d8
Pavel Machek <pavel@ucw.cz>
2020-08-03 13:20:06 +0200

    leds: add RGB color option, as that is different from multicolor.
    Multicolor is a bit too abstract. Yes, we can have
    Green-Magenta-Ultraviolet LED, but so far all the LEDs we support are
    RGB, and not even RGB-White or RGB-Yellow variants emerged.
    
    Multicolor is not a good fit for RGB LED. It does not really know
    about LED color.  In particular, there's no way to make LED "white".
    
    Userspace is interested in knowing "this LED can produce arbitrary
    color", which not all multicolor LEDs can.
    
    Signed-off-by: Pavel Machek <pavel@ucw.cz>

--- drivers/leds/leds-lp55xx-common.c
+++ drivers/leds/leds-lp55xx-common.c
@@ -639,7 +639,7 @@ static int lp55xx_parse_logical_led(struct device_node *np,
 	if (ret)
 		return ret;
 
-	if (led_color == LED_COLOR_ID_MULTI)
+	if (led_color == LED_COLOR_ID_RGB)
 		return lp55xx_parse_multi_led(np, cfg, child_number);
 
 	ret =  lp55xx_parse_common_child(np, cfg, child_number, &chan_nr);
