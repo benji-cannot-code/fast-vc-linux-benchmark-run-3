2378ebc3925ee375288d059f19518f4f0f456753
Krzysztof Kozlowski <krzk@kernel.org>
2020-08-04 21:26:50 +0200

    ARM: s3c24xx: include common.h header in s3c2443.c
    Include common.h header in the s3c2443.c to bring the prototypes of
    defined functions and fix W=1 compile warnings:
    
        arch/arm/mach-s3c24xx/s3c2443.c:60:12: warning: no previous prototype for 's3c2443_init' [-Wmissing-prototypes]
        arch/arm/mach-s3c24xx/s3c2443.c:77:13: warning: no previous prototype for 's3c2443_init_uarts' [-Wmissing-prototypes]
        arch/arm/mach-s3c24xx/s3c2443.c:88:13: warning: no previous prototype for 's3c2443_map_io' [-Wmissing-prototypes]
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- arch/arm/mach-s3c24xx/s3c2443.c
+++ arch/arm/mach-s3c24xx/s3c2443.c
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <plat/cpu.h>
 #include <plat/adc-core.h>
 
+#include "common.h"
 #include "fb-core.h"
 #include "nand-core.h"
 #include "spi-core.h"
