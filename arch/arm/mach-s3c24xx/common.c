2666ab6837161719753bbb9726ef2605997afbd3
Krzysztof Kozlowski <krzk@kernel.org>
2020-08-04 21:26:43 +0200

    clk: samsung: s3c24xx: declare s3c24xx_common_clk_init() in shared header
    The s3c2410_common_clk_init() and others are defined and used by the
    clk-s3c24xx driver and also used in the mach-s3c24xx machine code.  Move
    the declaration to a header to fix W=1 build warnings:
    
        drivers/clk/samsung/clk-s3c2410.c:320:13: warning: no previous prototype for 's3c2410_common_clk_init' [-Wmissing-prototypes]
          320 | void __init s3c2410_common_clk_init(struct device_node *np, unsigned long xti_f,
        drivers/clk/samsung/clk-s3c2412.c:205:13: warning: no previous prototype for 's3c2412_common_clk_init' [-Wmissing-prototypes]
          205 | void __init s3c2412_common_clk_init(struct device_node *np, unsigned long xti_f,
        drivers/clk/samsung/clk-s3c2443.c:341:13: warning: no previous prototype for 's3c2443_common_clk_init' [-Wmissing-prototypes]
          341 | void __init s3c2443_common_clk_init(struct device_node *np, unsigned long xti_f,
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>
    Acked-by: Chanwoo Choi <cw00.choi@samsung.com>
    Reviewed-by: Stephen Boyd <sboyd@kernel.org>

--- arch/arm/mach-s3c24xx/common.c
+++ arch/arm/mach-s3c24xx/common.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/platform_data/dma-s3c24xx.h>
 #include <linux/dmaengine.h>
+#include <linux/clk/samsung.h>
 
 #include <mach/hardware.h>
 #include <mach/regs-clock.h>
--- drivers/clk/samsung/clk-s3c2412.c
+++ drivers/clk/samsung/clk-s3c2412.c
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/clk-provider.h>
+#include <linux/clk/samsung.h>
 #include <linux/io.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
