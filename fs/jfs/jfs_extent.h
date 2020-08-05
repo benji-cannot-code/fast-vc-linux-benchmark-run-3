4516126717a4bf83f477e9cb2d5aaa5a7823ab86
Randy Dunlap <rdunlap@infradead.org>
2020-08-04 19:49:01 -0700

    jfs: delete duplicated words + other fixes
    Delete repeated words in fs/jfs/.
    {for, allocation, if, the}
    Insert "is" in one place to correct the grammar.
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    To: linux-fsdevel@vger.kernel.org
    Cc: jfs-discussion@lists.sourceforge.net

--- fs/jfs/jfs_extent.h
+++ fs/jfs/jfs_extent.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef	_H_JFS_EXTENT
 #define _H_JFS_EXTENT
 
-/*  get block allocation allocation hint as location of disk inode */
+/*  get block allocation hint as location of disk inode */
 #define	INOHINT(ip)	\
 	(addressPXD(&(JFS_IP(ip)->ixpxd)) + lengthPXD(&(JFS_IP(ip)->ixpxd)) - 1)
 
