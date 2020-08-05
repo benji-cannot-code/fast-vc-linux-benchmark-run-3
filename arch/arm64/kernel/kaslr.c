7e85ebfa303f3d7c9dd7b04e5b7d62c124f5b444
Linus Torvalds <torvalds@linux-foundation.org>
2020-08-05 12:39:48 -0700

    random: random.h should include archrandom.h, not the other way around
    This is hopefully the final piece of the crazy puzzle with random.h
    dependencies.
    
    And by "hopefully" I obviously mean "Linus is a hopeless optimist".
    
    Reported-and-tested-by: Daniel Díaz <daniel.diaz@linaro.org>
    Acked-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/arm64/kernel/kaslr.c
+++ arch/arm64/kernel/kaslr.c
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/types.h>
 #include <linux/pgtable.h>
+#include <linux/random.h>
 
-#include <asm/archrandom.h>
 #include <asm/cacheflush.h>
 #include <asm/fixmap.h>
 #include <asm/kernel-pgtable.h>
