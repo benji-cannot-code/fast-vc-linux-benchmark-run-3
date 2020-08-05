aacecfe9a350ae947afa78edd27a5d8f2e9c9578
Stefano Brivio <sbrivio@redhat.com>
2020-08-05 15:39:31 +0200

    ip_tunnel_core: Fix build for archs without _HAVE_ARCH_IPV6_CSUM
    On architectures defining _HAVE_ARCH_IPV6_CSUM, we get
    csum_ipv6_magic() defined by means of arch checksum.h headers. On
    other architectures, we actually need to include net/ip6_checksum.h
    to be able to use it.
    
    Without this include, building with defconfig breaks at least for
    s390.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Fixes: 4cb47a8644cc ("tunnels: PMTU discovery support for directly bridged IP packets")
    Signed-off-by: Stefano Brivio <sbrivio@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/ip_tunnel_core.c
+++ net/ipv4/ip_tunnel_core.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/protocol.h>
 #include <net/ip_tunnels.h>
 #include <net/ip6_tunnel.h>
+#include <net/ip6_checksum.h>
 #include <net/arp.h>
 #include <net/checksum.h>
 #include <net/dsfield.h>
