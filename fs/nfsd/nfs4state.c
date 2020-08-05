b4f5d0693c312f5ff1e59a64c955c639c060a060
J. Bruce Fields <bfields@redhat.com>
2020-08-05 15:10:11 -0400

    nfsd: fix oops on mixed NFSv4/NFSv3 client access
    If an NFSv2/v3 client breaks an NFSv4 client's delegation, it will hit a
    NULL dereference in nfsd_breaker_owns_lease().
    
    Easily reproduceable with for example
    
    	mount -overs=4.2 server:/export /mnt/
    	sleep 1h </mnt/file &
    	mount -overs=3 server:/export /mnt2/
    	touch /mnt2/file
    
    Reported-by: Robert Dinse <nanook@eskimo.com>
    Fixes: 28df3d1539de50 ("nfsd: clients don't need to break their own delegations")
    BugLink: https://bugzilla.kernel.org/show_bug.cgi?id=208807
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -4598,6 +4598,8 @@ static bool nfsd_breaker_owns_lease(struct file_lock *fl)
 	if (!i_am_nfsd())
 		return NULL;
 	rqst = kthread_data(current);
+	if (!rqst->rq_lease_breaker)
+		return NULL;
 	clp = *(rqst->rq_lease_breaker);
 	return dl->dl_stid.sc_client == clp;
 }
