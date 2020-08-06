178f77585911834647c56a6f4d83c35587e4a48f
Arnd Bergmann <arnd@arndb.de>
2020-08-06 20:20:54 +0200

    cpufreq: s3c24xx: move low-level clk reg access into platform code
    Rather than have the cpufreq drivers touch include the
    common headers to get the constants, add a small indirection.
    This is still not the proper way that would do this through
    the common clk API, but it lets us kill off the header file
    usage.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Link: https://lore.kernel.org/r/20200806182059.2431-37-krzk@kernel.org
    [krzk: Rebase and fix -Wold-style-definition]
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- drivers/cpufreq/s3c2410-cpufreq.c
+++ drivers/cpufreq/s3c2410-cpufreq.c
@@ -23,12 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
 
-#include <mach/map.h>
-
-#define S3C2410_CLKREG(x) ((x) + S3C24XX_VA_CLKPWR)
-
-#define S3C2410_CLKDIVN	    S3C2410_CLKREG(0x14)
-
 #define S3C2410_CLKDIVN_PDIVN	     (1<<0)
 #define S3C2410_CLKDIVN_HDIVN	     (1<<1)
 
@@ -44,7 +38,7 @@ static void s3c2410_cpufreq_setdivs(struct s3c_cpufreq_config *cfg)
 	if (cfg->divs.p_divisor != cfg->divs.h_divisor)
 		clkdiv |= S3C2410_CLKDIVN_PDIVN;
 
-	__raw_writel(clkdiv, S3C2410_CLKDIVN);
+	s3c24xx_write_clkdivn(clkdiv);
 }
 
 static int s3c2410_cpufreq_calcdivs(struct s3c_cpufreq_config *cfg)
