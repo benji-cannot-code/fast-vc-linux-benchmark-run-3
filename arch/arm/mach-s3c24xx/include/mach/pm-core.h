5e119a5471519b1cefbd161beef51b59af31a1f3
Arnd Bergmann <arnd@arndb.de>
2020-08-06 20:20:46 +0200

    ARM: s3c24xx: move s3cmci pinctrl handling into board files
    Rather than call the internal s3c_gpio_cfgall_range() function
    through a platform header, move the code into the set_power
    callback that is already exported by the board, and add
    a default implementation.
    
    In DT mode, the code already does not set the pin config,
    so nothing changes there.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Ulf Hansson <ulf.hansson@linaro.org>
    Link: https://lore.kernel.org/r/20200806182059.2431-29-krzk@kernel.org
    [krzk: Rebase and correct set_power in mach-h1940.c]
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- arch/arm/mach-s3c24xx/include/mach/pm-core.h
+++ arch/arm/mach-s3c24xx/include/mach/pm-core.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "regs-clock.h"
 #include "regs-irq.h"
+#include <mach/irqs.h>
 
 static inline void s3c_pm_debug_init_uart(void)
 {
