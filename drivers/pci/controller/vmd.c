9d82ddde1b2f7baecf3894399dae95c2d383c3d1
Jon Derrick <jonathan.derrick@intel.com>
2020-08-06 17:00:17 -0400

    PCI: vmd: Update VMD PM to correctly use generic PCI PM
    The pci_save_state() call in vmd_suspend() can be performed by
    pci_pm_suspend_irq(). This also allows VMD to benefit from the call into
    pci_prepare_to_sleep().
    
    The pci_restore_state() call in vmd_resume() was restoring state after
    pci_pm_resume()::pci_restore_standard_config() had already restored state.
    It's also been suspected that the config state should have been restored
    before re-requesting IRQs instead of afterwards.
    
    Remove the pci_save_state()/pci_restore_state() calls in
    vmd_suspend()/vmd_resume() to allow proper flow through generic PCI core
    Power Management code.
    
    Link: https://lore.kernel.org/r/20200806210017.5654-1-jonathan.derrick@intel.com
    Signed-off-by: Jon Derrick <jonathan.derrick@intel.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Cc: You-Sheng Yang <vicamo.yang@canonical.com>

--- drivers/pci/controller/vmd.c
+++ drivers/pci/controller/vmd.c
@@ -784,7 +784,6 @@ static int vmd_suspend(struct device *dev)
 	for (i = 0; i < vmd->msix_count; i++)
 		devm_free_irq(dev, pci_irq_vector(pdev, i), &vmd->irqs[i]);
 
-	pci_save_state(pdev);
 	return 0;
 }
 
@@ -802,7 +801,6 @@ static int vmd_resume(struct device *dev)
 			return err;
 	}
 
-	pci_restore_state(pdev);
 	return 0;
 }
 #endif
