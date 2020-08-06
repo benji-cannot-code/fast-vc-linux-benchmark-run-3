d00e31fb4bb0c82c590fee2bb1b22ed8ca2e601d
Arnd Bergmann <arnd@arndb.de>
2020-08-06 20:20:26 +0200

    usb: gadget: s3c-hsudc: remove platform header dependency
    There is no real phy driver, so s3c-hsudc just pokes the registers
    itself. Improve this a little by making it a platform data callback
    like we do for gpios.
    
    There is only one board using this driver, and it's unlikely
    that another would be added, so this is a minimal workaround.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Link: https://lore.kernel.org/r/20200806182059.2431-9-krzk@kernel.org
    [krzk: Include regs-s3c2443-clock.h in ifdef to fixup build on s3c6400]
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- arch/arm/plat-samsung/devs.c
+++ arch/arm/plat-samsung/devs.c
@@ -41,6 +41,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <mach/irqs.h>
 #include <mach/map.h>
 
+#ifdef CONFIG_PLAT_S3C24XX
+#include <mach/regs-s3c2443-clock.h>
+#endif /* CONFIG_PLAT_S3C24XX */
+
 #include <plat/cpu.h>
 #include <plat/devs.h>
 #include <plat/adc.h>
@@ -1038,6 +1042,8 @@ struct platform_device s3c_device_usb_hsudc = {
 void __init s3c24xx_hsudc_set_platdata(struct s3c24xx_hsudc_platdata *pd)
 {
 	s3c_set_platdata(pd, sizeof(*pd), &s3c_device_usb_hsudc);
+	pd->phy_init = s3c_hsudc_init_phy;
+	pd->phy_uninit = s3c_hsudc_uninit_phy;
 }
 #endif /* CONFIG_PLAT_S3C24XX */
 
