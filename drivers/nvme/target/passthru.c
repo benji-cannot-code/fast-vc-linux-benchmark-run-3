dadd0997f752b10e5d2a334eec28ba0d703b54c8
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2020-08-06 15:56:27 -0700

    nvmet: call blk_mq_free_request() directly
    Instead of calling blk_put_request() which calls blk_mq_free_request(),
    call blk_mq_free_request() directly for NVMeOF passthru. This is to
    mainly avoid an extra function call in the completion path
    nvmet_passthru_req_done().
    
    Signed-off-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Reviewed-by: Keith Busch <kbusch@kernel.org>
    Reviewed-by: Logan Gunthorpe <logang@deltatee.com>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- drivers/nvme/target/passthru.c
+++ drivers/nvme/target/passthru.c
@@ -166,7 +166,7 @@ static void nvmet_passthru_execute_cmd_work(struct work_struct *w)
 
 	req->cqe->result = nvme_req(rq)->result;
 	nvmet_req_complete(req, status);
-	blk_put_request(rq);
+	blk_mq_free_request(rq);
 }
 
 static void nvmet_passthru_req_done(struct request *rq,
@@ -176,7 +176,7 @@ static void nvmet_passthru_req_done(struct request *rq,
 
 	req->cqe->result = nvme_req(rq)->result;
 	nvmet_req_complete(req, nvme_req(rq)->status);
-	blk_put_request(rq);
+	blk_mq_free_request(rq);
 }
 
 static int nvmet_passthru_map_sg(struct nvmet_req *req, struct request *rq)
@@ -275,7 +275,7 @@ static void nvmet_passthru_execute_cmd(struct nvmet_req *req)
 	return;
 
 out_put_req:
-	blk_put_request(rq);
+	blk_mq_free_request(rq);
 out_put_ns:
 	if (ns)
 		nvme_put_ns(ns);
