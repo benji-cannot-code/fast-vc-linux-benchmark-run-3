69b4ac18a897d4f1170d178ec544f199c8e0353e
Ashley Thomas <Ashley.Thomas2@amd.com>
2020-08-06 19:18:37 -0700

    drm/amd/display: Power eDP panel back ON before link training retry
    [why]
    When link training failures occur for eDP, dp_disable_link_phy
    is called which powers OFF eDP panel. After link training retry
    delay, the next retry begins by calling dp_enable_link_phy
    which does not issue a correspnding eDP panel power ON, leaving
    panel powered OFF which leads to display OFF/dark.
    
    [how]
    Power ON eDP before next link training retry.
    
    Signed-off-by: Ashley Thomas <Ashley.Thomas2@amd.com>
    Acked-by: Aurabindo Pillai <aurabindo.pillai@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link_dp.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link_dp.c
@@ -1620,6 +1620,9 @@ bool perform_link_training_with_retries(
 
 	for (j = 0; j < attempts; ++j) {
 
+		DC_LOG_HW_LINK_TRAINING("%s: Beginning link training attempt %u of %d\n",
+			__func__, (unsigned int)j + 1, attempts);
+
 		dp_enable_link_phy(
 			link,
 			signal,
@@ -1647,6 +1650,9 @@ bool perform_link_training_with_retries(
 		 */
 		if (j == (attempts - 1))
 			break;
+
+		DC_LOG_WARNING("%s: Link training attempt %u of %d failed\n",
+			__func__, (unsigned int)j + 1, attempts);
 
 		dp_disable_link_phy(link, signal);
 
