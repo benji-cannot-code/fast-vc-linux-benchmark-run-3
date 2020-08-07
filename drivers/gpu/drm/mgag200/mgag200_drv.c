0bb98e6e222004789de542fb5ecd93f4a217a957
Sam Ravnborg <sam@ravnborg.org>
2020-08-07 20:05:47 +0200

    drm/mgag200: fix build on alpha arch
    When building imgag200 for the alpha architecture it fails like this:
    mgag200_drv.c:233:9: error: implicit declaration of function ‘vmalloc’
      233 |  bios = vmalloc(size);
          |         ^~~~~~~
          |         kmalloc
    
    When building for other architectures vmalloc.h is pulled in via some
    other header file - for example asm-generic/io.h.
    Use an explicit include of vmalloc.h to fix the build.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Fixes: e20dfd27f7aa ("drm/mgag200: Add support for G200 desktop cards")
    Cc: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: Egbert Eich <eich@suse.com>
    Cc: Takashi Iwai <tiwai@suse.de>
    Cc: Lyude Paul <lyude@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200807180547.GA923146@ravnborg.org

--- drivers/gpu/drm/mgag200/mgag200_drv.c
+++ drivers/gpu/drm/mgag200/mgag200_drv.c
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/console.h>
 #include <linux/module.h>
 #include <linux/pci.h>
+#include <linux/vmalloc.h>
 
 #include <drm/drm_drv.h>
 #include <drm/drm_file.h>
