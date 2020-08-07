1bced67c5219ea2f4deacc51ff4eec125934b937
Cédric Le Goater <clg@kaod.org>
2020-08-07 12:18:54 +0200

    powerpc/pci: unmap legacy INTx interrupts when a PHB is removed
    When a passthrough IO adapter is removed from a pseries machine using
    hash MMU and the XIVE interrupt mode, the POWER hypervisor expects the
    guest OS to clear all page table entries related to the adapter. If
    some are still present, the RTAS call which isolates the PCI slot
    returns error 9001 "valid outstanding translations" and the removal of
    the IO adapter fails. This is because when the PHBs are scanned, Linux
    maps automatically the INTx interrupts in the Linux interrupt number
    space but these are never removed.
    
    To solve this problem, we introduce a PPC platform specific
    pcibios_remove_bus() routine which clears all interrupt mappings when
    the bus is removed. This also clears the associated page table entries
    of the ESB pages when using XIVE.
    
    For this purpose, we record the logical interrupt numbers of the
    mapped interrupt under the PHB structure and let pcibios_remove_bus()
    do the clean up.
    
    Since some PCI adapters, like GPUs, use the "interrupt-map" property
    to describe interrupt mappings other than the legacy INTx interrupts,
    we can not restrict the size of the mapping array to PCI_NUM_INTX. The
    number of interrupt mappings is computed from the "interrupt-map"
    property and the mapping array is allocated accordingly.
    
    Signed-off-by: Cédric Le Goater <clg@kaod.org>
    Reviewed-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200807101854.844619-1-clg@kaod.org

--- arch/powerpc/include/asm/pci-bridge.h
+++ arch/powerpc/include/asm/pci-bridge.h
@@ -49,6 +49,9 @@ struct pci_controller_ops {
 
 /*
  * Structure of a PCI controller (host bridge)
+ *
+ * @irq_count: number of interrupt mappings
+ * @irq_map: interrupt mappings
  */
 struct pci_controller {
 	struct pci_bus *bus;
@@ -128,6 +131,9 @@ struct pci_controller {
 
 	void *private_data;
 	struct npu *npu;
+
+	unsigned int irq_count;
+	unsigned int *irq_map;
 };
 
 /* These are used for config access before all the PCI probing
