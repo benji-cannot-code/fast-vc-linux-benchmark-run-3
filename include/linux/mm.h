448466051aee03229576723e8bd491098eaca17a
John Hubbard <jhubbard@nvidia.com>
2020-08-06 23:19:51 -0700

    mm, dump_page: do not crash with bad compound_mapcount()
    If a compound page is being split while dump_page() is being run on that
    page, we can end up calling compound_mapcount() on a page that is no
    longer compound.  This leads to a crash (already seen at least once in the
    field), due to the VM_BUG_ON_PAGE() assertion inside compound_mapcount().
    
    (The above is from Matthew Wilcox's analysis of Qian Cai's bug report.)
    
    A similar problem is possible, via compound_pincount() instead of
    compound_mapcount().
    
    In order to avoid this kind of crash, make dump_page() slightly more
    robust, by providing a pair of simpler routines that don't contain
    assertions: head_mapcount() and head_pincount().
    
    For debug tools, we don't want to go *too* far in this direction, but this
    is a simple small fix, and the crash has already been seen, so it's a good
    trade-off.
    
    Reported-by: Qian Cai <cai@lca.pw>
    Suggested-by: Matthew Wilcox <willy@infradead.org>
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Mike Rapoport <rppt@linux.ibm.com>
    Cc: William Kucharski <william.kucharski@oracle.com>
    Link: http://lkml.kernel.org/r/20200804214807.169256-1-jhubbard@nvidia.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -780,6 +780,11 @@ static inline void *kvcalloc(size_t n, size_t size, gfp_t flags)
 extern void kvfree(const void *addr);
 extern void kvfree_sensitive(const void *addr, size_t len);
 
+static inline int head_mapcount(struct page *head)
+{
+	return atomic_read(compound_mapcount_ptr(head)) + 1;
+}
+
 /*
  * Mapcount of compound page as a whole, does not include mapped sub-pages.
  *
@@ -789,7 +794,7 @@ static inline int compound_mapcount(struct page *page)
 {
 	VM_BUG_ON_PAGE(!PageCompound(page), page);
 	page = compound_head(page);
-	return atomic_read(compound_mapcount_ptr(page)) + 1;
+	return head_mapcount(page);
 }
 
 /*
@@ -902,11 +907,16 @@ static inline bool hpage_pincount_available(struct page *page)
 	return PageCompound(page) && compound_order(page) > 1;
 }
 
+static inline int head_pincount(struct page *head)
+{
+	return atomic_read(compound_pincount_ptr(head));
+}
+
 static inline int compound_pincount(struct page *page)
 {
 	VM_BUG_ON_PAGE(!hpage_pincount_available(page), page);
 	page = compound_head(page);
-	return atomic_read(compound_pincount_ptr(page));
+	return head_pincount(page);
 }
 
 static inline void set_compound_order(struct page *page, unsigned int order)
