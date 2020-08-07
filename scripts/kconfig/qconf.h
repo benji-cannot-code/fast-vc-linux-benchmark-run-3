ca09a8089ec0f2b22bf58448e965681dbb096613
Masahiro Yamada <masahiroy@kernel.org>
2020-08-07 18:18:59 +0900

    kconfig: qconf: remove unused argument from ConfigList::updateList()
    This function allocates 'item' before using it, so the argument 'item'
    is always shadowed.
    
    Remove the meaningless argument.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>

--- scripts/kconfig/qconf.h
+++ scripts/kconfig/qconf.h
@@ -70,7 +70,7 @@ protected:
 public slots:
 	void setRootMenu(struct menu *menu);
 
-	void updateList(ConfigItem *item);
+	void updateList();
 	void setValue(ConfigItem* item, tristate val);
 	void changeValue(ConfigItem* item);
 	void updateSelection(void);
@@ -86,7 +86,7 @@ public:
 	void updateListAll(void)
 	{
 		updateAll = true;
-		updateList(NULL);
+		updateList();
 		updateAll = false;
 	}
 	void addColumn(colIdx idx)
