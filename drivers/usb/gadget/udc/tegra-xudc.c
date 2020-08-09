f8d23fdd8340ae7c9903671a50308807763e57aa
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2020-08-09 09:29:48 +0200

    usb: gadget: tegra-xudc: Avoid GFP_ATOMIC where it is not needed
    There is no need to use GFP_ATOMIC here. It is a probe function, no
    spinlock is taken.
    
    Reviewed-by: JC Kuo <jckuo@nvidia.com>
    Acked-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- drivers/usb/gadget/udc/tegra-xudc.c
+++ drivers/usb/gadget/udc/tegra-xudc.c
@@ -3734,7 +3734,7 @@ static int tegra_xudc_probe(struct platform_device *pdev)
 	unsigned int i;
 	int err;
 
-	xudc = devm_kzalloc(&pdev->dev, sizeof(*xudc), GFP_ATOMIC);
+	xudc = devm_kzalloc(&pdev->dev, sizeof(*xudc), GFP_KERNEL);
 	if (!xudc)
 		return -ENOMEM;
 
