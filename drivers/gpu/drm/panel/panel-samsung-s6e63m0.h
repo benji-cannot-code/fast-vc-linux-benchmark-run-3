26c22bda5daaf2211a6e055df86871a3da9f56e0
Linus Walleij <linus.walleij@linaro.org>
2020-08-09 23:51:02 +0200

    drm/panel: s6e63m0: Add DSI transport
    This makes it possible to use the s6e63m0 panel with a
    DSI host, such as in the Samsung GT-I8190 (Golden) mobile
    phone.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Tested-by: Stephan Gerhold <stephan@gerhold.net>
    Cc: Stephan Gerhold <stephan@gerhold.net>
    Cc: Paweł Chmiel <pawel.mikolaj.chmiel@gmail.com>
    Acked-by: Paul Cercueil <paul@crapouillou.net>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200809215104.1830206-3-linus.walleij@linaro.org

--- drivers/gpu/drm/panel/panel-samsung-s6e63m0.h
+++ drivers/gpu/drm/panel/panel-samsung-s6e63m0.h
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _PANEL_SAMSUNG_S6E63M0_H
 
 int s6e63m0_probe(struct device *dev,
-		  int (*dcs_write)(struct device *dev, const u8 *data, size_t len));
+		  int (*dcs_write)(struct device *dev, const u8 *data,
+				   size_t len),
+		  bool dsi_mode);
 int s6e63m0_remove(struct device *dev);
 
 #endif /* _PANEL_SAMSUNG_S6E63M0_H */
