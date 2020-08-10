d62a40aa3aa3f18849d238b053eb5ed7dce7dac5
Srikar Dronamraju <srikar@linux.vnet.ibm.com>
2020-08-10 12:48:31 +0530

    powerpc/numa: Detect support for coregroup
    Add support for grouping cores based on the device-tree classification.
    - The last domain in the associativity domains always refers to the
    core.
    - If primary reference domain happens to be the penultimate domain in
    the associativity domains device-tree property, then there are no
    coregroups. However if its not a penultimate domain, then there are
    coregroups. There can be more than one coregroup. For now we would be
    interested in the last or the smallest coregroups, i.e one sub-group
    per DIE.
    
    Currently there are no firmwares that are exposing this grouping. Hence
    allow the basis for grouping to be abstract.  Once the firmware starts
    using this grouping, code would be added to detect the type of grouping
    and adjust the sd domain flags accordingly.
    
    Signed-off-by: Srikar Dronamraju <srikar@linux.vnet.ibm.com>
    Reviewed-by: Gautham R. Shenoy <ego@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200810071834.92514-8-srikar@linux.vnet.ibm.com

--- arch/powerpc/include/asm/smp.h
+++ arch/powerpc/include/asm/smp.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern int boot_cpuid;
 extern int spinning_secondaries;
 extern u32 *cpu_to_phys_id;
+extern bool coregroup_enabled;
 
 extern void cpu_die(void);
 extern int cpu_to_chip_id(int cpu);
