4259a44574bf13a7af8d070e485b703f79ad802f
Alex Deucher <alexander.deucher@amd.com>
2020-08-11 12:02:21 -0400

    drm/amdgpu: note what type of reset we are using
    When we reset the GPU, note what type of reset will be
    used.  This makes debugging different reset scenarios
    more clear as the driver may use different reset
    methods depending on conditions on the system.
    
    Acked-by: Nirmoy Das <nirmoy.das@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/si.c
+++ drivers/gpu/drm/amd/amdgpu/si.c
@@ -1303,6 +1303,8 @@ static int si_asic_reset(struct amdgpu_device *adev)
 {
 	int r;
 
+	dev_info(adev->dev, "PCI CONFIG reset\n");
+
 	amdgpu_atombios_scratch_regs_engine_hung(adev, true);
 
 	r = si_gpu_pci_config_reset(adev);
