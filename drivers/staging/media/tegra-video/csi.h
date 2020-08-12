9ba1d6dabd5d7d8c8d8206d4879b5a10bf3cf719
Sowjanya Komatineni <skomatineni@nvidia.com>
2020-08-12 02:27:17 +0200

    media: tegra-video: Add support for external sensor capture
    This patch adds support to capture from the external sensor
    based on device graph in the device tree.
    
    Driver walks through the device graph to create media links
    between the entities and registers and unregisters video devices
    when the corresponding sub-devices are bound and unbound.
    
    Channel formats are enumerated based on available formats from
    the sensor and the corresponding matched formats from the Tegra
    supported video formats list.
    
    Each Tegra CSI instance can be configured as 4-lane or 2-lane
    based on supported lane configuration from the sensor through
    the device tree.
    
    Currently this driver supports V4L2 video node centric only.
    
    [hverkuil: changed video_unregister_device to vb2_video_unregister_device]
    
    Signed-off-by: Sowjanya Komatineni <skomatineni@nvidia.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/staging/media/tegra-video/csi.h
+++ drivers/staging/media/tegra-video/csi.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __TEGRA_CSI_H__
 
 #include <media/media-entity.h>
+#include <media/v4l2-async.h>
 #include <media/v4l2-subdev.h>
 
 /*
