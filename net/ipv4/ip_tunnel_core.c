cf6869a4d2e679a8ce0a05817f007a57f72330b8
David S. Miller <davem@davemloft.net>
2020-08-12 13:26:37 -0700

    Revert "ipv4: tunnel: fix compilation on ARCH=um"
    This reverts commit 06a7a37be55e29961c9ba2abec4d07c8e0e21861.
    
    The bug was already fixed, this added a dup include.
    
    Reported-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/ip_tunnel_core.c
+++ net/ipv4/ip_tunnel_core.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/geneve.h>
 #include <net/vxlan.h>
 #include <net/erspan.h>
-#include <net/ip6_checksum.h>
 
 const struct ip_tunnel_encap_ops __rcu *
 		iptun_encaps[MAX_IPTUN_ENCAP_OPS] __read_mostly;
