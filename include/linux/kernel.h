de52e650486a1382b59f677fdc255bcf45eedaf9
Arvind Sankar <nivedita@alum.mit.edu>
2020-08-11 18:34:16 -0700

    kernel.h: remove duplicate include of asm/div64.h
    This seems to have been added inadvertently in commit
      72deb455b5ec ("block: remove CONFIG_LBDAF")
    
    Fixes: 72deb455b5ec ("block: remove CONFIG_LBDAF")
    Signed-off-by: Arvind Sankar <nivedita@alum.mit.edu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Link: http://lkml.kernel.org/r/20200727034852.2813453-1-nivedita@alum.mit.edu
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/byteorder.h>
 #include <asm/div64.h>
 #include <uapi/linux/kernel.h>
-#include <asm/div64.h>
 
 #define STACK_MAGIC	0xdeadbeef
 
