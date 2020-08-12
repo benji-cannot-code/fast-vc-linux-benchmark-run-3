0e43fb5c59f9b709f9f30dca6e385131f726106e
Tian Tao <tiantao6@hisilicon.com>
2020-08-12 15:42:58 +0800

    drm/hisilicon: Remove the unused include statements
    Remove some unused include statements.
    
    Signed-off-by: Tian Tao <tiantao6@hisilicon.com>
    Reviewed-by: Thomas Zimmermann <tzimmermann@suse.de>
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/1597218179-3938-2-git-send-email-tiantao6@hisilicon.com

--- drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_vdac.c
+++ drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_vdac.c
@@ -12,10 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	Jianhua Li <lijianhua@huawei.com>
  */
 
-#include <drm/drm_gem_vram_helper.h>
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_probe_helper.h>
-#include <drm/drm_crtc_helper.h>
 #include <drm/drm_print.h>
 
 #include "hibmc_drm_drv.h"
