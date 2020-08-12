0dc7f455369981dcd68713276a0d960285dae920
Paul E. McKenney <paulmck@kernel.org>
2020-08-11 21:18:12 -0700

    rcuperf: Change rcuperf to rcuscale
    This commit further avoids conflation of rcuperf with the kernel's perf
    feature by renaming kernel/rcu/rcuperf.c to kernel/rcu/rcuscale.c, and
    also by similarly renaming the functions and variables inside this file.
    This has the side effect of changing the names of the kernel boot
    parameters, so kernel-parameters.txt and ver_functions.sh are also
    updated.  The rcutorture --torture type was also updated from rcuperf
    to rcuscale.
    
    [ paulmck: Fix bugs located by Stephen Rothwell. ]
    Reported-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- tools/testing/selftests/rcutorture/bin/kvm-recheck-rcuscale-ftrace.sh
+++ tools/testing/selftests/rcutorture/bin/kvm-recheck-rcuscale-ftrace.sh
@@ -2,12 +2,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #!/bin/bash
 # SPDX-License-Identifier: GPL-2.0+
 #
-# Analyze a given results directory for rcuperf performance measurements,
+# Analyze a given results directory for rcuscale performance measurements,
 # looking for ftrace data.  Exits with 0 if data was found, analyzed, and
-# printed.  Intended to be invoked from kvm-recheck-rcuperf.sh after
+# printed.  Intended to be invoked from kvm-recheck-rcuscale.sh after
 # argument checking.
 #
-# Usage: kvm-recheck-rcuperf-ftrace.sh resdir
+# Usage: kvm-recheck-rcuscale-ftrace.sh resdir
 #
 # Copyright (C) IBM Corporation, 2016
 #
--- tools/testing/selftests/rcutorture/configs/rcuscale/ver_functions.sh
+++ tools/testing/selftests/rcutorture/configs/rcuscale/ver_functions.sh
@@ -12,6 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #
 # Adds per-version torture-module parameters to kernels supporting them.
 per_version_boot_params () {
-	echo $1 rcuperf.shutdown=1 \
-		rcuperf.verbose=1
+	echo $1 rcuscale.shutdown=1 \
+		rcuscale.verbose=1
 }
