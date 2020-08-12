c5f13ee62987824a425841ec084687802f9ed157
Arvind Sankar <nivedita@alum.mit.edu>
2020-08-11 18:32:46 -0700

    sparc: drop unused MAX_PHYSADDR_BITS
    The macro is not used anywhere, so remove the definition.
    
    Signed-off-by: Arvind Sankar <nivedita@alum.mit.edu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Acked-by: Dave Hansen <dave.hansen@linux.intel.com>
    Acked-by: David S. Miller <davem@davemloft.net>
    Acked-by: Mike Rapoport <rppt@linux.ibm.com>
    Link: http://lkml.kernel.org/r/20200723231544.17274-4-nivedita@alum.mit.edu
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/sparc/include/asm/sparsemem.h
+++ arch/sparc/include/asm/sparsemem.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/page.h>
 
 #define SECTION_SIZE_BITS       30
-#define MAX_PHYSADDR_BITS       MAX_PHYS_ADDRESS_BITS
 #define MAX_PHYSMEM_BITS        MAX_PHYS_ADDRESS_BITS
 
 #endif /* !(__KERNEL__) */
