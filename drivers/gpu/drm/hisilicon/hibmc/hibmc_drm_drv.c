4a1cf2fc8dae906be7a3699dfda18fe862da0497
Tian Tao <tiantao6@hisilicon.com>
2020-08-13 11:39:15 +0800

    drm/hisilicon: Fix build error of no type of module_init
    Add missing include to fix build error:
    hibmc_drm_drv.c:385:1: warning: data definition has no type or storage
    class [enabled by default]
    hibmc_drm_drv.c:385:1: error: type defaults to ‘int’ in declaration
    of ‘module_init’ [-Werror=implicit-int]
    hibmc_drm_drv.c:385:1: warning: parameter names (without types) in function
    of ‘module_exit’ [-Werror=implicit-int]
    hibmc_drm_drv.c:385:292:1: warning: parameter names (without types) in
    function declaration [enabled by default]
    
    Signed-off-by: Tian Tao <tiantao6@hisilicon.com>
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/1597289955-27381-1-git-send-email-tiantao6@hisilicon.com

--- drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_drv.c
+++ drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_drv.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	Jianhua Li <lijianhua@huawei.com>
  */
 
+#include <linux/module.h>
 #include <linux/pci.h>
 
 #include <drm/drm_atomic_helper.h>
