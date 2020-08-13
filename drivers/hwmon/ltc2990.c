f3b307927dcdd7bc64808dccda02d5e1a49f01d6
Stephen Kitt <steve@sk2.org>
2020-08-13 18:02:22 +0200

    hwmon: use simple i2c probe function
    Many hwmon drivers don't use the id information provided by the old
    i2c probe function, and the remainder can easily be adapted to the new
    form ("probe_new") by calling i2c_match_id explicitly.
    
    This avoids scanning the identifier tables during probes.
    
    Drivers which didn't use the id are converted as-is; drivers which did
    are modified as follows:
    
    * if the information in i2c_client is sufficient, that's used instead
      (client->name);
    * anything else is handled by calling i2c_match_id() with the same
      level of error-handling (if any) as before.
    
    A few drivers aren't included in this patch because they have a
    different set of maintainers. They will be covered by other patches.
    
    Signed-off-by: Stephen Kitt <steve@sk2.org>
    Link: https://lore.kernel.org/r/20200813160222.1503401-1-steve@sk2.org
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/ltc2990.c
+++ drivers/hwmon/ltc2990.c
@@ -201,8 +201,7 @@ static const struct attribute_group ltc2990_group = {
 };
 __ATTRIBUTE_GROUPS(ltc2990);
 
-static int ltc2990_i2c_probe(struct i2c_client *i2c,
-			     const struct i2c_device_id *id)
+static int ltc2990_i2c_probe(struct i2c_client *i2c)
 {
 	int ret;
 	struct device *hwmon_dev;
@@ -270,7 +269,7 @@ static struct i2c_driver ltc2990_i2c_driver = {
 	.driver = {
 		.name = "ltc2990",
 	},
-	.probe    = ltc2990_i2c_probe,
+	.probe_new = ltc2990_i2c_probe,
 	.id_table = ltc2990_i2c_id,
 };
 
--- drivers/hwmon/shtc1.c
+++ drivers/hwmon/shtc1.c
@@ -186,15 +186,16 @@ static void shtc1_select_command(struct shtc1_data *data)
 	}
 }
 
-static int shtc1_probe(struct i2c_client *client,
-		       const struct i2c_device_id *id)
+static const struct i2c_device_id shtc1_id[];
+
+static int shtc1_probe(struct i2c_client *client)
 {
 	int ret;
 	u16 id_reg;
 	char id_reg_buf[2];
 	struct shtc1_data *data;
 	struct device *hwmon_dev;
-	enum shtcx_chips chip = id->driver_data;
+	enum shtcx_chips chip = i2c_match_id(shtc1_id, client)->driver_data;
 	struct i2c_adapter *adap = client->adapter;
 	struct device *dev = &client->dev;
 
@@ -260,7 +261,7 @@ MODULE_DEVICE_TABLE(i2c, shtc1_id);
 
 static struct i2c_driver shtc1_i2c_driver = {
 	.driver.name  = "shtc1",
-	.probe        = shtc1_probe,
+	.probe_new    = shtc1_probe,
 	.id_table     = shtc1_id,
 };
 
