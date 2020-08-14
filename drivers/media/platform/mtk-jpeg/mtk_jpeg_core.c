425613c648a402cab23ac49d2c6e94ecebc4b1d7
Xia Jiang <xia.jiang@mediatek.com>
2020-08-14 09:11:39 +0200

    media: platform: Delete the resetting hardware flow in the system PM ops
    Delete the resetting hardware flow in suspend and resume function
    because that resetting operation will be done in device_run().
    
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Signed-off-by: Xia Jiang <xia.jiang@mediatek.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/platform/mtk-jpeg/mtk_jpeg_core.c
+++ drivers/media/platform/mtk-jpeg/mtk_jpeg_core.c
@@ -1192,7 +1192,6 @@ static __maybe_unused int mtk_jpeg_pm_suspend(struct device *dev)
 {
 	struct mtk_jpeg_dev *jpeg = dev_get_drvdata(dev);
 
-	mtk_jpeg_dec_reset(jpeg->dec_reg_base);
 	mtk_jpeg_clk_off(jpeg);
 
 	return 0;
@@ -1203,7 +1202,6 @@ static __maybe_unused int mtk_jpeg_pm_resume(struct device *dev)
 	struct mtk_jpeg_dev *jpeg = dev_get_drvdata(dev);
 
 	mtk_jpeg_clk_on(jpeg);
-	mtk_jpeg_dec_reset(jpeg->dec_reg_base);
 
 	return 0;
 }
