97db6bc3f1d2d30dd78b873bd0ce7c47bc341b30
Lee Jones <lee.jones@linaro.org>
2020-08-14 12:39:32 +0100

    net: fddi: skfp: cfm: Remove seemingly unused variable 'ID_sccs'
    This variable is present in many source files and has not been used
    anywhere (at least internally) since it was introduced.
    
    Fixes the following W=1 kernel build warning(s):
    
     drivers/net/fddi/skfp/cfm.c: In function ‘cfm’:
     drivers/net/fddi/skfp/cfm.c:211:6: warning: variable ‘oldstate’ set but not used [-Wunused-but-set-variable]
     drivers/net/fddi/skfp/cfm.c:40:19: warning: ‘ID_sccs’ defined but not used [-Wunused-const-variable=]
    
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Jakub Kicinski <kuba@kernel.org>
    Cc: Lee Jones <lee.jones@linaro.org>
    Cc: netdev@vger.kernel.org
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/fddi/skfp/cfm.c
+++ drivers/net/fddi/skfp/cfm.c
@@ -37,10 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KERNEL
 #include "h/smtstate.h"
 
-#ifndef	lint
-static const char ID_sccs[] = "@(#)cfm.c	2.18 98/10/06 (C) SK " ;
-#endif
-
 /*
  * FSM Macros
  */
