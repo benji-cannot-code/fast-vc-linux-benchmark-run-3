785ab93aadc083ea028243304faa2d3ce3ba3c5b
Sam Ravnborg <sam@ravnborg.org>
2020-08-15 14:54:02 +0200

    drm/panel: samsung: Use dev_ based logging
    Standardize on the dev_ based logging and drop the include of drm_print.h.
    Fix a few cases where "x@" was used when printing the mode.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Reviewed-by: Guido Günther <agx@sigxcpu.org>
    Cc: Thierry Reding <thierry.reding@gmail.com>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Cc: Guido Günther <agx@sigxcpu.org>
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Cc: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200815125406.1153224-2-sam@ravnborg.org

--- drivers/gpu/drm/panel/panel-samsung-s6e63j0x03.c
+++ drivers/gpu/drm/panel/panel-samsung-s6e63j0x03.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_mipi_dsi.h>
 #include <drm/drm_modes.h>
 #include <drm/drm_panel.h>
-#include <drm/drm_print.h>
 
 #define MCS_LEVEL2_KEY		0xf0
 #define MCS_MTP_KEY		0xf1
@@ -407,7 +406,7 @@ static int s6e63j0x03_get_modes(struct drm_panel *panel,
 
 	mode = drm_mode_duplicate(connector->dev, &default_mode);
 	if (!mode) {
-		DRM_ERROR("failed to add mode %ux%ux@%u\n",
+		dev_err(panel->dev, "failed to add mode %ux%u@%u\n",
 			default_mode.hdisplay, default_mode.vdisplay,
 			drm_mode_vrefresh(&default_mode));
 		return -ENOMEM;
