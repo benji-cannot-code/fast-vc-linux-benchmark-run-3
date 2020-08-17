7bec19d8a40672aa46d9d5e0ee555831d5bb1bae
Carl Huang <cjhuang@codeaurora.org>
2020-08-17 13:31:51 +0300

    ath11k: initialize wmi config based on hw_params
    QCA6390 has very different wmi config parameters compared to IPQ8074,
    so use different function to initialize wmi init config parameters.
    
    Tested-on: QCA6390 hw2.0 PCI WLAN.HST.1.0.1-01740-QCAHSTSWPLZ_V2_TO_X86-1
    Tested-on: IPQ8074 hw2.0 AHB WLAN.HK.2.1.0.1-01238-QCAHKSWPL_SILICONZ-2
    
    Signed-off-by: Carl Huang <cjhuang@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/1597576599-8857-11-git-send-email-kvalo@codeaurora.org

--- drivers/net/wireless/ath/ath11k/hw.h
+++ drivers/net/wireless/ath/ath11k/hw.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef ATH11K_HW_H
 #define ATH11K_HW_H
 
+#include "wmi.h"
+
 /* Target configuration defines */
 
 /* Num VDEVS per radio */
@@ -117,6 +119,8 @@ struct ath11k_hw_ring_mask {
 
 struct ath11k_hw_ops {
 	u8 (*get_hw_mac_from_pdev_id)(int pdev_id);
+	void (*wmi_init_config)(struct ath11k_base *ab,
+				struct target_resource_config *config);
 };
 
 struct ath11k_hw_params {
