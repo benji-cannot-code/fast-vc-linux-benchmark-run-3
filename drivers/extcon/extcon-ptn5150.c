e81166ba24487222a160d30f0190e71d2709d910
Krzysztof Kozlowski <krzk@kernel.org>
2020-08-17 09:00:00 +0200

    extcon: ptn5150: Fix usage of atomic GPIO with sleeping GPIO chips
    The driver uses atomic version of gpiod_set_value() without any real
    reason.  It is called in a workqueue under mutex so it could sleep
    there.  Changing it to "can_sleep" flavor allows to use the driver with
    all GPIO chips.
    
    Fixes: 4ed754de2d66 ("extcon: Add support for ptn5150 extcon driver")
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>
    Reviewed-by: Vijai Kumar K <vijaikumar.kanagarajan@gmail.com>
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- drivers/extcon/extcon-ptn5150.c
+++ drivers/extcon/extcon-ptn5150.c
@@ -128,7 +128,7 @@ static void ptn5150_irq_work(struct work_struct *work)
 			case PTN5150_DFP_ATTACHED:
 				extcon_set_state_sync(info->edev,
 						EXTCON_USB_HOST, false);
-				gpiod_set_value(info->vbus_gpiod, 0);
+				gpiod_set_value_cansleep(info->vbus_gpiod, 0);
 				extcon_set_state_sync(info->edev, EXTCON_USB,
 						true);
 				break;
@@ -139,9 +139,9 @@ static void ptn5150_irq_work(struct work_struct *work)
 					PTN5150_REG_CC_VBUS_DETECTION_MASK) >>
 					PTN5150_REG_CC_VBUS_DETECTION_SHIFT);
 				if (vbus)
-					gpiod_set_value(info->vbus_gpiod, 0);
+					gpiod_set_value_cansleep(info->vbus_gpiod, 0);
 				else
-					gpiod_set_value(info->vbus_gpiod, 1);
+					gpiod_set_value_cansleep(info->vbus_gpiod, 1);
 
 				extcon_set_state_sync(info->edev,
 						EXTCON_USB_HOST, true);
@@ -157,7 +157,7 @@ static void ptn5150_irq_work(struct work_struct *work)
 					EXTCON_USB_HOST, false);
 			extcon_set_state_sync(info->edev,
 					EXTCON_USB, false);
-			gpiod_set_value(info->vbus_gpiod, 0);
+			gpiod_set_value_cansleep(info->vbus_gpiod, 0);
 		}
 	}
 
