6703f22182abaeaecd99d54b5b882fb7e920f08f
Paul E. McKenney <paulmck@kernel.org>
2020-08-17 16:44:48 -0700

    torture: Add gdb support
    This commit adds a "--gdb" parameter to kvm.sh, which causes
    "CONFIG_DEBUG_INFO=y" to be added to the Kconfig options, "nokaslr"
    to be added to the boot parameters, and "-s -S" to be added to the qemu
    arguments.  Furthermore, the scripting prints messages telling the user
    how to start up gdb for the run in question.
    
    Because of the interactive nature of gdb sessions, only one "--configs"
    scenario is permitted when "--gdb" is specified.  For most torture types,
    this means that a "--configs" argument is required, and that argument
    must specify the single scenario of interest.
    
    The usual cautions about breakpoints and timing apply, for example,
    staring at your gdb prompt for too long will likely get you many
    complaints, including RCU CPU stall warnings.  Omar Sandoval further
    suggests using gdb's "hbreak" command instead of the "break" command on
    systems supporting hardware breakpoints, and further using the "commands"
    option because the resulting non-interactive breakpoints are less likely
    to get you RCU CPU stall warnings.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- tools/testing/selftests/rcutorture/bin/kvm.sh
+++ tools/testing/selftests/rcutorture/bin/kvm.sh
@@ -32,6 +32,9 @@ TORTURE_DEFCONFIG=defconfig
 TORTURE_BOOT_IMAGE=""
 TORTURE_INITRD="$KVM/initrd"; export TORTURE_INITRD
 TORTURE_KCONFIG_ARG=""
+TORTURE_KCONFIG_GDB_ARG=""
+TORTURE_BOOT_GDB_ARG=""
+TORTURE_QEMU_GDB_ARG=""
 TORTURE_KCONFIG_KASAN_ARG=""
 TORTURE_KCONFIG_KCSAN_ARG=""
 TORTURE_KMAKE_ARG=""
@@ -57,6 +60,7 @@ usage () {
 	echo "       --defconfig string"
 	echo "       --dryrun sched|script"
 	echo "       --duration minutes"
+	echo "       --gdb"
 	echo "       --help"
 	echo "       --interactive"
 	echo "       --jitter N [ maxsleep (us) [ maxspin (us) ] ]"
@@ -128,6 +132,11 @@ do
 		checkarg --duration "(minutes)" $# "$2" '^[0-9]*$' '^error'
 		dur=$(($2*60))
 		shift
+		;;
+	--gdb)
+		TORTURE_KCONFIG_GDB_ARG="CONFIG_DEBUG_INFO=y"; export TORTURE_KCONFIG_GDB_ARG
+		TORTURE_BOOT_GDB_ARG="nokaslr"; export TORTURE_BOOT_GDB_ARG
+		TORTURE_QEMU_GDB_ARG="-s -S"; export TORTURE_QEMU_GDB_ARG
 		;;
 	--help|-h)
 		usage
@@ -254,6 +263,15 @@ do
 done
 touch $T/cfgcpu
 configs_derep="`echo $configs_derep | sed -e "s/\<CFLIST\>/$defaultconfigs/g"`"
+if test -n "$TORTURE_KCONFIG_GDB_ARG"
+then
+	if test "`echo $configs_derep | wc -w`" -gt 1
+	then
+		echo "The --config list is: $configs_derep."
+		echo "Only one --config permitted with --gdb, terminating."
+		exit 1
+	fi
+fi
 for CF1 in $configs_derep
 do
 	if test -f "$CONFIGFRAG/$CF1"
@@ -329,6 +347,9 @@ TORTURE_BUILDONLY="$TORTURE_BUILDONLY"; export TORTURE_BUILDONLY
 TORTURE_DEFCONFIG="$TORTURE_DEFCONFIG"; export TORTURE_DEFCONFIG
 TORTURE_INITRD="$TORTURE_INITRD"; export TORTURE_INITRD
 TORTURE_KCONFIG_ARG="$TORTURE_KCONFIG_ARG"; export TORTURE_KCONFIG_ARG
+TORTURE_KCONFIG_GDB_ARG="$TORTURE_KCONFIG_GDB_ARG"; export TORTURE_KCONFIG_GDB_ARG
+TORTURE_BOOT_GDB_ARG="$TORTURE_BOOT_GDB_ARG"; export TORTURE_BOOT_GDB_ARG
+TORTURE_QEMU_GDB_ARG="$TORTURE_QEMU_GDB_ARG"; export TORTURE_QEMU_GDB_ARG
 TORTURE_KCONFIG_KASAN_ARG="$TORTURE_KCONFIG_KASAN_ARG"; export TORTURE_KCONFIG_KASAN_ARG
 TORTURE_KCONFIG_KCSAN_ARG="$TORTURE_KCONFIG_KCSAN_ARG"; export TORTURE_KCONFIG_KCSAN_ARG
 TORTURE_KMAKE_ARG="$TORTURE_KMAKE_ARG"; export TORTURE_KMAKE_ARG
