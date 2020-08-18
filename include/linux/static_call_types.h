2e11e3a0f313e9c10c0f4370512343f8b26f6d23
Josh Poimboeuf <jpoimboe@redhat.com>
2020-08-18 15:57:41 +0200

    static_call: Add basic static call infrastructure
    Static calls are a replacement for global function pointers.  They use
    code patching to allow direct calls to be used instead of indirect
    calls.  They give the flexibility of function pointers, but with
    improved performance.  This is especially important for cases where
    retpolines would otherwise be used, as retpolines can significantly
    impact performance.
    
    The concept and code are an extension of previous work done by Ard
    Biesheuvel and Steven Rostedt:
    
      https://lkml.kernel.org/r/20181005081333.15018-1-ard.biesheuvel@linaro.org
      https://lkml.kernel.org/r/20181006015110.653946300@goodmis.org
    
    There are two implementations, depending on arch support:
    
     1) out-of-line: patched trampolines (CONFIG_HAVE_STATIC_CALL)
     2) basic function pointers
    
    For more details, see the comments in include/linux/static_call.h.
    
    [peterz: simplified interface]
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: https://lore.kernel.org/r/20200818135804.623259796@infradead.org

---
+++ include/linux/static_call_types.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _STATIC_CALL_TYPES_H
+#define _STATIC_CALL_TYPES_H
+
+#include <linux/stringify.h>
+
+#define STATIC_CALL_KEY_PREFIX		__SCK__
+#define STATIC_CALL_KEY(name)		__PASTE(STATIC_CALL_KEY_PREFIX, name)
+
+#define STATIC_CALL_TRAMP_PREFIX	__SCT__
+#define STATIC_CALL_TRAMP_PREFIX_STR	__stringify(STATIC_CALL_TRAMP_PREFIX)
+#define STATIC_CALL_TRAMP(name)		__PASTE(STATIC_CALL_TRAMP_PREFIX, name)
+#define STATIC_CALL_TRAMP_STR(name)	__stringify(STATIC_CALL_TRAMP(name))
+
+#endif /* _STATIC_CALL_TYPES_H */
