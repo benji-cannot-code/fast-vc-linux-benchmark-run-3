80be2e0479b35aa9fb894324fa4f6dc72ee315f7
Josh Poimboeuf <jpoimboe@redhat.com>
2020-08-18 15:57:42 +0200

    static_call: Add inline static call infrastructure
    Add infrastructure for an arch-specific CONFIG_HAVE_STATIC_CALL_INLINE
    option, which is a faster version of CONFIG_HAVE_STATIC_CALL.  At
    runtime, the static call sites are patched directly, rather than using
    the out-of-line trampolines.
    
    Compared to out-of-line static calls, the performance benefits are more
    modest, but still measurable.  Steven Rostedt did some tracepoint
    measurements:
    
      https://lkml.kernel.org/r/20181126155405.72b4f718@gandalf.local.home
    
    This code is heavily inspired by the jump label code (aka "static
    jumps"), as some of the concepts are very similar.
    
    For more details, see the comments in include/linux/static_call.h.
    
    [peterz: simplified interface; merged trampolines]
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: https://lore.kernel.org/r/20200818135804.684334440@infradead.org

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -389,6 +389,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	KEEP(*(__jump_table))						\
 	__stop___jump_table = .;
 
+#define STATIC_CALL_DATA						\
+	. = ALIGN(8);							\
+	__start_static_call_sites = .;					\
+	KEEP(*(.static_call_sites))					\
+	__stop_static_call_sites = .;
+
 /*
  * Allow architectures to handle ro_after_init data on their
  * own by defining an empty RO_AFTER_INIT_DATA.
@@ -399,6 +405,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	__start_ro_after_init = .;					\
 	*(.data..ro_after_init)						\
 	JUMP_TABLE_DATA							\
+	STATIC_CALL_DATA						\
 	__end_ro_after_init = .;
 #endif
 
