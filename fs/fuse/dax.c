430308c1ddc1c2754e65ee0344752491d53c8c40
Stefan Hajnoczi <stefanha@redhat.com>
2020-08-19 18:19:49 -0400

    virtiofs: implement FUSE_INIT map_alignment field
    The device communicates FUSE_SETUPMAPPING/FUSE_REMOVMAPPING alignment
    constraints via the FUST_INIT map_alignment field.  Parse this field and
    ensure our DAX mappings meet the alignment constraints.
    
    We don't actually align anything differently since our mappings are
    already 2MB aligned.  Just check the value when the connection is
    established.  If it becomes necessary to honor arbitrary alignments in
    the future we'll have to adjust how mappings are sized.
    
    The upshot of this commit is that we can be confident that mappings will
    work even when emulating x86 on Power and similar combinations where the
    host page sizes are different.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/dax.c
+++ fs/fuse/dax.c
@@ -10,7 +10,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dax.h>
 #include <linux/pfn_t.h>
 
-/* Default memory range size, 2MB */
+/*
+ * Default memory range size.  A power of 2 so it agrees with common FUSE_INIT
+ * map_alignment values 4KB and 64KB.
+ */
 #define FUSE_DAX_SHIFT	21
 #define FUSE_DAX_SZ	(1 << FUSE_DAX_SHIFT)
 #define FUSE_DAX_PAGES	(FUSE_DAX_SZ / PAGE_SIZE)
@@ -123,4 +126,14 @@ int fuse_dax_conn_alloc(struct fuse_conn *fc, struct dax_device *dax_dev)
 
 	fc->dax = fcd;
 	return 0;
+}
+
+bool fuse_dax_check_alignment(struct fuse_conn *fc, unsigned int map_alignment)
+{
+	if (fc->dax && (map_alignment > FUSE_DAX_SHIFT)) {
+		pr_warn("FUSE: map_alignment %u incompatible with dax mem range size %u\n",
+			map_alignment, FUSE_DAX_SZ);
+		return false;
+	}
+	return true;
 }
