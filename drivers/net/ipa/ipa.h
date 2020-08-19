49b77e5eacc9b1b5ecddbee9e2763f256ccb0b36
Wang Hai <wanghai38@huawei.com>
2020-08-19 10:46:45 +0800

    net: ipa: remove duplicate include
    Remove linux/notifier.h which is included more than once
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ipa/ipa.h
+++ drivers/net/ipa/ipa.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/notifier.h>
 #include <linux/pm_wakeup.h>
-#include <linux/notifier.h>
 
 #include "ipa_version.h"
 #include "gsi.h"
