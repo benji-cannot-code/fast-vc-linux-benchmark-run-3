4a029d540baa3eae2869957837d8db9459f0cb22
Michael Ellerman <mpe@ellerman.id.au>
2020-08-19 11:57:22 +1000

    selftests/powerpc: Include asm/cputable.h from utils.h
    utils.h provides have_hwcap() and have_hwcap2() which check for a
    feature bit. Those bits are defined in asm/cputable.h, so include it
    in utils.h so users of utils.h don't have to do it manually.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20200819015727.1977134-4-mpe@ellerman.id.au

--- tools/testing/selftests/powerpc/include/utils.h
+++ tools/testing/selftests/powerpc/include/utils.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdbool.h>
 #include <linux/auxvec.h>
 #include <linux/perf_event.h>
+#include <asm/cputable.h>
 #include "reg.h"
 
 /* Avoid headaches with PRI?64 - just use %ll? always */
