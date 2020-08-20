e3eda5237cb4461715a5d658e77e47b989af5cdf
He Zhe <zhe.he@windriver.com>
2020-08-20 20:54:40 +0800

    mips/oprofile: Fix fallthrough placement
    We want neither
    "
    include/linux/compiler_attributes.h:201:41: warning: statement will never
    be executed [-Wswitch-unreachable]
      201 | # define fallthrough __attribute__((__fallthrough__))
          |                      ^~~~~~~~~~~~~
    "
    nor
    "
    include/linux/compiler_attributes.h:201:41: warning: attribute
    'fallthrough' not preceding a case label or default label
      201 | # define fallthrough __attribute__((__fallthrough__))
          |                      ^~~~~~~~~~~~~
    "
    
    It's not worth adding one more macro. Let's simply place the fallthrough
    in between the expansions.
    
    Fixes: c9b029903466 ("MIPS: Use fallthrough for arch/mips")
    Cc: stable@vger.kernel.org
    Signed-off-by: He Zhe <zhe.he@windriver.com>
    Reviewed-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/oprofile/op_model_mipsxx.c
+++ arch/mips/oprofile/op_model_mipsxx.c
@@ -246,7 +246,6 @@ static int mipsxx_perfcount_handler(void)
 
 	switch (counters) {
 #define HANDLE_COUNTER(n)						\
-	fallthrough;							\
 	case n + 1:							\
 		control = r_c0_perfctrl ## n();				\
 		counter = r_c0_perfcntr ## n();				\
@@ -257,8 +256,11 @@ static int mipsxx_perfcount_handler(void)
 			handled = IRQ_HANDLED;				\
 		}
 	HANDLE_COUNTER(3)
+	fallthrough;
 	HANDLE_COUNTER(2)
+	fallthrough;
 	HANDLE_COUNTER(1)
+	fallthrough;
 	HANDLE_COUNTER(0)
 	}
 
