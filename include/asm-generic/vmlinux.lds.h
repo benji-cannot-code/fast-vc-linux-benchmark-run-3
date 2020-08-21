aa09066d09cb8360f928621366c78b5e0fec3361
Kees Cook <keescook@chromium.org>
2020-08-21 12:42:43 -0700

    vmlinux.lds.h: Add .gnu.version* to COMMON_DISCARDS
    For vmlinux linking, no architecture uses the .gnu.version* sections,
    so remove it via the COMMON_DISCARDS macro in preparation for adding
    --orphan-handling=warn more widely. This is a work-around for what
    appears to be a bug[1] in ld.bfd which warns for this synthetic section
    even when none is found in input objects, and even when no section is
    emitted for an output object[2].
    
    [1] https://sourceware.org/bugzilla/show_bug.cgi?id=26153
    [2] https://lore.kernel.org/lkml/202006221524.CEB86E036B@keescook/
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Fangrui Song <maskray@google.com>
    Cc: linux-arch@vger.kernel.org
    Link: https://lore.kernel.org/r/20200821194310.3089815-3-keescook@chromium.org

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -958,7 +958,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define COMMON_DISCARDS							\
 	*(.discard)							\
 	*(.discard.*)							\
-	*(.modinfo)
+	*(.modinfo)							\
+	/* ld.bfd warns about .gnu.version* even when not emitted */	\
+	*(.gnu.version*)						\
 
 #define DISCARDS							\
 	/DISCARD/ : {							\
