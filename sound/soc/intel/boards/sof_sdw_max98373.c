b92ec939e2474af15a8b151d1328c3fb2e802634
Bard Liao <yung-chuan.liao@linux.intel.com>
2020-08-21 14:56:03 -0500

    ASoC: Intel: sof_sdw: clean-up inclusion of header files
    "struct snd_soc_dapm_widget" and "struct snd_kcontrol_new" are used in most
    of these .c files. Adding the header files to prevent from depending on
    <sound/soc.h>
    
    Reported-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
    Reviewed-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
    Reviewed-by: Rander Wang <rander.wang@linux.intel.com>
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20200821195603.215535-17-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/boards/sof_sdw_max98373.c
+++ sound/soc/intel/boards/sof_sdw_max98373.c
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/device.h>
 #include <linux/errno.h>
+#include <sound/control.h>
 #include <sound/soc.h>
 #include <sound/soc-acpi.h>
+#include <sound/soc-dapm.h>
 #include "sof_sdw_common.h"
 #include "sof_maxim_common.h"
 
