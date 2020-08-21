644c25c3c315c42c96f49d5ad98d5b6c7f013111
Peter Chen <peter.chen@nxp.com>
2020-08-21 10:55:48 +0800

    usb: dwc3: allocate gadget structure dynamically
    The current code uses commit fac323471df6 ("usb: udc: allow adding
    and removing the same gadget device") as the workaround to let
    the gadget device is re-used, but it is not allowed from driver
    core point. In this commit, we allocate gadget structure dynamically,
    and free it at its release function. Since the gadget device's
    driver_data has already occupied by usb_composite_dev structure, we have
    to use gadget device's platform data to store dwc3 structure.
    
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Alan Stern <stern@rowland.harvard.edu>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- drivers/usb/dwc3/gadget.h
+++ drivers/usb/dwc3/gadget.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct dwc3;
 #define to_dwc3_ep(ep)		(container_of(ep, struct dwc3_ep, endpoint))
-#define gadget_to_dwc(g)	(container_of(g, struct dwc3, gadget))
+#define gadget_to_dwc(g)	(dev_get_platdata(&g->dev))
 
 /* DEPCFG parameter 1 */
 #define DWC3_DEPCFG_INT_NUM(n)		(((n) & 0x1f) << 0)
