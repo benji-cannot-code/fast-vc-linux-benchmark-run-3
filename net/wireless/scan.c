c167cc30eb1f60311e15227e17669fa19a6d98b9
Randy Dunlap <rdunlap@infradead.org>
2020-08-22 16:19:51 -0700

    net: wireless: scan.c: delete or fix duplicated words
    Drop repeated word "stored".
    Change "is is" to "it is".
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Cc: Johannes Berg <johannes.berg@intel.com>
    Cc: Kalle Valo <kvalo@codeaurora.org>
    Cc: linux-wireless@vger.kernel.org
    Link: https://lore.kernel.org/r/20200822231953.465-6-rdunlap@infradead.org
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- net/wireless/scan.c
+++ net/wireless/scan.c
@@ -56,7 +56,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Also note that the hidden_beacon_bss pointer is only relevant
  * if the driver uses something other than the IEs, e.g. private
- * data stored stored in the BSS struct, since the beacon IEs are
+ * data stored in the BSS struct, since the beacon IEs are
  * also linked into the probe response struct.
  */
 
@@ -1489,7 +1489,7 @@ static const struct element
 					 ielen - (mbssid_end - ie));
 
 	/*
-	 * If is is not the last subelement in current MBSSID IE or there isn't
+	 * If it is not the last subelement in current MBSSID IE or there isn't
 	 * a next MBSSID IE - profile is complete.
 	*/
 	if ((sub_elem->data + sub_elem->datalen < mbssid_end - 1) ||
