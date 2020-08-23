e59042bda3bd3efa49e921de5f8657c4016a7177
Felix Fietkau <nbd@nbd.name>
2020-08-23 18:21:18 +0200

    mt76: remove swq from struct mt76_sw_queue
    Since txq selection was moved to mac80211, it is no longer used
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt7615/dma.c
+++ drivers/net/wireless/mediatek/mt76/mt7615/dma.c
@@ -27,7 +27,6 @@ mt7615_init_tx_queue(struct mt7615_dev *dev, struct mt76_sw_queue *q,
 	if (err < 0)
 		return err;
 
-	INIT_LIST_HEAD(&q->swq);
 	q->q = hwq;
 
 	return 0;
@@ -85,7 +84,6 @@ mt7615_init_tx_queues(struct mt7615_dev *dev)
 
 	for (i = 1; i < MT_TXQ_MCU; i++) {
 		q = &dev->mt76.q_tx[i];
-		INIT_LIST_HEAD(&q->swq);
 		q->q = dev->mt76.q_tx[0].q;
 	}
 
