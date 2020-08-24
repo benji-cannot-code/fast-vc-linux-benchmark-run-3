07b14d9ac0a98520e9a22d1a56bec5e62a2c9cc7
Thomas Bogendoerfer <tsbogend@alpha.franken.de>
2020-08-24 18:32:49 +0200

    MIPS: Convert R10000_LLSC_WAR info a config option
    Use a new config option to enabel R1000_LLSC workaound and remove
    define from different war.h files.
    
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/include/asm/mach-ip22/war.h
+++ arch/mips/include/asm/mach-ip22/war.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define BCM1250_M3_WAR			0
 #define SIBYTE_1956_WAR			0
-#define R10000_LLSC_WAR			0
 #define MIPS34K_MISSED_ITLB_WAR		0
 
 #endif /* __ASM_MIPS_MACH_IP22_WAR_H */
--- arch/mips/include/asm/mach-ip30/war.h
+++ arch/mips/include/asm/mach-ip30/war.h
@@ -8,11 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define BCM1250_M3_WAR			0
 #define SIBYTE_1956_WAR			0
-#ifdef CONFIG_CPU_R10000
-#define R10000_LLSC_WAR			1
-#else
-#define R10000_LLSC_WAR			0
-#endif
 #define MIPS34K_MISSED_ITLB_WAR		0
 
 #endif /* __ASM_MIPS_MACH_IP30_WAR_H */
