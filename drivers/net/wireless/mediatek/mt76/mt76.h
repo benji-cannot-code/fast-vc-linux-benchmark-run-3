6fc62e22927b120a29a094d0944e9bd6763c699c
Felix Fietkau <nbd@nbd.name>
2020-08-24 09:41:27 +0200

    mt76: mt76x02: tune tx ring size
    Increase data queue size to improve performance.
    Reduce PS/mgmt queue size
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/mt76.h
+++ drivers/net/wireless/mediatek/mt76/mt76.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "util.h"
 #include "testmode.h"
 
-#define MT_TX_RING_SIZE     256
 #define MT_MCU_RING_SIZE    32
 #define MT_RX_BUF_SIZE      2048
 #define MT_SKB_HEAD_LEN     128
