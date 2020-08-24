661546dd1af88335328a5dde8a6343287496ace6
Gustavo A. R. Silva <gustavoars@kernel.org>
2020-08-24 15:36:14 -0500

    lib: Revert use of fallthrough pseudo-keyword in lib/
    The following build error for powerpc64 was reported by Nathan Chancellor:
    
      "$ scripts/config --file arch/powerpc/configs/powernv_defconfig -e KERNEL_XZ
    
       $ make -skj"$(nproc)" ARCH=powerpc CROSS_COMPILE=powerpc64le-linux- distclean powernv_defconfig zImage
       ...
       In file included from arch/powerpc/boot/../../../lib/decompress_unxz.c:234,
                        from arch/powerpc/boot/decompress.c:38:
       arch/powerpc/boot/../../../lib/xz/xz_dec_stream.c: In function 'dec_main':
       arch/powerpc/boot/../../../lib/xz/xz_dec_stream.c:586:4: error: 'fallthrough' undeclared (first use in this function)
         586 |    fallthrough;
             |    ^~~~~~~~~~~
    
       This will end up affecting distribution configurations such as Debian
       and OpenSUSE according to my testing. I am not sure what the solution
       is, the PowerPC wrapper does not set -D__KERNEL__ so I am not sure
       that compiler_attributes.h can be safely included."
    
    In order to avoid these sort of problems, it seems that the best
    solution is to use /* fall through */ comments instead of the
    fallthrough pseudo-keyword macro in lib/, for now.
    
    Reported-by: Nathan Chancellor <natechancellor@gmail.com>
    Fixes: df561f6688fe ("treewide: Use fallthrough pseudo-keyword")
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Reviewed-and-tested-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/bootconfig.c
+++ lib/bootconfig.c
@@ -818,7 +818,7 @@ int __init xbc_init(char *buf, const char **emsg, int *epos)
 							q - 2);
 				break;
 			}
-			fallthrough;
+			/* fall through */
 		case '=':
 			ret = xbc_parse_kv(&p, q, c);
 			break;
@@ -827,7 +827,7 @@ int __init xbc_init(char *buf, const char **emsg, int *epos)
 			break;
 		case '#':
 			q = skip_comment(q);
-			fallthrough;
+			/* fall through */
 		case ';':
 		case '\n':
 			ret = xbc_parse_key(&p, q);
--- lib/ts_fsm.c
+++ lib/ts_fsm.c
@@ -194,7 +194,7 @@ startover:
 				TOKEN_MISMATCH();
 
 			block_idx++;
-			fallthrough;
+			/* fall through */
 
 		case TS_FSM_ANY:
 			if (next == NULL)
