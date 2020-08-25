40e319086c212724f212a0392382f7c3e6595733
Julien Thierry <jthierry@redhat.com>
2020-08-25 13:47:39 +0100

    objtool: Move object file loading out of check()
    Structure objtool_file can be used by different subcommands. In fact
    it already is, by check and orc.
    
    Provide a function that allows to initialize objtool_file, that builtin
    can call, without relying on check to do the correct setup for them and
    explicitly hand the objtool_file to them.
    
    Reviewed-by: Miroslav Benes <mbenes@suse.cz>
    Signed-off-by: Julien Thierry <jthierry@redhat.com>
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>

--- tools/objtool/weak.c
+++ tools/objtool/weak.c
@@ -18,9 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	return ENOSYS;							\
 })
 
-const char __weak *objname;
-
-int __weak check(const char *_objname, bool orc)
+int __weak check(struct objtool_file *file, bool orc)
 {
 	UNSUPPORTED("check subcommand");
 }
