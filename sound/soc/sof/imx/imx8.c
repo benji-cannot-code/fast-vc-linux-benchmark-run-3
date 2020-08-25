a6c5fade6d2f46b062361a5a3a4b202b130557ac
Iulian Olaru <iulianolaru249@yahoo.com>
2020-08-25 16:50:40 -0700

    ASoC: SOF: sof-of-dev: Add .arch_ops field
    Add .arch_ops field in the sof_imx8x_ops structure.
    The inclusion of this field will allow the usage of functions from
    sof/core.c in order to print debug information such as the registers and
    a stack dump in case of a firmware ops.
    
    The SND_SOC_SOF_XTENSA is added in the imx/Kconfig file so the compilation
    is successful.
    
    Signed-off-by: Iulian Olaru <iulianolaru249@yahoo.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@gmail.com>
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Link: https://lore.kernel.org/r/20200825235040.1586478-8-ranjani.sridharan@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sof/imx/imx8.c
+++ sound/soc/sof/imx/imx8.c
@@ -425,6 +425,9 @@ struct snd_sof_dsp_ops sof_imx8_ops = {
 	/* firmware loading */
 	.load_firmware	= snd_sof_load_firmware_memcpy,
 
+	/* Firmware ops */
+	.arch_ops = &sof_xtensa_arch_ops,
+
 	/* DAI drivers */
 	.drv = imx8_dai,
 	.num_drv = ARRAY_SIZE(imx8_dai),
@@ -465,6 +468,9 @@ struct snd_sof_dsp_ops sof_imx8x_ops = {
 	/* firmware loading */
 	.load_firmware	= snd_sof_load_firmware_memcpy,
 
+	/* Firmware ops */
+	.arch_ops = &sof_xtensa_arch_ops,
+
 	/* DAI drivers */
 	.drv = imx8_dai,
 	.num_drv = ARRAY_SIZE(imx8_dai),
@@ -478,4 +484,5 @@ struct snd_sof_dsp_ops sof_imx8x_ops = {
 };
 EXPORT_SYMBOL(sof_imx8x_ops);
 
+MODULE_IMPORT_NS(SND_SOC_SOF_XTENSA);
 MODULE_LICENSE("Dual BSD/GPL");
