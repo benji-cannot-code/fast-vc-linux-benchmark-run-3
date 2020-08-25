942cbd9f8c3164c2e1ef2712eb13e4d13c842b65
Julien Thierry <jthierry@redhat.com>
2020-08-25 13:47:42 +0100

    objtool: Define 'struct orc_entry' only when needed
    Implementation of ORC requires some definitions that are currently
    provided by the target architecture headers. Do not depend on these
    definitions when the orc subcommand is not implemented.
    
    This avoid requiring arches with no orc implementation to provide dummy
    orc definitions.
    
    Signed-off-by: Julien Thierry <jthierry@redhat.com>
    Reviewed-by: Miroslav Benes <mbenes@suse.cz>
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>

--- tools/objtool/arch.h
+++ tools/objtool/arch.h
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "objtool.h"
 #include "cfi.h"
 
+#ifdef INSN_USE_ORC
 #include <asm/orc_types.h>
+#endif
 
 enum insn_type {
 	INSN_JUMP_CONDITIONAL,
