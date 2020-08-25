55fa9b704e93b75a80782fad4ad40cdd1ed08483
Julien Thierry <jthierry@redhat.com>
2020-08-25 13:47:40 +0100

    objtool: Move ORC logic out of check()
    Now that the objtool_file can be obtained outside of the check function,
    orc generation builtin no longer requires check to explicitly call its
    orc related functions.
    
    Signed-off-by: Julien Thierry <jthierry@redhat.com>
    Reviewed-by: Miroslav Benes <mbenes@suse.cz>
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>

--- tools/objtool/builtin-check.c
+++ tools/objtool/builtin-check.c
@@ -43,6 +43,7 @@ int cmd_check(int argc, const char **argv)
 {
 	const char *objname, *s;
 	struct objtool_file *file;
+	int ret;
 
 	argc = parse_options(argc, argv, check_options, check_usage, 0);
 
@@ -59,5 +60,12 @@ int cmd_check(int argc, const char **argv)
 	if (!file)
 		return 1;
 
-	return check(file, false);
+	ret = check(file);
+	if (ret)
+		return ret;
+
+	if (file->elf->changed)
+		return elf_write(file->elf);
+
+	return 0;
 }
--- tools/objtool/weak.c
+++ tools/objtool/weak.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	return ENOSYS;							\
 })
 
-int __weak check(struct objtool_file *file, bool orc)
+int __weak check(struct objtool_file *file)
 {
 	UNSUPPORTED("check subcommand");
 }
