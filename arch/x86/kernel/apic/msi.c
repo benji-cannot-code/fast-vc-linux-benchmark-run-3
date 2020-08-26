cc7c0a700088e2b63303f18261516d90933a6439
Thomas Gleixner <tglx@linutronix.de>
2020-08-26 13:17:01 +0200

    x86/pci: Set default irq domain in pcibios_add_device()
    Now that interrupt remapping sets the irqdomain pointer when a PCI device
    is added it's possible to store the default irq domain in the device struct
    in pcibios_add_device().
    
    If the bus to which a device is connected has an irq domain associated then
    this domain is used otherwise the default domain (PCI/MSI native or XEN
    PCI/MSI) is used. Using the bus domain ensures that special MSI bus domains
    like VMD work.
    
    This makes XEN and the non-remapped native case work solely based on the
    irq domain pointer in struct device for PCI/MSI and allows to remove the
    arch fallback and make most of the x86_msi ops private to XEN in the next
    steps.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20200826112333.900423047@linutronix.de

--- arch/x86/kernel/apic/msi.c
+++ arch/x86/kernel/apic/msi.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/apic.h>
 #include <asm/irq_remapping.h>
 
-static struct irq_domain *x86_pci_msi_default_domain __ro_after_init;
+struct irq_domain *x86_pci_msi_default_domain __ro_after_init;
 
 static void __irq_msi_compose_msg(struct irq_cfg *cfg, struct msi_msg *msg)
 {
