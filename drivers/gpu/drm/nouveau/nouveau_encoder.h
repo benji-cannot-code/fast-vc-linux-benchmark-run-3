265272b95b6906a9e86f11475bf03bfadbd8edcc
Lyude Paul <lyude@redhat.com>
2020-08-26 14:24:38 -0400

    drm/nouveau/kms/nv50-: Remove open-coded drm_dp_read_desc()
    Noticed this while going through our DP code - we use an open-coded
    version of drm_dp_read_desc() instead of just using the helper, so
    change that. This will also let us use quirks in the future if we end up
    needing them.
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Reviewed-by: Ben Skeggs <bskeggs@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200826182456.322681-3-lyude@redhat.com

--- drivers/gpu/drm/nouveau/nouveau_encoder.h
+++ drivers/gpu/drm/nouveau/nouveau_encoder.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_dp_mst_helper.h>
 #include "dispnv04/disp.h"
 struct nv50_head_atom;
+struct nouveau_connector;
 
 #define NV_DPMS_CLEARED 0x80
 
@@ -65,6 +66,7 @@ struct nouveau_encoder {
 			struct nv50_mstm *mstm;
 			int link_nr;
 			int link_bw;
+			struct drm_dp_desc desc;
 		} dp;
 	};
 
@@ -105,7 +107,7 @@ enum nouveau_dp_status {
 	NOUVEAU_DP_MST,
 };
 
-int nouveau_dp_detect(struct nouveau_encoder *);
+int nouveau_dp_detect(struct nouveau_connector *, struct nouveau_encoder *);
 enum drm_mode_status nv50_dp_mode_valid(struct drm_connector *,
 					struct nouveau_encoder *,
 					const struct drm_display_mode *,
