9aad9f2892ddd6cf991b665a36fd376ebca56f8a
Ido Schimmel <idosch@nvidia.com>
2020-08-26 19:48:53 +0300

    ipv4: nexthop: Remove unnecessary rtnl_dereference()
    The pointer is not RCU protected, so remove the unnecessary
    rtnl_dereference(). This suppresses the following warning:
    
    net/ipv4/nexthop.c:1101:24: error: incompatible types in comparison expression (different address spaces):
    net/ipv4/nexthop.c:1101:24:    struct rb_node [noderef] __rcu *
    net/ipv4/nexthop.c:1101:24:    struct rb_node *
    
    Signed-off-by: Ido Schimmel <idosch@nvidia.com>
    Reviewed-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/nexthop.c
+++ net/ipv4/nexthop.c
@@ -1099,7 +1099,7 @@ static int insert_nexthop(struct net *net, struct nexthop *new_nh,
 	while (1) {
 		struct nexthop *nh;
 
-		next = rtnl_dereference(*pp);
+		next = *pp;
 		if (!next)
 			break;
 
