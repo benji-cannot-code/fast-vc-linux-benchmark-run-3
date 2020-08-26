6294e11a0814b9f122b9d1b500b7b42ed7e3aa06
Evan Quan <evan.quan@amd.com>
2020-08-26 18:19:28 +0800

    drm/amd/pm: apply the CDR workarounds only with some specific UMC firmwares(V2)
    And different workaround will be applied based on hybrid cdr bit.
    
    V2: add pmfw version guard to make sure the new workaround applied only
        with pmfw >= 42.53.0
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/inc/smu_v11_0_ppsmc.h
+++ drivers/gpu/drm/amd/pm/inc/smu_v11_0_ppsmc.h
@@ -129,7 +129,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PPSMC_MSG_SetDriverDummyTableDramAddrHigh 0x4E
 #define PPSMC_MSG_SetDriverDummyTableDramAddrLow  0x4F
 
-#define PPSMC_Message_Count                      0x50
+#define PPSMC_MSG_GetUMCFWWA                     0x50
+
+#define PPSMC_Message_Count                      0x51
 
 typedef uint32_t PPSMC_Result;
 typedef uint32_t PPSMC_Msg;
