7bab4d34afd212adb2c32e46fbc7702761f46cf0
Stanimir Varbanov <stanimir.varbanov@linaro.org>
2020-08-26 14:20:13 +0200

    media: venus: hfi_plat: Add hfi platform buffers ops
    >From Venus v6 and beyond the buffer size and count have to be
    calculated in the v4l2 driver instead of getting them from
    firmware.
    
    Signed-off-by: Stanimir Varbanov <stanimir.varbanov@linaro.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/platform/qcom/venus/hfi_platform.h
+++ drivers/media/platform/qcom/venus/hfi_platform.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/videodev2.h>
 
 #include "hfi.h"
+#include "hfi_plat_bufs.h"
 #include "hfi_helper.h"
 
 #define MAX_PLANES		4
@@ -51,6 +52,8 @@ struct hfi_platform {
 	void (*codecs)(u32 *enc_codecs, u32 *dec_codecs, u32 *count);
 	const struct hfi_plat_caps *(*capabilities)(unsigned int *entries);
 	u8 (*num_vpp_pipes)(void);
+	int (*bufreq)(struct hfi_plat_buffers_params *params, u32 session_type,
+		      u32 buftype, struct hfi_buffer_requirements *bufreq);
 };
 
 extern const struct hfi_platform hfi_plat_v4;
