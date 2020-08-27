f018f3ad6a156f11686ca6ff191658b791b168bb
Huang Rui <ray.huang@amd.com>
2020-08-27 11:38:13 -0400

    drm/amd/powerplay: partially enable swsmu for vangogh
    This patch is to partially enable swSMU for vangogh for the moment.
    
    Signed-off-by: Huang Rui <ray.huang@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Aaron Liu <aaron.liu@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
+++ drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
@@ -467,6 +467,9 @@ static int smu_late_init(void *handle)
 	struct smu_context *smu = &adev->smu;
 	int ret = 0;
 
+	if (adev->asic_type == CHIP_VANGOGH)
+		return 0;
+
 	if (!smu->pm_enabled)
 		return 0;
 
@@ -1104,6 +1107,9 @@ static int smu_hw_init(void *handle)
 		smu_dpm_set_jpeg_enable(smu, true);
 		smu_set_gfx_cgpg(&adev->smu, true);
 	}
+
+	if (adev->asic_type == CHIP_VANGOGH)
+		return 0;
 
 	if (!smu->pm_enabled)
 		return 0;
