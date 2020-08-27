248168b40718c656248ecbd3f90784ea5286db2e
Miquel Raynal <miquel.raynal@bootlin.com>
2020-08-27 10:52:04 +0200

    mtd: rawnand: Make use of the ECC framework
    Just enable the ECC framework with raw NAND so that we can drop, one
    by one, all the unnecessary/redundant definitions.
    
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Link: https://lore.kernel.org/linux-mtd/20200827085208.16276-17-miquel.raynal@bootlin.com

--- drivers/mtd/nand/raw/nand_base.c
+++ drivers/mtd/nand/raw/nand_base.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/types.h>
 #include <linux/mtd/mtd.h>
+#include <linux/mtd/nand.h>
 #include <linux/mtd/nand_ecc.h>
 #include <linux/mtd/nand_bch.h>
 #include <linux/interrupt.h>
