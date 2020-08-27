3cd085b1e7e35856f74262b35506bc01fcad04fc
Barry Song <song.bao.hua@hisilicon.com>
2020-08-27 21:29:56 +1200

    iommu/arm-smmu-v3: replace module_param_named by module_param for disable_bypass
    Just use module_param() - going out of the way to specify a "different"
    name that's identical to the variable name is silly.
    
    Signed-off-by: Barry Song <song.bao.hua@hisilicon.com>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>
    Link: https://lore.kernel.org/r/20200827092957.22500-3-song.bao.hua@hisilicon.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -415,7 +415,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MSI_IOVA_LENGTH			0x100000
 
 static bool disable_bypass = 1;
-module_param_named(disable_bypass, disable_bypass, bool, 0444);
+module_param(disable_bypass, bool, 0444);
 MODULE_PARM_DESC(disable_bypass,
 	"Disable bypass streams such that incoming transactions from devices that are not attached to an iommu domain will report an abort back to the device and will not be allowed to pass through the SMMU.");
 
