addf432897e8960689612753ccac4e0d684dd9be
Sakari Ailus <sakari.ailus@linux.intel.com>
2020-08-28 08:24:18 +0200

    media: ccs-pll: Check for derating and overrating, support non-derating sensors
    Some sensors support derating (VT domain speed faster than OP) or
    overrating (VT domain speed slower than OP). While this was supported for
    the driver, the hardware support for the feature was never verified. Do
    that now, and for those devices without that support, VT and OP speeds
    have to match.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/i2c/ccs-pll.h
+++ drivers/media/i2c/ccs-pll.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CCS_PLL_FLAG_LINK_DECOUPLED				BIT(3)
 #define CCS_PLL_FLAG_EXT_IP_PLL_DIVIDER				BIT(4)
 #define CCS_PLL_FLAG_FLEXIBLE_OP_PIX_CLK_DIV			BIT(5)
+#define CCS_PLL_FLAG_FIFO_DERATING				BIT(6)
+#define CCS_PLL_FLAG_FIFO_OVERRATING				BIT(7)
 
 /**
  * struct ccs_pll_branch_fr - CCS PLL configuration (front)
