c7e5b66755c73360179bf6019b36a85030d1d1bd
Julien Grall <julien.grall@arm.com>
2020-08-28 19:11:52 +0100

    arm64/sve: Implement a helper to flush SVE registers
    Introduce a new helper that will zero all SVE registers but the first
    128-bits of each vector. This will be used by subsequent patches to
    avoid costly store/maipulate/reload sequences in places like do_sve_acc().
    
    Signed-off-by: Julien Grall <julien.grall@arm.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Reviewed-by: Dave Martin <Dave.Martin@arm.com>
    Link: https://lore.kernel.org/r/20200828181155.17745-6-broonie@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/fpsimdmacros.h
+++ arch/arm64/include/asm/fpsimdmacros.h
@@ -165,6 +165,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		| ((\np) << 5)
 .endm
 
+/* PFALSE P\np.B */
+.macro _sve_pfalse np
+	_sve_check_preg \np
+	.inst	0x2518e400			\
+		| (\np)
+.endm
+
 .macro __for from:req, to:req
 	.if (\from) == (\to)
 		_for__body %\from
@@ -197,6 +204,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		b.eq		921f
 		msr_s		SYS_ZCR_EL1, \xtmp2	//self-synchronising
 921:
+.endm
+
+/* Preserve the first 128-bits of Znz and zero the rest. */
+.macro _sve_flush_z nz
+	_sve_check_zreg \nz
+	mov	v\nz\().16b, v\nz\().16b
+.endm
+
+.macro sve_flush
+ _for n, 0, 31, _sve_flush_z	\n
+ _for n, 0, 15, _sve_pfalse	\n
+		_sve_wrffr	0
 .endm
 
 .macro sve_save nxbase, xpfpsr, nxtmp
