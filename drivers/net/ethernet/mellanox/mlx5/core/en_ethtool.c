aa9a1abc8c47a94627643486d839a539cc4b58aa
Magnus Karlsson <magnus.karlsson@intel.com>
2020-08-28 10:26:15 +0200

    xsk: i40e: ice: ixgbe: mlx5: Pass buffer pool to driver instead of umem
    Replace the explicit umem reference passed to the driver in AF_XDP
    zero-copy mode with the buffer pool instead. This in preparation for
    extending the functionality of the zero-copy mode so that umems can be
    shared between queues on the same netdev and also between netdevs. In
    this commit, only an umem reference has been added to the buffer pool
    struct. But later commits will add other entities to it. These are
    going to be entities that are different between different queue ids
    and netdevs even though the umem is shared between them.
    
    Signed-off-by: Magnus Karlsson <magnus.karlsson@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Björn Töpel <bjorn.topel@intel.com>
    Link: https://lore.kernel.org/bpf/1598603189-32145-2-git-send-email-magnus.karlsson@intel.com

--- drivers/net/ethernet/mellanox/mlx5/core/en_ethtool.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_ethtool.c
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "en.h"
 #include "en/port.h"
-#include "en/xsk/umem.h"
+#include "en/xsk/pool.h"
 #include "lib/clock.h"
 
 void mlx5e_ethtool_get_drvinfo(struct mlx5e_priv *priv,
