439ad6c23e4c402b75b4b4a64172f88f7f523faa
Randy Dunlap <rdunlap@infradead.org>
2020-08-28 17:01:26 -0700

    ia64: fix min_low_pfn/max_low_pfn build errors
    Fix min_low_pfn/max_low_pfn build errors for arch/ia64/: (e.g.)
    
     ERROR: "max_low_pfn" [drivers/rpmsg/virtio_rpmsg_bus.ko] undefined!
     ERROR: "min_low_pfn" [drivers/rpmsg/virtio_rpmsg_bus.ko] undefined!
     ERROR: "min_low_pfn" [drivers/hwtracing/intel_th/intel_th_msu.ko] undefined!
     ERROR: "max_low_pfn" [drivers/hwtracing/intel_th/intel_th_msu.ko] undefined!
     ERROR: "min_low_pfn" [drivers/crypto/cavium/nitrox/n5pf.ko] undefined!
     ERROR: "max_low_pfn" [drivers/crypto/cavium/nitrox/n5pf.ko] undefined!
     ERROR: "max_low_pfn" [drivers/md/dm-integrity.ko] undefined!
     ERROR: "min_low_pfn" [drivers/md/dm-integrity.ko] undefined!
     ERROR: "max_low_pfn" [crypto/tcrypt.ko] undefined!
     ERROR: "min_low_pfn" [crypto/tcrypt.ko] undefined!
     ERROR: "min_low_pfn" [security/keys/encrypted-keys/encrypted-keys.ko] undefined!
     ERROR: "max_low_pfn" [security/keys/encrypted-keys/encrypted-keys.ko] undefined!
     ERROR: "min_low_pfn" [arch/ia64/kernel/mca_recovery.ko] undefined!
     ERROR: "max_low_pfn" [arch/ia64/kernel/mca_recovery.ko] undefined!
    
    David suggested just exporting min_low_pfn & max_low_pfn in
    mm/memblock.c:
    https://lore.kernel.org/lkml/alpine.DEB.2.22.394.2006291911220.1118534@chino.kir.corp.google.com/
    
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Acked-by: David Rientjes <rientjes@google.com>
    Acked-by: Tony Luck <tony.luck@intel.com>
    Cc: linux-mm@kvack.org
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Mike Rapoport <rppt@linux.ibm.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Fenghua Yu <fenghua.yu@intel.com>
    Cc: linux-ia64@vger.kernel.org
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>

--- arch/ia64/kernel/ia64_ksyms.c
+++ arch/ia64/kernel/ia64_ksyms.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Architecture-specific kernel symbols
  */
 
-#ifdef CONFIG_VIRTUAL_MEM_MAP
+#if defined(CONFIG_VIRTUAL_MEM_MAP) || defined(CONFIG_DISCONTIGMEM)
 #include <linux/compiler.h>
 #include <linux/export.h>
 #include <linux/memblock.h>
