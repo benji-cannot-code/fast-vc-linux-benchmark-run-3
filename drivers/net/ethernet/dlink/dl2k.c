171789413dac40863926e12fb92b86fc35a5c8d4
YueHaibing <yuehaibing@huawei.com>
2020-08-29 19:56:23 +0800

    net: dl2k: Remove unused macro DRV_NAME
    There is no caller in tree any more.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/dlink/dl2k.c
+++ drivers/net/ethernet/dlink/dl2k.c
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 */
 
-#define DRV_NAME	"DL2000/TC902x-based linux driver"
 #include "dl2k.h"
 #include <linux/dma-mapping.h>
 
