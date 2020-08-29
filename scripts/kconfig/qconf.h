248a13958c52eb438d9fe8ae14aed51b80af4560
Masahiro Yamada <masahiroy@kernel.org>
2020-08-29 17:14:13 +0900

    kconfig: qconf: remove ConfigLineEdit class
    Now that "int", "hex", "string" menus are edited in-place, this class
    is no longer needed.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>

--- scripts/kconfig/qconf.h
+++ scripts/kconfig/qconf.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 class ConfigView;
 class ConfigList;
 class ConfigItem;
-class ConfigLineEdit;
 class ConfigMainWindow;
 
 class ConfigSettings : public QSettings {
@@ -188,22 +187,6 @@ public:
 			  const QModelIndex &index) const override;
 };
 
-class ConfigLineEdit : public QLineEdit {
-	Q_OBJECT
-	typedef class QLineEdit Parent;
-public:
-	ConfigLineEdit(ConfigView* parent);
-	ConfigView* parent(void) const
-	{
-		return (ConfigView*)Parent::parent();
-	}
-	void show(ConfigItem *i);
-	void keyPressEvent(QKeyEvent *e);
-
-public:
-	ConfigItem *item;
-};
-
 class ConfigView : public QWidget {
 	Q_OBJECT
 	typedef class QWidget Parent;
@@ -220,7 +203,6 @@ signals:
 	void showRangeChanged(bool);
 public:
 	ConfigList* list;
-	ConfigLineEdit* lineEdit;
 };
 
 class ConfigInfoView : public QTextBrowser {
