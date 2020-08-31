5d7e4d44d75412feaac5f0eba1b033427a1bd087
Zong Li <zong.li@sifive.com>
2020-08-31 15:33:49 +0800

    riscv: Define AT_VECTOR_SIZE_ARCH for ARCH_DLINFO
    AT_VECTOR_SIZE_ARCH should be defined with the maximum number of
    NEW_AUX_ENT entries that ARCH_DLINFO can contain, but it wasn't defined
    for RISC-V at all even though ARCH_DLINFO will contain one NEW_AUX_ENT
    for the VDSO address.
    
    Signed-off-by: Zong Li <zong.li@sifive.com>
    Reviewed-by: Palmer Dabbelt <palmerdabbelt@google.com>
    Reviewed-by: Pekka Enberg <penberg@kernel.org>
    Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>

--- arch/riscv/include/uapi/asm/auxvec.h
+++ arch/riscv/include/uapi/asm/auxvec.h
@@ -11,4 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* vDSO location */
 #define AT_SYSINFO_EHDR 33
 
+/* entries in ARCH_DLINFO */
+#define AT_VECTOR_SIZE_ARCH	1
+
 #endif /* _UAPI_ASM_RISCV_AUXVEC_H */
