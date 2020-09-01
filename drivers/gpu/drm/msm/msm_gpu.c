a2be8e73c3e2b64e9cbfea870561e8eeefea3aa7
Rob Clark <robdclark@chromium.org>
2020-09-01 08:41:56 -0700

    drm/msm/gpu: Add suspend/resume tracepoints
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/msm_gpu.c
+++ drivers/gpu/drm/msm/msm_gpu.c
@@ -203,6 +203,7 @@ int msm_gpu_pm_resume(struct msm_gpu *gpu)
 	int ret;
 
 	DBG("%s", gpu->name);
+	trace_msm_gpu_resume(0);
 
 	ret = enable_pwrrail(gpu);
 	if (ret)
@@ -228,6 +229,7 @@ int msm_gpu_pm_suspend(struct msm_gpu *gpu)
 	int ret;
 
 	DBG("%s", gpu->name);
+	trace_msm_gpu_suspend(0);
 
 	devfreq_suspend_device(gpu->devfreq.devfreq);
 
