aff8084acc605f9f18d056211f00885b525cdbaa
Shengjiu Wang <shengjiu.wang@nxp.com>
2020-09-01 19:01:08 +0800

    ASoC: fsl_sai: Support multiple data channel enable bits
    One data channel is one data line. From imx7ulp, the SAI IP is
    enhanced to support multiple data channels.
    
    If there is only two channels input and slots is 2, then enable one
    data channel is enough for data transfer. So enable the TCE/RCE and
    transmit/receive mask register according to the input channels and
    slots configuration.
    
    Move the data channel enablement from startup() to hw_params().
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    Acked-by: Nicolin Chen <nicoleotsuka@gmail.com>
    Link: https://lore.kernel.org/r/1598958068-10552-1-git-send-email-shengjiu.wang@nxp.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/fsl/fsl_sai.h
+++ sound/soc/fsl/fsl_sai.h
@@ -110,7 +110,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FSL_SAI_CR2_DIV_MASK	0xff
 
 /* SAI Transmit and Receive Configuration 3 Register */
-#define FSL_SAI_CR3_TRCE	BIT(16)
+#define FSL_SAI_CR3_TRCE(x)     ((x) << 16)
 #define FSL_SAI_CR3_TRCE_MASK	GENMASK(23, 16)
 #define FSL_SAI_CR3_WDFL(x)	(x)
 #define FSL_SAI_CR3_WDFL_MASK	0x1f
