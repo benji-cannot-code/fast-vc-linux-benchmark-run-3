a6e3c0755d1ae4d55738a27d2d93e618ce339288
Douglas Anderson <dianders@chromium.org>
2020-09-03 16:24:38 -0700

    mmc: Set PROBE_PREFER_ASYNCHRONOUS for drivers that existed in v4.14
    This is like commit 3d3451124f3d ("mmc: sdhci-msm: Prefer asynchronous
    probe") but applied to a whole pile of drivers.  This batch converts
    the drivers that appeared to be around in the v4.14 timeframe.
    
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Tested-by: Wolfram Sang <wsa+renesas@sang-engineering.com> # SDHI drivers
    Link: https://lore.kernel.org/r/20200903162412.3.Id1ff21470f08f427aedd0a6535dcd83ccc56b278@changeid
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/meson-gx-mmc.c
+++ drivers/mmc/host/meson-gx-mmc.c
@@ -1265,6 +1265,7 @@ static struct platform_driver meson_mmc_driver = {
 	.remove		= meson_mmc_remove,
 	.driver		= {
 		.name = DRIVER_NAME,
+		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 		.of_match_table = of_match_ptr(meson_mmc_of_match),
 	},
 };
