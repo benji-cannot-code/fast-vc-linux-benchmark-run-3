e9f2308c10a4cea35cb76f8497918fed70da203d
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2020-09-03 21:38:19 +0300

    regulator: bd718x7 initialize regulator config only once
    The BD718x7 driver initialized common configs for all regulators.
    Simplify initialization by moving the initialization of common configs
    out of the loop.
    
    Signed-off-by: Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
    Link: https://lore.kernel.org/r/2d3e68c94d4813410da8c4c7eac3332d167d19a8.1599029334.git.matti.vaittinen@fi.rohmeurope.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/bd718x7-regulator.c
+++ drivers/regulator/bd718x7-regulator.c
@@ -1320,6 +1320,9 @@ static int bd718xx_probe(struct platform_device *pdev)
 		}
 	}
 
+	config.dev = pdev->dev.parent;
+	config.regmap = mfd->chip.regmap;
+
 	for (i = 0; i < num_reg_data; i++) {
 
 		const struct regulator_desc *desc;
@@ -1329,8 +1332,6 @@ static int bd718xx_probe(struct platform_device *pdev)
 		r = &reg_data[i];
 		desc = &r->desc;
 
-		config.dev = pdev->dev.parent;
-		config.regmap = mfd->chip.regmap;
 
 		rdev = devm_regulator_register(&pdev->dev, desc, &config);
 		if (IS_ERR(rdev)) {
