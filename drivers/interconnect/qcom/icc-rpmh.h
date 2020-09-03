1cbae04e3d541092f4126bbf3a79eea583e10c48
Mike Tipton <mdtipton@codeaurora.org>
2020-09-03 12:21:45 -0700

    dt-bindings: interconnect: Add generic qcom bindings
    Add generic qcom interconnect bindings that are common across platforms. In
    particular, these include QCOM_ICC_TAG_* macros that clients can use when
    calling icc_set_tag().
    
    Signed-off-by: Mike Tipton <mdtipton@codeaurora.org>
    Acked-by: Rob Herring <robh@kernel.org>
    Link: https://lore.kernel.org/r/20200903192149.30385-3-mdtipton@codeaurora.org
    Signed-off-by: Georgi Djakov <georgi.djakov@linaro.org>

--- drivers/interconnect/qcom/icc-rpmh.h
+++ drivers/interconnect/qcom/icc-rpmh.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DRIVERS_INTERCONNECT_QCOM_ICC_RPMH_H__
 #define __DRIVERS_INTERCONNECT_QCOM_ICC_RPMH_H__
 
+#include <dt-bindings/interconnect/qcom,icc.h>
+
 #define to_qcom_provider(_provider) \
 	container_of(_provider, struct qcom_icc_provider, provider)
 
@@ -44,22 +46,6 @@ struct bcm_db {
 #define MAX_BCMS		64
 #define MAX_BCM_PER_NODE	3
 #define MAX_VCD			10
-
-/*
- * The AMC bucket denotes constraints that are applied to hardware when
- * icc_set_bw() completes, whereas the WAKE and SLEEP constraints are applied
- * when the execution environment transitions between active and low power mode.
- */
-#define QCOM_ICC_BUCKET_AMC		0
-#define QCOM_ICC_BUCKET_WAKE		1
-#define QCOM_ICC_BUCKET_SLEEP		2
-#define QCOM_ICC_NUM_BUCKETS		3
-#define QCOM_ICC_TAG_AMC		BIT(QCOM_ICC_BUCKET_AMC)
-#define QCOM_ICC_TAG_WAKE		BIT(QCOM_ICC_BUCKET_WAKE)
-#define QCOM_ICC_TAG_SLEEP		BIT(QCOM_ICC_BUCKET_SLEEP)
-#define QCOM_ICC_TAG_ACTIVE_ONLY	(QCOM_ICC_TAG_AMC | QCOM_ICC_TAG_WAKE)
-#define QCOM_ICC_TAG_ALWAYS		(QCOM_ICC_TAG_AMC | QCOM_ICC_TAG_WAKE |\
-					 QCOM_ICC_TAG_SLEEP)
 
 /**
  * struct qcom_icc_node - Qualcomm specific interconnect nodes
