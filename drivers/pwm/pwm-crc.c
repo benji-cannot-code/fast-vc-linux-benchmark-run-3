bf3689e28be850df493acf223335ad248153e496
Hans de Goede <hdegoede@redhat.com>
2020-09-03 13:23:29 +0200

    pwm: crc: Fix off-by-one error in the clock-divider calculations
    The CRC PWM controller has a clock-divider which divides the clock with
    a value between 1-128. But as can seen from the PWM_DIV_CLK_xxx
    defines, this range maps to a register value of 0-127.
    
    So after calculating the clock-divider we must subtract 1 to get the
    register value, unless the requested frequency was so high that the
    calculation has already resulted in a (rounded) divider value of 0.
    
    Note that before this fix, setting a period of PWM_MAX_PERIOD_NS which
    corresponds to the max. divider value of 128 could have resulted in a
    bug where the code would use 128 as divider-register value which would
    have resulted in an actual divider value of 0 (and the enable bit being
    set). A rounding error stopped this bug from actually happen. This
    same rounding error means that after the subtraction of 1 it is impossible
    to set the divider to 128. Also bump PWM_MAX_PERIOD_NS by 1 ns to allow
    setting a divider of 128 (register-value 127).
    
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Acked-by: Thierry Reding <thierry.reding@gmail.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200903112337.4113-10-hdegoede@redhat.com

--- drivers/pwm/pwm-crc.c
+++ drivers/pwm/pwm-crc.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PWM_MAX_LEVEL		0xFF
 
 #define PWM_BASE_CLK_MHZ	6	/* 6 MHz */
-#define PWM_MAX_PERIOD_NS	5461333	/* 183 Hz */
+#define PWM_MAX_PERIOD_NS	5461334	/* 183 Hz */
 
 /**
  * struct crystalcove_pwm - Crystal Cove PWM controller
@@ -38,6 +38,18 @@ struct crystalcove_pwm {
 static inline struct crystalcove_pwm *to_crc_pwm(struct pwm_chip *pc)
 {
 	return container_of(pc, struct crystalcove_pwm, chip);
+}
+
+static int crc_pwm_calc_clk_div(int period_ns)
+{
+	int clk_div;
+
+	clk_div = PWM_BASE_CLK_MHZ * period_ns / (256 * NSEC_PER_USEC);
+	/* clk_div 1 - 128, maps to register values 0-127 */
+	if (clk_div > 0)
+		clk_div--;
+
+	return clk_div;
 }
 
 static int crc_pwm_enable(struct pwm_chip *c, struct pwm_device *pwm)
@@ -69,11 +81,10 @@ static int crc_pwm_config(struct pwm_chip *c, struct pwm_device *pwm,
 	}
 
 	if (pwm_get_period(pwm) != period_ns) {
-		int clk_div;
+		int clk_div = crc_pwm_calc_clk_div(period_ns);
 
 		/* changing the clk divisor, need to disable fisrt */
 		crc_pwm_disable(c, pwm);
-		clk_div = PWM_BASE_CLK_MHZ * period_ns / (256 * NSEC_PER_USEC);
 
 		regmap_write(crc_pwm->regmap, PWM0_CLK_DIV,
 					clk_div | PWM_OUTPUT_ENABLE);
