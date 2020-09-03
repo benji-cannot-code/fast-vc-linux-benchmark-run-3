faebcfb242fdad4f2e2d3582f31b1ceb44ca9604
Xu Wang <vulab@iscas.ac.cn>
2020-09-03 08:37:38 +0000

    crypto: s5p-sss - remove redundant null check
    Because clk_disable_unprepare already checked NULL clock
    parameter, so the additional checks are unnecessary, just remove them.
    
    Signed-off-by: Xu Wang <vulab@iscas.ac.cn>
    Reviewed-by: Krzysztof Kozlowski <krzk@kernel.org>
    Acked-by: Kamil Konieczny <k.konieczny@samsung.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/s5p-sss.c
+++ drivers/crypto/s5p-sss.c
@@ -2308,8 +2308,7 @@ err_algs:
 	tasklet_kill(&pdata->tasklet);
 
 err_irq:
-	if (pdata->pclk)
-		clk_disable_unprepare(pdata->pclk);
+	clk_disable_unprepare(pdata->pclk);
 
 err_clk:
 	clk_disable_unprepare(pdata->clk);
@@ -2339,8 +2338,7 @@ static int s5p_aes_remove(struct platform_device *pdev)
 		pdata->use_hash = false;
 	}
 
-	if (pdata->pclk)
-		clk_disable_unprepare(pdata->pclk);
+	clk_disable_unprepare(pdata->pclk);
 
 	clk_disable_unprepare(pdata->clk);
 	s5p_dev = NULL;
