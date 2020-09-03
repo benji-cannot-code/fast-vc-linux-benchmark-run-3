c049016d7e99a5781aaacdc27ab2104e894d4c2b
Maxime Ripard <maxime@cerno.tech>
2020-09-03 10:01:33 +0200

    drm/vc4: hdmi: Remove unused CEC_CLOCK_DIV define
    The CEC_CLOCK_DIV define is not used anywhere in the driver, let's remove
    it.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>
    Tested-by: Chanwoo Choi <cw00.choi@samsung.com>
    Tested-by: Hoegeun Kwon <hoegeun.kwon@samsung.com>
    Tested-by: Stefan Wahren <stefan.wahren@i2se.com>
    Reviewed-by: Dave Stevenson <dave.stevenson@raspberrypi.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/53805cc47e99a137e5f08c92fb781cc2e2134299.1599120059.git-series.maxime@cerno.tech

--- drivers/gpu/drm/vc4/vc4_hdmi.c
+++ drivers/gpu/drm/vc4/vc4_hdmi.c
@@ -56,7 +56,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define HSM_CLOCK_FREQ 163682864
 #define CEC_CLOCK_FREQ 40000
-#define CEC_CLOCK_DIV  (HSM_CLOCK_FREQ / CEC_CLOCK_FREQ)
 
 static int vc4_hdmi_debugfs_regs(struct seq_file *m, void *unused)
 {
