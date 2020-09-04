06ce8146ea7b31da70b7b5f012fea228dc2a1ca0
Keyon Jie <yang.jie@linux.intel.com>
2020-09-04 16:27:30 +0300

    ASoC: SOF: add comp_ext to struct snd_sof_widget
    Add member comp_ext to struct snd_sof_widget, which will be used for
    topology extended tokens parsing.
    
    Signed-off-by: Keyon Jie <yang.jie@linux.intel.com>
    Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Link: https://lore.kernel.org/r/20200904132744.1699575-3-kai.vehmanen@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/sound/sof/topology.h
+++ include/sound/sof/topology.h
@@ -88,6 +88,9 @@ struct sof_ipc_comp {
  */
 #define SOF_BUF_UNDERRUN_PERMITTED	BIT(1)
 
+/* the UUID size in bytes, shared between FW and host */
+#define SOF_UUID_SIZE	16
+
 /* create new component buffer - SOF_IPC_TPLG_BUFFER_NEW */
 struct sof_ipc_buffer {
 	struct sof_ipc_comp comp;
@@ -300,5 +303,10 @@ enum sof_event_types {
 	SOF_EVENT_NONE = 0,
 	SOF_KEYWORD_DETECT_DAPM_EVENT,
 };
+
+/* extended data struct for UUID components */
+struct sof_ipc_comp_ext {
+	uint8_t uuid[SOF_UUID_SIZE];
+}  __packed;
 
 #endif
