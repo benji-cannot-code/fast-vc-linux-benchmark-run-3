fe161cf9b01a715ce3bc283ab89d812aec3db934
Julien Thierry <jthierry@redhat.com>
2020-09-04 16:30:20 +0100

    objtool: Make sync-check consider the target architecture
    Do not take into account outdated headers unrelated to the build of the
    current architecture.
    
    [ jpoimboe: use $SRCARCH directly ]
    
    Reviewed-by: Miroslav Benes <mbenes@suse.cz>
    Signed-off-by: Julien Thierry <jthierry@redhat.com>
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>

--- tools/objtool/sync-check.sh
+++ tools/objtool/sync-check.sh
@@ -2,6 +2,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #!/bin/sh
 # SPDX-License-Identifier: GPL-2.0
 
+if [ -z "$SRCARCH" ]; then
+	echo 'sync-check.sh: error: missing $SRCARCH environment variable' >&2
+	exit 1
+fi
+
+if [ "$SRCARCH" = "x86" ]; then
 FILES="
 arch/x86/include/asm/inat_types.h
 arch/x86/include/asm/orc_types.h
@@ -14,6 +20,7 @@ arch/x86/include/asm/insn.h     -I '^#include [\"<]\(asm/\)*inat.h[\">]'
 arch/x86/lib/inat.c             -I '^#include [\"<]\(../include/\)*asm/insn.h[\">]'
 arch/x86/lib/insn.c             -I '^#include [\"<]\(../include/\)*asm/in\(at\|sn\).h[\">]' -I '^#include [\"<]\(../include/\)*asm/emulate_prefix.h[\">]'
 "
+fi
 
 check_2 () {
   file1=$1
