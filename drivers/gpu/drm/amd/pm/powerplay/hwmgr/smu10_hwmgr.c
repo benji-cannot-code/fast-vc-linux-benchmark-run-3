db753940cabb2f7273c93a2575a485b0c0491582
Evan Quan <evan.quan@amd.com>
2020-09-04 15:49:08 +0800

    drm/amd/pm: lower Raven UMD Stable Pstate VCN values
    SMU FCLK,SOCCLK have dependency on VCN CLKs. Lower VCN values so that
    FCLK, SOCCLK reflect values set by UMD Stable Pstate.
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/powerplay/hwmgr/smu10_hwmgr.c
+++ drivers/gpu/drm/amd/pm/powerplay/hwmgr/smu10_hwmgr.c
@@ -649,7 +649,7 @@ static int smu10_dpm_force_dpm_level(struct pp_hwmgr *hwmgr,
 						NULL);
 		smum_send_msg_to_smc_with_parameter(hwmgr,
 						PPSMC_MSG_SetHardMinVcn,
-						SMU10_UMD_PSTATE_VCE,
+						SMU10_UMD_PSTATE_PROFILE_VCE,
 						NULL);
 
 		smum_send_msg_to_smc_with_parameter(hwmgr,
@@ -666,7 +666,7 @@ static int smu10_dpm_force_dpm_level(struct pp_hwmgr *hwmgr,
 						NULL);
 		smum_send_msg_to_smc_with_parameter(hwmgr,
 						PPSMC_MSG_SetSoftMaxVcn,
-						SMU10_UMD_PSTATE_VCE,
+						SMU10_UMD_PSTATE_PROFILE_VCE,
 						NULL);
 		break;
 	case AMD_DPM_FORCED_LEVEL_AUTO:
