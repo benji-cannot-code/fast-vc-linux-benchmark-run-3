fece1955c0654f4900454d34a4ab13b99f91cd51
Leon Romanovsky <leonro@mellanox.com>
2020-09-07 15:09:13 +0300

    RDMA: Restore ability to fail on PD deallocate
    The IB verbs objects are counted by the kernel and ib_core ensures that
    deallocate PD will success so it will be called once all other objects
    that depends on PD will be released. This is achieved by managing various
    reference counters on such objects.
    
    The mlx5 driver didn't follow this standard flow when allowed DEVX objects
    that are not managed by ib_core to be interleaved with the ones under
    ib_core responsibility.
    
    In such interleaved scenarios deallocate command can fail and ib_core will
    leave uobject in internal DB and attempt to clean it later to free
    resources anyway.
    
    This change partially restores returned value from dealloc_pd() for all
    drivers, but keeping in mind that non-DEVX devices and kernel verbs paths
    shouldn't fail.
    
    Fixes: 21a428a019c9 ("RDMA: Handle PD allocations by IB/core")
    Link: https://lore.kernel.org/r/20200907120921.476363-2-leon@kernel.org
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/infiniband/hw/vmw_pvrdma/pvrdma_verbs.c
+++ drivers/infiniband/hw/vmw_pvrdma/pvrdma_verbs.c
@@ -480,9 +480,9 @@ err:
  * @pd: the protection domain to be released
  * @udata: user data or null for kernel object
  *
- * @return: 0 on success, otherwise errno.
+ * @return: Always 0
  */
-void pvrdma_dealloc_pd(struct ib_pd *pd, struct ib_udata *udata)
+int pvrdma_dealloc_pd(struct ib_pd *pd, struct ib_udata *udata)
 {
 	struct pvrdma_dev *dev = to_vdev(pd->device);
 	union pvrdma_cmd_req req = {};
@@ -499,6 +499,7 @@ void pvrdma_dealloc_pd(struct ib_pd *pd, struct ib_udata *udata)
 			 ret);
 
 	atomic_dec(&dev->num_pds);
+	return 0;
 }
 
 /**
