0d123638537bf68985b4522340d38b24cd22efa0
Wang Hai <wanghai38@huawei.com>
2020-09-07 22:06:52 +0800

    blktrace: make function blk_trace_bio_get_cgid() static
    The sparse tool complains as follows:
    
    kernel/trace/blktrace.c:796:5: warning:
     symbol 'blk_trace_bio_get_cgid' was not declared. Should it be static?
    
    This function is not used outside of blktrace.c, so this commit
    marks it static.
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Wang Hai <wanghai38@huawei.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- kernel/trace/blktrace.c
+++ kernel/trace/blktrace.c
@@ -794,7 +794,7 @@ static u64 blk_trace_bio_get_cgid(struct request_queue *q, struct bio *bio)
 	return cgroup_id(bio_blkcg(bio)->css.cgroup);
 }
 #else
-u64 blk_trace_bio_get_cgid(struct request_queue *q, struct bio *bio)
+static u64 blk_trace_bio_get_cgid(struct request_queue *q, struct bio *bio)
 {
 	return 0;
 }
