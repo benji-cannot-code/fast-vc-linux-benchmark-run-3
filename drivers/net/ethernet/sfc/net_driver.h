2693f1ed088974b87d1e3dc9fe06dd834ef3074c
Edward Cree <ecree@solarflare.com>
2020-09-07 17:15:18 +0100

    sfc: remove EFX_DRIVER_VERSION
    Per-module versions for in-tree drivers are deprecated.
    
    Signed-off-by: Edward Cree <ecree@solarflare.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/sfc/net_driver.h
+++ drivers/net/ethernet/sfc/net_driver.h
@@ -39,8 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  **************************************************************************/
 
-#define EFX_DRIVER_VERSION	"4.1"
-
 #ifdef DEBUG
 #define EFX_WARN_ON_ONCE_PARANOID(x) WARN_ON_ONCE(x)
 #define EFX_WARN_ON_PARANOID(x) WARN_ON(x)
