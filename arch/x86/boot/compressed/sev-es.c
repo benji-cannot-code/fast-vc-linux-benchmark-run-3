b840bd4433a752a58a648799a8239f24f4df873a
Tom Lendacky <thomas.lendacky@amd.com>
2020-09-07 15:16:12 +0200

    x86/efi: Add GHCB mappings when SEV-ES is active
    Calling down to EFI runtime services can result in the firmware
    performing VMGEXIT calls. The firmware is likely to use the GHCB of the
    OS (e.g., for setting EFI variables), so each GHCB in the system needs
    to be identity-mapped in the EFI page tables, as unencrypted, to avoid
    page faults.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    [ jroedel@suse.de: Moved GHCB mapping loop to sev-es.c ]
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Link: https://lkml.kernel.org/r/20200907131613.12703-72-joro@8bytes.org

--- arch/x86/boot/compressed/sev-es.c
+++ arch/x86/boot/compressed/sev-es.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include "misc.h"
 
+#include <asm/pgtable_types.h>
 #include <asm/sev-es.h>
 #include <asm/trapnr.h>
 #include <asm/trap_pf.h>
