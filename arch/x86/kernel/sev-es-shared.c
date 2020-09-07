af375be7e6049140b22a8a84b99bfc9bbaee12f3
Martin Radev <martin.b.radev@gmail.com>
2020-09-07 15:16:13 +0200

    x86/sev-es: Check required CPU features for SEV-ES
    Make sure the machine supports RDRAND, otherwise there is no trusted
    source of randomness in the system.
    
    To also check this in the pre-decompression stage, make has_cpuflag()
    not depend on CONFIG_RANDOMIZE_BASE anymore.
    
    Signed-off-by: Martin Radev <martin.b.radev@gmail.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Link: https://lkml.kernel.org/r/20200907131613.12703-73-joro@8bytes.org

--- arch/x86/kernel/sev-es-shared.c
+++ arch/x86/kernel/sev-es-shared.c
@@ -10,6 +10,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * and is included directly into both code-bases.
  */
 
+#ifndef __BOOT_COMPRESSED
+#define error(v)	pr_err(v)
+#define has_cpuflag(f)	boot_cpu_has(f)
+#endif
+
+static bool __init sev_es_check_cpu_features(void)
+{
+	if (!has_cpuflag(X86_FEATURE_RDRAND)) {
+		error("RDRAND instruction not supported - no trusted source of randomness available\n");
+		return false;
+	}
+
+	return true;
+}
+
 static void sev_es_terminate(unsigned int reason)
 {
 	u64 val = GHCB_SEV_TERMINATE;
