30e1bb353bd0baa2252b6aa7c92b237d13569b7f
Joerg Roedel <jroedel@suse.de>
2020-09-07 15:15:44 +0200

    x86/sev-es: Adjust #VC IST Stack on entering NMI handler
    When an NMI hits in the #VC handler entry code before it has switched to
    another stack, any subsequent #VC exception in the NMI code-path will
    overwrite the interrupted #VC handler's stack.
    
    Make sure this doesn't happen by explicitly adjusting the #VC IST entry
    in the NMI handler for the time it can cause #VC exceptions.
    
     [ bp: Touchups, spelling fixes. ]
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/20200907131613.12703-44-joro@8bytes.org

--- arch/x86/kernel/nmi.c
+++ arch/x86/kernel/nmi.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/reboot.h>
 #include <asm/cache.h>
 #include <asm/nospec-branch.h>
+#include <asm/sev-es.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/nmi.h>
@@ -489,6 +490,12 @@ DEFINE_IDTENTRY_RAW(exc_nmi)
 	this_cpu_write(nmi_cr2, read_cr2());
 nmi_restart:
 
+	/*
+	 * Needs to happen before DR7 is accessed, because the hypervisor can
+	 * intercept DR7 reads/writes, turning those into #VC exceptions.
+	 */
+	sev_es_ist_enter(regs);
+
 	this_cpu_write(nmi_dr7, local_db_save());
 
 	irq_state = idtentry_enter_nmi(regs);
@@ -501,6 +508,8 @@ nmi_restart:
 	idtentry_exit_nmi(regs, irq_state);
 
 	local_db_restore(this_cpu_read(nmi_dr7));
+
+	sev_es_ist_exit();
 
 	if (unlikely(this_cpu_read(nmi_cr2) != read_cr2()))
 		write_cr2(this_cpu_read(nmi_cr2));
