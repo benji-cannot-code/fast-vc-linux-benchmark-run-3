29729e38c26bb734468d8043e4893aae539076c0
Joerg Roedel <jroedel@suse.de>
2020-09-07 15:15:37 +0200

    x86/sev-es: Add SEV-ES Feature Detection
    Add a sev_es_active() function for checking whether SEV-ES is enabled.
    Also cache the value of MSR_AMD64_SEV at boot to speed up the feature
    checking in the running code.
    
     [ bp: Remove "!!" in sev_active() too. ]
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Link: https://lkml.kernel.org/r/20200907131613.12703-37-joro@8bytes.org

--- arch/x86/mm/mem_encrypt.c
+++ arch/x86/mm/mem_encrypt.c
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * section is later cleared.
  */
 u64 sme_me_mask __section(.data) = 0;
+u64 sev_status __section(.data) = 0;
 EXPORT_SYMBOL(sme_me_mask);
 DEFINE_STATIC_KEY_FALSE(sev_enable_key);
 EXPORT_SYMBOL_GPL(sev_enable_key);
@@ -348,7 +349,13 @@ bool sme_active(void)
 
 bool sev_active(void)
 {
-	return sme_me_mask && sev_enabled;
+	return sev_status & MSR_AMD64_SEV_ENABLED;
+}
+
+/* Needs to be called from non-instrumentable code */
+bool noinstr sev_es_active(void)
+{
+	return sev_status & MSR_AMD64_SEV_ES_ENABLED;
 }
 
 /* Override for DMA direct allocation check - ARCH_HAS_FORCE_DMA_UNENCRYPTED */
