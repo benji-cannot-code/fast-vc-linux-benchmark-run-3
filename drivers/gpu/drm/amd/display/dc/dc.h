2d515d20dfa76b4b75c0a80b5a1536b9297bc9f0
Aric Cyr <aric.cyr@amd.com>
2020-09-08 10:35:37 -0400

    drm/amd/display: 3.2.103
    Signed-off-by: Aric Cyr <aric.cyr@amd.com>
    Acked-by: Qingqing Zhuo <qingqing.zhuo@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dc.h
+++ drivers/gpu/drm/amd/display/dc/dc.h
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "inc/hw/dmcu.h"
 #include "dml/display_mode_lib.h"
 
-#define DC_VER "3.2.102"
+#define DC_VER "3.2.103"
 
 #define MAX_SURFACES 3
 #define MAX_PLANES 6
