abfbc9e62758b3dd8a2b77fbb30b462fc03787ff
Chen Zhou <chenzhou10@huawei.com>
2020-09-08 21:22:01 +0800

    bpf: Remove duplicate headers
    Remove duplicate headers which are included twice.
    
    Signed-off-by: Chen Zhou <chenzhou10@huawei.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/20200908132201.184005-1-chenzhou10@huawei.com

--- net/core/bpf_sk_storage.c
+++ net/core/bpf_sk_storage.c
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/sock.h>
 #include <uapi/linux/sock_diag.h>
 #include <uapi/linux/btf.h>
-#include <linux/btf_ids.h>
 
 DEFINE_BPF_STORAGE_CACHE(sk_cache);
 
