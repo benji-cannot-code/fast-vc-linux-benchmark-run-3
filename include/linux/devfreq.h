a27c5d1155940a7ae2c00a048fd98b1b0e04465f
Leonard Crestez <leonard.crestez@nxp.com>
2020-09-08 19:24:45 +0900

    PM / devfreq: Add devfreq_get_devfreq_by_node function
    Split off part of devfreq_get_devfreq_by_phandle into a separate
    function. This allows callers to fetch devfreq instances by enumerating
    devicetree instead of explicit phandles.
    
    Acked-by: Krzysztof Kozlowski <krzk@kernel.org>
    Reviewed-by: Lukasz Luba <lukasz.luba@arm.com>
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>
    [cw00.choi: Export devfreq_get_devfreq_by_node function and
     add function to devfreq.h when CONFIG_PM_DEVFREQ is enabled.]
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- include/linux/devfreq.h
+++ include/linux/devfreq.h
@@ -262,6 +262,7 @@ void devm_devfreq_unregister_notifier(struct device *dev,
 				struct devfreq *devfreq,
 				struct notifier_block *nb,
 				unsigned int list);
+struct devfreq *devfreq_get_devfreq_by_node(struct device_node *node);
 struct devfreq *devfreq_get_devfreq_by_phandle(struct device *dev, int index);
 
 #if IS_ENABLED(CONFIG_DEVFREQ_GOV_SIMPLE_ONDEMAND)
@@ -413,6 +414,11 @@ static inline void devm_devfreq_unregister_notifier(struct device *dev,
 					struct notifier_block *nb,
 					unsigned int list)
 {
+}
+
+static inline struct devfreq *devfreq_get_devfreq_by_node(struct device_node *node)
+{
+	return ERR_PTR(-ENODEV);
 }
 
 static inline struct devfreq *devfreq_get_devfreq_by_phandle(struct device *dev,
