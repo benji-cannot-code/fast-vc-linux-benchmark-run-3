d4b8651dd0355a722ef0f33290a86f363ce047d8
Anson Huang <Anson.Huang@nxp.com>
2020-09-08 16:12:01 +0800

    pinctrl: imx: Support building i.MX pinctrl core driver as module
    Change PINCTRL_IMX to tristate to support loadable module build.
    
    And i.MX common pinctrl driver should depend on CONFIG_OF to make sure
    no build error when i.MX common pinctrl driver is enabled for different
    architectures without CONFIG_OF.
    
    Also add module author, description and license.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>
    Reviewed-by: Dong Aisheng <aisheng.dong@nxp.com>
    Link: https://lore.kernel.org/r/1599552721-24872-3-git-send-email-Anson.Huang@nxp.com
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/freescale/pinctrl-imx.c
+++ drivers/pinctrl/freescale/pinctrl-imx.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/io.h>
 #include <linux/mfd/syscon.h>
+#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/of_address.h>
@@ -899,3 +900,7 @@ const struct dev_pm_ops imx_pinctrl_pm_ops = {
 					imx_pinctrl_resume)
 };
 EXPORT_SYMBOL_GPL(imx_pinctrl_pm_ops);
+
+MODULE_AUTHOR("Dong Aisheng <aisheng.dong@nxp.com>");
+MODULE_DESCRIPTION("NXP i.MX common pinctrl driver");
+MODULE_LICENSE("GPL v2");
