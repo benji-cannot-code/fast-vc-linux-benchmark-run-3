4bbcdcdf572fb4f81e9b2988f12e60af7d106649
Christoph Hellwig <hch@lst.de>
2020-09-08 17:56:22 +0200

    dma-direct: remove __dma_to_phys
    There is no harm in just always clearing the SME encryption bit, while
    significantly simplifying the interface.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>

--- arch/mips/include/asm/dma-direct.h
+++ arch/mips/include/asm/dma-direct.h
@@ -4,6 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _MIPS_DMA_DIRECT_H 1
 
 dma_addr_t __phys_to_dma(struct device *dev, phys_addr_t paddr);
-phys_addr_t __dma_to_phys(struct device *dev, dma_addr_t daddr);
+phys_addr_t dma_to_phys(struct device *dev, dma_addr_t daddr);
 
 #endif /* _MIPS_DMA_DIRECT_H */
