d2a9bb4cff2403afdfde1c8c7926b9b10bf0a9fd
Chen Zhou <chenzhou10@huawei.com>
2020-09-08 21:35:52 +0800

    drm/amd/display: Remove duplicate include
    Remove duplicate header which is included twice.
    
    Signed-off-by: Chen Zhou <chenzhou10@huawei.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dcn30/dcn30_hwseq.c
+++ drivers/gpu/drm/amd/display/dc/dcn30/dcn30_hwseq.c
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dcn30_dpp.h"
 #include "dcn10/dcn10_cm_common.h"
 #include "dcn30_cm_common.h"
-#include "clk_mgr.h"
 #include "reg_helper.h"
 #include "abm.h"
 #include "clk_mgr.h"
