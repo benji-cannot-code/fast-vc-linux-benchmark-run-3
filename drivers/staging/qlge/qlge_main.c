ecf8b195234904916655bffcf2852fec5fe8b939
Thomas Gleixner <tglx@linutronix.de>
2020-09-08 14:34:50 +0200

    net/qlge: Convert to SPDX license identifiers
    All files related to this driver contain the following notice:
    
      See LICENSE.qlge for copyright and licensing details.
    
    LICENSE.qlge can be found in
    Documentation/networking/device_drivers/qlogic/. The file contains:
    
      - A copyright notice
    
        This copyright notice is redundant as all files contain the same
        copyright notice already
    
      - A license notice
    
        You may modify and redistribute the device driver code under the
        GNU General Public License (a copy of which is attached hereto as
        Exhibit A) published by the Free Software Foundation (version 2).
    
        This can be replaced with the corresponding SPDX license identifier
        (GPL-2.0-only) in the source files which reference this license
        file.
    
      - The full GPLv2 license text
    
        A redundant copy of LICENSES/preferred/GPL-2.0
    
    Remove the notices and add the SPDX license identifier GPL-2.0-only to the
    source files.
    
    Finally remove the now redundant LICENSE.qlge file.
    
    Reviewed-by: Richard Fontana <rfontana@redhat.com>
    Reviewed-by: Jilayne Lovejoy <opensource@jilayne.com>
    Reviewed-by: Alexios Zavras <alexios.zavras@intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Lukas Bulwahn <lukas.bulwahn@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/qlge/qlge_main.c
+++ drivers/staging/qlge/qlge_main.c
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * QLogic qlge NIC HBA Driver
  * Copyright (c)  2003-2008 QLogic Corporation
- * See LICENSE.qlge for copyright and licensing details.
  * Author:     Linux qlge network device driver by
  *                      Ron Mercer <ron.mercer@qlogic.com>
  */
