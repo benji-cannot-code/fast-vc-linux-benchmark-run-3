0912be1e9d151ecc6c296bce58d0b6b68b377d73
Masahiro Yamada <masahiroy@kernel.org>
2020-09-08 13:27:08 +0900

    kbuild: preprocess module linker script
    There was a request to preprocess the module linker script like we
    do for the vmlinux one. (https://lkml.org/lkml/2020/8/21/512)
    
    The difference between vmlinux.lds and module.lds is that the latter
    is needed for external module builds, thus must be cleaned up by
    'make mrproper' instead of 'make clean'. Also, it must be created
    by 'make modules_prepare'.
    
    You cannot put it in arch/$(SRCARCH)/kernel/, which is cleaned up by
    'make clean'. I moved arch/$(SRCARCH)/kernel/module.lds to
    arch/$(SRCARCH)/include/asm/module.lds.h, which is included from
    scripts/module.lds.S.
    
    scripts/module.lds is fine because 'make clean' keeps all the
    build artifacts under scripts/.
    
    You can add arch-specific sections in <asm/module.lds.h>.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
    Tested-by: Jessica Yu <jeyu@kernel.org>
    Acked-by: Will Deacon <will@kernel.org>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Acked-by: Palmer Dabbelt <palmerdabbelt@google.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Acked-by: Jessica Yu <jeyu@kernel.org>

---
+++ include/asm-generic/module.lds.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __ASM_GENERIC_MODULE_LDS_H
+#define __ASM_GENERIC_MODULE_LDS_H
+
+/*
+ * <asm/module.lds.h> can specify arch-specific sections for linking modules.
+ * Empty for the asm-generic header.
+ */
+
+#endif /* __ASM_GENERIC_MODULE_LDS_H */
