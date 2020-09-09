4d7e2cdb8a60bf90ca17f7a1a9b0d5d9349c982b
Geert Uytterhoeven <geert+renesas@glider.be>
2020-09-09 15:15:33 +0200

    pinctrl: Rename sh-pfc to renesas
    The drivers/pinctrl/sh-pfc subdirectory was originally created to group
    pin control drivers for various Renesas SuperH and SH-Mobile platforms.
    However, the name "sh-pfc" no longer reflects its contents, as the
    directory now contains pin control drivers for Renesas SuperH, ARM32,
    and ARM64 SoCs.
    
    Hence rename the subdirectory from drivers/pinctrl/sh-pfc to
    drivers/pinctrl/renesas, and the related Kconfig symbol from
    PINCTRL_SH_PFC to PINCTRL_RENESAS.
    
    Rename the git branch in MAINTAINERS, too, for consistency.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://lore.kernel.org/r/20200909131534.12897-3-geert+renesas@glider.be

--- drivers/pinctrl/renesas/pfc-r8a77970.c
+++ drivers/pinctrl/renesas/pfc-r8a77970.c
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2016 Renesas Electronics Corp.
  * Copyright (C) 2017 Cogent Embedded, Inc. <source@cogentembedded.com>
  *
- * This file is based on the drivers/pinctrl/sh-pfc/pfc-r8a7795.c
+ * This file is based on the drivers/pinctrl/renesas/pfc-r8a7795.c
  *
  * R-Car Gen3 processor support - PFC hardware block.
  *
