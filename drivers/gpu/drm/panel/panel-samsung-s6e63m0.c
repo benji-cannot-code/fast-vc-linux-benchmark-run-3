7cb4422b6c2bb76daed977cb26da40a76884a50d
YueHaibing <yuehaibing@huawei.com>
2020-09-09 21:41:37 +0800

    drm/panel: s6e63m0: Add missing MODULE_LICENSE
    Kbuild warns when this file is built as a loadable module:
    
    WARNING: modpost: missing MODULE_LICENSE() in drivers/gpu/drm/panel/panel-samsung-s6e63m0.o
    
    Add the missing license/author/description tags.
    
    Fixes: b7b23e447687 ("drm/panel: s6e63m0: Break out SPI transport")
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200909134137.32284-1-yuehaibing@huawei.com

--- drivers/gpu/drm/panel/panel-samsung-s6e63m0.c
+++ drivers/gpu/drm/panel/panel-samsung-s6e63m0.c
@@ -535,3 +535,7 @@ int s6e63m0_remove(struct device *dev)
 	return 0;
 }
 EXPORT_SYMBOL_GPL(s6e63m0_remove);
+
+MODULE_AUTHOR("Paweł Chmiel <pawel.mikolaj.chmiel@gmail.com>");
+MODULE_DESCRIPTION("s6e63m0 LCD Driver");
+MODULE_LICENSE("GPL v2");
