5698e3517fc388cb5472a60481585cd06d242bb1
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2020-09-09 20:02:48 +0200

    platform_device: switch to simpler IDA interface
    We don't need to specify any ranges when allocating IDs so we can switch
    to ida_alloc() and ida_free() instead of the ida_simple_ counterparts.
    
    ida_simple_get(ida, 0, 0, gfp) is equivalent to
    ida_alloc_range(ida, 0, UINT_MAX, gfp) which is equivalent to
    ida_alloc(ida, gfp). Note: IDR will never actually allocate an ID
    larger than INT_MAX.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Link: https://lore.kernel.org/r/20200909180248.10093-1-brgl@bgdev.pl
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/platform.c
+++ drivers/base/platform.c
@@ -574,7 +574,7 @@ int platform_device_add(struct platform_device *pdev)
 		 * that we remember it must be freed, and we append a suffix
 		 * to avoid namespace collision with explicit IDs.
 		 */
-		ret = ida_simple_get(&platform_devid_ida, 0, 0, GFP_KERNEL);
+		ret = ida_alloc(&platform_devid_ida, GFP_KERNEL);
 		if (ret < 0)
 			goto err_out;
 		pdev->id = ret;
@@ -615,7 +615,7 @@ int platform_device_add(struct platform_device *pdev)
 
  failed:
 	if (pdev->id_auto) {
-		ida_simple_remove(&platform_devid_ida, pdev->id);
+		ida_free(&platform_devid_ida, pdev->id);
 		pdev->id = PLATFORM_DEVID_AUTO;
 	}
 
@@ -646,7 +646,7 @@ void platform_device_del(struct platform_device *pdev)
 		device_del(&pdev->dev);
 
 		if (pdev->id_auto) {
-			ida_simple_remove(&platform_devid_ida, pdev->id);
+			ida_free(&platform_devid_ida, pdev->id);
 			pdev->id = PLATFORM_DEVID_AUTO;
 		}
 
