cc08efa6a19bc3dc99b1386843f2e7ec6beefccb
Alex Dewar <alex.dewar90@gmail.com>
2020-09-09 20:02:08 +0100

    drm/bridge: dw-mipi-dsi: Use kmemdup cf. kmalloc+memcpy
    kmemdup can be used instead of kmalloc+memcpy. Replace an occurrence of
    this pattern.
    
    Issue identified with Coccinelle.
    
    Signed-off-by: Alex Dewar <alex.dewar90@gmail.com>
    Acked-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200909190213.156302-1-alex.dewar90@gmail.com

--- drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi.c
@@ -1050,11 +1050,9 @@ static void debugfs_create_files(void *data)
 	};
 	int i;
 
-	dsi->debugfs_vpg = kmalloc(sizeof(debugfs), GFP_KERNEL);
+	dsi->debugfs_vpg = kmemdup(debugfs, sizeof(debugfs), GFP_KERNEL);
 	if (!dsi->debugfs_vpg)
 		return;
-
-	memcpy(dsi->debugfs_vpg, debugfs, sizeof(debugfs));
 
 	for (i = 0; i < ARRAY_SIZE(debugfs); i++)
 		debugfs_create_file(dsi->debugfs_vpg[i].name, 0644,
