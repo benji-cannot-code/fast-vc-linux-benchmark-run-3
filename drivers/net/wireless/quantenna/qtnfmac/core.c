9ce6b49f574722108a43646a1f11b664d4cf24e3
YueHaibing <yuehaibing@huawei.com>
2020-09-09 21:59:51 +0800

    qtnfmac: Remove unused macro QTNF_DMP_MAX_LEN
    There is no caller in tree, so remove it.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/20200909135951.4028-1-yuehaibing@huawei.com

--- drivers/net/wireless/quantenna/qtnfmac/core.c
+++ drivers/net/wireless/quantenna/qtnfmac/core.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "util.h"
 #include "switchdev.h"
 
-#define QTNF_DMP_MAX_LEN 48
 #define QTNF_PRIMARY_VIF_IDX	0
 
 static bool slave_radar = true;
