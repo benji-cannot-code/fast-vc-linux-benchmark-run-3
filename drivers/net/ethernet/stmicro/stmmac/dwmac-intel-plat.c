385fdbb8bc0d6bb10702593a275c7806a8e060bb
Zhang Changzhong <zhangchangzhong@huawei.com>
2020-09-09 22:09:00 +0800

    net: stmmac: dwmac-intel-plat: remove redundant null check before clk_disable_unprepare()
    Because clk_prepare_enable() and clk_disable_unprepare() already checked
    NULL clock parameter, so the additional checks are unnecessary, just
    remove them.
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Zhang Changzhong <zhangchangzhong@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-intel-plat.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-intel-plat.c
@@ -150,9 +150,7 @@ static int intel_eth_plat_probe(struct platform_device *pdev)
 
 	ret = stmmac_dvr_probe(&pdev->dev, plat_dat, &stmmac_res);
 	if (ret) {
-		if (dwmac->tx_clk)
-			clk_disable_unprepare(dwmac->tx_clk);
-
+		clk_disable_unprepare(dwmac->tx_clk);
 		goto err_remove_config_dt;
 	}
 
@@ -170,9 +168,7 @@ static int intel_eth_plat_remove(struct platform_device *pdev)
 	int ret;
 
 	ret = stmmac_pltfr_remove(pdev);
-
-	if (dwmac->tx_clk)
-		clk_disable_unprepare(dwmac->tx_clk);
+	clk_disable_unprepare(dwmac->tx_clk);
 
 	return ret;
 }
