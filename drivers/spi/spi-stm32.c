c5d56a6231d27ae26ee46dd80194038a5dcb7258
Dan Carpenter <dan.carpenter@oracle.com>
2020-09-09 12:43:04 +0300

    spi: stm32: fix pm_runtime_get_sync() error checking
    The pm_runtime_get_sync() can return either 0 or 1 on success but this
    code treats 1 as a failure.
    
    Fixes: db96bf976a4f ("spi: stm32: fixes suspend/resume management")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Reviewed-by: Alain Volmat <alain.volmat@st.com>
    Link: https://lore.kernel.org/r/20200909094304.GA420136@mwanda
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-stm32.c
+++ drivers/spi/spi-stm32.c
@@ -2065,7 +2065,7 @@ static int stm32_spi_resume(struct device *dev)
 	}
 
 	ret = pm_runtime_get_sync(dev);
-	if (ret) {
+	if (ret < 0) {
 		dev_err(dev, "Unable to power device:%d\n", ret);
 		return ret;
 	}
