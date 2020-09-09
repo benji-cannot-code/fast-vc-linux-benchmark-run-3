a5e121a0da503ae6b23b7f2f6004d8708c67d823
Fabian Frederick <fabf@skynet.be>
2020-09-09 20:26:24 +0200

    selftests: netfilter: remove unused cnt and simplify command testing
    cnt was not used in nft_meta.sh
    This patch also fixes 2 shellcheck SC2181 warnings:
    "check exit code directly with e.g. 'if mycmd;', not indirectly with
    $?."
    
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- tools/testing/selftests/netfilter/nft_meta.sh
+++ tools/testing/selftests/netfilter/nft_meta.sh
@@ -8,8 +8,7 @@ ksft_skip=4
 sfx=$(mktemp -u "XXXXXXXX")
 ns0="ns0-$sfx"
 
-nft --version > /dev/null 2>&1
-if [ $? -ne 0 ];then
+if ! nft --version > /dev/null 2>&1; then
 	echo "SKIP: Could not run test without nft tool"
 	exit $ksft_skip
 fi
@@ -87,8 +86,7 @@ check_one_counter()
 	local want="packets $2"
 	local verbose="$3"
 
-	cnt=$(ip netns exec "$ns0" nft list counter inet filter $cname | grep -q "$want")
-	if [ $? -ne 0 ];then
+	if ! ip netns exec "$ns0" nft list counter inet filter $cname | grep -q "$want"; then
 		echo "FAIL: $cname, want \"$want\", got"
 		ret=1
 		ip netns exec "$ns0" nft list counter inet filter $cname
