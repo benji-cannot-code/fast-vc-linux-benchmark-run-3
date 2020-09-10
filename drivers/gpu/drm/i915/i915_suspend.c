f3aae47377cd8c4918155e40faba63b1bffbf27e
Jani Nikula <jani.nikula@intel.com>
2020-09-10 12:52:27 +0300

    drm/i915: move gmbus restore to i915_restore_display
    Logically part of the display restore.
    
    Note: This has been in place since the introduction of gmbus
    support. The gmbus code also does the resets before transfers. Is this
    really needed, or a historical accident?
    
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200910095227.9466-3-jani.nikula@intel.com

--- drivers/gpu/drm/i915/i915_suspend.c
+++ drivers/gpu/drm/i915/i915_suspend.c
@@ -70,6 +70,8 @@ static void i915_restore_display(struct drm_i915_private *dev_priv)
 		I915_WRITE(FBC_CONTROL, dev_priv->regfile.saveFBC_CONTROL);
 
 	intel_vga_redisable(dev_priv);
+
+	intel_gmbus_reset(dev_priv);
 }
 
 int i915_save_state(struct drm_i915_private *dev_priv)
@@ -141,8 +143,6 @@ int i915_restore_state(struct drm_i915_private *dev_priv)
 		for (i = 0; i < 3; i++)
 			I915_WRITE(SWF3(i), dev_priv->regfile.saveSWF3[i]);
 	}
-
-	intel_gmbus_reset(dev_priv);
 
 	return 0;
 }
