d386c15428bad86d40fcb3c25830729a3e3cb5d3
Kan Liang <kan.liang@linux.intel.com>
2020-09-11 07:48:05 -0700

    perf tools: Rename group to topdown
    The group.h/c only include TopDown group related functions. The name
    "group" is too generic and inaccurate. Use the name "topdown" to replace
    it.
    
    Move topdown related functions to a dedicated file, topdown.c.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Acked-by: Jiri Olsa <jolsa@redhat.com>
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lore.kernel.org/lkml/20200911144808.27603-2-kan.liang@linux.intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/topdown.h
+++ tools/perf/util/topdown.h
@@ -1,9 +1,11 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
-#ifndef GROUP_H
-#define GROUP_H 1
+#ifndef TOPDOWN_H
+#define TOPDOWN_H 1
 
 bool arch_topdown_check_group(bool *warn);
 void arch_topdown_group_warn(void);
+
+int topdown_filter_events(const char **attr, char **str, bool use_group);
 
 #endif
