4f9f6d1ce42ec906b7e9c091c4c967f98ac012d8
Rikard Falkeborn <rikard.falkeborn@gmail.com>
2020-09-11 22:35:09 +0200

    virtio-mem: Constify mem_id_table
    mem_id_table is not modified, so make it const to allow the compiler to
    put it in read-only memory.
    
    Signed-off-by: Rikard Falkeborn <rikard.falkeborn@gmail.com>
    Link: https://lore.kernel.org/r/20200911203509.26505-4-rikard.falkeborn@gmail.com
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: David Hildenbrand <david@redhat.com>

--- drivers/virtio/virtio_mem.c
+++ drivers/virtio/virtio_mem.c
@@ -1958,7 +1958,7 @@ static unsigned int virtio_mem_features[] = {
 #endif
 };
 
-static struct virtio_device_id virtio_mem_id_table[] = {
+static const struct virtio_device_id virtio_mem_id_table[] = {
 	{ VIRTIO_ID_MEM, VIRTIO_DEV_ANY_ID },
 	{ 0 },
 };
