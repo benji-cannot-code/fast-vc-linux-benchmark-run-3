038c1cb7e6d173a221bd3dce4166b680d6bc032b
Kan Liang <kan.liang@linux.intel.com>
2020-09-11 07:48:06 -0700

    perf record: Support sample-read topdown metric group
    With the hardware TopDown metrics feature, sample-read feature should be
    supported for a topdown group, e.g., sample a non-topdown event and read
    a topdown metric group. But the current perf record code errors out.
    
    For a topdown metric group, the slots event must be the leader of the
    group, but the leader slots event doesn't support sampling.
    
    To support sample-read the topdown metric group, use the 2nd event of
    the group as the "leader" for the purposes of sampling.
    
    Only the platform with Topdown metic feature supports sample-read the
    topdown group. Add arch_topdown_sample_read() to indicate whether the
    topdown group supports sample-read.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Acked-by: Jiri Olsa <jolsa@redhat.com>
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lore.kernel.org/lkml/20200911144808.27603-3-kan.liang@linux.intel.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/arch/x86/util/topdown.c
+++ tools/perf/arch/x86/util/topdown.c
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
 #include <stdio.h>
 #include "api/fs/fs.h"
+#include "util/pmu.h"
 #include "util/topdown.h"
 
 /*
@@ -26,4 +27,38 @@ void arch_topdown_group_warn(void)
 	fprintf(stderr,
 		"nmi_watchdog enabled with topdown. May give wrong results.\n"
 		"Disable with echo 0 > /proc/sys/kernel/nmi_watchdog\n");
+}
+
+#define TOPDOWN_SLOTS		0x0400
+
+static bool is_topdown_slots_event(struct evsel *counter)
+{
+	if (!counter->pmu_name)
+		return false;
+
+	if (strcmp(counter->pmu_name, "cpu"))
+		return false;
+
+	if (counter->core.attr.config == TOPDOWN_SLOTS)
+		return true;
+
+	return false;
+}
+
+/*
+ * Check whether a topdown group supports sample-read.
+ *
+ * Only Topdown metic supports sample-read. The slots
+ * event must be the leader of the topdown group.
+ */
+
+bool arch_topdown_sample_read(struct evsel *leader)
+{
+	if (!pmu_have_event("cpu", "slots"))
+		return false;
+
+	if (is_topdown_slots_event(leader))
+		return true;
+
+	return false;
 }
