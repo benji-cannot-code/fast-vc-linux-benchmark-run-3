07a8642d469642146619a86b134c8f17e1c81752
Jason Yan <yanaijie@huawei.com>
2020-09-12 15:21:58 +0800

    dmaengine: ioat: Make two symbols static
    This eliminates the following sparse warning:
    
    drivers/dma/ioat/dma.c:29:5: warning: symbol 'completion_timeout' was
    not declared. Should it be static?
    drivers/dma/ioat/dma.c:33:5: warning: symbol 'idle_timeout' was not
    declared. Should it be static?
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Jason Yan <yanaijie@huawei.com>
    Acked-by: Dave Jiang <dave.jiang@intel.com>
    Link: https://lore.kernel.org/r/20200912072158.602585-1-yanaijie@huawei.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/ioat/dma.c
+++ drivers/dma/ioat/dma.c
@@ -27,11 +27,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "../dmaengine.h"
 
-int completion_timeout = 200;
+static int completion_timeout = 200;
 module_param(completion_timeout, int, 0644);
 MODULE_PARM_DESC(completion_timeout,
 		"set ioat completion timeout [msec] (default 200 [msec])");
-int idle_timeout = 2000;
+static int idle_timeout = 2000;
 module_param(idle_timeout, int, 0644);
 MODULE_PARM_DESC(idle_timeout,
 		"set ioat idel timeout [msec] (default 2000 [msec])");
