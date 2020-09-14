edd18796cbf55fb8827caab70f11287ca1acf324
derek.fang <derek.fang@realtek.com>
2020-09-14 16:57:19 +0800

    ASoC: rt1015: Fix the failure to flush DAC data before playback
    Fix the failure to flush DAC data before playback.
    
    Signed-off-by: derek.fang <derek.fang@realtek.com>
    Link: https://lore.kernel.org/r/1600073839-6762-2-git-send-email-derek.fang@realtek.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt1015.h
+++ sound/soc/codecs/rt1015.h
@@ -374,6 +374,11 @@ enum {
 	RT1015_Bypass_Boost,
 };
 
+enum {
+	RT1015_HW_28 = 0,
+	RT1015_HW_29,
+};
+
 struct rt1015_priv {
 	struct snd_soc_component *component;
 	struct regmap *regmap;
@@ -391,6 +396,8 @@ struct rt1015_priv {
 	int amp_ver;
 	int dac_is_used;
 	int cali_done;
+	int hw_config;
+	struct delayed_work flush_work;
 };
 
 #endif /* __RT1015_H__ */
