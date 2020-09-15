c7e5c7a3d985f3d9a3689fb69fafc2170f57072a
Tian Tao <tiantao6@hisilicon.com>
2020-09-15 08:51:42 +0800

    vhost_vdpa: Fix duplicate included kernel.h
    linux/kernel.h is included more than once, Remove the one that isn't
    necessary.
    
    Signed-off-by: Tian Tao <tiantao6@hisilicon.com>
    Link: https://lore.kernel.org/r/1600131102-24672-1-git-send-email-tiantao6@hisilicon.com
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Jason Wang <jasowang@redhat.com>

--- drivers/vhost/vdpa.c
+++ drivers/vhost/vdpa.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/nospec.h>
 #include <linux/vhost.h>
 #include <linux/virtio_net.h>
-#include <linux/kernel.h>
 
 #include "vhost.h"
 
