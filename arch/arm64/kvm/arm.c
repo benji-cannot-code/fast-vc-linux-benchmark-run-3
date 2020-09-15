3da98115bafd502af18fbc74af2a8dea2dccec16
Andrew Scull <ascull@google.com>
2020-09-15 11:46:27 +0100

    KVM: arm64: Remove kvm_host_data_t typedef
    The kvm_host_data_t typedef is used inconsistently and goes against the
    kernel's coding style. Remove it in favour of the full struct specifier.
    
    Signed-off-by: Andrew Scull <ascull@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20200915104643.2543892-4-ascull@google.com

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 __asm__(".arch_extension	virt");
 #endif
 
-DEFINE_PER_CPU(kvm_host_data_t, kvm_host_data);
+DEFINE_PER_CPU(struct kvm_host_data, kvm_host_data);
 static DEFINE_PER_CPU(unsigned long, kvm_arm_hyp_stack_page);
 
 /* The VMID used in the VTTBR */
@@ -1539,7 +1539,7 @@ static int init_hyp_mode(void)
 	}
 
 	for_each_possible_cpu(cpu) {
-		kvm_host_data_t *cpu_data;
+		struct kvm_host_data *cpu_data;
 
 		cpu_data = per_cpu_ptr(&kvm_host_data, cpu);
 		err = create_hyp_mappings(cpu_data, cpu_data + 1, PAGE_HYP);
