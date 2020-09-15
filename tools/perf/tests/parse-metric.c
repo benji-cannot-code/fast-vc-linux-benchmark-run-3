8c07a148ae0a0f6575c4619fcf3897099efbab08
YueHaibing <yuehaibing@huawei.com>
2020-09-15 16:15:41 +0800

    perf metric: Remove duplicate include
    Remove duplicate header which is included twice.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: http://lore.kernel.org/lkml/20200915081541.41004-1-yuehaibing@huawei.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/tests/parse-metric.c
+++ tools/perf/tests/parse-metric.c
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "debug.h"
 #include "expr.h"
 #include "stat.h"
-#include <perf/cpumap.h>
-#include <perf/evlist.h>
 
 static struct pmu_event pme_test[] = {
 {
