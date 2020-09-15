ea38034c5f7d5e66d0d8035b4e1c95647b8cbeb4
Fenghua Yu <fenghua.yu@intel.com>
2020-09-15 09:30:10 -0700

    x86/msr-index: Define an IA32_PASID MSR
    The IA32_PASID MSR (0xd93) contains the Process Address Space Identifier
    (PASID), a 20-bit value. Bit 31 must be set to indicate the value
    programmed in the MSR is valid. Hardware uses the PASID to identify a
    process address space and direct responses to the right address space.
    
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Link: https://lkml.kernel.org/r/1600187413-163670-7-git-send-email-fenghua.yu@intel.com

--- arch/x86/include/asm/msr-index.h
+++ arch/x86/include/asm/msr-index.h
@@ -258,6 +258,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MSR_IA32_LASTINTFROMIP		0x000001dd
 #define MSR_IA32_LASTINTTOIP		0x000001de
 
+#define MSR_IA32_PASID			0x00000d93
+#define MSR_IA32_PASID_VALID		BIT_ULL(31)
+
 /* DEBUGCTLMSR bits (others vary by model): */
 #define DEBUGCTLMSR_LBR			(1UL <<  0) /* last branch recording */
 #define DEBUGCTLMSR_BTF_SHIFT		1
