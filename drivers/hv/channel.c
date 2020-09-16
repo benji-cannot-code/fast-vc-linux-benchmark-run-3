19280dc184bbfc951154e60f296f94887e1a64b4
Boqun Feng <boqun.feng@gmail.com>
2020-09-16 11:48:11 +0800

    Drivers: hv: vmbus: Move virt_to_hvpfn() to hyperv header
    There will be more places other than vmbus where we need to calculate
    the Hyper-V page PFN from a virtual address, so move virt_to_hvpfn() to
    hyperv generic header.
    
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>
    Reviewed-by: Michael Kelley <mikelley@microsoft.com>
    Link: https://lore.kernel.org/r/20200916034817.30282-6-boqun.feng@gmail.com
    Signed-off-by: Wei Liu <wei.liu@kernel.org>

--- drivers/hv/channel.c
+++ drivers/hv/channel.c
@@ -23,19 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "hyperv_vmbus.h"
 
-static unsigned long virt_to_hvpfn(void *addr)
-{
-	phys_addr_t paddr;
-
-	if (is_vmalloc_addr(addr))
-		paddr = page_to_phys(vmalloc_to_page(addr)) +
-					 offset_in_page(addr);
-	else
-		paddr = __pa(addr);
-
-	return  paddr >> HV_HYP_PAGE_SHIFT;
-}
-
 /*
  * hv_gpadl_size - Return the real size of a gpadl, the size that Hyper-V uses
  *
