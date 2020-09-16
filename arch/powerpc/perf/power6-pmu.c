a75198e76c975c770be0baf563d77daaf24b84ec
Michael Ellerman <mpe@ellerman.id.au>
2020-09-16 21:56:37 +1000

    powerpc/perf: Add declarations to fix sparse warnings
    Sparse warns about all the init functions:
      symbol init_ppc970_pmu was not declared. Should it be static?
      symbol init_power5p_pmu was not declared. Should it be static?
      symbol init_power5_pmu was not declared. Should it be static?
      symbol init_power6_pmu was not declared. Should it be static?
      symbol init_power7_pmu was not declared. Should it be static?
      symbol init_power9_pmu was not declared. Should it be static?
      symbol init_power8_pmu was not declared. Should it be static?
      symbol init_generic_compat_pmu was not declared. Should it be static?
    
    They're already declared in internal.h, so just make sure all the C
    files include that directly or indirectly.
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Reviewed-by: Madhavan Srinivasan <maddy@linux.ibm.com>
    Link: https://lore.kernel.org/r/20200916115637.3100484-2-mpe@ellerman.id.au

--- arch/powerpc/perf/power6-pmu.c
+++ arch/powerpc/perf/power6-pmu.c
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/reg.h>
 #include <asm/cputable.h>
 
+#include "internal.h"
+
 /*
  * Bits in event code for POWER6
  */
