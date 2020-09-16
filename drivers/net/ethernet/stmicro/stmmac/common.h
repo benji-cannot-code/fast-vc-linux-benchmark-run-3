1176e395b55376033c1c67aad631d8f749db6032
Song, Yoong Siang <yoong.siang.song@intel.com>
2020-09-16 15:40:20 +0800

    net: stmmac: Add support to Ethtool get/set ring parameters
    This patch add support to --show-ring & --set-ring Ethtool functions:
    - Adding min, max, power of two check to new ring parameter's value.
    - Bring down the network interface before changing the value of ring
      parameters.
    - Bring up the network interface after changing the value of ring
      parameters.
    
    Signed-off-by: Song, Yoong Siang <yoong.siang.song@intel.com>
    Signed-off-by: Voon Weifeng <weifeng.voon@intel.com>
    Signed-off-by: Ong Boon Leong <boon.leong.ong@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/stmicro/stmmac/common.h
+++ drivers/net/ethernet/stmicro/stmmac/common.h
@@ -43,9 +43,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define STMMAC_CHAN0	0	/* Always supported and default for all chips */
 
-/* These need to be power of two, and >= 4 */
-#define DMA_TX_SIZE 512
-#define DMA_RX_SIZE 512
+/* TX and RX Descriptor Length, these need to be power of two.
+ * TX descriptor length less than 64 may cause transmit queue timed out error.
+ * RX descriptor length less than 64 may cause inconsistent Rx chain error.
+ */
+#define DMA_MIN_TX_SIZE		64
+#define DMA_MAX_TX_SIZE		1024
+#define DMA_DEFAULT_TX_SIZE	512
+#define DMA_MIN_RX_SIZE		64
+#define DMA_MAX_RX_SIZE		1024
+#define DMA_DEFAULT_RX_SIZE	512
 #define STMMAC_GET_ENTRY(x, size)	((x + 1) & (size - 1))
 
 #undef FRAME_FILTER_DEBUG
