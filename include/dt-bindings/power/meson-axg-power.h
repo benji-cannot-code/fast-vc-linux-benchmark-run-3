3116131545dc2c7d4414549db28879137fe2e2e9
Neil Armstrong <narmstrong@baylibre.com>
2020-09-17 08:47:01 +0200

    dt-bindings: power: amlogic, meson-ee-pwrc: add Amlogic AXG power controller bindings
    This add the bindings of the Power Controller found in the Amlogic AXG SoCs.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Kevin Hilman <khilman@baylibre.com>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Acked-by: Martin Blumenstingl <martin.blumenstingl@googlemail.com>
    Link: https://lore.kernel.org/r/20200917064702.1459-2-narmstrong@baylibre.com

---
+++ include/dt-bindings/power/meson-axg-power.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: (GPL-2.0+ or MIT) */
+/*
+ * Copyright (c) 2020 BayLibre, SAS
+ * Author: Neil Armstrong <narmstrong@baylibre.com>
+ */
+
+#ifndef _DT_BINDINGS_MESON_AXG_POWER_H
+#define _DT_BINDINGS_MESON_AXG_POWER_H
+
+#define PWRC_AXG_VPU_ID			0
+#define PWRC_AXG_ETHERNET_MEM_ID	1
+#define PWRC_AXG_AUDIO_ID		2
+
+#endif
