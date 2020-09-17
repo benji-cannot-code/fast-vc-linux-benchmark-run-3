2e9652f8aab8b54978a427942a53caac0bc47ea4
Krish Sadhukhan <krish.sadhukhan@oracle.com>
2020-09-17 21:20:36 +0000

    x86/cpu: Add hardware-enforced cache coherency as a CPUID feature
    In some hardware implementations, coherency between the encrypted and
    unencrypted mappings of the same physical page is enforced. In such a system,
    it is not required for software to flush the page from all CPU caches in the
    system prior to changing the value of the C-bit for a page. This hardware-
    enforced cache coherency is indicated by EAX[10] in CPUID leaf 0x8000001f.
    
     [ bp: Use one of the free slots in word 3. ]
    
    Suggested-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Krish Sadhukhan <krish.sadhukhan@oracle.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/20200917212038.5090-2-krish.sadhukhan@oracle.com

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -97,7 +97,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define X86_FEATURE_SYSCALL32		( 3*32+14) /* "" syscall in IA32 userspace */
 #define X86_FEATURE_SYSENTER32		( 3*32+15) /* "" sysenter in IA32 userspace */
 #define X86_FEATURE_REP_GOOD		( 3*32+16) /* REP microcode works well */
-/* free					( 3*32+17) */
+#define X86_FEATURE_SME_COHERENT	( 3*32+17) /* "" AMD hardware-enforced cache coherency */
 #define X86_FEATURE_LFENCE_RDTSC	( 3*32+18) /* "" LFENCE synchronizes RDTSC */
 #define X86_FEATURE_ACC_POWER		( 3*32+19) /* AMD Accumulated Power Mechanism */
 #define X86_FEATURE_NOPL		( 3*32+20) /* The NOPL (0F 1F) instructions */
