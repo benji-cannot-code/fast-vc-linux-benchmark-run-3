0299116bfb2f724e192c601fcf14c5b288ba61cf
Randy Dunlap <rdunlap@infradead.org>
2020-09-17 21:35:19 -0700

    net: tipc: delete duplicated words
    Drop repeated words in net/tipc/.
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Jakub Kicinski <kuba@kernel.org>
    Cc: Jon Maloy <jmaloy@redhat.com>
    Cc: Ying Xue <ying.xue@windriver.com>
    Cc: tipc-discussion@lists.sourceforge.net
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tipc/socket.c
+++ net/tipc/socket.c
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CONN_PROBING_INTV	msecs_to_jiffies(3600000)  /* [ms] => 1 h */
 #define TIPC_MAX_PORT		0xffffffff
 #define TIPC_MIN_PORT		1
-#define TIPC_ACK_RATE		4       /* ACK at 1/4 of of rcv window size */
+#define TIPC_ACK_RATE		4       /* ACK at 1/4 of rcv window size */
 
 enum {
 	TIPC_LISTEN = TCP_LISTEN,
