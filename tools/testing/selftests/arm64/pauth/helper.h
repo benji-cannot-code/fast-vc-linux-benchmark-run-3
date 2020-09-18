6db86099998f1089b20d72d0a1ea320e6c7498a4
Boyan Karatotev <boyan.karatotev@arm.com>
2020-09-18 11:47:12 +0100

    kselftests/arm64: add a basic Pointer Authentication test
    PAuth signs and verifies return addresses on the stack. It does so by
    inserting a Pointer Authentication code (PAC) into some of the unused top
    bits of an address. This is achieved by adding paciasp/autiasp instructions
    at the beginning and end of a function.
    
    This feature is partially backwards compatible with earlier versions of the
    ARM architecture. To coerce the compiler into emitting fully backwards
    compatible code the main file is compiled to target an earlier ARM version.
    This allows the tests to check for the feature and print meaningful error
    messages instead of crashing.
    
    Add a test to verify that corrupting the return address results in a
    SIGSEGV on return.
    
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>
    Reviewed-by: Vincenzo Frascino <Vincenzo.Frascino@arm.com>
    Reviewed-by: Amit Daniel Kachhap <amit.kachhap@arm.com>
    Acked-by: Shuah Khan <skhan@linuxfoundation.org>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20200918104715.182310-2-boian4o1@gmail.com
    Signed-off-by: Will Deacon <will@kernel.org>

---
+++ tools/testing/selftests/arm64/pauth/helper.h
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (C) 2020 ARM Limited */
+
+#ifndef _HELPER_H_
+#define _HELPER_H_
+
+void pac_corruptor(void);
+
+#endif
