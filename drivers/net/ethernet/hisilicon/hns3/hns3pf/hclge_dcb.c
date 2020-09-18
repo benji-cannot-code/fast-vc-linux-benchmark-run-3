349c462b987b72e7ae6e71e335aeac6bf3e87a70
Wang Hai <wanghai38@huawei.com>
2020-09-18 21:06:53 +0800

    net: hns3: Supply missing hclge_dcb.h include file
    If the header file containing a function's prototype isn't included by
    the sourcefile containing the associated function, the build system
    complains of missing prototypes.
    
    Fixes the following W=1 kernel build warning(s):
    
    drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_dcb.c:453:6: warning: no previous prototype for ‘hclge_dcb_ops_set’ [-Wmissing-prototypes]
    
    Signed-off-by: Wang Hai <wanghai38@huawei.com>
    Reviewed-by: Saeed Mahameed <saeedm@nvidia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_dcb.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_dcb.c
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // Copyright (c) 2016-2017 Hisilicon Limited.
 
 #include "hclge_main.h"
+#include "hclge_dcb.h"
 #include "hclge_tm.h"
 #include "hnae3.h"
 
