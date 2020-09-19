16b25cf235ad1ea44713308b99757ec1c0179b7a
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2020-09-19 15:36:56 +0200

    gpio: exar: include idr.h
    This driver uses IDA APIs but doesn't include the relevant header. This
    fixes it.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/gpio/gpio-exar.c
+++ drivers/gpio/gpio-exar.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bitops.h>
 #include <linux/device.h>
 #include <linux/gpio/driver.h>
+#include <linux/idr.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
