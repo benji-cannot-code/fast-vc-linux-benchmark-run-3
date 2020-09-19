c6aa26acb659f138a39f8844701f729bb3a06a33
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2020-09-19 15:32:15 +0200

    gpio: exar: add a newline after the copyright notice
    It's customary to have a newline between the copyright header and the
    includes. Add one to gpio-exar.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

--- drivers/gpio/gpio-exar.c
+++ drivers/gpio/gpio-exar.c
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2015 Sudip Mukherjee <sudip.mukherjee@codethink.co.uk>
  */
+
 #include <linux/bitops.h>
 #include <linux/device.h>
 #include <linux/gpio/driver.h>
