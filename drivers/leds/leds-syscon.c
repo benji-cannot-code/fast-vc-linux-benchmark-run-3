6409b3ba57c4e242b157f5403112a3b7c94dbf99
Marek Behún <marek.behun@nic.cz>
2020-09-19 20:03:03 +0200

    leds: syscon: use struct led_init_data when registering
    By using struct led_init_data when registering we do not need to parse
    `label` DT property. Moreover `label` is deprecated and if it is not
    present but `color` and `function` are, LED core will compose a name
    from these properties instead.
    
    Signed-off-by: Marek Behún <marek.behun@nic.cz>
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Pavel Machek <pavel@ucw.cz>

--- drivers/leds/leds-syscon.c
+++ drivers/leds/leds-syscon.c
@@ -56,6 +56,7 @@ static void syscon_led_set(struct led_classdev *led_cdev,
 
 static int syscon_led_probe(struct platform_device *pdev)
 {
+	struct led_init_data init_data = {};
 	struct device *dev = &pdev->dev;
 	struct device_node *np = dev_of_node(dev);
 	struct device *parent;
@@ -85,8 +86,6 @@ static int syscon_led_probe(struct platform_device *pdev)
 		return -EINVAL;
 	if (of_property_read_u32(np, "mask", &sled->mask))
 		return -EINVAL;
-	sled->cdev.name =
-		of_get_property(np, "label", NULL) ? : np->name;
 	sled->cdev.default_trigger =
 		of_get_property(np, "linux,default-trigger", NULL);
 
@@ -116,7 +115,9 @@ static int syscon_led_probe(struct platform_device *pdev)
 	}
 	sled->cdev.brightness_set = syscon_led_set;
 
-	ret = devm_led_classdev_register(dev, &sled->cdev);
+	init_data.fwnode = of_fwnode_handle(np);
+
+	ret = devm_led_classdev_register_ext(dev, &sled->cdev, &init_data);
 	if (ret < 0)
 		return ret;
 
