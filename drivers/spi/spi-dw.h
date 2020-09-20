68332880a7d590e4db133d992b7b9521b445bd99
Serge Semin <Sergey.Semin@baikalelectronics.ru>
2020-09-20 14:28:45 +0300

    spi: dw: Discard IRQ threshold macro
    The macro has been unused since a half of FIFO length was defined to be a
    marker of the IRQ. Let's remove it definition.
    
    Signed-off-by: Serge Semin <Sergey.Semin@baikalelectronics.ru>
    Link: https://lore.kernel.org/r/20200920112914.26501-2-Sergey.Semin@baikalelectronics.ru
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/spi/spi-dw.h
+++ drivers/spi/spi-dw.h
@@ -93,9 +93,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SPI_DMA_RDMAE			(1 << 0)
 #define SPI_DMA_TDMAE			(1 << 1)
 
-/* TX RX interrupt level threshold, max can be 256 */
-#define SPI_INT_THRESHOLD		32
-
 enum dw_ssi_type {
 	SSI_MOTO_SPI = 0,
 	SSI_TI_SSP,
