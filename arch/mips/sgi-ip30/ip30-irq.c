ce8ef8bf6fce486317e294b21c7a5f644790995d
Thomas Bogendoerfer <tsbogend@alpha.franken.de>
2020-09-20 22:51:50 +0200

    MIPS: SGI-IP30: Move irq bits to better header files
    Move HEART specific parts of mach-ip30/irq.h to asm/sgi/heart.h and IP30
    specific parts to sgi-ip30/ip30-common.h.
    
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/sgi-ip30/ip30-irq.c
+++ arch/mips/sgi-ip30/ip30-irq.c
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/irq_cpu.h>
 #include <asm/sgi/heart.h>
 
+#include "ip30-common.h"
+
 struct heart_irq_data {
 	u64	*irq_mask;
 	int	cpu;
