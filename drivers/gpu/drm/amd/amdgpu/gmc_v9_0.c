27095d876cee4e9f6e0280b02547bafb5698e036
Liu Shixin <liushixin2@huawei.com>
2020-09-21 16:24:29 +0800

    drm/amdgpu/gmc9: simplify the return expression of gmc_v9_0_suspend
    Simplify the return expression.
    
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/gmc_v9_0.c
+++ drivers/gpu/drm/amd/amdgpu/gmc_v9_0.c
@@ -1684,14 +1684,9 @@ static int gmc_v9_0_hw_fini(void *handle)
 
 static int gmc_v9_0_suspend(void *handle)
 {
-	int r;
 	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
 
-	r = gmc_v9_0_hw_fini(adev);
-	if (r)
-		return r;
-
-	return 0;
+	return gmc_v9_0_hw_fini(adev);
 }
 
 static int gmc_v9_0_resume(void *handle)
