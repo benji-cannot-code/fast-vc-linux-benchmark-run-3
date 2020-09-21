e714f42a0f27bef86760bd2c723b9bea9d3302f8
Vladyslav Tarasiuk <vladyslavt@nvidia.com>
2020-09-21 13:41:03 +0200

    net/mlx5e: Validate stop_room size upon user input
    Stop room is a space that may be taken by WQEs in the SQ during a packet
    transmit. It is used to check if next packet has enough room in the SQ.
    Stop room guarantees this packet can be served and if not, the queue is
    stopped, so no more packets are passed to the driver until it's ready.
    
    Currently, stop_room size is calculated and validated upon tx queues
    allocation. This makes it impossible to know if user provided valid
    input for certain parameters when interface is down.
    
    Instead, store stop_room in mlx5e_sq_param and create
    mlx5e_validate_params(), to validate its fields upon user input even
    when the interface is down.
    
    Signed-off-by: Vladyslav Tarasiuk <vladyslavt@nvidia.com>
    Reviewed-by: Tariq Toukan <tariqt@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_ethtool.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_ethtool.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "en.h"
 #include "en/port.h"
+#include "en/params.h"
 #include "en/xsk/pool.h"
 #include "lib/clock.h"
 
@@ -369,6 +370,10 @@ int mlx5e_ethtool_set_ringparam(struct mlx5e_priv *priv,
 	new_channels.params = priv->channels.params;
 	new_channels.params.log_rq_mtu_frames = log_rq_size;
 	new_channels.params.log_sq_size = log_sq_size;
+
+	err = mlx5e_validate_params(priv, &new_channels.params);
+	if (err)
+		goto unlock;
 
 	if (!test_bit(MLX5E_STATE_OPENED, &priv->state)) {
 		priv->channels.params = new_channels.params;
