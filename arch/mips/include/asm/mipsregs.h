15dd39f934104105283b71a11e7711c242b76a8e
Huacai Chen <chenhc@lemote.com>
2020-09-21 17:12:27 +0800

    MIPS: Loongson-3: Enable COP2 usage in kernel
    Loongson-3's COP2 is Multi-Media coprocessor, it is disabled in kernel
    mode by default. However, gslq/gssq (16-bytes load/store instructions)
    overrides the instruction format of lwc2/swc2. If we wan't to use gslq/
    gssq for optimization in kernel, we should enable COP2 usage in kernel.
    
    Please pay attention that in this patch we only enable COP2 in kernel,
    which means it will lose ST0_CU2 when a process go to user space (try
    to use COP2 in user space will trigger an exception and then grab COP2,
    which is similar to FPU). And as a result, we need to modify the context
    switching code because the new scheduled process doesn't contain ST0_CU2
    in its THERAD_STATUS probably.
    
    For zboot, we disable gslq/gssq be generated by toolchain.
    
    Signed-off-by: Huacai Chen <chenhc@lemote.com>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/include/asm/mipsregs.h
+++ arch/mips/include/asm/mipsregs.h
@@ -390,6 +390,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ST0_CU3			0x80000000
 #define ST0_XX			0x80000000	/* MIPS IV naming */
 
+/* in-kernel enabled CUs */
+#ifdef CONFIG_CPU_LOONGSON64
+#define ST0_KERNEL_CUMASK      (ST0_CU0 | ST0_CU2)
+#else
+#define ST0_KERNEL_CUMASK      ST0_CU0
+#endif
+
 /*
  * Bitfields and bit numbers in the coprocessor 0 IntCtl register. (MIPSR2)
  */
