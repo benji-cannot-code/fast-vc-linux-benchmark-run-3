bbca8d0de03a97dc57680d09a95595dbc1634be5
Dietmar Eggemann <dietmar.eggemann@arm.com>
2020-09-22 10:39:34 +0200

    sched/cpupri: Remove pri_to_cpu[1]
    pri_to_cpu[1] isn't used since cpupri_set(..., newpri) is
    never called with newpri = 99.
    
    The valid RT priorities RT1..RT99 (p->rt_priority = [1..99]) map into
    cpupri (idx of pri_to_cpu[]) = [2..100]
    
    Current mapping:
    
    p->rt_priority   p->prio   newpri   cpupri
    
                                   -1       -1 (CPUPRI_INVALID)
    
                                  100        0 (CPUPRI_NORMAL)
    
                 1        98       98        2
               ...
                49        50       50       50
                50        49       49       51
               ...
                99         0        0      100
    
    So cpupri = 1 isn't used.
    
    Reduce the size of pri_to_cpu[] by 1 and adapt the cpupri
    implementation accordingly. This will save a useless for loop with an
    atomic_read in cpupri_find_fitness() calling __cpupri_find().
    
    New mapping:
    
    p->rt_priority   p->prio   newpri   cpupri
    
                                   -1       -1 (CPUPRI_INVALID)
    
                                  100        0 (CPUPRI_NORMAL)
    
                 1        98       98        1
               ...
                49        50       50       49
                50        49       49       50
               ...
                99         0        0       99
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200922083934.19275-3-dietmar.eggemann@arm.com

--- kernel/sched/cpupri.h
+++ kernel/sched/cpupri.h
@@ -1,11 +1,11 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
 
-#define CPUPRI_NR_PRIORITIES	(MAX_RT_PRIO + 1)
+#define CPUPRI_NR_PRIORITIES	MAX_RT_PRIO
 
 #define CPUPRI_INVALID		-1
 #define CPUPRI_NORMAL		 0
-/* values 2-100 are RT priorities 0-99 */
+/* values 1-99 are for RT1-RT99 priorities */
 
 struct cpupri_vec {
 	atomic_t		count;
