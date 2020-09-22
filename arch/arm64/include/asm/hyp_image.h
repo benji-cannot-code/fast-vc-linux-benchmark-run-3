2ba941a04e1e88fdd075ab1f064e765c033ea468
David Brazdil <dbrazdil@google.com>
2020-09-22 21:49:01 +0100

    kvm: arm64: Partially link nVHE hyp code, simplify HYPCOPY
    Relying on objcopy to prefix the ELF section names of the nVHE hyp code
    is brittle and prevents us from using wildcards to match specific
    section names.
    
    Improve the build rules by partially linking all '.nvhe.o' files and
    prefixing their ELF section names using a linker script. Continue using
    objcopy for prefixing ELF symbol names.
    
    One immediate advantage of this approach is that all subsections
    matching a pattern can be merged into a single prefixed section, eg.
    .text and .text.* can be linked into a single '.hyp.text'. This removes
    the need for -fno-reorder-functions on GCC and will be useful in the
    future too: LTO builds use .text subsections, compilers routinely
    generate .rodata subsections, etc.
    
    Partially linking all hyp code into a single object file also makes it
    easier to analyze.
    
    Signed-off-by: David Brazdil <dbrazdil@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Acked-by: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20200922204910.7265-2-dbrazdil@google.com

---
+++ arch/arm64/include/asm/hyp_image.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2020 Google LLC.
+ * Written by David Brazdil <dbrazdil@google.com>
+ */
+
+#ifndef __ARM64_HYP_IMAGE_H__
+#define __ARM64_HYP_IMAGE_H__
+
+#ifdef LINKER_SCRIPT
+
+/*
+ * KVM nVHE ELF section names are prefixed with .hyp, to separate them
+ * from the kernel proper.
+ */
+#define HYP_SECTION_NAME(NAME)	.hyp##NAME
+
+/* Defines an ELF hyp section from input section @NAME and its subsections. */
+#define HYP_SECTION(NAME) \
+	HYP_SECTION_NAME(NAME) : { *(NAME NAME##.*) }
+
+#endif /* LINKER_SCRIPT */
+
+#endif /* __ARM64_HYP_IMAGE_H__ */
