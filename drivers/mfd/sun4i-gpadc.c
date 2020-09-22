f3f0155f39f01b2ceefc3647be151991f28ed486
Rikard Falkeborn <rikard.falkeborn@gmail.com>
2020-09-22 21:26:58 +0200

    mfd: sun4i-gpadc: Constify static struct resource
    Constify adc_resources[]. The only usage is to assign its address to the
    resources field of the mfd_cell struct. This allows the compiler to put
    it in read-only memory. Done with the help of Coccinelle.
    
    Signed-off-by: Rikard Falkeborn <rikard.falkeborn@gmail.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/sun4i-gpadc.c
+++ drivers/mfd/sun4i-gpadc.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ARCH_SUN5I_A13 1
 #define ARCH_SUN6I_A31 2
 
-static struct resource adc_resources[] = {
+static const struct resource adc_resources[] = {
 	DEFINE_RES_IRQ_NAMED(SUN4I_GPADC_IRQ_FIFO_DATA, "FIFO_DATA_PENDING"),
 	DEFINE_RES_IRQ_NAMED(SUN4I_GPADC_IRQ_TEMP_DATA, "TEMP_DATA_PENDING"),
 };
