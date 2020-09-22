e3aa70fbf066f287236dc0c7d69ad4de34336a71
Rikard Falkeborn <rikard.falkeborn@gmail.com>
2020-09-22 21:26:54 +0200

    mfd: tps65xxx: Constify static struct resource in OMAP2+ drivers
    Constify a number of static struct resource. The only usage of the
    structs are to assign their address to the resources field in the
    mfd_cell struct. This allows the compiler to put them in read-only
    memory. Done with the help of Coccinelle.
    
    Signed-off-by: Rikard Falkeborn <rikard.falkeborn@gmail.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- drivers/mfd/tps65910.c
+++ drivers/mfd/tps65910.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of.h>
 #include <linux/of_device.h>
 
-static struct resource rtc_resources[] = {
+static const struct resource rtc_resources[] = {
 	{
 		.start  = TPS65910_IRQ_RTC_ALARM,
 		.end    = TPS65910_IRQ_RTC_ALARM,
