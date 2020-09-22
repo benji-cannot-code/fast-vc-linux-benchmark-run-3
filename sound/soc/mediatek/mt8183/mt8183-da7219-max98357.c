12cf5f840870438a50c121a8923ce25e1c54cb0e
Cheng-Yi Chiang <cychiang@chromium.org>
2020-09-22 14:23:16 +0800

    ASoC: hdmi-codec: Use set_jack ops to set jack
    Use set_jack ops to set jack so machine drivers do not need to include
    hdmi-codec.h explicitly.
    
    Signed-off-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Reviewed-by: Tzung-Bi Shih <tzungbi@google.com>
    Link: https://lore.kernel.org/r/20200922062316.1172935-1-cychiang@chromium.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/mediatek/mt8183/mt8183-da7219-max98357.c
+++ sound/soc/mediatek/mt8183/mt8183-da7219-max98357.c
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/of_device.h>
 #include <linux/pinctrl/consumer.h>
-#include <sound/hdmi-codec.h>
 #include <sound/jack.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
@@ -376,8 +375,8 @@ static int mt8183_da7219_max98357_hdmi_init(struct snd_soc_pcm_runtime *rtd)
 	if (ret)
 		return ret;
 
-	return hdmi_codec_set_jack_detect(asoc_rtd_to_codec(rtd, 0)->component,
-					  &priv->hdmi_jack);
+	return snd_soc_component_set_jack(asoc_rtd_to_codec(rtd, 0)->component,
+					  &priv->hdmi_jack, NULL);
 }
 
 static struct snd_soc_dai_link mt8183_da7219_dai_links[] = {
