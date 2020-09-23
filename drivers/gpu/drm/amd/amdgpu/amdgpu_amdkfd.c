51cbf1c68afb3e016128114dfc8ac9e9972d0c58
kernel test robot <lkp@intel.com>
2020-09-23 10:28:28 +0800

    drm/amdgpu: kfd_initialized can be static
    Fixes: c7651b73586600 ("drm/amdgpu: Fix handling of KFD initialization failures")
    Signed-off-by: kernel test robot <lkp@intel.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd.c
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 uint64_t amdgpu_amdkfd_total_mem_size;
 
-bool kfd_initialized;
+static bool kfd_initialized;
 
 int amdgpu_amdkfd_init(void)
 {
