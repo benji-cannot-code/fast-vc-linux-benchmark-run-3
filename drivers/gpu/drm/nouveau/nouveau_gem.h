872d97d8097d7ee188705b7479b7054ec0d94ade
Thomas Zimmermann <tzimmermann@suse.de>
2020-09-23 12:21:47 +0200

    drm/nouveau: Introduce GEM object functions
    GEM object functions deprecate several similar callback interfaces in
    struct drm_driver. This patch replaces the per-driver callbacks with
    per-instance callbacks in nouveau.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Acked-by: Christian König <christian.koenig@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200923102159.24084-11-tzimmermann@suse.de

--- drivers/gpu/drm/nouveau/nouveau_gem.h
+++ drivers/gpu/drm/nouveau/nouveau_gem.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "nouveau_drv.h"
 #include "nouveau_bo.h"
 
+extern const struct drm_gem_object_funcs nouveau_gem_object_funcs;
+
 static inline struct nouveau_bo *
 nouveau_gem_object(struct drm_gem_object *gem)
 {
