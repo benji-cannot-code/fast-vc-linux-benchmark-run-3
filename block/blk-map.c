aafe09a35463583b21bd33c01623ff5e0820b246
Christoph Hellwig <hch@lst.de>
2020-09-23 17:07:13 +0200

    block: fix bmd->is_null_mapped initialization
    bmd is allocated using kmalloc in bio_alloc_map_data, so make sure
    is_null_mapped is properly initialized to false for the !null_mapped
    case.
    
    Fixes: f3256075ba49 ("block: remove the BIO_NULL_MAPPED flag")
    Reported-by: Marc Hartmayer <mhartmay@linux.ibm.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-map.c
+++ block/blk-map.c
@@ -149,6 +149,7 @@ static int bio_copy_user_iov(struct request *rq, struct rq_map_data *map_data,
 	 * shortlived one.
 	 */
 	bmd->is_our_pages = !map_data;
+	bmd->is_null_mapped = (map_data && map_data->null_mapped);
 
 	nr_pages = DIV_ROUND_UP(offset + len, PAGE_SIZE);
 	if (nr_pages > BIO_MAX_PAGES)
@@ -219,8 +220,6 @@ static int bio_copy_user_iov(struct request *rq, struct rq_map_data *map_data,
 	}
 
 	bio->bi_private = bmd;
-	if (map_data && map_data->null_mapped)
-		bmd->is_null_mapped = true;
 
 	bounce_bio = bio;
 	ret = blk_rq_append_bio(rq, &bounce_bio);
