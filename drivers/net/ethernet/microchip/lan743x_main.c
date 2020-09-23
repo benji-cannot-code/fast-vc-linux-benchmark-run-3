d2c96720335ca36e35220567543c04e701db00fe
Zheng Yongjun <zhengyongjun3@huawei.com>
2020-09-23 11:21:40 +0800

    net: microchip: Make `lan743x_pm_suspend` function return right value
    drivers/net/ethernet/microchip/lan743x_main.c: In function lan743x_pm_suspend:
    
    `ret` is set but not used. In fact, `pci_prepare_to_sleep` function value should
    be the right value of `lan743x_pm_suspend` function, therefore, fix it.
    
    Signed-off-by: Zheng Yongjun <zhengyongjun3@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/microchip/lan743x_main.c
+++ drivers/net/ethernet/microchip/lan743x_main.c
@@ -3039,7 +3039,6 @@ static int lan743x_pm_suspend(struct device *dev)
 	struct pci_dev *pdev = to_pci_dev(dev);
 	struct net_device *netdev = pci_get_drvdata(pdev);
 	struct lan743x_adapter *adapter = netdev_priv(netdev);
-	int ret;
 
 	lan743x_pcidev_shutdown(pdev);
 
@@ -3052,9 +3051,7 @@ static int lan743x_pm_suspend(struct device *dev)
 		lan743x_pm_set_wol(adapter);
 
 	/* Host sets PME_En, put D3hot */
-	ret = pci_prepare_to_sleep(pdev);
-
-	return 0;
+	return pci_prepare_to_sleep(pdev);;
 }
 
 static int lan743x_pm_resume(struct device *dev)
