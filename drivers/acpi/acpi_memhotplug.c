27b78475a2287bd50aa903e418140e9358ef8000
Hanjun Guo <guohanjun@huawei.com>
2020-09-24 10:56:55 +0800

    ACPI: memhotplug: Remove leftover ACPICA debug functionality
    After commit 0a34764411aa ("ACPI / scan: Make memory hotplug drive
    use struct acpi_scan_handler"), all the ACPICA debug functionality
    was removed, remove the leftover ACPICA debug code.
    
    Signed-off-by: Hanjun Guo <guohanjun@huawei.com>
    [ rjw: Subject edit ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpi_memhotplug.c
+++ drivers/acpi/acpi_memhotplug.c
@@ -23,13 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ACPI_MEMORY_DEVICE_HID			"PNP0C80"
 #define ACPI_MEMORY_DEVICE_NAME			"Hotplug Mem Device"
 
-#define _COMPONENT		ACPI_MEMORY_DEVICE_COMPONENT
-
-#undef PREFIX
-#define 	PREFIX		"ACPI:memory_hp:"
-
-ACPI_MODULE_NAME("acpi_memhotplug");
-
 static const struct acpi_device_id memory_device_ids[] = {
 	{ACPI_MEMORY_DEVICE_HID, 0},
 	{"", 0},
