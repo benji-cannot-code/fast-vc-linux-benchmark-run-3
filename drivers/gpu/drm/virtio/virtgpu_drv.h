5221a4d67f6146421064498f0b77b4796ba0f9f8
Gurchetan Singh <gurchetansingh@chromium.org>
2020-09-23 17:31:57 -0700

    drm/virtio: blob prep: make CPU responses more generic
    RESOURCE_MAP_BLOB / RESOURCE_UNMAP_BLOB can use this.
    
    Signed-off-by: Gurchetan Singh <gurchetansingh@chromium.org>
    Acked-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20200924003214.662-2-gurchetansingh@chromium.org
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- drivers/gpu/drm/virtio/virtgpu_drv.h
+++ drivers/gpu/drm/virtio/virtgpu_drv.h
@@ -50,9 +50,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRIVER_MINOR 1
 #define DRIVER_PATCHLEVEL 0
 
-#define UUID_INITIALIZING 0
-#define UUID_INITIALIZED 1
-#define UUID_INITIALIZATION_FAILED 2
+#define STATE_INITIALIZING 0
+#define STATE_OK 1
+#define STATE_ERR 2
 
 struct virtio_gpu_object_params {
 	uint32_t format;
