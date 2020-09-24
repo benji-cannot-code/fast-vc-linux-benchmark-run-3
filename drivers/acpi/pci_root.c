eeefe916c72aed61f462d981e1eff802a2ac6f82
Hanjun Guo <guohanjun@huawei.com>
2020-09-24 10:57:02 +0800

    ACPI: PCI: Remove unused ACPICA debug code
    The ACPICA debug code _COMPONENT and ACPI_MODULE_NAME() are
    not used, so can be removed.
    
    Signed-off-by: Hanjun Guo <guohanjun@huawei.com>
    [ rjw: Subject edit ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/pci_root.c
+++ drivers/acpi/pci_root.c
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "internal.h"
 
-#define _COMPONENT		ACPI_PCI_COMPONENT
-ACPI_MODULE_NAME("pci_root");
 #define ACPI_PCI_ROOT_CLASS		"pci_bridge"
 #define ACPI_PCI_ROOT_DEVICE_NAME	"PCI Root Bridge"
 static int acpi_pci_root_add(struct acpi_device *device,
