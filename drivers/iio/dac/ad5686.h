6dda3de75e406fa47b9f61852c28bea78046efcc
Michael Auchter <michael.auchter@ni.com>
2020-09-24 14:52:12 -0500

    iio: dac: ad5686: add support for AD5338R
    The AD5338R is a 10-bit DAC with 2 outputs and an internal 2.5V
    reference (enabled by default). The register configuration is nearly
    identical to the AD5696R DAC that's already supported by this driver,
    with the channel selection bits being the only thing different.
    
    Signed-off-by: Michael Auchter <michael.auchter@ni.com>
    Link: https://lore.kernel.org/r/20200924195215.49443-1-michael.auchter@ni.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/dac/ad5686.h
+++ drivers/iio/dac/ad5686.h
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 enum ad5686_supported_device_ids {
 	ID_AD5310R,
 	ID_AD5311R,
+	ID_AD5338R,
 	ID_AD5671R,
 	ID_AD5672R,
 	ID_AD5674R,
