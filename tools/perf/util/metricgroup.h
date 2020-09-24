291f42f70d48bdaa6459a50b84e728035e0aeff1
Namhyung Kim <namhyung@kernel.org>
2020-09-24 21:44:53 +0900

    perf tools: Copy metric events properly when expand cgroups
    The metricgroup__copy_metric_events() is to handle metrics events when
    expanding event for cgroups.  As the metric events keep pointers to
    evsel, it should be refreshed when events are cloned during the
    operation.
    
    The perf_stat__collect_metric_expr() is also called in case an event has
    a metric directly.
    
    During the copy, it references evsel by index as the evlist now has
    cloned evsels for the given cgroup.
    
    Also kernel test robot found an issue in the python module import so add
    empty implementations of those two functions to fix it.
    
    Reported-by: kernel test robot <rong.a.chen@intel.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Acked-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Ian Rogers <irogers@google.com>
    Cc: John Garry <john.garry@huawei.com>
    Cc: Kajol Jain <kjain@linux.ibm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lore.kernel.org/lkml/20200924124455.336326-4-namhyung@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/metricgroup.h
+++ tools/perf/util/metricgroup.h
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdbool.h>
 #include "pmu-events/pmu-events.h"
 
+struct evlist;
 struct evsel;
 struct evlist;
 struct option;
 struct rblist;
 struct pmu_events_map;
+struct cgroup;
 
 struct metric_event {
 	struct rb_node nd;
@@ -56,4 +58,8 @@ void metricgroup__print(bool metrics, bool groups, char *filter,
 bool metricgroup__has_metric(const char *metric);
 int arch_get_runtimeparam(struct pmu_event *pe __maybe_unused);
 void metricgroup__rblist_exit(struct rblist *metric_events);
+
+int metricgroup__copy_metric_events(struct evlist *evlist, struct cgroup *cgrp,
+				    struct rblist *new_metric_events,
+				    struct rblist *old_metric_events);
 #endif
