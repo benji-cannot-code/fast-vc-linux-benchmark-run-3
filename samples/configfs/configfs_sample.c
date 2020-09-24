552a01566a1604e5a40f6e3fa7c7f2248c6d1ff0
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2020-09-24 14:45:25 +0200

    samples: configfs: don't use spaces before tabs
    The copyright notice alarms checkpatch.pl of usin spaces before tabs.
    Fix this.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- samples/configfs/configfs_sample.c
+++ samples/configfs/configfs_sample.c
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *      macros defined by configfs.h
  *
  * Based on sysfs:
- * 	sysfs is Copyright (C) 2001, 2002, 2003 Patrick Mochel
+ *      sysfs is Copyright (C) 2001, 2002, 2003 Patrick Mochel
  *
  * configfs Copyright (C) 2005 Oracle.  All rights reserved.
  */
