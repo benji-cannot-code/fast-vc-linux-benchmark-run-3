f98e157f7ff0d0ef707bf6afde2e50fa293b14f1
Arnd Bergmann <arnd@arndb.de>
2020-09-24 17:39:11 +0200

    timekeeping: remove xtime_update
    There are no more users of xtime_update aside from legacy_timer_tick(),
    so fold it into that function and remove the declaration.
    
    update_process_times() is now only called inside of the kernel/time/
    code, so the declaration can be moved there.
    
    Reviewed-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Tested-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- include/linux/timekeeping.h
+++ include/linux/timekeeping.h
@@ -11,8 +11,6 @@ void timekeeping_init(void);
 extern int timekeeping_suspended;
 
 /* Architecture timer tick functions: */
-extern void update_process_times(int user);
-extern void xtime_update(unsigned long ticks);
 extern void legacy_timer_tick(unsigned long ticks);
 
 /*
