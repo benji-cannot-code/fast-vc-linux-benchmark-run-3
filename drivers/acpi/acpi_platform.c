3c630c914d2f707028db80ee98d3708284567a3a
Hanjun Guo <guohanjun@huawei.com>
2020-09-24 10:56:56 +0800

    ACPI: platform: Remove ACPI_MODULE_NAME()
    ACPI_MODULE_NAME() is not used in the acpi_platform.c, remove it.
    
    Signed-off-by: Hanjun Guo <guohanjun@huawei.com>
    [ rjw: Subject edit ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpi_platform.c
+++ drivers/acpi/acpi_platform.c
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "internal.h"
 
-ACPI_MODULE_NAME("platform");
-
 static const struct acpi_device_id forbidden_id_list[] = {
 	{"PNP0000",  0},	/* PIC */
 	{"PNP0100",  0},	/* Timer */
