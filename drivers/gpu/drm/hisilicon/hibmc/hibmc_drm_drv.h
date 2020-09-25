9587ce0617c31d3c81a381fa2ea6e7d403e28638
Tian Tao <tiantao6@hisilicon.com>
2020-09-25 15:48:06 +0800

    drm/hisilicon: Deleted the drm_device declaration
    drm_framebuffer.h already declares struct drm_device, so there's no
    need to declare it in hibmc_drm_drv.h
    
    v2:
    fixed spelling errors in commit message.
    
    v3:
    rewrite the commit message.
    
    Signed-off-by: Tian Tao <tiantao6@hisilicon.com>
    Acked-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/1601020086-43652-1-git-send-email-tiantao6@hisilicon.com

--- drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_drv.h
+++ drivers/gpu/drm/hisilicon/hibmc/hibmc_drm_drv.h
@@ -23,8 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_fb_helper.h>
 #include <drm/drm_framebuffer.h>
 
-struct drm_device;
-
 struct hibmc_connector {
 	struct drm_connector base;
 
