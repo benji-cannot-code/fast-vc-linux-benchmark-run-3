da39fbda9726932d2ccbbdd741c426d6dd27b527
Thomas Zimmermann <tzimmermann@suse.de>
2020-09-25 13:55:58 +0200

    dma-buf: Add struct dma-buf-map for storing struct dma_buf.vaddr_ptr
    The new type struct dma_buf_map represents a mapping of dma-buf memory
    into kernel space. It contains a flag, is_iomem, that signals users to
    access the mapped memory with I/O operations instead of regular loads
    and stores.
    
    It was assumed that DMA buffer memory can be accessed with regular load
    and store operations. Some architectures, such as sparc64, require the
    use of I/O operations to access dma-map buffers that are located in I/O
    memory. Providing struct dma_buf_map allows drivers to implement this.
    This was specifically a problem when refreshing the graphics framebuffer
    on such systems. [1]
    
    As the first step, struct dma_buf stores an instance of struct dma_buf_map
    internally. Afterwards, dma-buf's vmap and vunmap interfaces are be
    converted. Finally, affected drivers can be fixed.
    
    v3:
    	* moved documentation into separate patch
    	* test for NULL pointers with !<ptr>
    
    [1] https://lore.kernel.org/dri-devel/20200725191012.GA434957@ravnborg.org/
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Acked-by: Sumit Semwal <sumit.semwal@linaro.org>
    Acked-by: Sam Ravnborg <sam@ravnborg.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200925115601.23955-2-tzimmermann@suse.de

--- include/linux/dma-buf.h
+++ include/linux/dma-buf.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DMA_BUF_H__
 #define __DMA_BUF_H__
 
+#include <linux/dma-buf-map.h>
 #include <linux/file.h>
 #include <linux/err.h>
 #include <linux/scatterlist.h>
@@ -310,7 +311,7 @@ struct dma_buf {
 	const struct dma_buf_ops *ops;
 	struct mutex lock;
 	unsigned vmapping_counter;
-	void *vmap_ptr;
+	struct dma_buf_map vmap_ptr;
 	const char *exp_name;
 	const char *name;
 	spinlock_t name_lock;
