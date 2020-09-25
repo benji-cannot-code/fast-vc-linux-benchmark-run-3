518dff5696b86fbe53acb9d944c7a5c690d2d914
Bingbu Cao <bingbu.cao@intel.com>
2020-09-25 03:59:20 +0200

    media: ov2740: change the minimal exposure value to 4
    The minimal valid exposure value should be 4 lines instead of 8 for
    ov2740 mannual exposure control.
    
    Signed-off-by: Bingbu Cao <bingbu.cao@intel.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/i2c/ov2740.c
+++ drivers/media/i2c/ov2740.c
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Exposure controls from sensor */
 #define OV2740_REG_EXPOSURE		0x3500
-#define OV2740_EXPOSURE_MIN		8
+#define OV2740_EXPOSURE_MIN		4
 #define OV2740_EXPOSURE_MAX_MARGIN	8
 #define OV2740_EXPOSURE_STEP		1
 
