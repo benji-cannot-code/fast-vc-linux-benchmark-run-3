cf6d9f9357b15131202e26d409b07fa4036ecb57
Luca Coelho <luciano.coelho@intel.com>
2020-09-26 00:30:51 +0300

    iwlwifi: remove iwl_validate_sar_geo_profile() export
    Only iwlmvm uses this function and it's so simple that it's clearer if
    it's spelled out in the code anyway, so remove it and add the check
    where needed.
    
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Link: https://lore.kernel.org/r/iwlwifi.20200926002540.9e2f296f5cfc.I4b2c025768b5ceff93a80ba0ae9ee7784d6d7402@changeid
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/fw/acpi.h
+++ drivers/net/wireless/intel/iwlwifi/fw/acpi.h
@@ -195,9 +195,6 @@ int iwl_sar_get_wgds_table(struct iwl_fw_runtime *fwrt);
 
 bool iwl_sar_geo_support(struct iwl_fw_runtime *fwrt);
 
-int iwl_validate_sar_geo_profile(struct iwl_fw_runtime *fwrt,
-				 struct iwl_host_cmd *cmd);
-
 int iwl_sar_geo_init(struct iwl_fw_runtime *fwrt,
 		     struct iwl_per_chain_offset_group *table);
 
@@ -277,12 +274,6 @@ static inline int iwl_sar_get_wgds_table(struct iwl_fw_runtime *fwrt)
 static inline bool iwl_sar_geo_support(struct iwl_fw_runtime *fwrt)
 {
 	return false;
-}
-
-static inline int iwl_validate_sar_geo_profile(struct iwl_fw_runtime *fwrt,
-					       struct iwl_host_cmd *cmd)
-{
-	return -ENOENT;
 }
 
 static inline int iwl_sar_geo_init(struct iwl_fw_runtime *fwrt,
