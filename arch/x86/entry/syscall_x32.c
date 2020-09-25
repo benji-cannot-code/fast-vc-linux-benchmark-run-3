ab4678c1b04b006c105cd9c8d95e71a2e5c6b737
Christoph Hellwig <hch@lst.de>
2020-09-25 06:51:44 +0200

    fs: remove compat_sys_vmsplice
    Now that import_iovec handles compat iovecs, the native vmsplice syscall
    can be used for the compat case as well.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/x86/entry/syscall_x32.c
+++ arch/x86/entry/syscall_x32.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __x32_sys_writev	__x64_sys_writev
 #define __x32_sys_getsockopt	__x64_sys_getsockopt
 #define __x32_sys_setsockopt	__x64_sys_setsockopt
+#define __x32_sys_vmsplice	__x64_sys_vmsplice
 
 #define __SYSCALL_64(nr, sym)
 
