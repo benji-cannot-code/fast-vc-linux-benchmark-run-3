96081c0178178f70b16c76b5d5d0b530189e2fa5
Jacob Pan <jacob.pan.linux@gmail.com>
2020-09-25 09:32:44 -0700

    iommu/uapi: Use named union for user data
    IOMMU UAPI data size is filled by the user space which must be validated
    by the kernel. To ensure backward compatibility, user data can only be
    extended by either re-purpose padding bytes or extend the variable sized
    union at the end. No size change is allowed before the union. Therefore,
    the minimum size is the offset of the union.
    
    To use offsetof() on the union, we must make it named.
    
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Link: https://lore.kernel.org/linux-iommu/20200611145518.0c2817d6@x1.home/
    Link: https://lore.kernel.org/r/1601051567-54787-4-git-send-email-jacob.jun.pan@linux.intel.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- include/uapi/linux/iommu.h
+++ include/uapi/linux/iommu.h
@@ -268,7 +268,7 @@ struct iommu_cache_invalidate_info {
 	union {
 		struct iommu_inv_pasid_info pasid_info;
 		struct iommu_inv_addr_info addr_info;
-	};
+	} granu;
 };
 
 /**
@@ -334,7 +334,7 @@ struct iommu_gpasid_bind_data {
 	/* Vendor specific data */
 	union {
 		struct iommu_gpasid_bind_data_vtd vtd;
-	};
+	} vendor;
 };
 
 #endif /* _UAPI_IOMMU_H */
