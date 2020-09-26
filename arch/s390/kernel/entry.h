f187fdb7d64a87af5b6817789f5bc99d8db33316
Vasily Gorbik <gor@linux.ibm.com>
2020-09-27 01:34:25 +0200

    s390: remove orphaned extern variables declarations
    arch/s390/kernel/entry.h: suspend_zero_pages - only declaration left
    after commit 394216275c7d ("s390: remove broken hibernate / power
    management support")
    
    arch/s390/include/asm/setup.h: vmhalt_cmd - only declaration left after
    commit 99ca4e582d4a ("[S390] kernel: Shutdown Actions Interface")
    
    arch/s390/include/asm/setup.h: vmpoff_cmd - only declaration left after
    commit 99ca4e582d4a ("[S390] kernel: Shutdown Actions Interface")
    
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- arch/s390/kernel/entry.h
+++ arch/s390/kernel/entry.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/idle.h>
 
 extern void *restart_stack;
-extern unsigned long suspend_zero_pages;
 
 void system_call(void);
 void pgm_check_handler(void);
