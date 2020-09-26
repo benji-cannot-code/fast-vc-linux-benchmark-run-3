0104f1f28f4164b79ad59a2562d9fb43e228fbb0
Ursula Braun <ubraun@linux.ibm.com>
2020-09-26 12:44:31 +0200

    net/smc: introduce CLC first contact extension
    SMC Version 2 defines a first contact extension for CLC accept
    and CLC confirm. This patch covers sending and receiving of the
    CLC first contact extension.
    
    Signed-off-by: Ursula Braun <ubraun@linux.ibm.com>
    Signed-off-by: Karsten Graul <kgraul@linux.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/smc/smc.h
+++ net/smc/smc.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 					 * devices
 					 */
 
+#define SMC_MAX_HOSTNAME_LEN	32
 #define SMC_MAX_EID_LEN		32
 
 extern struct proto smc_proto;
