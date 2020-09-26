894961da562a12aeffc60820fc5f0074506aa5f9
Ursula Braun <ubraun@linux.ibm.com>
2020-09-26 12:44:23 +0200

    net/smc: prepare for more proposed ISM devices
    SMCD Version 2 allows proposing of up to 8 ISM devices in addition
    to the native ISM device of SMCD Version 1.
    This patch prepares the struct smc_init_info to deal with these
    additional 8 ISM devices.
    
    Signed-off-by: Ursula Braun <ubraun@linux.ibm.com>
    Signed-off-by: Karsten Graul <kgraul@linux.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/smc/smc.h
+++ net/smc/smc.h
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SMCPROTO_SMC		0	/* SMC protocol, IPv4 */
 #define SMCPROTO_SMC6		1	/* SMC protocol, IPv6 */
 
+#define SMC_MAX_ISM_DEVS	8	/* max # of proposed non-native ISM
+					 * devices
+					 */
+
 extern struct proto smc_proto;
 extern struct proto smc_proto6;
 
