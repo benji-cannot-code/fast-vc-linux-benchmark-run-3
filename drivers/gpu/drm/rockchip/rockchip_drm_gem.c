2d9dff5ae9141291a985f1dd853f81ec6e0e5086
Thomas Zimmermann <tzimmermann@suse.de>
2020-09-28 10:16:43 +0200

    drm/rockchip: Include <drm/drm_gem_cma_helper> for drm_gem_cm_vm_ops
    Include <drm/drm_gem_cma_helper.h> to get drm_gem_cma_vm_ops. Fallout
    from the recent conversion to GEM object functions.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Sam Ravnborg <sam@ravnborg.org>
    Reported-by: kernel test robot <lkp@intel.com>
    Fixes: 0d590af3140d ("drm/rockchip: Convert to drm_gem_object_funcs")
    Cc: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Christian König <christian.koenig@amd.com>
    Cc: Sandy Huang <hjc@rock-chips.com>
    Cc: "Heiko Stübner" <heiko@sntech.de>
    Cc: dri-devel@lists.freedesktop.org
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-rockchip@lists.infradead.org
    Link: https://patchwork.freedesktop.org/patch/msgid/20200928081643.8575-1-tzimmermann@suse.de

--- drivers/gpu/drm/rockchip/rockchip_drm_gem.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_gem.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <drm/drm.h>
 #include <drm/drm_gem.h>
+#include <drm/drm_gem_cma_helper.h>
 #include <drm/drm_prime.h>
 #include <drm/drm_vma_manager.h>
 
