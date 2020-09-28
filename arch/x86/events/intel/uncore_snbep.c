10794d80adcc6323360da83290715b38e643edb9
Alexander Antonov <alexander.antonov@linux.intel.com>
2020-09-28 13:21:33 +0300

    perf/x86/intel/uncore: Fix for iio mapping on Skylake Server
    Introduced early attributes /sys/devices/uncore_iio_<pmu_idx>/die* are
    initialized by skx_iio_set_mapping(), however, for example, for multiple
    segment platforms skx_iio_get_topology() returns -EPERM before a list of
    attributes in skx_iio_mapping_group will have been initialized.
    As a result the list is being NULL. Thus the warning
    "sysfs: (bin_)attrs not set by subsystem for group: uncore_iio_*/" appears
    and uncore_iio pmus are not available in sysfs. Clear IIO attr_update
    to properly handle the cases when topology information cannot be
    retrieved.
    
    Fixes: bb42b3d39781 ("perf/x86/intel/uncore: Expose an Uncore unit to IIO PMON mapping")
    Reported-by: Kyle Meyer <kyle.meyer@hpe.com>
    Suggested-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Alexander Antonov <alexander.antonov@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Alexei Budankov <alexey.budankov@linux.intel.com>
    Reviewed-by: Kan Liang <kan.liang@linux.intel.com>
    Link: https://lkml.kernel.org/r/20200928102133.61041-1-alexander.antonov@linux.intel.com

--- arch/x86/events/intel/uncore_snbep.c
+++ arch/x86/events/intel/uncore_snbep.c
@@ -3755,7 +3755,9 @@ static int skx_iio_set_mapping(struct intel_uncore_type *type)
 
 	ret = skx_iio_get_topology(type);
 	if (ret)
-		return ret;
+		goto clear_attr_update;
+
+	ret = -ENOMEM;
 
 	/* One more for NULL. */
 	attrs = kcalloc((uncore_max_dies() + 1), sizeof(*attrs), GFP_KERNEL);
@@ -3787,8 +3789,9 @@ err:
 	kfree(eas);
 	kfree(attrs);
 	kfree(type->topology);
+clear_attr_update:
 	type->attr_update = NULL;
-	return -ENOMEM;
+	return ret;
 }
 
 static void skx_iio_cleanup_mapping(struct intel_uncore_type *type)
