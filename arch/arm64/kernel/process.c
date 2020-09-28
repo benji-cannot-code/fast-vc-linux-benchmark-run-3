5dbac17296289ee9daeef47e58f0e9ca46b8169a
Will Deacon <will@kernel.org>
2020-09-28 14:03:00 +0100

    arm64: Add support for PR_SPEC_DISABLE_NOEXEC prctl() option
    The PR_SPEC_DISABLE_NOEXEC option to the PR_SPEC_STORE_BYPASS prctl()
    allows the SSB mitigation to be enabled only until the next execve(),
    at which point the state will revert back to PR_SPEC_ENABLE and the
    mitigation will be disabled.
    
    Add support for PR_SPEC_DISABLE_NOEXEC on arm64.
    
    Reported-by: Anthony Steinhauser <asteinhauser@google.com>
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/lockdep.h>
 #include <linux/mman.h>
 #include <linux/mm.h>
+#include <linux/nospec.h>
 #include <linux/stddef.h>
 #include <linux/sysctl.h>
 #include <linux/unistd.h>
@@ -610,6 +611,11 @@ void arch_setup_new_exec(void)
 	current->mm->context.flags = is_compat_task() ? MMCF_AARCH32 : 0;
 
 	ptrauth_thread_init_user(current);
+
+	if (task_spec_ssb_noexec(current)) {
+		arch_prctl_spec_ctrl_set(current, PR_SPEC_STORE_BYPASS,
+					 PR_SPEC_ENABLE);
+	}
 }
 
 #ifdef CONFIG_ARM64_TAGGED_ADDR_ABI
