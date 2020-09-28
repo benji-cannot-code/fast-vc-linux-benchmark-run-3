4682f1d60f3eca099419bfd6620912dbbab72ebe
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2020-09-28 14:06:14 +0200

    regmap: destroy mutex (if used) in regmap_exit()
    While not destroying mutexes doesn't lead to memory leaks, it's still
    the correct thing to do for mutex debugging accounting.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Link: https://lore.kernel.org/r/20200928120614.23172-1-brgl@bgdev.pl
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/base/regmap/regmap.c
+++ drivers/base/regmap/regmap.c
@@ -1371,6 +1371,8 @@ void regmap_exit(struct regmap *map)
 	}
 	if (map->hwlock)
 		hwspin_lock_free(map->hwlock);
+	if (map->lock == regmap_lock_mutex)
+		mutex_destroy(&map->mutex);
 	kfree_const(map->name);
 	kfree(map->patch);
 	kfree(map);
