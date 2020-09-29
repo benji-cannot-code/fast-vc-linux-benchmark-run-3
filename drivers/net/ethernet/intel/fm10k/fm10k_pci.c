628bb5a40d14133fa98315e56b4e92ae916d434b
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2020-09-29 22:25:19 +0200

    net: intel: Remove in_interrupt() warnings
    in_interrupt() is ill defined and does not provide what the name
    suggests. The usage especially in driver code is deprecated and a tree wide
    effort to clean up and consolidate the (ab)usage of in_interrupt() and
    related checks is happening.
    
    In this case the checks cover only parts of the contexts in which these
    functions cannot be called. They fail to detect preemption or interrupt
    disabled invocations.
    
    As the functions which are invoked from the various places contain already
    a broad variety of checks (always enabled or debug option dependent) cover
    all invalid conditions already, there is no point in having inconsistent
    warnings in those drivers.
    
    Just remove them.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Alexander Duyck <alexander.h.duyck@linux.intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/intel/fm10k/fm10k_pci.c
+++ drivers/net/ethernet/intel/fm10k/fm10k_pci.c
@@ -222,8 +222,6 @@ static bool fm10k_prepare_for_reset(struct fm10k_intfc *interface)
 {
 	struct net_device *netdev = interface->netdev;
 
-	WARN_ON(in_interrupt());
-
 	/* put off any impending NetWatchDogTimeout */
 	netif_trans_update(netdev);
 
