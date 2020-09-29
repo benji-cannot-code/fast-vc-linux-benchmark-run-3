0de010eb033c1baaaab162eacefa296d84860bb1
Dmitry Osipenko <digetx@gmail.com>
2020-09-30 01:18:46 +0300

    i2c: tegra: Handle potential error of tegra_i2c_flush_fifos()
    Technically the tegra_i2c_flush_fifos() may fail and transfer should be
    aborted in this case, but this shouldn't ever happen in practice unless
    there is a bug somewhere in the driver. Let's add the error check just
    for completeness.
    
    Reviewed-by: Michał Mirosław <mirq-linux@rere.qmqm.pl>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Reviewed-by: Thierry Reding <treding@nvidia.com>
    Tested-by: Thierry Reding <treding@nvidia.com>
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Signed-off-by: Wolfram Sang <wsa@kernel.org>

--- drivers/i2c/busses/i2c-tegra.c
+++ drivers/i2c/busses/i2c-tegra.c
@@ -1178,7 +1178,9 @@ static int tegra_i2c_xfer_msg(struct tegra_i2c_dev *i2c_dev,
 	bool dma;
 	u16 xfer_time = 100;
 
-	tegra_i2c_flush_fifos(i2c_dev);
+	err = tegra_i2c_flush_fifos(i2c_dev);
+	if (err)
+		return err;
 
 	i2c_dev->msg_buf = msg->buf;
 	i2c_dev->msg_buf_remaining = msg->len;
