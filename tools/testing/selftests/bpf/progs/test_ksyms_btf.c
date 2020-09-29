bb240e710bc5284850dcffdf11f11fb9f365f2df
Hao Luo <haoluo@google.com>
2020-09-29 16:50:46 -0700

    selftests/bpf: Ksyms_btf to test typed ksyms
    Selftests for typed ksyms. Tests two types of ksyms: one is a struct,
    the other is a plain int. This tests two paths in the kernel. Struct
    ksyms will be converted into PTR_TO_BTF_ID by the verifier while int
    typed ksyms will be converted into PTR_TO_MEM.
    
    Signed-off-by: Hao Luo <haoluo@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/20200929235049.2533242-4-haoluo@google.com

---
+++ tools/testing/selftests/bpf/progs/test_ksyms_btf.c
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) 2020 Google */
+
+#include "vmlinux.h"
+
+#include <bpf/bpf_helpers.h>
+
+__u64 out__runqueues_addr = -1;
+__u64 out__bpf_prog_active_addr = -1;
+
+extern const struct rq runqueues __ksym; /* struct type global var. */
+extern const int bpf_prog_active __ksym; /* int type global var. */
+
+SEC("raw_tp/sys_enter")
+int handler(const void *ctx)
+{
+	out__runqueues_addr = (__u64)&runqueues;
+	out__bpf_prog_active_addr = (__u64)&bpf_prog_active;
+
+	return 0;
+}
+
+char _license[] SEC("license") = "GPL";
