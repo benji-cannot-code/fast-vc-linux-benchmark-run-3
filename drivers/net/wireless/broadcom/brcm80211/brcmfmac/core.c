5fe420b3f58a1fbb947ef7e51faf8d63abcf0310
Thomas Gleixner <tglx@linutronix.de>
2020-09-29 22:25:35 +0200

    net: brcmfmac: Convey execution context via argument to brcmf_netif_rx()
    bcrmgf_netif_rx() uses in_interrupt to chose between netif_rx() and
    netif_rx_ni(). in_interrupt() usage in drivers is phased out.
    
    Convey the execution mode via an 'inirq' argument through the various
    callchains leading to brcmf_netif_rx():
    
    brcmf_pcie_isr_thread()		    <- Task context
      brcmf_proto_msgbuf_rx_trigger()
        brcmf_msgbuf_process_rx()
          brcmf_msgbuf_process_msgtype()
            brcmf_msgbuf_process_rx_complete()
    	  brcmf_netif_mon_rx()
    	     brcmf_netif_rx(isirq = false)
    	  brcmf_netif_rx(isirq = false)
    
    brcmf_sdio_readframes()  <- Task context sdio_claim_host() might sleep
      brcmf_rx_frame(isirq = false)
    
    brcmf_sdio_rxglom()      <- Task context sdio_claim_host() might sleep
      brcmf_rx_frame(isirq = false)
    
    brcmf_usb_rx_complete()  <- Interrupt context
      brcmf_rx_frame(isirq = true)
    
    brcmf_rx_frame()
      brcmf_proto_rxreorder()
        brcmf_proto_bcdc_rxreorder()
          brcmf_fws_rxreorder()
            brcmf_netif_rx()
          brcmf_netif_rx()
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Arend van Spriel <arend.vanspriel@broadcom.com>
    Cc: Kalle Valo <kvalo@codeaurora.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/core.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/core.c
@@ -396,7 +396,7 @@ void brcmf_txflowblock_if(struct brcmf_if *ifp,
 	spin_unlock_irqrestore(&ifp->netif_stop_lock, flags);
 }
 
-void brcmf_netif_rx(struct brcmf_if *ifp, struct sk_buff *skb)
+void brcmf_netif_rx(struct brcmf_if *ifp, struct sk_buff *skb, bool inirq)
 {
 	/* Most of Broadcom's firmwares send 802.11f ADD frame every time a new
 	 * STA connects to the AP interface. This is an obsoleted standard most
@@ -419,14 +419,15 @@ void brcmf_netif_rx(struct brcmf_if *ifp, struct sk_buff *skb)
 	ifp->ndev->stats.rx_packets++;
 
 	brcmf_dbg(DATA, "rx proto=0x%X\n", ntohs(skb->protocol));
-	if (in_interrupt())
+	if (inirq) {
 		netif_rx(skb);
-	else
+	} else {
 		/* If the receive is not processed inside an ISR,
 		 * the softirqd must be woken explicitly to service
 		 * the NET_RX_SOFTIRQ.  This is handled by netif_rx_ni().
 		 */
 		netif_rx_ni(skb);
+	}
 }
 
 void brcmf_netif_mon_rx(struct brcmf_if *ifp, struct sk_buff *skb)
@@ -475,7 +476,7 @@ void brcmf_netif_mon_rx(struct brcmf_if *ifp, struct sk_buff *skb)
 	skb->pkt_type = PACKET_OTHERHOST;
 	skb->protocol = htons(ETH_P_802_2);
 
-	brcmf_netif_rx(ifp, skb);
+	brcmf_netif_rx(ifp, skb, false);
 }
 
 static int brcmf_rx_hdrpull(struct brcmf_pub *drvr, struct sk_buff *skb,
@@ -497,7 +498,8 @@ static int brcmf_rx_hdrpull(struct brcmf_pub *drvr, struct sk_buff *skb,
 	return 0;
 }
 
-void brcmf_rx_frame(struct device *dev, struct sk_buff *skb, bool handle_event)
+void brcmf_rx_frame(struct device *dev, struct sk_buff *skb, bool handle_event,
+		    bool inirq)
 {
 	struct brcmf_if *ifp;
 	struct brcmf_bus *bus_if = dev_get_drvdata(dev);
@@ -509,14 +511,14 @@ void brcmf_rx_frame(struct device *dev, struct sk_buff *skb, bool handle_event)
 		return;
 
 	if (brcmf_proto_is_reorder_skb(skb)) {
-		brcmf_proto_rxreorder(ifp, skb);
+		brcmf_proto_rxreorder(ifp, skb, inirq);
 	} else {
 		/* Process special event packets */
 		if (handle_event)
 			brcmf_fweh_process_skb(ifp->drvr, skb,
 					       BCMILCP_SUBTYPE_VENDOR_LONG);
 
-		brcmf_netif_rx(ifp, skb);
+		brcmf_netif_rx(ifp, skb, inirq);
 	}
 }
 
