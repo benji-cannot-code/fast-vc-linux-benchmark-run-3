0a6c7f471f59713520583799a137ec50d7d285fb
Armin Wolf <W_Armin@gmx.de>
2020-09-29 19:13:26 +0200

    lib8390: Replace panic() call with BUILD_BUG_ON
    Replace panic() call in lib8390.c with BUILD_BUG_ON()
    since checking the size of struct e8390_pkt_hdr should
    happen at compile-time.
    
    Signed-off-by: Armin Wolf <W_Armin@gmx.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/8390/lib8390.c
+++ drivers/net/ethernet/8390/lib8390.c
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
   */
 
+#include <linux/build_bug.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/jiffies.h>
@@ -1019,8 +1020,7 @@ static void __NS8390_init(struct net_device *dev, int startp)
 	    ? (0x48 | ENDCFG_WTS | (ei_local->bigendian ? ENDCFG_BOS : 0))
 	    : 0x48;
 
-	if (sizeof(struct e8390_pkt_hdr) != 4)
-		panic("8390.c: header struct mispacked\n");
+	BUILD_BUG_ON(sizeof(struct e8390_pkt_hdr) != 4);
 	/* Follow National Semi's recommendations for initing the DP83902. */
 	ei_outb_p(E8390_NODMA+E8390_PAGE0+E8390_STOP, e8390_base+E8390_CMD); /* 0x21 */
 	ei_outb_p(endcfg, e8390_base + EN0_DCFG);	/* 0x48 or 0x49 */
