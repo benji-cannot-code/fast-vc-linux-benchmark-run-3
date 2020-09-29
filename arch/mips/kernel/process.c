ee286a1ebadbb8b7ddb4c0e9b191f9724482ad65
Pujin Shi <shipujin.t@gmail.com>
2020-09-29 17:30:47 +0800

    MIPS: process: include exec.h header in process.c
    arch/mips/kernel/process.c:696:15: error: no previous prototype for 'arch_align_stack' [-Werror=missing-prototypes]
    
    Signed-off-by: Pujin Shi <shipujin.t@gmail.com>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/kernel/process.c
+++ arch/mips/kernel/process.c
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/inst.h>
 #include <asm/stacktrace.h>
 #include <asm/irq_regs.h>
+#include <asm/exec.h>
 
 #ifdef CONFIG_HOTPLUG_CPU
 void arch_cpu_idle_dead(void)
