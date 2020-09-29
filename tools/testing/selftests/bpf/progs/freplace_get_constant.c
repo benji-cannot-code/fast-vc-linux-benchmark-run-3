ddb08ae1f0365080e90ede8edaaef9eaa2badbf6
Toke Høiland-Jørgensen <toke@redhat.com>
2020-09-29 14:45:57 +0200

    selftests: Add selftest for disallowing modify_return attachment to freplace
    This adds a selftest that ensures that modify_return tracing programs
    cannot be attached to freplace programs. The security_ prefix is added to
    the freplace program because that would otherwise let it pass the check for
    modify_return.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/160138355713.48470.3811074984255709369.stgit@toke.dk

--- tools/testing/selftests/bpf/progs/freplace_get_constant.c
+++ tools/testing/selftests/bpf/progs/freplace_get_constant.c
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 volatile __u64 test_get_constant = 0;
 SEC("freplace/get_constant")
-int new_get_constant(long val)
+int security_new_get_constant(long val)
 {
 	if (val != 123)
 		return 0;
