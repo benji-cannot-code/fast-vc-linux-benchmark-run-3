4ecea64566834b3ff26f7e8cd1ed45969dbd41f3
Kalle Valo <kvalo@codeaurora.org>
2020-09-29 20:15:31 +0300

    ath11k: add interface_modes to hw_params
    As QCA6390 does not support mesh interfaces, move the interface_modes to
    hw_params. Also create interface combinations dynamically so that it's easy to
    change the values.
    
    Now QCA6390 does not claim to support mesh interfaces to user space, but
    IPQ8074 continues to do that.
    
    Tested-on: QCA6390 hw2.0 PCI WLAN.HST.1.0.1-01740-QCAHSTSWPLZ_V2_TO_X86-1
    
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Link: https://lore.kernel.org/r/1601399736-3210-4-git-send-email-kvalo@codeaurora.org

--- drivers/net/wireless/ath/ath11k/core.c
+++ drivers/net/wireless/ath/ath11k/core.c
@@ -59,6 +59,10 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.htt_peer_map_v2 = true,
 		.tcl_0_only = false,
 		.spectral_fft_sz = 2,
+
+		.interface_modes = BIT(NL80211_IFTYPE_STATION) |
+					BIT(NL80211_IFTYPE_AP) |
+					BIT(NL80211_IFTYPE_MESH_POINT),
 	},
 	{
 		.hw_rev = ATH11K_HW_IPQ6018_HW10,
@@ -89,6 +93,10 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.htt_peer_map_v2 = true,
 		.tcl_0_only = false,
 		.spectral_fft_sz = 4,
+
+		.interface_modes = BIT(NL80211_IFTYPE_STATION) |
+					BIT(NL80211_IFTYPE_AP) |
+					BIT(NL80211_IFTYPE_MESH_POINT),
 	},
 	{
 		.name = "qca6390 hw2.0",
@@ -119,6 +127,9 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.htt_peer_map_v2 = false,
 		.tcl_0_only = true,
 		.spectral_fft_sz = 0,
+
+		.interface_modes = BIT(NL80211_IFTYPE_STATION) |
+					BIT(NL80211_IFTYPE_AP),
 	},
 };
 
