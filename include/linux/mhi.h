49a72ad870fdc3527cf9bb26fb4f39be86d4c60d
Clark Williams <williams@redhat.com>
2020-09-29 23:22:11 +0530

    bus: mhi: Remove include of rwlock_types.h
    rwlock.h should not be included directly. Instead linux/splinlock.h
    should be included. Including it directly will break the RT build.
    
    Also there is no point in including _types.h headers directly. There is
    no benefit in including the type without the accessor.
    
    Fixes: 0cbf260820fa7 ("bus: mhi: core: Add support for registering MHI controllers")
    Reviewed-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Clark Williams <williams@redhat.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Link: https://lore.kernel.org/r/20200929175218.8178-13-manivannan.sadhasivam@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/mhi.h
+++ include/linux/mhi.h
@@ -10,10 +10,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/dma-direction.h>
 #include <linux/mutex.h>
-#include <linux/rwlock_types.h>
 #include <linux/skbuff.h>
 #include <linux/slab.h>
-#include <linux/spinlock_types.h>
+#include <linux/spinlock.h>
 #include <linux/wait.h>
 #include <linux/workqueue.h>
 
