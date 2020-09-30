46a32982c7ecc9ae41f60f71f6918b760ae0f345
Sakari Ailus <sakari.ailus@linux.intel.com>
2020-09-30 14:08:03 +0200

    media: v4l2-fwnode: v4l2_fwnode_endpoint_parse caller must init vep argument
    Document that the caller of v4l2_fwnode_endpoint_parse() must init the
    fields of struct v4l2_fwnode_endpoint (vep argument) fields.
    
    It used to be that the fields were zeroed by v4l2_fwnode_endpoint_parse
    when bus type was set to V4L2_MBUS_UNKNOWN but with recent changes (Fixes:
    line below) that no longer makes sense.
    
    Fixes: bb4bba9232fc ("media: v4l2-fwnode: Make bus configuration a struct")
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- include/media/v4l2-fwnode.h
+++ include/media/v4l2-fwnode.h
@@ -232,6 +232,9 @@ struct v4l2_fwnode_connector {
  * guessing @vep.bus_type between CSI-2 D-PHY, parallel and BT.656 busses is
  * supported. NEVER RELY ON GUESSING @vep.bus_type IN NEW DRIVERS!
  *
+ * The caller is required to initialise all fields of @vep, either with
+ * explicitly values, or by zeroing them.
+ *
  * The function does not change the V4L2 fwnode endpoint state if it fails.
  *
  * NOTE: This function does not parse properties the size of which is variable
@@ -273,6 +276,9 @@ void v4l2_fwnode_endpoint_free(struct v4l2_fwnode_endpoint *vep);
  * FEATURE REQUIRES "bus-type" PROPERTY IN DT BINDINGS. For old drivers,
  * guessing @vep.bus_type between CSI-2 D-PHY, parallel and BT.656 busses is
  * supported. NEVER RELY ON GUESSING @vep.bus_type IN NEW DRIVERS!
+ *
+ * The caller is required to initialise all fields of @vep, either with
+ * explicitly values, or by zeroing them.
  *
  * The function does not change the V4L2 fwnode endpoint state if it fails.
  *
