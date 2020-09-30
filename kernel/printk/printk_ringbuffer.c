2c8f826c5fecc14599db9e3eb83af9890b2703b1
John Ogness <john.ogness@linutronix.de>
2020-09-30 11:07:33 +0206

    printk: avoid and/or handle record truncation
    If a reader provides a buffer that is smaller than the message text,
    the @text_len field of @info will have a value larger than the buffer
    size. If readers blindly read @text_len bytes of data without
    checking the size, they will read beyond their buffer.
    
    Add this check to record_print_text() to properly recognize when such
    truncation has occurred.
    
    Add a maximum size argument to the ringbuffer function to extend
    records so that records can not be created that are larger than the
    buffer size of readers.
    
    When extending records (LOG_CONT), do not extend records beyond
    LOG_LINE_MAX since that is the maximum size available in the buffers
    used by consoles and syslog.
    
    Fixes: f5f022e53b87 ("printk: reimplement log_cont using record extension")
    Reported-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Reviewed-by: Petr Mladek <pmladek@suse.com>
    Signed-off-by: Petr Mladek <pmladek@suse.com>
    Link: https://lore.kernel.org/r/20200930090134.8723-2-john.ogness@linutronix.de

--- kernel/printk/printk_ringbuffer.c
+++ kernel/printk/printk_ringbuffer.c
@@ -203,7 +203,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	// specify additional 5 bytes text space to extend
  *	prb_rec_init_wr(&r, 5);
  *
- *	if (prb_reserve_in_last(&e, &test_rb, &r, printk_caller_id())) {
+ *	// try to extend, but only if it does not exceed 32 bytes
+ *	if (prb_reserve_in_last(&e, &test_rb, &r, printk_caller_id()), 32) {
  *		snprintf(&r.text_buf[r.info->text_len],
  *			 r.text_buf_size - r.info->text_len, "hello");
  *
@@ -1310,6 +1311,7 @@ static struct prb_desc *desc_reopen_last(struct prb_desc_ring *desc_ring,
  * @rb:        The ringbuffer to re-reserve and extend data in.
  * @r:         The record structure to allocate buffers for.
  * @caller_id: The caller ID of the caller (reserving writer).
+ * @max_size:  Fail if the extended size would be greater than this.
  *
  * This is the public function available to writers to re-reserve and extend
  * data.
@@ -1344,7 +1346,7 @@ static struct prb_desc *desc_reopen_last(struct prb_desc_ring *desc_ring,
  *            @r->info->text_len after concatenating.
  */
 bool prb_reserve_in_last(struct prb_reserved_entry *e, struct printk_ringbuffer *rb,
-			 struct printk_record *r, u32 caller_id)
+			 struct printk_record *r, u32 caller_id, unsigned int max_size)
 {
 	struct prb_desc_ring *desc_ring = &rb->desc_ring;
 	struct printk_info *info;
@@ -1390,6 +1392,9 @@ bool prb_reserve_in_last(struct prb_reserved_entry *e, struct printk_ringbuffer 
 		if (!data_check_size(&rb->text_data_ring, r->text_buf_size))
 			goto fail;
 
+		if (r->text_buf_size > max_size)
+			goto fail;
+
 		r->text_buf = data_alloc(rb, &rb->text_data_ring, r->text_buf_size,
 					 &d->text_blk_lpos, id);
 	} else {
@@ -1409,6 +1414,9 @@ bool prb_reserve_in_last(struct prb_reserved_entry *e, struct printk_ringbuffer 
 		r->text_buf_size += info->text_len;
 
 		if (!data_check_size(&rb->text_data_ring, r->text_buf_size))
+			goto fail;
+
+		if (r->text_buf_size > max_size)
 			goto fail;
 
 		r->text_buf = data_realloc(rb, &rb->text_data_ring, r->text_buf_size,
