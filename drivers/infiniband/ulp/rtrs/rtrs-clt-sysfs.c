df4302c26ddfa1eaae46dc1773d0b035e1d0013f
Rikard Falkeborn <rikard.falkeborn@gmail.com>
2020-10-01 00:40:04 +0200

    RDMA/rtrs: Constify static struct attribute_group
    The only usage of these is to pass their address to sysfs_create_group()
    and sysfs_remove_group(), both which takes const pointers. Make it const
    to allow the compiler to put them in read-only memory.
    
    Link: https://lore.kernel.org/r/20200930224004.24279-3-rikard.falkeborn@gmail.com
    Signed-off-by: Rikard Falkeborn <rikard.falkeborn@gmail.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/infiniband/ulp/rtrs/rtrs-clt-sysfs.c
+++ drivers/infiniband/ulp/rtrs/rtrs-clt-sysfs.c
@@ -313,7 +313,7 @@ static struct attribute *rtrs_clt_stats_attrs[] = {
 	NULL
 };
 
-static struct attribute_group rtrs_clt_stats_attr_group = {
+static const struct attribute_group rtrs_clt_stats_attr_group = {
 	.attrs = rtrs_clt_stats_attrs,
 };
 
@@ -389,7 +389,7 @@ static struct attribute *rtrs_clt_sess_attrs[] = {
 	NULL,
 };
 
-static struct attribute_group rtrs_clt_sess_attr_group = {
+static const struct attribute_group rtrs_clt_sess_attr_group = {
 	.attrs = rtrs_clt_sess_attrs,
 };
 
@@ -461,7 +461,7 @@ static struct attribute *rtrs_clt_attrs[] = {
 	NULL,
 };
 
-static struct attribute_group rtrs_clt_attr_group = {
+static const struct attribute_group rtrs_clt_attr_group = {
 	.attrs = rtrs_clt_attrs,
 };
 
