2236ceba561c82573afc72afe0e62d60bf33ebbe
Nikolay Borisov <nborisov@suse.com>
2020-10-01 09:40:39 +0300

    btrfs: rename BTRFS_INODE_ORDERED_DATA_CLOSE flag
    Commit 8d875f95da43 ("btrfs: disable strict file flushes for
    renames and truncates") eliminated the notion of ordered operations and
    instead BTRFS_INODE_ORDERED_DATA_CLOSE only remained as a flag
    indicating that a file's content should be synced to disk in case a
    file is truncated and any writes happen to it concurrently. In fact
    this intendend behavior was broken until it was fixed in
    f6dc45c7a93a ("Btrfs: fix filemap_flush call in btrfs_file_release").
    
    All things considered let's give the flag a more descriptive name. Also
    slightly reword comments.
    
    Signed-off-by: Nikolay Borisov <nborisov@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/btrfs_inode.h
+++ fs/btrfs/btrfs_inode.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * new data the application may have written before commit.
  */
 enum {
-	BTRFS_INODE_ORDERED_DATA_CLOSE,
+	BTRFS_INODE_FLUSH_ON_CLOSE,
 	BTRFS_INODE_DUMMY,
 	BTRFS_INODE_IN_DEFRAG,
 	BTRFS_INODE_HAS_ASYNC_EXTENT,
