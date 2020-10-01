8d6b2d6f2fbf58feb5639674ae0127cb752884d7
Ashley Thomas <Ashley.Thomas2@amd.com>
2020-10-01 00:16:05 -0700

    drm/amd/display: Source minimum HBlank support
    [Why]
    Some sink devices wish to have access to the minimum
    HBlank supported by the ASIC.
    
    [How]
    Make the ASIC minimum HBlank available in Source
    Device information address 0x340.
    
    Signed-off-by: Ashley Thomas <Ashley.Thomas2@amd.com>
    Acked-by: Aurabindo Pillai <aurabindo.pillai@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/dce80/dce80_resource.c
+++ drivers/gpu/drm/amd/display/dc/dce80/dce80_resource.c
@@ -966,6 +966,7 @@ static bool dce80_construct(
 	dc->caps.i2c_speed_in_khz = 40;
 	dc->caps.i2c_speed_in_khz_hdcp = 40;
 	dc->caps.max_cursor_size = 128;
+	dc->caps.min_horizontal_blanking_period = 80;
 	dc->caps.dual_link_dvi = true;
 	dc->caps.extended_aux_timeout_support = false;
 
@@ -1165,6 +1166,7 @@ static bool dce81_construct(
 	dc->caps.i2c_speed_in_khz = 40;
 	dc->caps.i2c_speed_in_khz_hdcp = 40;
 	dc->caps.max_cursor_size = 128;
+	dc->caps.min_horizontal_blanking_period = 80;
 	dc->caps.is_apu = true;
 
 	/*************************************************
@@ -1363,6 +1365,7 @@ static bool dce83_construct(
 	dc->caps.i2c_speed_in_khz = 40;
 	dc->caps.i2c_speed_in_khz_hdcp = 40;
 	dc->caps.max_cursor_size = 128;
+	dc->caps.min_horizontal_blanking_period = 80;
 	dc->caps.is_apu = true;
 
 	/*************************************************
