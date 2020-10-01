d648ca375a4ca74d1db8fb93af672fe7c509eafb
Len Brown <len.brown@intel.com>
2020-09-30 21:04:05 -0400

    tools/power turbostat: update version number
    goodbye summer...
    
    Signed-off-by: Len Brown <len.brown@intel.com>

--- tools/power/x86/turbostat/turbostat.c
+++ tools/power/x86/turbostat/turbostat.c
@@ -5713,7 +5713,7 @@ int get_and_dump_counters(void)
 }
 
 void print_version() {
-	fprintf(outf, "turbostat version 20.03.20"
+	fprintf(outf, "turbostat version 20.09.30"
 		" - Len Brown <lenb@kernel.org>\n");
 }
 
