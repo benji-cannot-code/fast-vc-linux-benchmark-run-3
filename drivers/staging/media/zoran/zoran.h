d161408208296283cbea18faa2643c733770c86c
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-10-01 11:17:51 +0200

    media: zoran: get rid of an unused var
    The jpeg_error in lowercase is not used anywhere. Drop it.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/staging/media/zoran/zoran.h
+++ drivers/staging/media/zoran/zoran.h
@@ -265,7 +265,6 @@ struct zoran {
 
 	/* Additional stuff for testing */
 	unsigned int ghost_int;
-	int jpeg_error;
 	int intr_counter_GIRQ1;
 	int intr_counter_GIRQ0;
 	int intr_counter_CodRepIRQ;
