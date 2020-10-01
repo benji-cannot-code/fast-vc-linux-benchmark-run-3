2883f970563e895d32d16944aa2010202a4a42c1
Chuck Lever <chuck.lever@oracle.com>
2020-10-01 18:59:38 -0400

    NFSD: Remove vestigial typedefs
    Clean up: These are not used.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: J. Bruce Fields <bfields@redhat.com>

--- fs/nfsd/nfsproc.c
+++ fs/nfsd/nfsproc.c
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "xdr.h"
 #include "vfs.h"
 
-typedef struct svc_rqst	svc_rqst;
-typedef struct svc_buf	svc_buf;
-
 #define NFSDDBG_FACILITY		NFSDDBG_PROC
 
 
