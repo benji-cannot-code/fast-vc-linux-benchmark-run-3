e9aa12159e5d98244bcbca035afbd3ea258f59b2
Gabriel Krisman Bertazi <krisman@collabora.com>
2020-10-03 23:25:31 -0400

    elf: Expose ELF header in compat_start_thread()
    Like it is done for SET_PERSONALITY with x86, which requires the ELF header
    to select correct personality parameters, x86 requires the headers on
    compat_start_thread() to choose starting CS for ELF32 binaries, instead of
    relying on the going-away TIF_IA32/X32 flags.
    
    Add an indirection macro to ELF invocations of START_THREAD, that x86 can
    reimplement to receive the extra parameter just for ELF files.  This
    requires no changes to other architectures who don't need the header
    information, they can continue to use the original start_thread for ELF and
    non-ELF binaries, and it prevents affecting non-ELF code paths for x86.
    
    Signed-off-by: Gabriel Krisman Bertazi <krisman@collabora.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20201004032536.1229030-6-krisman@collabora.com

--- fs/compat_binfmt_elf.c
+++ fs/compat_binfmt_elf.c
@@ -107,8 +107,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #ifdef	compat_start_thread
-#undef	start_thread
-#define	start_thread		compat_start_thread
+#define COMPAT_START_THREAD(ex, regs, new_ip, new_sp)	\
+	compat_start_thread(regs, new_ip, new_sp)
+#endif
+
+#ifdef	COMPAT_START_THREAD
+#undef	START_THREAD
+#define START_THREAD		COMPAT_START_THREAD
 #endif
 
 #ifdef	compat_arch_setup_additional_pages
--- include/linux/elf.h
+++ include/linux/elf.h
@@ -23,6 +23,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	SET_PERSONALITY(ex)
 #endif
 
+#ifndef START_THREAD
+#define START_THREAD(elf_ex, regs, elf_entry, start_stack)	\
+	start_thread(regs, elf_entry, start_stack)
+#endif
+
 #define ELF32_GNU_PROPERTY_ALIGN	4
 #define ELF64_GNU_PROPERTY_ALIGN	8
 
