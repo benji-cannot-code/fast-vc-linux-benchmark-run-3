0acb16420462518a7ba6de2fc133ebdb9aa92079
Gabriel Krisman Bertazi <krisman@collabora.com>
2020-10-03 23:25:33 -0400

    elf: Expose ELF header on arch_setup_additional_pages()
    Like it is done for SET_PERSONALITY with ARM, which requires the ELF
    header to select correct personality parameters, x86 requires the
    headers when selecting which VDSO to load, instead of relying on the
    going-away TIF_IA32/X32 flags.
    
    Add an indirection macro to arch_setup_additional_pages(), that x86 can
    reimplement to receive the extra parameter just for ELF files.  This
    requires no changes to other architectures, who can continue to use the
    original arch_setup_additional_pages for ELF and non-ELF binaries.
    
    Signed-off-by: Gabriel Krisman Bertazi <krisman@collabora.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20201004032536.1229030-8-krisman@collabora.com

--- fs/compat_binfmt_elf.c
+++ fs/compat_binfmt_elf.c
@@ -116,11 +116,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define START_THREAD		COMPAT_START_THREAD
 #endif
 
-#ifdef	compat_arch_setup_additional_pages
+#ifdef compat_arch_setup_additional_pages
+#define COMPAT_ARCH_SETUP_ADDITIONAL_PAGES(bprm, ex, interpreter) \
+	compat_arch_setup_additional_pages(bprm, interpreter)
+#endif
+
+#ifdef	COMPAT_ARCH_SETUP_ADDITIONAL_PAGES
 #undef	ARCH_HAS_SETUP_ADDITIONAL_PAGES
 #define ARCH_HAS_SETUP_ADDITIONAL_PAGES 1
-#undef	arch_setup_additional_pages
-#define	arch_setup_additional_pages compat_arch_setup_additional_pages
+#undef	ARCH_SETUP_ADDITIONAL_PAGES
+#define	ARCH_SETUP_ADDITIONAL_PAGES COMPAT_ARCH_SETUP_ADDITIONAL_PAGES
 #endif
 
 #ifdef	compat_elf_read_implies_exec
--- include/linux/elf.h
+++ include/linux/elf.h
@@ -28,6 +28,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	start_thread(regs, elf_entry, start_stack)
 #endif
 
+#if defined(ARCH_HAS_SETUP_ADDITIONAL_PAGES) && !defined(ARCH_SETUP_ADDITIONAL_PAGES)
+#define ARCH_SETUP_ADDITIONAL_PAGES(bprm, ex, interpreter) \
+	arch_setup_additional_pages(bprm, interpreter)
+#endif
+
 #define ELF32_GNU_PROPERTY_ALIGN	4
 #define ELF64_GNU_PROPERTY_ALIGN	8
 
