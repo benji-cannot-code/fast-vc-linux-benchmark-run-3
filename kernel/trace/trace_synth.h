3b60feacc18c9ca91f9604e436062608a0956bc6
Tom Zanussi <zanussi@kernel.org>
2020-10-04 17:14:03 -0500

    tracing: Change STR_VAR_MAX_LEN
    32 is too small for this value, and anyway it makes more sense to use
    MAX_FILTER_STR_VAL, as this is also the value used for variable-length
    __strings.
    
    Link: https://lkml.kernel.org/r/6adfd1668ac1fd8670bd58206944a762061a5559.1601848695.git.zanussi@kernel.org
    
    Tested-by: Axel Rasmussen <axelrasmussen@google.com>
    Signed-off-by: Tom Zanussi <zanussi@kernel.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/trace_synth.h
+++ kernel/trace/trace_synth.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SYNTH_SYSTEM		"synthetic"
 #define SYNTH_FIELDS_MAX	32
 
-#define STR_VAR_LEN_MAX		32 /* must be multiple of sizeof(u64) */
+#define STR_VAR_LEN_MAX		MAX_FILTER_STR_VAL /* must be multiple of sizeof(u64) */
 
 struct synth_field {
 	char *type;
