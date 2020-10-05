fc8aeff7d4bdf4d5ba48d4a5fcecb4e18032c1ca
Andrzej Pietrasiewicz <andrzej.p@collabora.com>
2020-10-04 21:15:46 -0700

    Input: add input_device_enabled()
    A helper function for drivers to decide if the device is used or not.
    A lockdep check is introduced as inspecting ->users should be done under
    input device's mutex.
    
    Signed-off-by: Andrzej Pietrasiewicz <andrzej.p@collabora.com>
    Link: https://lore.kernel.org/r/20200608112211.12125-2-andrzej.p@collabora.com
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- include/linux/input.h
+++ include/linux/input.h
@@ -503,6 +503,8 @@ bool input_match_device_id(const struct input_dev *dev,
 
 void input_enable_softrepeat(struct input_dev *dev, int delay, int period);
 
+bool input_device_enabled(struct input_dev *dev);
+
 extern struct class input_class;
 
 /**
