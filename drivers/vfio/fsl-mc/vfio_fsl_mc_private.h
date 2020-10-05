e9aa79f280a5965ea173b3603dec5027e0f9c27b
Diana Craciun <diana.craciun@oss.nxp.com>
2020-10-05 20:36:46 +0300

    vfio/fsl-mc: Scan DPRC objects on vfio-fsl-mc driver bind
    The DPRC (Data Path Resource Container) device is a bus device and has
    child devices attached to it. When the vfio-fsl-mc driver is probed
    the DPRC is scanned and the child devices discovered and initialized.
    
    Signed-off-by: Bharat Bhushan <Bharat.Bhushan@nxp.com>
    Signed-off-by: Diana Craciun <diana.craciun@oss.nxp.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- drivers/vfio/fsl-mc/vfio_fsl_mc_private.h
+++ drivers/vfio/fsl-mc/vfio_fsl_mc_private.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct vfio_fsl_mc_device {
 	struct fsl_mc_device		*mc_dev;
+	struct notifier_block        nb;
 };
 
 #endif /* VFIO_FSL_MC_PRIVATE_H */
