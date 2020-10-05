687449ba5a16b1483848f4439f4f31798dbb2011
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-10-05 12:56:22 +0300

    gpiolib: Update header block in gpiolib-cdev.h
    The dev_t is defined in types.h while struct gpio_device forward declaration
    is missed. Take into account above and update header block in gpiolib-cdev.h.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: https://lore.kernel.org/r/20201005095622.73616-1-andriy.shevchenko@linux.intel.com
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpiolib-cdev.h
+++ drivers/gpio/gpiolib-cdev.h
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef GPIOLIB_CDEV_H
 #define GPIOLIB_CDEV_H
 
-#include <linux/device.h>
+#include <linux/types.h>
+
+struct gpio_device;
 
 #ifdef CONFIG_GPIO_CDEV
 
