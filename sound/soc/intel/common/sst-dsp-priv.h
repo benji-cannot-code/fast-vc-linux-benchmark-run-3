a2150b724b8186cf62fa37a98c720de190d8611f
Cezary Rojewski <cezary.rojewski@intel.com>
2020-10-06 08:49:04 +0200

    ASoC: Intel: Remove SST-legacy specific constants
    As sound/soc/intel/haswell and /baytrail are no more, all SST-legacy
    specific constants and registers are redundant so remove them.
    
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Acked-by: Liam Girdwood <liam.r.girdwood@intel.com>
    Link: https://lore.kernel.org/r/20201006064907.16277-11-cezary.rojewski@intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/common/sst-dsp-priv.h
+++ sound/soc/intel/common/sst-dsp-priv.h
@@ -16,9 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "../skylake/skl-sst-dsp.h"
 
-/* do we need to remove or keep */
-#define DSP_DRAM_ADDR_OFFSET		0x400000
-
 /*
  * DSP Operations exported by platform Audio DSP driver.
  */
--- sound/soc/intel/common/sst-ipc.h
+++ sound/soc/intel/common/sst-ipc.h
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/workqueue.h>
 #include <linux/sched.h>
 
-#define IPC_MAX_MAILBOX_BYTES	256
-
 struct sst_ipc_message {
 	u64 header;
 	void *data;
