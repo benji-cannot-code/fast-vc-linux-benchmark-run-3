e8a758d88a902852180f562ba3b997444de0fbc0
Mike Travis <mike.travis@hpe.com>
2020-10-06 16:34:27 -0500

    drivers/misc/sgi-xp: Adjust references in UV kernel modules
    Remove the define is_uv() is_uv_system and just use the latter as is.
    This removes a conflict with a new symbol in the generated uv_mmrs.h
    file (is_uv()).
    
    Signed-off-by: Mike Travis <mike.travis@hpe.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Reviewed-by: Dimitri Sivanich <dimitri.sivanich@hpe.com>
    Reviewed-by: Steve Wahl <steve.wahl@hpe.com>
    Link: https://lkml.kernel.org/r/20201005203929.148656-4-mike.travis@hpe.com

--- drivers/misc/sgi-xp/xp.h
+++ drivers/misc/sgi-xp/xp.h
@@ -18,11 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #if defined CONFIG_X86_UV || defined CONFIG_IA64_SGI_UV
 #include <asm/uv/uv.h>
-#define is_uv()		is_uv_system()
-#endif
-
-#ifndef is_uv
-#define is_uv()		0
 #endif
 
 #ifdef USE_DBUG_ON
@@ -80,7 +75,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define XPC_MSG_SIZE(_payload_size) \
 				ALIGN(XPC_MSG_HDR_MAX_SIZE + (_payload_size), \
-				      is_uv() ? 64 : 128)
+				      is_uv_system() ? 64 : 128)
 
 
 /*
