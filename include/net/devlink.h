bc84dc6278370af8f40d6d9436bd731a32caaf75
Moshe Shemesh <moshe@mellanox.com>
2020-10-07 09:00:45 +0300

    devlink: Add reload stats
    Add reload stats to hold the history per reload action type and limit.
    
    For example, the number of times fw_activate has been performed on this
    device since the driver module was added or if the firmware activation
    was performed with or without reset.
    
    Add devlink notification on stats update.
    
    Expose devlink reload stats to the user through devlink dev get command.
    
    Examples:
    $ devlink dev show
    pci/0000:82:00.0:
      stats:
          reload:
            driver_reinit 2 fw_activate 1 fw_activate_no_reset 0
    pci/0000:82:00.1:
      stats:
          reload:
            driver_reinit 1 fw_activate 0 fw_activate_no_reset 0
    
    $ devlink dev show -jp
    {
        "dev": {
            "pci/0000:82:00.0": {
                "stats": {
                    "reload": {
                        "driver_reinit": 2,
                        "fw_activate": 1,
                        "fw_activate_no_reset": 0
                    }
                }
            },
            "pci/0000:82:00.1": {
                "stats": {
                    "reload": {
                        "driver_reinit": 1,
                        "fw_activate": 0,
                        "fw_activate_no_reset": 0
                    }
                }
            }
        }
    }
    
    Signed-off-by: Moshe Shemesh <moshe@mellanox.com>
    Reviewed-by: Jiri Pirko <jiri@nvidia.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/net/devlink.h
+++ include/net/devlink.h
@@ -21,6 +21,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <uapi/linux/devlink.h>
 #include <linux/xarray.h>
 
+#define DEVLINK_RELOAD_STATS_ARRAY_SIZE \
+	(__DEVLINK_RELOAD_LIMIT_MAX * __DEVLINK_RELOAD_ACTION_MAX)
+
+struct devlink_dev_stats {
+	u32 reload_stats[DEVLINK_RELOAD_STATS_ARRAY_SIZE];
+};
+
 struct devlink_ops;
 
 struct devlink {
@@ -39,6 +46,7 @@ struct devlink {
 	struct list_head trap_policer_list;
 	const struct devlink_ops *ops;
 	struct xarray snapshot_ids;
+	struct devlink_dev_stats stats;
 	struct device *dev;
 	possible_net_t _net;
 	struct mutex lock; /* Serializes access to devlink instance specific objects such as
