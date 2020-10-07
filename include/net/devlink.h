47756919d81be2a777f0618c1e202907be24305c
Moshe Shemesh <moshe@mellanox.com>
2020-10-07 09:00:46 +0300

    devlink: Add remote reload stats
    Add remote reload stats to hold the history of actions performed due
    devlink reload commands initiated by remote host. For example, in case
    firmware activation with reset finished successfully but was initiated
    by remote host.
    
    The function devlink_remote_reload_actions_performed() is exported to
    enable drivers update on remote reload actions performed as it was not
    initiated by their own devlink instance.
    
    Expose devlink remote reload stats to the user through devlink dev get
    command.
    
    Examples:
    $ devlink dev show
    pci/0000:82:00.0:
      stats:
          reload:
            driver_reinit 2 fw_activate 1 fw_activate_no_reset 0
          remote_reload:
            driver_reinit 0 fw_activate 0 fw_activate_no_reset 0
    pci/0000:82:00.1:
      stats:
          reload:
            driver_reinit 1 fw_activate 0 fw_activate_no_reset 0
          remote_reload:
            driver_reinit 1 fw_activate 1 fw_activate_no_reset 0
    
    $ devlink dev show -jp
    {
        "dev": {
            "pci/0000:82:00.0": {
                "stats": {
                    "reload": {
                        "driver_reinit": 2,
                        "fw_activate": 1,
                        "fw_activate_no_reset": 0
                    },
                    "remote_reload": {
                        "driver_reinit": 0,
                        "fw_activate": 0,
                        "fw_activate_no_reset": 0
                    }
                }
            },
            "pci/0000:82:00.1": {
                "stats": {
                    "reload": {
                        "driver_reinit": 1,
                        "fw_activate": 0,
                        "fw_activate_no_reset": 0
                    },
                    "remote_reload": {
                        "driver_reinit": 1,
                        "fw_activate": 1,
                        "fw_activate_no_reset": 0
                    }
                }
            }
        }
    }
    
    Signed-off-by: Moshe Shemesh <moshe@mellanox.com>
    Reviewed-by: Jakub Kicinski <kuba@kernel.org>
    Reviewed-by: Jiri Pirko <jiri@nvidia.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/net/devlink.h
+++ include/net/devlink.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct devlink_dev_stats {
 	u32 reload_stats[DEVLINK_RELOAD_STATS_ARRAY_SIZE];
+	u32 remote_reload_stats[DEVLINK_RELOAD_STATS_ARRAY_SIZE];
 };
 
 struct devlink_ops;
@@ -1568,6 +1569,9 @@ void
 devlink_health_reporter_recovery_done(struct devlink_health_reporter *reporter);
 
 bool devlink_is_reload_failed(const struct devlink *devlink);
+void devlink_remote_reload_actions_performed(struct devlink *devlink,
+					     enum devlink_reload_limit limit,
+					     u32 actions_performed);
 
 void devlink_flash_update_begin_notify(struct devlink *devlink);
 void devlink_flash_update_end_notify(struct devlink *devlink);
