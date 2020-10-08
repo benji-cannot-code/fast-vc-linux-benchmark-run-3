84d94a12b9b0e39f3bffc35860a9d5fc3395d361
Ard Biesheuvel <ardb@kernel.org>
2020-10-08 17:36:02 +0200

    arm64: mm: tidy up top of kernel VA space
    Tidy up the way the top of the kernel VA space is organized, by mirroring
    the 256 MB region we have below the vmalloc space, and populating it top
    down with the PCI I/O space, some guard regions, and the fixmap region.
    The latter region is itself populated top down, and today only covers
    about 4 MB, and so 224 MB is ample, and no guard region is therefore
    required.
    
    The resulting layout is identical between 48-bit/4k and 52-bit/64k
    configurations.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Reviewed-by: Steve Capper <steve.capper@arm.com>
    Link: https://lore.kernel.org/r/20201008153602.9467-5-ardb@kernel.org
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	and fixed mappings
  */
 #define VMALLOC_START		(MODULES_END)
-#define VMALLOC_END		(- PUD_SIZE - VMEMMAP_SIZE - SZ_64K)
+#define VMALLOC_END		(VMEMMAP_START - SZ_256M)
 
 #define vmemmap			((struct page *)VMEMMAP_START - (memstart_addr >> PAGE_SHIFT))
 
