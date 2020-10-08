b9e8bc8863849ba97e012132037c2a4ebfd96f40
Harald Freudenberger <freude@linux.ibm.com>
2020-10-08 08:43:09 +0200

    s390/zcrypt: fix wrong format specifications
    Fixes 5 wrong format specification findings found by the
    kernel test robot in ap_queue.c:
    
    warning: format specifies type 'unsigned char' but the argument has type 'int' [-Wformat]
                                   __func__, status.response_code,
    
    Signed-off-by: Harald Freudenberger <freude@linux.ibm.com>
    Reported-by: kernel test robot <lkp@intel.com>
    Fixes: 2ea2a6099ae3 ("s390/ap: add error response code field for ap queue devices")
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- drivers/s390/crypto/ap_queue.c
+++ drivers/s390/crypto/ap_queue.c
@@ -198,7 +198,7 @@ static enum ap_sm_wait ap_sm_read(struct ap_queue *aq)
 	default:
 		aq->dev_state = AP_DEV_STATE_ERROR;
 		aq->last_err_rc = status.response_code;
-		AP_DBF_WARN("%s RC 0x%02hhx on 0x%02x.%04x -> AP_DEV_STATE_ERROR\n",
+		AP_DBF_WARN("%s RC 0x%02x on 0x%02x.%04x -> AP_DEV_STATE_ERROR\n",
 			    __func__, status.response_code,
 			    AP_QID_CARD(aq->qid), AP_QID_QUEUE(aq->qid));
 		return AP_SM_WAIT_NONE;
@@ -263,7 +263,7 @@ static enum ap_sm_wait ap_sm_write(struct ap_queue *aq)
 	default:
 		aq->dev_state = AP_DEV_STATE_ERROR;
 		aq->last_err_rc = status.response_code;
-		AP_DBF_WARN("%s RC 0x%02hhx on 0x%02x.%04x -> AP_DEV_STATE_ERROR\n",
+		AP_DBF_WARN("%s RC 0x%02x on 0x%02x.%04x -> AP_DEV_STATE_ERROR\n",
 			    __func__, status.response_code,
 			    AP_QID_CARD(aq->qid), AP_QID_QUEUE(aq->qid));
 		return AP_SM_WAIT_NONE;
@@ -301,7 +301,7 @@ static enum ap_sm_wait ap_sm_reset(struct ap_queue *aq)
 	default:
 		aq->dev_state = AP_DEV_STATE_ERROR;
 		aq->last_err_rc = status.response_code;
-		AP_DBF_WARN("%s RC 0x%02hhx on 0x%02x.%04x -> AP_DEV_STATE_ERROR\n",
+		AP_DBF_WARN("%s RC 0x%02x on 0x%02x.%04x -> AP_DEV_STATE_ERROR\n",
 			    __func__, status.response_code,
 			    AP_QID_CARD(aq->qid), AP_QID_QUEUE(aq->qid));
 		return AP_SM_WAIT_NONE;
@@ -344,7 +344,7 @@ static enum ap_sm_wait ap_sm_reset_wait(struct ap_queue *aq)
 	default:
 		aq->dev_state = AP_DEV_STATE_ERROR;
 		aq->last_err_rc = status.response_code;
-		AP_DBF_WARN("%s RC 0x%02hhx on 0x%02x.%04x -> AP_DEV_STATE_ERROR\n",
+		AP_DBF_WARN("%s RC 0x%02x on 0x%02x.%04x -> AP_DEV_STATE_ERROR\n",
 			    __func__, status.response_code,
 			    AP_QID_CARD(aq->qid), AP_QID_QUEUE(aq->qid));
 		return AP_SM_WAIT_NONE;
@@ -385,7 +385,7 @@ static enum ap_sm_wait ap_sm_setirq_wait(struct ap_queue *aq)
 	default:
 		aq->dev_state = AP_DEV_STATE_ERROR;
 		aq->last_err_rc = status.response_code;
-		AP_DBF_WARN("%s RC 0x%02hhx on 0x%02x.%04x -> AP_DEV_STATE_ERROR\n",
+		AP_DBF_WARN("%s RC 0x%02x on 0x%02x.%04x -> AP_DEV_STATE_ERROR\n",
 			    __func__, status.response_code,
 			    AP_QID_CARD(aq->qid), AP_QID_QUEUE(aq->qid));
 		return AP_SM_WAIT_NONE;
