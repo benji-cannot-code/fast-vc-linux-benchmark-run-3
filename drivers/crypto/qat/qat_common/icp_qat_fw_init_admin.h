a3018e99086b9cb60b432f24f684bc94c7f9b7fb
Giovanni Cabiddu <giovanni.cabiddu@intel.com>
2020-10-12 21:38:24 +0100

    crypto: qat - add packed to init admin structures
    Add packed attribute to the structures icp_qat_fw_init_admin_req and
    icp_qat_fw_init_admin_resp as they are accessed by firmware.
    
    Signed-off-by: Giovanni Cabiddu <giovanni.cabiddu@intel.com>
    Reviewed-by: Fiona Trahe <fiona.trahe@intel.com>
    Reviewed-by: Wojciech Ziemba <wojciech.ziemba@intel.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/qat/qat_common/icp_qat_fw_init_admin.h
+++ drivers/crypto/qat/qat_common/icp_qat_fw_init_admin.h
@@ -40,7 +40,7 @@ struct icp_qat_fw_init_admin_req {
 	};
 
 	__u32 resrvd4;
-};
+} __packed;
 
 struct icp_qat_fw_init_admin_resp {
 	__u8 flags;
@@ -93,7 +93,7 @@ struct icp_qat_fw_init_admin_resp {
 			__u64 resrvd8;
 		};
 	};
-};
+} __packed;
 
 #define ICP_QAT_FW_COMN_HEARTBEAT_OK 0
 #define ICP_QAT_FW_COMN_HEARTBEAT_BLOCKED 1
