b5cb280d07e82bc838955f40c6f17b2b2f911397
Bob Peterson <rpeterso@redhat.com>
2020-10-12 13:57:37 -0500

    gfs2: eliminate GLF_QUEUED flag in favor of list_empty(gl_holders)
    Before this patch, glock.c maintained a flag, GLF_QUEUED, which indicated
    when a glock had a holder queued. It was only checked for inode glocks,
    although set and cleared by all glocks, and it was only used to determine
    whether the glock should be held for the minimum hold time before releasing.
    
    The problem is that the flag is not accurate at all. If a process holds
    the glock, the flag is set. When they dequeue the glock, it only cleared
    the flag in cases when the state actually changed. So if the state doesn't
    change, the flag may still be set, even when nothing is queued.
    
    This happens to iopen glocks often: the get held in SH, then the file is
    closed, but the glock remains in SH mode.
    
    We don't need a special flag to indicate this: we can simply tell whether
    the glock has any items queued to the holders queue. It's a waste of cpu
    time to maintain it.
    
    This patch eliminates the flag in favor of simply checking list_empty
    on the glock holders.
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- fs/gfs2/trace_gfs2.h
+++ fs/gfs2/trace_gfs2.h
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	{(1UL << GLF_REPLY_PENDING),		"r" },		\
 	{(1UL << GLF_INITIAL),			"I" },		\
 	{(1UL << GLF_FROZEN),			"F" },		\
-	{(1UL << GLF_QUEUED),			"q" },		\
 	{(1UL << GLF_LRU),			"L" },		\
 	{(1UL << GLF_OBJECT),			"o" },		\
 	{(1UL << GLF_BLOCKING),			"b" })
