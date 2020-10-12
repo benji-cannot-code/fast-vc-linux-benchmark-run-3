2914235436c963fd541b1c85e2cd6d8a807bed28
Giovanni Cabiddu <giovanni.cabiddu@intel.com>
2020-10-12 21:38:25 +0100

    crypto: qat - rename ME in AE
    Rename occurrences of ME in the admin module with the acronym AE
    (Acceleration Engine) as the two are equivalent.
    This is to keep a single acronym for engined in the codebase and
    follow the documentation in https://01.org/intel-quickassist-technology.
    
    Signed-off-by: Giovanni Cabiddu <giovanni.cabiddu@intel.com>
    Reviewed-by: Fiona Trahe <fiona.trahe@intel.com>
    Reviewed-by: Wojciech Ziemba <wojciech.ziemba@intel.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/qat/qat_common/icp_qat_fw_init_admin.h
+++ drivers/crypto/qat/qat_common/icp_qat_fw_init_admin.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "icp_qat_fw.h"
 
 enum icp_qat_fw_init_admin_cmd_id {
-	ICP_QAT_FW_INIT_ME = 0,
+	ICP_QAT_FW_INIT_AE = 0,
 	ICP_QAT_FW_TRNG_ENABLE = 1,
 	ICP_QAT_FW_TRNG_DISABLE = 2,
 	ICP_QAT_FW_CONSTANTS_CFG = 3,
