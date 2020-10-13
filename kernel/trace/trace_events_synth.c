81ec14b2569db53b1778d2360bb28cde219f1115
Tom Zanussi <zanussi@kernel.org>
2020-10-13 09:17:54 -0500

    tracing: Check that the synthetic event and field names are legal
    Call the is_good_name() function used by probe events to make sure
    synthetic event and field names don't contain illegal characters and
    cause unexpected parsing of synthetic event commands.
    
    Link: https://lkml.kernel.org/r/c4d4bb59d3ac39bcbd70fba0cf837d6b1cedb015.1602598160.git.zanussi@kernel.org
    
    Fixes: 4b147936fa50 (tracing: Add support for 'synthetic' events)
    Reported-by: Masami Hiramatsu <mhiramat@kernel.org>
    Reviewed-by: Masami Hiramatsu <mhiramat@kernel.org>
    Tested-by: Masami Hiramatsu <mhiramat@kernel.org>
    Signed-off-by: Tom Zanussi <zanussi@kernel.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/trace_events_synth.c
+++ kernel/trace/trace_events_synth.c
@@ -573,6 +573,10 @@ static struct synth_field *parse_synth_field(int argc, const char **argv,
 		ret = -ENOMEM;
 		goto free;
 	}
+	if (!is_good_name(field->name)) {
+		ret = -EINVAL;
+		goto free;
+	}
 
 	if (field_type[0] == ';')
 		field_type++;
@@ -1112,6 +1116,11 @@ static int __create_synth_event(int argc, const char *name, const char **argv)
 		return -EINVAL;
 
 	mutex_lock(&event_mutex);
+
+	if (!is_good_name(name)) {
+		ret = -EINVAL;
+		goto out;
+	}
 
 	event = find_synth_event(name);
 	if (event) {
