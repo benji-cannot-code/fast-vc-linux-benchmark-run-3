f9acb94ffc021d9cb0784047d048e80a70db8bbb
David Hildenbrand <david@redhat.com>
2020-10-13 16:55:28 -0700

    mm/page_isolation: cleanup set_migratetype_isolate()
    Let's clean it up a bit, simplifying the exit paths.
    
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Baoquan He <bhe@redhat.com>
    Reviewed-by: Pankaj Gupta <pankaj.gupta.linux@gmail.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Jason Wang <jasowang@redhat.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Qian Cai <cai@lca.pw>
    Link: http://lkml.kernel.org/r/20200816125333.7434-5-david@redhat.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page_isolation.c
+++ mm/page_isolation.c
@@ -18,12 +18,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static int set_migratetype_isolate(struct page *page, int migratetype, int isol_flags)
 {
-	struct page *unmovable = NULL;
-	struct zone *zone;
+	struct zone *zone = page_zone(page);
+	struct page *unmovable;
 	unsigned long flags;
-	int ret = -EBUSY;
-
-	zone = page_zone(page);
 
 	spin_lock_irqsave(&zone->lock, flags);
 
@@ -52,13 +49,13 @@ static int set_migratetype_isolate(struct page *page, int migratetype, int isol_
 									NULL);
 
 		__mod_zone_freepage_state(zone, -nr_pages, mt);
-		ret = 0;
+		spin_unlock_irqrestore(&zone->lock, flags);
+		drain_all_pages(zone);
+		return 0;
 	}
 
 	spin_unlock_irqrestore(&zone->lock, flags);
-	if (!ret) {
-		drain_all_pages(zone);
-	} else if ((isol_flags & REPORT_FAILURE) && unmovable) {
+	if (isol_flags & REPORT_FAILURE) {
 		/*
 		 * printk() with zone->lock held will likely trigger a
 		 * lockdep splat, so defer it here.
@@ -66,7 +63,7 @@ static int set_migratetype_isolate(struct page *page, int migratetype, int isol_
 		dump_page(unmovable, "unmovable page");
 	}
 
-	return ret;
+	return -EBUSY;
 }
 
 static void unset_migratetype_isolate(struct page *page, unsigned migratetype)
