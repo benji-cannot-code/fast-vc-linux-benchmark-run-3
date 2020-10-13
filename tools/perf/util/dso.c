d1a3a1c7d563f5b95996735fee106384eaba17a4
Jiri Olsa <jolsa@kernel.org>
2020-10-13 21:24:35 +0200

    perf tools: Pass build id object to sysfs__read_build_id()
    Passing build id object to sysfs__read_build_id function, so it can
    populate the size of the build_id object.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Acked-by: Ian Rogers <irogers@google.com>
    Link: https://lore.kernel.org/r/20201013192441.1299447-4-jolsa@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/dso.c
+++ tools/perf/util/dso.c
@@ -1347,8 +1347,7 @@ void dso__read_running_kernel_build_id(struct dso *dso, struct machine *machine)
 	if (machine__is_default_guest(machine))
 		return;
 	sprintf(path, "%s/sys/kernel/notes", machine->root_dir);
-	if (sysfs__read_build_id(path, dso->bid.data,
-				 sizeof(dso->bid.data)) == 0)
+	if (sysfs__read_build_id(path, &dso->bid) == 0)
 		dso->has_build_id = true;
 }
 
@@ -1366,8 +1365,7 @@ int dso__kernel_module_get_build_id(struct dso *dso,
 		 "%s/sys/module/%.*s/notes/.note.gnu.build-id",
 		 root_dir, (int)strlen(name) - 1, name);
 
-	if (sysfs__read_build_id(filename, dso->bid.data,
-				 sizeof(dso->bid.data)) == 0)
+	if (sysfs__read_build_id(filename, &dso->bid) == 0)
 		dso->has_build_id = true;
 
 	return 0;
