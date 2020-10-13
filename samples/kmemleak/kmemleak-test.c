d2ed4973e0b5610fb926c7f56505d9c725cf5d5f
Hui Su <sh_def@163.com>
2020-10-13 16:48:53 -0700

    mm,kmemleak-test.c: move kmemleak-test.c to samples dir
    kmemleak-test.c is just a kmemleak test module, which also can not be used
    as a built-in kernel module.  Thus, i think it may should not be in mm
    dir, and move the kmemleak-test.c to samples/kmemleak/kmemleak-test.c.
    Fix the spelling of built-in by the way.
    
    Signed-off-by: Hui Su <sh_def@163.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Rob Herring <robh@kernel.org>
    Cc: Masahiro Yamada <yamada.masahiro@socionext.com>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Cc: Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>
    Cc: Divya Indi <divya.indi@oracle.com>
    Cc: Tomas Winkler <tomas.winkler@intel.com>
    Cc: David Howells <dhowells@redhat.com>
    Link: https://lkml.kernel.org/r/20200925183729.GA172837@rlk
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- samples/kmemleak/kmemleak-test.c
+++ samples/kmemleak/kmemleak-test.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * mm/kmemleak-test.c
+ * samples/kmemleak/kmemleak-test.c
  *
  * Copyright (C) 2008 ARM Limited
  * Written by Catalin Marinas <catalin.marinas@arm.com>
