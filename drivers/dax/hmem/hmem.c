556e3136c56577990b208957c0704381afe81e65
Dan Williams <dan.j.williams@intel.com>
2020-10-13 16:49:13 -0700

    ACPI: HMAT: refactor hmat_register_target_device to hmem_register_device
    In preparation for exposing "Soft Reserved" memory ranges without an HMAT,
    move the hmem device registration to its own compilation unit and make the
    implementation generic.
    
    The generic implementation drops usage acpi_map_pxm_to_online_node() that
    was translating ACPI proximity domain values and instead relies on
    numa_map_to_online_node() to determine the numa node for the device.
    
    [joao.m.martins@oracle.com: CONFIG_DEV_DAX_HMEM_DEVICES should depend on CONFIG_DAX=y]
      Link: https://lkml.kernel.org/r/8f34727f-ec2d-9395-cb18-969ec8a5d0d4@oracle.com
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Ben Skeggs <bskeggs@redhat.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Brice Goglin <Brice.Goglin@inria.fr>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Dave Jiang <dave.jiang@intel.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Ira Weiny <ira.weiny@intel.com>
    Cc: Jason Gunthorpe <jgg@mellanox.com>
    Cc: Jeff Moyer <jmoyer@redhat.com>
    Cc: Jia He <justin.he@arm.com>
    Cc: Joao Martins <joao.m.martins@oracle.com>
    Cc: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Mike Rapoport <rppt@linux.ibm.com>
    Cc: Paul Mackerras <paulus@ozlabs.org>
    Cc: Pavel Tatashin <pasha.tatashin@soleen.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Vishal Verma <vishal.l.verma@intel.com>
    Cc: Wei Yang <richard.weiyang@linux.alibaba.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Cc: Hulk Robot <hulkci@huawei.com>
    Cc: Jason Yan <yanaijie@huawei.com>
    Cc: "Jérôme Glisse" <jglisse@redhat.com>
    Cc: Juergen Gross <jgross@suse.com>
    Cc: kernel test robot <lkp@intel.com>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Link: https://lkml.kernel.org/r/159643096584.4062302.5035370788475153738.stgit@dwillia2-desk3.amr.corp.intel.com
    Link: https://lore.kernel.org/r/158318761484.2216124.2049322072599482736.stgit@dwillia2-desk3.amr.corp.intel.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/dax/hmem/hmem.c
+++ drivers/dax/hmem/hmem.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/memregion.h>
 #include <linux/module.h>
 #include <linux/pfn_t.h>
-#include "bus.h"
+#include "../bus.h"
 
 static int dax_hmem_probe(struct platform_device *pdev)
 {
