e34627fba30ac16c87eacbc1dc69e94c05edf07a
Ben Gardon <bgardon@google.com>
2020-10-14 20:26:44 +0200

    kvm: x86/mmu: Allocate and free TDP MMU roots
    The TDP MMU must be able to allocate paging structure root pages and track
    the usage of those pages. Implement a similar, but separate system for root
    page allocation to that of the x86 shadow paging implementation. When
    future patches add synchronization model changes to allow for parallel
    page faults, these pages will need to be handled differently from the
    x86 shadow paging based MMU's root pages.
    
    Tested by running kvm-unit-tests and KVM selftests on an Intel Haswell
    machine. This series introduced no new failures.
    
    This series can be viewed in Gerrit at:
    	https://linux-review.googlesource.com/c/virt/kvm/kvm/+/2538
    
    Signed-off-by: Ben Gardon <bgardon@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/mmu/tdp_mmu.h
+++ arch/x86/kvm/mmu/tdp_mmu.h
@@ -8,4 +8,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 void kvm_mmu_init_tdp_mmu(struct kvm *kvm);
 void kvm_mmu_uninit_tdp_mmu(struct kvm *kvm);
+
+bool is_tdp_mmu_root(struct kvm *kvm, hpa_t root);
+hpa_t kvm_tdp_mmu_get_vcpu_root_hpa(struct kvm_vcpu *vcpu);
+void kvm_tdp_mmu_free_root(struct kvm *kvm, struct kvm_mmu_page *root);
+
 #endif /* __KVM_X86_MMU_TDP_MMU_H */
