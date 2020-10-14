5d09089a4b27a81330d87c1added988c0763774d
Jordan Niethe <jniethe5@gmail.com>
2020-10-14 18:28:37 +1100

    powerpc/64s: Convert some cpu_setup() and cpu_restore() functions to C
    The only thing keeping the cpu_setup() and cpu_restore() functions
    used in the cputable entries for Power7, Power8, Power9 and Power10 in
    assembly was cpu_restore() being called before there was a stack in
    generic_secondary_smp_init(). Commit ("powerpc/64: Set up a kernel
    stack for secondaries before cpu_restore()") means that it is now
    possible to use C.
    
    Rewrite the functions in C so they are a little bit easier to read.
    This is not changing their functionality.
    
    Signed-off-by: Jordan Niethe <jniethe5@gmail.com>
    [mpe: Tweak copyright and authorship notes]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20201014072837.24539-2-jniethe5@gmail.com

---
+++ arch/powerpc/include/asm/cpu_setup_power.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2020 IBM Corporation
+ */
+void __setup_cpu_power7(unsigned long offset, struct cpu_spec *spec);
+void __restore_cpu_power7(void);
+void __setup_cpu_power8(unsigned long offset, struct cpu_spec *spec);
+void __restore_cpu_power8(void);
+void __setup_cpu_power9(unsigned long offset, struct cpu_spec *spec);
+void __restore_cpu_power9(void);
+void __setup_cpu_power10(unsigned long offset, struct cpu_spec *spec);
+void __restore_cpu_power10(void);
