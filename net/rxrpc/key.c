abe3f028bd77a8696327cceaf793e2a260a2c31a
David Howells <dhowells@redhat.com>
2020-10-15 15:47:45 +0100

    rxrpc: Fix example key name in a comment
    Fix an example of an rxrpc key name in a comment.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- net/rxrpc/key.c
+++ net/rxrpc/key.c
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Written by David Howells (dhowells@redhat.com)
  *
  * RxRPC keys should have a description of describing their purpose:
- *	"afs@CAMBRIDGE.REDHAT.COM>
+ *	"afs@example.com"
  */
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
