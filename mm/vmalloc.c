e552eea8342a5da9d0851f4807db6cf4db55d5e0
Christoph Hellwig <hch@lst.de>
2020-10-17 16:15:46 -0700

    mm: remove the filename in the top of file comment in vmalloc.c
    No point in having the filename inside the file.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Link: https://lkml.kernel.org/r/20201002124035.1539300-3-hch@lst.de
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1,8 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- *  linux/mm/vmalloc.c
- *
  *  Copyright (C) 1993  Linus Torvalds
  *  Support of BIGMEM added by Gerhard Wichert, Siemens AG, July 1999
  *  SMP-safe vmalloc/vfree/ioremap, Tigran Aivazian <tigran@veritas.com>, May 2000
