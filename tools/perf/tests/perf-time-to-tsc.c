e1b66182fba549b0a52af159e7b63d2bf24d8c30
Leo Yan <leo.yan@linaro.org>
2020-10-19 18:02:35 +0800

    perf tests tsc: Make tsc testing as a common testing
    x86 arch provides the testing for conversion between tsc and perf time,
    the testing is located in x86 arch folder.  Move this testing out from
    x86 arch folder and place it into the common testing folder, so allows
    to execute tsc testing on other architectures (e.g. Arm64).
    
    This patch removes the inclusion of "arch-tests.h" from the testing
    code, this can avoid building failure if any arch has no this header
    file.
    
    Committer testing:
    
      $ perf test -v tsc
      Couldn't bump rlimit(MEMLOCK), failures may take place when creating BPF maps, etc
      70: Convert perf time to TSC                                        :
      --- start ---
      test child forked, pid 4032834
      mmap size 528384B
      1st event perf time 165409788843605 tsc 336578703793868
      rdtsc          time 165409788854986 tsc 336578703837038
      2nd event perf time 165409788855487 tsc 336578703838935
      test child finished with 0
      ---- end ----
      Convert perf time to TSC: Ok
      $
    
    Signed-off-by: Leo Yan <leo.yan@linaro.org>
    Acked-by: Jiri Olsa <jolsa@redhat.com>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Link: https://lore.kernel.org/r/20201019100236.23675-2-leo.yan@linaro.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/tests/perf-time-to-tsc.c
+++ tools/perf/tests/perf-time-to-tsc.c
@@ -19,10 +19,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "thread_map.h"
 #include "record.h"
 #include "tsc.h"
-#include "util/mmap.h"
-#include "tests/tests.h"
-
-#include "arch-tests.h"
+#include "mmap.h"
+#include "tests.h"
 
 #define CHECK__(x) {				\
 	while ((x) < 0) {			\
