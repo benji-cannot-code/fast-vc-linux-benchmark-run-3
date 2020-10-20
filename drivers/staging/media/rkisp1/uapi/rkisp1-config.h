fa10670d515d360ed3dfabea1c8cad93d9b1791f
Dafna Hirschfeld <dafna.hirschfeld@collabora.com>
2020-10-20 15:25:14 +0200

    media: staging: rkisp1: uapi: add "WITH Linux-syscall-note"
    Add "WITH Linux-syscall-note" to the uapi
    header SPDX comment.
    
    Signed-off-by: Dafna Hirschfeld <dafna.hirschfeld@collabora.com>
    Acked-by: Helen Koike <helen.koike@collabora.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/staging/media/rkisp1/uapi/rkisp1-config.h
+++ drivers/staging/media/rkisp1/uapi/rkisp1-config.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/* SPDX-License-Identifier: ((GPL-2.0+ WITH Linux-syscall-note) OR MIT) */
 /*
  * Rockchip ISP1 userspace API
  * Copyright (C) 2017 Rockchip Electronics Co., Ltd.
