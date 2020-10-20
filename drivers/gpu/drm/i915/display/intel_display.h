ba330f9a1e0f74ffb40b0270cd3bba91d4dc90b8
Pankaj Bharadiya <pankaj.laxminarayan.bharadiya@intel.com>
2020-10-20 21:44:25 +0530

    drm/i915/display: Add Nearest-neighbor based integer scaling support
    Integer scaling (IS) is a nearest-neighbor upscaling technique that
    simply scales up the existing pixels by an integer
    (i.e., whole number) multiplier.Nearest-neighbor (NN) interpolation
    works by filling in the missing color values in the upscaled image
    with that of the coordinate-mapped nearest source pixel value.
    
    Both IS and NN preserve the clarity of the original image. Integer
    scaling is particularly useful for pixel art games that rely on
    sharp, blocky images to deliver their distinctive look.
    
    Introduce functions to configure the scaler filter coefficients to
    enable nearest-neighbor filtering.
    
    Bspec: 49247
    
    changes since v6:
    * Trust compiler, remove pointless inline keyword from cnl_coef_tap()
      & cnl_nearest_filter_coef() functions (Ville)
    changes since v4:
    * Make cnl_coef_tap(), cnl_nearest_filter_coef() inline (Uma)
    changes since v3:
    * None
    changes since v2:
    * Move APIs from 5/5 into this patch.
    * Change filter programming related function names to cnl_*, move
      filter select bits related code into inline function (Ville)
    changes since v1:
    * Rearrange skl_scaler_setup_nearest_neighbor_filter() to iterate the
      registers directly instead of the phases and taps (Ville)
    
    changes since RFC:
    * Refine the skl_scaler_setup_nearest_neighbor_filter() logic (Ville)
    
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Uma Shankar <uma.shankar@intel.com>
    Signed-off-by: Shashank Sharma <shashank.sharma@intel.com>
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>
    Signed-off-by: Pankaj Bharadiya <pankaj.laxminarayan.bharadiya@intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201020161427.6941-4-pankaj.laxminarayan.bharadiya@intel.com

--- drivers/gpu/drm/i915/display/intel_display.h
+++ drivers/gpu/drm/i915/display/intel_display.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <drm/drm_util.h>
 
 enum link_m_n_set;
+enum drm_scaling_filter;
 struct dpll;
 struct drm_connector;
 struct drm_device;
@@ -600,6 +601,9 @@ void intel_crtc_arm_fifo_underrun(struct intel_crtc *crtc,
 
 u16 skl_scaler_calc_phase(int sub, int scale, bool chroma_center);
 void skl_scaler_disable(const struct intel_crtc_state *old_crtc_state);
+u32 skl_scaler_get_filter_select(enum drm_scaling_filter filter, int set);
+void skl_scaler_setup_filter(struct drm_i915_private *dev_priv, enum pipe pipe,
+			     int id, int set, enum drm_scaling_filter filter);
 void ilk_pfit_disable(const struct intel_crtc_state *old_crtc_state);
 u32 glk_plane_color_ctl(const struct intel_crtc_state *crtc_state,
 			const struct intel_plane_state *plane_state);
