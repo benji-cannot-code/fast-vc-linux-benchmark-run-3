912577641a100a436f5d1e32f84ba738f7d95ab0
Chanwoo Choi <cw00.choi@samsung.com>
2020-10-20 15:12:12 +0900

    PM / devfreq: Remove redundant governor_name from struct devfreq
    The devfreq structure instance contains the governor_name and a governor
    instance. When need to show the governor name, better to use the name
    of devfreq_governor structure. So, governor_name variable in struct devfreq
    is a redundant and unneeded variable. Remove the redundant governor_name
    of struct devfreq and then use the name of devfreq_governor instance.
    
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- drivers/devfreq/governor.h
+++ drivers/devfreq/governor.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/devfreq.h>
 
+#define DEVFREQ_NAME_LEN			16
+
 #define to_devfreq(DEV)	container_of((DEV), struct devfreq, dev)
 
 /* Devfreq events */
