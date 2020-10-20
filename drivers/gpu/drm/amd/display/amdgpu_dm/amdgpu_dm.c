1387d20a3c47b88d3f510a2f367da635ff19ae54
Krunoslav Kovac <Krunoslav.Kovac@amd.com>
2020-10-20 16:23:15 -0400

    drm/amd/display: Engage PSR synchronously
    [Why & How]
    The intended use is to force PSR into active state and ignore all
    events until explicit EXIT.
    A new event force_static is added to power module. It is then sent
    to FW.
    
    Signed-off-by: Krunoslav Kovac <Krunoslav.Kovac@amd.com>
    Acked-by: Bindu Ramamurthy <bindu.r@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -9612,7 +9612,7 @@ bool amdgpu_dm_psr_enable(struct dc_stream_state *stream)
 					   &stream, 1,
 					   &params);
 
-	return dc_link_set_psr_allow_active(link, true, false);
+	return dc_link_set_psr_allow_active(link, true, false, false);
 }
 
 /*
@@ -9626,7 +9626,7 @@ static bool amdgpu_dm_psr_disable(struct dc_stream_state *stream)
 
 	DRM_DEBUG_DRIVER("Disabling psr...\n");
 
-	return dc_link_set_psr_allow_active(stream->link, false, true);
+	return dc_link_set_psr_allow_active(stream->link, false, true, false);
 }
 
 /*
