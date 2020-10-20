8c5afc0c1e007d760d3ca61a7495aa332e8470ca
Tao Zhou <tao.zhou1@amd.com>
2020-10-20 16:47:10 +0800

    drm/amd/pm: update driver if version for dimgrey_cavefish
    Per PMFW 59.9.0.
    
    Signed-off-by: Tao Zhou <tao.zhou1@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/inc/smu_v11_0.h
+++ drivers/gpu/drm/amd/pm/inc/smu_v11_0.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SMU11_DRIVER_IF_VERSION_Sienna_Cichlid 0x3A
 #define SMU11_DRIVER_IF_VERSION_Navy_Flounder 0x5
 #define SMU11_DRIVER_IF_VERSION_VANGOGH 0x01
-#define SMU11_DRIVER_IF_VERSION_Dimgrey_Cavefish 0x9
+#define SMU11_DRIVER_IF_VERSION_Dimgrey_Cavefish 0xB
 
 /* MP Apertures */
 #define MP0_Public			0x03800000
