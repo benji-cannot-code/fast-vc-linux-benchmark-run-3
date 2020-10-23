a7ce07fbfeed67cc5469e77226580c6e8507a4d1
Takashi Iwai <tiwai@suse.de>
2020-10-23 09:46:55 +0200

    drm/amd/display: Don't invoke kgdb_breakpoint() unconditionally
    ASSERT_CRITICAL() invokes kgdb_breakpoint() whenever either
    CONFIG_KGDB or CONFIG_HAVE_KGDB is set.  This, however, may lead to a
    kernel panic when no kdb stuff is attached, since the
    kgdb_breakpoint() call issues INT3.  It's nothing but a surprise for
    normal end-users.
    
    For avoiding the pitfall, make the kgdb_breakpoint() call only when
    CONFIG_DEBUG_KERNEL_DC is set.
    
    https://bugzilla.opensuse.org/show_bug.cgi?id=1177973
    Cc: <stable@vger.kernel.org>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Nicholas Kazlauskas <nicholas.kazlauskas@amd.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/display/dc/os_types.h
+++ drivers/gpu/drm/amd/display/dc/os_types.h
@@ -95,7 +95,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * general debug capabilities
  *
  */
-#if defined(CONFIG_HAVE_KGDB) || defined(CONFIG_KGDB)
+#if defined(CONFIG_DEBUG_KERNEL_DC) && (defined(CONFIG_HAVE_KGDB) || defined(CONFIG_KGDB))
 #define ASSERT_CRITICAL(expr) do {	\
 	if (WARN_ON(!(expr))) { \
 		kgdb_breakpoint(); \
