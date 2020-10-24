3b21ba08320688c2790d0be10b1fb75bedd4e1ba
Thomas Gleixner <tglx@linutronix.de>
2020-10-24 22:35:11 +0100

    genirq/msi: Allow shadow declarations of msi_msg:: $member
    Architectures like x86 have their MSI messages in various bits of the data,
    address_lo and address_hi field. Composing or decomposing these messages
    with bitmasks and shifts is possible, but unreadable gunk.
    
    Allow architectures to provide an architecture specific representation for
    each member of msi_msg. Provide empty defaults for each and stick them into
    an union.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20201024213535.443185-12-dwmw2@infradead.org

--- include/asm-generic/msi.h
+++ include/asm-generic/msi.h
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
+#ifdef CONFIG_GENERIC_MSI_IRQ_DOMAIN
+
 #ifndef NUM_MSI_ALLOC_SCRATCHPAD_REGS
 # define NUM_MSI_ALLOC_SCRATCHPAD_REGS	2
 #endif
@@ -30,5 +32,7 @@ typedef struct msi_alloc_info {
 } msi_alloc_info_t;
 
 #define GENERIC_MSI_DOMAIN_OPS		1
+
+#endif /* CONFIG_GENERIC_MSI_IRQ_DOMAIN */
 
 #endif
