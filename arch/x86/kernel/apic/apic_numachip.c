c87e07827c8df2a0fd3d394db4d8936664c52aef
Thomas Gleixner <tglx@linutronix.de>
2020-10-24 22:35:07 +0100

    x86/apic: Get rid of apic:: Dest_logical
    struct apic has two members which store information about the destination
    mode: dest_logical and irq_dest_mode.
    
    dest_logical contains a mask which was historically used to set the
    destination mode in IPI messages. Over time the usage was reduced and the
    logical/physical functions were seperated.
    
    There are only a few places which still use 'dest_logical' but they can
    use 'irq_dest_mode' instead.
    
    irq_dest_mode is actually a boolean where 0 means physical destination mode
    and 1 means logical destination mode. Of course the name does not reflect
    the functionality. This will be cleaned up in a subsequent change.
    
    Remove apic::dest_logical and fixup the remaining users.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20201024213535.443185-8-dwmw2@infradead.org

--- arch/x86/kernel/apic/apic_numachip.c
+++ arch/x86/kernel/apic/apic_numachip.c
@@ -251,11 +251,9 @@ static const struct apic apic_numachip1 __refconst = {
 	.irq_dest_mode			= 0, /* physical */
 
 	.disable_esr			= 0,
-	.dest_logical			= 0,
+
 	.check_apicid_used		= NULL,
-
 	.init_apic_ldr			= flat_init_apic_ldr,
-
 	.ioapic_phys_id_map		= NULL,
 	.setup_apic_routing		= NULL,
 	.cpu_present_to_apicid		= default_cpu_present_to_apicid,
@@ -300,11 +298,9 @@ static const struct apic apic_numachip2 __refconst = {
 	.irq_dest_mode			= 0, /* physical */
 
 	.disable_esr			= 0,
-	.dest_logical			= 0,
+
 	.check_apicid_used		= NULL,
-
 	.init_apic_ldr			= flat_init_apic_ldr,
-
 	.ioapic_phys_id_map		= NULL,
 	.setup_apic_routing		= NULL,
 	.cpu_present_to_apicid		= default_cpu_present_to_apicid,
