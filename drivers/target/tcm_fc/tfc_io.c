e629426d132f42dcff80c07b691102b10d2e9785
Arnd Bergmann <arnd@arndb.de>
2020-10-26 17:06:12 +0100

    scsi: libfc: Move scsi/fc_encode.h to libfc
    Most of this file is only used inside of libfc, so move it to where it is
    actually used, with only fc_fill_fc_hdr() left inside of the header.
    
    Link: https://lore.kernel.org/r/20201026160705.3706396-1-arnd@kernel.org
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>

--- drivers/target/tcm_fc/tfc_io.c
+++ drivers/target/tcm_fc/tfc_io.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ratelimit.h>
 #include <asm/unaligned.h>
 #include <scsi/libfc.h>
-#include <scsi/fc_encode.h>
 
 #include <target/target_core_base.h>
 #include <target/target_core_fabric.h>
