3220bbe0ac0d032040aa2e1dc260bfb7deaffa3a
Jens Axboe <axboe@kernel.dk>
2020-10-26 14:32:28 -0600

    entry: Add support for TIF_NOTIFY_SIGNAL
    Add TIF_NOTIFY_SIGNAL handling in the generic entry code, which if set,
    will return true if signal_pending() is used in a wait loop. That causes an
    exit of the loop so that notify_signal tracehooks can be run. If the wait
    loop is currently inside a system call, the system call is restarted once
    task_work has been processed.
    
    In preparation for only having arch_do_signal() handle syscall restarts if
    _TIF_SIGPENDING isn't set, rename it to arch_do_signal_or_restart().  Pass
    in a boolean that tells the architecture specific signal handler if it
    should attempt to get a signal, or just process a potential syscall
    restart.
    
    For !CONFIG_GENERIC_ENTRY archs, add the TIF_NOTIFY_SIGNAL handling to
    get_signal(). This is done to minimize the needed architecture changes to
    support this feature.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Oleg Nesterov <oleg@redhat.com>
    Link: https://lore.kernel.org/r/20201026203230.386348-3-axboe@kernel.dk

--- include/linux/entry-common.h
+++ include/linux/entry-common.h
@@ -38,6 +38,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # define _TIF_UPROBE			(0)
 #endif
 
+#ifndef _TIF_NOTIFY_SIGNAL
+# define _TIF_NOTIFY_SIGNAL		(0)
+#endif
+
 /*
  * TIF flags handled in syscall_enter_from_usermode()
  */
@@ -70,7 +74,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define EXIT_TO_USER_MODE_WORK						\
 	(_TIF_SIGPENDING | _TIF_NOTIFY_RESUME | _TIF_UPROBE |		\
-	 _TIF_NEED_RESCHED | _TIF_PATCH_PENDING |			\
+	 _TIF_NEED_RESCHED | _TIF_PATCH_PENDING | _TIF_NOTIFY_SIGNAL |	\
 	 ARCH_EXIT_TO_USER_MODE_WORK)
 
 /**
@@ -227,12 +231,13 @@ static __always_inline void arch_exit_to_user_mode(void) { }
 #endif
 
 /**
- * arch_do_signal -  Architecture specific signal delivery function
+ * arch_do_signal_or_restart -  Architecture specific signal delivery function
  * @regs:	Pointer to currents pt_regs
+ * @has_signal:	actual signal to handle
  *
  * Invoked from exit_to_user_mode_loop().
  */
-void arch_do_signal(struct pt_regs *regs);
+void arch_do_signal_or_restart(struct pt_regs *regs, bool has_signal);
 
 /**
  * arch_syscall_exit_tracehook - Wrapper around tracehook_report_syscall_exit()
