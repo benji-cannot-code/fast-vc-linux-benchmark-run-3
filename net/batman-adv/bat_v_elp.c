bb9e6c4e6902143768ebf57086aa33f1495f0cdf
Sven Eckelmann <sven@narfation.org>
2020-10-26 21:01:59 +0100

    batman-adv: Add new include for min/max helpers
    The commit b296a6d53339 ("kernel.h: split out min()/max() et al. helpers")
    moved the min/max helper functionality from kernel.h to minmax.h. Adjust
    the kernel code accordingly to avoid fragile indirect includes.
    
    Signed-off-by: Sven Eckelmann <sven@narfation.org>
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

--- net/batman-adv/bat_v_elp.c
+++ net/batman-adv/bat_v_elp.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/jiffies.h>
 #include <linux/kernel.h>
 #include <linux/kref.h>
+#include <linux/minmax.h>
 #include <linux/netdevice.h>
 #include <linux/nl80211.h>
 #include <linux/prandom.h>
--- net/batman-adv/icmp_socket.c
+++ net/batman-adv/icmp_socket.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_ether.h>
 #include <linux/kernel.h>
 #include <linux/list.h>
+#include <linux/minmax.h>
 #include <linux/module.h>
 #include <linux/netdevice.h>
 #include <linux/pkt_sched.h>
