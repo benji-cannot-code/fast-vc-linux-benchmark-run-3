0f3c168dba16fc63babee52588f6beb725d8670d
Krzysztof Kozlowski <krzk@kernel.org>
2020-10-26 16:38:08 +0100

    mmc: s3cmci: include GPIO descriptor consumer header
    The driver uses GPIO Descriptor Consumer Interface so include proper
    header to fix compile test errors like:
    
        drivers/mmc/host/s3cmci.c: In function ‘s3cmci_check_sdio_irq’:
        drivers/mmc/host/s3cmci.c:304:7: error:
            implicit declaration of function ‘gpiod_get_value’; did you mean ‘gpio_get_value’? [-Werror=implicit-function-declaration]
    
        drivers/mmc/host/s3cmci.c: In function ‘s3cmci_probe_pdata’:
        drivers/mmc/host/s3cmci.c:1451:19: error:
            implicit declaration of function ‘devm_gpiod_get_index’ [-Werror=implicit-function-declaration]
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>
    Link: https://lore.kernel.org/r/20201026153810.89512-1-krzk@kernel.org
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/host/s3cmci.c
+++ drivers/mmc/host/s3cmci.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpufreq.h>
 #include <linux/debugfs.h>
 #include <linux/seq_file.h>
-#include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
 #include <linux/io.h>
