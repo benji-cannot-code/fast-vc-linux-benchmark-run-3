8698746ef6a84def818263c5224024286f39737a
Andi Kleen <andi@firstfloor.org>
2020-10-26 17:27:36 -0700

    perf stat: Add --quiet option
    Add a new --quiet option to 'perf stat'. This is useful with 'perf stat
    record' to write the data only to the perf.data file, which can lower
    measurement overhead because the data doesn't need to be formatted.
    
    On my 4C desktop:
    
      % time ./perf stat record  -e $(python -c 'print ",".join(["cycles"]*1000)')  -a -I 1000 sleep 5
      ...
      real    0m5.377s
      user    0m0.238s
      sys     0m0.452s
      % time ./perf stat record --quiet -e $(python -c 'print ",".join(["cycles"]*1000)')  -a -I 1000 sleep 5
    
      real    0m5.452s
      user    0m0.183s
      sys     0m0.423s
    
    In this example it cuts the user time by 20%. On systems with more cores
    the savings are higher.
    
    Signed-off-by: Andi Kleen <andi@firstfloor.org>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Cc: Alexey Budankov <alexey.budankov@linux.intel.com>
    Link: http://lore.kernel.org/lkml/20201027002737.30942-1-andi@firstfloor.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/stat.h
+++ tools/perf/util/stat.h
@@ -123,6 +123,7 @@ struct perf_stat_config {
 	bool			 metric_no_group;
 	bool			 metric_no_merge;
 	bool			 stop_read_counter;
+	bool			 quiet;
 	FILE			*output;
 	unsigned int		 interval;
 	unsigned int		 timeout;
