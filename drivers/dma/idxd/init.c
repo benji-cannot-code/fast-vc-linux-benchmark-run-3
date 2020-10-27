686f618f816e829557be2bc35a68a28893f9d95e
Dave Jiang <dave.jiang@intel.com>
2020-10-27 10:34:40 -0700

    dmaengine: idxd: Clean up descriptors with fault error
    Add code to "complete" a descriptor when the descriptor or its completion
    address hit a fault error when SVA mode is being used. This error can be
    triggered due to bad programming by the user. A lock is introduced in order
    to protect the descriptor completion lists since the fault handler will run
    from the system work queue after being scheduled in the interrupt handler.
    
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Link: https://lore.kernel.org/r/160382008092.3911367.12766483427643278985.stgit@djiang5-desk3.ch.intel.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- drivers/dma/idxd/init.c
+++ drivers/dma/idxd/init.c
@@ -98,6 +98,7 @@ static int idxd_setup_interrupts(struct idxd_device *idxd)
 	for (i = 0; i < msixcnt; i++) {
 		idxd->irq_entries[i].id = i;
 		idxd->irq_entries[i].idxd = idxd;
+		spin_lock_init(&idxd->irq_entries[i].list_lock);
 	}
 
 	msix = &idxd->msix_entries[0];
