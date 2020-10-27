bab1a09520ffb55664e40b8107eec18319b475d1
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-10-27 10:51:14 +0100

    drm: kernel-doc: document drm_dp_set_subconnector_property() params
    Changeset e5b92773287c ("drm: report dp downstream port type as a subconnector property")
    added a new function to the kAPI, but didn't add any documentation
    for the parameters for drm_dp_set_subconnector_property().
    
    Fixes: e5b92773287c ("drm: report dp downstream port type as a subconnector property")
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/0870be85a77bea4ba5cf1715010834289a4e10b1.1603791716.git.mchehab+huawei@kernel.org

--- drivers/gpu/drm/drm_dp_helper.c
+++ drivers/gpu/drm/drm_dp_helper.c
@@ -1029,7 +1029,8 @@ EXPORT_SYMBOL(drm_dp_downstream_debug);
 
 /**
  * drm_dp_subconnector_type() - get DP branch device type
- *
+ * @dpcd: DisplayPort configuration data
+ * @port_cap: port capabilities
  */
 enum drm_mode_subconnector
 drm_dp_subconnector_type(const u8 dpcd[DP_RECEIVER_CAP_SIZE],
@@ -1080,6 +1081,10 @@ EXPORT_SYMBOL(drm_dp_subconnector_type);
 
 /**
  * drm_mode_set_dp_subconnector_property - set subconnector for DP connector
+ * @connector: connector to set property on
+ * @status: connector status
+ * @dpcd: DisplayPort configuration data
+ * @port_cap: port capabilities
  *
  * Called by a driver on every detect event.
  */
