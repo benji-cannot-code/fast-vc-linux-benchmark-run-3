b863b02a2d462e3825036a5b70551b98a8bf80b3
Sandeep Singh <sandeep.singh@amd.com>
2020-10-28 14:30:10 +0530

    AMD_SFH: Fix for incorrect Sensor index
    It appears like the accelerometer/magnetometer and gyroscope indices were
    interchanged in the patch series which got into for-5.11/amd-sfh-hid until
    Mandoli/Richard reported to us.  Ideally sensor indices should be 0,1,2 for the
    accelerometer, Gyroscope, Magnetometer respectively, but this interchanged
    possibly could be because i was using a test MP2 firmware on my machine.
    
    This patch fixes the earlier commit with the right sensor indices and also
    removing unused structures _hid_report_descriptor, _hid_device_descriptor as
    reported by Richard.
    
    Fixes: 4f567b9f8141 ("SFH: PCIe driver to add support of AMD sensor fusion hub 4f567b9f8141")
    Reported-by: Mandoli <lipheng@hanmail.net>
    Reported-by: Richard Neumann <mail@richard-neumann.de>
    Signed-off-by: Sandeep Singh <sandeep.singh@amd.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/amd-sfh-hid/amd_sfh_pcie.c
+++ drivers/hid/amd-sfh-hid/amd_sfh_pcie.c
@@ -21,9 +21,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRIVER_NAME	"pcie_mp2_amd"
 #define DRIVER_DESC	"AMD(R) PCIe MP2 Communication Driver"
 
-#define ACEL_EN		BIT(1)
-#define GYRO_EN		BIT(2)
-#define MAGNO_EN	BIT(3)
+#define ACEL_EN		BIT(0)
+#define GYRO_EN		BIT(1)
+#define MAGNO_EN		BIT(2)
 #define ALS_EN		BIT(19)
 
 void amd_start_sensor(struct amd_mp2_dev *privdata, struct amd_mp2_sensor_info info)
