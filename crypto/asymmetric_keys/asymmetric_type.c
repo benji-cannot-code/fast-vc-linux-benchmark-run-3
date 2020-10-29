34982ac6d2b75d05195ce7c6501f9a1c4e91b91a
Krzysztof Kozlowski <krzk@kernel.org>
2020-10-29 16:48:30 +0100

    KEYS: asymmetric: Fix kerneldoc
    Fix W=1 compile warnings (invalid kerneldoc):
    
        crypto/asymmetric_keys/asymmetric_type.c:160: warning: Function parameter or member 'kid1' not described in 'asymmetric_key_id_same'
        crypto/asymmetric_keys/asymmetric_type.c:160: warning: Function parameter or member 'kid2' not described in 'asymmetric_key_id_same'
        crypto/asymmetric_keys/asymmetric_type.c:160: warning: Excess function parameter 'kid_1' description in 'asymmetric_key_id_same'
        crypto/asymmetric_keys/asymmetric_type.c:160: warning: Excess function parameter 'kid_2' description in 'asymmetric_key_id_same'
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Acked-by: Randy Dunlap <rdunlap@infradead.org>
    Reviewed-by: Ben Boeckel <mathstuf@gmail.com>
    Reviewed-by: Jarkko Sakkinen <jarkko.sakkinen@iki.fi>

--- crypto/asymmetric_keys/asymmetric_type.c
+++ crypto/asymmetric_keys/asymmetric_type.c
@@ -153,7 +153,8 @@ EXPORT_SYMBOL_GPL(asymmetric_key_generate_id);
 
 /**
  * asymmetric_key_id_same - Return true if two asymmetric keys IDs are the same.
- * @kid_1, @kid_2: The key IDs to compare
+ * @kid1: The key ID to compare
+ * @kid2: The key ID to compare
  */
 bool asymmetric_key_id_same(const struct asymmetric_key_id *kid1,
 			    const struct asymmetric_key_id *kid2)
@@ -169,7 +170,8 @@ EXPORT_SYMBOL_GPL(asymmetric_key_id_same);
 /**
  * asymmetric_key_id_partial - Return true if two asymmetric keys IDs
  * partially match
- * @kid_1, @kid_2: The key IDs to compare
+ * @kid1: The key ID to compare
+ * @kid2: The key ID to compare
  */
 bool asymmetric_key_id_partial(const struct asymmetric_key_id *kid1,
 			       const struct asymmetric_key_id *kid2)
