96637928a7fc759f006aaec5e1768ff9aeef017d
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-10-30 18:59:15 +0200

    irqdomain: Remove unused of_device_id forward declaration
    There is no users of of_device_id in irqdomain.h. Drop it.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://lore.kernel.org/r/20201030165919.86234-2-andriy.shevchenko@linux.intel.com

--- include/linux/irqdomain.h
+++ include/linux/irqdomain.h
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct device_node;
 struct irq_domain;
-struct of_device_id;
 struct irq_chip;
 struct irq_data;
 struct cpumask;
