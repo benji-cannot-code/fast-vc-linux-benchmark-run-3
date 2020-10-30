769b7776e053b093dfe7957ed287a1119778dfdf
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-10-30 08:40:50 +0100

    docs: ABI: cleanup several ABI documents
    There are some ABI documents that, while they don't generate
    any warnings, they have issues when parsed by get_abi.pl script
    on its output result.
    
    Address them, in order to provide a clean output.
    
    Reviewed-by: Tom Rix <trix@redhat.com> # for fpga-manager
    Reviewed-By: Kajol Jain<kjain@linux.ibm.com> # for sysfs-bus-event_source-devices-hv_gpci and sysfs-bus-event_source-devices-hv_24x7
    Acked-by: Jonathan Cameron <Jonathan.Cameron@huawei.com> #for IIO
    Acked-by: Oded Gabbay <oded.gabbay@gmail.com> # for Habanalabs
    Acked-by: Vaibhav Jain <vaibhav@linux.ibm.com> # for sysfs-bus-papr-pmem
    Acked-by: Cezary Rojewski <cezary.rojewski@intel.com> # for catpt
    Acked-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Acked-by: Ilya Dryomov <idryomov@gmail.com> # for rbd
    Acked-by: Jonathan Corbet <corbet@lwn.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Link: https://lore.kernel.org/r/5bc78e5b68ed1e9e39135173857cb2e753be868f.1604042072.git.mchehab+huawei@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- scripts/get_abi.pl
+++ scripts/get_abi.pl
@@ -317,8 +317,6 @@ sub output_rest {
 				$len = length($name) if (length($name) > $len);
 			}
 
-			print "What:\n\n";
-
 			print "+-" . "-" x $len . "-+\n";
 			foreach my $name (@names) {
 				printf "| %s", $name . " " x ($len - length($name)) . " |\n";
