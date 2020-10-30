c0e513157f1cf202070c43cfa1841fc2628f4986
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-10-30 08:40:26 +0100

    scripts: get_abi.pl: output users in ReST format
    Right now, the script only outputs Users on search. Print it
    also in ReST format.
    
    Acked-by: Jonathan Corbet <corbet@lwn.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Link: https://lore.kernel.org/r/83def2a692c4c678f21ee6e76b66a54d2be1e796.1604042072.git.mchehab+huawei@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- scripts/get_abi.pl
+++ scripts/get_abi.pl
@@ -372,6 +372,14 @@ sub output_rest {
 				print "- :ref:`$content <$label>`\n\n";
 			}
 		}
+
+		if (defined($data{$what}->{users})) {
+			my $users = $data{$what}->{users};
+
+			$users =~ s/\n/\n\t/g;
+			printf "Users:\n\t%s\n\n", $users if ($users ne "");
+		}
+
 	}
 }
 
