c912c42ac9232f7cb233d3a09870c7f24020dab5
Weiyi Lu <weiyi.lu@mediatek.com>
2020-10-30 12:36:21 +0100

    soc: mediatek: pm-domains: Add default power off flag
    For some power domain, like conn on MT8192, it should be default OFF.
    Because the power on/off control relies the function of connectivity chip
    and its firmware. And if project choose other chip vendor solution,
    those necessary connectivity functions will not provided.
    
    Signed-off-by: Weiyi Lu <weiyi.lu@mediatek.com>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Link: https://lore.kernel.org/r/20201030113622.201188-16-enric.balletbo@collabora.com
    Signed-off-by: Matthias Brugger <matthias.bgg@gmail.com>

--- drivers/soc/mediatek/mtk-pm-domains.h
+++ drivers/soc/mediatek/mtk-pm-domains.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MTK_SCPD_ACTIVE_WAKEUP		BIT(0)
 #define MTK_SCPD_FWAIT_SRAM		BIT(1)
 #define MTK_SCPD_SRAM_ISO		BIT(2)
+#define MTK_SCPD_KEEP_DEFAULT_OFF	BIT(3)
 #define MTK_SCPD_CAPS(_scpd, _x)	((_scpd)->data->caps & (_x))
 
 #define SPM_VDE_PWR_CON			0x0210
