f27259ea899e1404cb6874162a47eec0ec66aee2
Arnd Bergmann <arnd@arndb.de>
2020-10-30 15:26:23 +0100

    m68k: m68328: move platform code to separate files
    The dragen2 and ucsimm/ucdimm files require a bit of
    custom code compared to the other dragonball platforms,
    move them into separate files as a preparation for a
    build fix.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Ungerer <gerg@linux-m68k.org>

--- arch/m68k/68000/m68328.c
+++ arch/m68k/68000/m68328.c
@@ -26,9 +26,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "bootlogo.h"
 #endif
 
+#include "m68328.h"
+
 /***************************************************************************/
 
-int m68328_hwclk(int set, struct rtc_time *t);
 
 /***************************************************************************/
 
