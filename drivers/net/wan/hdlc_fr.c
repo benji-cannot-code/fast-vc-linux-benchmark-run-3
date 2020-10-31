74a7b880be34400d137e826792cf59df688e237b
Xie He <xie.he.0141@gmail.com>
2020-10-31 11:10:39 -0700

    net: hdlc_fr: Simpify fr_rx by using "goto rx_drop" to drop frames
    When the fr_rx function drops a received frame (because the protocol type
    is not supported, or because the PVC virtual device that corresponds to
    the DLCI number and the protocol type doesn't exist), the function frees
    the skb and returns.
    
    The code for freeing the skb and returning is repeated several times, this
    patch uses "goto rx_drop" to replace them so that the code looks cleaner.
    
    Cc: Krzysztof Halasa <khc@pm.waw.pl>
    Cc: Willem de Bruijn <willemdebruijn.kernel@gmail.com>
    Signed-off-by: Xie He <xie.he.0141@gmail.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/wan/hdlc_fr.c
+++ drivers/net/wan/hdlc_fr.c
@@ -905,8 +905,7 @@ static int fr_rx(struct sk_buff *skb)
 		netdev_info(frad, "No PVC for received frame's DLCI %d\n",
 			    dlci);
 #endif
-		dev_kfree_skb_any(skb);
-		return NET_RX_DROP;
+		goto rx_drop;
 	}
 
 	if (pvc->state.fecn != fh->fecn) {
@@ -964,14 +963,12 @@ static int fr_rx(struct sk_buff *skb)
 		default:
 			netdev_info(frad, "Unsupported protocol, OUI=%x PID=%x\n",
 				    oui, pid);
-			dev_kfree_skb_any(skb);
-			return NET_RX_DROP;
+			goto rx_drop;
 		}
 	} else {
 		netdev_info(frad, "Unsupported protocol, NLPID=%x length=%i\n",
 			    data[3], skb->len);
-		dev_kfree_skb_any(skb);
-		return NET_RX_DROP;
+		goto rx_drop;
 	}
 
 	if (dev) {
@@ -983,12 +980,12 @@ static int fr_rx(struct sk_buff *skb)
 		netif_rx(skb);
 		return NET_RX_SUCCESS;
 	} else {
-		dev_kfree_skb_any(skb);
-		return NET_RX_DROP;
+		goto rx_drop;
 	}
 
- rx_error:
+rx_error:
 	frad->stats.rx_errors++; /* Mark error */
+rx_drop:
 	dev_kfree_skb_any(skb);
 	return NET_RX_DROP;
 }
