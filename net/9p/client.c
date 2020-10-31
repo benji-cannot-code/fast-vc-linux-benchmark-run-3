8ae6b799f4a76cf8a346a18b2d62943bed3159c8
Andrew Lunn <andrew@lunn.ch>
2020-10-31 19:26:55 +0100

    net: 9p: Fix kerneldoc warnings of missing parameters etc
    net/9p/client.c:420: warning: Function parameter or member 'c' not described in 'p9_client_cb'
    net/9p/client.c:420: warning: Function parameter or member 'req' not described in 'p9_client_cb'
    net/9p/client.c:420: warning: Function parameter or member 'status' not described in 'p9_client_cb'
    net/9p/client.c:568: warning: Function parameter or member 'uidata' not described in 'p9_check_zc_errors'
    net/9p/trans_common.c:23: warning: Function parameter or member 'nr_pages' not described in 'p9_release_pages'
    net/9p/trans_common.c:23: warning: Function parameter or member 'pages' not described in 'p9_release_pages'
    net/9p/trans_fd.c:132: warning: Function parameter or member 'rreq' not described in 'p9_conn'
    net/9p/trans_fd.c:132: warning: Function parameter or member 'wreq' not described in 'p9_conn'
    net/9p/trans_fd.c:56: warning: Function parameter or member 'privport' not described in 'p9_fd_opts'
    net/9p/trans_rdma.c:113: warning: Function parameter or member 'cqe' not described in 'p9_rdma_context'
    net/9p/trans_rdma.c:129: warning: Function parameter or member 'privport' not described in 'p9_rdma_opts'
    net/9p/trans_virtio.c:215: warning: Function parameter or member 'limit' not described in 'pack_sg_list_p'
    net/9p/trans_virtio.c:83: warning: Function parameter or member 'chan_list' not described in 'virtio_chan'
    net/9p/trans_virtio.c:83: warning: Function parameter or member 'p9_max_pages' not described in 'virtio_chan'
    net/9p/trans_virtio.c:83: warning: Function parameter or member 'ring_bufs_avail' not described in 'virtio_chan'
    net/9p/trans_virtio.c:83: warning: Function parameter or member 'tag' not described in 'virtio_chan'
    net/9p/trans_virtio.c:83: warning: Function parameter or member 'vc_wq' not described in 'virtio_chan'
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>
    Acked-by: Dominique Martinet <asmadeus@codewreck.org>
    Link: https://lore.kernel.org/r/20201031182655.1082065-1-andrew@lunn.ch
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/9p/client.c
+++ net/9p/client.c
@@ -413,8 +413,9 @@ static void p9_tag_cleanup(struct p9_client *c)
 
 /**
  * p9_client_cb - call back from transport to client
- * c: client state
- * req: request received
+ * @c: client state
+ * @req: request received
+ * @status: request status, one of REQ_STATUS_*
  *
  */
 void p9_client_cb(struct p9_client *c, struct p9_req_t *req, int status)
@@ -556,6 +557,7 @@ out_err:
  * p9_check_zc_errors - check 9p packet for error return and process it
  * @c: current client instance
  * @req: request to parse and check for error conditions
+ * @uidata: external buffer containing error
  * @in_hdrlen: Size of response protocol buffer.
  *
  * returns error code if one is discovered, otherwise returns 0
