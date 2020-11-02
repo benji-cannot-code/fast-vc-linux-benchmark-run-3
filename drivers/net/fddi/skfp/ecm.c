6e144bfcfec0e181d3ff3d40df66d01acdac2909
Lee Jones <lee.jones@linaro.org>
2020-11-02 11:44:44 +0000

    net: fddi: skfp: ecm: Remove seemingly unused variable 'ID_sccs'
    This variable is present in many source files and has not been used
    anywhere (at least internally) since it was introduced.
    
    Fixes the following W=1 kernel build warning(s):
    
     drivers/net/fddi/skfp/ecm.c: In function ‘ecm_fsm’:
     drivers/net/fddi/skfp/ecm.c:44:19: warning: ‘ID_sccs’ defined but not used [-Wunused-const-variable=]
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Link: https://lore.kernel.org/r/20201102114512.1062724-3-lee.jones@linaro.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/fddi/skfp/ecm.c
+++ drivers/net/fddi/skfp/ecm.c
@@ -41,10 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KERNEL
 #include "h/smtstate.h"
 
-#ifndef	lint
-static const char ID_sccs[] = "@(#)ecm.c	2.7 99/08/05 (C) SK " ;
-#endif
-
 /*
  * FSM Macros
  */
