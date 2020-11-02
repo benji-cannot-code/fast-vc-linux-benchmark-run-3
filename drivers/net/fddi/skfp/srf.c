1dd8413a7c8671531a71c6152bce04c7a150db1d
Lee Jones <lee.jones@linaro.org>
2020-11-02 11:44:58 +0000

    net: fddi: skfp: srf: Remove defined but unused variable 'ID_sccs'
    Fixes the following W=1 kernel build warning(s):
    
     drivers/net/fddi/skfp/srf.c:30:19: warning: ‘ID_sccs’ defined but not used [-Wunused-const-variable=]
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Link: https://lore.kernel.org/r/20201102114512.1062724-17-lee.jones@linaro.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/fddi/skfp/srf.c
+++ drivers/net/fddi/skfp/srf.c
@@ -27,11 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef	SLIM_SMT
 #ifndef	BOOT
 
-#ifndef	lint
-static const char ID_sccs[] = "@(#)srf.c	1.18 97/08/04 (C) SK " ;
-#endif
-
-
 /*
  * function declarations
  */
