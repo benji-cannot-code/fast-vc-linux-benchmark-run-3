6b3f4a54c6fae5c7148e303f88f861119d38d9bf
Lee Jones <lee.jones@linaro.org>
2020-11-02 11:44:52 +0000

    net: fddi: skfp: smtdef: Remove defined but unused variable 'ID_sccs'
    Fixes the following W=1 kernel build warning(s):
    
     drivers/net/fddi/skfp/smtdef.c:26:19: warning: ‘ID_sccs’ defined but not used [-Wunused-const-variable=]
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Link: https://lore.kernel.org/r/20201102114512.1062724-11-lee.jones@linaro.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/fddi/skfp/smtdef.c
+++ drivers/net/fddi/skfp/smtdef.c
@@ -23,10 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OEM_USER_DATA	"SK-NET FDDI V2.0 Userdata"
 #endif
 
-#ifndef	lint
-static const char ID_sccs[] = "@(#)smtdef.c	2.53 99/08/11 (C) SK " ;
-#endif
-
 /*
  * defaults
  */
