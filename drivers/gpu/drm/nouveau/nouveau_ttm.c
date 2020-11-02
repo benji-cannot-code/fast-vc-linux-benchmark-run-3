d136f237ea79607dac7f007b3cff88dae996bd20
Maxime Ripard <maxime@cerno.tech>
2020-11-02 11:50:50 +0100

    drm/nouveau/ttm: Add limits.h
    It seems that a recent commit broke the nouveau compilation when swiotlb is
    disabled (which is the case on our ARM defconfig for example).
    
    Daniel says
    
    """
    Since the proper fix is maybe stuck in the usual "drm abuses swiotlb
    internals" bikeshed, maybe best if we push a fix to including limits.h
    in nouveau and call it done?
    """
    
    So let's go down the simplest path to fix our build, and goes back to it
    later if needed.
    
    Link: https://patchwork.freedesktop.org/patch/397835/
    Fixes: 4dbafbd30aef ("drm/nouveu: fix swiotlb include")
    Acked-by: Daniel Vetter <daniel@ffwll.ch>
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/nouveau/nouveau_ttm.c
+++ drivers/gpu/drm/nouveau/nouveau_ttm.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include <linux/limits.h>
 #include <linux/swiotlb.h>
 
 #include "nouveau_drv.h"
