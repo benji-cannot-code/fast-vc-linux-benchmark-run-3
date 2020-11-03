f25e66d9e8f5aa41703215858ea2fb39992e1f94
Khalil Blaiech <kblaiech@nvidia.com>
2020-11-03 14:54:39 -0500

    i2c: mlxbf: Fix resrticted cast warning of sparse
    Address warnings "warning: cast to restricted __be32" reported
    by sparse.
    
    Fixes: b5b5b32081cd206b ("i2c: mlxbf: I2C SMBus driver for Mellanox BlueField SoC")
    Reported-by: kernel test robot <lkp@intel.com>
    Reviewed-by: Leon Romanovsky <leonro@nvidia.com>
    Reviewed-by: Vadim Pasternak <vadimp@nvidia.com>
    Signed-off-by: Khalil Blaiech <kblaiech@nvidia.com>
    Signed-off-by: Wolfram Sang <wsa@kernel.org>

--- drivers/i2c/busses/i2c-mlxbf.c
+++ drivers/i2c/busses/i2c-mlxbf.c
@@ -511,7 +511,7 @@ static u32 mlxbf_i2c_read(void __iomem *io, int reg)
  */
 static u32 mlxbf_i2c_read_data(void __iomem *io, int reg)
 {
-	return (u32)be32_to_cpu(mlxbf_i2c_read(io, reg));
+	return ioread32be(io + reg);
 }
 
 /*
@@ -525,7 +525,7 @@ static u32 mlxbf_i2c_read_data(void __iomem *io, int reg)
  */
 static void mlxbf_i2c_write_data(void __iomem *io, int reg, u32 val)
 {
-	mlxbf_i2c_write(io, reg, (u32)cpu_to_be32(val));
+	iowrite32be(val, io + reg);
 }
 
 /*
