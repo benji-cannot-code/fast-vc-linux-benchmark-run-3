73637ea9937db480b4a9a6d6c06a080367d415a0
Lars Poeschel <poeschel@lemonage.de>
2020-11-03 10:58:23 +0100

    auxdisplay: hd44780: Remove clear_fast
    We remove the hd44780_clear_fast (display) clear implementation. With
    the new timeout the normal clear_display is reasonably fast. So there is
    no need for a clear_fast anymore.
    
    Link: https://lore.kernel.org/lkml/20200922092121.GG16421@1wt.eu/
    
    Signed-off-by: Lars Poeschel <poeschel@lemonage.de>
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- drivers/auxdisplay/charlcd.c
+++ drivers/auxdisplay/charlcd.c
@@ -126,20 +126,11 @@ static void charlcd_print(struct charlcd *lcd, char c)
 		lcd->ops->gotoxy(lcd);
 }
 
-static void charlcd_clear_fast(struct charlcd *lcd)
+static void charlcd_clear_display(struct charlcd *lcd)
 {
-	struct hd44780_common *hdc = lcd->drvdata;
-	int pos;
-
-	charlcd_home(lcd);
-
-	if (lcd->ops->clear_fast)
-		lcd->ops->clear_fast(lcd);
-	else
-		for (pos = 0; pos < min(2, lcd->height) * hdc->hwidth; pos++)
-			lcd->ops->print(lcd, ' ');
-
-	charlcd_home(lcd);
+	lcd->ops->clear_display(lcd);
+	lcd->addr.x = 0;
+	lcd->addr.y = 0;
 }
 
 /*
@@ -410,7 +401,7 @@ static void charlcd_write_char(struct charlcd *lcd, char c)
 			break;
 		case '\f':
 			/* quickly clear the display */
-			charlcd_clear_fast(lcd);
+			charlcd_clear_display(lcd);
 			break;
 		case '\n':
 			/*
@@ -449,7 +440,7 @@ static void charlcd_write_char(struct charlcd *lcd, char c)
 
 		if (!strcmp(priv->esc_seq.buf, "[2J")) {
 			/* clear the display */
-			charlcd_clear_fast(lcd);
+			charlcd_clear_display(lcd);
 			processed = 1;
 		} else if (!strcmp(priv->esc_seq.buf, "[H")) {
 			/* cursor to home */
