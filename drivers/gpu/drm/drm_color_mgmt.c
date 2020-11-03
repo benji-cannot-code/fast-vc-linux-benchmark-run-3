bb299f422178ddb0a732848db0b26400c4d12223
Simon Ser <contact@emersion.fr>
2020-11-03 10:31:15 +0000

    drm: unify formatting for color management documentation
    Other properties are documented with a colon character after the
    property name. Consistently using a colon character allows the docs to
    be machine-readable.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/7dhjUKqlDC0IiRRON0VhWnkuoxcRDLG4EFKzyhcEA@cp7-web-043.plabs.ch

--- drivers/gpu/drm/drm_color_mgmt.c
+++ drivers/gpu/drm/drm_color_mgmt.c
@@ -98,12 +98,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * &drm_plane specific COLOR_ENCODING and COLOR_RANGE properties. They
  * are set up by calling drm_plane_create_color_properties().
  *
- * "COLOR_ENCODING"
+ * "COLOR_ENCODING":
  * 	Optional plane enum property to support different non RGB
  * 	color encodings. The driver can provide a subset of standard
  * 	enum values supported by the DRM plane.
  *
- * "COLOR_RANGE"
+ * "COLOR_RANGE":
  * 	Optional plane enum property to support different non RGB
  * 	color parameter ranges. The driver can provide a subset of
  * 	standard enum values supported by the DRM plane.
