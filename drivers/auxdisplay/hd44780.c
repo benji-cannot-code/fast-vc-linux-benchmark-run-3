9da38ce253f3ee6d433d074312c33782a0d768ad
Lars Poeschel <poeschel@lemonage.de>
2020-11-03 10:58:13 +0100

    auxdisplay: add home to charlcd_ops
    This adds a home function to the charlcd_ops struct and offer an
    implementation for hd44780_common. This implementation is used by our
    two hd44780 drivers. This is to make charlcd device independent.
    
    Reviewed-by: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Lars Poeschel <poeschel@lemonage.de>
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- drivers/auxdisplay/hd44780.c
+++ drivers/auxdisplay/hd44780.c
@@ -129,6 +129,7 @@ static const struct charlcd_ops hd44780_ops_gpio8 = {
 	.backlight	= hd44780_backlight,
 	.print		= hd44780_common_print,
 	.gotoxy		= hd44780_common_gotoxy,
+	.home		= hd44780_common_home,
 };
 
 /* Send a command to the LCD panel in 4 bit GPIO mode */
@@ -174,6 +175,7 @@ static const struct charlcd_ops hd44780_ops_gpio4 = {
 	.backlight	= hd44780_backlight,
 	.print		= hd44780_common_print,
 	.gotoxy		= hd44780_common_gotoxy,
+	.home		= hd44780_common_home,
 };
 
 static int hd44780_probe(struct platform_device *pdev)
