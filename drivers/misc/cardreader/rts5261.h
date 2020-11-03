9bf451a942da043272db60567c28a77b48d41eb2
Rui Feng <rui_feng@realsil.com.cn>
2020-11-03 17:54:45 +0800

    misc: rtsx: Fix aspm for RTS5261
    This patch fix the bug that LDO is off when aspm is enabled.
    
    Signed-off-by: Rui Feng <rui_feng@realsil.com.cn>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://lore.kernel.org/r/1604397285-2850-1-git-send-email-rui_feng@realsil.com.cn
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/misc/cardreader/rts5261.h
+++ drivers/misc/cardreader/rts5261.h
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*New add*/
 #define rts5261_vendor_setting_valid(reg)	((reg) & 0x010000)
-#define rts5261_reg_to_aspm(reg)		(((reg) >> 28) ^ 0x03)
+#define rts5261_reg_to_aspm(reg) \
+	(((~(reg) >> 28) & 0x02) | (((reg) >> 28) & 0x01))
 #define rts5261_reg_check_reverse_socket(reg)	((reg) & 0x04)
 #define rts5261_reg_to_card_drive_sel(reg)	((((reg) >> 6) & 0x01) << 6)
 #define rts5261_reg_to_sd30_drive_sel_1v8(reg)	(((reg) >> 22) ^ 0x03)
