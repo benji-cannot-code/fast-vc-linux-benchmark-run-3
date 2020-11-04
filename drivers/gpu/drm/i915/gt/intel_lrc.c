7c84abe05cb84fed7f782c1732ca18529db23d73
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2020-11-04 13:47:42 +0000

    drm/i915: Improve record of hung engines in error state
    Between events which trigger engine and GPU resets and capturing the error
    state we lose information on which engine triggered the reset. Improve
    this by passing in the hung engine mask down to error capture.
    
    Result is that the list of engines in user visible "GPU HANG: ecode
    <gen>:<engines>:<ecode>, <process>" is now a list of hanging and not just
    active engines. Most importantly the displayed process is now the one
    which was actually hung.
    
    v2:
     * Stub prototype. (Chris)
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201104134743.916027-1-tvrtko.ursulin@linux.intel.com

--- drivers/gpu/drm/i915/gt/intel_lrc.c
+++ drivers/gpu/drm/i915/gt/intel_lrc.c
@@ -3038,6 +3038,8 @@ static struct execlists_capture *capture_regs(struct intel_engine_cs *engine)
 	if (!cap->error->gt->engine)
 		goto err_gt;
 
+	cap->error->gt->engine->hung = true;
+
 	return cap;
 
 err_gt:
