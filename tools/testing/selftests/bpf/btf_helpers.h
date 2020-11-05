aa41f44269fb6669fbb05f4b8768150bf72f5bcf
Andrii Nakryiko <andrii@kernel.org>
2020-11-04 20:34:00 -0800

    selftests/bpf: Add split BTF dedup selftests
    Add selftests validating BTF deduplication for split BTF case. Add a helper
    macro that allows to validate entire BTF with raw BTF dump, not just
    type-by-type. This saves tons of code and complexity.
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Song Liu <songliubraving@fb.com>
    Link: https://lore.kernel.org/bpf/20201105043402.2530976-11-andrii@kernel.org

--- tools/testing/selftests/bpf/btf_helpers.h
+++ tools/testing/selftests/bpf/btf_helpers.h
@@ -9,5 +9,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int fprintf_btf_type_raw(FILE *out, const struct btf *btf, __u32 id);
 const char *btf_type_raw_dump(const struct btf *btf, int type_id);
+int btf_validate_raw(struct btf *btf, int nr_types, const char *exp_types[]);
 
+#define VALIDATE_RAW_BTF(btf, raw_types...)				\
+	btf_validate_raw(btf,						\
+			 sizeof((const char *[]){raw_types})/sizeof(void *),\
+			 (const char *[]){raw_types})
+
+const char *btf_type_c_dump(const struct btf *btf);
 #endif
