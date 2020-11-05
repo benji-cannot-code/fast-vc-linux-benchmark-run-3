a3b00f1a4309fa57d10769cad29fa969cc198552
Maxime Ripard <maxime@cerno.tech>
2020-11-05 14:56:52 +0100

    drm/vc4: kms: Rename NUM_CHANNELS
    The NUM_CHANNELS define has a pretty generic name and was right before the
    function using it. Let's move to something that makes the hardware-specific
    nature more obvious, and to a more appropriate place.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>
    Tested-by: Hoegeun Kwon <hoegeun.kwon@samsung.com>
    Reviewed-by: Hoegeun Kwon <hoegeun.kwon@samsung.com>
    Reviewed-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201105135656.383350-4-maxime@cerno.tech

--- drivers/gpu/drm/vc4/vc4_kms.c
+++ drivers/gpu/drm/vc4/vc4_kms.c
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "vc4_drv.h"
 #include "vc4_regs.h"
 
+#define HVS_NUM_CHANNELS 3
+
 struct vc4_ctm_state {
 	struct drm_private_state base;
 	struct drm_color_ctm *ctm;
@@ -661,12 +663,10 @@ static int vc4_load_tracker_obj_init(struct vc4_dev *vc4)
 	return drmm_add_action_or_reset(&vc4->base, vc4_load_tracker_obj_fini, NULL);
 }
 
-#define NUM_CHANNELS 3
-
 static int
 vc4_atomic_check(struct drm_device *dev, struct drm_atomic_state *state)
 {
-	unsigned long unassigned_channels = GENMASK(NUM_CHANNELS - 1, 0);
+	unsigned long unassigned_channels = GENMASK(HVS_NUM_CHANNELS - 1, 0);
 	struct drm_crtc_state *old_crtc_state, *new_crtc_state;
 	struct drm_crtc *crtc;
 	int i, ret;
