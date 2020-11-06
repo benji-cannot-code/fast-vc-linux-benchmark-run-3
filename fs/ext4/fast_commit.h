588928bcb333b23e6fcdb8ca872a209d7b894d5b
Harshad Shirwadkar <harshadshirwadkar@gmail.com>
2020-11-05 19:58:55 -0800

    ext4: clean up the JBD2 API that initializes fast commits
    This patch removes jbd2_fc_init() API and its related functions to
    simplify enabling fast commits. With this change, the number of fast
    commit blocks to use is solely determined by the JBD2 layer. So, we
    move the default value for minimum number of fast commit blocks from
    ext4/fast_commit.h to include/linux/jbd2.h. However, whether or not to
    use fast commits is determined by the file system. The file system
    just sets the fast commit feature using
    jbd2_journal_set_features(). JBD2 layer then determines how many
    blocks to use for fast commits (based on the value found in the JBD2
    superblock).
    
    Note that the JBD2 feature flag of fast commits is just an indication
    that there are fast commit blocks present on disk. It doesn't tell
    JBD2 layer about the intent of the file system of whether to it wants
    to use fast commit or not. That's why, we blindly clear the fast
    commit flag in journal_reset() after the recovery is done.
    
    Suggested-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Harshad Shirwadkar <harshadshirwadkar@gmail.com>
    Link: https://lore.kernel.org/r/20201106035911.1942128-7-harshadshirwadkar@gmail.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/fast_commit.h
+++ fs/ext4/fast_commit.h
@@ -4,9 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __FAST_COMMIT_H__
 #define __FAST_COMMIT_H__
 
-/* Number of blocks in journal area to allocate for fast commits */
-#define EXT4_NUM_FC_BLKS		256
-
 /* Fast commit tags */
 #define EXT4_FC_TAG_ADD_RANGE		0x0001
 #define EXT4_FC_TAG_DEL_RANGE		0x0002
