16c83f8f63f4f98b90aab74cae33e557ed24b082
Steven Rostedt (VMware) <rostedt@goodmis.org>
2020-11-05 21:32:39 -0500

    pstore/ftrace: Add recursion protection to the ftrace callback
    If a ftrace callback does not supply its own recursion protection and
    does not set the RECURSION_SAFE flag in its ftrace_ops, then ftrace will
    make a helper trampoline to do so before calling the callback instead of
    just calling the callback directly.
    
    The default for ftrace_ops is going to change. It will expect that handlers
    provide their own recursion protection, unless its ftrace_ops states
    otherwise.
    
    Link: https://lkml.kernel.org/r/20201028115612.990886844@goodmis.org
    Link: https://lkml.kernel.org/r/20201106023546.720372267@goodmis.org
    
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Josh  Poimboeuf <jpoimboe@redhat.com>
    Cc: Jiri Kosina <jikos@kernel.org>
    Cc: Miroslav Benes <mbenes@suse.cz>
    Cc: Petr Mladek <pmladek@suse.com>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Thomas Meyer <thomas@m3y3r.de>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- fs/pstore/ftrace.c
+++ fs/pstore/ftrace.c
@@ -29,6 +29,7 @@ static void notrace pstore_ftrace_call(unsigned long ip,
 				       struct ftrace_ops *op,
 				       struct pt_regs *regs)
 {
+	int bit;
 	unsigned long flags;
 	struct pstore_ftrace_record rec = {};
 	struct pstore_record record = {
@@ -41,6 +42,10 @@ static void notrace pstore_ftrace_call(unsigned long ip,
 	if (unlikely(oops_in_progress))
 		return;
 
+	bit = ftrace_test_recursion_trylock();
+	if (bit < 0)
+		return;
+
 	local_irq_save(flags);
 
 	rec.ip = ip;
@@ -50,6 +55,7 @@ static void notrace pstore_ftrace_call(unsigned long ip,
 	psinfo->write(&record);
 
 	local_irq_restore(flags);
+	ftrace_test_recursion_unlock(bit);
 }
 
 static struct ftrace_ops pstore_ftrace_ops __read_mostly = {
