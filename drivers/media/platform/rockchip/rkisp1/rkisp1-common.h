2dae0a51eb3bd849a694b1e28441ca3e26840e34
Helen Koike <helen.koike@collabora.com>
2020-11-06 13:19:37 +0100

    media: rockchip: rkisp1: destage Rockchip ISP1 driver
    All the items in the TODO list were addressed, uapi was reviewed,
    documentation written, checkpatch errors fixed, several bugs fixed.
    
    There is no big reason to keep this driver in staging, so move it out.
    
    Dt-bindings Verified with:
    make ARCH=arm64 dt_binding_check DT_SCHEMA_FILES=Documentation/devicetree/bindings/media/rockchip-isp1.yaml
    
    Fields of MAINTAINERS file sorted according to output of
    ./scripts/parse-maintainers.pl --input=MAINTAINERS --output=MAINTAINERS
    --order
    
    [dt-bindings: media: rkisp1: move rockchip-isp1 bindings out of staging]
    [dt-bindings: media: rkisp1: move rockchip-isp1 bindings out of staging]
    [hverkuil: fix various checkpatch alignment warnings]
    
    Signed-off-by: Helen Koike <helen.koike@collabora.com>
    Acked-by: Rob Herring <robh@kernel.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/platform/rockchip/rkisp1/rkisp1-common.h
+++ drivers/media/platform/rockchip/rkisp1/rkisp1-common.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/clk.h>
 #include <linux/mutex.h>
+#include <linux/rkisp1-config.h>
 #include <media/media-device.h>
 #include <media/media-entity.h>
 #include <media/v4l2-ctrls.h>
@@ -21,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/videobuf2-v4l2.h>
 
 #include "rkisp1-regs.h"
-#include "uapi/rkisp1-config.h"
 
 /*
  * flags on the 'direction' field in struct 'rkisp1_isp_mbus_info' that indicate
