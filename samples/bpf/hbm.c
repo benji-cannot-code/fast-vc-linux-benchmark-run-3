a0a91d78c35e4231ca9a4032f44995fbc0c7e00f
Menglong Dong <dong.menglong@zte.com.cn>
2020-11-06 04:13:54 -0500

    samples/bpf: Remove duplicate include in hbm
    The 'bpf/bpf.h' include in 'samples/bpf/hbm.c' is duplicated.
    
    Signed-off-by: Menglong Dong <dong.menglong@zte.com.cn>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Link: https://lore.kernel.org/bpf/1604654034-52821-1-git-send-email-dong.menglong@zte.com.cn

--- samples/bpf/hbm.c
+++ samples/bpf/hbm.c
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cgroup_helpers.h"
 #include "hbm.h"
 #include "bpf_util.h"
-#include <bpf/bpf.h>
 #include <bpf/libbpf.h>
 
 bool outFlag = true;
