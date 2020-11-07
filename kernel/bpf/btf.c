3ff066a98d39423082be7bf99cb4a937bcb0ceb7
Wang Qing <wangqing@vivo.com>
2020-11-07 16:10:50 +0800

    bpf, btf: Remove the duplicate btf_ids.h include
    Remove duplicate btf_ids.h header which is included twice.
    
    Signed-off-by: Wang Qing <wangqing@vivo.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Link: https://lore.kernel.org/bpf/1604736650-11197-1-git-send-email-wangqing@vivo.com

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/skmsg.h>
 #include <linux/perf_event.h>
 #include <linux/bsearch.h>
-#include <linux/btf_ids.h>
 #include <net/sock.h>
 
 /* BTF (BPF Type Format) is the meta data format which describes
