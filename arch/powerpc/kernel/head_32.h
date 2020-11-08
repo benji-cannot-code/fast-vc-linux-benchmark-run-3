13df19ac3561fd98ab836eed008e230d74627f45
Christophe Leroy <christophe.leroy@csgroup.eu>
2020-11-08 16:57:37 +0000

    powerpc: Remove RFI macro
    RFI macro is just there to add an infinite loop past
    rfi in order to avoid prefetch on 40x in half a dozen
    of places in entry_32 and head_32.
    
    Those places are already full of #ifdefs, so just add a
    few more to explicitely show those loops and remove RFI.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/f7e9cb9e9240feec63cb330abf40b67d1aad852f.1604854583.git.christophe.leroy@csgroup.eu

--- arch/powerpc/kernel/head_32.h
+++ arch/powerpc/kernel/head_32.h
@@ -223,7 +223,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 	mtspr	SPRN_SRR1,r10
 	mtspr	SPRN_SRR0,r11
-	RFI				/* jump to handler, enable MMU */
+	rfi				/* jump to handler, enable MMU */
+#ifdef CONFIG_40x
+	b .	/* Prevent prefetch past rfi */
+#endif
 99:	b	ret_from_kernel_syscall
 .endm
 
