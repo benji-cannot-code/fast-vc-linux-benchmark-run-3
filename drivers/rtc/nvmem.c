5ffe78d03fa7f2612552f44ce290e48499a343c2
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2020-11-09 17:34:07 +0100

    rtc: nvmem: emit an error message when nvmem registration fails
    Some users check the return value of devm_rtc_nvmem_register() only in
    order to emit an error message and then continue probing. This is fine
    as an rtc can function without exposing nvmem but let's generalize it:
    let's make the registration function emit the error message so that
    users don't have to.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Link: https://lore.kernel.org/r/20201109163409.24301-7-brgl@bgdev.pl

--- drivers/rtc/nvmem.c
+++ drivers/rtc/nvmem.c
@@ -14,14 +14,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int devm_rtc_nvmem_register(struct rtc_device *rtc,
 		       struct nvmem_config *nvmem_config)
 {
+	struct device *dev = rtc->dev.parent;
 	struct nvmem_device *nvmem;
 
 	if (!nvmem_config)
 		return -ENODEV;
 
-	nvmem_config->dev = rtc->dev.parent;
+	nvmem_config->dev = dev;
 	nvmem_config->owner = rtc->owner;
-	nvmem = devm_nvmem_register(rtc->dev.parent, nvmem_config);
+	nvmem = devm_nvmem_register(dev, nvmem_config);
+	if (IS_ERR(nvmem))
+		dev_err(dev, "failed to register nvmem device for RTC\n");
 
 	return PTR_ERR_OR_ZERO(nvmem);
 }
