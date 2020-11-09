a3d64df0b2a1eabc84fe45ece00205b35bb0591c
Johannes Berg <johannes.berg@intel.com>
2020-11-09 10:57:47 +0100

    cfg80211: remove WDS code
    Remove all the code that was there to configure WDS interfaces,
    now that there's no way to reach it anymore.
    
    Link: https://lore.kernel.org/r/20201109105103.8f5b98e4068d.I5f5129041649ef2862b69683574bb3344743727b@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -758,7 +758,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	of any other interfaces, and other interfaces will again take
  *	precedence when they are used.
  *
- * @NL80211_CMD_SET_WDS_PEER: Set the MAC address of the peer on a WDS interface.
+ * @NL80211_CMD_SET_WDS_PEER: Set the MAC address of the peer on a WDS interface
+ *	(no longer supported).
  *
  * @NL80211_CMD_SET_MULTICAST_TO_UNICAST: Configure if this AP should perform
  *	multicast to unicast conversion. When enabled, all multicast packets
