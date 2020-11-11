30b8e01dffe5522659e65b41fabb81c75eaaea06
Nick Desaulniers <ndesaulniers@google.com>
2020-11-10 17:47:14 -0800

    usb: fix a few cases of -Wfallthrough
    The "fallthrough" pseudo-keyword was added as a portable way to denote
    intentional fallthrough. Clang will still warn on cases where there is a
    fallthrough to an immediate break. Add explicit breaks for those cases.
    
    Reviewed-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Link: https://lore.kernel.org/r/20201111014716.260633-1-ndesaulniers@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/core/config.c
+++ drivers/usb/core/config.c
@@ -1077,6 +1077,7 @@ int usb_get_bos_descriptor(struct usb_device *dev)
 		case USB_PTM_CAP_TYPE:
 			dev->bos->ptm_cap =
 				(struct usb_ptm_cap_descriptor *)buffer;
+			break;
 		default:
 			break;
 		}
