ed87ced87030106ac7846a841880f88e8d804a64
Tian Tao <tiantao6@hisilicon.com>
2020-11-11 15:57:56 +0800

    drm/msm/dp: remove duplicate include statement
    linux/rational.h is included more than once, Remove the one that isn't
    necessary.
    
    Signed-off-by: Tian Tao <tiantao6@hisilicon.com>
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/dp/dp_catalog.c
+++ drivers/gpu/drm/msm/dp/dp_catalog.c
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt)	"[drm-dp] %s: " fmt, __func__
 
-#include <linux/rational.h>
 #include <linux/delay.h>
 #include <linux/iopoll.h>
 #include <linux/phy/phy.h>
