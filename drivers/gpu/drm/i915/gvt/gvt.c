9495b6ff8c0e5e92a68f992e6f6cbb26a20ae7a9
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2020-11-11 18:28:11 +0100

    drm/i915/gvt: treat intel_gvt_mpt as const in gvt code
    The current interface of intel_gvt_register_hypervisor() expects a
    non-const pointer to struct intel_gvt_mpt, even though the mediator
    never modifies (or should modifiy) the content of this struct.
    
    Change the function signature and relevant struct members to const to
    properly express the API's intent and allow instances of intel_gvt_mpt
    to be allocated as const.
    
    While I was here, I also made KVM's instance of this struct const to
    reduce the number of writable function pointers in the kernel.
    
    Cc: Zhenyu Wang <zhenyuw@linux.intel.com>
    Cc: intel-gvt-dev@lists.freedesktop.org
    Reviewed-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Julian Stecklina <julian.stecklina@cyberus-technology.de>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20201111172811.558443-1-julian.stecklina@cyberus-technology.de

--- drivers/gpu/drm/i915/gvt/gvt.c
+++ drivers/gpu/drm/i915/gvt/gvt.c
@@ -416,7 +416,7 @@ intel_gvt_pm_resume(struct intel_gvt *gvt)
 }
 
 int
-intel_gvt_register_hypervisor(struct intel_gvt_mpt *m)
+intel_gvt_register_hypervisor(const struct intel_gvt_mpt *m)
 {
 	int ret;
 	void *gvt;
