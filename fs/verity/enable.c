c99a3a05023be72caa479efaeb5b13c336301109
Eric Biggers <ebiggers@google.com>
2020-11-13 13:19:15 -0800

    fs-verity: remove filenames from file comments
    Embedding the file path inside kernel source code files isn't
    particularly useful as often files are moved around and the paths become
    incorrect.  checkpatch.pl warns about this since v5.10-rc1.
    
    Acked-by: Luca Boccassi <luca.boccassi@microsoft.com>
    Link: https://lore.kernel.org/r/20201113211918.71883-2-ebiggers@kernel.org
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- fs/verity/enable.c
+++ fs/verity/enable.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
 /*
- * fs/verity/enable.c: ioctl to enable verity on a file
+ * Ioctl to enable verity on a file
  *
  * Copyright 2019 Google LLC
  */
