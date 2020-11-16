fd7c1ab3953810eddec911a0d70e94df5f1d2e31
Lee Jones <lee.jones@linaro.org>
2020-11-16 17:36:40 +0000

    drm/radeon/evergreen_dma: Move 'evergreen_gpu_check_soft_reset()'s prototype to shared header
    Fixes the following W=1 kernel build warning(s):
    
     drivers/gpu/drm/radeon/evergreen.c:3825:5: warning: no previous prototype for ‘evergreen_gpu_check_soft_reset’ [-Wmissing-prototypes]
     3825 | u32 evergreen_gpu_check_soft_reset(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: amd-gfx@lists.freedesktop.org
    Cc: dri-devel@lists.freedesktop.org
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/evergreen_dma.c
+++ drivers/gpu/drm/radeon/evergreen_dma.c
@@ -25,9 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "radeon.h"
 #include "radeon_asic.h"
+#include "evergreen.h"
 #include "evergreend.h"
-
-u32 evergreen_gpu_check_soft_reset(struct radeon_device *rdev);
 
 /**
  * evergreen_dma_fence_ring_emit - emit a fence on the DMA ring
