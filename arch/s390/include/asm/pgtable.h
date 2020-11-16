a2f80733214911bff159a4e45c789eb789289844
Heiko Carstens <hca@linux.ibm.com>
2020-11-16 08:06:41 +0100

    s390/mm: use invalid asce instead of kernel asce
    Create a region 3 page table which contains only invalid entries, and
    use that via "s390_invalid_asce" instead of the kernel ASCE whenever
    there is either
    - no user address space available, e.g. during early startup
    - as an intermediate ASCE when address spaces are switched
    
    This makes sure that user space accesses in such situations are
    guaranteed to fail.
    
    Reviewed-by: Sven Schnelle <svens@linux.ibm.com>
    Reviewed-by: Alexander Gordeev <agordeev@linux.ibm.com>
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- arch/s390/include/asm/pgtable.h
+++ arch/s390/include/asm/pgtable.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern pgd_t swapper_pg_dir[];
 extern void paging_init(void);
+extern unsigned long s390_invalid_asce;
 
 enum {
 	PG_DIRECT_MAP_4K = 0,
