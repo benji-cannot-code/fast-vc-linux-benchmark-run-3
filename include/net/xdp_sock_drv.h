7cab2018ae8518a7e09292a68c5360b04361a488
Magnus Karlsson <magnus.karlsson@intel.com>
2020-11-16 12:12:46 +0100

    xsk: Introduce batched Tx descriptor interfaces
    Introduce batched descriptor interfaces in the xsk core code for the
    Tx path to be used in the driver to write a code path with higher
    performance. This interface will be used by the i40e driver in the
    next patch. Though other drivers would likely benefit from this new
    interface too.
    
    Note that batching is only implemented for the common case when
    there is only one socket bound to the same device and queue id. When
    this is not the case, we fall back to the old non-batched version of
    the function.
    
    Signed-off-by: Magnus Karlsson <magnus.karlsson@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Link: https://lore.kernel.org/bpf/1605525167-14450-5-git-send-email-magnus.karlsson@gmail.com

--- include/net/xdp_sock_drv.h
+++ include/net/xdp_sock_drv.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 void xsk_tx_completed(struct xsk_buff_pool *pool, u32 nb_entries);
 bool xsk_tx_peek_desc(struct xsk_buff_pool *pool, struct xdp_desc *desc);
+u32 xsk_tx_peek_release_desc_batch(struct xsk_buff_pool *pool, struct xdp_desc *desc, u32 max);
 void xsk_tx_release(struct xsk_buff_pool *pool);
 struct xsk_buff_pool *xsk_get_pool_from_qid(struct net_device *dev,
 					    u16 queue_id);
@@ -127,6 +128,12 @@ static inline bool xsk_tx_peek_desc(struct xsk_buff_pool *pool,
 				    struct xdp_desc *desc)
 {
 	return false;
+}
+
+static inline u32 xsk_tx_peek_release_desc_batch(struct xsk_buff_pool *pool, struct xdp_desc *desc,
+						 u32 max)
+{
+	return 0;
 }
 
 static inline void xsk_tx_release(struct xsk_buff_pool *pool)
