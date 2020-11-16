2d0aeae8760d37ae98112b4030a1f342003cf24c
Lee Jones <lee.jones@linaro.org>
2020-11-16 17:36:45 +0000

    drm/radeon/ci_dpm: Move 'ci_*()'s prototypes to shared header
    Fixes the following W=1 kernel build warning(s):
    
     drivers/gpu/drm/radeon/cik.c:1868:5: warning: no previous prototype for ‘ci_mc_load_microcode’ [-Wmissing-prototypes]
     1868 | int ci_mc_load_microcode(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/cik.c:5847:6: warning: no previous prototype for ‘cik_enter_rlc_safe_mode’ [-Wmissing-prototypes]
     5847 | void cik_enter_rlc_safe_mode(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/cik.c:5868:6: warning: no previous prototype for ‘cik_exit_rlc_safe_mode’ [-Wmissing-prototypes]
     5868 | void cik_exit_rlc_safe_mode(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~~~~~~~~~~
     drivers/gpu/drm/radeon/cik.c:6286:6: warning: no previous prototype for ‘cik_update_cg’ [-Wmissing-prototypes]
     6286 | void cik_update_cg(struct radeon_device *rdev,
     | ^~~~~~~~~~~~~
    
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: amd-gfx@lists.freedesktop.org
    Cc: dri-devel@lists.freedesktop.org
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/cik.c
+++ drivers/gpu/drm/radeon/cik.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "atom.h"
 #include "evergreen.h"
 #include "cik_blit_shaders.h"
+#include "cik.h"
 #include "cikd.h"
 #include "clearstate_ci.h"
 #include "r600.h"
