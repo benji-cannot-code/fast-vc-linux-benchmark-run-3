9efe4bdd870b0748c238538e08467eff030a4f2b
Florian Westphal <fw@strlen.de>
2020-11-16 10:48:12 +0100

    mptcp: rework poll+nospace handling
    MPTCP maintains a status bit, MPTCP_SEND_SPACE, that is set when at
    least one subflow and the mptcp socket itself are writeable.
    
    mptcp_poll returns EPOLLOUT if the bit is set.
    
    mptcp_sendmsg makes sure MPTCP_SEND_SPACE gets cleared when last write
    has used up all subflows or the mptcp socket wmem.
    
    This reworks nospace handling as follows:
    
    MPTCP_SEND_SPACE is replaced with MPTCP_NOSPACE, i.e. inverted meaning.
    This bit is set when the mptcp socket is not writeable.
    The mptcp-level ack path schedule will then schedule the mptcp worker
    to allow it to free already-acked data (and reduce wmem usage).
    
    This will then wake userspace processes that wait for a POLLOUT event.
    
    sendmsg will set MPTCP_NOSPACE only when it has to wait for more
    wmem (blocking I/O case).
    
    poll path will set MPTCP_NOSPACE in case the mptcp socket is
    not writeable.
    
    Normal tcp-level notification (SOCK_NOSPACE) is only enabled
    in case the subflow socket has no available wmem.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -87,7 +87,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* MPTCP socket flags */
 #define MPTCP_DATA_READY	0
-#define MPTCP_SEND_SPACE	1
+#define MPTCP_NOSPACE		1
 #define MPTCP_WORK_RTX		2
 #define MPTCP_WORK_EOF		3
 #define MPTCP_FALLBACK_DONE	4
