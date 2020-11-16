53cdb9f20ccc6b14452f87868bcd6b16aecc8082
Lee Jones <lee.jones@linaro.org>
2020-11-16 17:36:47 +0000

    drm/radeon/si_dma: Move 'si_gpu_check_soft_reset()'s prototype to shared header
    Fixes the following W=1 kernel build warning(s):
    
     drivers/gpu/drm/radeon/si.c:3768:5: warning: no previous prototype for ‘si_gpu_check_soft_reset’ [-Wmissing-prototypes]
     3768 | u32 si_gpu_check_soft_reset(struct radeon_device *rdev)
     | ^~~~~~~~~~~~~~~~~~~~~~~
    
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Daniel Vetter <daniel@ffwll.ch>
    Cc: amd-gfx@lists.freedesktop.org
    Cc: dri-devel@lists.freedesktop.org
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/radeon/si.h
+++ drivers/gpu/drm/radeon/si.h
@@ -28,5 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct radeon_device;
 
 int si_mc_load_microcode(struct radeon_device *rdev);
+u32 si_gpu_check_soft_reset(struct radeon_device *rdev);
 
 #endif                         /* __SI_H__ */
