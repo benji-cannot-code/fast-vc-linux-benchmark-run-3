6854bbaeb9a9154508906d088101e440ce90d5d5
Geert Uytterhoeven <geert+renesas@glider.be>
2020-11-17 11:30:16 +0100

    ARM: shmobile: r8a7778: Introduce HPBREG_BASE
    Replace the hardcoded address of the HPB Register block by a macro.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://lore.kernel.org/r/20201117103022.2136527-2-geert+renesas@glider.be

--- arch/arm/mach-shmobile/setup-r8a7778.c
+++ arch/arm/mach-shmobile/setup-r8a7778.c
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "common.h"
 
+#define HPBREG_BASE	0xfe700000
+
 #define INT2SMSKCR0	0x82288 /* 0xfe782288 */
 #define INT2SMSKCR1	0x8228c /* 0xfe78228c */
 
@@ -23,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void __init r8a7778_init_irq_dt(void)
 {
-	void __iomem *base = ioremap(0xfe700000, 0x00100000);
+	void __iomem *base = ioremap(HPBREG_BASE, 0x00100000);
 
 	BUG_ON(!base);
 
