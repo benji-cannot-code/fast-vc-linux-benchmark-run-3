f4651205d11944e040cc6a909a5850c80b3d6f00
Lukas Bulwahn <lukas.bulwahn@gmail.com>
2020-11-17 06:37:03 +0100

    ring-buffer: Remove obsolete rb_event_is_commit()
    Commit a389d86f7fd0 ("ring-buffer: Have nested events still record running
    time stamp") removed the only uses of rb_event_is_commit() in
    rb_update_event() and rb_update_write_stamp().
    
    Hence, since then, make CC=clang W=1 warns:
    
      kernel/trace/ring_buffer.c:2763:1:
        warning: unused function 'rb_event_is_commit' [-Wunused-function]
    
    Remove this obsolete function.
    
    Link: https://lkml.kernel.org/r/20201117053703.11275-1-lukas.bulwahn@gmail.com
    
    Reviewed-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Lukas Bulwahn <lukas.bulwahn@gmail.com>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/ring_buffer.c
+++ kernel/trace/ring_buffer.c
@@ -2630,9 +2630,6 @@ rb_add_time_stamp(struct ring_buffer_event *event, u64 delta, bool abs)
 	return skip_time_extend(event);
 }
 
-static inline bool rb_event_is_commit(struct ring_buffer_per_cpu *cpu_buffer,
-				     struct ring_buffer_event *event);
-
 #ifndef CONFIG_HAVE_UNSTABLE_SCHED_CLOCK
 static inline bool sched_clock_stable(void)
 {
@@ -2758,20 +2755,6 @@ static unsigned rb_calculate_event_length(unsigned length)
 		length += RB_ALIGNMENT;
 
 	return length;
-}
-
-static __always_inline bool
-rb_event_is_commit(struct ring_buffer_per_cpu *cpu_buffer,
-		   struct ring_buffer_event *event)
-{
-	unsigned long addr = (unsigned long)event;
-	unsigned long index;
-
-	index = rb_event_index(event);
-	addr &= PAGE_MASK;
-
-	return cpu_buffer->commit_page->page == (void *)addr &&
-		rb_commit_index(cpu_buffer) == index;
 }
 
 static u64 rb_time_delta(struct ring_buffer_event *event)
