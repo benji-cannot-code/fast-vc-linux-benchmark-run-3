78843d344473e8e05e113f6f9fac9a0fa58577a2
Srujana Challa <schalla@marvell.com>
2020-11-18 17:14:14 +0530

    octeontx2-pf: move lmt flush to include/linux/soc
    On OcteonTX2 platform CPT instruction enqueue and NIX
    packet send are only possible via LMTST operations which
    uses LDEOR instruction. This patch moves lmt flush
    function from OcteonTX2 nic driver to include/linux/soc
    since it will be used by OcteonTX2 CPT and NIC driver for
    LMTST.
    
    Signed-off-by: Suheil Chandran <schandran@marvell.com>
    Signed-off-by: Srujana Challa <schalla@marvell.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/net_tstamp.h>
 #include <linux/ptp_clock_kernel.h>
 #include <linux/timecounter.h>
+#include <linux/soc/marvell/octeontx2/asm.h>
 
 #include <mbox.h>
 #include <npc.h>
@@ -463,21 +464,9 @@ static inline u64 otx2_atomic64_add(u64 incr, u64 *ptr)
 	return result;
 }
 
-static inline u64 otx2_lmt_flush(uint64_t addr)
-{
-	u64 result = 0;
-
-	__asm__ volatile(".cpu  generic+lse\n"
-			 "ldeor xzr,%x[rf],[%[rs]]"
-			 : [rf]"=r"(result)
-			 : [rs]"r"(addr));
-	return result;
-}
-
 #else
 #define otx2_write128(lo, hi, addr)
 #define otx2_atomic64_add(incr, ptr)		({ *ptr += incr; })
-#define otx2_lmt_flush(addr)			({ 0; })
 #endif
 
 /* Alloc pointer from pool/aura */
