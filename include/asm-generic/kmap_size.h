5e58285df81f97cae6d3fc387f8bd5a7e0509e13
Thomas Gleixner <tglx@linutronix.de>
2020-11-18 20:48:39 +0100

    mm/highmem: Provide and use CONFIG_DEBUG_KMAP_LOCAL
    CONFIG_KMAP_LOCAL can be enabled by x86/32bit even if CONFIG_HIGHMEM is not
    enabled for temporary MMIO space mappings.
    
    Provide it as a seperate config option which depends on CONFIG_KMAP_LOCAL
    and let CONFIG_DEBUG_HIGHMEM select it.
    
    This won't increase the debug coverage of this significantly but it paves
    the way to do so.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20201118204006.869487226@linutronix.de

--- include/asm-generic/kmap_size.h
+++ include/asm-generic/kmap_size.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_GENERIC_KMAP_SIZE_H
 
 /* For debug this provides guard pages between the maps */
-#ifdef CONFIG_DEBUG_HIGHMEM
+#ifdef CONFIG_DEBUG_KMAP_LOCAL
 # define KM_MAX_IDX	33
 #else
 # define KM_MAX_IDX	16
