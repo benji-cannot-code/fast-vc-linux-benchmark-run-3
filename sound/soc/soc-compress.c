fac78705535c17af6ff6def2277d1ce9ca0cb776
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2020-11-19 08:50:09 +0900

    ASoC: soc-component: add mark for snd_soc_component_compr_open/free()
    soc_compr_open() does rollback when failed (A),
    but, it is almost same as soc_compr_free().
    
    	static int soc_compr_open(xxx)
    	{
    		...
    		if (ret < 0)
    			goto xxx_err;
    		...
    		return 0;
    
     ^	machine_err:
     |		...
     |	out:
    (A)		...
     |	pm_err:
     |		...
     v		return ret;
    	}
    
    The difference is
    soc_compr_free()  is for all dai/component/substream,
    rollback          is for succeeded part only.
    
    This kind of duplicated code can be a hotbed of bugs,
    thus, we want to share soc_compr_free() and rollback.
    	1) snd_soc_dai_compr_startup/shutdown()
    =>	2) snd_soc_component_compr_open/free()
    	3) snd_soc_link_compr_startup/shutdown()
    
    This patch is for 2) snd_soc_component_compr_open/free(),
    and adds new cstream mark.
    It will mark cstream when startup() was suceeded.
    If rollback happen *after* that, it will check rollback flag
    and marked cstream.
    
    It cares *previous* startup() only now,
    but we might want to check *whole* marked cstream in the future.
    This patch is using macro so that it can be easily adjust to it.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/87lfey5iwk.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/soc-compress.c
+++ sound/soc/soc-compress.c
@@ -44,7 +44,7 @@ static int soc_compr_free(struct snd_compr_stream *cstream)
 
 	snd_soc_link_compr_shutdown(cstream);
 
-	snd_soc_component_compr_free(cstream, NULL);
+	snd_soc_component_compr_free(cstream, 0);
 
 	snd_soc_dai_compr_shutdown(cpu_dai, cstream, 0);
 
@@ -60,7 +60,6 @@ static int soc_compr_free(struct snd_compr_stream *cstream)
 static int soc_compr_open(struct snd_compr_stream *cstream)
 {
 	struct snd_soc_pcm_runtime *rtd = cstream->private_data;
-	struct snd_soc_component *component = NULL;
 	struct snd_soc_dai *cpu_dai = asoc_rtd_to_cpu(rtd, 0);
 	int stream = cstream->direction; /* SND_COMPRESS_xxx is same as SNDRV_PCM_STREAM_xxx */
 	int ret;
@@ -75,7 +74,7 @@ static int soc_compr_open(struct snd_compr_stream *cstream)
 	if (ret < 0)
 		goto out;
 
-	ret = snd_soc_component_compr_open(cstream, &component);
+	ret = snd_soc_component_compr_open(cstream);
 	if (ret < 0)
 		goto machine_err;
 
@@ -90,7 +89,7 @@ static int soc_compr_open(struct snd_compr_stream *cstream)
 	return 0;
 
 machine_err:
-	snd_soc_component_compr_free(cstream, component);
+	snd_soc_component_compr_free(cstream, 1);
 
 	snd_soc_dai_compr_shutdown(cpu_dai, cstream, 1);
 out:
@@ -106,7 +105,6 @@ static int soc_compr_open_fe(struct snd_compr_stream *cstream)
 	struct snd_soc_pcm_runtime *fe = cstream->private_data;
 	struct snd_pcm_substream *fe_substream =
 		 fe->pcm->streams[cstream->direction].substream;
-	struct snd_soc_component *component;
 	struct snd_soc_dai *cpu_dai = asoc_rtd_to_cpu(fe, 0);
 	struct snd_soc_dpcm *dpcm;
 	struct snd_soc_dapm_widget_list *list;
@@ -143,7 +141,7 @@ static int soc_compr_open_fe(struct snd_compr_stream *cstream)
 	if (ret < 0)
 		goto out;
 
-	ret = snd_soc_component_compr_open(cstream, &component);
+	ret = snd_soc_component_compr_open(cstream);
 	if (ret < 0)
 		goto open_err;
 
@@ -164,7 +162,7 @@ static int soc_compr_open_fe(struct snd_compr_stream *cstream)
 	return 0;
 
 machine_err:
-	snd_soc_component_compr_free(cstream, component);
+	snd_soc_component_compr_free(cstream, 1);
 open_err:
 	snd_soc_dai_compr_shutdown(cpu_dai, cstream, 1);
 out:
@@ -210,7 +208,7 @@ static int soc_compr_free_fe(struct snd_compr_stream *cstream)
 
 	snd_soc_link_compr_shutdown(cstream);
 
-	snd_soc_component_compr_free(cstream, NULL);
+	snd_soc_component_compr_free(cstream, 0);
 
 	snd_soc_dai_compr_shutdown(cpu_dai, cstream, 0);
 
