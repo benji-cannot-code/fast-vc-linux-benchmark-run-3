1129910ab9dec6eba2b6634a0393d4b68a83b935
Nathan Chancellor <natechancellor@gmail.com>
2020-11-19 12:52:44 -0700

    memory: tegra30-emc: Remove unnecessary of_node_put in tegra_emc_probe
    Clang warns:
    
      drivers/memory/tegra/tegra30-emc.c:1275:15: warning: variable 'np' is uninitialized when used here [-Wuninitialized]
                    of_node_put(np);
                                ^~
      drivers/memory/tegra/tegra30-emc.c:1269:24: note: initialize the variable 'np' to silence this warning
    
    There does not need to be an of_node_put call in this error handling
    block after the shuffling of the np assignment. Remove it so there is
    no use of uninitialized memory.
    
    Fixes: 5e00fd90183a ("memory: tegra30-emc: Continue probing if timings are missing in device-tree")
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Reviewed-by: Dmitry Osipenko <digetx@gmail.com>
    Link: https://lore.kernel.org/r/20201119195244.1517236-1-natechancellor@gmail.com
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- drivers/memory/tegra/tegra30-emc.c
+++ drivers/memory/tegra/tegra30-emc.c
@@ -1272,10 +1272,8 @@ static int tegra_emc_probe(struct platform_device *pdev)
 	int err;
 
 	emc = devm_kzalloc(&pdev->dev, sizeof(*emc), GFP_KERNEL);
-	if (!emc) {
-		of_node_put(np);
+	if (!emc)
 		return -ENOMEM;
-	}
 
 	emc->mc = devm_tegra_memory_controller_get(&pdev->dev);
 	if (IS_ERR(emc->mc))
