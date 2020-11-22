48c3ea9dcecf863a58ab94784937fba100763cde
Pavel Begunkov <asml.silence@gmail.com>
2020-11-22 15:35:47 +0000

    sbitmap: replace CAS with atomic and
    sbitmap_deferred_clear() does CAS loop to propagate cleared bits,
    replace it with equivalent atomic bitwise and. That's slightly faster
    and makes wait-free instead of lock-free as before.
    
    The atomic can be relaxed (i.e. barrier-less) because following
    sbitmap_get*() deal with synchronisation, see comments in
    sbitmap_queue_clear().
    
    It's ok to cast to atomic_long_t, that's what bitops/lock.h does.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- lib/sbitmap.c
+++ lib/sbitmap.c
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 static inline bool sbitmap_deferred_clear(struct sbitmap_word *map)
 {
-	unsigned long mask, val;
+	unsigned long mask;
 
 	if (!READ_ONCE(map->cleared))
 		return false;
@@ -28,10 +28,8 @@ static inline bool sbitmap_deferred_clear(struct sbitmap_word *map)
 	/*
 	 * Now clear the masked bits in our free word
 	 */
-	do {
-		val = map->word;
-	} while (cmpxchg(&map->word, val, val & ~mask) != val);
-
+	atomic_long_andnot(mask, (atomic_long_t *)&map->word);
+	BUILD_BUG_ON(sizeof(atomic_long_t) != sizeof(map->word));
 	return true;
 }
 
