30a09ffb88ee570269053de528878e1ce731a97d
Bjorn Andersson <bjorn.andersson@linaro.org>
2020-11-21 21:41:33 -0800

    remoteproc: sysmon: Expose the shutdown result
    A graceful shutdown of the Qualcomm remote processors where
    traditionally performed by invoking a shared memory state signal and
    waiting for the associated ack.
    
    This was later superseded by the "sysmon" mechanism, where some form of
    shared memory bus is used to send a "graceful shutdown request" message
    and one of more signals comes back to indicate its success.
    
    But when this newer mechanism is in effect the firmware is shut down by
    the time the older mechanism, implemented in the remoteproc drivers,
    attempts to perform a graceful shutdown - and as such it will never
    receive an ack back.
    
    This patch therefor track the success of the latest shutdown attempt in
    sysmon and exposes a new function in the API that the remoteproc driver
    can use to query the success and the necessity of invoking the older
    mechanism.
    
    Tested-by: Steev Klimaszewski <steev@kali.org>
    Reviewed-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>
    Link: https://lore.kernel.org/r/20201122054135.802935-3-bjorn.andersson@linaro.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/remoteproc/qcom_common.h
+++ drivers/remoteproc/qcom_common.h
@@ -52,6 +52,7 @@ struct qcom_sysmon *qcom_add_sysmon_subdev(struct rproc *rproc,
 					   const char *name,
 					   int ssctl_instance);
 void qcom_remove_sysmon_subdev(struct qcom_sysmon *sysmon);
+bool qcom_sysmon_shutdown_acked(struct qcom_sysmon *sysmon);
 #else
 static inline struct qcom_sysmon *qcom_add_sysmon_subdev(struct rproc *rproc,
 							 const char *name,
@@ -62,6 +63,11 @@ static inline struct qcom_sysmon *qcom_add_sysmon_subdev(struct rproc *rproc,
 
 static inline void qcom_remove_sysmon_subdev(struct qcom_sysmon *sysmon)
 {
+}
+
+static inline bool qcom_sysmon_shutdown_acked(struct qcom_sysmon *sysmon)
+{
+	return false;
 }
 #endif
 
