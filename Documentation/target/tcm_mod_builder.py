2ba444e8eaf3eb67cf02a7a226c7e2e6d05ab54f
Finn Behrens <me@kloenk.de>
2020-11-23 15:15:33 +0100

    tweewide: Fix most Shebang lines
    Change every shebang which does not need an argument to use /usr/bin/env.
    This is needed as not every distro has everything under /usr/bin,
    sometimes not even bash.
    
    Signed-off-by: Finn Behrens <me@kloenk.de>
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>

--- Documentation/target/tcm_mod_builder.py
+++ Documentation/target/tcm_mod_builder.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#!/usr/bin/python
+#!/usr/bin/env python
 # The TCM v4 multi-protocol fabric module generation script for drivers/target/$NEW_MOD
 #
 # Copyright (c) 2010 Rising Tide Systems
