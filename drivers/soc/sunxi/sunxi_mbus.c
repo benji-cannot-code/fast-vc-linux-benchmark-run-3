9670b91caa99bb219d2d8f2158e197b45bb44a8f
Maxime Ripard <maxime@cerno.tech>
2020-11-24 11:35:46 +0100

    soc: sunxi: Fix compilation of sunxi_mbus
    dma_direct_set_offset has been moved from dma-mapping.h to
    dma-map-ops.h, but our driver hasn't been updated resulting in a build
    breakage. Let's change the header to fix the build.
    
    Fixes: 16fee29b0735 ("dma-mapping: remove the dma_direct_set_offset export")
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>
    Link: https://lore.kernel.org/r/20201124103546.839711-1-maxime@cerno.tech'
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/soc/sunxi/sunxi_mbus.c
+++ drivers/soc/sunxi/sunxi_mbus.c
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Copyright (C) 2020 Maxime Ripard <maxime@cerno.tech> */
 
 #include <linux/device.h>
-#include <linux/dma-mapping.h>
+#include <linux/dma-map-ops.h>
 #include <linux/init.h>
 #include <linux/notifier.h>
 #include <linux/of.h>
