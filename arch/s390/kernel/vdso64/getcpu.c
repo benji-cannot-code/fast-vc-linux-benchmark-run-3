1ce1431b31386f2105806f75de0b82a8689ef0fa
Heiko Carstens <hca@linux.ibm.com>
2020-11-24 14:02:35 +0100

    s390/vdso: add missing prototypes for vdso functions
    clang W=1 warns about missing prototypes:
    
    >> arch/s390/kernel/vdso64/getcpu.c:8:5: warning: no previous prototype for function '__s390_vdso_getcpu' [-Wmissing-prototypes]
       int __s390_vdso_getcpu(unsigned *cpu, unsigned *node, struct getcpu_cache *unused)
           ^
    
    Add a local header file in order to get rid of this warnings.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- arch/s390/kernel/vdso64/getcpu.c
+++ arch/s390/kernel/vdso64/getcpu.c
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/compiler.h>
 #include <linux/getcpu.h>
 #include <asm/timex.h>
+#include "vdso.h"
 
 int __s390_vdso_getcpu(unsigned *cpu, unsigned *node, struct getcpu_cache *unused)
 {
