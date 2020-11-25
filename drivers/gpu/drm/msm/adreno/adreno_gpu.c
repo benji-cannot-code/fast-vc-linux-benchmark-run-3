0356b6dbc303a15b13ef12d26abd295f6f001707
Sharat Masetty <smasetty@codeaurora.org>
2020-11-25 12:30:15 +0530

    drm/msm/a6xx: Add support for using system cache(LLC)
    The last level system cache can be partitioned to 32 different
    slices of which GPU has two slices preallocated. One slice is
    used for caching GPU buffers and the other slice is used for
    caching the GPU SMMU pagetables. This talks to the core system
    cache driver to acquire the slice handles, configure the SCID's
    to those slices and activates and deactivates the slices upon
    GPU power collapse and restore.
    
    Some support from the IOMMU driver is also needed to make use
    of the system cache to set the right TCR attributes. GPU then
    has the ability to override a few cacheability parameters which
    it does to override write-allocate to write-no-allocate as the
    GPU hardware does not benefit much from it.
    
    DOMAIN_ATTR_IO_PGTABLE_CFG is another domain level attribute used
    by the IOMMU driver for pagetable configuration which will be used
    to set a quirk initially to set the right attributes to cache the
    hardware pagetables into the system cache.
    
    Signed-off-by: Sharat Masetty <smasetty@codeaurora.org>
    [saiprakash.ranjan: fix to set attr before device attach to iommu and rebase]
    Signed-off-by: Sai Prakash Ranjan <saiprakash.ranjan@codeaurora.org>
    Signed-off-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/adreno/adreno_gpu.c
+++ drivers/gpu/drm/msm/adreno/adreno_gpu.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/soc/qcom/mdt_loader.h>
 #include <soc/qcom/ocmem.h>
 #include "adreno_gpu.h"
+#include "a6xx_gpu.h"
 #include "msm_gem.h"
 #include "msm_mmu.h"
 
@@ -190,6 +191,9 @@ struct msm_gem_address_space *
 adreno_iommu_create_address_space(struct msm_gpu *gpu,
 		struct platform_device *pdev)
 {
+	struct adreno_gpu *adreno_gpu = to_adreno_gpu(gpu);
+	struct a6xx_gpu *a6xx_gpu = to_a6xx_gpu(adreno_gpu);
+	struct io_pgtable_domain_attr pgtbl_cfg;
 	struct iommu_domain *iommu;
 	struct msm_mmu *mmu;
 	struct msm_gem_address_space *aspace;
@@ -199,7 +203,20 @@ adreno_iommu_create_address_space(struct msm_gpu *gpu,
 	if (!iommu)
 		return NULL;
 
+	/*
+	 * This allows GPU to set the bus attributes required to use system
+	 * cache on behalf of the iommu page table walker.
+	 */
+	if (!IS_ERR(a6xx_gpu->htw_llc_slice)) {
+		pgtbl_cfg.quirks = IO_PGTABLE_QUIRK_ARM_OUTER_WBWA;
+		iommu_domain_set_attr(iommu, DOMAIN_ATTR_IO_PGTABLE_CFG, &pgtbl_cfg);
+	}
+
 	mmu = msm_iommu_new(&pdev->dev, iommu);
+	if (IS_ERR(mmu)) {
+		iommu_domain_free(iommu);
+		return ERR_CAST(mmu);
+	}
 
 	/*
 	 * Use the aperture start or SZ_16M, whichever is greater. This will
