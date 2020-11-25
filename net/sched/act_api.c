8681ed21805c2137cf5f4ba94bace6d51e063176
wenxu <wenxu@ucloud.cn>
2020-11-25 12:01:23 +0800

    net/sched: sch_frag: add generic packet fragment support.
    Currently kernel tc subsystem can do conntrack in cat_ct. But when several
    fragment packets go through the act_ct, function tcf_ct_handle_fragments
    will defrag the packets to a big one. But the last action will redirect
    mirred to a device which maybe lead the reassembly big packet over the mtu
    of target device.
    
    This patch add support for a xmit hook to mirred, that gets executed before
    xmiting the packet. Then, when act_ct gets loaded, it configs that hook.
    The frag xmit hook maybe reused by other modules.
    
    Signed-off-by: wenxu <wenxu@ucloud.cn>
    Acked-by: Cong Wang <cong.wang@bytedance.com>
    Acked-by: Jamal Hadi Salim <jhs@mojatatu.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/sched/act_api.c
+++ net/sched/act_api.c
@@ -23,6 +23,22 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/act_api.h>
 #include <net/netlink.h>
 
+#ifdef CONFIG_INET
+DEFINE_STATIC_KEY_FALSE(tcf_frag_xmit_count);
+EXPORT_SYMBOL_GPL(tcf_frag_xmit_count);
+#endif
+
+int tcf_dev_queue_xmit(struct sk_buff *skb, int (*xmit)(struct sk_buff *skb))
+{
+#ifdef CONFIG_INET
+	if (static_branch_unlikely(&tcf_frag_xmit_count))
+		return sch_frag_xmit_hook(skb, xmit);
+#endif
+
+	return xmit(skb);
+}
+EXPORT_SYMBOL_GPL(tcf_dev_queue_xmit);
+
 static void tcf_action_goto_chain_exec(const struct tc_action *a,
 				       struct tcf_result *res)
 {
