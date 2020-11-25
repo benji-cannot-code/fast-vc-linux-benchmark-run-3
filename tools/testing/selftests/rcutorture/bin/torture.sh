5473f5c676ee6b2a99b57b17974cf08570af4b87
Paul E. McKenney <paulmck@kernel.org>
2020-11-24 18:57:47 -0800

    torture: Make torture.sh rcuscale and refscale deal with allmodconfig
    The .mod.c files created by allmodconfig builds interfers with the approach
    torture.sh uses to enumerate types of rcuscale and refscale runs.  This
    commit therefore tightens the pattern matching to avoid this interference.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- tools/testing/selftests/rcutorture/bin/torture.sh
+++ tools/testing/selftests/rcutorture/bin/torture.sh
@@ -303,7 +303,7 @@ fi
 
 if test "$do_refscale" = yes
 then
-	primlist="`grep '\.name[ 	]*=' kernel/rcu/refscale*.c | sed -e 's/^[^"]*"//' -e 's/".*$//'`"
+	primlist="`grep '\.name[ 	]*=' kernel/rcu/refscale.c | sed -e 's/^[^"]*"//' -e 's/".*$//'`"
 else
 	primlist=
 fi
@@ -315,7 +315,7 @@ done
 
 if test "$do_rcuscale" = yes
 then
-	primlist="`grep '\.name[ 	]*=' kernel/rcu/rcuscale*.c | sed -e 's/^[^"]*"//' -e 's/".*$//'`"
+	primlist="`grep '\.name[ 	]*=' kernel/rcu/rcuscale.c | sed -e 's/^[^"]*"//' -e 's/".*$//'`"
 else
 	primlist=
 fi
