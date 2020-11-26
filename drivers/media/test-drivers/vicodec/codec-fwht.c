83eac6ff6729a906881e365eb9d87fb96247fb5f
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2020-11-26 14:02:18 +0100

    media: vicodec: mark the stateless FWHT API as stable
    The FWHT stateless 'uAPI' was staging and marked explicitly in the
    V4L2 specification that it will change and is unstable.
    Note that these control IDs were never exported as a public API,
    they were only defined in kernel-local headers (fwht-ctrls.h).
    
    Now, the FWHT stateless controls is ready to be part
    of the stable uAPI.
    
    While not too late:
    
    - Rename V4L2_CID_MPEG_VIDEO_FWHT_PARAMS to V4L2_CID_STATELESS_FWHT_PARAMS.
    
    - Move the contents of fwht-ctrls.h to v4l2-controls.h.
    
    - Move the public parts of drivers/media/test-drivers/vicodec/codec-fwht.h
      to v4l2-controls.h.
    
    - Add V4L2_CTRL_TYPE_FWHT_PARAMS control initialization and validation.
    
    - Add p_fwht_params to struct v4l2_ext_control.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/test-drivers/vicodec/codec-fwht.c
+++ drivers/media/test-drivers/vicodec/codec-fwht.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/string.h>
 #include <linux/kernel.h>
+#include <linux/videodev2.h>
 #include "codec-fwht.h"
 
 #define OVERFLOW_BIT BIT(14)
--- include/media/v4l2-ctrls.h
+++ include/media/v4l2-ctrls.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * This will move to the public headers once this API is fully stable.
  */
 #include <media/mpeg2-ctrls.h>
-#include <media/fwht-ctrls.h>
 #include <media/vp8-ctrls.h>
 #include <media/hevc-ctrls.h>
 
