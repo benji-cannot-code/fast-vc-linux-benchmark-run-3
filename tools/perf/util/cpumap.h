e946ddd75aa40cafe6f07faec26c9257f85a95f1
James Clark <james.clark@arm.com>
2020-11-26 16:13:28 +0200

    perf stat aggregation: Add separate thread member
    A separate field isn't strictly required. The core field could be
    re-used for thread IDs as a single field was used previously.
    
    But separating them will avoid confusion and catch potential errors
    where core IDs are read as thread IDs and vice versa.
    
    Also remove the placeholder id field which is now no longer used.
    
    Signed-off-by: James Clark <james.clark@arm.com>
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Acked-by: Jiri Olsa <jolsa@redhat.com>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Tested-by: John Garry <john.garry@huawei.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Richter <tmricht@linux.ibm.com>
    Link: https://lore.kernel.org/r/20201126141328.6509-13-james.clark@arm.com
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/util/cpumap.h
+++ tools/perf/util/cpumap.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <perf/cpumap.h>
 
 struct aggr_cpu_id {
-	int id;
+	int thread;
 	int node;
 	int socket;
 	int die;
