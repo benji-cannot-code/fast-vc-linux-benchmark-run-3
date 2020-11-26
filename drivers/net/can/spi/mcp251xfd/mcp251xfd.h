0b684d710cea571a5337df389527c19e74a06968
Marc Kleine-Budde <mkl@pengutronix.de>
2020-11-26 14:21:40 +0100

    can: mcp251xfd: mcp25xxfd_ring_alloc(): add define instead open coding the maximum number of RX objects
    This patch add a define for the maximum number of RX objects instead of open
    coding it.
    
    Link: https://lore.kernel.org/r/20201126132144.351154-2-mkl@pengutronix.de
    Tested-by: Thomas Kopp <thomas.kopp@microchip.com>
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/spi/mcp251xfd/mcp251xfd.h
+++ drivers/net/can/spi/mcp251xfd/mcp251xfd.h
@@ -369,6 +369,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * FIFO setup: tef: 8*12 bytes = 96 bytes, tx: 8*16 bytes = 128 bytes
  * FIFO setup: tef: 4*12 bytes = 48 bytes, tx: 4*72 bytes = 288 bytes
  */
+#define MCP251XFD_RX_OBJ_NUM_MAX 32
 #define MCP251XFD_TX_OBJ_NUM_CAN 8
 #define MCP251XFD_TX_OBJ_NUM_CANFD 4
 
