1581ac4580cf7cf9fe57a437102b887f58359038
Christoph Hellwig <hch@lst.de>
2020-11-26 10:41:07 +0100

    block: stop using bdget_disk for partition 0
    We can just dereference the point in struct gendisk instead.  Also
    remove the now unused export.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/genhd.c
+++ block/genhd.c
@@ -931,7 +931,6 @@ struct block_device *bdget_disk(struct gendisk *disk, int partno)
 
 	return bdev;
 }
-EXPORT_SYMBOL(bdget_disk);
 
 /*
  * print a full list of all partitions - intended for places where the root
