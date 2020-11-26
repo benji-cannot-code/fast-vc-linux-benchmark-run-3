d6b4d6a759318c883cdd4dc6597b8a52eaae835e
Chris Wilson <chris@chris-wilson.co.uk>
2020-11-26 14:04:04 +0000

    drm/i915/gt: Check for a completed last request once
    Pull the repeated check for the last active request being completed to a
    single spot, when deciding whether or not execlist preemption is
    required.
    
    In doing so, we remove the tasklet kick, introduced with the completion
    checks in commit 35f3fd8182ba ("drm/i915/execlists: Workaround switching
    back to a completed context"), if we find the request was completed but
    have not yet seen the corresponding CS event. This was devolving into a
    busy spin of the tasklet while we waited for the event as the delivery
    was not as instantaneous as expected. Under load this is sufficient to
    exhaust the tasklet softirq timeslice, and force ksoftirqd. Quite
    noticeable overhead for no apparent improvement in latency.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201126140407.31952-2-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gt/intel_lrc.c
+++ drivers/gpu/drm/i915/gt/intel_lrc.c
@@ -2142,12 +2142,9 @@ static void execlists_dequeue(struct intel_engine_cs *engine)
 	 */
 
 	if ((last = *active)) {
-		if (need_preempt(engine, last, rb)) {
-			if (i915_request_completed(last)) {
-				tasklet_hi_schedule(&execlists->tasklet);
-				return;
-			}
-
+		if (i915_request_completed(last)) {
+			goto check_secondary;
+		} else if (need_preempt(engine, last, rb)) {
 			ENGINE_TRACE(engine,
 				     "preempting last=%llx:%lld, prio=%d, hint=%d\n",
 				     last->fence.context,
@@ -2175,11 +2172,6 @@ static void execlists_dequeue(struct intel_engine_cs *engine)
 			last = NULL;
 		} else if (need_timeslice(engine, last, rb) &&
 			   timeslice_expired(execlists, last)) {
-			if (i915_request_completed(last)) {
-				tasklet_hi_schedule(&execlists->tasklet);
-				return;
-			}
-
 			ENGINE_TRACE(engine,
 				     "expired last=%llx:%lld, prio=%d, hint=%d, yield?=%s\n",
 				     last->fence.context,
@@ -2215,6 +2207,7 @@ static void execlists_dequeue(struct intel_engine_cs *engine)
 			 * we hopefully coalesce several updates into a single
 			 * submission.
 			 */
+check_secondary:
 			if (!list_is_last(&last->sched.link,
 					  &engine->active.requests)) {
 				/*
