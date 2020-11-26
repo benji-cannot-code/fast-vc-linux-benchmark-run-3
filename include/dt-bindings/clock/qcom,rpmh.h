2989012589d84447e7408d25f847d47b9fb339f8
Vinod Koul <vkoul@kernel.org>
2020-11-26 12:58:41 +0530

    dt-bindings: clock: Introduce RPMHCC bindings for SDX55
    Add compatible for SDX55 RPMHCC and DT include.
    
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Acked-by: Rob Herring <robh@kernel.org>
    Link: https://lore.kernel.org/r/20201126072844.35370-4-manivannan.sadhasivam@linaro.org
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- include/dt-bindings/clock/qcom,rpmh.h
+++ include/dt-bindings/clock/qcom,rpmh.h
@@ -23,5 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RPMH_LN_BB_CLK1				13
 #define RPMH_LN_BB_CLK1_A			14
 #define RPMH_CE_CLK				15
+#define RPMH_QPIC_CLK				16
 
 #endif
