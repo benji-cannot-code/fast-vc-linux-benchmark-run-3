6cb67f231da9884a5769e0213fbb3a3e7917cb30
Martin Schiller <ms@dev.tdt.de>
2020-11-26 07:35:53 +0100

    net/x25: handle additional netdev events
    1. Add / remove x25_link_device by NETDEV_REGISTER/UNREGISTER and also
       by NETDEV_POST_TYPE_CHANGE/NETDEV_PRE_TYPE_CHANGE.
    
       This change is needed so that the x25_neigh struct for an interface
       is already created when it shows up and is kept independently if the
       interface goes UP or DOWN.
    
       This is used in an upcomming commit, where x25 params of an neighbour
       will get configurable through ioctls.
    
    2. NETDEV_CHANGE event makes it possible to handle carrier loss and
       detection. If carrier is lost, clean up everything related to this
       neighbour by calling x25_link_terminated().
    
    3. Also call x25_link_terminated() for NETDEV_DOWN events and remove the
       call to x25_clear_forward_by_dev() in x25_route_device_down(), as
       this is already called by x25_kill_by_neigh() which gets called by
       x25_link_terminated().
    
    4. Do nothing for NETDEV_UP and NETDEV_GOING_DOWN events, as these will
       be handled in layer 2 (LAPB) and layer3 (X.25) will be informed by
       layer2 when layer2 link is established and layer3 link should be
       initiated.
    
    Signed-off-by: Martin Schiller <ms@dev.tdt.de>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/x25/x25_link.c
+++ net/x25/x25_link.c
@@ -233,6 +233,9 @@ void x25_link_established(struct x25_neigh *nb)
 void x25_link_terminated(struct x25_neigh *nb)
 {
 	nb->state = X25_LINK_STATE_0;
+	skb_queue_purge(&nb->queue);
+	x25_stop_t20timer(nb);
+
 	/* Out of order: clear existing virtual calls (X.25 03/93 4.6.3) */
 	x25_kill_by_neigh(nb);
 }
@@ -278,9 +281,6 @@ void x25_link_device_up(struct net_device *dev)
  */
 static void __x25_remove_neigh(struct x25_neigh *nb)
 {
-	skb_queue_purge(&nb->queue);
-	x25_stop_t20timer(nb);
-
 	if (nb->node.next) {
 		list_del(&nb->node);
 		x25_neigh_put(nb);
