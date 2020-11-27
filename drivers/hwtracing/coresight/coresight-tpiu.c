fd39565fe5cf33e5534128317b4d304d5983edbf
Mathieu Poirier <mathieu.poirier@linaro.org>
2020-11-27 10:52:55 -0700

    coresight-tpiu: Fix W=1 warning in TPIU driver
      CC      drivers/hwtracing/coresight/coresight-etm4x-core.o
      CC      drivers/hwtracing/coresight/coresight-etm4x-sysfs.o
      CC      drivers/hwtracing/coresight/coresight-stm.o
    drivers/hwtracing/coresight/coresight-tpiu.c:53: warning: Cannot understand  * @base:       memory mapped base address for this component.
     on line 53 - I thought it was a doc line
      CC      drivers/hwtracing/coresight/coresight-cpu-debug.o
      CC      drivers/hwtracing/coresight/coresight-catu.o
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Link: https://lore.kernel.org/r/20201127175256.1092685-15-mathieu.poirier@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hwtracing/coresight/coresight-tpiu.c
+++ drivers/hwtracing/coresight/coresight-tpiu.c
@@ -50,7 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 DEFINE_CORESIGHT_DEVLIST(tpiu_devs, "tpiu");
 
-/**
+/*
  * @base:	memory mapped base address for this component.
  * @atclk:	optional clock for the core parts of the TPIU.
  * @csdev:	component vitals needed by the framework.
