5947ae20659702d157db395a861f4fa2e7787f53
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2020-11-30 08:45:19 +0900

    ASoC: soc-core: tidyup jack.h
    soc-core.c don't need sound/jack.h anymore, but asoc.h needs it.
    This patch fixup header magic.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/87y2iju3zm.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/of_graph.h>
 #include <linux/dmi.h>
 #include <sound/core.h>
-#include <sound/jack.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
