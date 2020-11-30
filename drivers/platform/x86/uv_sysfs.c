0c6494d69265cbf009a293561dbe3c4736582002
Zou Wei <zou_wei@huawei.com>
2020-11-30 19:11:53 +0800

    x86/platform/uv: Make uv_pcibus_kset and uv_hubs_kset static
    Fix the following sparse warnings:
    
      drivers/platform/x86/uv_sysfs.c:22:13: warning: symbol \
    	  'uv_pcibus_kset' was not declared. Should it be static?
      drivers/platform/x86/uv_sysfs.c:23:13: warning: symbol \
    	  'uv_hubs_kset' was not declared. Should it be static?
    
    Signed-off-by: Zou Wei <zou_wei@huawei.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Acked-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://lkml.kernel.org/r/1606734713-43919-1-git-send-email-zou_wei@huawei.com

--- drivers/platform/x86/uv_sysfs.c
+++ drivers/platform/x86/uv_sysfs.c
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define INVALID_CNODE -1
 
 struct kobject *sgi_uv_kobj;
-struct kset *uv_pcibus_kset;
-struct kset *uv_hubs_kset;
+static struct kset *uv_pcibus_kset;
+static struct kset *uv_hubs_kset;
 static struct uv_bios_hub_info *hub_buf;
 static struct uv_bios_port_info **port_buf;
 static struct uv_hub **uv_hubs;
