23942b21e83445689f103bad8ca698151f57e61f
Ravi Bangoria <ravi.bangoria@linux.ibm.com>
2020-11-30 09:14:06 +0530

    powerpc/xmon: Fix build failure for 8xx
    With CONFIG_PPC_8xx and CONFIG_XMON set, kernel build fails with
    
      arch/powerpc/xmon/xmon.c:1379:12: error: 'find_free_data_bpt' defined
      but not used [-Werror=unused-function]
    
    Fix it by enclosing find_free_data_bpt() inside #ifndef CONFIG_PPC_8xx.
    
    Fixes: 30df74d67d48 ("powerpc/watchpoint/xmon: Support 2nd DAWR")
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Ravi Bangoria <ravi.bangoria@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20201130034406.288047-1-ravi.bangoria@linux.ibm.com

--- arch/powerpc/xmon/xmon.c
+++ arch/powerpc/xmon/xmon.c
@@ -1384,6 +1384,7 @@ static long check_bp_loc(unsigned long addr)
 	return 1;
 }
 
+#ifndef CONFIG_PPC_8xx
 static int find_free_data_bpt(void)
 {
 	int i;
@@ -1395,6 +1396,7 @@ static int find_free_data_bpt(void)
 	printf("Couldn't find free breakpoint register\n");
 	return -1;
 }
+#endif
 
 static void print_data_bpts(void)
 {
