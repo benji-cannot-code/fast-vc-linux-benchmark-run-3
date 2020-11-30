73d68e5e2bbc5f7c3e23bb814fe40fdba7e002bf
Samuel Cabrero <scabrero@suse.de>
2020-11-30 19:02:52 +0100

    cifs: Set witness notification handler for messages from userspace daemon
    + Set a handler for the witness notification messages received from the
      userspace daemon.
    
    + Handle the resource state change notification. When the resource
      becomes unavailable or available set the tcp status to
      CifsNeedReconnect for all channels.
    
    Signed-off-by: Samuel Cabrero <scabrero@suse.de>
    Reviewed-by: Aurelien Aptel <aaptel@suse.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/cifs/cifs_swn.h
+++ fs/cifs/cifs_swn.h
@@ -10,9 +10,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _CIFS_SWN_H
 
 struct cifs_tcon;
+struct sk_buff;
+struct genl_info;
 
 extern int cifs_swn_register(struct cifs_tcon *tcon);
 
 extern int cifs_swn_unregister(struct cifs_tcon *tcon);
+
+extern int cifs_swn_notify(struct sk_buff *skb, struct genl_info *info);
 
 #endif /* _CIFS_SWN_H */
