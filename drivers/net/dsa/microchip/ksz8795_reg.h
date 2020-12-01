a255760bc4f6cc14b723a8e8865f6487eb589bb5
Michael Grzeschik <m.grzeschik@pengutronix.de>
2020-12-01 21:45:05 +0100

    net: dsa: microchip: ksz8795: use port_cnt instead of TOTOAL_PORT_NUM
    To get the driver working with other chips using different port counts
    the dyn_mac_table should be flushed depending on the amount of available
    ports. This patch remove the extra define TOTOAL_PORT_NUM and is
    making use of the dynamic port_cnt variable instead.
    
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Michael Grzeschik <m.grzeschik@pengutronix.de>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/dsa/microchip/ksz8795_reg.h
+++ drivers/net/dsa/microchip/ksz8795_reg.h
@@ -847,8 +847,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define KS_PRIO_IN_REG			4
 
-#define TOTAL_PORT_NUM			5
-
 #define KSZ8795_COUNTER_NUM		0x20
 
 /* Common names used by other drivers */
