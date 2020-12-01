dcf6dd01a801152a393c31a07f78eb6fcc3ef587
Guvenc Gulce <guvenc@linux.ibm.com>
2020-12-01 20:20:48 +0100

    net/smc: Add support for obtaining SMCD device list
    Deliver SMCD device information via netlink based
    diagnostic interface.
    
    Signed-off-by: Guvenc Gulce <guvenc@linux.ibm.com>
    Signed-off-by: Karsten Graul <kgraul@linux.ibm.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/smc/smc_netlink.c
+++ net/smc/smc_netlink.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/smc.h>
 
 #include "smc_core.h"
+#include "smc_ism.h"
 #include "smc_netlink.h"
 
 #define SMC_CMD_MAX_ATTR 1
@@ -43,6 +44,11 @@ static const struct genl_ops smc_gen_nl_ops[] = {
 		.cmd = SMC_NETLINK_GET_LGR_SMCD,
 		/* can be retrieved by unprivileged users */
 		.dumpit = smcd_nl_get_lgr,
+	},
+	{
+		.cmd = SMC_NETLINK_GET_DEV_SMCD,
+		/* can be retrieved by unprivileged users */
+		.dumpit = smcd_nl_get_device,
 	},
 };
 
