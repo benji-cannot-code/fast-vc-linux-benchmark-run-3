7bc59e7feced6385a3f01cc5239ad40cc6be1a76
Johannes Berg <johannes.berg@intel.com>
2020-12-02 12:59:50 +0100

    um: Support dynamic IRQ allocation
    It's cumbersome and error-prone to keep adding fixed IRQ numbers,
    and for proper device wakeup support for the virtio/vhost-user
    support we need to have different IRQs for each device. Even if
    in theory two IRQs (with and without wake) might be sufficient,
    it's much easier to reason about it when we have dynamic number
    assignment. It also makes it easier to add new devices that may
    dynamically exist or depending on the configuration, etc.
    
    Add support for this, up to 64 IRQs (the same limit as epoll FDs
    we have right now). Since it's not easy to port all the existing
    places to dynamic allocation (some data is statically initialized)
    keep the low numbers are reserved for the existing hard-coded IRQ
    numbers.
    
    Acked-By: Anton Ivanov <anton.ivanov@cambridgegreys.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Acked-By: Anton Ivanov <anton.ivanov@cambridgegreys.com>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- arch/um/include/asm/irq.h
+++ arch/um/include/asm/irq.h
@@ -25,14 +25,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VECTOR_BASE_IRQ		(VIRTIO_IRQ + 1)
 #define VECTOR_IRQ_SPACE	8
 
-#define LAST_IRQ (VECTOR_IRQ_SPACE + VECTOR_BASE_IRQ - 1)
+#define UM_FIRST_DYN_IRQ (VECTOR_IRQ_SPACE + VECTOR_BASE_IRQ)
 
 #else
 
-#define LAST_IRQ VIRTIO_IRQ
+#define UM_FIRST_DYN_IRQ (VIRTIO_IRQ + 1)
 
 #endif
 
-#define NR_IRQS (LAST_IRQ + 1)
+#define NR_IRQS			64
 
 #endif
--- arch/um/include/shared/irq_kern.h
+++ arch/um/include/shared/irq_kern.h
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <asm/ptrace.h>
 
-extern int um_request_irq(unsigned int irq, int fd, int type,
-			  irq_handler_t handler,
-			  unsigned long irqflags,  const char * devname,
-			  void *dev_id);
-void um_free_irq(unsigned int irq, void *dev);
-#endif
+#define UM_IRQ_ALLOC	-1
 
+int um_request_irq(int irq, int fd, int type, irq_handler_t handler,
+		   unsigned long irqflags,  const char * devname,
+		   void *dev_id);
+void um_free_irq(int irq, void *dev_id);
+#endif
