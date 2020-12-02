6eedd64a940498ea80d82f1732cfbf85673ffc8b
Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
2020-12-02 10:08:54 +0530

    powerpc/book3s64/kup: Check max key supported before enabling kup
    Don't enable KUEP/KUAP if we support less than or equal to 3 keys.
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20201202043854.76406-1-aneesh.kumar@linux.ibm.com

--- arch/powerpc/include/asm/kup.h
+++ arch/powerpc/include/asm/kup.h
@@ -45,6 +45,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #else /* !__ASSEMBLY__ */
 
+extern bool disable_kuep;
+extern bool disable_kuap;
+
 #include <linux/pgtable.h>
 
 void setup_kup(void);
