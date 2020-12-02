8f6cc3d9345c79d679029d1d8412cece6d2d3a7c
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-12-02 09:27:14 +0100

    x86/sgx: Fix a typo in kernel-doc markup
    Fix the following kernel-doc warning:
    
      arch/x86/include/uapi/asm/sgx.h:19: warning: expecting prototype \
        for enum sgx_epage_flags. Prototype was for enum sgx_page_flags instead
    
     [ bp: Launder the commit message. ]
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lkml.kernel.org/r/ca11a4540d981cbd5f026b6cbc8931aa55654e00.1606897462.git.mchehab+huawei@kernel.org

--- arch/x86/include/uapi/asm/sgx.h
+++ arch/x86/include/uapi/asm/sgx.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ioctl.h>
 
 /**
- * enum sgx_epage_flags - page control flags
+ * enum sgx_page_flags - page control flags
  * %SGX_PAGE_MEASURE:	Measure the page contents with a sequence of
  *			ENCLS[EEXTEND] operations.
  */
