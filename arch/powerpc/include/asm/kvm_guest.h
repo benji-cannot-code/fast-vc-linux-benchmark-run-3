98f90f77fbb8031cc627704b7821ecbaf17a0968
Srikar Dronamraju <srikar@linux.vnet.ibm.com>
2020-12-02 10:34:54 +0530

    powerpc: Rename is_kvm_guest() to check_kvm_guest()
    We want to reuse the is_kvm_guest() name in a subsequent patch but
    with a new body. Hence rename is_kvm_guest() to check_kvm_guest(). No
    additional changes.
    
    Signed-off-by: Srikar Dronamraju <srikar@linux.vnet.ibm.com>
    Acked-by: Waiman Long <longman@redhat.com>
    Signed-off-by: kernel test robot <lkp@intel.com> # int -> bool fix
    [mpe: Fold in fix from lkp to use true/false not 0/1]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20201202050456.164005-3-srikar@linux.vnet.ibm.com

--- arch/powerpc/include/asm/kvm_guest.h
+++ arch/powerpc/include/asm/kvm_guest.h
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_POWERPC_KVM_GUEST_H_
 
 #if defined(CONFIG_PPC_PSERIES) || defined(CONFIG_KVM_GUEST)
-bool is_kvm_guest(void);
+bool check_kvm_guest(void);
 #else
-static inline bool is_kvm_guest(void) { return false; }
+static inline bool check_kvm_guest(void) { return false; }
 #endif
 
 #endif /* _ASM_POWERPC_KVM_GUEST_H_ */
