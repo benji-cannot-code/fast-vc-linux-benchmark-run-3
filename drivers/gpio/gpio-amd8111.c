7b2059d226faa8c0acdad54750502c984412bf32
Enrico Weigelt, metux IT consult <info@metux.net>
2020-12-03 19:24:23 +0100

    drivers: gpio: amd8111: use SPDX-License-Identifier
    Prefer SPDX-License-Identifier over hand-written texts.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    Link: https://lore.kernel.org/r/20201203182423.5499-3-info@metux.net
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpio-amd8111.c
+++ drivers/gpio/gpio-amd8111.c
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+// SPDX-License-Identifier: GPL-2.0
 /*
  * GPIO driver for AMD 8111 south bridges
  *
@@ -21,10 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Hardware driver for Intel i810 Random Number Generator (RNG)
  * Copyright 2000,2001 Jeff Garzik <jgarzik@pobox.com>
  * Copyright 2000,2001 Philipp Rumpf <prumpf@mandrakesoft.com>
- *
- * This file is licensed under  the terms of the GNU General Public
- * License version 2. This program is licensed "as is" without any
- * warranty of any kind, whether express or implied.
  */
 #include <linux/ioport.h>
 #include <linux/module.h>
