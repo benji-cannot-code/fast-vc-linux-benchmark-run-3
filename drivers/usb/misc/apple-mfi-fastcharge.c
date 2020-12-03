47225d8b918218fe30ab8bc38d0c29b4d2fb9ecf
Dan Carpenter <dan.carpenter@oracle.com>
2020-12-03 11:42:10 +0300

    USB: apple-mfi-fastcharge: Fix use after free in probe
    This code frees "mfi" and then derefences it on the next line to get
    the error code.
    
    Fixes: b0eec52fbe63 ("USB: apple-mfi-fastcharge: Fix kfree after failed kzalloc")
    Reviewed-by: Bastien Nocera <hadess@hadess.net>
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Link: https://lore.kernel.org/r/X8ik4j8yJitVUyfU@mwanda
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/misc/apple-mfi-fastcharge.c
+++ drivers/usb/misc/apple-mfi-fastcharge.c
@@ -179,6 +179,7 @@ static int mfi_fc_probe(struct usb_device *udev)
 {
 	struct power_supply_config battery_cfg = {};
 	struct mfi_device *mfi = NULL;
+	int err;
 
 	if (!mfi_fc_match(udev))
 		return -ENODEV;
@@ -195,8 +196,9 @@ static int mfi_fc_probe(struct usb_device *udev)
 						&battery_cfg);
 	if (IS_ERR(mfi->battery)) {
 		dev_err(&udev->dev, "Can't register battery\n");
+		err = PTR_ERR(mfi->battery);
 		kfree(mfi);
-		return PTR_ERR(mfi->battery);
+		return err;
 	}
 
 	mfi->udev = usb_get_dev(udev);
