38672d6a29370f9284ed40b9175a8a6bc0f5e051
Simon Wunderlich <sw@simonwunderlich.de>
2020-12-04 08:41:16 +0100

    batman-adv: Drop unused soft-interface.h include in fragmentation.c
    The commit 992b03b88e36 ("batman-adv: Don't always reallocate the
    fragmentation skb head") removed the last user of functions from
    soft-interface.h.
    
    Signed-off-by: Simon Wunderlich <sw@simonwunderlich.de>

--- net/batman-adv/fragmentation.c
+++ net/batman-adv/fragmentation.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "originator.h"
 #include "routing.h"
 #include "send.h"
-#include "soft-interface.h"
 
 /**
  * batadv_frag_clear_chain() - delete entries in the fragment buffer chain
