0e835cd1d2fdd1bcbc15f8d9da8bfee8a95e6b9c
Arnd Bergmann <arnd@arndb.de>
2020-12-04 17:52:22 +0100

    regulator: da9121: include linux/gpio/consumer.h
    When CONFIG_GPIOLIB is disabled, the declarations from linux/gpio/consumer.h
    are not visible:
    
    drivers/regulator/da9121-regulator.c:371:14: error: implicit declaration of function 'fwnode_gpiod_get_index' [-Werror,-Wimplicit-function-declaration]
            ena_gpiod = fwnode_gpiod_get_index(of_fwnode_handle(np), "enable", 0,
                        ^
    drivers/regulator/da9121-regulator.c:372:7: error: use of undeclared identifier 'GPIOD_OUT_HIGH'
                                                    GPIOD_OUT_HIGH |
                                                    ^
    drivers/regulator/da9121-regulator.c:373:7: error: use of undeclared identifier 'GPIOD_FLAGS_BIT_NONEXCLUSIVE'
                                                    GPIOD_FLAGS_BIT_NONEXCLUSIVE,
    
    Include this explicitly to help compile testing.
    
    Fixes: 46c413d5bb23 ("regulator: da9121: Add support for device variants via devicetree")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Link: https://lore.kernel.org/r/20201204165229.3754763-1-arnd@kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/regulator/da9121-regulator.c
+++ drivers/regulator/da9121-regulator.c
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/of_device.h>
 #include <linux/of_gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/regulator/of_regulator.h>
 #include <linux/regulator/machine.h>
 #include <linux/regulator/driver.h>
