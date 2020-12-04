0965adc9e64c4535ad9d070f2a7822298bc7d316
Ben Skeggs <bskeggs@redhat.com>
2020-12-04 12:36:38 +1000

    drm/nouveau/mxm: switch to instanced constructor
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>
    Reviewed-by: Lyude Paul <lyude@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/subdev/mxm.h
+++ drivers/gpu/drm/nouveau/include/nvkm/subdev/mxm.h
@@ -4,5 +4,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NVKM_MXM_H__
 #include <core/subdev.h>
 
-int nv50_mxm_new(struct nvkm_device *, int, struct nvkm_subdev **);
+int nv50_mxm_new(struct nvkm_device *, enum nvkm_subdev_type, int inst, struct nvkm_subdev **);
 #endif
