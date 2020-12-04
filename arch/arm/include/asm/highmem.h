9008083b5535c71399bfff3b6c039be55bf67a05
Arnd Bergmann <arnd@arndb.de>
2020-12-04 17:59:22 +0100

    ARM: highmem: Fix cache_is_vivt() reference
    The reference to cache_is_vivt() was moved into a header file,
    which now causes a build failure in rare randconfig builds:
    
    arch/arm/include/asm/highmem.h:52:43: error: implicit declaration of function 'cache_is_vivt' [-Werror,-Wimplicit-function-declaration]
    
    Add an explicit include to make it build reliably.
    
    Fixes: 2a15ba82fa6c ("ARM: highmem: Switch to generic kmap atomic")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Ira Weiny <ira.weiny@intel.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Link: https://lore.kernel.org/r/20201204165930.3877571-1-arnd@kernel.org

--- arch/arm/include/asm/highmem.h
+++ arch/arm/include/asm/highmem.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_HIGHMEM_H
 #define _ASM_HIGHMEM_H
 
+#include <asm/cachetype.h>
 #include <asm/fixmap.h>
 
 #define PKMAP_BASE		(PAGE_OFFSET - PMD_SIZE)
