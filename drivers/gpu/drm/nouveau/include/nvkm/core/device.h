470dce808cca38cacd8e8ec90958e781cc5c8f6e
Ben Skeggs <bskeggs@redhat.com>
2020-12-04 11:23:38 +1000

    drm/nouveau/gsp: switch to instanced constructor
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>
    Reviewed-by: Lyude Paul <lyude@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/core/device.h
+++ drivers/gpu/drm/nouveau/include/nvkm/core/device.h
@@ -61,7 +61,6 @@ struct nvkm_device {
 		struct notifier_block nb;
 	} acpi;
 
-	struct nvkm_gsp *gsp;
 	struct nvkm_i2c *i2c;
 	struct nvkm_subdev *ibus;
 	struct nvkm_iccsense *iccsense;
@@ -139,7 +138,6 @@ struct nvkm_device_chip {
 #include <core/layout.h>
 #undef NVKM_LAYOUT_INST
 #undef NVKM_LAYOUT_ONCE
-	int (*gsp     )(struct nvkm_device *, int idx, struct nvkm_gsp **);
 	int (*i2c     )(struct nvkm_device *, int idx, struct nvkm_i2c **);
 	int (*ibus    )(struct nvkm_device *, int idx, struct nvkm_subdev **);
 	int (*iccsense)(struct nvkm_device *, int idx, struct nvkm_iccsense **);
--- drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
@@ -2428,7 +2428,7 @@ nv140_chipset = {
 	.fb       = { 0x00000001, gv100_fb_new },
 	.fuse     = { 0x00000001, gm107_fuse_new },
 	.gpio     = { 0x00000001, gk104_gpio_new },
-	.gsp = gv100_gsp_new,
+	.gsp      = { 0x00000001, gv100_gsp_new },
 	.i2c = gm200_i2c_new,
 	.ibus = gm200_ibus_new,
 	.imem = nv50_instmem_new,
@@ -2472,7 +2472,7 @@ nv162_chipset = {
 	.fb       = { 0x00000001, gv100_fb_new },
 	.fuse     = { 0x00000001, gm107_fuse_new },
 	.gpio     = { 0x00000001, gk104_gpio_new },
-	.gsp = gv100_gsp_new,
+	.gsp      = { 0x00000001, gv100_gsp_new },
 	.i2c = gm200_i2c_new,
 	.ibus = gm200_ibus_new,
 	.imem = nv50_instmem_new,
@@ -2510,7 +2510,7 @@ nv164_chipset = {
 	.fb       = { 0x00000001, gv100_fb_new },
 	.fuse     = { 0x00000001, gm107_fuse_new },
 	.gpio     = { 0x00000001, gk104_gpio_new },
-	.gsp = gv100_gsp_new,
+	.gsp      = { 0x00000001, gv100_gsp_new },
 	.i2c = gm200_i2c_new,
 	.ibus = gm200_ibus_new,
 	.imem = nv50_instmem_new,
@@ -2549,7 +2549,7 @@ nv166_chipset = {
 	.fb       = { 0x00000001, gv100_fb_new },
 	.fuse     = { 0x00000001, gm107_fuse_new },
 	.gpio     = { 0x00000001, gk104_gpio_new },
-	.gsp = gv100_gsp_new,
+	.gsp      = { 0x00000001, gv100_gsp_new },
 	.i2c = gm200_i2c_new,
 	.ibus = gm200_ibus_new,
 	.imem = nv50_instmem_new,
@@ -2589,7 +2589,7 @@ nv167_chipset = {
 	.fb       = { 0x00000001, gv100_fb_new },
 	.fuse     = { 0x00000001, gm107_fuse_new },
 	.gpio     = { 0x00000001, gk104_gpio_new },
-	.gsp = gv100_gsp_new,
+	.gsp      = { 0x00000001, gv100_gsp_new },
 	.i2c = gm200_i2c_new,
 	.ibus = gm200_ibus_new,
 	.imem = nv50_instmem_new,
@@ -2627,7 +2627,7 @@ nv168_chipset = {
 	.fb       = { 0x00000001, gv100_fb_new },
 	.fuse     = { 0x00000001, gm107_fuse_new },
 	.gpio     = { 0x00000001, gk104_gpio_new },
-	.gsp = gv100_gsp_new,
+	.gsp      = { 0x00000001, gv100_gsp_new },
 	.i2c = gm200_i2c_new,
 	.ibus = gm200_ibus_new,
 	.imem = nv50_instmem_new,
@@ -3249,7 +3249,6 @@ nvkm_device_ctor(const struct nvkm_device_func *func,
 #include <core/layout.h>
 #undef NVKM_LAYOUT_INST
 #undef NVKM_LAYOUT_ONCE
-		_(NVKM_SUBDEV_GSP     ,      gsp);
 		_(NVKM_SUBDEV_I2C     ,      i2c);
 		_(NVKM_SUBDEV_IBUS    ,     ibus);
 		_(NVKM_SUBDEV_ICCSENSE, iccsense);
