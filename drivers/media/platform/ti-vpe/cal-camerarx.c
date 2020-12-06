0f031881e5b9cf6f6ae4f8728ea9250f384c5416
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2020-12-07 00:53:39 +0100

    media: ti-vpe: cal: Rename struct cal_fmt to cal_format_info
    The cal_fmt structure stores information about a format. Its name is
    ambiguous, as it could be understood as storing a format. Furthermore,
    instances of the structure are called 'fmt' through the code, which
    leads to confusion with the 'format' variables. Rename the structure to
    cal_format_info, and the corresponding 'fmt' variables to 'fmtinfo'.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Benoit Parrot <bparrot@ti.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/platform/ti-vpe/cal-camerarx.c
+++ drivers/media/platform/ti-vpe/cal-camerarx.c
@@ -668,7 +668,7 @@ static int cal_camerarx_sd_enum_frame_size(struct v4l2_subdev *sd,
 					   struct v4l2_subdev_frame_size_enum *fse)
 {
 	struct cal_camerarx *phy = to_cal_camerarx(sd);
-	const struct cal_fmt *fmtinfo;
+	const struct cal_format_info *fmtinfo;
 
 	if (fse->index > 0)
 		return -EINVAL;
@@ -721,7 +721,7 @@ static int cal_camerarx_sd_set_fmt(struct v4l2_subdev *sd,
 				   struct v4l2_subdev_format *format)
 {
 	struct cal_camerarx *phy = to_cal_camerarx(sd);
-	const struct cal_fmt *fmtinfo;
+	const struct cal_format_info *fmtinfo;
 	struct v4l2_mbus_framefmt *fmt;
 	unsigned int bpp;
 
