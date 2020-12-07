bc641b12053a367e9677ccc16ea8792352152032
Nathan Lynch <nathanl@linux.ibm.com>
2020-12-07 15:51:35 -0600

    powerpc/rtas: rtas_ibm_suspend_me -> rtas_ibm_suspend_me_unsafe
    The pseries partition suspend sequence requires that all active CPUs
    call H_JOIN, which suspends all but one of them with interrupts
    disabled. The "chosen" CPU is then to call ibm,suspend-me to complete
    the suspend. Upon returning from ibm,suspend-me, the chosen CPU is to
    use H_PROD to wake the joined CPUs.
    
    Using on_each_cpu() for this, as rtas_ibm_suspend_me() does to
    implement partition migration, is susceptible to deadlock with other
    users of on_each_cpu() and with users of stop_machine APIs. The
    callback passed to on_each_cpu() is not allowed to synchronize with
    other CPUs in the way it is used here.
    
    Complicating the fix is the fact that rtas_ibm_suspend_me() also
    occupies the function name that should be used to provide a more
    conventional wrapper for ibm,suspend-me. Rename rtas_ibm_suspend_me()
    to rtas_ibm_suspend_me_unsafe() to free up the name and indicate that
    it should not gain users.
    
    Signed-off-by: Nathan Lynch <nathanl@linux.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20201207215200.1785968-4-nathanl@linux.ibm.com

--- arch/powerpc/kernel/rtas.c
+++ arch/powerpc/kernel/rtas.c
@@ -844,7 +844,7 @@ static void rtas_percpu_suspend_me(void *info)
 	__rtas_suspend_cpu((struct rtas_suspend_me_data *)info, 1);
 }
 
-int rtas_ibm_suspend_me(u64 handle)
+int rtas_ibm_suspend_me_unsafe(u64 handle)
 {
 	long state;
 	long rc;
@@ -950,7 +950,7 @@ int rtas_call_reentrant(int token, int nargs, int nret, int *outputs, ...)
 }
 
 #else /* CONFIG_PPC_PSERIES */
-int rtas_ibm_suspend_me(u64 handle)
+int rtas_ibm_suspend_me_unsafe(u64 handle)
 {
 	return -ENOSYS;
 }
@@ -1186,7 +1186,7 @@ SYSCALL_DEFINE1(rtas, struct rtas_args __user *, uargs)
 		int rc = 0;
 		u64 handle = ((u64)be32_to_cpu(args.args[0]) << 32)
 		              | be32_to_cpu(args.args[1]);
-		rc = rtas_ibm_suspend_me(handle);
+		rc = rtas_ibm_suspend_me_unsafe(handle);
 		if (rc == -EAGAIN)
 			args.rets[0] = cpu_to_be32(RTAS_NOT_SUSPENDABLE);
 		else if (rc == -EIO)
