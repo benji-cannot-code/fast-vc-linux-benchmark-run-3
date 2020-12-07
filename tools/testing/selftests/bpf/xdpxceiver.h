477ed15fb4996b26abcab873c165f3f27edfb6f0
Weqaar Janjua <weqaar.janjua@gmail.com>
2020-12-07 21:53:32 +0000

    selftests/bpf: Xsk selftests - Socket Teardown - SKB, DRV
    Adds following tests:
    
    1. AF_XDP SKB mode
       c. Socket Teardown
          Create a Tx and a Rx socket, Tx from one socket, Rx on another.
          Destroy both sockets, then repeat multiple times. Only nopoll mode
          is used
    
    2. AF_XDP DRV/Native mode
       c. Socket Teardown
       * Only copy mode is supported because veth does not currently support
         zero-copy mode
    
    Signed-off-by: Weqaar Janjua <weqaar.a.janjua@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Tested-by: Yonghong Song <yhs@fb.com>
    Acked-by: Björn Töpel <bjorn.topel@intel.com>
    Link: https://lore.kernel.org/bpf/20201207215333.11586-5-weqaar.a.janjua@intel.com

--- tools/testing/selftests/bpf/xdpxceiver.h
+++ tools/testing/selftests/bpf/xdpxceiver.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAX_INTERFACE_NAME_CHARS 7
 #define MAX_INTERFACES_NAMESPACE_CHARS 10
 #define MAX_SOCKS 1
+#define MAX_TEARDOWN_ITER 10
 #define PKT_HDR_SIZE (sizeof(struct ethhdr) + sizeof(struct iphdr) + \
 			sizeof(struct udphdr))
 #define MIN_PKT_SIZE 64
@@ -58,6 +59,7 @@ static u32 opt_xdp_flags = XDP_FLAGS_UPDATE_IF_NOEXIST;
 static int opt_queue;
 static int opt_pkt_count;
 static int opt_poll;
+static int opt_teardown;
 static u32 opt_xdp_bind_flags = XDP_USE_NEED_WAKEUP;
 static u8 pkt_data[XSK_UMEM__DEFAULT_FRAME_SIZE];
 static u32 pkt_counter;
