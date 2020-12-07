fdb7f7f8cf310659f9cb4545460ffbf85f4e0c7b
Colin Ian King <colin.king@canonical.com>
2020-12-07 14:24:10 +0000

    hwmon: (ltc2992) Fix less than zero comparisons with an unsigned integer
    There are several occurrances of a less than zero error check on
    a u32 unsigned integer. These will never be true. Fix this by making
    reg_value a plain int.
    
    Addresses-Coverity: ("Unsigned comparison against zero")
    Fixes: e126370240e0 ("hwmon: (ltc2992) Add support")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Link: https://lore.kernel.org/r/20201207142410.168987-1-colin.king@canonical.com
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- drivers/hwmon/ltc2992.c
+++ drivers/hwmon/ltc2992.c
@@ -481,7 +481,7 @@ static int ltc2992_read_gpios_in(struct device *dev, u32 attr, int nr_gpio, long
 
 static int ltc2992_read_in_alarm(struct ltc2992_state *st, int channel, long *val, u32 attr)
 {
-	u32 reg_val;
+	int reg_val;
 	u32 mask;
 
 	if (attr == hwmon_in_max_alarm)
@@ -535,7 +535,7 @@ static int ltc2992_read_in(struct device *dev, u32 attr, int channel, long *val)
 
 static int ltc2992_get_current(struct ltc2992_state *st, u32 reg, u32 channel, long *val)
 {
-	u32 reg_val;
+	int reg_val;
 
 	reg_val = ltc2992_read_reg(st, reg, 2);
 	if (reg_val < 0)
@@ -559,7 +559,7 @@ static int ltc2992_set_current(struct ltc2992_state *st, u32 reg, u32 channel, l
 
 static int ltc2992_read_curr_alarm(struct ltc2992_state *st, int channel, long *val, u32 attr)
 {
-	u32 reg_val;
+	int reg_val;
 	u32 mask;
 
 	if (attr == hwmon_curr_max_alarm)
@@ -610,7 +610,7 @@ static int ltc2992_read_curr(struct device *dev, u32 attr, int channel, long *va
 
 static int ltc2992_get_power(struct ltc2992_state *st, u32 reg, u32 channel, long *val)
 {
-	u32 reg_val;
+	int reg_val;
 
 	reg_val = ltc2992_read_reg(st, reg, 3);
 	if (reg_val < 0)
@@ -634,7 +634,7 @@ static int ltc2992_set_power(struct ltc2992_state *st, u32 reg, u32 channel, lon
 
 static int ltc2992_read_power_alarm(struct ltc2992_state *st, int channel, long *val, u32 attr)
 {
-	u32 reg_val;
+	int reg_val;
 	u32 mask;
 
 	if (attr == hwmon_power_max_alarm)
