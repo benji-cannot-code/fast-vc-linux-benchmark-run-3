d1417345acf4d893791bc074ddf563870d8b2774
David E. Box <david.e.box@linux.intel.com>
2020-12-07 14:39:50 -0800

    PCI/PTM: Save/restore Precision Time Measurement Capability for suspend/resume
    The PCI subsystem does not currently save and restore the configuration
    space for the Precision Time Measurement (PTM) Extended Capability leading
    to the possibility of the feature returning disabled on S3 resume.  This
    has been observed on Intel Coffee Lake desktops. Add save/restore of the
    PTM control register. This saves the PTM Enable, Root Select, and Effective
    Granularity bits.
    
    Suggested-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://lore.kernel.org/r/20201207223951.19667-1-david.e.box@linux.intel.com
    Signed-off-by: David E. Box <david.e.box@linux.intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -1567,6 +1567,7 @@ int pci_save_state(struct pci_dev *dev)
 	pci_save_ltr_state(dev);
 	pci_save_dpc_state(dev);
 	pci_save_aer_state(dev);
+	pci_save_ptm_state(dev);
 	return pci_save_vc_state(dev);
 }
 EXPORT_SYMBOL(pci_save_state);
@@ -1678,6 +1679,7 @@ void pci_restore_state(struct pci_dev *dev)
 	pci_restore_vc_state(dev);
 	pci_restore_rebar_state(dev);
 	pci_restore_dpc_state(dev);
+	pci_restore_ptm_state(dev);
 
 	pci_aer_clear_status(dev);
 	pci_restore_aer_state(dev);
