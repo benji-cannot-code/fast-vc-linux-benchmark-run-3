a9e2251a477906abdb5db3d6108b6da12957a065
Randy Dunlap <rdunlap@infradead.org>
2020-12-08 11:55:47 -0800

    usb: cdns3: fix warning when USB_CDNS_HOST is not set
    Fix a build warning when USB_CDNS_HOST is not enabled:
    
    In file included from ../drivers/usb/cdns3/core.c:23:0:
    ../drivers/usb/cdns3/host-export.h:27:51: warning: ‘struct usb_hcd’ declared inside parameter list will not be visible outside of this definition or declaration
     static inline int xhci_cdns3_suspend_quirk(struct usb_hcd *hcd)
                                                       ^~~~~~~
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Cc: Peter Chen <peter.chen@nxp.com>
    Cc: Pawel Laszczak <pawell@cadence.com>
    Cc: Roger Quadros <rogerq@ti.com>
    Cc: linux-usb@vger.kernel.org
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Peter Chen <peter.chen@nxp.com>

--- drivers/usb/cdns3/host-export.h
+++ drivers/usb/cdns3/host-export.h
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_CDNS3_HOST_EXPORT
 #define __LINUX_CDNS3_HOST_EXPORT
 
-#if IS_ENABLED(CONFIG_USB_CDNS_HOST)
-
 struct usb_hcd;
+
+#if IS_ENABLED(CONFIG_USB_CDNS_HOST)
 
 int cdns_host_init(struct cdns *cdns);
 int xhci_cdns3_suspend_quirk(struct usb_hcd *hcd);
