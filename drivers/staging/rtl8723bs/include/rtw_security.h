821b52b152db89a97bf03ec64dd24db5c2639d0a
Ross Schmidt <ross.schm.dev@gmail.com>
2020-12-07 22:07:14 -0600

    staging: rtl8723bs: use WLAN_EID_VENDOR_SPECIFIC
    Replace unique WLAN_EID_GENERIC, _WPA_IE_ID_, _SSN_IE_1_, and
    _VENDOR_SPECIFIC_IE_ macros with kernel provided WLAN_EID_VENDOR_SPECIFIC
    from linux/ieee80211.h.
    
    Signed-off-by: Ross Schmidt <ross.schm.dev@gmail.com>
    Link: https://lore.kernel.org/r/20201208040733.379197-3-ross.schm.dev@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/include/rtw_security.h
+++ drivers/staging/rtl8723bs/include/rtw_security.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 const char *security_type_str(u8 value);
 
-#define _WPA_IE_ID_	0xdd
 #define _WPA2_IE_ID_	0x30
 
 #define SHA256_MAC_LEN 32
