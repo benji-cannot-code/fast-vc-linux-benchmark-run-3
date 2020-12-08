05df2ed2d4c4cf1404ccef3f26cb8d3ec9e7d549
Jani Nikula <jani.nikula@intel.com>
2020-12-08 12:29:11 +0200

    drm/i915/gvt: make mmio_context.h self-contained
    Add necessary #includes and forward declarations to make the header
    compile on its own.
    
    Reviewed-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Zhenyu Wang <zhenyuw@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/979ba0ea394e9589f7332b94e91ac200faf995c5.1607422863.git.jani.nikula@intel.com

--- drivers/gpu/drm/i915/gvt/mmio_context.h
+++ drivers/gpu/drm/i915/gvt/mmio_context.h
@@ -37,6 +37,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __GVT_RENDER_H__
 #define __GVT_RENDER_H__
 
+#include <linux/types.h>
+
+#include "gt/intel_engine_types.h"
+#include "i915_reg.h"
+
+struct i915_request;
+struct intel_context;
+struct intel_engine_cs;
+struct intel_gvt;
+struct intel_vgpu;
+
 struct engine_mmio {
 	enum intel_engine_id id;
 	i915_reg_t reg;
