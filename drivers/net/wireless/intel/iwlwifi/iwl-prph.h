99dd23f0ee0f7363dfee419fccb1f8aab9f3d95b
Johannes Berg <johannes.berg@intel.com>
2020-12-09 23:16:30 +0200

    iwlwifi: mvm: fix 22000 series driver NMI
    For triggering an NMI in the firmware, we should only set BIT(24)
    in the corresponding register, not the entire mask that's usable
    by the driver.
    
    This currently doesn't matter because the firmware only enables
    BIT(24), but we'll start using BIT(25) for other purposes with an
    upcoming API change.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Link: https://lore.kernel.org/r/iwlwifi.20201209231352.2f982365d085.Id09daabfd331ba9e120abcbbedd2ad6448902ed0@changeid
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/iwl-prph.h
+++ drivers/net/wireless/intel/iwlwifi/iwl-prph.h
@@ -110,7 +110,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DEVICE_SET_NMI_VAL_DRV BIT(7)
 /* Device NMI register and value for 9000 family and above hw's */
 #define UREG_NIC_SET_NMI_DRIVER 0x00a05c10
-#define UREG_NIC_SET_NMI_DRIVER_NMI_FROM_DRIVER_MSK 0xff000000
+#define UREG_NIC_SET_NMI_DRIVER_NMI_FROM_DRIVER BIT(24)
 
 /* Shared registers (0x0..0x3ff, via target indirect or periphery */
 #define SHR_BASE	0x00a10000
