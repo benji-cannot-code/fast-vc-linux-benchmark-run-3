c19e5348d7d6824aba3b459c7fe4a2623ab230f0
Nirmoy Das <nirmoy.das@amd.com>
2020-12-09 12:51:32 +0100

    drm/amdgpu: clean up bo in vce and vcn test
    BO created with amdgpu_bo_create_reserved() wasn't clean
    properly before, which causes:
    
    [   21.056218] WARNING: CPU: 0 PID: 7 at drivers/gpu/drm/ttm/ttm_bo.c:518 ttm_bo_release+0x2bf/0x310 [ttm]
    <snip>
    [   21.056430] Call Trace:
    [   21.056525]  amdgpu_bo_unref+0x1a/0x30 [amdgpu]
    [   21.056635]  amdgpu_vcn_dec_send_msg+0x1b2/0x270 [amdgpu]
    [   21.056740]  amdgpu_vcn_dec_get_create_msg.constprop.0+0xd8/0x100 [amdgpu]
    [   21.056843]  amdgpu_vcn_dec_ring_test_ib+0x27/0x180 [amdgpu]
    [   21.056936]  amdgpu_ib_ring_tests+0xf1/0x150 [amdgpu]
    [   21.057024]  amdgpu_device_delayed_init_work_handler+0x11/0x30 [amdgpu]
    [   21.057030]  process_one_work+0x1df/0x370
    [   21.057033]  worker_thread+0x46/0x340
    [   21.057034]  ? process_one_work+0x370/0x370
    [   21.057037]  kthread+0x11b/0x140
    [   21.057039]  ? __kthread_bind_mask+0x60/0x60
    [   21.057043]  ret_from_fork+0x22/0x30
    
    Signed-off-by: Nirmoy Das <nirmoy.das@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Link: https://patchwork.freedesktop.org/patch/406382/
    Signed-off-by: Christian König <christian.koenig@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_vcn.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_vcn.c
@@ -466,6 +466,7 @@ static int amdgpu_vcn_dec_send_msg(struct amdgpu_ring *ring,
 	struct amdgpu_job *job;
 	struct amdgpu_ib *ib;
 	uint64_t addr;
+	void *msg = NULL;
 	int i, r;
 
 	r = amdgpu_job_alloc_with_ib(adev, 64,
@@ -475,6 +476,7 @@ static int amdgpu_vcn_dec_send_msg(struct amdgpu_ring *ring,
 
 	ib = &job->ibs[0];
 	addr = amdgpu_bo_gpu_offset(bo);
+	msg = amdgpu_bo_kptr(bo);
 	ib->ptr[0] = PACKET0(adev->vcn.internal.data0, 0);
 	ib->ptr[1] = addr;
 	ib->ptr[2] = PACKET0(adev->vcn.internal.data1, 0);
@@ -493,7 +495,7 @@ static int amdgpu_vcn_dec_send_msg(struct amdgpu_ring *ring,
 
 	amdgpu_bo_fence(bo, f, false);
 	amdgpu_bo_unreserve(bo);
-	amdgpu_bo_unref(&bo);
+	amdgpu_bo_free_kernel(&bo, NULL, (void **)&msg);
 
 	if (fence)
 		*fence = dma_fence_get(f);
@@ -506,7 +508,7 @@ err_free:
 
 err:
 	amdgpu_bo_unreserve(bo);
-	amdgpu_bo_unref(&bo);
+	amdgpu_bo_free_kernel(&bo, NULL, (void **)&msg);
 	return r;
 }
 
@@ -762,6 +764,7 @@ int amdgpu_vcn_enc_ring_test_ib(struct amdgpu_ring *ring, long timeout)
 error:
 	dma_fence_put(fence);
 	amdgpu_bo_unreserve(bo);
-	amdgpu_bo_unref(&bo);
+	amdgpu_bo_free_kernel(&bo, NULL, NULL);
+
 	return r;
 }
