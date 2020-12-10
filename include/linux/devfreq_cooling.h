076c66119d3a53a5af78ac4c3e6088bd98b12e23
Lukasz Luba <lukasz.luba@arm.com>
2020-12-10 14:30:12 +0000

    thermal: devfreq_cooling: add new registration functions with Energy Model
    The Energy Model (EM) framework supports devices such as Devfreq. Create
    new registration function which automatically register EM for the thermal
    devfreq_cooling devices. This patch prepares the code for coming changes
    which are going to replace old power model with the new EM.
    
    Reviewed-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Signed-off-by: Lukasz Luba <lukasz.luba@arm.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/20201210143014.24685-4-lukasz.luba@arm.com

--- include/linux/devfreq_cooling.h
+++ include/linux/devfreq_cooling.h
@@ -66,6 +66,9 @@ struct thermal_cooling_device *
 of_devfreq_cooling_register(struct device_node *np, struct devfreq *df);
 struct thermal_cooling_device *devfreq_cooling_register(struct devfreq *df);
 void devfreq_cooling_unregister(struct thermal_cooling_device *dfc);
+struct thermal_cooling_device *
+devfreq_cooling_em_register(struct devfreq *df,
+			    struct devfreq_cooling_power *dfc_power);
 
 #else /* !CONFIG_DEVFREQ_THERMAL */
 
@@ -84,6 +87,13 @@ of_devfreq_cooling_register(struct device_node *np, struct devfreq *df)
 
 static inline struct thermal_cooling_device *
 devfreq_cooling_register(struct devfreq *df)
+{
+	return ERR_PTR(-EINVAL);
+}
+
+static inline struct thermal_cooling_device *
+devfreq_cooling_em_register(struct devfreq *df,
+			    struct devfreq_cooling_power *dfc_power)
 {
 	return ERR_PTR(-EINVAL);
 }
