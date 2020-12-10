f0c40d687c3ceac24119f2d73097b6b58ef42c52
Tom Lendacky <thomas.lendacky@amd.com>
2020-12-10 11:09:36 -0600

    x86/cpu: Add VM page flush MSR availablility as a CPUID feature
    On systems that do not have hardware enforced cache coherency between
    encrypted and unencrypted mappings of the same physical page, the
    hypervisor can use the VM page flush MSR (0xc001011e) to flush the cache
    contents of an SEV guest page. When a small number of pages are being
    flushed, this can be used in place of issuing a WBINVD across all CPUs.
    
    CPUID 0x8000001f_eax[2] is used to determine if the VM page flush MSR is
    available. Add a CPUID feature to indicate it is supported and define the
    MSR.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Message-Id: <f1966379e31f9b208db5257509c4a089a87d33d0.1607620209.git.thomas.lendacky@amd.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -238,6 +238,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define X86_FEATURE_VMCALL		( 8*32+18) /* "" Hypervisor supports the VMCALL instruction */
 #define X86_FEATURE_VMW_VMMCALL		( 8*32+19) /* "" VMware prefers VMMCALL hypercall instruction */
 #define X86_FEATURE_SEV_ES		( 8*32+20) /* AMD Secure Encrypted Virtualization - Encrypted State */
+#define X86_FEATURE_VM_PAGE_FLUSH	( 8*32+21) /* "" VM Page Flush MSR is supported */
 
 /* Intel-defined CPU features, CPUID level 0x00000007:0 (EBX), word 9 */
 #define X86_FEATURE_FSGSBASE		( 9*32+ 0) /* RDFSBASE, WRFSBASE, RDGSBASE, WRGSBASE instructions*/
