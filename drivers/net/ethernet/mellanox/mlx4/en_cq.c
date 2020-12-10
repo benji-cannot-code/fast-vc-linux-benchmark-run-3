92a011b4eb6a223d04365d15e48cae8bc9507c44
Thomas Gleixner <tglx@linutronix.de>
2020-12-10 20:25:56 +0100

    net/mlx4: Replace irq_to_desc() abuse
    No driver has any business with the internals of an interrupt
    descriptor. Storing a pointer to it just to use yet another helper at the
    actual usage site to retrieve the affinity mask is creative at best. Just
    because C does not allow encapsulation does not mean that the kernel has no
    limits.
    
    Retrieve a pointer to the affinity mask itself and use that. It's still
    using an interface which is usually not for random drivers, but definitely
    less hideous than the previous hack.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tariq Toukan <tariqt@nvidia.com>
    Link: https://lore.kernel.org/r/20201210194044.580936243@linutronix.de

--- drivers/net/ethernet/mellanox/mlx4/en_cq.c
+++ drivers/net/ethernet/mellanox/mlx4/en_cq.c
@@ -91,7 +91,7 @@ int mlx4_en_activate_cq(struct mlx4_en_priv *priv, struct mlx4_en_cq *cq,
 			int cq_idx)
 {
 	struct mlx4_en_dev *mdev = priv->mdev;
-	int err = 0;
+	int irq, err = 0;
 	int timestamp_en = 0;
 	bool assigned_eq = false;
 
@@ -117,10 +117,8 @@ int mlx4_en_activate_cq(struct mlx4_en_priv *priv, struct mlx4_en_cq *cq,
 
 			assigned_eq = true;
 		}
-
-		cq->irq_desc =
-			irq_to_desc(mlx4_eq_get_irq(mdev->dev,
-						    cq->vector));
+		irq = mlx4_eq_get_irq(mdev->dev, cq->vector);
+		cq->aff_mask = irq_get_affinity_mask(irq);
 	} else {
 		/* For TX we use the same irq per
 		ring we assigned for the RX    */
--- drivers/net/ethernet/mellanox/mlx4/mlx4_en.h
+++ drivers/net/ethernet/mellanox/mlx4/mlx4_en.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 #include <linux/cpu_rmap.h>
 #include <linux/ptp_clock_kernel.h>
+#include <linux/irq.h>
 #include <net/xdp.h>
 
 #include <linux/mlx4/device.h>
@@ -381,7 +382,7 @@ struct mlx4_en_cq {
 	struct mlx4_cqe *buf;
 #define MLX4_EN_OPCODE_ERROR	0x1e
 
-	struct irq_desc *irq_desc;
+	const struct cpumask *aff_mask;
 };
 
 struct mlx4_en_port_profile {
