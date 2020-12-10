47e146ae625fbe4046e8fb420f009882a5a7c3f1
Sasha Neftin <sasha.neftin@intel.com>
2020-12-10 08:42:09 +0200

    igc: Expose the NVM version
    Expose the NVM map version via drvinfo in ethtool
    NVM image version is reported as firmware version for i225 device
    Minor typo fix - remove space
    
    Signed-off-by: Sasha Neftin <sasha.neftin@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>

--- drivers/net/ethernet/intel/igc/igc_defines.h
+++ drivers/net/ethernet/intel/igc/igc_defines.h
@@ -161,6 +161,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IGC_NVM_RW_REG_START	1    /* Start operation */
 #define IGC_NVM_RW_ADDR_SHIFT	2    /* Shift to the address bits */
 #define IGC_NVM_POLL_READ	0    /* Flag for polling for read complete */
+#define IGC_NVM_DEV_STARTER	5    /* Dev_starter Version */
 
 /* NVM Word Offsets */
 #define NVM_CHECKSUM_REG		0x003F
