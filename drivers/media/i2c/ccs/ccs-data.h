9ad649f35e3b844d748dc9cd27198f3fb24a768f
Sakari Ailus <sakari.ailus@linux.intel.com>
2020-12-11 12:30:09 +0100

    media: ccs: Small definition cleanup
    struct device is used in ccs-data.h but no definition is included. Add a
    forward definition.
    
    Also ccs-data.c includes linux/types.h but this is already included in
    ccs-data.h which ccs-data.c includes. Do don't include linux/types.h in
    ccs-data.c.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/i2c/ccs/ccs-data.h
+++ drivers/media/i2c/ccs/ccs-data.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
+struct device;
+
 /**
  * struct ccs_data_block_version - CCS static data version
  * @version_major: Major version number
