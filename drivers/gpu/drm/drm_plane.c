3127c9bd68d775fc4802724853dbb948d2e1e90b
Simon Ser <contact@emersion.fr>
2020-12-11 19:46:34 +0100

    drm: require each CRTC to have a unique primary plane
    User-space expects to be able to pick a primary plane for each CRTC
    exposed by the driver. Make sure this assumption holds in
    drm_mode_config_validate.
    
    Use the legacy drm_crtc.primary field to check this, because it's
    simpler and we require drivers to set it anyways. Accumulate a set of
    primary planes which are already used for a CRTC in a bitmask. Error out
    if a primary plane is re-used.
    
    v2: new patch
    
    v3:
    - Use u64 instead of __u64 (Jani)
    - Use `unsigned int` instead of `unsigned` (Jani)
    
    v4:
    - Use u32 instead of u64 for plane mask (Ville)
    - Use drm_plane_mask instead of BIT (Ville)
    - Fix typos (Ville)
    
    Signed-off-by: Simon Ser <contact@emersion.fr>
    Reviewed-by: Daniel Vetter <daniel@ffwll.ch>
    Acked-by: Ville Syrjala <ville.syrjala@linux.intel.com>
    Cc: Pekka Paalanen <ppaalanen@gmail.com>
    Cc: Jani Nikula <jani.nikula@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201211184634.74534-4-contact@emersion.fr

--- drivers/gpu/drm/drm_plane.c
+++ drivers/gpu/drm/drm_plane.c
@@ -55,6 +55,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * enum drm_plane_type). A plane can be compatible with multiple CRTCs, see
  * &drm_plane.possible_crtcs.
  *
+ * Each CRTC must have a unique primary plane userspace can attach to enable
+ * the CRTC. In other words, userspace must be able to attach a different
+ * primary plane to each CRTC at the same time. Primary planes can still be
+ * compatible with multiple CRTCs. There must be exactly as many primary planes
+ * as there are CRTCs.
+ *
  * Legacy uAPI doesn't expose the primary and cursor planes directly. DRM core
  * relies on the driver to set the primary and optionally the cursor plane used
  * for legacy IOCTLs. This is done by calling drm_crtc_init_with_planes(). All
