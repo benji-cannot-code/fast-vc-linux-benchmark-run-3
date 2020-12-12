147f081462c8a200323404d3cb95e3cd4be578d7
Parav Pandit <parav@nvidia.com>
2020-12-11 22:12:21 -0800

    net/mlx5: SF, Add port add delete functionality
    To handle SF port management outside of the eswitch as independent
    software layer, introduce eswitch notifier APIs so that mlx5 upper
    layer who wish to support sf port management in switchdev mode can
    perform its task whenever eswitch mode is set to switchdev or before
    eswitch is disabled.
    
    Initialize sf port table on such eswitch event.
    
    Add SF port add and delete functionality in switchdev mode.
    Destroy all SF ports when eswitch is disabled.
    Expose SF port add and delete to user via devlink commands.
    
    $ devlink dev eswitch set pci/0000:06:00.0 mode switchdev
    
    $ devlink port show
    pci/0000:06:00.0/65535: type eth netdev ens2f0np0 flavour physical port 0 splittable false
    
    $ devlink port add pci/0000:06:00.0 flavour pcisf pfnum 0 sfnum 88
    pci/0000:06:00.0/32768: type eth netdev eth6 flavour pcisf controller 0 pfnum 0 sfnum 88 external false splittable false
      function:
        hw_addr 00:00:00:00:00:00 state inactive opstate detached
    
    $ devlink port show ens2f0npf0sf88
    pci/0000:06:00.0/32768: type eth netdev ens2f0npf0sf88 flavour pcisf controller 0 pfnum 0 sfnum 88 external false splittable false
      function:
        hw_addr 00:00:00:00:00:00 state inactive opstate detached
    
    or by its unique port index:
    $ devlink port show pci/0000:06:00.0/32768
    pci/0000:06:00.0/32768: type eth netdev ens2f0npf0sf88 flavour pcisf controller 0 pfnum 0 sfnum 88 external false splittable false
      function:
        hw_addr 00:00:00:00:00:00 state inactive opstate detached
    
    $ devlink port show ens2f0npf0sf88 -jp
    {
        "port": {
            "pci/0000:06:00.0/32768": {
                "type": "eth",
                "netdev": "ens2f0npf0sf88",
                "flavour": "pcisf",
                "controller": 0,
                "pfnum": 0,
                "sfnum": 88,
                "external": false,
                "splittable": false,
                "function": {
                    "hw_addr": "00:00:00:00:00:00",
                    "state": "inactive",
                    "opstate": "detached"
                }
            }
        }
    }
    
    Signed-off-by: Parav Pandit <parav@nvidia.com>
    Reviewed-by: Vu Pham <vuhuong@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/devlink.c
+++ drivers/net/ethernet/mellanox/mlx5/core/devlink.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "fs_core.h"
 #include "eswitch.h"
 #include "sf/dev/dev.h"
+#include "sf/sf.h"
 
 static int mlx5_devlink_flash_update(struct devlink *devlink,
 				     struct devlink_flash_update_params *params,
@@ -191,6 +192,10 @@ static const struct devlink_ops mlx5_devlink_ops = {
 	.eswitch_encap_mode_get = mlx5_devlink_eswitch_encap_mode_get,
 	.port_function_hw_addr_get = mlx5_devlink_port_function_hw_addr_get,
 	.port_function_hw_addr_set = mlx5_devlink_port_function_hw_addr_set,
+#endif
+#ifdef CONFIG_MLX5_SF_MANAGER
+	.port_new = mlx5_devlink_sf_port_new,
+	.port_del = mlx5_devlink_sf_port_del,
 #endif
 	.flash_update = mlx5_devlink_flash_update,
 	.info_get = mlx5_devlink_info_get,
--- include/linux/mlx5/driver.h
+++ include/linux/mlx5/driver.h
@@ -511,6 +511,8 @@ struct mlx5_eq_table;
 struct mlx5_irq_table;
 struct mlx5_vhca_state_notifier;
 struct mlx5_sf_dev_table;
+struct mlx5_sf_hw_table;
+struct mlx5_sf_table;
 
 struct mlx5_rate_limit {
 	u32			rate;
@@ -611,6 +613,10 @@ struct mlx5_priv {
 	struct mlx5_vhca_state_notifier *vhca_state_notifier;
 	struct mlx5_sf_dev_table *sf_dev_table;
 	struct mlx5_core_dev *parent_mdev;
+#endif
+#ifdef CONFIG_MLX5_SF_MANAGER
+	struct mlx5_sf_hw_table *sf_hw_table;
+	struct mlx5_sf_table *sf_table;
 #endif
 };
 
