ad0c1393f724aebf496b5e2625a55b1567ebc599
Masahiro Yamada <masahiroy@kernel.org>
2020-12-13 01:54:30 +0900

    kbuild: LD_VERSION redenomination
    Commit ccbef1674a15 ("Kbuild, lto: add ld-version and ld-ifversion
    macros") introduced scripts/ld-version.sh for GCC LTO.
    
    At that time, this script handled 5 version fields because GCC LTO
    needed the downstream binutils. (https://lkml.org/lkml/2014/4/8/272)
    
    The code snippet from the submitted patch was as follows:
    
        # We need HJ Lu's Linux binutils because mainline binutils does not
        # support mixing assembler and LTO code in the same ld -r object.
        # XXX check if the gcc plugin ld is the expected one too
        # XXX some Fedora binutils should also support it. How to check for that?
        ifeq ($(call ld-ifversion,-ge,22710001,y),y)
            ...
    
    However, GCC LTO was not merged into the mainline after all.
    (https://lkml.org/lkml/2014/4/8/272)
    
    So, the 4th and 5th fields were never used, and finally removed by
    commit 0d61ed17dd30 ("ld-version: Drop the 4th and 5th version
    components").
    
    Since then, the last 4-digits returned by this script is always zeros.
    
    Remove the meaningless last 4-digits. This makes the version format
    consistent with GCC_VERSION, CLANG_VERSION, LLD_VERSION.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
    Acked-by: Will Deacon <will@kernel.org>
    Acked-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- scripts/ld-version.sh
+++ scripts/ld-version.sh
@@ -7,6 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	gsub(".*version ", "");
 	gsub("-.*", "");
 	split($1,a, ".");
-	print a[1]*100000000 + a[2]*1000000 + a[3]*10000;
+	print a[1]*10000 + a[2]*100 + a[3];
 	exit
 	}
