a9a58026dbda70f6681bcd805295c80a2fefc0f0
Daniel Lezcano <daniel.lezcano@linaro.org>
2020-12-15 00:38:09 +0100

    thermal/core: Remove unused macro THERMAL_TRIPS_NONE
    The macro THERMAL_TRIPS_NONE is no longer used, remove it.
    
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Reviewed-by: Thara Gopinath <thara.gopinath@linaro.org>
    Link: https://lore.kernel.org/r/20201214233811.485669-6-daniel.lezcano@linaro.org

--- include/linux/thermal.h
+++ include/linux/thermal.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/workqueue.h>
 #include <uapi/linux/thermal.h>
 
-#define THERMAL_TRIPS_NONE	-1
 #define THERMAL_MAX_TRIPS	12
 
 /* invalid cooling state */
