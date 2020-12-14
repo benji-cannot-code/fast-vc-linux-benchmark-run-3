c508f978310f4627f89ae4ab60b3c0a22d2dd42c
Andra Paraschiv <andraprs@amazon.com>
2020-12-14 18:11:18 +0200

    vm_sockets: Add flags field in the vsock address data structure
    vsock enables communication between virtual machines and the host they
    are running on. With the multi transport support (guest->host and
    host->guest), nested VMs can also use vsock channels for communication.
    
    In addition to this, by default, all the vsock packets are forwarded to
    the host, if no host->guest transport is loaded. This behavior can be
    implicitly used for enabling vsock communication between sibling VMs.
    
    Add a flags field in the vsock address data structure that can be used
    to explicitly mark the vsock connection as being targeted for a certain
    type of communication. This way, can distinguish between different use
    cases such as nested VMs and sibling VMs.
    
    This field can be set when initializing the vsock address variable used
    for the connect() call.
    
    Changelog
    
    v3 -> v4
    
    * Update the size of "svm_flags" field to be 1 byte instead of 2 bytes.
    
    v2 -> v3
    
    * Add "svm_flags" as a new field, not reusing "svm_reserved1".
    
    v1 -> v2
    
    * Update the field name to "svm_flags".
    * Split the current patch in 2 patches.
    
    Signed-off-by: Andra Paraschiv <andraprs@amazon.com>
    Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/uapi/linux/vm_sockets.h
+++ include/uapi/linux/vm_sockets.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _UAPI_VM_SOCKETS_H
 
 #include <linux/socket.h>
+#include <linux/types.h>
 
 /* Option name for STREAM socket buffer size.  Use as the option name in
  * setsockopt(3) or getsockopt(3) to set or get an unsigned long long that
@@ -149,10 +150,13 @@ struct sockaddr_vm {
 	unsigned short svm_reserved1;
 	unsigned int svm_port;
 	unsigned int svm_cid;
+	__u8 svm_flags;
 	unsigned char svm_zero[sizeof(struct sockaddr) -
 			       sizeof(sa_family_t) -
 			       sizeof(unsigned short) -
-			       sizeof(unsigned int) - sizeof(unsigned int)];
+			       sizeof(unsigned int) -
+			       sizeof(unsigned int) -
+			       sizeof(__u8)];
 };
 
 #define IOCTL_VM_SOCKETS_GET_LOCAL_CID		_IO(7, 0xb9)
