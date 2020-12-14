db2bb8604cc7b677302979472449fe34443f3023
Rafał Miłecki <rafal@milecki.pl>
2020-12-14 19:07:42 +0100

    dt-bindings: power: document Broadcom's PMB binding
    Broadcom's PMB is power controller used for disabling and enabling SoC
    devices.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Acked-by: Florian Fainelli <f.fainelli@gmail.com>
    Acked-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>

---
+++ include/dt-bindings/soc/bcm-pmb.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0-or-later OR MIT */
+
+#ifndef __DT_BINDINGS_SOC_BCM_PMB_H
+#define __DT_BINDINGS_SOC_BCM_PMB_H
+
+#define BCM_PMB_PCIE0				0x01
+#define BCM_PMB_PCIE1				0x02
+#define BCM_PMB_PCIE2				0x03
+#define BCM_PMB_HOST_USB			0x04
+
+#endif
