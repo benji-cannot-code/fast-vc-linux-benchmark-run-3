ab13f8c7700764b4232c5dbdabdf53f8f3ad22a6
John Hubbard <jhubbard@nvidia.com>
2020-12-14 19:05:08 -0800

    selftests/vm: use a common gup_test.h
    Avoid the need to copy-paste the gup_test ioctl commands and the struct
    gup_test definition, between the kernel and the user space application, by
    providing a new header file for these.  This allows easier and safer
    adding of new ioctl calls, as well as reducing the overall line count.
    
    Details: The header file has to be able to compile independently, because
    of the arguably unfortunate way that the Makefile is written: the Makefile
    tries to build all of its prerequisites, when really it should be only
    building the .c files, and leaving the other prerequisites (LOCAL_HDRS) as
    pure dependencies.
    
    That Makefile limitation is probably not worth fixing, but it explains why
    one of the includes had to be moved into the new header file.
    
    Also: simplify the ioctl struct (struct gup_test), by deleting the unused
    __expansion[10] field.  This sort of thing is what you might see in a
    stable ABI, but this low-level, kernel-developer-oriented selftests/vm
    system is very much not subject to ABI stability.  So "expansion" and
    "reserved" fields are unnecessary here.
    
    Link: https://lkml.kernel.org/r/20201026064021.3545418-3-jhubbard@nvidia.com
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>
    Cc: Jérôme Glisse <jglisse@redhat.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Ralph Campbell <rcampbell@nvidia.com>
    Cc: Shuah Khan <shuah@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

---
+++ mm/gup_test.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef __GUP_TEST_H
+#define __GUP_TEST_H
+
+#include <linux/types.h>
+
+#define GUP_FAST_BENCHMARK	_IOWR('g', 1, struct gup_test)
+#define GUP_BENCHMARK		_IOWR('g', 2, struct gup_test)
+#define PIN_FAST_BENCHMARK	_IOWR('g', 3, struct gup_test)
+#define PIN_BENCHMARK		_IOWR('g', 4, struct gup_test)
+#define PIN_LONGTERM_BENCHMARK	_IOWR('g', 5, struct gup_test)
+
+struct gup_test {
+	__u64 get_delta_usec;
+	__u64 put_delta_usec;
+	__u64 addr;
+	__u64 size;
+	__u32 nr_pages_per_call;
+	__u32 flags;
+};
+
+#endif	/* __GUP_TEST_H */
