1af77b0c6da136ffb0fdf1a369795ae471c5332a
Marc Kleine-Budde <mkl@pengutronix.de>
2020-12-16 00:17:45 +0100

    can: tcan4x5x: rework SPI access
    This patch reworks the SPI access and fixes several probems:
    - tcan4x5x_regmap_gather_write(), tcan4x5x_regmap_read():
      Do not place variable "addr" on stack and use it as buffer for SPI
      transfer. Buffers for SPI transfers must be allocated from DMA save
      memory.
    - tcan4x5x_regmap_gather_write(), tcan4x5x_regmap_read():
      Halfe number of SPI transfers by using a single buffer + memcpy().
      This improves the performance, especially on SPI controllers, which
      use interrupt based transfers.
    - Use "8" bits per word, not "32". This makes it possible to use this
      driver on SoCs like the Raspberry Pi, which SPI host controller
      drivers only support 8 bits per word.
    
    Note: this breaks half duplex only controllers. Support for them will be
    re-added in the next patch.
    
    Reviewed-by: Dan Murphy <dmurphy@ti.com>
    Tested-by: Sean Nyekjaer <sean@geanix.com>
    Link: https://lore.kernel.org/r/20201215231746.1132907-16-mkl@pengutronix.de
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/m_can/tcan4x5x.h
+++ drivers/net/can/m_can/tcan4x5x.h
@@ -18,6 +18,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "m_can.h"
 
+#define TCAN4X5X_SANITIZE_SPI 1
+
+struct __packed tcan4x5x_buf_cmd {
+	u8 cmd;
+	__be16 addr;
+	u8 len;
+};
+
+struct __packed tcan4x5x_map_buf {
+	struct tcan4x5x_buf_cmd cmd;
+	u8 data[256 * sizeof(u32)];
+} ____cacheline_aligned;
+
 struct tcan4x5x_priv {
 	struct m_can_classdev cdev;
 
@@ -28,7 +41,17 @@ struct tcan4x5x_priv {
 	struct gpio_desc *device_wake_gpio;
 	struct gpio_desc *device_state_gpio;
 	struct regulator *power;
+
+	struct tcan4x5x_map_buf map_buf_rx;
+	struct tcan4x5x_map_buf map_buf_tx;
 };
+
+static inline void
+tcan4x5x_spi_cmd_set_len(struct tcan4x5x_buf_cmd *cmd, u8 len)
+{
+	/* number of u32 */
+	cmd->len = len >> 2;
+}
 
 int tcan4x5x_regmap_init(struct tcan4x5x_priv *priv);
 
