c5de04ded0ba242420e09095a396401eb6778894
Tomi Valkeinen <tomi.valkeinen@ti.com>
2020-12-15 12:46:46 +0200

    drm/panel: panel-dsi-cm: drop unneeded includes
    Drop unneeded includes.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Reviewed-by: Sam Ravnborg <sam@ravnborg.org>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Sebastian Reichel <sebastian.reichel@collabora.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201215104657.802264-74-tomi.valkeinen@ti.com

--- drivers/gpu/drm/panel/panel-dsi-cm.c
+++ drivers/gpu/drm/panel/panel-dsi-cm.c
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/gpio/consumer.h>
 #include <linux/jiffies.h>
 #include <linux/module.h>
-#include <linux/sched/signal.h>
-#include <linux/slab.h>
 #include <linux/of_device.h>
 #include <linux/regulator/consumer.h>
 
