95407f0bcf84b4feee550a4b73a8ad0c258f111d
Mike Rapoport <rppt@linux.ibm.com>
2020-12-14 19:10:11 -0800

    m68k/mm: enable use of generic memory_model.h for !DISCONTIGMEM
    The pg_data_map and pg_data_table arrays as well as page_to_pfn() and
    pfn_to_page() are required only for DISCONTIGMEM. Other memory models can
    use the generic definitions in asm-generic/memory_model.h.
    
    Link: https://lkml.kernel.org/r/20201101170454.9567-13-rppt@kernel.org
    Signed-off-by: Mike Rapoport <rppt@linux.ibm.com>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Greg Ungerer <gerg@linux-m68k.org>
    Cc: John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Matt Turner <mattst88@gmail.com>
    Cc: Meelis Roos <mroos@linux.ee>
    Cc: Michael Schmitz <schmitzmic@gmail.com>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Vineet Gupta <vgupta@synopsys.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/m68k/mm/init.c
+++ arch/m68k/mm/init.c
@@ -43,12 +43,12 @@ EXPORT_SYMBOL(empty_zero_page);
 
 #ifdef CONFIG_MMU
 
-pg_data_t pg_data_map[MAX_NUMNODES];
-EXPORT_SYMBOL(pg_data_map);
-
 int m68k_virt_to_node_shift;
 
 #ifdef CONFIG_DISCONTIGMEM
+pg_data_t pg_data_map[MAX_NUMNODES];
+EXPORT_SYMBOL(pg_data_map);
+
 pg_data_t *pg_data_table[65];
 EXPORT_SYMBOL(pg_data_table);
 #endif
