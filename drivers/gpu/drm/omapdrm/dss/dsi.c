a1742b482aedff95fbc97260252d1e9304db1f22
Tomi Valkeinen <tomi.valkeinen@ti.com>
2020-12-15 12:46:49 +0200

    drm/omap: dsi: display_enable cleanup
    We can drop dsi_display_enable(), which just calls
    _dsi_display_enable(), and rename _dsi_display_enable() to
    dsi_display_enable().
    
    The WARN_ON(!dsi_bus_is_locked(dsi)) in dsi_display_enable is extra and
    can be dropped, as _dsi_display_enable() has the same WARN_ON().
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Sebastian Reichel <sebastian.reichel@collabora.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201215104657.802264-77-tomi.valkeinen@ti.com

--- drivers/gpu/drm/omapdrm/dss/dsi.c
+++ drivers/gpu/drm/omapdrm/dss/dsi.c
@@ -3741,7 +3741,7 @@ static void dsi_display_uninit_dsi(struct dsi_data *dsi, bool disconnect_lanes,
 	}
 }
 
-static void _dsi_display_enable(struct dsi_data *dsi)
+static void dsi_display_enable(struct dsi_data *dsi)
 {
 	int r;
 
@@ -3768,16 +3768,6 @@ err_init_dsi:
 err_get_dsi:
 	mutex_unlock(&dsi->lock);
 	DSSDBG("dsi_display_ulps_enable FAILED\n");
-}
-
-static void dsi_display_enable(struct omap_dss_device *dssdev)
-{
-	struct dsi_data *dsi = to_dsi_data(dssdev);
-	DSSDBG("dsi_display_enable\n");
-
-	WARN_ON(!dsi_bus_is_locked(dsi));
-
-	_dsi_display_enable(dsi);
 }
 
 static void _dsi_display_disable(struct dsi_data *dsi,
@@ -3854,7 +3844,7 @@ static void dsi_set_ulps_auto(struct dsi_data *dsi, bool enable)
 			return;
 
 		dsi_bus_lock(dsi);
-		_dsi_display_enable(dsi);
+		dsi_display_enable(dsi);
 		dsi_enable_te(dsi, true);
 		dsi_bus_unlock(dsi);
 	}
@@ -4945,7 +4935,7 @@ static void dsi_bridge_enable(struct drm_bridge *bridge)
 
 	dsi_bus_lock(dsi);
 
-	dsi_display_enable(dssdev);
+	dsi_display_enable(dsi);
 
 	dsi_enable_video_output(dssdev, VC_VIDEO);
 
