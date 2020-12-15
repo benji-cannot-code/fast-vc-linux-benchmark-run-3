d13473ebd6725add910002db3c0e6dbc4b21b68a
Jianxin Xiong <jianxin.xiong@intel.com>
2020-12-15 13:27:13 -0800

    RDMA/umem: Support importing dma-buf as user memory region
    Dma-buf is a standard cross-driver buffer sharing mechanism that can be
    used to support peer-to-peer access from RDMA devices.
    
    Device memory exported via dma-buf is associated with a file descriptor.
    This is passed to the user space as a property associated with the buffer
    allocation. When the buffer is registered as a memory region, the file
    descriptor is passed to the RDMA driver along with other parameters.
    
    Implement the common code for importing dma-buf object and mapping dma-buf
    pages.
    
    Link: https://lore.kernel.org/r/1608067636-98073-2-git-send-email-jianxin.xiong@intel.com
    Signed-off-by: Jianxin Xiong <jianxin.xiong@intel.com>
    Reviewed-by: Sean Hefty <sean.hefty@intel.com>
    Acked-by: Michael J. Ruhl <michael.j.ruhl@intel.com>
    Acked-by: Christian Koenig <christian.koenig@amd.com>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/infiniband/core/umem.c
+++ drivers/infiniband/core/umem.c
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (c) 2005 Topspin Communications.  All rights reserved.
  * Copyright (c) 2005 Cisco Systems.  All rights reserved.
  * Copyright (c) 2005 Mellanox Technologies. All rights reserved.
+ * Copyright (c) 2020 Intel Corporation. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -279,6 +280,8 @@ void ib_umem_release(struct ib_umem *umem)
 {
 	if (!umem)
 		return;
+	if (umem->is_dmabuf)
+		return ib_umem_dmabuf_release(to_ib_umem_dmabuf(umem));
 	if (umem->is_odp)
 		return ib_umem_odp_release(to_ib_umem_odp(umem));
 
