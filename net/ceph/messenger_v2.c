727d3e102d8b5766d49a1f181dac1c9d7d4f2b49
Ilya Dryomov <idryomov@gmail.com>
2020-12-15 16:40:59 +0100

    libceph: fix auth_signature buffer allocation in secure mode
    auth_signature frame is 68 bytes in plain mode and 96 bytes in
    secure mode but we are requesting 68 bytes in both modes.  By luck,
    this doesn't actually result in any invalid memory accesses because
    the allocation is satisfied out of kmalloc-96 slab and so exactly
    96 bytes are allocated, but KASAN rightfully complains.
    
    Fixes: cd1a677cad99 ("libceph, ceph: implement msgr2.1 protocol (crc and secure modes)")
    Reported-by: Luis Henriques <lhenriques@suse.de>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- net/ceph/messenger_v2.c
+++ net/ceph/messenger_v2.c
@@ -1334,7 +1334,8 @@ static int prepare_auth_signature(struct ceph_connection *con)
 	void *buf;
 	int ret;
 
-	buf = alloc_conn_buf(con, head_onwire_len(SHA256_DIGEST_SIZE, false));
+	buf = alloc_conn_buf(con, head_onwire_len(SHA256_DIGEST_SIZE,
+						  con_secure(con)));
 	if (!buf)
 		return -ENOMEM;
 
