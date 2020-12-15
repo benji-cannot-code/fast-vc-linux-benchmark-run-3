5cc27084f70d29a14a372de09123cd065aff2680
Sebastian Reichel <sebastian.reichel@collabora.com>
2020-12-15 12:46:19 +0200

    drm/omap: drop dssdev display field
    All displays are now using a drm_panel or a drm_bridge that models the
    connector instead of dssdev, so this field is always 0 and can be
    dropped.
    
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201215104657.802264-47-tomi.valkeinen@ti.com

--- drivers/gpu/drm/omapdrm/dss/base.c
+++ drivers/gpu/drm/omapdrm/dss/base.c
@@ -199,7 +199,7 @@ void omapdss_device_disconnect(struct omap_dss_device *src,
 	}
 
 	if (!dst->id && !omapdss_device_is_connected(dst)) {
-		WARN_ON(!dst->display);
+		WARN_ON(1);
 		return;
 	}
 
