0bb802539c9b4d72ea252c5fb78e843ce5ec8bd5
John Hubbard <jhubbard@nvidia.com>
2020-12-14 19:05:14 -0800

    selftests/vm: minor cleanup: Makefile and gup_test.c
    A few cleanups that don't deserve separate patches, but that also should
    not clutter up other functional changes:
    
    1. Remove an unnecessary #include <prctl.h>
    
    2. Restore the sorted order of TEST_GEN_FILES.
    
    3. Add -lpthread to the common LDLIBS, as it is harmless and several
       tests use it. This gets rid of one special rule already.
    
    Link: https://lkml.kernel.org/r/20201026064021.3545418-5-jhubbard@nvidia.com
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>
    Cc: Jérôme Glisse <jglisse@redhat.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Ralph Campbell <rcampbell@nvidia.com>
    Cc: Shuah Khan <shuah@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- tools/testing/selftests/vm/gup_test.c
+++ tools/testing/selftests/vm/gup_test.c
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
-#include <sys/prctl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include "../../../../mm/gup_test.h"
