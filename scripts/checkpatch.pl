05f47d32db9f005a8cc5d2c05dff3691e28339b8
Dwaipayan Ray <dwaipayanray1@gmail.com>
2020-12-15 20:44:56 -0800

    checkpatch: fix spelling errors and remove repeated word
    Delete repeated word in scripts/checkpatch.pl:
      "are are" -> "are"
    
    Fix typos:
      "commments" -> "comments"
      "falsly" -> "falsely"
    
    Link: https://lkml.kernel.org/r/20201113152316.62975-1-dwaipayanray1@gmail.com
    Signed-off-by: Dwaipayan Ray <dwaipayanray1@gmail.com>
    Acked-by: Joe Perches <joe@perches.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -2842,7 +2842,7 @@ sub process {
 
 
 				# stable@vger.kernel.org or stable@kernel.org shouldn't
-				# have an email name. In addition commments should strictly
+				# have an email name. In addition comments should strictly
 				# begin with a #
 				if ($email =~ /^.*stable\@(?:vger\.)?kernel\.org/i) {
 					if (($comment ne "" && $comment !~ /^#.+/) ||
@@ -5001,7 +5001,7 @@ sub process {
 ## 		    $line !~ /^.\s*$Type\s+$Ident(?:\s*=[^,{]*)?\s*,\s*$Type\s*$Ident.*/) {
 ##
 ## 			# Remove any bracketed sections to ensure we do not
-## 			# falsly report the parameters of functions.
+## 			# falsely report the parameters of functions.
 ## 			my $ln = $line;
 ## 			while ($ln =~ s/\([^\(\)]*\)//g) {
 ## 			}
@@ -7110,7 +7110,7 @@ sub process {
 		exit(0);
 	}
 
-	# This is not a patch, and we are are in 'no-patch' mode so
+	# This is not a patch, and we are in 'no-patch' mode so
 	# just keep quiet.
 	if (!$chk_patch && !$is_patch) {
 		exit(0);
