d1b8281c3a0a0c60c2a719b2a92093c92e49896e
Leah Neukirchen <leah@vuxu.org>
2020-12-16 11:03:06 +0100

    bpf: Remove unnecessary <argp.h> include from preload/iterators
    This program does not use argp (which is a glibcism). Instead include <errno.h>
    directly, which was pulled in by <argp.h>.
    
    Signed-off-by: Leah Neukirchen <leah@vuxu.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Song Liu <songliubraving@fb.com>
    Link: https://lore.kernel.org/bpf/20201216100306.30942-1-leah@vuxu.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/bpf/preload/iterators/iterators.c
+++ kernel/bpf/preload/iterators/iterators.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
 /* Copyright (c) 2020 Facebook */
-#include <argp.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
