59121823ede01d7c426cb882bce6bc6dea9af5c5
Nick Desaulniers <ndesaulniers@google.com>
2020-12-15 20:45:37 -0800

    gcov: remove support for GCC < 4.9
    Since commit 0bddd227f3dc ("Documentation: update for gcc 4.9
    requirement") the minimum supported version of GCC is gcc-4.9.  It's now
    safe to remove this code.
    
    Similar to commit 10415533a906 ("gcov: Remove old GCC 3.4 support") but
    that was for GCC 4.8 and this is for GCC 4.9.
    
    Link: https://github.com/ClangBuiltLinux/linux/issues/427
    Link: https://lkml.kernel.org/r/20201111030557.2015680-1-ndesaulniers@google.com
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Reviewed-by: Peter Oberparleiter <oberpar@linux.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/gcov/gcc_4_7.c
+++ kernel/gcov/gcc_4_7.c
@@ -26,10 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GCOV_COUNTERS			9
 #elif (__GNUC__ > 5) || (__GNUC__ == 5 && __GNUC_MINOR__ >= 1)
 #define GCOV_COUNTERS			10
-#elif __GNUC__ == 4 && __GNUC_MINOR__ >= 9
-#define GCOV_COUNTERS			9
 #else
-#define GCOV_COUNTERS			8
+#define GCOV_COUNTERS			9
 #endif
 
 #define GCOV_TAG_FUNCTION_LENGTH	3
