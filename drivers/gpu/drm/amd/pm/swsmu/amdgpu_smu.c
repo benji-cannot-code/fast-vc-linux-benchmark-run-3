b63415003c836e05e1335b709cbedfe06090f822
Jinzhou Su <Jinzhou.Su@amd.com>
2020-12-17 17:37:38 +0800

    drm/amd/pm: Add interface for request WGPs
    When user specifies a reduced WGP(CU) config via disalbe_cu module
    parameter, this does not disable the clocks which uses additional
    power. This interface send active WGP number to SMU and SMU will
    cooperate with RLC to power off relative WGPs.
    
    v2: Add request active WGPs in Vangogh smu post init.
    
    Signed-off-by: Jinzhou.Su <Jinzhou.Su@amd.com>
    Reviewed-by: Jiansong Chen <Jiansong.Chen@amd.com>
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
+++ drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
@@ -479,9 +479,6 @@ static int smu_late_init(void *handle)
 
 	smu_set_fine_grain_gfx_freq_parameters(smu);
 
-	if (adev->asic_type == CHIP_VANGOGH)
-		return 0;
-
 	if (!smu->pm_enabled)
 		return 0;
 
@@ -490,6 +487,9 @@ static int smu_late_init(void *handle)
 		dev_err(adev->dev, "Failed to post smu init!\n");
 		return ret;
 	}
+
+	if (adev->asic_type == CHIP_VANGOGH)
+		return 0;
 
 	ret = smu_set_default_od_settings(smu);
 	if (ret) {
