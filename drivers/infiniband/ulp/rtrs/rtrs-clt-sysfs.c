648e449f6c74efef43f1c4832bd7b8423d500280
Guoqing Jiang <guoqing.jiang@cloud.ionos.com>
2020-12-17 15:19:03 +0100

    RDMA/rtrs: Call kobject_put in the failure path
    Per the comment of kobject_init_and_add, we need to free the memory
    by call kobject_put.
    
    Fixes: 215378b838df ("RDMA/rtrs: client: sysfs interface functions")
    Fixes: 91b11610af8d ("RDMA/rtrs: server: sysfs interface functions")
    Link: https://lore.kernel.org/r/20201217141915.56989-8-jinpu.wang@cloud.ionos.com
    Signed-off-by: Guoqing Jiang <guoqing.jiang@cloud.ionos.com>
    Reviewed-by: Md Haris Iqbal <haris.iqbal@cloud.ionos.com>
    Reviewed-by: Gioh Kim <gi-oh.kim@cloud.ionos.com>
    Signed-off-by: Jack Wang <jinpu.wang@cloud.ionos.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/infiniband/ulp/rtrs/rtrs-clt-sysfs.c
+++ drivers/infiniband/ulp/rtrs/rtrs-clt-sysfs.c
@@ -409,6 +409,7 @@ int rtrs_clt_create_sess_files(struct rtrs_clt_sess *sess)
 				   "%s", str);
 	if (err) {
 		pr_err("kobject_init_and_add: %d\n", err);
+		kobject_put(&sess->kobj);
 		return err;
 	}
 	err = sysfs_create_group(&sess->kobj, &rtrs_clt_sess_attr_group);
@@ -420,6 +421,7 @@ int rtrs_clt_create_sess_files(struct rtrs_clt_sess *sess)
 				   &sess->kobj, "stats");
 	if (err) {
 		pr_err("kobject_init_and_add: %d\n", err);
+		kobject_put(&sess->stats->kobj_stats);
 		goto remove_group;
 	}
 
