8872d6cd0686bbe9e5748e68677b995f39cf95d0
Matthias Brugger <mbrugger@suse.com>
2020-12-18 11:57:07 +0100

    hwrng: iproc-rng200 - Fix disable of the block.
    When trying to disable the block we bitwise or the control
    register with value zero. This is confusing as using bitwise or with
    value zero doesn't have any effect at all. Drop this as we already set
    the enable bit to zero by appling inverted RNG_RBGEN_MASK.
    
    Signed-off-by: Matthias Brugger <mbrugger@suse.com>
    Acked-by: Scott Branden <scott.branden@broadcom.com>
    Acked-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/char/hw_random/iproc-rng200.c
+++ drivers/char/hw_random/iproc-rng200.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RNG_CTRL_OFFSET					0x00
 #define RNG_CTRL_RNG_RBGEN_MASK				0x00001FFF
 #define RNG_CTRL_RNG_RBGEN_ENABLE			0x00000001
-#define RNG_CTRL_RNG_RBGEN_DISABLE			0x00000000
 
 #define RNG_SOFT_RESET_OFFSET				0x04
 #define RNG_SOFT_RESET					0x00000001
@@ -62,7 +61,6 @@ static void iproc_rng200_restart(void __iomem *rng_base)
 	/* Disable RBG */
 	val = ioread32(rng_base + RNG_CTRL_OFFSET);
 	val &= ~RNG_CTRL_RNG_RBGEN_MASK;
-	val |= RNG_CTRL_RNG_RBGEN_DISABLE;
 	iowrite32(val, rng_base + RNG_CTRL_OFFSET);
 
 	/* Clear all interrupt status */
@@ -175,7 +173,6 @@ static void iproc_rng200_cleanup(struct hwrng *rng)
 	/* Disable RNG hardware */
 	val = ioread32(priv->base + RNG_CTRL_OFFSET);
 	val &= ~RNG_CTRL_RNG_RBGEN_MASK;
-	val |= RNG_CTRL_RNG_RBGEN_DISABLE;
 	iowrite32(val, priv->base + RNG_CTRL_OFFSET);
 }
 
