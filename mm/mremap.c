ff6ec688be2784f5caa99ba2d549efb1e99b929c
Christophe Leroy <christophe.leroy@csgroup.eu>
2020-12-18 14:07:58 +0000

    mm: Remove arch_remap() and mm-arch-hooks.h
    powerpc was the last provider of arch_remap() and the last
    user of mm-arch-hooks.h.
    
    Since commit 526a9c4a7234 ("powerpc/vdso: Provide vdso_remap()"),
    arch_remap() hence mm-arch-hooks.h are not used anymore.
    
    Remove them.
    
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- mm/mremap.c
+++ mm/mremap.c
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/syscalls.h>
 #include <linux/mmu_notifier.h>
 #include <linux/uaccess.h>
-#include <linux/mm-arch-hooks.h>
 #include <linux/userfaultfd_k.h>
 
 #include <asm/cacheflush.h>
@@ -563,8 +562,6 @@ static unsigned long move_vma(struct vm_area_struct *vma,
 		new_addr = err;
 	} else {
 		mremap_userfaultfd_prep(new_vma, uf);
-		arch_remap(mm, old_addr, old_addr + old_len,
-			   new_addr, new_addr + new_len);
 	}
 
 	/* Conceal VM_ACCOUNT so old reservation is not undone */
