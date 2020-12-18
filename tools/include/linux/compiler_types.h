7432ce5235f1abf6a4e2ea960aa347b7dac6be1a
Arnaldo Carvalho de Melo <acme@redhat.com>
2020-12-18 09:59:05 -0300

    tools headers: Get tools's linux/compiler.h closer to the kernel's
    We're cherry picking stuff from the kernel to allow for the other
    headers that we keep in sync via tools/perf/check-headers.sh to work,
    so introduce linux/compiler_types.h and from there get the compiler
    specific stuff.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

---
+++ tools/include/linux/compiler_types.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __LINUX_COMPILER_TYPES_H
+#define __LINUX_COMPILER_TYPES_H
+
+/* Compiler specific macros. */
+#ifdef __GNUC__
+#include <linux/compiler-gcc.h>
+#endif
+
+#endif /* __LINUX_COMPILER_TYPES_H */
