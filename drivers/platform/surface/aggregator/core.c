6d079a5f0eb7018ca712770502acf411942309a0
Maximilian Luz <luzmaximilian@gmail.com>
2020-12-21 19:39:56 +0100

    platform/surface: aggregator: Add dedicated bus and device type
    The Surface Aggregator EC provides varying functionality, depending on
    the Surface device. To manage this functionality, we use dedicated
    client devices for each subsystem or virtual device of the EC. While
    some of these clients are described as standard devices in ACPI and the
    corresponding client drivers can be implemented as platform drivers in
    the kernel (making use of the controller API already present), many
    devices, especially on newer Surface models, cannot be found there.
    
    To simplify management of these devices, we introduce a new bus and
    client device type for the Surface Aggregator subsystem. The new device
    type takes care of managing the controller reference, essentially
    guaranteeing its validity for as long as the client device exists, thus
    alleviating the need to manually establish device links for that purpose
    in the client driver (as has to be done with the platform devices).
    
    Signed-off-by: Maximilian Luz <luzmaximilian@gmail.com>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://lore.kernel.org/r/20201221183959.1186143-7-luzmaximilian@gmail.com
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/platform/surface/aggregator/core.c
+++ drivers/platform/surface/aggregator/core.c
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sysfs.h>
 
 #include <linux/surface_aggregator/controller.h>
+
+#include "bus.h"
 #include "controller.h"
 
 #define CREATE_TRACE_POINTS
@@ -740,6 +742,9 @@ static void ssam_serial_hub_remove(struct serdev_device *serdev)
 	sysfs_remove_group(&serdev->dev.kobj, &ssam_sam_group);
 	ssam_controller_lock(ctrl);
 
+	/* Remove all client devices. */
+	ssam_controller_remove_clients(ctrl);
+
 	/* Act as if suspending to silence events. */
 	status = ssam_ctrl_notif_display_off(ctrl);
 	if (status) {
@@ -792,6 +797,10 @@ static int __init ssam_core_init(void)
 {
 	int status;
 
+	status = ssam_bus_register();
+	if (status)
+		goto err_bus;
+
 	status = ssh_ctrl_packet_cache_init();
 	if (status)
 		goto err_cpkg;
@@ -811,6 +820,8 @@ err_register:
 err_evitem:
 	ssh_ctrl_packet_cache_destroy();
 err_cpkg:
+	ssam_bus_unregister();
+err_bus:
 	return status;
 }
 module_init(ssam_core_init);
@@ -820,6 +831,7 @@ static void __exit ssam_core_exit(void)
 	serdev_device_driver_unregister(&ssam_serial_hub);
 	ssam_event_item_cache_destroy();
 	ssh_ctrl_packet_cache_destroy();
+	ssam_bus_unregister();
 }
 module_exit(ssam_core_exit);
 
