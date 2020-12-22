5f9e99dcd3b2a345f134ca87999519dab331865f
Vincenzo Frascino <vincenzo.frascino@arm.com>
2020-12-22 12:01:28 -0800

    arm64: mte: add in-kernel MTE helpers
    Provide helper functions to manipulate allocation and pointer tags for
    kernel addresses.
    
    Low-level helper functions (mte_assign_*, written in assembly) operate tag
    values from the [0x0, 0xF] range.  High-level helper functions
    (mte_get/set_*) use the [0xF0, 0xFF] range to preserve compatibility with
    normal kernel pointers that have 0xFF in their top byte.
    
    MTE_GRANULE_SIZE and related definitions are moved to mte-def.h header
    that doesn't have any dependencies and is safe to include into any
    low-level header.
    
    Link: https://lkml.kernel.org/r/c31bf759b4411b2d98cdd801eb928e241584fd1f.1606161801.git.andreyknvl@google.com
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Co-developed-by: Andrey Konovalov <andreyknvl@google.com>
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Tested-by: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: Branislav Rankov <Branislav.Rankov@arm.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Evgenii Stepanov <eugenis@google.com>
    Cc: Kevin Brodsky <kevin.brodsky@arm.com>
    Cc: Marco Elver <elver@google.com>
    Cc: Vasily Gorbik <gor@linux.ibm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/arm64/include/asm/mte.h
+++ arch/arm64/include/asm/mte.h
@@ -6,14 +6,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_MTE_H
 #define __ASM_MTE_H
 
-#define MTE_GRANULE_SIZE	UL(16)
-#define MTE_GRANULE_MASK	(~(MTE_GRANULE_SIZE - 1))
-#define MTE_TAG_SHIFT		56
-#define MTE_TAG_SIZE		4
+#include <asm/compiler.h>
+#include <asm/mte-def.h>
+
+#define __MTE_PREAMBLE		ARM64_ASM_PREAMBLE ".arch_extension memtag\n"
 
 #ifndef __ASSEMBLY__
 
+#include <linux/bitfield.h>
 #include <linux/page-flags.h>
+#include <linux/types.h>
 
 #include <asm/pgtable-types.h>
 
@@ -46,7 +48,9 @@ long get_mte_ctrl(struct task_struct *task);
 int mte_ptrace_copy_tags(struct task_struct *child, long request,
 			 unsigned long addr, unsigned long data);
 
-#else
+void mte_assign_mem_tag_range(void *addr, size_t size);
+
+#else /* CONFIG_ARM64_MTE */
 
 /* unused if !CONFIG_ARM64_MTE, silence the compiler */
 #define PG_mte_tagged	0
@@ -81,7 +85,11 @@ static inline int mte_ptrace_copy_tags(struct task_struct *child,
 	return -EIO;
 }
 
-#endif
+static inline void mte_assign_mem_tag_range(void *addr, size_t size)
+{
+}
+
+#endif /* CONFIG_ARM64_MTE */
 
 #endif /* __ASSEMBLY__ */
 #endif /* __ASM_MTE_H  */
