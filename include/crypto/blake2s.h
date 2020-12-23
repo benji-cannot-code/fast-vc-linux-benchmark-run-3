c94c6e6d03a455f809a1ce4be928f23a42da1cb2
Eric Biggers <ebiggers@google.com>
2020-12-23 00:09:58 -0800

    crypto: blake2s - include <linux/bug.h> instead of <asm/bug.h>
    Address the following checkpatch warning:
    
    	WARNING: Use #include <linux/bug.h> instead of <asm/bug.h>
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/blake2s.h
+++ include/crypto/blake2s.h
@@ -7,11 +7,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _CRYPTO_BLAKE2S_H
 #define _CRYPTO_BLAKE2S_H
 
+#include <linux/bug.h>
 #include <linux/types.h>
 #include <linux/kernel.h>
 #include <linux/string.h>
-
-#include <asm/bug.h>
 
 enum blake2s_lengths {
 	BLAKE2S_BLOCK_SIZE = 64,
