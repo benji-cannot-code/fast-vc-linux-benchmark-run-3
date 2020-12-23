b1d9947eb7623901ae82b475ba54233b3874ef8b
Eric Biggers <ebiggers@google.com>
2020-12-23 00:09:52 -0800

    crypto: blake2s - remove unneeded includes
    It doesn't make sense for the generic implementation of BLAKE2s to
    include <crypto/internal/simd.h> and <linux/jump_label.h>, as these are
    things that would only be useful in an architecture-specific
    implementation.  Remove these unnecessary includes.
    
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/blake2s_generic.c
+++ crypto/blake2s_generic.c
@@ -5,11 +5,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <crypto/internal/blake2s.h>
-#include <crypto/internal/simd.h>
 #include <crypto/internal/hash.h>
 
 #include <linux/types.h>
-#include <linux/jump_label.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 
