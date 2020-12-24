7ff7aeda9df51eaabf1420a5d22cba92c5065eee
Tzung-Bi Shih <tzungbi@google.com>
2020-12-24 18:06:03 +0800

    ASoC: rt1015: sort header inclusions
    Sorts header inclusions.  ASCII value of 'r' is less than 's'.
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@google.com>
    Link: https://lore.kernel.org/r/20201224100607.3006171-2-tzungbi@google.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/rt1015.c
+++ sound/soc/codecs/rt1015.c
@@ -25,10 +25,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/initval.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
+#include <sound/rt1015.h>
 #include <sound/soc-dapm.h>
 #include <sound/soc.h>
 #include <sound/tlv.h>
-#include <sound/rt1015.h>
 
 #include "rl6231.h"
 #include "rt1015.h"
