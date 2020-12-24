18b5fdacdf98e90f5390c540fcb38e591cfde4cc
Chris Wilson <chris@chris-wilson.co.uk>
2020-12-24 13:55:41 +0000

    drm/i915/gt: Shrink the critical section for irq signaling
    Let's only wait for the list iterator when decoupling the virtual
    breadcrumb, as the signaling of all the requests may take a long time,
    during which we do not want to keep the tasklet spinning.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Matthew Brost <matthew.brost@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201224135544.1713-6-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gt/intel_breadcrumbs.c
+++ drivers/gpu/drm/i915/gt/intel_breadcrumbs.c
@@ -235,6 +235,7 @@ static void signal_irq_work(struct irq_work *work)
 		intel_breadcrumbs_disarm_irq(b);
 
 	rcu_read_lock();
+	atomic_inc(&b->signaler_active);
 	list_for_each_entry_rcu(ce, &b->signalers, signal_link) {
 		struct i915_request *rq;
 
@@ -270,6 +271,7 @@ static void signal_irq_work(struct irq_work *work)
 			}
 		}
 	}
+	atomic_dec(&b->signaler_active);
 	rcu_read_unlock();
 
 	llist_for_each_safe(signal, sn, signal) {
