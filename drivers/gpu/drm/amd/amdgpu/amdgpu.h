6880e83c696977bc3b9504c1f1ad435fb470cc67
Hawking Zhang <Hawking.Zhang@amd.com>
2020-12-24 15:49:18 +0800

    drm/amdgpu: add amdgpu_hdp structure
    amdgpu_hdp hold all the callbacks for hdp
    
    Signed-off-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Reviewed-by: Likun Gao <Likun.Gao@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu.h
@@ -108,6 +108,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "amdgpu_gfxhub.h"
 #include "amdgpu_df.h"
 #include "amdgpu_smuio.h"
+#include "amdgpu_hdp.h"
 
 #define MAX_GPU_INSTANCE		16
 
@@ -921,6 +922,9 @@ struct amdgpu_device {
 
 	/* nbio */
 	struct amdgpu_nbio		nbio;
+
+	/* hdp */
+	struct amdgpu_hdp		hdp;
 
 	/* smuio */
 	struct amdgpu_smuio		smuio;
