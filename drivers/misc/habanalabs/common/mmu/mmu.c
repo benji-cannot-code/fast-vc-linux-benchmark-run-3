1e349d80d5e954b99e29ff083cc70a90bc923de4
Ofir Bitton <obitton@habana.ai>
2020-12-28 14:36:47 +0200

    habanalabs: separate common code to dedicated folders
    We separate some of the common code source files to different
    folders for a better maintainability and testability.
    
    Signed-off-by: Ofir Bitton <obitton@habana.ai>
    Reviewed-by: Oded Gabbay <ogabbay@kernel.org>
    Signed-off-by: Oded Gabbay <ogabbay@kernel.org>

--- drivers/misc/habanalabs/common/mmu/mmu.c
+++ drivers/misc/habanalabs/common/mmu/mmu.c
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/slab.h>
 
-#include "habanalabs.h"
+#include "../habanalabs.h"
 
 bool hl_is_dram_va(struct hl_device *hdev, u64 virt_addr)
 {
