553c46e680277368d8106052e8c41334044db7cf
Tian Tao <tiantao6@hisilicon.com>
2020-12-30 16:20:20 +0800

    csky: remove unused including <linux/version.h>
    Remove including <linux/version.h> that don't need it.
    
    Signed-off-by: Tian Tao <tiantao6@hisilicon.com>
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>

--- arch/csky/include/asm/thread_info.h
+++ arch/csky/include/asm/thread_info.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 
-#include <linux/version.h>
 #include <asm/types.h>
 #include <asm/page.h>
 #include <asm/processor.h>
