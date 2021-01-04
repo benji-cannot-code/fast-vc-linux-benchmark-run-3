01265cdd23d634c51c0ba20ed7bca893f7fac926
Cédric Le Goater <clg@kaod.org>
2021-01-04 15:32:00 +0100

    powerpc/watchdog: Declare soft_nmi_interrupt() prototype
    soft_nmi_interrupt() usage requires PPC_WATCHDOG to be configured.
    Check the CONFIG definition to declare the prototype.
    
    It fixes this W=1 compile error :
    
    ../arch/powerpc/kernel/watchdog.c:250:6: error: no previous prototype for ‘soft_nmi_interrupt’ [-Werror=missing-prototypes]
      250 | void soft_nmi_interrupt(struct pt_regs *regs)
          |      ^~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Cédric Le Goater <clg@kaod.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20210104143206.695198-18-clg@kaod.org

--- arch/powerpc/kernel/watchdog.c
+++ arch/powerpc/kernel/watchdog.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/smp.h>
 
 #include <asm/paca.h>
+#include <asm/nmi.h>
 
 /*
  * The powerpc watchdog ensures that each CPU is able to service timers.
