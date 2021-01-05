d87bff3124d63cbf6489e73c211db11eaf3aa079
Parav Pandit <parav@nvidia.com>
2021-01-05 12:32:00 +0200

    vdpa: Define vdpa mgmt device, ops and a netlink interface
    To add one or more VDPA devices, define a management device which
    allows adding or removing vdpa device. A management device defines
    set of callbacks to manage vdpa devices.
    
    To begin with, it defines add and remove callbacks through which a user
    defined vdpa device can be added or removed.
    
    A unique management device is identified by its unique handle identified
    by management device name and optionally the bus name.
    
    Hence, introduce routine through which driver can register a
    management device and its callback operations for adding and remove
    a vdpa device.
    
    Introduce vdpa netlink socket family so that user can query management
    device and its attributes.
    
    Example of show vdpa management device which allows creating vdpa device of
    networking class (device id = 0x1) of virtio specification 1.1
    section 5.1.1.
    
    $ vdpa mgmtdev show
    vdpasim_net:
      supported_classes:
        net
    
    Example of showing vdpa management device in JSON format.
    
    $ vdpa mgmtdev show -jp
    {
        "show": {
            "vdpasim_net": {
                "supported_classes": [ "net" ]
            }
        }
    }
    
    Signed-off-by: Parav Pandit <parav@nvidia.com>
    Reviewed-by: Eli Cohen <elic@nvidia.com>
    Reviewed-by: Jason Wang <jasowang@redhat.com>
    Link: https://lore.kernel.org/r/20210105103203.82508-4-parav@nvidia.com
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    Including a bugfix:
    
    vpda: correctly size vdpa_nl_policy
    
    We need to ensure last entry of vdpa_nl_policy[]
    is zero, otherwise out-of-bounds access is hurting us.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reported-by: syzbot <syzkaller@googlegroups.com>
    Cc: Parav Pandit <parav@nvidia.com>
    Cc: Eli Cohen <elic@nvidia.com>
    Cc: Jason Wang <jasowang@redhat.com>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Link: https://lore.kernel.org/r/20210210134911.4119555-1-eric.dumazet@gmail.com
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

---
+++ include/uapi/linux/vdpa.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0+ WITH Linux-syscall-note */
+/*
+ * vdpa device management interface
+ * Copyright (c) 2020 Mellanox Technologies Ltd. All rights reserved.
+ */
+
+#ifndef _UAPI_LINUX_VDPA_H_
+#define _UAPI_LINUX_VDPA_H_
+
+#define VDPA_GENL_NAME "vdpa"
+#define VDPA_GENL_VERSION 0x1
+
+enum vdpa_command {
+	VDPA_CMD_UNSPEC,
+	VDPA_CMD_MGMTDEV_NEW,
+	VDPA_CMD_MGMTDEV_GET,		/* can dump */
+};
+
+enum vdpa_attr {
+	VDPA_ATTR_UNSPEC,
+
+	/* bus name (optional) + dev name together make the parent device handle */
+	VDPA_ATTR_MGMTDEV_BUS_NAME,		/* string */
+	VDPA_ATTR_MGMTDEV_DEV_NAME,		/* string */
+	VDPA_ATTR_MGMTDEV_SUPPORTED_CLASSES,	/* u64 */
+
+	/* new attributes must be added above here */
+	VDPA_ATTR_MAX,
+};
+
+#endif
