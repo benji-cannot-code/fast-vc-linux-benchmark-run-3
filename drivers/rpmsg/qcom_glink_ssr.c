ebe82c5aa3846b2ab9c41044fb6a2af72e3a5726
Alex Elder <elder@linaro.org>
2021-01-05 17:55:28 -0600

    rpmsg: glink: add include of header file
    With an x86_64 architecture W=1 build, qcom_glink_ssr_notify() is
    reported as having no previous prototype.  The prototype is found in
    "qcom_glink.h", so we just need "qcom_glink_ssr.c" to include that
    file.
    
    Signed-off-by: Alex Elder <elder@linaro.org>
    Link: https://lore.kernel.org/r/20210105235528.32538-1-elder@linaro.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/rpmsg/qcom_glink_ssr.c
+++ drivers/rpmsg/qcom_glink_ssr.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/notifier.h>
 #include <linux/rpmsg.h>
+#include <linux/rpmsg/qcom_glink.h>
 #include <linux/remoteproc/qcom_rproc.h>
 
 /**
