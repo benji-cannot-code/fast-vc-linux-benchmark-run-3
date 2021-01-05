d738355df19fae4447698c09b89384e798130645
Shayne Chen <shayne.chen@mediatek.com>
2021-01-05 16:55:25 +0800

    mt76: testmode: make tx queued limit adjustable
    Originally, tx queued limit is set to 1000 to prevent from running out
    of tx token. If a new testmode tx is triggered while the previous one
    hasn't finished yet, we'll wait a period of time until tx_done equals to
    tx_queued. Normally, current queued limit can finish in 10 seconds.
    
    However, if ipg is configured to a larger value, less than 1000 packets
    can be done in the default timeout period, which may lead to a crash
    when a new testmode tx triggered.
    
    To deal with this, make tx queued limit dynamically adjusted according
    to ipg value.
    
    Signed-off-by: Shayne Chen <shayne.chen@mediatek.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- drivers/net/wireless/mediatek/mt76/testmode.h
+++ drivers/net/wireless/mediatek/mt76/testmode.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MT76_TESTMODE_H
 #define __MT76_TESTMODE_H
 
+#define MT76_TM_TIMEOUT	10
+
 /**
  * enum mt76_testmode_attr - testmode attributes inside NL80211_ATTR_TESTDATA
  *
