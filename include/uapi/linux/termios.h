c2c6e06581dee3d9f581ffa91cfa6df9e635d6ab
Jiri Slaby <jslaby@suse.cz>
2021-01-05 13:02:39 +0100

    tty: drop termiox user definitions
    As was concluded in a follow-up discussion of commit e0efb3168d34 (tty:
    Remove dead termiox code) [1], termiox ioctls never worked, so there is
    barely anyone using this interface. We can safely remove the user
    definitions for this never adopted interface.
    
    [1] https://lore.kernel.org/lkml/c1c9fc04-02eb-2260-195b-44c357f057c0@kernel.org/t/#u
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Link: https://lore.kernel.org/r/20210105120239.28031-12-jslaby@suse.cz
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/uapi/linux/termios.h
+++ include/uapi/linux/termios.h
@@ -6,19 +6,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <asm/termios.h>
 
-#define NFF	5
-
-struct termiox
-{
-	__u16	x_hflag;
-	__u16	x_cflag;
-	__u16	x_rflag[NFF];
-	__u16	x_sflag;
-};
-
-#define	RTSXOFF		0x0001		/* RTS flow control on input */
-#define	CTSXON		0x0002		/* CTS flow control on output */
-#define	DTRXOFF		0x0004		/* DTR flow control on input */
-#define DSRXON		0x0008		/* DCD flow control on output */
-
 #endif
