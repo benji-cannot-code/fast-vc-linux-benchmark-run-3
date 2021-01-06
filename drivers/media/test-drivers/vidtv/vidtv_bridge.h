55ad866bb88d63b17778904e8a6934962e949159
Daniel W. S. Almeida <dwlsalmeida@gmail.com>
2021-01-06 04:04:48 +0100

    media: vidtv: use a simpler name in platform_{device|driver}
    Change from "vidtv_bridge" to simply "vidtv" so that vidtv looks
    more similar to the other media virtual drivers in /sys/bus/platform.
    
    Signed-off-by: Daniel W. S. Almeida <dwlsalmeida@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/media/test-drivers/vidtv/vidtv_bridge.h
+++ drivers/media/test-drivers/vidtv/vidtv_bridge.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * For now, only one frontend is supported. See vidtv_start_streaming()
  */
 #define NUM_FE 1
+#define VIDTV_PDEV_NAME "vidtv"
 
 #include <linux/i2c.h>
 #include <linux/platform_device.h>
