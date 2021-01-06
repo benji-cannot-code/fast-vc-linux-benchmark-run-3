7896cb33213636c8694b3d05737f2c291895fa8d
Jonathan Lemon <jonathan.lemon@gmail.com>
2021-01-06 14:18:32 -0800

    skbuff: replace sock_zerocopy_put() with skb_zcopy_put()
    Replace sock_zerocopy_put with the generic skb_zcopy_put()
    function.  Pass 'true' as the success argument, as this
    is identical to no change.
    
    Signed-off-by: Jonathan Lemon <jonathan.lemon@gmail.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -1252,13 +1252,6 @@ void sock_zerocopy_callback(struct ubuf_info *uarg, bool success)
 }
 EXPORT_SYMBOL_GPL(sock_zerocopy_callback);
 
-void sock_zerocopy_put(struct ubuf_info *uarg)
-{
-	if (uarg)
-		uarg->callback(uarg, uarg->zerocopy);
-}
-EXPORT_SYMBOL_GPL(sock_zerocopy_put);
-
 void sock_zerocopy_put_abort(struct ubuf_info *uarg, bool have_uref)
 {
 	if (uarg) {
@@ -1268,7 +1261,7 @@ void sock_zerocopy_put_abort(struct ubuf_info *uarg, bool have_uref)
 		uarg->len--;
 
 		if (have_uref)
-			sock_zerocopy_put(uarg);
+			skb_zcopy_put(uarg);
 	}
 }
 EXPORT_SYMBOL_GPL(sock_zerocopy_put_abort);
