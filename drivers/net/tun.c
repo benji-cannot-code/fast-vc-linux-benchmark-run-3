30ceab437493f4130ebacf1a3b3591bb87e3aeeb
Jonathan Lemon <jonathan.lemon@gmail.com>
2021-01-06 14:18:34 -0800

    skbuff: Add skb parameter to the ubuf zerocopy callback
    Add an optional skb parameter to the zerocopy callback parameter,
    which is passed down from skb_zcopy_clear().  This gives access
    to the original skb, which is needed for upcoming RX zero-copy
    error handling.
    
    Signed-off-by: Jonathan Lemon <jonathan.lemon@gmail.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1820,7 +1820,7 @@ drop:
 		skb_shinfo(skb)->tx_flags |= SKBTX_SHARED_FRAG;
 	} else if (msg_control) {
 		struct ubuf_info *uarg = msg_control;
-		uarg->callback(uarg, false);
+		uarg->callback(NULL, uarg, false);
 	}
 
 	skb_reset_network_header(skb);
