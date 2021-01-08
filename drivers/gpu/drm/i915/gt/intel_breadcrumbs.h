89cbbf6f941bf4776600d5d087945449c1c92339
Chris Wilson <chris@chris-wilson.co.uk>
2021-01-08 20:40:23 +0000

    drm/i915/gt: Restore ce->signal flush before releasing virtual engine
    Before we mark the virtual engine as no longer inflight, flush any
    ongoing signaling that may be using the ce->signal_link along the
    previous breadcrumbs. On switch to a new physical engine, that link will
    be inserted into the new set of breadcrumbs, causing confusion to an
    ongoing iterator.
    
    This patch undoes a last minute mistake introduced into commit
    bab0557c8dca ("drm/i915/gt: Remove virtual breadcrumb before transfer"),
    whereby instead of unconditionally applying the flush, it was only
    applied if the request itself was going to be reused.
    
    v2: Generalise and cancel all remaining ce->signals
    
    Fixes: bab0557c8dca ("drm/i915/gt: Remove virtual breadcrumb before transfer")
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Andi Shyti <andi.shyti@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210108204026.20682-4-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gt/intel_breadcrumbs.h
+++ drivers/gpu/drm/i915/gt/intel_breadcrumbs.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __INTEL_BREADCRUMBS__
 #define __INTEL_BREADCRUMBS__
 
+#include <linux/atomic.h>
 #include <linux/irq_work.h>
 
 #include "intel_engine_types.h"
@@ -44,5 +45,8 @@ void intel_engine_print_breadcrumbs(struct intel_engine_cs *engine,
 
 bool i915_request_enable_breadcrumb(struct i915_request *request);
 void i915_request_cancel_breadcrumb(struct i915_request *request);
+
+void intel_context_remove_breadcrumbs(struct intel_context *ce,
+				      struct intel_breadcrumbs *b);
 
 #endif /* __INTEL_BREADCRUMBS__ */
