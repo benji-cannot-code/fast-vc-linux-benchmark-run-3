6bc6760e3b5479bb6b83a7850442394c3a2dd22b
Alexandre Belloni <alexandre.belloni@bootlin.com>
2021-01-10 00:11:46 +0100

    iio:common:ms_sensors:ms_sensors_i2c: rework CRC calculation helper
    The CRC calculation always happens on 8 words which is why there is an
    extra element in the prom array of struct ms_tp_dev. However, on ms5637 and
    similar, only 7 words are readable.
    
    Then, set MS_SENSORS_TP_PROM_WORDS_NB to 8 and stop passing a len parameter
    to ms_sensors_tp_crc_valid as this simply hide the fact that it is
    hardcoded.
    
    Finally, use the newly introduced hw->prom_len to know how many words can be
    read.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Link: https://lore.kernel.org/r/20210109231148.1168104-5-alexandre.belloni@bootlin.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/common/ms_sensors/ms_sensors_i2c.h
+++ drivers/iio/common/ms_sensors/ms_sensors_i2c.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c.h>
 #include <linux/mutex.h>
 
-#define MS_SENSORS_TP_PROM_WORDS_NB		7
+#define MS_SENSORS_TP_PROM_WORDS_NB		8
 
 /**
  * struct ms_ht_dev - Humidity/Temperature sensor device structure
@@ -48,7 +48,7 @@ struct ms_tp_dev {
 	struct i2c_client *client;
 	struct mutex lock;
 	const struct ms_tp_hw_data *hw;
-	u16 prom[MS_SENSORS_TP_PROM_WORDS_NB + 1];
+	u16 prom[MS_SENSORS_TP_PROM_WORDS_NB];
 	u8 res_index;
 };
 
