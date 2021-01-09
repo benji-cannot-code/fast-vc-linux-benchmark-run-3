090a2bfc316f5836347833817ad465d35447b936
Pavel Begunkov <asml.silence@gmail.com>
2021-01-09 16:02:58 +0000

    bvec/iter: disallow zero-length segment bvecs
    zero-length bvec segments are allowed in general, but not handled by bio
    and down the block layer so filtered out. This inconsistency may be
    confusing and prevent from optimisations. As zero-length segments are
    useless and places that were generating them are patched, declare them
    not allowed.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Reviewed-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- lib/iov_iter.c
+++ lib/iov_iter.c
@@ -73,8 +73,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	__start.bi_bvec_done = skip;			\
 	__start.bi_idx = 0;				\
 	for_each_bvec(__v, i->bvec, __bi, __start) {	\
-		if (!__v.bv_len)			\
-			continue;			\
 		(void)(STEP);				\
 	}						\
 }
