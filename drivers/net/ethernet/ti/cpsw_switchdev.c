eb0a834f78bdd2f9981b5131072ce511b6274dcc
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-01-09 02:01:50 +0200

    net: switchdev: remove the transaction structure from port attributes
    Since the introduction of the switchdev API, port attributes were
    transmitted to drivers for offloading using a two-step transactional
    model, with a prepare phase that was supposed to catch all errors, and a
    commit phase that was supposed to never fail.
    
    Some classes of failures can never be avoided, like hardware access, or
    memory allocation. In the latter case, merely attempting to move the
    memory allocation to the preparation phase makes it impossible to avoid
    memory leaks, since commit 91cf8eceffc1 ("switchdev: Remove unused
    transaction item queue") which has removed the unused mechanism of
    passing on the allocated memory between one phase and another.
    
    It is time we admit that separating the preparation from the commit
    phase is something that is best left for the driver to decide, and not
    something that should be baked into the API, especially since there are
    no switchdev callers that depend on this.
    
    This patch removes the struct switchdev_trans member from switchdev port
    attribute notifier structures, and converts drivers to not look at this
    member.
    
    In part, this patch contains a revert of my previous commit 2e554a7a5d8a
    ("net: dsa: propagate switchdev vlan_filtering prepare phase to
    drivers").
    
    For the most part, the conversion was trivial except for:
    - Rocker's world implementation based on Broadcom OF-DPA had an odd
      implementation of ofdpa_port_attr_bridge_flags_set. The conversion was
      done mechanically, by pasting the implementation twice, then only
      keeping the code that would get executed during prepare phase on top,
      then only keeping the code that gets executed during the commit phase
      on bottom, then simplifying the resulting code until this was obtained.
    - DSA's offloading of STP state, bridge flags, VLAN filtering and
      multicast router could be converted right away. But the ageing time
      could not, so a shim was introduced and this was left for a further
      commit.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Jiri Pirko <jiri@nvidia.com>
    Reviewed-by: Kurt Kanzenbach <kurt@linutronix.de> # hellcreek
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org> # RTL8366RB
    Reviewed-by: Ido Schimmel <idosch@nvidia.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/ti/cpsw_switchdev.c
+++ drivers/net/ethernet/ti/cpsw_switchdev.c
@@ -25,15 +25,11 @@ struct cpsw_switchdev_event_work {
 	unsigned long event;
 };
 
-static int cpsw_port_stp_state_set(struct cpsw_priv *priv,
-				   struct switchdev_trans *trans, u8 state)
+static int cpsw_port_stp_state_set(struct cpsw_priv *priv, u8 state)
 {
 	struct cpsw_common *cpsw = priv->cpsw;
 	u8 cpsw_state;
 	int ret = 0;
-
-	if (switchdev_trans_ph_prepare(trans))
-		return 0;
 
 	switch (state) {
 	case BR_STATE_FORWARDING:
@@ -61,15 +57,11 @@ static int cpsw_port_stp_state_set(struct cpsw_priv *priv,
 }
 
 static int cpsw_port_attr_br_flags_set(struct cpsw_priv *priv,
-				       struct switchdev_trans *trans,
 				       struct net_device *orig_dev,
 				       unsigned long brport_flags)
 {
 	struct cpsw_common *cpsw = priv->cpsw;
 	bool unreg_mcast_add = false;
-
-	if (switchdev_trans_ph_prepare(trans))
-		return 0;
 
 	if (brport_flags & BR_MCAST_FLOOD)
 		unreg_mcast_add = true;
@@ -83,7 +75,6 @@ static int cpsw_port_attr_br_flags_set(struct cpsw_priv *priv,
 }
 
 static int cpsw_port_attr_br_flags_pre_set(struct net_device *netdev,
-					   struct switchdev_trans *trans,
 					   unsigned long flags)
 {
 	if (flags & ~(BR_LEARNING | BR_MCAST_FLOOD))
@@ -93,8 +84,7 @@ static int cpsw_port_attr_br_flags_pre_set(struct net_device *netdev,
 }
 
 static int cpsw_port_attr_set(struct net_device *ndev,
-			      const struct switchdev_attr *attr,
-			      struct switchdev_trans *trans)
+			      const struct switchdev_attr *attr)
 {
 	struct cpsw_priv *priv = netdev_priv(ndev);
 	int ret;
@@ -103,15 +93,15 @@ static int cpsw_port_attr_set(struct net_device *ndev,
 
 	switch (attr->id) {
 	case SWITCHDEV_ATTR_ID_PORT_PRE_BRIDGE_FLAGS:
-		ret = cpsw_port_attr_br_flags_pre_set(ndev, trans,
+		ret = cpsw_port_attr_br_flags_pre_set(ndev,
 						      attr->u.brport_flags);
 		break;
 	case SWITCHDEV_ATTR_ID_PORT_STP_STATE:
-		ret = cpsw_port_stp_state_set(priv, trans, attr->u.stp_state);
+		ret = cpsw_port_stp_state_set(priv, attr->u.stp_state);
 		dev_dbg(priv->dev, "stp state: %u\n", attr->u.stp_state);
 		break;
 	case SWITCHDEV_ATTR_ID_PORT_BRIDGE_FLAGS:
-		ret = cpsw_port_attr_br_flags_set(priv, trans, attr->orig_dev,
+		ret = cpsw_port_attr_br_flags_set(priv, attr->orig_dev,
 						  attr->u.brport_flags);
 		break;
 	default:
