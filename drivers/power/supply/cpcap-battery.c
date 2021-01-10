2e3dad981372d7f449c5fab8f0e8be0c4142b20a
Pavel Machek <pavel@ucw.cz>
2021-01-10 21:54:03 +0200

    power: supply: cpcap-battery: Fix typo
    Fix typo
    
    Cc: Arthur Demchenkov <spinal.by@gmail.com>
    Cc: Carl Philipp Klemm <philipp@uvos.xyz>
    Cc: Merlijn Wajer <merlijn@wizzup.org>
    Cc: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Pavel Machek <pavel@ucw.cz>
    [tony@atomide.com: separated out from charger changes]
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

--- drivers/power/supply/cpcap-battery.c
+++ drivers/power/supply/cpcap-battery.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2017 Tony Lindgren <tony@atomide.com>
  *
- * Some parts of the code based on earlie Motorola mapphone Linux kernel
+ * Some parts of the code based on earlier Motorola mapphone Linux kernel
  * drivers:
  *
  * Copyright (C) 2009-2010 Motorola, Inc.
