003cf58e3829aaf8814048f3cfb0c2d947119697
Chris Wilson <chris@chris-wilson.co.uk>
2021-01-11 10:57:35 +0000

    drm/i915/selftests: Include engine name after reset failure
    During igt_reset_nop_engine, an engine reset unexpectedly failed. For the
    next time this happens, mention which engine that was.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210111105735.21515-4-chris@chris-wilson.co.uk

--- drivers/gpu/drm/i915/gt/selftest_hangcheck.c
+++ drivers/gpu/drm/i915/gt/selftest_hangcheck.c
@@ -507,7 +507,8 @@ static int igt_reset_nop_engine(void *arg)
 			}
 			err = intel_engine_reset(engine, NULL);
 			if (err) {
-				pr_err("i915_reset_engine failed\n");
+				pr_err("intel_engine_reset(%s) failed, err:%d\n",
+				       engine->name, err);
 				break;
 			}
 
@@ -609,7 +610,8 @@ static int __igt_reset_engine(struct intel_gt *gt, bool active)
 
 			err = intel_engine_reset(engine, NULL);
 			if (err) {
-				pr_err("i915_reset_engine failed\n");
+				pr_err("intel_engine_reset(%s) failed, err:%d\n",
+				       engine->name, err);
 				break;
 			}
 
