90890d8a4a8c4b211115ea1661dd700f00f36fc1
Yong Wu <yong.wu@mediatek.com>
2021-01-11 19:18:44 +0800

    dt-bindings: memory: mediatek: Extend LARB_NR_MAX to 32
    Extend the max larb number definition as mt8192 has larb_nr over 16.
    
    Signed-off-by: Yong Wu <yong.wu@mediatek.com>
    Acked-by: Rob Herring <robh@kernel.org>
    Acked-by: Krzysztof Kozlowski <krzk@kernel.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Link: https://lore.kernel.org/r/20210111111914.22211-4-yong.wu@mediatek.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- include/dt-bindings/memory/mtk-memory-port.h
+++ include/dt-bindings/memory/mtk-memory-port.h
@@ -7,10 +7,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __DT_BINDINGS_MEMORY_MTK_MEMORY_PORT_H_
 #define __DT_BINDINGS_MEMORY_MTK_MEMORY_PORT_H_
 
-#define MTK_LARB_NR_MAX			16
+#define MTK_LARB_NR_MAX			32
 
 #define MTK_M4U_ID(larb, port)		(((larb) << 5) | (port))
-#define MTK_M4U_TO_LARB(id)		(((id) >> 5) & 0xf)
+#define MTK_M4U_TO_LARB(id)		(((id) >> 5) & 0x1f)
 #define MTK_M4U_TO_PORT(id)		((id) & 0x1f)
 
 #endif
