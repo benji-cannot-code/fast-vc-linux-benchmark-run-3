c8839f7592d14898ef863f3130de071a7fb53529
Yong Wu <yong.wu@mediatek.com>
2021-01-11 19:19:11 +0800

    iommu/mediatek: Support master use iova over 32bit
    After extending v7s, our pagetable already support iova reach
    16GB(34bit). the master got the iova via dma_alloc_attrs may reach
    34bits, but its HW register still is 32bit. then how to set the
    bit32/bit33 iova? this depend on a SMI larb setting(bank_sel).
    
    we separate whole 16GB iova to four banks:
    bank: 0: 0~4G; 1: 4~8G; 2: 8-12G; 3: 12-16G;
    The bank number is (iova >> 32).
    
    We will preassign which bank the larbs belong to. currently we don't
    have a interface for master to adjust its bank number.
    
    Each a bank is a iova_region which is a independent iommu-domain.
    the iova range for each iommu-domain can't cross 4G.
    
    Signed-off-by: Yong Wu <yong.wu@mediatek.com>
    Acked-by: Krzysztof Kozlowski <krzk@kernel.org> #for memory part
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Link: https://lore.kernel.org/r/20210111111914.22211-31-yong.wu@mediatek.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- include/soc/mediatek/smi.h
+++ include/soc/mediatek/smi.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct mtk_smi_larb_iommu {
 	struct device *dev;
 	unsigned int   mmu;
+	unsigned char  bank[32];
 };
 
 /*
