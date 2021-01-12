6505305ad1bdf8e2f9876174e0fc90bb0fa9ac59
Thomas Zimmermann <tzimmermann@suse.de>
2021-01-12 09:10:35 +0100

    drm: Move struct drm_device.hose to legacy section
    The field is only relevant for legacy DRM drivers. Its only non-legacy
    user in the DRM core is in drm_file.c. This code is now protected by
    CONFIG_DRM_LEGACY. Radeon, the only driver that used the field, has been
    changed to maintain it's own copy.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210112081035.6882-7-tzimmermann@suse.de

--- drivers/gpu/drm/drm_file.c
+++ drivers/gpu/drm/drm_file.c
@@ -368,6 +368,7 @@ static int drm_open_helper(struct file *filp, struct drm_minor *minor)
 	list_add(&priv->lhead, &dev->filelist);
 	mutex_unlock(&dev->filelist_mutex);
 
+#ifdef CONFIG_DRM_LEGACY
 #ifdef __alpha__
 	/*
 	 * Default the hose
@@ -387,6 +388,7 @@ static int drm_open_helper(struct file *filp, struct drm_minor *minor)
 				dev->hose = b->sysdata;
 		}
 	}
+#endif
 #endif
 
 	return 0;
