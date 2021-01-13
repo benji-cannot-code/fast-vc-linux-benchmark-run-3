dbcaf9acb6423b04bec2a65babc4f2625c3c1295
Guo Ren <guoren@linux.alibaba.com>
2021-01-13 15:28:16 +0800

    csky: Fixup swapon
    Current csky's swappon is broken by wrong swap PTE entry format.
    Now redesign the new format for abiv1 & abiv2 and make swappon +
    zram work properly on csky machines.
    
    C-SKY PTE has VALID, DIRTY to emulate PRESENT, READ, WRITE, EXEC
    attributes. GLOBAL bit is shared by two pages in the same tlb
    entry. So we need to keep GLOBAL, VALID, PRESENT zero in swp_pte.
    
    To distinguish PAGE_NONE and swp_pte, we need to use an additional
    bit (abiv1 is _PAGE_READ, abiv2 is _PAGE_WRITE).
    
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Cc: Arnd Bergmann <arnd@arndb.de>

--- arch/csky/include/asm/pgtable.h
+++ arch/csky/include/asm/pgtable.h
@@ -42,13 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define pfn_pte(pfn, prot) __pte(((unsigned long long)(pfn) << PAGE_SHIFT) \
 				| pgprot_val(prot))
 
-#define _PAGE_CHG_MASK	(PAGE_MASK | _PAGE_ACCESSED | _PAGE_MODIFIED | \
-			 _CACHE_MASK)
-
-#define __swp_type(x)			(((x).val >> 4) & 0xff)
-#define __swp_offset(x)			((x).val >> 12)
-#define __swp_entry(type, offset)	((swp_entry_t) {((type) << 4) | \
-					((offset) << 12) })
 #define __pte_to_swp_entry(pte)		((swp_entry_t) { pte_val(pte) })
 #define __swp_entry_to_pte(x)		((pte_t) { (x).val })
 
@@ -62,8 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define _PAGE_BASE	(_PAGE_PRESENT | _PAGE_ACCESSED)
 
-#define PAGE_NONE	__pgprot(_PAGE_BASE | \
-				_CACHE_CACHED)
+#define PAGE_NONE	__pgprot(_PAGE_PROT_NONE)
 #define PAGE_READ	__pgprot(_PAGE_BASE | _PAGE_READ | \
 				_CACHE_CACHED)
 #define PAGE_WRITE	__pgprot(_PAGE_BASE | _PAGE_READ | _PAGE_WRITE | \
@@ -79,6 +71,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 				_PAGE_WRITE | _PAGE_DIRTY | _PAGE_MODIFIED | \
 				_PAGE_GLOBAL | \
 				_CACHE_UNCACHED | _PAGE_SO)
+
+#define _PAGE_CHG_MASK	(~(unsigned long) \
+				(_PAGE_PRESENT | _PAGE_READ | _PAGE_WRITE | \
+				_CACHE_MASK | _PAGE_GLOBAL))
+
+#define MAX_SWAPFILES_CHECK() \
+		BUILD_BUG_ON(MAX_SWAPFILES_SHIFT != 5)
 
 #define __P000	PAGE_NONE
 #define __P001	PAGE_READ
