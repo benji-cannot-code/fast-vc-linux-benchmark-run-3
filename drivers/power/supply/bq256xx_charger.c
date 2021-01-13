3625ab47323cf56f5c080ee8ef456397fde584e6
Ricardo Rivera-Matos <r-rivera-matos@ti.com>
2021-01-13 16:53:52 -0600

    power: supply: bq256xx: Fix BQ256XX_NUM_WD_VAL and bq256xx_watchdog_time[] overrun
    Corrects BQ256XX_NUM_WD_VAL from value of "8" to "4" and fixes the issue when 'i'
    is equal to array size then array index over runs the array
    
    Fixes: 32e4978bb920 ("power: supply: bq256xx: Introduce the BQ256XX charger driver")
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Ricardo Rivera-Matos <r-rivera-matos@ti.com>
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

--- drivers/power/supply/bq256xx_charger.c
+++ drivers/power/supply/bq256xx_charger.c
@@ -136,7 +136,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BQ256XX_NTC_FAULT_COLD		(BIT(2) | BIT(0))
 #define BQ256XX_NTC_FAULT_HOT		(BIT(2) | BIT(1))
 
-#define BQ256XX_NUM_WD_VAL	8
+#define BQ256XX_NUM_WD_VAL	4
 #define BQ256XX_WATCHDOG_MASK	GENMASK(5, 4)
 #define BQ256XX_WATCHDOG_MAX	1600000
 #define BQ256XX_WATCHDOG_DIS	0
@@ -1509,6 +1509,10 @@ static int bq256xx_hw_init(struct bq256xx_device *bq)
 	int i;
 
 	for (i = 0; i < BQ256XX_NUM_WD_VAL; i++) {
+		if (bq->watchdog_timer == bq256xx_watchdog_time[i]) {
+			wd_reg_val = i;
+			break;
+		}
 		if (bq->watchdog_timer > bq256xx_watchdog_time[i] &&
 		    bq->watchdog_timer < bq256xx_watchdog_time[i + 1])
 			wd_reg_val = i;
