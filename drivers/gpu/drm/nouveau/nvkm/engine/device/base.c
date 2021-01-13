e3be30852c90c9de72d75372e5c5b7f86166aac4
Ben Skeggs <bskeggs@redhat.com>
2021-01-13 17:12:52 +1000

    drm/nouveau/dmaobj/ga10[24]: initial support
    Appears to be compatible with GV100 code, and not required on GA100, as
    it shouldn't have display.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
@@ -2685,6 +2685,7 @@ nv172_chipset = {
 	.mmu = tu102_mmu_new,
 	.pci = gp100_pci_new,
 	.timer = gk20a_timer_new,
+	.dma = gv100_dma_new,
 };
 
 static const struct nvkm_device_chip
@@ -2702,6 +2703,7 @@ nv174_chipset = {
 	.mmu = tu102_mmu_new,
 	.pci = gp100_pci_new,
 	.timer = gk20a_timer_new,
+	.dma = gv100_dma_new,
 };
 
 static int
