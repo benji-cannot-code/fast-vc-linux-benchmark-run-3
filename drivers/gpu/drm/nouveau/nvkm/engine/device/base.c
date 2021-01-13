8051e49626d180035d5a7c1e3a31e5463dbb4f88
Ben Skeggs <bskeggs@redhat.com>
2021-01-13 17:12:52 +1000

    drm/nouveau/mc/ga10[024]: initial support
    Fortunately, all the interrupts we need to bring up basic display support
    are contained in a single leaf register, allowing this basic (but hackish)
    implementation.
    
    There's a bunch more invasive patches to come implementing all this in a
    better/more complete way, but trying to get a minimal series out first.
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
@@ -2658,6 +2658,7 @@ nv170_chipset = {
 	.name = "GA100",
 	.bios = nvkm_bios_new,
 	.devinit = ga100_devinit_new,
+	.mc = ga100_mc_new,
 	.pci = gp100_pci_new,
 };
 
@@ -2666,6 +2667,7 @@ nv172_chipset = {
 	.name = "GA102",
 	.bios = nvkm_bios_new,
 	.devinit = ga100_devinit_new,
+	.mc = ga100_mc_new,
 	.pci = gp100_pci_new,
 };
 
@@ -2674,6 +2676,7 @@ nv174_chipset = {
 	.name = "GA104",
 	.bios = nvkm_bios_new,
 	.devinit = ga100_devinit_new,
+	.mc = ga100_mc_new,
 	.pci = gp100_pci_new,
 };
 
