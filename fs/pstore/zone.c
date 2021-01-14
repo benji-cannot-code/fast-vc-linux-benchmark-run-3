95c8570eb58ff56a7e62ecd702d251ddcc96eeb5
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2021-01-14 09:04:40 +0100

    pstore/zone: fix a kernel-doc markup
    The documented struct is psz_head and not psz_buffer.
    
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Link: https://lore.kernel.org/r/a67ca4d12c3ef277dadb9e0d0df8450158e637cc.1610610937.git.mchehab+huawei@kernel.org
    Signed-off-by: Jonathan Corbet <corbet@lwn.net>

--- fs/pstore/zone.c
+++ fs/pstore/zone.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "internal.h"
 
 /**
- * struct psz_head - header of zone to flush to storage
+ * struct psz_buffer - header of zone to flush to storage
  *
  * @sig: signature to indicate header (PSZ_SIG xor PSZONE-type value)
  * @datalen: length of data in @data
