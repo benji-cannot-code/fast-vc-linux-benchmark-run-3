afcbe3532c439f33e754bd933d256a22762c5b17
Tom Rix <trix@redhat.com>
2021-01-14 13:28:27 -0800

    x86: Remove definition of DEBUG
    Defining DEBUG should only be done in development. So remove it.
    
    Signed-off-by: Tom Rix <trix@redhat.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Acked-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Link: https://lkml.kernel.org/r/20210114212827.47584-1-trix@redhat.com

--- arch/x86/mm/mmio-mod.c
+++ arch/x86/mm/mmio-mod.c
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt) "mmiotrace: " fmt
 
-#define DEBUG 1
-
 #include <linux/moduleparam.h>
 #include <linux/debugfs.h>
 #include <linux/slab.h>
