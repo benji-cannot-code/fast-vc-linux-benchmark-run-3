1f491c9d76fe38dad2292124fa7d452ce96a2ef1
Srujana Challa <schalla@marvell.com>
2021-01-15 19:22:27 +0530

    crypto: octeontx2 - register with linux crypto framework
    CPT offload module utilises the linux crypto framework to offload
    crypto processing. This patch registers supported algorithms by
    calling registration functions provided by the kernel crypto API.
    
    The module currently supports:
    - AES block cipher in CBC,ECB and XTS mode.
    - 3DES block cipher in CBC and ECB mode.
    - AEAD algorithms.
      authenc(hmac(sha1),cbc(aes)),
      authenc(hmac(sha256),cbc(aes)),
      authenc(hmac(sha384),cbc(aes)),
      authenc(hmac(sha512),cbc(aes)),
      authenc(hmac(sha1),ecb(cipher_null)),
      authenc(hmac(sha256),ecb(cipher_null)),
      authenc(hmac(sha384),ecb(cipher_null)),
      authenc(hmac(sha512),ecb(cipher_null)),
      rfc4106(gcm(aes)).
    
    Signed-off-by: Suheil Chandran <schandran@marvell.com>
    Signed-off-by: Lukasz Bartosik <lbartosik@marvell.com>
    Signed-off-by: Srujana Challa <schalla@marvell.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/marvell/octeontx2/otx2_cptvf_main.c
+++ drivers/crypto/marvell/octeontx2/otx2_cptvf_main.c
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "otx2_cpt_common.h"
 #include "otx2_cptvf.h"
 #include "otx2_cptlf.h"
+#include "otx2_cptvf_algs.h"
 #include <rvu_reg.h>
 
 #define OTX2_CPTVF_DRV_NAME "octeontx2-cptvf"
@@ -215,6 +216,8 @@ static void cptvf_lf_shutdown(struct otx2_cptlfs_info *lfs)
 	otx2_cptlf_free_irqs_affinity(lfs);
 	/* Disable instruction queue */
 	otx2_cptlf_disable_iqueues(lfs);
+	/* Unregister crypto algorithms */
+	otx2_cpt_crypto_exit(lfs->pdev, THIS_MODULE);
 	/* Unregister LFs interrupts */
 	otx2_cptlf_unregister_interrupts(lfs);
 	/* Cleanup LFs software side */
@@ -279,9 +282,16 @@ static int cptvf_lf_init(struct otx2_cptvf_dev *cptvf)
 		goto unregister_intr;
 
 	atomic_set(&lfs->state, OTX2_CPTLF_STARTED);
-
+	/* Register crypto algorithms */
+	ret = otx2_cpt_crypto_init(lfs->pdev, THIS_MODULE, lfs_num, 1);
+	if (ret) {
+		dev_err(&lfs->pdev->dev, "algorithms registration failed\n");
+		goto disable_irqs;
+	}
 	return 0;
 
+disable_irqs:
+	otx2_cptlf_free_irqs_affinity(lfs);
 unregister_intr:
 	otx2_cptlf_unregister_interrupts(lfs);
 cleanup_lf_sw:
