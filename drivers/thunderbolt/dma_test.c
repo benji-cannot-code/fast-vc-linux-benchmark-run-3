ae514b6e2ae48bb4682c42e20c845f13fc13976a
Mika Westerberg <mika.westerberg@linux.intel.com>
2021-01-15 11:01:21 +0300

    thunderbolt: dma_test: Drop unnecessary include
    It seems <linux/acpi.h> is not actually needed in this driver so we can
    drop it.
    
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>

--- drivers/thunderbolt/dma_test.c
+++ drivers/thunderbolt/dma_test.c
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	    Mika Westerberg <mika.westerberg@linux.intel.com>
  */
 
-#include <linux/acpi.h>
 #include <linux/completion.h>
 #include <linux/debugfs.h>
 #include <linux/module.h>
