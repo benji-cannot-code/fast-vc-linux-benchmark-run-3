0d86165d1cbd03899a48d73b8e2477942b2d42f8
Tom Rix <trix@redhat.com>
2021-01-15 07:33:48 -0800

    tracing: Remove definition of DEBUG in trace_mmiotrace.c
    Defining DEBUG should only be done in development.
    So remove DEBUG.
    
    Link: https://lkml.kernel.org/r/20210115153348.131791-1-trix@redhat.com
    
    Signed-off-by: Tom Rix <trix@redhat.com>
    Reviewed-by: Karol Herbst <kherbst@redhat.com>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/trace/trace_mmiotrace.c
+++ kernel/trace/trace_mmiotrace.c
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2008 Pekka Paalanen <pq@iki.fi>
  */
 
-#define DEBUG 1
-
 #include <linux/kernel.h>
 #include <linux/mmiotrace.h>
 #include <linux/pci.h>
