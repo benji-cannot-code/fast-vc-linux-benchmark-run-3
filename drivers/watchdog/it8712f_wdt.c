2af678fdfd67e642dfb86f214cbf87610030c25e
Tom Rix <trix@redhat.com>
2021-01-15 07:32:37 -0800

    watchdog: it8712f_wdt: remove definition of DEBUG
    Defining DEBUG should only be done in development.
    So remove DEBUG.
    
    Signed-off-by: Tom Rix <trix@redhat.com>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Link: https://lore.kernel.org/r/20210115153237.131357-1-trix@redhat.com
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Wim Van Sebroeck <wim@linux-watchdog.org>

--- drivers/watchdog/it8712f_wdt.c
+++ drivers/watchdog/it8712f_wdt.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/io.h>
 #include <linux/ioport.h>
 
-#define DEBUG
 #define NAME "it8712f_wdt"
 
 MODULE_AUTHOR("Jorge Boncompte - DTI2 <jorge@dti2.net>");
