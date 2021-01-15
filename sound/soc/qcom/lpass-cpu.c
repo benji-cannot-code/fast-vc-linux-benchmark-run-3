08c7a7ad96d915cfd28b0fded0fd89c91c073f7b
Stephen Boyd <swboyd@chromium.org>
2021-01-14 19:43:25 -0800

    ASoC: qcom: Add some names to regmap configs
    This device can sometimes have multiple regmaps. Let's add a name so
    that we can differentiate in debugfs more easily.
    
    Cc: V Sujith Kumar Reddy <vsujithk@codeaurora.org>
    Cc: Srinivasa Rao <srivasam@codeaurora.org>
    Cc: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Cc: Cheng-Yi Chiang <cychiang@chromium.org>
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>
    Reviewed-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Link: https://lore.kernel.org/r/20210115034327.617223-3-swboyd@chromium.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/qcom/lpass-cpu.c
+++ sound/soc/qcom/lpass-cpu.c
@@ -475,6 +475,7 @@ static bool lpass_cpu_regmap_volatile(struct device *dev, unsigned int reg)
 }
 
 static struct regmap_config lpass_cpu_regmap_config = {
+	.name = "lpass_cpu",
 	.reg_bits = 32,
 	.reg_stride = 4,
 	.val_bits = 32,
@@ -675,6 +676,7 @@ static bool lpass_hdmi_regmap_volatile(struct device *dev, unsigned int reg)
 }
 
 static struct regmap_config lpass_hdmi_regmap_config = {
+	.name = "lpass_hdmi",
 	.reg_bits = 32,
 	.reg_stride = 4,
 	.val_bits = 32,
