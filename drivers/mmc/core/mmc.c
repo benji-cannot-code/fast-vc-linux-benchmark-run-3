0a53b82d3fa4c3ba5c2862895ba07111522fdab1
Yue Hu <huyue2@yulong.com>
2021-01-15 11:45:06 +0800

    mmc: core: remove redundant card null check to mmc_can_sleep()
    Note that only _mmc_suspend() will call mmc_can_sleep(). And card is
    checked before in mmc_can_poweroff_notify().
    
    Signed-off-by: Yue Hu <huyue2@yulong.com>
    Link: https://lore.kernel.org/r/20210115034506.646-1-zbestahu@gmail.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1896,7 +1896,7 @@ err:
 
 static int mmc_can_sleep(struct mmc_card *card)
 {
-	return (card && card->ext_csd.rev >= 3);
+	return card->ext_csd.rev >= 3;
 }
 
 static int mmc_sleep(struct mmc_host *host)
