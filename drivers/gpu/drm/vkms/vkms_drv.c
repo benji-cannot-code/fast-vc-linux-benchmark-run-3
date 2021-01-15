79ff6f33febc5438beeb0d37595f49bccb69fe5b
Colin Ian King <colin.king@canonical.com>
2021-01-15 13:09:11 +0000

    drm/vkms: Fix missing kmalloc allocation failure check
    Currently the kmalloc allocation for config is not being null
    checked and could potentially lead to a null pointer dereference.
    Fix this by adding the missing null check.
    
    Addresses-Coverity: ("Dereference null return value")
    Fixes: 2df7af93fdad ("drm/vkms: Add vkms_config type")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Reviewed-by: Sumera Priyadarsini <sylphrenadin@gmail.com>
    Signed-off-by: Melissa Wen <melissa.srw@gmail.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210115130911.71073-1-colin.king@canonical.com

--- drivers/gpu/drm/vkms/vkms_drv.c
+++ drivers/gpu/drm/vkms/vkms_drv.c
@@ -189,7 +189,11 @@ out_unregister:
 
 static int __init vkms_init(void)
 {
-	struct vkms_config *config = kmalloc(sizeof(*config), GFP_KERNEL);
+	struct vkms_config *config;
+
+	config = kmalloc(sizeof(*config), GFP_KERNEL);
+	if (!config)
+		return -ENOMEM;
 
 	default_config = config;
 
