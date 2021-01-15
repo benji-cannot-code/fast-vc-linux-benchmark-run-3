09979f74abb1ef217a2a2e684344a9d00fef056c
Srujana Challa <schalla@marvell.com>
2021-01-15 19:22:20 +0530

    crypto: octeontx2 - add mailbox communication with AF
    In the resource virtualization unit (RVU) each of the PF and AF
    (admin function) share a 64KB of reserved memory region for
    communication. This patch initializes PF <=> AF mailbox IRQs,
    registers handlers for processing these communication messages.
    
    Signed-off-by: Suheil Chandran <schandran@marvell.com>
    Signed-off-by: Lukasz Bartosik <lbartosik@marvell.com>
    Signed-off-by: Srujana Challa <schalla@marvell.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/marvell/octeontx2/otx2_cpt_common.h
+++ drivers/crypto/marvell/octeontx2/otx2_cpt_common.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/crypto.h>
 #include "otx2_cpt_hw_types.h"
 #include "rvu.h"
+#include "mbox.h"
 
 #define OTX2_CPT_RVU_FUNC_ADDR_S(blk, slot, offs) \
 		(((blk) << 20) | ((slot) << 12) | (offs))
@@ -30,4 +31,7 @@ static inline u64 otx2_cpt_read64(void __iomem *reg_base, u64 blk, u64 slot,
 	return readq_relaxed(reg_base +
 			     OTX2_CPT_RVU_FUNC_ADDR_S(blk, slot, offs));
 }
+
+int otx2_cpt_send_ready_msg(struct otx2_mbox *mbox, struct pci_dev *pdev);
+int otx2_cpt_send_mbox_msg(struct otx2_mbox *mbox, struct pci_dev *pdev);
 #endif /* __OTX2_CPT_COMMON_H */
