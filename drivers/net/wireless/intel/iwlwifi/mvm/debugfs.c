7aaa8b3db1ff8323ef0edc02ea243b364ebdf05b
Mordechay Goodstein <mordechay.goodstein@intel.com>
2021-01-17 13:10:33 +0200

    iwlwifi: mvm: add support for new flush queue response
    In the new api all the flush in the FW is done before we
    get the response and in the response we only get the updated
    read pointer and all queued packets don't get anymore rx_tx
    per packet to free the queued packet, so driver needs to free
    all queued packets on flushed queue at once after flush response.
    
    Signed-off-by: Mordechay Goodstein <mordechay.goodstein@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Link: https://lore.kernel.org/r/iwlwifi.20210117130510.4bd0eca8c0ef.I1601aad2eb2cc83f6f73b8ca52be57bb9fd626ab@changeid
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/debugfs.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/debugfs.c
@@ -92,7 +92,7 @@ static ssize_t iwl_dbgfs_tx_flush_write(struct iwl_mvm *mvm, char *buf,
 				    "FLUSHING all tids queues on sta_id = %d\n",
 				    flush_arg);
 		mutex_lock(&mvm->mutex);
-		ret = iwl_mvm_flush_sta_tids(mvm, flush_arg, 0xFFFF, 0)
+		ret = iwl_mvm_flush_sta_tids(mvm, flush_arg, 0xFFFF)
 			? : count;
 		mutex_unlock(&mvm->mutex);
 		return ret;
@@ -102,7 +102,7 @@ static ssize_t iwl_dbgfs_tx_flush_write(struct iwl_mvm *mvm, char *buf,
 			    flush_arg);
 
 	mutex_lock(&mvm->mutex);
-	ret =  iwl_mvm_flush_tx_path(mvm, flush_arg, 0) ? : count;
+	ret =  iwl_mvm_flush_tx_path(mvm, flush_arg) ? : count;
 	mutex_unlock(&mvm->mutex);
 
 	return ret;
