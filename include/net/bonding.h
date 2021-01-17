7feac04b3a9968a15457c7f8de058f72fd2b454d
Tariq Toukan <tariqt@nvidia.com>
2021-01-17 16:59:46 +0200

    net/bonding: Implement TLS TX device offload
    Implement TLS TX device offload for bonding interfaces.
    This allows kTLS sockets running on a bond to benefit from the
    device offload on capable lower devices.
    
    To allow a simple and fast maintenance of the TLS context in SW and
    lower devices, we bind the TLS socket to a specific lower dev.
    To achieve a behavior similar to SW kTLS, we support only balance-xor
    and 802.3ad modes, with xmit_hash_policy=layer3+4. This is enforced
    in bond_sk_check(), done in a previous patch.
    
    For the above configuration, the SW implementation keeps picking the
    same exact lower dev for all the socket's SKBs. The device offload
    behaves similarly, making the decision once at the connection creation.
    
    Per socket, the TLS module should work directly with the lowest netdev
    in chain, to call the tls_dev_ops operations.
    
    As the bond interface is being bypassed by the TLS module, interacting
    directly against the lower devs, there is no way for the bond interface
    to disable its device offload capabilities, as long as the mode/policy
    config allows it.
    Hence, the feature flag is not directly controllable, but just reflects
    the current offload status based on the logic under bond_sk_check().
    
    Signed-off-by: Tariq Toukan <tariqt@nvidia.com>
    Reviewed-by: Boris Pismenny <borisp@nvidia.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/net/bonding.h
+++ include/net/bonding.h
@@ -90,6 +90,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BOND_XFRM_FEATURES (NETIF_F_HW_ESP | NETIF_F_HW_ESP_TX_CSUM | \
 			    NETIF_F_GSO_ESP)
 
+#define BOND_TLS_FEATURES (NETIF_F_HW_TLS_TX)
+
 #ifdef CONFIG_NET_POLL_CONTROLLER
 extern atomic_t netpoll_block_tx;
 
