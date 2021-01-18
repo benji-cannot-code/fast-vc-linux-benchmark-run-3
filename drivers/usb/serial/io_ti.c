40c325c239ba3b217ff204eb9393fc5e3be52804
Johan Hovold <johan@kernel.org>
2021-01-18 12:14:24 +0100

    USB: serial: io_ti: fix a debug-message copy-paste error
    Fix a copy-paste error in the ti_vread_sync() debug message.
    
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Johan Hovold <johan@kernel.org>

--- drivers/usb/serial/io_ti.c
+++ drivers/usb/serial/io_ti.c
@@ -267,7 +267,7 @@ static int ti_vread_sync(struct usb_device *dev, __u8 request,
 	if (status < 0)
 		return status;
 	if (status != size) {
-		dev_dbg(&dev->dev, "%s - wanted to write %d, but only wrote %d\n",
+		dev_dbg(&dev->dev, "%s - wanted to read %d, but only read %d\n",
 			__func__, size, status);
 		return -ECOMM;
 	}
