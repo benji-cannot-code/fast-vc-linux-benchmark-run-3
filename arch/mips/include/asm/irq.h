a28d0960a4be29934f5b8e58267d055ffed31cb1
Yanteng Si <siyanteng@loongson.cn>
2021-01-18 21:20:09 +0800

    MIPS: IRQ: Add prototype for function init_IRQ
    This commit adds a prototype to fix error at W=1:
    
    arch/mips/kernel/irq.c:52:13: error: no previous prototype for
    'init_IRQ' [-Werror=missing-prototypes]
    
    Signed-off-by: Yanteng Si <siyanteng@loongson.cn>
    Reviewed-by: Huacai Chen <chenhuacai@kernel.org>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/include/asm/irq.h
+++ arch/mips/include/asm/irq.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IRQ_STACK_SIZE			THREAD_SIZE
 #define IRQ_STACK_START			(IRQ_STACK_SIZE - 16)
 
+extern void __init init_IRQ(void);
 extern void *irq_stack[NR_CPUS];
 
 /*
