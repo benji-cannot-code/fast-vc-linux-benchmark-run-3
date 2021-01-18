d419d5cb5bb263b31382d337f04887a466d345b2
Arnd Bergmann <arnd@arndb.de>
2021-01-18 12:45:46 +0100

    c6x: remove architecture
    The c6x architecture was added to the kernel in 2011 at a time when
    running Linux on DSPs was widely seen as the logical evolution.
    It appears the trend has gone back to running Linux on Arm based SoCs
    with DSP, using a better supported software ecosystem, and having better
    real-time behavior for the DSP code. An example of this is TI's own
    Keystone2 platform.
    
    The upstream kernel port appears to no longer have any users. Mark
    Salter remained avaialable to review patches, but mentioned that
    he no longer has access to working hardware himself. Without any
    users, it's best to just remove the code completely to reduce the
    work for cross-architecture code changes.
    
    Many thanks to Mark for maintaining the code for the past ten years.
    
    Link: https://lore.kernel.org/lkml/41dc7795afda9f776d8cd0d3075f776cf586e97c.camel@redhat.com/
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/c6x/include/uapi/asm/setup.h
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
-#ifndef _UAPI_ASM_C6X_SETUP_H
-#define _UAPI_ASM_C6X_SETUP_H
-
-#define COMMAND_LINE_SIZE   1024
-
-#endif /* _UAPI_ASM_C6X_SETUP_H */
