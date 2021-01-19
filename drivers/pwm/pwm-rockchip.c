1f188fd87d8e08157aeb77838cd4b276982b2255
Simon South <simon@simonsouth.net>
2021-01-19 11:12:08 -0500

    pwm: rockchip: Eliminate potential race condition when probing
    Commit 48cf973cae33 ("pwm: rockchip: Avoid glitches on already running
    PWMs") introduced a potential race condition in rockchip_pwm_probe(): A
    consumer could enable an inactive PWM, or disable a running one, between
    rockchip_pwm_probe() registering the device via pwmchip_add() and checking
    whether it is enabled (to determine whether it was started by a
    bootloader). This could result in a device's PWM clock being either enabled
    once more than necessary, potentially causing it to continue running when
    no longer needed, or disabled once more than necessary, producing a warning
    from the kernel.
    
    Eliminate these possibilities by modifying rockchip_pwm_probe() so it
    checks whether a device is enabled before registering it rather than after.
    
    Fixes: 48cf973cae33 ("pwm: rockchip: Avoid glitches on already running PWMs")
    Reported-by: Trent Piepho <tpiepho@gmail.com>
    Signed-off-by: Simon South <simon@simonsouth.net>
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- drivers/pwm/pwm-rockchip.c
+++ drivers/pwm/pwm-rockchip.c
@@ -289,6 +289,7 @@ static int rockchip_pwm_probe(struct platform_device *pdev)
 	const struct of_device_id *id;
 	struct rockchip_pwm_chip *pc;
 	u32 enable_conf, ctrl;
+	bool enabled;
 	int ret, count;
 
 	id = of_match_device(rockchip_pwm_dt_ids, &pdev->dev);
@@ -350,6 +351,10 @@ static int rockchip_pwm_probe(struct platform_device *pdev)
 		pc->chip.of_pwm_n_cells = 3;
 	}
 
+	enable_conf = pc->data->enable_conf;
+	ctrl = readl_relaxed(pc->base + pc->data->regs.ctrl);
+	enabled = (ctrl & enable_conf) == enable_conf;
+
 	ret = pwmchip_add(&pc->chip);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "pwmchip_add() failed: %d\n", ret);
@@ -357,9 +362,7 @@ static int rockchip_pwm_probe(struct platform_device *pdev)
 	}
 
 	/* Keep the PWM clk enabled if the PWM appears to be up and running. */
-	enable_conf = pc->data->enable_conf;
-	ctrl = readl_relaxed(pc->base + pc->data->regs.ctrl);
-	if ((ctrl & enable_conf) != enable_conf)
+	if (!enabled)
 		clk_disable(pc->clk);
 
 	clk_disable(pc->pclk);
