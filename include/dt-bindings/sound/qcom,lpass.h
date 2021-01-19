4c6c59c572f40f5ba9cdaf67f2d7b4b9997b630a
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2021-01-19 17:15:26 +0000

    ASoC: dt-bindings: lpass: Fix and common up lpass dai ids
    Existing header file design of having separate SoC specific header files
    for the common lpass driver has mutiple issues.
    This design is prone to break as an when new SoC header is added
    as the common DAI ids of other SoCs will be overwritten by the
    new ones.
    
    One of them surfaced by recent patch that adds support to sc7180, this
    one totally broke LPASS drivers on other Qualcomm SoCs.
    
    Before this gets worst, fix this by having a common header qcom,lpass.h.
    This should fix the issue and any new DAI ids should be added to the
    common header. This will be more sustainable then the existing design!
    
    Fixes: 12fbfc4cabec6595 ("ASoC: Add sc7180-lpass binding header hdmi define")
    Reported-by: Jun Nie <jun.nie@linaro.org>
    Reported-by: Stephan Gerhold <stephan@gerhold.net>
    Tested-by: Srinivasa Rao <srivasam@codeaurora.org>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Link: https://lore.kernel.org/r/20210119171527.32145-2-srinivas.kandagatla@linaro.org
    Signed-off-by: Mark Brown <broonie@kernel.org>

---
+++ include/dt-bindings/sound/qcom,lpass.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __DT_QCOM_LPASS_H
+#define __DT_QCOM_LPASS_H
+
+#define MI2S_PRIMARY	0
+#define MI2S_SECONDARY	1
+#define MI2S_TERTIARY	2
+#define MI2S_QUATERNARY	3
+#define MI2S_QUINARY	4
+
+#define LPASS_DP_RX	5
+
+#define LPASS_MCLK0	0
+
+#endif /* __DT_QCOM_LPASS_H */
