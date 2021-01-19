fdd30455e5bd0b44617f528e5dc05dfa98bc5221
Rasmus Villemoes <rasmus.villemoes@prevas.dk>
2021-01-19 16:07:48 +0100

    soc: fsl: qe: store muram_vbase as a void pointer instead of u8
    The two functions cpm_muram_offset() and cpm_muram_dma() both need a
    cast currently, one casts muram_vbase to do the pointer arithmetic on
    void pointers, the other casts the passed-in address u8*.
    
    It's simpler and more consistent to just always use void* and drop all
    the casting.
    
    Signed-off-by: Rasmus Villemoes <rasmus.villemoes@prevas.dk>
    Acked-by: Li Yang <leoyang.li@nxp.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/soc/fsl/qe/qe_common.c
+++ drivers/soc/fsl/qe/qe_common.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static struct gen_pool *muram_pool;
 static spinlock_t cpm_muram_lock;
-static u8 __iomem *muram_vbase;
+static void __iomem *muram_vbase;
 static phys_addr_t muram_pbase;
 
 struct muram_block {
@@ -226,7 +226,7 @@ EXPORT_SYMBOL(cpm_muram_addr);
 
 unsigned long cpm_muram_offset(const void __iomem *addr)
 {
-	return addr - (void __iomem *)muram_vbase;
+	return addr - muram_vbase;
 }
 EXPORT_SYMBOL(cpm_muram_offset);
 
@@ -236,6 +236,6 @@ EXPORT_SYMBOL(cpm_muram_offset);
  */
 dma_addr_t cpm_muram_dma(void __iomem *addr)
 {
-	return muram_pbase + ((u8 __iomem *)addr - muram_vbase);
+	return muram_pbase + (addr - muram_vbase);
 }
 EXPORT_SYMBOL(cpm_muram_dma);
