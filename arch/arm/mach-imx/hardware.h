d4d9938bb6bd945857cd723ad437cd3ec31caf3f
Fabio Estevam <festevam@gmail.com>
2021-01-18 21:42:51 -0300

    ARM: imx: Remove unused IMX_GPIO_NR() macro
    The IMX_GPIO_NR() macro was only used by non-DT i.MX platforms.
    
    As i.MX transitioned to a DT-only platform, get rid of this
    unused macro.
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Signed-off-by: Shawn Guo <shawnguo@kernel.org>

--- arch/arm/mach-imx/hardware.h
+++ arch/arm/mach-imx/hardware.h
@@ -107,8 +107,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	.type = _type,							\
 }
 
-/* There's an off-by-one between the gpio bank number and the gpiochip */
-/* range e.g. GPIO_1_5 is gpio 5 under linux */
-#define IMX_GPIO_NR(bank, nr)		(((bank) - 1) * 32 + (nr))
-
 #endif /* __ASM_ARCH_MXC_HARDWARE_H__ */
