a2ed0c610dfe4bdfac05ec17664d71c5709af3f8
Rasmus Villemoes <rasmus.villemoes@prevas.dk>
2021-01-19 16:07:58 +0100

    ethernet: ucc_geth: remove bd_mem_part and all associated code
    The bd_mem_part member of ucc_geth_info always has the value
    MEM_PART_SYSTEM, and AFAICT, there has never been any code setting it
    to any other value. Moreover, muram is a somewhat precious resource,
    so there's no point using that when normal memory serves just as well.
    
    Apart from removing a lot of dead code, this is also motivated by
    wanting to clean up the "store result from kmalloc() in a u32" mess.
    
    Signed-off-by: Rasmus Villemoes <rasmus.villemoes@prevas.dk>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/soc/fsl/qe/qe.h
+++ include/soc/fsl/qe/qe.h
@@ -28,12 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define QE_NUM_OF_BRGS	16
 #define QE_NUM_OF_PORTS	1024
 
-/* Memory partitions
-*/
-#define MEM_PART_SYSTEM		0
-#define MEM_PART_SECONDARY	1
-#define MEM_PART_MURAM		2
-
 /* Clocks and BRGs */
 enum qe_clock {
 	QE_CLK_NONE = 0,
