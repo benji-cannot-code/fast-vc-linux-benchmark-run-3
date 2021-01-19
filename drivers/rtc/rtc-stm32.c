6dda05c41eb9aa2d0a7fbd0337469bdeb2c80959
Alexandre Belloni <alexandre.belloni@bootlin.com>
2021-01-19 23:06:52 +0100

    rtc: stm32: use rtc_lock/rtc_unlock
    Avoid accessing directly rtc->ops_lock and use the RTC core helpers.
    
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Link: https://lore.kernel.org/r/20210119220653.677750-14-alexandre.belloni@bootlin.com

--- drivers/rtc/rtc-stm32.c
+++ drivers/rtc/rtc-stm32.c
@@ -210,7 +210,7 @@ static irqreturn_t stm32_rtc_alarm_irq(int irq, void *dev_id)
 	const struct stm32_rtc_events *evts = &rtc->data->events;
 	unsigned int status, cr;
 
-	mutex_lock(&rtc->rtc_dev->ops_lock);
+	rtc_lock(rtc->rtc_dev);
 
 	status = readl_relaxed(rtc->base + regs->sr);
 	cr = readl_relaxed(rtc->base + regs->cr);
@@ -227,7 +227,7 @@ static irqreturn_t stm32_rtc_alarm_irq(int irq, void *dev_id)
 		stm32_rtc_clear_event_flags(rtc, evts->alra);
 	}
 
-	mutex_unlock(&rtc->rtc_dev->ops_lock);
+	rtc_unlock(rtc->rtc_dev);
 
 	return IRQ_HANDLED;
 }
