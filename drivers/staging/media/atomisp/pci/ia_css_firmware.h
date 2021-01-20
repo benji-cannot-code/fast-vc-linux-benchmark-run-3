1bb1891812874a1ef6fb334648f76fd6ec8276e7
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2021-01-20 10:11:33 +0100

    media: atomisp/pci: add missing include
    Fix two smatch warnings:
    
    drivers/staging/media/atomisp//pci/ia_css_firmware.h:52:29: warning: 'struct device' declared inside parameter list will not be visible outside of this definition or declaration
    drivers/staging/media/atomisp//pci/ia_css_control.h:49:24: warning: 'struct device' declared inside parameter list will not be visible outside of this definition or declaration
    
    Add '#include <linux/device.h>' to ia_css_firmware.h so struct device is defined.
    
    ia_css_control.h includes ia_css_firmware.h, so it is sufficient to
    just modify ia_css_firmware.h.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>

--- drivers/staging/media/atomisp/pci/ia_css_firmware.h
+++ drivers/staging/media/atomisp/pci/ia_css_firmware.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * This file contains firmware loading/unloading support functionality
  */
 
+#include <linux/device.h>
 #include "ia_css_err.h"
 #include "ia_css_env.h"
 
