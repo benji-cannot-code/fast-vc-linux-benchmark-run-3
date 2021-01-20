cf1f60a4827474fa90709de7c54fb01dcdd5c53f
Linus Walleij <linus.walleij@linaro.org>
2021-01-20 23:50:45 +0100

    usb: typec: tcpci_maxim: Drop GPIO includes
    This driver includes the legacy GPIO header <linux/gpio.h>,
    the new GPIO header <linux/gpio/consumer.h> and
    the deprecated OF GPIO header <linux/of_gpio.h> yet
    fail to use symbols from any of them, so drop these
    includes.
    
    Cc: Guenter Roeck <linux@roeck-us.net>
    Cc: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Reviewed-by: Guenter Roeck <linux@roeck-us.net>
    Reviewed-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20210120225045.173556-1-linus.walleij@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/typec/tcpm/tcpci_maxim.c
+++ drivers/usb/typec/tcpm/tcpci_maxim.c
@@ -6,13 +6,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * MAXIM TCPCI based TCPC driver
  */
 
-#include <linux/gpio.h>
-#include <linux/gpio/consumer.h>
 #include <linux/interrupt.h>
 #include <linux/i2c.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/of_gpio.h>
 #include <linux/regmap.h>
 #include <linux/usb/pd.h>
 #include <linux/usb/tcpm.h>
