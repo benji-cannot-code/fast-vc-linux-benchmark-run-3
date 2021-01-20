3a793f602e9e31fc9dad7ecf6f2c83561fee9604
Michal Suchanek <msuchanek@suse.de>
2021-01-20 14:28:38 +0100

    powerpc: Fix build error in paravirt.h
    ./arch/powerpc/include/asm/paravirt.h:83:44: error: implicit declaration
    of function 'smp_processor_id'; did you mean 'raw_smp_processor_id'?
    
    smp_processor_id is defined in linux/smp.h but it is not included.
    
    The build error happens only when the patch is applied to 5.3 kernel but
    it only works by chance in mainline.
    
    Fixes: ca3f969dcb11 ("powerpc/paravirt: Use is_kvm_guest() in vcpu_is_preempted()")
    Signed-off-by: Michal Suchanek <msuchanek@suse.de>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20210120132838.15589-1-msuchanek@suse.de

--- arch/powerpc/include/asm/paravirt.h
+++ arch/powerpc/include/asm/paravirt.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #ifdef CONFIG_PPC_SPLPAR
+#include <linux/smp.h>
 #include <asm/kvm_guest.h>
 #include <asm/cputhreads.h>
 
