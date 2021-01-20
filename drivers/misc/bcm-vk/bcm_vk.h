59fc4ccb87e4b165a9a1a53bd82fb8e2f5c28fb6
Scott Branden <scott.branden@broadcom.com>
2021-01-20 09:58:18 -0800

    misc: bcm-vk: add misc device to Broadcom VK driver
    Add misc device base support to create and remove devnode.
    Additional misc functions for open/read/write/release/ioctl/sysfs, etc
    will be added in follow on commits to allow for individual review.
    
    Co-developed-by: Desmond Yan <desmond.yan@broadcom.com>
    Acked-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Desmond Yan <desmond.yan@broadcom.com>
    Signed-off-by: Scott Branden <scott.branden@broadcom.com>
    Link: https://lore.kernel.org/r/20210120175827.14820-5-scott.branden@broadcom.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/bcm-vk/bcm_vk.h
+++ drivers/misc/bcm-vk/bcm_vk.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BCM_VK_H
 
 #include <linux/firmware.h>
+#include <linux/miscdevice.h>
 #include <linux/pci.h>
 #include <linux/sched/signal.h>
 
@@ -215,6 +216,7 @@ struct bcm_vk {
 
 	struct bcm_vk_dauth_info dauth_info;
 
+	struct miscdevice miscdev;
 	int devid; /* dev id allocated */
 
 	struct workqueue_struct *wq_thread;
