5957d2d075cac55923d2a1f7acb1fc65942ce9ef
Arnd Bergmann <arnd@arndb.de>
2021-01-20 14:18:57 +0100

    dmaengine: remove sirfsoc driver
    The CSR SiRF prima2/atlas platforms are getting removed, so this driver
    is no longer needed.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Barry Song <baohua@kernel.org>
    Cc: Barry Song <baohua@kernel.org>
    Link: https://lore.kernel.org/r/20210120131859.2056308-2-arnd@kernel.org
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- include/linux/sirfsoc_dma.h
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _SIRFSOC_DMA_H_
-#define _SIRFSOC_DMA_H_
-
-bool sirfsoc_dma_filter_id(struct dma_chan *chan, void *chan_id);
-
-#endif
