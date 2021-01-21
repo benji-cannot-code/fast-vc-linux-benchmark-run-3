950b3ead0f14e5af6eea73b372b7743464780aea
Johan Hovold <johan@kernel.org>
2021-01-21 11:29:16 +0100

    USB: serial: xr: use termios flag helpers
    Use the termios flag helpers consistently, including for CRTSCTS.
    
    Signed-off-by: Johan Hovold <johan@kernel.org>

--- drivers/usb/serial/xr_serial.c
+++ drivers/usb/serial/xr_serial.c
@@ -402,7 +402,6 @@ static int xr_set_baudrate(struct tty_struct *tty,
 static void xr_set_flow_mode(struct tty_struct *tty,
 			     struct usb_serial_port *port)
 {
-	unsigned int cflag = tty->termios.c_cflag;
 	u8 flow, gpio_mode;
 	int ret;
 
@@ -410,7 +409,7 @@ static void xr_set_flow_mode(struct tty_struct *tty,
 	if (ret)
 		return;
 
-	if (cflag & CRTSCTS) {
+	if (C_CRTSCTS(tty)) {
 		dev_dbg(&port->dev, "Enabling hardware flow ctrl\n");
 
 		/*
