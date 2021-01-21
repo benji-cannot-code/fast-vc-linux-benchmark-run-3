17d44582bb535e1a06e41a203d902f1e3d8868cd
Willy Tarreau <w@1wt.eu>
2021-01-21 08:20:31 +0100

    tools/nolibc: Fix position of -lgcc in the documented example
    The documentation header in the nolibc.h file provides an example command
    line, but it places the -lgcc argument before the source files, which
    can fail with libgcc.a (e.g. on ARM when uidiv is needed). This commit
    therefore moves the -lgcc to the end of the command line, hopefully
    before this example leaks into makefiles.  This is a port of nolibc's
    upstream commit b5e282089223 to the Linux kernel.
    
    Fixes: 66b6f755ad45 ("rcutorture: Import a copy of nolibc")
    Tested-by: Valentin Schneider <valentin.schneider@arm.com>
    Tested-by: Mark Rutland <mark.rutland@arm.com> [arm64]
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- tools/include/nolibc/nolibc.h
+++ tools/include/nolibc/nolibc.h
@@ -72,7 +72,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * A simple static executable may be built this way :
  *      $ gcc -fno-asynchronous-unwind-tables -fno-ident -s -Os -nostdlib \
- *            -static -include nolibc.h -lgcc -o hello hello.c
+ *            -static -include nolibc.h -o hello hello.c -lgcc
  *
  * A very useful calling convention table may be found here :
  *      http://man7.org/linux/man-pages/man2/syscall.2.html
