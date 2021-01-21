7eceb4631581b498ef0711bd3b1edcd44e3af330
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2021-01-21 08:18:12 +0100

    staging: hikey9xx: hi6421v600-regulator: fix delay logic
    The original driver, which can be seen at
    commit 42f24d9d446a ("staging: regulator: add a regulator driver for HiSilicon 6421v600 SPMI PMIC")
    had a complex logic to ensure that there won't be multiple power
    enable/disable commands running at the same time. At the original
    logic, it were ensured that:
    
    - a next power up/down would wait for at least the on/off period;
    - an extra delay would be granted. It turns that such extra delay
      has a value of zero, but it was relying on gettimeofday()
      call, which can take some time.
    
    This was later simplified, but there are still some possible
    issues. In order to avoid that, let's simply add a delay
    to wait for the power up line to stabilize after powering up
    a device.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Link: https://lore.kernel.org/r/6733dac9813ba6688def404142cb7b964accf758.1611212783.git.mchehab+huawei@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/mfd/hi6421-spmi-pmic.h
+++ include/linux/mfd/hi6421-spmi-pmic.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/irqdomain.h>
 
-#define HISI_REGS_ENA_PROTECT_TIME	(0)	/* in microseconds */
 #define HISI_ECO_MODE_ENABLE		(1)
 #define HISI_ECO_MODE_DISABLE		(0)
 
