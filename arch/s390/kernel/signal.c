33c50e7de232f1970917550f9b2876967afa6a92
Sven Schnelle <svens@linux.ibm.com>
2021-01-21 10:36:13 +0100

    s390: add missing include to arch/s390/kernel/signal.c
    This fixes the following warning:
    
    CHECK   linux/arch/s390/kernel/signal.c
    linux/arch/s390/kernel/signal.c:465:6: warning: symbol 'arch_do_signal_or_restart' was not declared. Should it be static?
    
    Signed-off-by: Sven Schnelle <svens@linux.ibm.com>
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- arch/s390/kernel/signal.c
+++ arch/s390/kernel/signal.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/smp.h>
 #include <linux/kernel.h>
 #include <linux/signal.h>
+#include <linux/entry-common.h>
 #include <linux/errno.h>
 #include <linux/wait.h>
 #include <linux/ptrace.h>
