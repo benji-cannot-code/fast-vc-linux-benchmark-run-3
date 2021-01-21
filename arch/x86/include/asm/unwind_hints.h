eb50e79f26c499bf1a0b53afe873c42e37b366e8
Josh Poimboeuf <jpoimboe@redhat.com>
2021-01-21 15:29:24 -0600

    objtool: Combine UNWIND_HINT_RET_OFFSET and UNWIND_HINT_FUNC
    The ORC metadata generated for UNWIND_HINT_FUNC isn't actually very
    func-like.  With certain usages it can cause stack state mismatches
    because it doesn't set the return address (CFI_RA).
    
    Also, users of UNWIND_HINT_RET_OFFSET no longer need to set a custom
    return stack offset.  Instead they just need to specify a func-like
    situation, so the current ret_offset code is hacky for no good reason.
    
    Solve both problems by simplifying the RET_OFFSET handling and
    converting it into a more useful UNWIND_HINT_FUNC.
    
    If we end up needing the old 'ret_offset' functionality again in the
    future, we should be able to support it pretty easily with the addition
    of a custom 'sp_offset' in UNWIND_HINT_FUNC.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Link: https://lore.kernel.org/r/db9d1f5d79dddfbb3725ef6d8ec3477ad199948d.1611263462.git.jpoimboe@redhat.com

--- arch/x86/include/asm/unwind_hints.h
+++ arch/x86/include/asm/unwind_hints.h
@@ -49,17 +49,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	UNWIND_HINT_REGS base=\base offset=\offset partial=1
 .endm
 
-.macro UNWIND_HINT_FUNC sp_offset=8
-	UNWIND_HINT sp_reg=ORC_REG_SP sp_offset=\sp_offset type=UNWIND_HINT_TYPE_CALL
-.endm
-
-/*
- * RET_OFFSET: Used on instructions that terminate a function; mostly RETURN
- * and sibling calls. On these, sp_offset denotes the expected offset from
- * initial_func_cfi.
- */
-.macro UNWIND_HINT_RET_OFFSET sp_offset=8
-	UNWIND_HINT sp_reg=ORC_REG_SP type=UNWIND_HINT_TYPE_RET_OFFSET sp_offset=\sp_offset
+.macro UNWIND_HINT_FUNC
+	UNWIND_HINT sp_reg=ORC_REG_SP sp_offset=8 type=UNWIND_HINT_TYPE_FUNC
 .endm
 
 #endif /* __ASSEMBLY__ */
