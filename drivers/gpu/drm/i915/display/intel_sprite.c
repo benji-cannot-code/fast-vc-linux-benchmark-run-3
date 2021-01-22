410d4410de10930b784ce40ccc42661fec1ad47c
Manasi Navare <manasi.d.navare@intel.com>
2021-01-22 15:26:40 -0800

    drm/i915/display/vrr: Send VRR push to flip the frame
    VRR achieves vblank stretching using the HW PUSH functionality.
    So once the VRR is enabled during modeset then for each flip
    request from userspace, in the atomic tail pipe_update_end()
    we need to set the VRR push bit in HW for it to terminate
    the vblank at configured flipline or anytime after flipline
    or latest at the Vmax.
    
    The HW clears the PUSH bit after the double buffer updates
    are completed.
    
    v2:
    * Move send push to after irq en (Manasi)
    * Call send push unconditionally (Jani N)
    
    v3:
    * Stall w.r.t Vrr vmax (Manasi, Gary Smith)
    
    v4:
    * Remove the rmw (Ville)
    
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Cc: Gary Smith <gary.k.smith@intel.com>
    Signed-off-by: Manasi Navare <manasi.d.navare@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210122232647.22688-11-manasi.d.navare@intel.com

--- drivers/gpu/drm/i915/display/intel_sprite.c
+++ drivers/gpu/drm/i915/display/intel_sprite.c
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "intel_dsi.h"
 #include "intel_sprite.h"
 #include "i9xx_plane.h"
+#include "intel_vrr.h"
 
 int intel_usecs_to_scanlines(const struct drm_display_mode *adjusted_mode,
 			     int usecs)
@@ -266,6 +267,9 @@ void intel_pipe_update_end(struct intel_crtc_state *new_crtc_state)
 	}
 
 	local_irq_enable();
+
+	/* Send VRR Push to terminate Vblank */
+	intel_vrr_send_push(new_crtc_state);
 
 	if (intel_vgpu_active(dev_priv))
 		return;
