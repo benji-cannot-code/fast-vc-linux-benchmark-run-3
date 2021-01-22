8271de0515a87cc64b396f91d697378fcd46a85a
Claudiu Beznea <claudiu.beznea@microchip.com>
2021-01-22 14:21:35 +0200

    drivers: soc: atmel: add per soc id and version match masks
    SAMA7G5 has different masks for chip ID and chip version on CIDR
    register compared to previous AT91 SoCs. For this the commit adapts
    the code for SAMA7G5 addition by introducing 2 new members in
    struct at91_soc and fill them properly and also preparing the
    parsing of proper DT binding.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Signed-off-by: Nicolas Ferre <nicolas.ferre@microchip.com>
    Link: https://lore.kernel.org/r/1611318097-8970-6-git-send-email-claudiu.beznea@microchip.com

--- drivers/soc/atmel/soc.h
+++ drivers/soc/atmel/soc.h
@@ -17,14 +17,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct at91_soc {
 	u32 cidr_match;
+	u32 cidr_mask;
+	u32 version_mask;
 	u32 exid_match;
 	const char *name;
 	const char *family;
 };
 
-#define AT91_SOC(__cidr, __exid, __name, __family)		\
+#define AT91_SOC(__cidr, __cidr_mask, __version_mask, __exid,	\
+		 __name, __family)				\
 	{							\
 		.cidr_match = (__cidr),				\
+		.cidr_mask = (__cidr_mask),			\
+		.version_mask = (__version_mask),		\
 		.exid_match = (__exid),				\
 		.name = (__name),				\
 		.family = (__family),				\
