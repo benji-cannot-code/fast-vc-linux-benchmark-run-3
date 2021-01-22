ca4686ed25eb885716a5b75dff15bb8f091c8669
Uwe Kleine-König <uwe@kleine-koenig.org>
2021-01-22 10:24:49 +0100

    isa: Make the remove callback for isa drivers return void
    The driver core ignores the return value of the remove callback, so
    don't give isa drivers the chance to provide a value.
    
    Adapt all isa_drivers with a remove callbacks accordingly; they all
    return 0 unconditionally anyhow.
    
    Acked-by: Marc Kleine-Budde <mkl@pengutronix.de> # for drivers/net/can/sja1000/tscan1.c
    Acked-by: William Breathitt Gray <vilhelm.gray@gmail.com>
    Acked-by: Wolfram Sang <wsa@kernel.org> # for drivers/i2c/
    Reviewed-by: Takashi Iway <tiwai@suse.de> # for sound/
    Reviewed-by: Hans Verkuil <hverkuil-cisco@xs4all.nl> # for drivers/media/
    Signed-off-by: Uwe Kleine-König <uwe@kleine-koenig.org>
    Link: https://lore.kernel.org/r/20210122092449.426097-4-uwe@kleine-koenig.org
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- drivers/scsi/fdomain_isa.c
+++ drivers/scsi/fdomain_isa.c
@@ -176,7 +176,7 @@ static int fdomain_isa_param_match(struct device *dev, unsigned int ndev)
 	return 1;
 }
 
-static int fdomain_isa_remove(struct device *dev, unsigned int ndev)
+static void fdomain_isa_remove(struct device *dev, unsigned int ndev)
 {
 	struct Scsi_Host *sh = dev_get_drvdata(dev);
 	int base = sh->io_port;
@@ -184,7 +184,6 @@ static int fdomain_isa_remove(struct device *dev, unsigned int ndev)
 	fdomain_destroy(sh);
 	release_region(base, FDOMAIN_REGION_SIZE);
 	dev_set_drvdata(dev, NULL);
-	return 0;
 }
 
 static struct isa_driver fdomain_isa_driver = {
