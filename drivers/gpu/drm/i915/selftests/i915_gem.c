41203e9971446e2c3a592fea8fd59d79c0cf11a3
Chris Wilson <chris@chris-wilson.co.uk>
2021-01-23 14:55:43 +0000

    drm/i915/gem: Move freeze/freeze_late next to suspend/suspend_late
    Push the hibernate pm routines next to the suspend pm routines in
    gem/i915_gem_pm.c. This has the side-effect of putting the wbinvd()
    abusers next to each other.
    
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    Fixes: 30d2bfd09383 ("drm/i915/gem: Almagamate clflushes on freeze")
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Matthew Auld <matthew.auld@intel.com>
    Reviewed-by: Matthew Auld <matthew.auld@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210123145543.10533-1-chris@chris-wilson.co.uk
    (cherry picked from commit 6d8f02207420e76db693a00ccb44792474e297fc)
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/selftests/i915_gem.c
+++ drivers/gpu/drm/i915/selftests/i915_gem.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "gem/selftests/igt_gem_utils.h"
 #include "gem/selftests/mock_context.h"
+#include "gem/i915_gem_pm.h"
 #include "gt/intel_gt.h"
 #include "gt/intel_gt_pm.h"
 
