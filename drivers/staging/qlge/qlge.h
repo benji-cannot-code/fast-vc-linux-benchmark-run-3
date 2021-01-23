c79aa82f63cde1e1990bd17cb880a447a8e2f4ac
Coiby Xu <coiby.xu@gmail.com>
2021-01-23 18:46:11 +0800

    staging: qlge: remove mpi_core_to_log which sends coredump to the kernel ring buffer
    devlink health could be used to get coredump. No need to send so much
    data to the kernel ring buffer.
    
    Signed-off-by: Coiby Xu <coiby.xu@gmail.com>
    Link: https://lore.kernel.org/r/20210123104613.38359-7-coiby.xu@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/qlge/qlge.h
+++ drivers/staging/qlge/qlge.h
@@ -2154,7 +2154,6 @@ struct qlge_adapter {
 	u32 port_init;
 	u32 link_status;
 	struct qlge_mpi_coredump *mpi_coredump;
-	u32 core_is_dumped;
 	u32 link_config;
 	u32 led_config;
 	u32 max_frame_size;
@@ -2167,7 +2166,6 @@ struct qlge_adapter {
 	struct delayed_work mpi_work;
 	struct delayed_work mpi_port_cfg_work;
 	struct delayed_work mpi_idc_work;
-	struct delayed_work mpi_core_to_log;
 	struct completion ide_completion;
 	const struct nic_operations *nic_ops;
 	u16 device_id;
@@ -2258,7 +2256,6 @@ int qlge_write_cfg(struct qlge_adapter *qdev, void *ptr, int size, u32 bit,
 void qlge_queue_fw_error(struct qlge_adapter *qdev);
 void qlge_mpi_work(struct work_struct *work);
 void qlge_mpi_reset_work(struct work_struct *work);
-void qlge_mpi_core_to_log(struct work_struct *work);
 int qlge_wait_reg_rdy(struct qlge_adapter *qdev, u32 reg, u32 bit, u32 ebit);
 void qlge_queue_asic_error(struct qlge_adapter *qdev);
 void qlge_set_ethtool_ops(struct net_device *ndev);
