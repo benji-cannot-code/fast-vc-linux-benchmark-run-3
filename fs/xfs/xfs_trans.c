913166c67aab21ef57f22730791a1d26a2598669
Darrick J. Wong <djwong@kernel.org>
2021-01-22 16:48:37 -0800

    xfs: flush eof/cowblocks if we can't reserve quota for file blocks
    If a fs modification (data write, reflink, xattr set, fallocate, etc.)
    is unable to reserve enough quota to handle the modification, try
    clearing whatever space the filesystem might have been hanging onto in
    the hopes of speeding up the filesystem.  The flushing behavior will
    become particularly important when we add deferred inode inactivation
    because that will increase the amount of space that isn't actively tied
    to user data.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Brian Foster <bfoster@redhat.com>

--- fs/xfs/xfs_trans.c
+++ fs/xfs/xfs_trans.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "xfs_inode.h"
 #include "xfs_dquot_item.h"
 #include "xfs_dquot.h"
+#include "xfs_icache.h"
 
 kmem_zone_t	*xfs_trans_zone;
 
@@ -1047,8 +1048,10 @@ xfs_trans_alloc_inode(
 {
 	struct xfs_trans	*tp;
 	struct xfs_mount	*mp = ip->i_mount;
+	bool			retried = false;
 	int			error;
 
+retry:
 	error = xfs_trans_alloc(mp, resv, dblocks,
 			rblocks / mp->m_sb.sb_rextsize,
 			force ? XFS_TRANS_RESERVE : 0, &tp);
@@ -1066,6 +1069,13 @@ xfs_trans_alloc_inode(
 	}
 
 	error = xfs_trans_reserve_quota_nblks(tp, ip, dblocks, rblocks, force);
+	if ((error == -EDQUOT || error == -ENOSPC) && !retried) {
+		xfs_trans_cancel(tp);
+		xfs_iunlock(ip, XFS_ILOCK_EXCL);
+		xfs_blockgc_free_quota(ip, 0);
+		retried = true;
+		goto retry;
+	}
 	if (error)
 		goto out_cancel;
 
