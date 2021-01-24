51b7c3ae70ebcb589712878ba1888e9b99c956f1
Heiko Carstens <hca@linux.ibm.com>
2021-01-24 19:37:55 +0100

    s390/vdso: remove VDSO32_LBASE compat leftover
    Reviewed-by: Alexander Gordeev <agordeev@linux.ibm.com>
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- arch/s390/include/asm/vdso.h
+++ arch/s390/include/asm/vdso.h
@@ -5,8 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <vdso/datapage.h>
 
-/* Default link addresses for the vDSOs */
-#define VDSO32_LBASE	0
+/* Default link address for the vDSO */
 #define VDSO64_LBASE	0
 
 #define VDSO_VERSION_STRING	LINUX_2.6.29
