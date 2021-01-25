ca46bcaf93763aee75ff222cec3f59842b0d647b
Pavel Tatashin <pasha.tatashin@soleen.com>
2021-01-25 14:19:09 -0500

    arm64: trans_pgd: make trans_pgd_map_page generic
    kexec is going to use a different allocator, so make
    trans_pgd_map_page to accept allocator as an argument, and also
    kexec is going to use a different map protection, so also pass
    it via argument.
    
    Signed-off-by: Pavel Tatashin <pasha.tatashin@soleen.com>
    Reviewed-by: Matthias Brugger <mbrugger@suse.com>
    Link: https://lore.kernel.org/r/20210125191923.1060122-5-pasha.tatashin@soleen.com
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/trans_pgd.h
+++ arch/arm64/include/asm/trans_pgd.h
@@ -13,10 +13,25 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <asm/pgtable-types.h>
 
+/*
+ * trans_alloc_page
+ *	- Allocator that should return exactly one zeroed page, if this
+ *	  allocator fails, trans_pgd_create_copy() and trans_pgd_map_page()
+ *	  return -ENOMEM error.
+ *
+ * trans_alloc_arg
+ *	- Passed to trans_alloc_page as an argument
+ */
+
+struct trans_pgd_info {
+	void * (*trans_alloc_page)(void *arg);
+	void *trans_alloc_arg;
+};
+
 int trans_pgd_create_copy(pgd_t **dst_pgdp, unsigned long start,
 			  unsigned long end);
 
-int trans_pgd_map_page(pgd_t *trans_pgd, void *page, unsigned long dst_addr,
-		       pgprot_t pgprot);
+int trans_pgd_map_page(struct trans_pgd_info *info, pgd_t *trans_pgd,
+		       void *page, unsigned long dst_addr, pgprot_t pgprot);
 
 #endif /* _ASM_TRANS_TABLE_H */
