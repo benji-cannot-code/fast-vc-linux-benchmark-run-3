8d72f8f19f52f90f4d4973f1d12d0b42063dd550
Nathan Fontenot <nathan.fontenot@amd.com>
2021-01-25 11:35:17 -0600

    cpupower: Update family checks when decoding HW pstates
    The family checks in get_cof() and get_did() need to use the
    correct MSR format depending on the family. Add a cpupower
    capability for using the pstatedef (family 17h and newer) to
    control this instead of direct family checks.
    
    Signed-off-by: Nathan Fontenot <nathan.fontenot@amd.com>
    Reviewed-by: Robert Richter <rrichter@amd.com>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>

--- tools/power/cpupower/utils/helpers/amd.c
+++ tools/power/cpupower/utils/helpers/amd.c
@@ -46,10 +46,10 @@ static int get_did(int family, union core_pstate pstate)
 {
 	int t;
 
-	if (family == 0x12)
-		t = pstate.val & 0xf;
-	else if (family == 0x17 || family == 0x18)
+	if (cpupower_cpu_info.caps & CPUPOWER_CAP_AMD_PSTATEDEF)
 		t = pstate.pstatedef.did;
+	else if (family == 0x12)
+		t = pstate.val & 0xf;
 	else
 		t = pstate.pstate.did;
 
@@ -62,7 +62,7 @@ static int get_cof(int family, union core_pstate pstate)
 	int fid, did, cof;
 
 	did = get_did(family, pstate);
-	if (family == 0x17 || family == 0x18) {
+	if (cpupower_cpu_info.caps & CPUPOWER_CAP_AMD_PSTATEDEF) {
 		fid = pstate.pstatedef.fid;
 		cof = 200 * fid / did;
 	} else {
