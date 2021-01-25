67b67e6c3cfb7c8f55df0afd2b6e2d306a7440cf
Chengyang Fan <cy.fan@huawei.com>
2021-01-25 17:53:38 +0800

    powerpc: remove unneeded semicolons
    Remove superfluous semicolons after function definitions.
    
    Signed-off-by: Chengyang Fan <cy.fan@huawei.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20210125095338.1719405-1-cy.fan@huawei.com

--- arch/powerpc/include/asm/paca.h
+++ arch/powerpc/include/asm/paca.h
@@ -290,9 +290,9 @@ extern void free_unused_pacas(void);
 
 #else /* CONFIG_PPC64 */
 
-static inline void allocate_paca_ptrs(void) { };
-static inline void allocate_paca(int cpu) { };
-static inline void free_unused_pacas(void) { };
+static inline void allocate_paca_ptrs(void) { }
+static inline void allocate_paca(int cpu) { }
+static inline void free_unused_pacas(void) { }
 
 #endif /* CONFIG_PPC64 */
 
