089959761f2e8fa2e93781610afdfa97a9f89f56
Chengyang Fan <cy.fan@huawei.com>
2021-01-25 17:48:25 +0800

    MIPS: asm: spram: remove unneeded semicolon
    Remove a superfluous semicolon after function definition.
    
    Signed-off-by: Chengyang Fan <cy.fan@huawei.com>
    Signed-off-by: Thomas Bogendoerfer <tsbogend@alpha.franken.de>

--- arch/mips/include/asm/spram.h
+++ arch/mips/include/asm/spram.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #if defined(CONFIG_MIPS_SPRAM)
 extern __init void spram_config(void);
 #else
-static inline void spram_config(void) { };
+static inline void spram_config(void) { }
 #endif /* CONFIG_MIPS_SPRAM */
 
 #endif /* _MIPS_SPRAM_H */
