27db1163de583bcf34a6df5c3deed5a8c23ba23a
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2021-01-26 17:58:34 +0100

    amba: Make the remove callback return void
    All amba drivers return 0 in their remove callback. Together with the
    driver core ignoring the return value anyhow, it doesn't make sense to
    return a value here.
    
    Change the remove prototype to return void, which makes it explicit that
    returning an error value doesn't work as expected. This simplifies changing
    the core remove callback to return void, too.
    
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Acked-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Acked-by: Krzysztof Kozlowski <krzk@kernel.org> # for drivers/memory
    Acked-by: Mark Brown <broonie@kernel.org>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Suzuki K Poulose <suzuki.poulose@arm.com> # for hwtracing/coresight
    Acked-By: Vinod Koul <vkoul@kernel.org> # for dmaengine
    Acked-by: Guenter Roeck <linux@roeck-us.net> # for watchdog
    Acked-by: Wolfram Sang <wsa@kernel.org> # for I2C
    Acked-by: Takashi Iwai <tiwai@suse.de> # for sound
    Acked-by: Vladimir Zapolskiy <vz@mleia.com> # for memory/pl172
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://lore.kernel.org/r/20210126165835.687514-5-u.kleine-koenig@pengutronix.de
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

--- drivers/hwtracing/coresight/coresight-tmc-core.c
+++ drivers/hwtracing/coresight/coresight-tmc-core.c
@@ -560,7 +560,7 @@ out:
 	spin_unlock_irqrestore(&drvdata->spinlock, flags);
 }
 
-static int tmc_remove(struct amba_device *adev)
+static void tmc_remove(struct amba_device *adev)
 {
 	struct tmc_drvdata *drvdata = dev_get_drvdata(&adev->dev);
 
@@ -571,8 +571,6 @@ static int tmc_remove(struct amba_device *adev)
 	 */
 	misc_deregister(&drvdata->miscdev);
 	coresight_unregister(drvdata->csdev);
-
-	return 0;
 }
 
 static const struct amba_id tmc_ids[] = {
