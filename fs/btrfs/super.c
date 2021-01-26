6a6de743a5a5b4880476a8b6d8c839177392838c
Qu Wenruo <wqu@suse.com>
2021-01-26 16:33:47 +0800

    btrfs: introduce the skeleton of btrfs_subpage structure
    For sectorsize < page size support, we need a structure to record extra
    status info for each sector of a page.
    
    Introduce the skeleton structure, all subpage related code would go to
    subpage.[ch].
    
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "tests/btrfs-tests.h"
 #include "block-group.h"
 #include "discard.h"
-
 #include "qgroup.h"
 #define CREATE_TRACE_POINTS
 #include <trace/events/btrfs.h>
