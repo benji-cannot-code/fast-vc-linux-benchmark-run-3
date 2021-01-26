8f7635af02d3a45829b1b801c8d94d2330bc2137
Qu Wenruo <wqu@suse.com>
2021-01-26 16:34:00 +0800

    btrfs: introduce btrfs_subpage for data inodes
    To support subpage sector size, data also need extra info to make sure
    which sectors in a page are uptodate/dirty/...
    
    This patch will make pages for data inodes get btrfs_subpage structure
    attached, and detached when the page is freed.
    
    This patch also slightly changes the timing when
    set_page_extent_mapped() is called to make sure:
    
    - We have page->mapping set
      page->mapping->host is used to grab btrfs_fs_info, thus we can only
      call this function after page is mapped to an inode.
    
      One call site attaches pages to inode manually, thus we have to modify
      the timing of set_page_extent_mapped() a bit.
    
    - As soon as possible, before other operations
      Since memory allocation can fail, we have to do extra error handling.
      Calling set_page_extent_mapped() as soon as possible can simply the
      error handling for several call sites.
    
    The idea is pretty much the same as iomap_page, but with more bitmaps
    for btrfs specific cases.
    
    Currently the plan is to switch iomap if iomap can provide sector
    aligned write back (only write back dirty sectors, but not the full
    page, data balance require this feature).
    
    So we will stick to btrfs specific bitmap for now.
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -4721,6 +4721,9 @@ again:
 		ret = -ENOMEM;
 		goto out;
 	}
+	ret = set_page_extent_mapped(page);
+	if (ret < 0)
+		goto out_unlock;
 
 	if (!PageUptodate(page)) {
 		ret = btrfs_readpage(NULL, page);
@@ -4738,7 +4741,6 @@ again:
 	wait_on_page_writeback(page);
 
 	lock_extent_bits(io_tree, block_start, block_end, &cached_state);
-	set_page_extent_mapped(page);
 
 	ordered = btrfs_lookup_ordered_extent(inode, block_start);
 	if (ordered) {
@@ -8126,7 +8128,7 @@ static int __btrfs_releasepage(struct page *page, gfp_t gfp_flags)
 {
 	int ret = try_release_extent_mapping(page, gfp_flags);
 	if (ret == 1)
-		detach_page_private(page);
+		clear_page_extent_mapped(page);
 	return ret;
 }
 
@@ -8286,7 +8288,7 @@ again:
 	}
 
 	ClearPageChecked(page);
-	detach_page_private(page);
+	clear_page_extent_mapped(page);
 }
 
 /*
@@ -8365,7 +8367,12 @@ again:
 	wait_on_page_writeback(page);
 
 	lock_extent_bits(io_tree, page_start, page_end, &cached_state);
-	set_page_extent_mapped(page);
+	ret2 = set_page_extent_mapped(page);
+	if (ret2 < 0) {
+		ret = vmf_error(ret2);
+		unlock_extent_cached(io_tree, page_start, page_end, &cached_state);
+		goto out_unlock;
+	}
 
 	/*
 	 * we can't set the delalloc bits if there are pending ordered
