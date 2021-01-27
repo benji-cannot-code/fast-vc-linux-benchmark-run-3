c6e44024b0597ebce7209252fefd550779f2e1b3
Bjorn Helgaas <bhelgaas@google.com>
2021-01-27 12:13:59 -0600

    octeontx2-af: Fix 'physical' typos
    Fix misspellings of "physical".
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Willem de Bruijn <willemb@google.com>
    Link: https://lore.kernel.org/r/20210127181359.3008316-1-helgaas@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
-/* Marvell OcteonTx2 RVU Physcial Function ethernet driver
+/* Marvell OcteonTx2 RVU Physical Function ethernet driver
  *
  * Copyright (C) 2020 Marvell International Ltd.
  *
