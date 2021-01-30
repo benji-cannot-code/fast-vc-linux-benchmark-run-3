0061ebb2b262a9236d76a78cefa2ac1d01804f9d
Nathan Chancellor <nathan@kernel.org>
2021-01-29 17:46:51 -0700

    vmlinux.lds.h: Define SANTIZER_DISCARDS with CONFIG_GCOV_KERNEL=y
    clang produces .eh_frame sections when CONFIG_GCOV_KERNEL is enabled,
    even when -fno-asynchronous-unwind-tables is in KBUILD_CFLAGS:
    
    $ make CC=clang vmlinux
    ...
    ld: warning: orphan section `.eh_frame' from `init/main.o' being placed in section `.eh_frame'
    ld: warning: orphan section `.eh_frame' from `init/version.o' being placed in section `.eh_frame'
    ld: warning: orphan section `.eh_frame' from `init/do_mounts.o' being placed in section `.eh_frame'
    ld: warning: orphan section `.eh_frame' from `init/do_mounts_initrd.o' being placed in section `.eh_frame'
    ld: warning: orphan section `.eh_frame' from `init/initramfs.o' being placed in section `.eh_frame'
    ld: warning: orphan section `.eh_frame' from `init/calibrate.o' being placed in section `.eh_frame'
    ld: warning: orphan section `.eh_frame' from `init/init_task.o' being placed in section `.eh_frame'
    ...
    
    $ rg "GCOV_KERNEL|GCOV_PROFILE_ALL" .config
    CONFIG_GCOV_KERNEL=y
    CONFIG_ARCH_HAS_GCOV_PROFILE_ALL=y
    CONFIG_GCOV_PROFILE_ALL=y
    
    This was already handled for a couple of other options in
    commit d812db78288d ("vmlinux.lds.h: Avoid KASAN and KCSAN's unwanted
    sections") and there is an open LLVM bug for this issue. Take advantage
    of that section for this config as well so that there are no more orphan
    warnings.
    
    Link: https://bugs.llvm.org/show_bug.cgi?id=46478
    Link: https://github.com/ClangBuiltLinux/linux/issues/1069
    Reported-by: kernel test robot <lkp@intel.com>
    Reviewed-by: Fangrui Song <maskray@google.com>
    Reviewed-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>
    Fixes: d812db78288d ("vmlinux.lds.h: Avoid KASAN and KCSAN's unwanted sections")
    Cc: stable@vger.kernel.org
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Link: https://lore.kernel.org/r/20210130004650.2682422-1-nathan@kernel.org

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -989,12 +989,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 /*
- * Clang's -fsanitize=kernel-address and -fsanitize=thread produce
- * unwanted sections (.eh_frame and .init_array.*), but
- * CONFIG_CONSTRUCTORS wants to keep any .init_array.* sections.
+ * Clang's -fprofile-arcs, -fsanitize=kernel-address, and
+ * -fsanitize=thread produce unwanted sections (.eh_frame
+ * and .init_array.*), but CONFIG_CONSTRUCTORS wants to
+ * keep any .init_array.* sections.
  * https://bugs.llvm.org/show_bug.cgi?id=46478
  */
-#if defined(CONFIG_KASAN_GENERIC) || defined(CONFIG_KCSAN)
+#if defined(CONFIG_GCOV_KERNEL) || defined(CONFIG_KASAN_GENERIC) || defined(CONFIG_KCSAN)
 # ifdef CONFIG_CONSTRUCTORS
 #  define SANITIZER_DISCARDS						\
 	*(.eh_frame)
