5fa71f1d8754ba0008b0641877184a9563cfc1bc
Nicholas Piggin <npiggin@gmail.com>
2021-01-30 23:08:32 +1000

    powerpc/cell: tidy up pervasive declarations
    These are declared in ras.h and defined in ras.c so remove them from
    pervasive.h
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20210130130852.2952424-23-npiggin@gmail.com

--- arch/powerpc/platforms/cell/pervasive.c
+++ arch/powerpc/platforms/cell/pervasive.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cpu_has_feature.h>
 
 #include "pervasive.h"
+#include "ras.h"
 
 static void cbe_power_save(void)
 {
