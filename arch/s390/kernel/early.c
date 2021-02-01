ab2c1753d0feb6810661c10ac8b704e0eddd7b20
Heiko Carstens <hca@linux.ibm.com>
2021-02-01 21:53:08 +0100

    s390/time: use stcke instead of stck
    Use STORE CLOCK EXTENDED instead of STORE CLOCK in early tod clock
    setup. This is just to remove another usage of stck, trying to remove
    all usages of STORE CLOCK.  This doesn't fix anything.
    
    Reviewed-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- arch/s390/kernel/early.c
+++ arch/s390/kernel/early.c
@@ -36,12 +36,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void __init reset_tod_clock(void)
 {
-	u64 time;
+	char time[STORE_CLOCK_EXT_SIZE];
 
-	if (store_tod_clock(&time) == 0)
+	if (store_tod_clock_ext(time) == 0)
 		return;
 	/* TOD clock not running. Set the clock to Unix Epoch. */
-	if (set_tod_clock(TOD_UNIX_EPOCH) != 0 || store_tod_clock(&time) != 0)
+	if (set_tod_clock(TOD_UNIX_EPOCH) != 0 || store_tod_clock_ext(time) != 0)
 		disabled_wait();
 
 	memset(tod_clock_base, 0, 16);
