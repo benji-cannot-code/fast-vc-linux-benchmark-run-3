8039cb087e6b33b96692a78bbccb0f37e57a0402
Pali Rohár <pali@kernel.org>
2021-02-01 16:08:03 +0100

    usb: host: xhci: mvebu: make USB 3.0 PHY optional for Armada 3720
    Older ATF does not provide SMC call for USB 3.0 phy power on functionality
    and therefore initialization of xhci-hcd is failing when older version of
    ATF is used. In this case phy_power_on() function returns -EOPNOTSUPP.
    
    [    3.108467] mvebu-a3700-comphy d0018300.phy: unsupported SMC call, try updating your firmware
    [    3.117250] phy phy-d0018300.phy.0: phy poweron failed --> -95
    [    3.123465] xhci-hcd: probe of d0058000.usb failed with error -95
    
    This patch introduces a new plat_setup callback for xhci platform drivers
    which is called prior calling usb_add_hcd() function. This function at its
    beginning skips PHY init if hcd->skip_phy_initialization is set.
    
    Current init_quirk callback for xhci platform drivers is called from
    xhci_plat_setup() function which is called after chip reset completes.
    It happens in the middle of the usb_add_hcd() function and therefore this
    callback cannot be used for setting if PHY init should be skipped or not.
    
    For Armada 3720 this patch introduce a new xhci_mvebu_a3700_plat_setup()
    function configured as a xhci platform plat_setup callback. This new
    function calls phy_power_on() and in case it returns -EOPNOTSUPP then
    XHCI_SKIP_PHY_INIT quirk is set to instruct xhci-plat to skip PHY
    initialization.
    
    This patch fixes above failure by ignoring 'not supported' error in
    xhci-hcd driver. In this case it is expected that phy is already power on.
    
    It fixes initialization of xhci-hcd on Espressobin boards where is older
    Marvell's Arm Trusted Firmware without SMC call for USB 3.0 phy power.
    
    This is regression introduced in commit bd3d25b07342 ("arm64: dts: marvell:
    armada-37xx: link USB hosts with their PHYs") where USB 3.0 phy was defined
    and therefore xhci-hcd on Espressobin with older ATF started failing.
    
    Fixes: bd3d25b07342 ("arm64: dts: marvell: armada-37xx: link USB hosts with their PHYs")
    Cc: <stable@vger.kernel.org> # 5.1+: ea17a0f153af: phy: marvell: comphy: Convert internal SMCC firmware return codes to errno
    Cc: <stable@vger.kernel.org> # 5.1+: f768e718911e: usb: host: xhci-plat: add priv quirk for skip PHY initialization
    Tested-by: Tomasz Maciej Nowak <tmn505@gmail.com>
    Tested-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com> # On R-Car
    Reviewed-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com> # xhci-plat
    Acked-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Signed-off-by: Pali Rohár <pali@kernel.org>
    Link: https://lore.kernel.org/r/20210201150803.7305-1-pali@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/xhci-plat.h
+++ drivers/usb/host/xhci-plat.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct xhci_plat_priv {
 	const char *firmware_name;
 	unsigned long long quirks;
+	int (*plat_setup)(struct usb_hcd *);
 	void (*plat_start)(struct usb_hcd *);
 	int (*init_quirk)(struct usb_hcd *);
 	int (*suspend_quirk)(struct usb_hcd *);
