9d49f18dd59db17bc9474887557a9924c93b303c
Suzuki K Poulose <suzuki.poulose@arm.com>
2021-02-01 11:13:25 -0700

    coresight: Introduce device access abstraction
    We are about to introduce support for sysreg access to ETMv4.4+
    component. Since there are generic routines that access the
    registers (e.g, CS_LOCK/UNLOCK , claim/disclaim operations, timeout)
    and in order to preserve the logic of these operations at a
    single place we introduce an abstraction layer for the accesses
    to a given device.
    
    Link: https://lore.kernel.org/r/20210110224850.1880240-4-suzuki.poulose@arm.com
    Cc: Mike Leach <mike.leach@linaro.org>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Link: https://lore.kernel.org/r/20210201181351.1475223-6-mathieu.poirier@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/hwtracing/coresight/coresight-funnel.c
+++ drivers/hwtracing/coresight/coresight-funnel.c
@@ -243,6 +243,7 @@ static int funnel_probe(struct device *dev, struct resource *res)
 		}
 		drvdata->base = base;
 		desc.groups = coresight_funnel_groups;
+		desc.access = CSDEV_ACCESS_IOMEM(base);
 	}
 
 	dev_set_drvdata(dev, drvdata);
