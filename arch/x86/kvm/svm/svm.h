d0b60e046fd6c2f7e85768382b3a9248e7998016
Michael Roth <michael.roth@amd.com>
2021-02-02 13:01:25 -0600

    KVM: SVM: remove uneeded fields from host_save_users_msrs
    Now that the set of host user MSRs that need to be individually
    saved/restored are the same with/without SEV-ES, we can drop the
    .sev_es_restored flag and just iterate through the list unconditionally
    for both cases. A subsequent patch can then move these loops to a
    common path.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Message-Id: <20210202190126.2185715-3-michael.roth@amd.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -24,11 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define __sme_page_pa(x) __sme_set(page_to_pfn(x) << PAGE_SHIFT)
 
-static const struct svm_host_save_msrs {
-	u32 index;		/* Index of the MSR */
-	bool sev_es_restored;	/* True if MSR is restored on SEV-ES VMEXIT */
-} host_save_user_msrs[] = {
-	{ .index = MSR_TSC_AUX,			.sev_es_restored = false },
+static const u32 host_save_user_msrs[] = {
+	MSR_TSC_AUX,
 };
 #define NR_HOST_SAVE_USER_MSRS ARRAY_SIZE(host_save_user_msrs)
 
