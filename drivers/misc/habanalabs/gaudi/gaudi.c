1f05b8bff311426652fbc47b3c67415a64c9ba38
Ohad Sharabi <osharabi@habana.ai>
2021-02-02 13:33:34 +0200

    habanalabs: update security map after init CPU Qs
    when reading CPU_BOOT_DEV_STS0 reg after FW reports SRAM AVAILABLE the
    value in the register might not yet be updated by FW.
    to overcome this issue another "up-to-date" read of this register is
    done at the end of CPU queues init.
    
    Signed-off-by: Ohad Sharabi <osharabi@habana.ai>
    Reviewed-by: Oded Gabbay <ogabbay@kernel.org>
    Signed-off-by: Oded Gabbay <ogabbay@kernel.org>

--- drivers/misc/habanalabs/gaudi/gaudi.c
+++ drivers/misc/habanalabs/gaudi/gaudi.c
@@ -537,7 +537,6 @@ static int gaudi_get_fixed_properties(struct hl_device *hdev)
 	prop->fw_security_disabled = true;
 	prop->fw_security_status_valid = false;
 	prop->hard_reset_done_by_fw = false;
-	prop->fw_cpucp_ack_with_pi = false;
 
 	return 0;
 }
@@ -3728,6 +3727,7 @@ static int gaudi_init_cpu(struct hl_device *hdev)
 static int gaudi_init_cpu_queues(struct hl_device *hdev, u32 cpu_timeout)
 {
 	struct gaudi_device *gaudi = hdev->asic_specific;
+	struct asic_fixed_properties *prop = &hdev->asic_prop;
 	struct hl_eq *eq;
 	u32 status;
 	struct hl_hw_queue *cpu_pq =
@@ -3783,6 +3783,10 @@ static int gaudi_init_cpu_queues(struct hl_device *hdev, u32 cpu_timeout)
 			"Failed to communicate with Device CPU (CPU-CP timeout)\n");
 		return -EIO;
 	}
+
+	/* update FW application security bits */
+	if (prop->fw_security_status_valid)
+		prop->fw_app_security_map = RREG32(mmCPU_BOOT_DEV_STS0);
 
 	gaudi->hw_cap_initialized |= HW_CAP_CPU_Q;
 	return 0;
