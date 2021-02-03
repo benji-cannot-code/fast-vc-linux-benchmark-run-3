f53bf3d1f976258de57aa0992fa0d9f09718b72a
Ben Skeggs <bskeggs@redhat.com>
2021-02-04 08:38:32 +1000

    drm/nouveau/mspdec: switch to instanced constructor
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>
    Reviewed-by: Lyude Paul <lyude@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/mspdec/priv.h
+++ drivers/gpu/drm/nouveau/nvkm/engine/mspdec/priv.h
@@ -4,8 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NVKM_MSPDEC_PRIV_H__
 #include <engine/mspdec.h>
 
-int nvkm_mspdec_new_(const struct nvkm_falcon_func *, struct nvkm_device *,
-		     int index, struct nvkm_engine **);
+int nvkm_mspdec_new_(const struct nvkm_falcon_func *, struct nvkm_device *, enum nvkm_subdev_type,
+		     int, struct nvkm_engine **);
 
 void g98_mspdec_init(struct nvkm_falcon *);
 
