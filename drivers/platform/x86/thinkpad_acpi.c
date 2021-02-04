943cd78c5b78936b7954200663832dbf29e0b776
Hans de Goede <hdegoede@redhat.com>
2021-02-04 15:01:58 +0100

    platform/x86: thinkpad_acpi: Replace ifdef CONFIG_ACPI_PLATFORM_PROFILE with depends on
    With the #if IS_ENABLED(CONFIG_ACPI_PLATFORM_PROFILE), we get the following
    errors when thinkpad_acpi is builtin while CONFIG_ACPI_PLATFORM_PROFILE=m :
    
    drivers/platform/x86/thinkpad_acpi.c:10186: undefined reference to `platform_profile_notify'
    drivers/platform/x86/thinkpad_acpi.c:10226: undefined reference to `platform_profile_register'
    drivers/platform/x86/thinkpad_acpi.c:10246: undefined reference to `platform_profile_remove'
    
    This could be fixed by changing the IS_ENABLED to IS_REACHABLE, but
    I believe that it is better to just switch to using depends on.
    
    Using depends on ensures that platform-profile support is always
    available when thinkpad_acpi is build, hopefully leading to less
    confusing bug-reports about it sometimes not working.
    
    Cc: Mark Pearson <markpearson@lenovo.com>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Link: https://lore.kernel.org/r/20210204140158.268289-2-hdegoede@redhat.com

--- drivers/platform/x86/thinkpad_acpi.c
+++ drivers/platform/x86/thinkpad_acpi.c
@@ -9996,8 +9996,6 @@ static struct ibm_struct proxsensor_driver_data = {
 	.exit = proxsensor_exit,
 };
 
-#if IS_ENABLED(CONFIG_ACPI_PLATFORM_PROFILE)
-
 /*************************************************************************
  * DYTC Platform Profile interface
  */
@@ -10252,7 +10250,6 @@ static struct ibm_struct  dytc_profile_driver_data = {
 	.name = "dytc-profile",
 	.exit = dytc_profile_exit,
 };
-#endif /* CONFIG_ACPI_PLATFORM_PROFILE */
 
 /*************************************************************************
  * Keyboard language interface
@@ -10477,11 +10474,9 @@ static void tpacpi_driver_event(const unsigned int hkey_event)
 
 	if (hkey_event == TP_HKEY_EV_THM_CSM_COMPLETED) {
 		lapsensor_refresh();
-#if IS_ENABLED(CONFIG_ACPI_PLATFORM_PROFILE)
 		/* If we are already accessing DYTC then skip dytc update */
 		if (!atomic_add_unless(&dytc_ignore_event, -1, 0))
 			dytc_profile_refresh();
-#endif
 	}
 }
 
@@ -10925,12 +10920,10 @@ static struct ibm_init_struct ibms_init[] __initdata = {
 		.init = tpacpi_proxsensor_init,
 		.data = &proxsensor_driver_data,
 	},
-#if IS_ENABLED(CONFIG_ACPI_PLATFORM_PROFILE)
 	{
 		.init = tpacpi_dytc_profile_init,
 		.data = &dytc_profile_driver_data,
 	},
-#endif
 	{
 		.init = tpacpi_kbdlang_init,
 		.data = &kbdlang_driver_data,
