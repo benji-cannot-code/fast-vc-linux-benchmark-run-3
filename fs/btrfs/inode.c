fa8ec6c08eefbf2aeb82a6e7a94cdd590220ec6f
Naohiro Aota <naohiro.aota@wdc.com>
2021-02-04 19:22:05 +0900

    btrfs: zoned: use ZONE_APPEND write for zoned mode
    Enable zone append writing for zoned mode. When using zone append, a
    bio is issued to the start of a target zone and the device decides to
    place it inside the zone. Upon completion the device reports the actual
    written position back to the host.
    
    Three parts are necessary to enable zone append mode. First, modify the
    bio to use REQ_OP_ZONE_APPEND in btrfs_submit_bio_hook() and adjust the
    bi_sector to point the beginning of the zone.
    
    Second, record the returned physical address (and disk/partno) to the
    ordered extent in end_bio_extent_writepage() after the bio has been
    completed. We cannot resolve the physical address to the logical address
    because we can neither take locks nor allocate a buffer in this end_bio
    context. So, we need to record the physical address to resolve it later
    in btrfs_finish_ordered_io().
    
    And finally, rewrite the logical addresses of the extent mapping and
    checksum data according to the physical address using btrfs_rmap_block.
    If the returned address matches the originally allocated address, we can
    skip this rewriting process.
    
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Signed-off-by: Naohiro Aota <naohiro.aota@wdc.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "delalloc-space.h"
 #include "block-group.h"
 #include "space-info.h"
+#include "zoned.h"
 
 struct btrfs_iget_args {
 	u64 ino;
@@ -2874,6 +2875,9 @@ static int btrfs_finish_ordered_io(struct btrfs_ordered_extent *ordered_extent)
 		ret = -EIO;
 		goto out;
 	}
+
+	if (ordered_extent->disk)
+		btrfs_rewrite_logical_zoned(ordered_extent);
 
 	btrfs_free_io_failure_record(inode, start, end);
 
