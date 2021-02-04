694435258a81221f832c5e5375cc1be51d160499
Brian Vazquez <brianvv@google.com>
2021-02-04 18:18:38 +0000

    net: add EXPORT_INDIRECT_CALLABLE wrapper
    When a static function is annotated with INDIRECT_CALLABLE_SCOPE and
    CONFIG_RETPOLINE is set, the static keyword is removed. Sometimes the
    function needs to be exported but EXPORT_SYMBOL can't be used because if
    CONFIG_RETPOLINE is not set, we will attempt to export a static symbol.
    
    This patch introduces a new indirect call wrapper:
    EXPORT_INDIRECT_CALLABLE. This basically does EXPORT_SYMBOL when
    CONFIG_RETPOLINE is set, but does nothing when it's not.
    
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Brian Vazquez <brianvv@google.com>
    Link: https://lore.kernel.org/r/20210204181839.558951-1-brianvv@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/linux/indirect_call_wrapper.h
+++ include/linux/indirect_call_wrapper.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define INDIRECT_CALLABLE_DECLARE(f)	f
 #define INDIRECT_CALLABLE_SCOPE
+#define EXPORT_INDIRECT_CALLABLE(f)	EXPORT_SYMBOL(f)
 
 #else
 #define INDIRECT_CALL_1(f, f1, ...) f(__VA_ARGS__)
@@ -45,6 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define INDIRECT_CALL_4(f, f4, f3, f2, f1, ...) f(__VA_ARGS__)
 #define INDIRECT_CALLABLE_DECLARE(f)
 #define INDIRECT_CALLABLE_SCOPE		static
+#define EXPORT_INDIRECT_CALLABLE(f)
 #endif
 
 /*
