f72a63465470805cabf2b6f623a54d93ab432f19
Heiko Carstens <hca@linux.ibm.com>
2021-02-05 16:19:32 +0100

    s390/vdso: implement generic vdso time namespace support
    Implement generic vdso time namespace support which also enables time
    namespaces for s390. This is quite similar to what arm64 has.
    
    Reviewed-by: Alexander Gordeev <agordeev@linux.ibm.com>
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
    Signed-off-by: Vasily Gorbik <gor@linux.ibm.com>

--- arch/s390/include/asm/vdso.h
+++ arch/s390/include/asm/vdso.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Default link address for the vDSO */
 #define VDSO64_LBASE	0
 
+#define __VVAR_PAGES	2
+
 #define VDSO_VERSION_STRING	LINUX_2.6.29
 
 #ifndef __ASSEMBLY__
