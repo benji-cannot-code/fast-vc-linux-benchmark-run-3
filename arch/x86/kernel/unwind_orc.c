b19eb75cf036f254baeb4584354587289b3536cd
Josh Poimboeuf <jpoimboe@redhat.com>
2021-02-05 08:24:03 -0600

    x86/unwind/orc: Silence warnings caused by missing ORC data
    The ORC unwinder attempts to fall back to frame pointers when ORC data
    is missing for a given instruction.  It sets state->error, but then
    tries to keep going as a best-effort type of thing.  That may result in
    further warnings if the unwinder gets lost.
    
    Until we have some way to register generated code with the unwinder,
    missing ORC will be expected, and occasionally going off the rails will
    also be expected.  So don't warn about it.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Tested-by: Ivan Babrou <ivan@cloudflare.com>
    Link: https://lkml.kernel.org/r/06d02c4bbb220bd31668db579278b0352538efbb.1612534649.git.jpoimboe@redhat.com

--- arch/x86/kernel/unwind_orc.c
+++ arch/x86/kernel/unwind_orc.c
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define orc_warn_current(args...)					\
 ({									\
-	if (state->task == current)					\
+	if (state->task == current && !state->error)			\
 		orc_warn(args);						\
 })
 
