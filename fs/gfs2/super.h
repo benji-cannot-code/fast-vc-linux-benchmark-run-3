abfae1c8c515fa0ef7db0deeb208ece209b465b2
Andrew Price <anprice@redhat.com>
2021-02-05 17:10:17 +0000

    gfs2: Enable rgrplvb for sb_fs_format 1802
    Turn on rgrplvb by default for sb_fs_format > 1801.
    
    Mount options still have to override this so a new args field to
    differentiate between 'off' and 'not specified' is added, and the new
    default is applied only when it's not specified.
    
    Signed-off-by: Andrew Price <anprice@redhat.com>
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- fs/gfs2/super.h
+++ fs/gfs2/super.h
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dcache.h>
 #include "incore.h"
 
+/* Supported fs format version range */
+#define GFS2_FS_FORMAT_MIN (1801)
+#define GFS2_FS_FORMAT_MAX (1802)
+
 extern void gfs2_lm_unmount(struct gfs2_sbd *sdp);
 
 static inline unsigned int gfs2_jindex_size(struct gfs2_sbd *sdp)
