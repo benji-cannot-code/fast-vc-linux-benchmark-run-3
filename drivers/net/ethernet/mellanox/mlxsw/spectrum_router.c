7892bda8c40658ebb5a367f8c79badd1844736f5
Amit Cohen <amcohen@nvidia.com>
2021-02-07 10:22:50 +0200

    IPv4: Add "offload failed" indication to routes
    After installing a route to the kernel, user space receives an
    acknowledgment, which means the route was installed in the kernel, but not
    necessarily in hardware.
    
    The asynchronous nature of route installation in hardware can lead to a
    routing daemon advertising a route before it was actually installed in
    hardware. This can result in packet loss or mis-routed packets until the
    route is installed in hardware.
    
    To avoid such cases, previous patch set added the ability to emit
    RTM_NEWROUTE notifications whenever RTM_F_OFFLOAD/RTM_F_TRAP flags
    are changed, this behavior is controlled by sysctl.
    
    With the above mentioned behavior, it is possible to know from user-space
    if the route was offloaded, but if the offload fails there is no indication
    to user-space. Following a failure, a routing daemon will wait indefinitely
    for a notification that will never come.
    
    This patch adds an "offload_failed" indication to IPv4 routes, so that
    users will have better visibility into the offload process.
    
    'struct fib_alias', and 'struct fib_rt_info' are extended with new field
    that indicates if route offload failed. Note that the new field is added
    using unused bit and therefore there is no need to increase structs size.
    
    Signed-off-by: Amit Cohen <amcohen@nvidia.com>
    Signed-off-by: Ido Schimmel <idosch@nvidia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_router.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_router.c
@@ -4964,6 +4964,7 @@ mlxsw_sp_fib4_entry_hw_flags_set(struct mlxsw_sp *mlxsw_sp,
 	fri.type = fib4_entry->type;
 	fri.offload = should_offload;
 	fri.trap = !should_offload;
+	fri.offload_failed = false;
 	fib_alias_hw_flags_set(mlxsw_sp_net(mlxsw_sp), &fri);
 }
 
@@ -4986,6 +4987,7 @@ mlxsw_sp_fib4_entry_hw_flags_clear(struct mlxsw_sp *mlxsw_sp,
 	fri.type = fib4_entry->type;
 	fri.offload = false;
 	fri.trap = false;
+	fri.offload_failed = false;
 	fib_alias_hw_flags_set(mlxsw_sp_net(mlxsw_sp), &fri);
 }
 
