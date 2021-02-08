b32e3a8f1acc7117d209c45f0379f60851f78248
David Woodhouse <dwmw@amazon.co.uk>
2021-02-08 23:23:25 +0000

    KVM: x86/xen: Allow reset of Xen attributes
    In order to support Xen SHUTDOWN_soft_reset (for guest kexec, etc.) the
    VMM needs to be able to tear everything down and return the Xen features
    to a clean slate.
    
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>
    Message-Id: <20210208232326.1830370-1-dwmw2@infradead.org>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/xen.c
+++ arch/x86/kvm/xen.c
@@ -117,12 +117,17 @@ int kvm_xen_hvm_set_attr(struct kvm *kvm, struct kvm_xen_hvm_attr *data)
 		break;
 
 	case KVM_XEN_ATTR_TYPE_SHARED_INFO:
+		if (data->u.shared_info.gfn == GPA_INVALID) {
+			kvm->arch.xen.shinfo_set = false;
+			r = 0;
+			break;
+		}
 		r = kvm_xen_shared_info_init(kvm, data->u.shared_info.gfn);
 		break;
 
 
 	case KVM_XEN_ATTR_TYPE_UPCALL_VECTOR:
-		if (data->u.vector < 0x10)
+		if (data->u.vector && data->u.vector < 0x10)
 			r = -EINVAL;
 		else {
 			kvm->arch.xen.upcall_vector = data->u.vector;
@@ -151,10 +156,11 @@ int kvm_xen_hvm_get_attr(struct kvm *kvm, struct kvm_xen_hvm_attr *data)
 		break;
 
 	case KVM_XEN_ATTR_TYPE_SHARED_INFO:
-		if (kvm->arch.xen.shinfo_set) {
+		if (kvm->arch.xen.shinfo_set)
 			data->u.shared_info.gfn = gpa_to_gfn(kvm->arch.xen.shinfo_cache.gpa);
-			r = 0;
-		}
+		else
+			data->u.shared_info.gfn = GPA_INVALID;
+		r = 0;
 		break;
 
 	case KVM_XEN_ATTR_TYPE_UPCALL_VECTOR:
@@ -183,6 +189,11 @@ int kvm_xen_vcpu_set_attr(struct kvm_vcpu *vcpu, struct kvm_xen_vcpu_attr *data)
 		BUILD_BUG_ON(sizeof(struct vcpu_info) !=
 			     sizeof(struct compat_vcpu_info));
 
+		if (data->u.gpa == GPA_INVALID) {
+			vcpu->arch.xen.vcpu_info_set = false;
+			break;
+		}
+
 		r = kvm_gfn_to_hva_cache_init(vcpu->kvm,
 					      &vcpu->arch.xen.vcpu_info_cache,
 					      data->u.gpa,
@@ -194,6 +205,11 @@ int kvm_xen_vcpu_set_attr(struct kvm_vcpu *vcpu, struct kvm_xen_vcpu_attr *data)
 		break;
 
 	case KVM_XEN_VCPU_ATTR_TYPE_VCPU_TIME_INFO:
+		if (data->u.gpa == GPA_INVALID) {
+			vcpu->arch.xen.vcpu_time_info_set = false;
+			break;
+		}
+
 		r = kvm_gfn_to_hva_cache_init(vcpu->kvm,
 					      &vcpu->arch.xen.vcpu_time_info_cache,
 					      data->u.gpa,
@@ -221,17 +237,19 @@ int kvm_xen_vcpu_get_attr(struct kvm_vcpu *vcpu, struct kvm_xen_vcpu_attr *data)
 
 	switch (data->type) {
 	case KVM_XEN_VCPU_ATTR_TYPE_VCPU_INFO:
-		if (vcpu->arch.xen.vcpu_info_set) {
+		if (vcpu->arch.xen.vcpu_info_set)
 			data->u.gpa = vcpu->arch.xen.vcpu_info_cache.gpa;
-			r = 0;
-		}
+		else
+			data->u.gpa = GPA_INVALID;
+		r = 0;
 		break;
 
 	case KVM_XEN_VCPU_ATTR_TYPE_VCPU_TIME_INFO:
-		if (vcpu->arch.xen.vcpu_time_info_set) {
+		if (vcpu->arch.xen.vcpu_time_info_set)
 			data->u.gpa = vcpu->arch.xen.vcpu_time_info_cache.gpa;
-			r = 0;
-		}
+		else
+			data->u.gpa = GPA_INVALID;
+		r = 0;
 		break;
 
 	default:
