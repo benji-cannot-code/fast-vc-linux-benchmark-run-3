d78b53f5cb50f8483e49020012e12fe8b686e9dd
Ben Skeggs <bskeggs@redhat.com>
2021-02-08 11:30:21 +1000

    drm/nouveau/nvkm: remove nvkm_subdev.index
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>
    Reviewed-by: Lyude Paul <lyude@redhat.com>

--- drivers/gpu/drm/nouveau/include/nvkm/core/device.h
+++ drivers/gpu/drm/nouveau/include/nvkm/core/device.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <core/event.h>
 enum nvkm_subdev_type;
 
-#define nvkm_devidx nvkm_subdev_type
-
 enum nvkm_device_type {
 	NVKM_DEVICE_PCI,
 	NVKM_DEVICE_AGP,
