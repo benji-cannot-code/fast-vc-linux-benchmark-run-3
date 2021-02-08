11af9fa695806b51b74ffa624a5241824a2a7820
Yang Li <yang.lee@linux.alibaba.com>
2021-02-08 18:24:00 +0800

    selftests/ipc: remove unneeded semicolon
    Eliminate the following coccicheck warning:
    ./tools/testing/selftests/ipc/msgque.c:72:3-4: Unneeded semicolon
    ./tools/testing/selftests/ipc/msgque.c:183:2-3: Unneeded semicolon
    ./tools/testing/selftests/ipc/msgque.c:191:2-3: Unneeded semicolon
    
    Signed-off-by: Yang Li <yang.lee@linux.alibaba.com>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>

--- tools/testing/selftests/ipc/msgque.c
+++ tools/testing/selftests/ipc/msgque.c
@@ -70,7 +70,7 @@ int restore_queue(struct msgque_data *msgque)
 			printf("msgsnd failed (%m)\n");
 			ret = -errno;
 			goto destroy;
-		};
+		}
 	}
 	return 0;
 
@@ -181,7 +181,7 @@ int fill_msgque(struct msgque_data *msgque)
 				IPC_NOWAIT) != 0) {
 		printf("First message send failed (%m)\n");
 		return -errno;
-	};
+	}
 
 	msgbuf.mtype = ANOTHER_MSG_TYPE;
 	memcpy(msgbuf.mtext, ANOTHER_TEST_STRING, sizeof(ANOTHER_TEST_STRING));
@@ -189,7 +189,7 @@ int fill_msgque(struct msgque_data *msgque)
 				IPC_NOWAIT) != 0) {
 		printf("Second message send failed (%m)\n");
 		return -errno;
-	};
+	}
 	return 0;
 }
 
