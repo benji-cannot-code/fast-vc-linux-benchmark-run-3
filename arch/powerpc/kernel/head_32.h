d3733d6dc25a4b19c474164237f88cc4f56d48b4
Christophe Leroy <christophe.leroy@csgroup.eu>
2021-02-08 07:17:40 +0000

    powerpc/32: Preserve cr1 in exception prolog stack check to fix build error
    THREAD_ALIGN_SHIFT = THREAD_SHIFT + 1 = PAGE_SHIFT + 1
    Maximum PAGE_SHIFT is 18 for 256k pages so
    THREAD_ALIGN_SHIFT is 19 at the maximum.
    
    No need to clobber cr1, it can be preserved when moving r1
    into CR when we check stack overflow.
    
    This reduces the number of instructions in Machine Check Exception
    prolog and fixes a build failure reported by the kernel test robot
    on v5.10 stable when building with RTAS + VMAP_STACK + KVM. That
    build failure is due to too many instructions in the prolog hence
    not fitting between 0x200 and 0x300. Allthough the problem doesn't
    show up in mainline, it is still worth the change.
    
    Fixes: 98bf2d3f4970 ("powerpc/32s: Fix RTAS machine check with VMAP stack")
    Cc: stable@vger.kernel.org
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/5ae4d545e3ac58e133d2599e0deb88843cb494fc.1612768623.git.christophe.leroy@csgroup.eu

--- arch/powerpc/kernel/head_32.h
+++ arch/powerpc/kernel/head_32.h
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	lwz	r1,TASK_STACK-THREAD(r1)
 	addi	r1, r1, THREAD_SIZE - INT_FRAME_SIZE
 1:
-	mtcrf	0x7f, r1
+	mtcrf	0x3f, r1
 	bt	32 - THREAD_ALIGN_SHIFT, stack_overflow
 #else
 	subi	r11, r1, INT_FRAME_SIZE		/* use r1 if kernel */
