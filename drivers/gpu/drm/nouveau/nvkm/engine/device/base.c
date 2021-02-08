2620850da8e06cc4916e58da3ab787dc77dfd4c9
Ben Skeggs <bskeggs@redhat.com>
2021-02-08 16:01:44 +1000

    drm/nouveau/top/ga100: initial support
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>
    Reviewed-by: Lyude Paul <lyude@redhat.com>

--- drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/device/base.c
@@ -2582,6 +2582,7 @@ nv170_chipset = {
 	.pci      = { 0x00000001, gp100_pci_new },
 	.privring = { 0x00000001, gm200_privring_new },
 	.timer    = { 0x00000001, gk20a_timer_new },
+	.top      = { 0x00000001, ga100_top_new },
 };
 
 static const struct nvkm_device_chip
@@ -2599,6 +2600,7 @@ nv172_chipset = {
 	.pci      = { 0x00000001, gp100_pci_new },
 	.privring = { 0x00000001, gm200_privring_new },
 	.timer    = { 0x00000001, gk20a_timer_new },
+	.top      = { 0x00000001, ga100_top_new },
 	.disp     = { 0x00000001, ga102_disp_new },
 	.dma      = { 0x00000001, gv100_dma_new },
 };
@@ -2618,6 +2620,7 @@ nv174_chipset = {
 	.pci      = { 0x00000001, gp100_pci_new },
 	.privring = { 0x00000001, gm200_privring_new },
 	.timer    = { 0x00000001, gk20a_timer_new },
+	.top      = { 0x00000001, ga100_top_new },
 	.disp     = { 0x00000001, ga102_disp_new },
 	.dma      = { 0x00000001, gv100_dma_new },
 };
