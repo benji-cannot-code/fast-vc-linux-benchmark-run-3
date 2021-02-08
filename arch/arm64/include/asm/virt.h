abfdf7610e30dd52f3aa2b6d797d2e63ea9d3336
Marc Zyngier <maz@kernel.org>
2021-02-08 09:57:13 +0000

    arm64: Provide an 'upgrade to VHE' stub hypercall
    As we are about to change the way a VHE system boots, let's
    provide the core helper, in the form of a stub hypercall that
    enables VHE and replicates the full EL1 context at EL2, thanks
    to EL1 and VHE-EL2 being extremely similar.
    
    On exception return, the kernel carries on at EL2. Fancy!
    
    Nothing calls this new hypercall yet, so no functional change.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Acked-by: David Brazdil <dbrazdil@google.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/20210208095732.3267263-5-maz@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/virt.h
+++ arch/arm64/include/asm/virt.h
@@ -36,8 +36,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define HVC_RESET_VECTORS 2
 
+/*
+ * HVC_VHE_RESTART - Upgrade the CPU from EL1 to EL2, if possible
+ */
+#define HVC_VHE_RESTART	3
+
 /* Max number of HYP stub hypercalls */
-#define HVC_STUB_HCALL_NR 3
+#define HVC_STUB_HCALL_NR 4
 
 /* Error returned when an invalid stub number is passed into x0 */
 #define HVC_STUB_ERR	0xbadca11
