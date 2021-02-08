66a7cd2952e22619c88a07c6b66ae631fe781ea9
Vadim Pasternak <vadimp@nvidia.com>
2021-02-08 22:16:03 +0200

    i2c: mux: mlxcpld: Get rid of adapter numbers enforcement
    Do not set the argument 'force_nr' of i2c_mux_add_adapter() routine,
    instead provide argument 'chan_id'.
    Rename mux ids array from 'adap_ids' to 'chan_ids'.
    
    The motivation is to prepare infrastructure to be able to:
    - Create only the child adapters which are actually needed - for which
      channel ids are specified.
    - To assign 'nrs' to these child adapters dynamically, with no 'nr'
      enforcement.
    
    Signed-off-by: Vadim Pasternak <vadimp@nvidia.com>
    Acked-by: Peter Rosin <peda@axentia.se>
    Signed-off-by: Wolfram Sang <wsa@kernel.org>

--- include/linux/platform_data/mlxcpld.h
+++ include/linux/platform_data/mlxcpld.h
@@ -12,12 +12,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Platform data for the CPLD I2C multiplexers */
 
 /* mlxcpld_mux_plat_data - per mux data, used with i2c_register_board_info
- * @adap_ids - adapter array
+ * @chan_ids - channels array
  * @num_adaps - number of adapters
  * @sel_reg_addr - mux select register offset in CPLD space
  */
 struct mlxcpld_mux_plat_data {
-	int *adap_ids;
+	int *chan_ids;
 	int num_adaps;
 	int sel_reg_addr;
 };
