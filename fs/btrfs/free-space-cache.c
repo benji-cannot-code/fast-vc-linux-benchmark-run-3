a545e63ad54504f55f144b82b2a2bf404bdde567
Nikolay Borisov <nborisov@suse.com>
2021-02-08 10:26:54 +0200

    btrfs: fix race between extent freeing/allocation when using bitmaps
    During allocation the allocator will try to allocate an extent using
    cluster policy. Once the current cluster is exhausted it will remove the
    entry under btrfs_free_cluster::lock and subsequently acquire
    btrfs_free_space_ctl::tree_lock to dispose of the already-deleted entry
    and adjust btrfs_free_space_ctl::total_bitmap. This poses a problem
    because there exists a race condition between removing the entry under
    one lock and doing the necessary accounting holding a different lock
    since extent freeing only uses the 2nd lock. This can result in the
    following situation:
    
    T1:                                    T2:
    btrfs_alloc_from_cluster               insert_into_bitmap <holds tree_lock>
     if (entry->bytes == 0)                   if (block_group && !list_empty(&block_group->cluster_list)) {
        rb_erase(entry)
    
     spin_unlock(&cluster->lock);
       (total_bitmaps is still 4)           spin_lock(&cluster->lock);
                                             <doesn't find entry in cluster->root>
     spin_lock(&ctl->tree_lock);             <goes to new_bitmap label, adds
    <blocked since T2 holds tree_lock>       <a new entry and calls add_new_bitmap>
    					    recalculate_thresholds  <crashes,
                                                  due to total_bitmaps
    					      becoming 5 and triggering
    					      an ASSERT>
    
    To fix this ensure that once depleted, the cluster entry is deleted when
    both cluster lock and tree locks are held in the allocator (T1), this
    ensures that even if there is a race with a concurrent
    insert_into_bitmap call it will correctly find the entry in the cluster
    and add the new space to it.
    
    CC: <stable@vger.kernel.org> # 4.4+
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: Nikolay Borisov <nborisov@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/free-space-cache.c
+++ fs/btrfs/free-space-cache.c
@@ -3126,8 +3126,6 @@ u64 btrfs_alloc_from_cluster(struct btrfs_block_group *block_group,
 			entry->bytes -= bytes;
 		}
 
-		if (entry->bytes == 0)
-			rb_erase(&entry->offset_index, &cluster->root);
 		break;
 	}
 out:
@@ -3144,7 +3142,10 @@ out:
 	ctl->free_space -= bytes;
 	if (!entry->bitmap && !btrfs_free_space_trimmed(entry))
 		ctl->discardable_bytes[BTRFS_STAT_CURR] -= bytes;
+
+	spin_lock(&cluster->lock);
 	if (entry->bytes == 0) {
+		rb_erase(&entry->offset_index, &cluster->root);
 		ctl->free_extents--;
 		if (entry->bitmap) {
 			kmem_cache_free(btrfs_free_space_bitmap_cachep,
@@ -3157,6 +3158,7 @@ out:
 		kmem_cache_free(btrfs_free_space_cachep, entry);
 	}
 
+	spin_unlock(&cluster->lock);
 	spin_unlock(&ctl->tree_lock);
 
 	return ret;
