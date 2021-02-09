db49d9b3c6918c805795773675fbef0bb59ce9a8
Peter Zijlstra <peterz@infradead.org>
2021-02-09 09:30:03 +0100

    lockdep: Noinstr annotate warn_bogus_irq_restore()
      vmlinux.o: warning: objtool: lock_is_held_type()+0x107: call to warn_bogus_irq_restore() leaves .noinstr.text section
    
    As per the general rule that WARNs are allowed to violate noinstr to
    get out, annotate it away.
    
    Fixes: 997acaf6b4b5 ("lockdep: report broken irq restoration")
    Reported-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Acked-by: Randy Dunlap <rdunlap@infradead.org> # build-tested
    Link: https://lkml.kernel.org/r/YCKyYg53mMp4E7YI@hirez.programming.kicks-ass.net

--- kernel/locking/irqflag-debug.c
+++ kernel/locking/irqflag-debug.c
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/export.h>
 #include <linux/irqflags.h>
 
-void warn_bogus_irq_restore(void)
+noinstr void warn_bogus_irq_restore(void)
 {
+	instrumentation_begin();
 	WARN_ONCE(1, "raw_local_irq_restore() called with IRQs enabled\n");
+	instrumentation_end();
 }
 EXPORT_SYMBOL(warn_bogus_irq_restore);
