4b61bd30cdf6d44c50bc41f96d74ae199e2a5698
Thomas Gleixner <tglx@linutronix.de>
2021-02-10 00:40:44 +0100

    x86/irq/64: Adjust the per CPU irq stack pointer by 8
    The per CPU hardirq_stack_ptr contains the pointer to the irq stack in the
    form that it is ready to be assigned to [ER]SP so that the first push ends
    up on the top entry of the stack.
    
    But the stack switching on 64 bit has the following rules:
    
        1) Store the current stack pointer (RSP) in the top most stack entry
           to allow the unwinder to link back to the previous stack
    
        2) Set RSP to the top most stack entry
    
        3) Invoke functions on the irq stack
    
        4) Pop RSP from the top most stack entry (stored in #1) so it's back
           to the original stack.
    
    That requires all stack switching code to decrement the stored pointer by 8
    in order to be able to store the current RSP and then set RSP to that
    location. That's a pointless exercise.
    
    Do the -8 adjustment right when storing the pointer and make the data type
    a void pointer to avoid confusion vs. the struct irq_stack data type which
    is on 64bit only used to declare the backing store. Move the definition
    next to the inuse flag so they likely end up in the same cache
    line. Sticking them into a struct to enforce it is a seperate change.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Link: https://lore.kernel.org/r/20210210002512.354260928@linutronix.de

--- arch/x86/include/asm/irq_stack.h
+++ arch/x86/include/asm/irq_stack.h
@@ -24,7 +24,7 @@ static __always_inline void __run_on_irqstack(void (*func)(void))
 	void *tos = __this_cpu_read(hardirq_stack_ptr);
 
 	__this_cpu_write(hardirq_stack_inuse, true);
-	asm_call_on_stack(tos - 8, func, NULL);
+	asm_call_on_stack(tos, func, NULL);
 	__this_cpu_write(hardirq_stack_inuse, false);
 }
 
@@ -35,7 +35,7 @@ __run_sysvec_on_irqstack(void (*func)(struct pt_regs *regs),
 	void *tos = __this_cpu_read(hardirq_stack_ptr);
 
 	__this_cpu_write(hardirq_stack_inuse, true);
-	asm_call_sysvec_on_stack(tos - 8, func, regs);
+	asm_call_sysvec_on_stack(tos, func, regs);
 	__this_cpu_write(hardirq_stack_inuse, false);
 }
 
@@ -46,7 +46,7 @@ __run_irq_on_irqstack(void (*func)(struct irq_desc *desc),
 	void *tos = __this_cpu_read(hardirq_stack_ptr);
 
 	__this_cpu_write(hardirq_stack_inuse, true);
-	asm_call_irq_on_stack(tos - 8, func, desc);
+	asm_call_irq_on_stack(tos, func, desc);
 	__this_cpu_write(hardirq_stack_inuse, false);
 }
 
