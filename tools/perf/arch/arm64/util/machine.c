070840325b5825cc7382a9d98af5e6830bfa7790
Arnaldo Carvalho de Melo <acme@redhat.com>
2021-02-09 10:17:01 -0300

    perf arm64/s390: Fix printf conversion specifier for IP addresses
    We need to use "%#" PRIx64 for u64 values, not "%lx". In arm64's and
    s390x cases the compiler doesn't complain, but lets fix this in case
    this code gets copied to a 32-bit arch, like with powerpc 32-bit that
    got fixed in the previous patch.
    
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Hewenliang <hewenliang4@huawei.com>
    Cc: Hu Shiyuan <hushiyuan@huawei.com>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Nick Desaulniers <ndesaulniers@google.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Richter <tmricht@linux.ibm.com>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/arch/arm64/util/machine.c
+++ tools/perf/arch/arm64/util/machine.c
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
 
+#include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
 #include "debug.h"
@@ -24,5 +25,5 @@ void arch__symbols__fixup_end(struct symbol *p, struct symbol *c)
 		p->end += SYMBOL_LIMIT;
 	else
 		p->end = c->start;
-	pr_debug4("%s sym:%s end:%#lx\n", __func__, p->name, p->end);
+	pr_debug4("%s sym:%s end:%#" PRIx64 "\n", __func__, p->name, p->end);
 }
