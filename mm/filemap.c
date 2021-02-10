65342b2f0865a0391c10e6096db19ea5769152eb
Will Deacon <will@kernel.org>
2021-02-10 11:15:11 +0000

    mm: filemap: Fix microblaze build failure with 'mmu_defconfig'
    Commit f9ce0be71d1f ("mm: Cleanup faultaround and finish_fault()
    codepaths") added a call to 'update_mmu_cache()' in mm/filemap.c, which
    breaks the build for microblaze:
    
      | mm/filemap.c: In function 'filemap_map_pages':
      | mm/filemap.c:3153:3: error: implicit declaration of function 'update_mmu_cache'; did you mean 'update_mmu_tlb'?
    
    Include asm/tlbflush.h in mm/filemap.c to make sure that the function
    (or indeed, macro) is available.
    
    Reported-by: Guenter Roeck <linux@roeck-us.net>
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Link: https://lore.kernel.org/r/20210209202449.GA104837@roeck-us.net
    Signed-off-by: Will Deacon <will@kernel.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ramfs.h>
 #include <linux/page_idle.h>
 #include <asm/pgalloc.h>
+#include <asm/tlbflush.h>
 #include "internal.h"
 
 #define CREATE_TRACE_POINTS
