2cd7d74f2b716a4df3784220ffd62b3fb503c2ab
George McCollister <george.mccollister@gmail.com>
2021-02-09 19:02:13 -0600

    net: dsa: xrs700x: add HSR offloading support
    Add offloading for HSR/PRP (IEC 62439-3) tag insertion, tag removal
    forwarding and duplication supported by the xrs7000 series switches.
    
    Only HSR v1 and PRP v1 are supported by the xrs7000 series switches (HSR
    v0 is not).
    
    Signed-off-by: George McCollister <george.mccollister@gmail.com>
    Reviewed-by: Vladimir Oltean <olteanv@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/dsa/xrs700x/xrs700x_reg.h
+++ drivers/net/dsa/xrs700x/xrs700x_reg.h
@@ -50,6 +50,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Port Configuration Registers - HSR/PRP */
 #define XRS_HSR_CFG(x)			(XRS_PORT_HSR_BASE(x) + 0x0)
+#define XRS_HSR_CFG_HSR_PRP		BIT(0)
+#define XRS_HSR_CFG_HSR			0
+#define XRS_HSR_CFG_PRP			BIT(8)
+#define XRS_HSR_CFG_LANID_A		0
+#define XRS_HSR_CFG_LANID_B		BIT(10)
 
 /* Port Configuration Registers - PTP */
 #define XRS_PTP_RX_SYNC_DELAY_NS_LO(x)	(XRS_PORT_PTP_BASE(x) + 0x2)
--- net/dsa/tag_xrs700x.c
+++ net/dsa/tag_xrs700x.c
@@ -12,11 +12,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static struct sk_buff *xrs700x_xmit(struct sk_buff *skb, struct net_device *dev)
 {
-	struct dsa_port *dp = dsa_slave_to_port(dev);
+	struct dsa_port *partner, *dp = dsa_slave_to_port(dev);
 	u8 *trailer;
 
 	trailer = skb_put(skb, 1);
 	trailer[0] = BIT(dp->index);
+
+	if (dp->hsr_dev)
+		dsa_hsr_foreach_port(partner, dp->ds, dp->hsr_dev)
+			if (partner != dp)
+				trailer[0] |= BIT(partner->index);
 
 	return skb;
 }
