dbd413ab450dbd41b472e447beab41079d6f959f
Gregory Greenman <gregory.greenman@intel.com>
2021-02-10 17:15:03 +0200

    iwlwifi: mvm: add RFI-M support
    RF Interference Mitigation is a new feature targeted to handle the
    problem of interference between DDR memory and WiFi. The role of
    the driver is to configure FW with the table holding a mapping
    between problematic channels/bands and the corresponding frequencies.
    
    This patch adds RFI infrastructure and adds two debugfs hooks:
    - send RFI configuration command (currently with a default table) which
      will reset feature in the FW
    - read the table, used by the FW (which can be a subset of the table
      that driver sent).
    
    Signed-off-by: Gregory Greenman <gregory.greenman@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Link: https://lore.kernel.org/r/iwlwifi.20210210171218.2cea55a09bc7.I634b79795abad499ce442631d6672ffef8fc6d41@changeid
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/fw-api.h
+++ drivers/net/wireless/intel/iwlwifi/mvm/fw-api.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
 /*
- * Copyright (C) 2012-2014, 2018 Intel Corporation
+ * Copyright (C) 2012-2014, 2018, 2020 Intel Corporation
  * Copyright (C) 2013-2015 Intel Mobile Communications GmbH
  * Copyright (C) 2016-2017 Intel Deutschland GmbH
  */
@@ -37,5 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "fw/api/stats.h"
 #include "fw/api/location.h"
 #include "fw/api/tx.h"
+#include "fw/api/rfi.h"
 
 #endif /* __fw_api_h__ */
