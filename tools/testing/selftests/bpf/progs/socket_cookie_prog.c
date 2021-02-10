c633b545b9d585c2b54ce0e5726c662adf194c9f
Florent Revest <revest@chromium.org>
2021-02-10 12:14:05 +0100

    selftests/bpf: Use vmlinux.h in socket_cookie_prog.c
    When migrating from the bpf.h's to the vmlinux.h's definition of struct
    bps_sock, an interesting LLVM behavior happened. LLVM started producing
    two fetches of ctx->sk in the sockops program this means that the
    verifier could not keep track of the NULL-check on ctx->sk. Therefore,
    we need to extract ctx->sk in a variable before checking and
    dereferencing it.
    
    Signed-off-by: Florent Revest <revest@chromium.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: KP Singh <kpsingh@kernel.org>
    Acked-by: Andrii Nakryiko <andrii@kernel.org>
    Link: https://lore.kernel.org/bpf/20210210111406.785541-4-revest@chromium.org

--- tools/testing/selftests/bpf/progs/socket_cookie_prog.c
+++ tools/testing/selftests/bpf/progs/socket_cookie_prog.c
@@ -2,11 +2,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
 // Copyright (c) 2018 Facebook
 
-#include <linux/bpf.h>
-#include <sys/socket.h>
+#include "vmlinux.h"
 
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
+
+#define AF_INET6 10
 
 struct socket_cookie {
 	__u64 cookie_key;
@@ -42,7 +43,7 @@ int set_cookie(struct bpf_sock_addr *ctx)
 SEC("sockops")
 int update_cookie(struct bpf_sock_ops *ctx)
 {
-	struct bpf_sock *sk;
+	struct bpf_sock *sk = ctx->sk;
 	struct socket_cookie *p;
 
 	if (ctx->family != AF_INET6)
@@ -51,10 +52,10 @@ int update_cookie(struct bpf_sock_ops *ctx)
 	if (ctx->op != BPF_SOCK_OPS_TCP_CONNECT_CB)
 		return 1;
 
-	if (!ctx->sk)
+	if (!sk)
 		return 1;
 
-	p = bpf_sk_storage_get(&socket_cookies, ctx->sk, 0, 0);
+	p = bpf_sk_storage_get(&socket_cookies, sk, 0, 0);
 	if (!p)
 		return 1;
 
