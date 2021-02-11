7c7e2731f4a7d6c1d3b6cf11cbb4d2fc81f2d118
Subbaraya Sundeep <sbhatta@marvell.com>
2021-02-11 21:28:29 +0530

    octeontx2-af: cn10k: Add support for programmable channels
    NIX uses unique channel numbers to identify the packet sources/sinks
    like CGX,LBK and SDP. The channel numbers assigned to each block are
    hardwired in CN9xxx silicon.
    The fixed channel numbers in CN9xxx are:
    
    0x0 | a << 8 | b            - LBK(0..3)_CH(0..63)
    0x0 | a << 8                - Reserved
    0x700 | a                   - SDP_CH(0..255)
    0x800 | a << 8 | b << 4 | c - CGX(0..7)_LMAC(0..3)_CH(0..15)
    
    All the channels in the above fixed enumerator(with maximum
    number of blocks) are not required since some chips
    have less number of blocks.
    For CN10K silicon the channel numbers need to be programmed by
    software in each block with the base channel number and range of
    channels. This patch calculates and assigns the channel numbers
    to efficiently distribute the channel number range(0-4095) among
    all the blocks. The assignment is made based on the actual number of
    blocks present and also contiguously leaving no holes.
    The channel numbers remaining after the math are used as new CPT
    replay channels present in CN10K. Also since channel numbers are
    not fixed the transmit channel link number needed by AF consumers
    is calculated by AF and sent along with nix_lf_alloc mailbox response.
    
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/marvell/octeontx2/af/rpm.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rpm.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef RPM_H
 #define RPM_H
 
+#include <linux/bits.h>
+
 /* PCI device IDs */
 #define PCI_DEVID_CN10K_RPM		0xA060
 
@@ -16,6 +18,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RPMX_CMRX_SW_INT                0x180
 #define RPMX_CMRX_SW_INT_W1S            0x188
 #define RPMX_CMRX_SW_INT_ENA_W1S        0x198
+#define RPMX_CMRX_LINK_CFG		0x1070
+
+#define RPMX_CMRX_LINK_RANGE_MASK	GENMASK_ULL(19, 16)
+#define RPMX_CMRX_LINK_BASE_MASK	GENMASK_ULL(11, 0)
 
 #define RPM_LMAC_FWI			0xa
 
