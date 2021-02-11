73ea06891efec23c83a803c4cfea4bdbef3d391f
Rafał Miłecki <rafal@milecki.pl>
2021-02-11 13:12:34 +0100

    net: broadcom: rename BCM4908 driver & update DT binding
    compatible string was updated to match normal naming convention so
    update driver as well
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bcm4908_enet.h
+++ drivers/net/ethernet/broadcom/bcm4908_enet.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0-only */
-#ifndef __BCM4908ENET_H
-#define __BCM4908ENET_H
+#ifndef __BCM4908_ENET_H
+#define __BCM4908_ENET_H
 
 #define ENET_CONTROL					0x000
 #define ENET_MIB_CTRL					0x004
