6b228182857cacf131c8247e5b2be0afb38f9157
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2021-02-11 15:40:00 +0200

    media: atomisp: Remove unused header
    sfi.h is not anyhow used by the driver. Remove it.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/staging/media/atomisp/include/linux/atomisp_platform.h
+++ drivers/staging/media/atomisp/include/linux/atomisp_platform.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/processor.h>
 
 #include <linux/i2c.h>
-#include <linux/sfi.h>
 #include <media/v4l2-subdev.h>
 #include "atomisp.h"
 
