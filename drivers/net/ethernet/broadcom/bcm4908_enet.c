892951c87a5ab745a00b7c47dee4bdc1aedd6b6e
Rafał Miłecki <rafal@milecki.pl>
2021-02-12 16:21:35 +0100

    net: broadcom: bcm4908_enet: set MTU on open & on request
    Hardware comes up with default max frame size set to 1518. When using it
    with switch it results in actual Ethernet MTU 1492:
    1518 - 14 (Ethernet header) - 4 (Broadcom's tag) - 4 (802.1q) - 4 (FCS)
    
    Above means hardware in its default state can't handle standard Ethernet
    traffic (MTU 1500).
    
    Define maximum possible Ethernet overhead and always set MAC max frame
    length accordingly. This change fixes handling Ethernet frames of length
    1506 - 1514.
    
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bcm4908_enet.c
+++ drivers/net/ethernet/broadcom/bcm4908_enet.c
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/delay.h>
 #include <linux/etherdevice.h>
+#include <linux/if_vlan.h>
 #include <linux/interrupt.h>
 #include <linux/module.h>
 #include <linux/of.h>
@@ -30,9 +31,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 						 ENET_DMA_CH_CFG_INT_BUFF_DONE)
 #define ENET_DMA_MAX_BURST_LEN			8 /* in 64 bit words */
 
-#define ENET_MTU_MIN				60
-#define ENET_MTU_MAX				1500 /* Is it possible to support 2044? */
-#define ENET_MTU_MAX_EXTRA_SIZE			32 /* L2 */
+#define ENET_MTU_MAX				ETH_DATA_LEN /* Is it possible to support 2044? */
+#define BRCM_MAX_TAG_LEN			6
+#define ENET_MAX_ETH_OVERHEAD			(ETH_HLEN + BRCM_MAX_TAG_LEN + VLAN_HLEN + \
+						 ETH_FCS_LEN + 4) /* 32 */
 
 struct bcm4908_enet_dma_ring_bd {
 	__le32 ctl;
@@ -134,6 +136,11 @@ static void bcm4908_enet_intrs_off(struct bcm4908_enet *enet)
 static void bcm4908_enet_intrs_ack(struct bcm4908_enet *enet)
 {
 	enet_write(enet, ENET_DMA_CH_RX_CFG + ENET_DMA_CH_CFG_INT_STAT, ENET_DMA_INT_DEFAULTS);
+}
+
+static void bcm4908_enet_set_mtu(struct bcm4908_enet *enet, int mtu)
+{
+	enet_umac_write(enet, UMAC_MAX_FRAME_LEN, mtu + ENET_MAX_ETH_OVERHEAD);
 }
 
 /***
@@ -247,7 +254,7 @@ static int bcm4908_enet_dma_alloc_rx_buf(struct bcm4908_enet *enet, unsigned int
 	u32 tmp;
 	int err;
 
-	slot->len = ENET_MTU_MAX + ENET_MTU_MAX_EXTRA_SIZE;
+	slot->len = ENET_MTU_MAX + ENET_MAX_ETH_OVERHEAD;
 
 	slot->skb = netdev_alloc_skb(enet->netdev, slot->len);
 	if (!slot->skb)
@@ -374,6 +381,8 @@ static void bcm4908_enet_dma_rx_ring_disable(struct bcm4908_enet *enet,
 static void bcm4908_enet_gmac_init(struct bcm4908_enet *enet)
 {
 	u32 cmd;
+
+	bcm4908_enet_set_mtu(enet, enet->netdev->mtu);
 
 	cmd = enet_umac_read(enet, UMAC_CMD);
 	enet_umac_write(enet, UMAC_CMD, cmd | CMD_SW_RESET);
@@ -560,7 +569,7 @@ static int bcm4908_enet_poll(struct napi_struct *napi, int weight)
 
 		len = (ctl & DMA_CTL_LEN_DESC_BUFLENGTH) >> DMA_CTL_LEN_DESC_BUFLENGTH_SHIFT;
 
-		if (len < ENET_MTU_MIN ||
+		if (len < ETH_ZLEN ||
 		    (ctl & (DMA_CTL_STATUS_SOP | DMA_CTL_STATUS_EOP)) != (DMA_CTL_STATUS_SOP | DMA_CTL_STATUS_EOP)) {
 			enet->netdev->stats.rx_dropped++;
 			break;
@@ -584,11 +593,21 @@ static int bcm4908_enet_poll(struct napi_struct *napi, int weight)
 	return handled;
 }
 
+static int bcm4908_enet_change_mtu(struct net_device *netdev, int new_mtu)
+{
+	struct bcm4908_enet *enet = netdev_priv(netdev);
+
+	bcm4908_enet_set_mtu(enet, new_mtu);
+
+	return 0;
+}
+
 static const struct net_device_ops bcm4908_enet_netdev_ops = {
 	.ndo_open = bcm4908_enet_open,
 	.ndo_stop = bcm4908_enet_stop,
 	.ndo_start_xmit = bcm4908_enet_start_xmit,
 	.ndo_set_mac_address = eth_mac_addr,
+	.ndo_change_mtu = bcm4908_enet_change_mtu,
 };
 
 static int bcm4908_enet_probe(struct platform_device *pdev)
@@ -626,7 +645,7 @@ static int bcm4908_enet_probe(struct platform_device *pdev)
 	eth_hw_addr_random(netdev);
 	netdev->netdev_ops = &bcm4908_enet_netdev_ops;
 	netdev->min_mtu = ETH_ZLEN;
-	netdev->mtu = ENET_MTU_MAX;
+	netdev->mtu = ETH_DATA_LEN;
 	netdev->max_mtu = ENET_MTU_MAX;
 	netif_napi_add(netdev, &enet->napi, bcm4908_enet_poll, 64);
 
