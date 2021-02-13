d03a76f8a5c14c677606586965d2e194979324c8
Sukadev Bhattiprolu <sukadev@linux.ibm.com>
2021-02-12 20:42:50 -0800

    ibmvnic: serialize access to work queue on remove
    The work queue is used to queue reset requests like CHANGE-PARAM or
    FAILOVER resets for the worker thread. When the adapter is being removed
    the adapter state is set to VNIC_REMOVING and the work queue is flushed
    so no new work is added. However the check for adapter being removed is
    racy in that the adapter can go into REMOVING state just after we check
    and we might end up adding work just as it is being flushed (or after).
    
    The ->rwi_lock is already being used to serialize queue/dequeue work.
    Extend its usage ensure there is no race when scheduling/flushing work.
    
    Fixes: 6954a9e4192b ("ibmvnic: Flush existing work items before device removal")
    Signed-off-by: Sukadev Bhattiprolu <sukadev@linux.ibm.com>
    Cc:Uwe Kleine-König <uwe@kleine-koenig.org>
    Cc:Saeed Mahameed <saeed@kernel.org>
    Reviewed-by: Dany Madden <drt@linux.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/ibm/ibmvnic.h
+++ drivers/net/ethernet/ibm/ibmvnic.h
@@ -1082,6 +1082,7 @@ struct ibmvnic_adapter {
 	struct tasklet_struct tasklet;
 	enum vnic_state state;
 	enum ibmvnic_reset_reason reset_reason;
+	/* when taking both state and rwi locks, take state lock first */
 	spinlock_t rwi_lock;
 	struct list_head rwi_list;
 	struct work_struct ibmvnic_reset;
@@ -1098,6 +1099,8 @@ struct ibmvnic_adapter {
 	struct ibmvnic_tunables desired;
 	struct ibmvnic_tunables fallback;
 
-	/* Used for serializatin of state field */
+	/* Used for serialization of state field. When taking both state
+	 * and rwi locks, take state lock first.
+	 */
 	spinlock_t state_lock;
 };
