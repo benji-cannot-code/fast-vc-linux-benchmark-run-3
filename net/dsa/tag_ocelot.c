8da27c085ecb52e925a2860b11d5f808d2d385b4
Vladimir Oltean <vladimir.oltean@nxp.com>
2021-02-14 00:37:57 +0200

    net: dsa: tag_ocelot: single out PTP-related transmit tag processing
    There is one place where we cannot avoid accessing driver data, and that
    is 2-step PTP TX timestamping, since the switch wants us to provide a
    timestamp request ID through the injection header, which naturally must
    come from a sequence number kept by the driver (it is generated by the
    .port_txtstamp method prior to the tagger's xmit).
    
    However, since other drivers like dsa_loop do not claim PTP support
    anyway, the DSA_SKB_CB(skb)->clone will always be NULL anyway, so if we
    move all PTP-related dereferences of struct ocelot and struct ocelot_port
    into a separate function, we can effectively ensure that this is dead
    code when the ocelot tagger is attached to non-ocelot switches, and the
    stateful portion of the tagger is more self-contained.
    
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dsa/tag_ocelot.c
+++ net/dsa/tag_ocelot.c
@@ -6,6 +6,25 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <soc/mscc/ocelot.h>
 #include "dsa_priv.h"
 
+static void ocelot_xmit_ptp(struct dsa_port *dp, void *injection,
+			    struct sk_buff *clone)
+{
+	struct ocelot *ocelot = dp->ds->priv;
+	struct ocelot_port *ocelot_port;
+	u64 rew_op;
+
+	ocelot_port = ocelot->ports[dp->index];
+	rew_op = ocelot_port->ptp_cmd;
+
+	/* Retrieve timestamp ID populated inside skb->cb[0] of the
+	 * clone by ocelot_port_add_txtstamp_skb
+	 */
+	if (ocelot_port->ptp_cmd == IFH_REW_OP_TWO_STEP_PTP)
+		rew_op |= clone->cb[0] << 3;
+
+	ocelot_ifh_set_rew_op(injection, rew_op);
+}
+
 static struct sk_buff *ocelot_xmit(struct sk_buff *skb,
 				   struct net_device *netdev)
 {
@@ -30,17 +49,8 @@ static struct sk_buff *ocelot_xmit(struct sk_buff *skb,
 	ocelot_ifh_set_qos_class(injection, skb->priority);
 
 	/* TX timestamping was requested */
-	if (clone) {
-		u64 rew_op = ocelot_port->ptp_cmd;
-
-		/* Retrieve timestamp ID populated inside skb->cb[0] of the
-		 * clone by ocelot_port_add_txtstamp_skb
-		 */
-		if (ocelot_port->ptp_cmd == IFH_REW_OP_TWO_STEP_PTP)
-			rew_op |= clone->cb[0] << 3;
-
-		ocelot_ifh_set_rew_op(injection, rew_op);
-	}
+	if (clone)
+		ocelot_xmit_ptp(dp, injection, clone);
 
 	return skb;
 }
