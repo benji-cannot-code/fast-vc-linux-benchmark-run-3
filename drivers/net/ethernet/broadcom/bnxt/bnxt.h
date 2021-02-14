d6872df81b8e1ffdcc15171217a562a47aec6945
Michael Chan <michael.chan@broadcom.com>
2021-02-14 18:04:55 -0500

    bnxt_en: Update firmware interface spec to 1.10.2.16.
    The main changes are the echo request/response from firmware for error
    detection and the NO_FCS feature to transmit frames without FCS.
    
    Signed-off-by: Michael Chan <michael.chan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.h
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define DRV_VER_MAJ	1
 #define DRV_VER_MIN	10
-#define DRV_VER_UPD	1
+#define DRV_VER_UPD	2
 
 #include <linux/ethtool.h>
 #include <linux/interrupt.h>
