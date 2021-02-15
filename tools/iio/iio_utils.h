7515b89cea5b33f88c0cc2a22af9e9ac6cb9f900
Alexandru Ardelean <alexandru.ardelean@analog.com>
2021-02-15 12:40:43 +0200

    tools: iio: convert iio_generic_buffer to use new IIO buffer API
    This change makes use of the new IIO buffer API to read data from an IIO
    buffer.
    It doesn't read the /sys/bus/iio/devices/iio:deviceX/scan_elements dir
    anymore, it reads /sys/bus/iio/devices/iio:deviceX/bufferY, where all the
    scan_elements have been merged together with the old/classical buffer
    attributes.
    
    And it makes use of the new IIO_BUFFER_GET_FD_IOCTL ioctl to get an FD for
    the IIO buffer for which to read data from.
    It also does a quick sanity check to see that -EBUSY is returned if reading
    the chardev after the ioctl() has succeeded.
    
    This was tested with the following cases:
     1. Tested buffer0 works with ioctl()
     2. Tested that buffer0 can't be opened via /dev/iio:deviceX after ioctl()
        This check should be omitted under normal operation; it's being done
        here to check that the driver change is sane
     3. Moved valid buffer0 to be buffer1, and tested that data comes from it
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    Link: https://lore.kernel.org/r/20210215104043.91251-25-alexandru.ardelean@analog.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- tools/iio/iio_utils.h
+++ tools/iio/iio_utils.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Made up value to limit allocation sizes */
 #define IIO_MAX_NAME_LENGTH 64
 
-#define FORMAT_SCAN_ELEMENTS_DIR "%s/scan_elements"
+#define FORMAT_SCAN_ELEMENTS_DIR "%s/buffer%d"
 #define FORMAT_TYPE_FILE "%s_type"
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
@@ -62,7 +62,7 @@ int iioutils_get_param_float(float *output, const char *param_name,
 			     const char *device_dir, const char *name,
 			     const char *generic_name);
 void bsort_channel_array_by_index(struct iio_channel_info *ci_array, int cnt);
-int build_channel_array(const char *device_dir,
+int build_channel_array(const char *device_dir, int buffer_idx,
 			struct iio_channel_info **ci_array, int *counter);
 int find_type_by_name(const char *name, const char *type);
 int write_sysfs_int(const char *filename, const char *basedir, int val);
