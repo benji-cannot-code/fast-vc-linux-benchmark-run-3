67c788c422f3053f7a9227a94c7f7a5455b6b99c
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2021-02-19 14:33:06 +0100

    leds: trigger: Fix error path to not unlock the unlocked mutex
    ttyname is allocated before the mutex is taken, so it must not be
    unlocked in the error path.
    
    Fixes: fd4a641ac88f ("leds: trigger: implement a tty trigger")
    Reported-by: Pavel Machek <pavel@ucw.cz>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Link: https://lore.kernel.org/r/20210219133307.4840-2-u.kleine-koenig@pengutronix.de
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/leds/trigger/ledtrig-tty.c
+++ drivers/leds/trigger/ledtrig-tty.c
@@ -52,10 +52,8 @@ static ssize_t ttyname_store(struct device *dev,
 
 	if (size) {
 		ttyname = kmemdup_nul(buf, size, GFP_KERNEL);
-		if (!ttyname) {
-			ret = -ENOMEM;
-			goto out_unlock;
-		}
+		if (!ttyname)
+			return -ENOMEM;
 	} else {
 		ttyname = NULL;
 	}
@@ -70,7 +68,6 @@ static ssize_t ttyname_store(struct device *dev,
 
 	trigger_data->ttyname = ttyname;
 
-out_unlock:
 	mutex_unlock(&trigger_data->mutex);
 
 	if (ttyname && !running)
