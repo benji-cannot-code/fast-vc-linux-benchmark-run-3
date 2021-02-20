1fcf603cfaf329ecea01fbbec677062d62bd13f9
William Durand <will+git@drnd.me>
2021-02-20 17:28:59 +0000

    staging: rtl8192e: rename bdHTCapBuf to bd_ht_cap_buf in bss_ht struct
    Rename bdHTCapBuf to bd_ht_cap_buf to silence a checkpatch warning about
    CamelCase.
    
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: William Durand <will+git@drnd.me>
    Link: https://lore.kernel.org/r/20210220172909.15812-4-will+git@drnd.me
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtllib_softmac.c
+++ drivers/staging/rtl8192e/rtllib_softmac.c
@@ -2239,7 +2239,7 @@ rtllib_rx_assoc_resp(struct rtllib_device *ieee, struct sk_buff *skb,
 					return 1;
 				}
 				memcpy(ieee->pHTInfo->PeerHTCapBuf,
-				       network->bssht.bdHTCapBuf,
+				       network->bssht.bd_ht_cap_buf,
 				       network->bssht.bdHTCapLen);
 				memcpy(ieee->pHTInfo->PeerHTInfoBuf,
 				       network->bssht.bdHTInfoBuf,
