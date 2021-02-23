e961a05cf0a3119b5954ce5941766fe90078efea
Oleksij Rempel <o.rempel@pengutronix.de>
2021-02-23 08:01:26 +0100

    net: introduce CAN specific pointer in the struct net_device
    Since 20dd3850bcf8 ("can: Speed up CAN frame receiption by using
    ml_priv") the CAN framework uses per device specific data in the AF_CAN
    protocol. For this purpose the struct net_device->ml_priv is used. Later
    the ml_priv usage in CAN was extended for other users, one of them being
    CAN_J1939.
    
    Later in the kernel ml_priv was converted to an union, used by other
    drivers. E.g. the tun driver started storing it's stats pointer.
    
    Since tun devices can claim to be a CAN device, CAN specific protocols
    will wrongly interpret this pointer, which will cause system crashes.
    Mostly this issue is visible in the CAN_J1939 stack.
    
    To fix this issue, we request a dedicated CAN pointer within the
    net_device struct.
    
    Reported-by: syzbot+5138c4dd15a0401bec7b@syzkaller.appspotmail.com
    Fixes: 20dd3850bcf8 ("can: Speed up CAN frame receiption by using ml_priv")
    Fixes: ffd956eef69b ("can: introduce CAN midlayer private and allocate it automatically")
    Fixes: 9d71dd0c7009 ("can: add support of SAE J1939 protocol")
    Fixes: 497a5757ce4e ("tun: switch to net core provided statistics counters")
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>
    Link: https://lore.kernel.org/r/20210223070127.4538-1-o.rempel@pengutronix.de
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/can/j1939/socket.c
+++ net/can/j1939/socket.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#include <linux/can/can-ml.h>
 #include <linux/can/core.h>
 #include <linux/can/skb.h>
 #include <linux/errqueue.h>
@@ -454,6 +455,7 @@ static int j1939_sk_bind(struct socket *sock, struct sockaddr *uaddr, int len)
 		j1939_jsk_del(priv, jsk);
 		j1939_local_ecu_put(priv, jsk->addr.src_name, jsk->addr.sa);
 	} else {
+		struct can_ml_priv *can_ml;
 		struct net_device *ndev;
 
 		ndev = dev_get_by_index(net, addr->can_ifindex);
@@ -462,15 +464,8 @@ static int j1939_sk_bind(struct socket *sock, struct sockaddr *uaddr, int len)
 			goto out_release_sock;
 		}
 
-		if (ndev->type != ARPHRD_CAN) {
-			dev_put(ndev);
-			ret = -ENODEV;
-			goto out_release_sock;
-		}
-
-		if (!ndev->ml_priv) {
-			netdev_warn_once(ndev,
-					 "No CAN mid layer private allocated, please fix your driver and use alloc_candev()!\n");
+		can_ml = can_get_ml_priv(ndev);
+		if (!can_ml) {
 			dev_put(ndev);
 			ret = -ENODEV;
 			goto out_release_sock;
