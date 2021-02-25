c66ccbb26c1ce28a5feaff5556136e011acbe131
Uwe Kleine-König <uwe@kleine-koenig.org>
2021-02-25 23:18:34 +0100

    vio: make remove callback return void
    The driver core ignores the return value of struct bus_type::remove()
    because there is only little that can be done. To simplify the quest to
    make this function return void, let struct vio_driver::remove() return
    void, too. All users already unconditionally return 0, this commit makes
    it obvious that returning an error code is a bad idea.
    
    Note there are two nominally different implementations for a vio bus:
    one in arch/sparc/kernel/vio.c and the other in
    arch/powerpc/platforms/pseries/vio.c. This patch only adapts the powerpc
    one.
    
    Before this patch for a device that was bound to a driver without a
    remove callback vio_cmo_bus_remove(viodev) wasn't called. As the device
    core still considers the device unbound after vio_bus_remove() returns
    calling this unconditionally is the consistent behaviour which is
    implemented here.
    
    Signed-off-by: Uwe Kleine-König <uwe@kleine-koenig.org>
    Reviewed-by: Tyrel Datwyler <tyreld@linux.ibm.com>
    Acked-by: Lijun Pan <ljp@linux.ibm.com>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    [mpe: Drop unneeded hvcs_remove() forward declaration, squash in
     change from sfr to drop ibmvnic_remove() forward declaration]
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lore.kernel.org/r/20210225221834.160083-1-uwe@kleine-koenig.org

--- drivers/scsi/ibmvscsi/ibmvfc.c
+++ drivers/scsi/ibmvscsi/ibmvfc.c
@@ -6039,7 +6039,7 @@ out:
  * Return value:
  * 	0
  **/
-static int ibmvfc_remove(struct vio_dev *vdev)
+static void ibmvfc_remove(struct vio_dev *vdev)
 {
 	struct ibmvfc_host *vhost = dev_get_drvdata(&vdev->dev);
 	LIST_HEAD(purge);
@@ -6071,7 +6071,6 @@ static int ibmvfc_remove(struct vio_dev *vdev)
 	spin_unlock(&ibmvfc_driver_lock);
 	scsi_host_put(vhost->host);
 	LEAVE;
-	return 0;
 }
 
 /**
