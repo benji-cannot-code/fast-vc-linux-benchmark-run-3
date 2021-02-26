3fa0a0bc4e9b831177cde8cabf267d7f50539c4d
Anshuman Khandual <anshuman.khandual@arm.com>
2021-02-25 17:17:41 -0800

    s390/mm: define arch_get_mappable_range()
    This overrides arch_get_mappabble_range() on s390 platform which will be
    used with recently added generic framework.  It modifies the existing
    range check in vmem_add_mapping() using arch_get_mappable_range().  It
    also adds a VM_BUG_ON() check that would ensure that mhp_range_allowed()
    has already been called on the hotplug path.
    
    Link: https://lkml.kernel.org/r/1612149902-7867-4-git-send-email-anshuman.khandual@arm.com
    Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Acked-by: Heiko Carstens <hca@linux.ibm.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Cc: Vasily Gorbik <gor@linux.ibm.com>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Jason Wang <jasowang@redhat.com>
    Cc: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: "Michael S. Tsirkin" <mst@redhat.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Pankaj Gupta <pankaj.gupta@cloud.ionos.com>
    Cc: Pankaj Gupta <pankaj.gupta.linux@gmail.com>
    Cc: teawater <teawaterz@linux.alibaba.com>
    Cc: Wei Yang <richard.weiyang@linux.alibaba.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/s390/mm/vmem.c
+++ arch/s390/mm/vmem.c
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *    Author(s): Heiko Carstens <heiko.carstens@de.ibm.com>
  */
 
+#include <linux/memory_hotplug.h>
 #include <linux/memblock.h>
 #include <linux/pfn.h>
 #include <linux/mm.h>
@@ -533,11 +534,22 @@ void vmem_remove_mapping(unsigned long start, unsigned long size)
 	mutex_unlock(&vmem_mutex);
 }
 
+struct range arch_get_mappable_range(void)
+{
+	struct range mhp_range;
+
+	mhp_range.start = 0;
+	mhp_range.end =  VMEM_MAX_PHYS - 1;
+	return mhp_range;
+}
+
 int vmem_add_mapping(unsigned long start, unsigned long size)
 {
+	struct range range = arch_get_mappable_range();
 	int ret;
 
-	if (start + size > VMEM_MAX_PHYS ||
+	if (start < range.start ||
+	    start + size > range.end + 1 ||
 	    start + size < start)
 		return -ERANGE;
 
