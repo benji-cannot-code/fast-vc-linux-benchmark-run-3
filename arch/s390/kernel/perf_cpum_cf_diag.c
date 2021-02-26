b94f51ca90d6d8fdd36dfbb33627932a47cd4406
Thomas Richter <tmricht@linux.ibm.com>
2021-02-26 15:00:56 +0100

    s390/cpumf: rename header file to hwctrset.h
    Signed-off-by: Thomas Richter <tmricht@linux.ibm.com>
    Suggested-by: Hendrick Brueckner <brueckner@linux.ibm.com>
    Acked-by: Heiko Carstens <hca@linux.ibm.com>
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- arch/s390/kernel/perf_cpum_cf_diag.c
+++ arch/s390/kernel/perf_cpum_cf_diag.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/timex.h>
 #include <asm/debug.h>
 
-#include <asm/perf_cpum_cf_diag.h>
+#include <asm/hwctrset.h>
 
 #define	CF_DIAG_CTRSET_DEF		0xfeef	/* Counter set header mark */
 						/* interval in seconds */
