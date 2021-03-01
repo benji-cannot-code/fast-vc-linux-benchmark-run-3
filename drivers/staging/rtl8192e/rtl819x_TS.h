e52389de5af2a88a45514c108282bda116fff565
William Durand <will+git@drnd.me>
2021-03-01 21:53:26 +0000

    staging: rtl8192e: rename TsCommonInfo to ts_common_info in rx_ts_record struct
    Rename TsCommonInfo to ts_common_info to silence a checkpatch warning
    about CamelCase.
    
    Signed-off-by: William Durand <will+git@drnd.me>
    Link: https://lore.kernel.org/r/20210301215335.767-2-will+git@drnd.me
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8192e/rtl819x_TS.h
+++ drivers/staging/rtl8192e/rtl819x_TS.h
@@ -43,7 +43,7 @@ struct tx_ts_record {
 };
 
 struct rx_ts_record {
-	struct ts_common_info TsCommonInfo;
+	struct ts_common_info ts_common_info;
 	u16				RxIndicateSeq;
 	u16				RxTimeoutIndicateSeq;
 	struct list_head		RxPendingPktList;
