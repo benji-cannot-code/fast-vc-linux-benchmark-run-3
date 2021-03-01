1b14543603e4dffa748108ad2c152c7ecc5ce395
Namhyung Kim <namhyung@kernel.org>
2021-03-01 23:04:02 +0900

    perf test: Fix cpu and thread map leaks in sw_clock_freq test
    The evlist has the maps with its own refcounts so we don't need to set
    the pointers to NULL.  Otherwise following error was reported by Asan.
    
    Also change the goto label since it doesn't need to have two.
    
      # perf test -v 25
      25: Software clock events period values        :
      --- start ---
      test child forked, pid 149154
      mmap size 528384B
      mmap size 528384B
    
      =================================================================
      ==149154==ERROR: LeakSanitizer: detected memory leaks
    
      Direct leak of 32 byte(s) in 1 object(s) allocated from:
        #0 0x7fef5cd071f8 in __interceptor_realloc ../../../../src/libsanitizer/asan/asan_malloc_linux.cpp:164
        #1 0x56260d5e8b8e in perf_thread_map__realloc /home/namhyung/project/linux/tools/lib/perf/threadmap.c:23
        #2 0x56260d3df7a9 in thread_map__new_by_tid util/thread_map.c:63
        #3 0x56260d2ac6b2 in __test__sw_clock_freq tests/sw-clock.c:65
        #4 0x56260d26d8fb in run_test tests/builtin-test.c:428
        #5 0x56260d26d8fb in test_and_print tests/builtin-test.c:458
        #6 0x56260d26fa53 in __cmd_test tests/builtin-test.c:679
        #7 0x56260d26fa53 in cmd_test tests/builtin-test.c:825
        #8 0x56260d2dbb64 in run_builtin /home/namhyung/project/linux/tools/perf/perf.c:313
        #9 0x56260d165a88 in handle_internal_command /home/namhyung/project/linux/tools/perf/perf.c:365
        #10 0x56260d165a88 in run_argv /home/namhyung/project/linux/tools/perf/perf.c:409
        #11 0x56260d165a88 in main /home/namhyung/project/linux/tools/perf/perf.c:539
        #12 0x7fef5c83cd09 in __libc_start_main ../csu/libc-start.c:308
    
        ...
      test child finished with 1
      ---- end ----
      Software clock events period values      : FAILED!
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>
    Acked-by: Jiri Olsa <jolsa@redhat.com>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Leo Yan <leo.yan@linaro.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: https://lore.kernel.org/r/20210301140409.184570-5-namhyung@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- tools/perf/tests/sw-clock.c
+++ tools/perf/tests/sw-clock.c
@@ -43,8 +43,8 @@ static int __test__sw_clock_freq(enum perf_sw_ids clock_id)
 		.disabled = 1,
 		.freq = 1,
 	};
-	struct perf_cpu_map *cpus;
-	struct perf_thread_map *threads;
+	struct perf_cpu_map *cpus = NULL;
+	struct perf_thread_map *threads = NULL;
 	struct mmap *md;
 
 	attr.sample_freq = 500;
@@ -67,13 +67,10 @@ static int __test__sw_clock_freq(enum perf_sw_ids clock_id)
 	if (!cpus || !threads) {
 		err = -ENOMEM;
 		pr_debug("Not enough memory to create thread/cpu maps\n");
-		goto out_free_maps;
+		goto out_delete_evlist;
 	}
 
 	perf_evlist__set_maps(&evlist->core, cpus, threads);
-
-	cpus	= NULL;
-	threads = NULL;
 
 	if (evlist__open(evlist)) {
 		const char *knob = "/proc/sys/kernel/perf_event_max_sample_rate";
@@ -130,10 +127,9 @@ out_init:
 		err = -1;
 	}
 
-out_free_maps:
+out_delete_evlist:
 	perf_cpu_map__put(cpus);
 	perf_thread_map__put(threads);
-out_delete_evlist:
 	evlist__delete(evlist);
 	return err;
 }
