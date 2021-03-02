9cb671146d69dbd70bc572e8bed4ffa0ac0d25ed
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2021-03-01 18:31:25 -0600

    ALSA: hda: intel-sdw-acpi: add missing include files
    We rely on implicit includes, list out explicitly what this code
    relies on.
    
    Suggested-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Reviewed-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
    Reviewed-by: Bard Liao <bard.liao@intel.com>
    Acked-by: Mark Brown <broonie@kernel.org>
    Acked-by: Vinod Koul <vkoul@kernel.org>
    Link: https://lore.kernel.org/r/20210302003125.1178419-8-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/hda/intel-sdw-acpi.c
+++ sound/hda/intel-sdw-acpi.c
@@ -7,7 +7,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/acpi.h>
+#include <linux/bits.h>
+#include <linux/bitfield.h>
+#include <linux/device.h>
+#include <linux/errno.h>
 #include <linux/export.h>
+#include <linux/fwnode.h>
 #include <linux/module.h>
 #include <linux/soundwire/sdw_intel.h>
 #include <linux/string.h>
