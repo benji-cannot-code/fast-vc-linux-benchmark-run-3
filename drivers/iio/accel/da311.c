c44d5120a6d0d8f10ae632fde53a6bf6bcae97c9
Gwendal Grignou <gwendal@chromium.org>
2021-03-09 15:43:14 -0800

    iio: Remove kernel-doc keyword in file header comment
    Remove kernel-doc keyword from function header comment.
    It fixes issues spotted by scripts/kernel-doc like:
    drivers/iio/<driver>.c:3: info: Scanning doc for function <component name>
    drivers/iio/<driver>.c:X: warning: expecting prototype for <component name>.
      Prototype was for <function>() instead
    
    To reproduce the errors:
    scripts/kernel-doc -v -none $(find drivers/iio/ -name \*.c \
    -exec head -2 {} \+ | grep -B2 -e '\*\*' | grep '==' | cut -d ' ' -f 2)
    
    After, confirm these errors are gone with:
    scripts/kernel-doc -v -none $(git show --name-only  | grep -e "^driver")
    
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Link: https://lore.kernel.org/r/20210309234314.2208256-1-gwendal@chromium.org
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/accel/da311.c
+++ drivers/iio/accel/da311.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0-only
-/**
+/*
  * IIO driver for the MiraMEMS DA311 3-axis accelerometer
  *
  * Copyright (c) 2016 Hans de Goede <hdegoede@redhat.com>
