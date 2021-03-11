afa4d39fdb606bc30e4b636ef60c853d2e971b68
Vladimir Murzin <vladimir.murzin@arm.com>
2021-03-11 13:32:16 +0100

    ARM: 9069/1: NOMMU: Fix conversion for_each_membock() to for_each_mem_range()
    for_each_mem_range() uses a loop variable, yet looking into code it is
    not just iteration counter but more complex entity which encodes
    information about memblock. Thus condition i == 0 looks fragile.
    Indeed, it broke boot of R-class platforms since it never took i == 0
    path (due to i was set to 1). Fix that with restoring original flag
    check.
    
    Fixes: b10d6bca8720 ("arch, drivers: replace for_each_membock() with for_each_mem_range()")
    Signed-off-by: Vladimir Murzin <vladimir.murzin@arm.com>
    Acked-by: Mike Rapoport <rppt@linux.ibm.com>
    Signed-off-by: Russell King <rmk+kernel@armlinux.org.uk>

--- arch/arm/mm/pmsa-v7.c
+++ arch/arm/mm/pmsa-v7.c
@@ -236,6 +236,7 @@ void __init pmsav7_adjust_lowmem_bounds(void)
 	phys_addr_t mem_end;
 	phys_addr_t reg_start, reg_end;
 	unsigned int mem_max_regions;
+	bool first = true;
 	int num;
 	u64 i;
 
@@ -264,7 +265,7 @@ void __init pmsav7_adjust_lowmem_bounds(void)
 #endif
 
 	for_each_mem_range(i, &reg_start, &reg_end) {
-		if (i == 0) {
+		if (first) {
 			phys_addr_t phys_offset = PHYS_OFFSET;
 
 			/*
@@ -276,6 +277,7 @@ void __init pmsav7_adjust_lowmem_bounds(void)
 			mem_start = reg_start;
 			mem_end = reg_end;
 			specified_mem_size = mem_end - mem_start;
+			first = false;
 		} else {
 			/*
 			 * memblock auto merges contiguous blocks, remove
