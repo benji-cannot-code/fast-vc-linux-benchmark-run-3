a115f55b8028fd90265b548005fbc14a65ea51f9
Marco Cesati <marcocesati@gmail.com>
2021-03-12 09:26:12 +0100

    staging: rtl8723bs: remove typedefs in odm_EdcaTurboCheck.h
    This commit fixes the following checkpatch.pl warning:
    
        WARNING: do not add new typedefs
        #11: FILE: hal/odm_EdcaTurboCheck.h:11:
        +typedef struct _EDCA_TURBO_ {
    
    Signed-off-by: Marco Cesati <marco.cesati@gmail.com>
    Link: https://lore.kernel.org/r/20210312082638.25512-8-marco.cesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/odm_EdcaTurboCheck.h
+++ drivers/staging/rtl8723bs/hal/odm_EdcaTurboCheck.h
@@ -9,12 +9,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ODMEDCATURBOCHECK_H__
 #define __ODMEDCATURBOCHECK_H__
 
-typedef struct _EDCA_TURBO_ {
+struct EDCA_T { /* _EDCA_TURBO_ */
 	bool bCurrentTurboEDCA;
 	bool bIsCurRDLState;
 
 	u32 prv_traffic_idx; /*  edca turbo */
-} EDCA_T, *pEDCA_T;
+};
 
 void odm_EdcaTurboCheck(void *pDM_VOID);
 void ODM_EdcaTurboInit(void *pDM_VOID);
