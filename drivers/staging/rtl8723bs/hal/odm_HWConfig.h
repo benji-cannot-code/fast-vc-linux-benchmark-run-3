3eb3d195a11766c63beb43baf0a6f3db15ca9960
Marco Cesati <marcocesati@gmail.com>
2021-03-12 09:26:13 +0100

    staging: rtl8723bs: remove typedefs in odm_HWConfig.h
    This commit fixes the following checkpatch.pl warnings:
    
        WARNING: do not add new typedefs
        #33: FILE: hal/odm_HWConfig.h:33:
        +typedef struct _Phy_Rx_AGC_Info {
    
        WARNING: do not add new typedefs
        #41: FILE: hal/odm_HWConfig.h:41:
        +typedef struct _Phy_Status_Rpt_8192cd {
    
        WARNING: do not add new typedefs
        #80: FILE: hal/odm_HWConfig.h:80:
        +typedef struct _Phy_Status_Rpt_8812 {
    
    Signed-off-by: Marco Cesati <marco.cesati@gmail.com>
    Link: https://lore.kernel.org/r/20210312082638.25512-9-marco.cesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/odm_HWConfig.h
+++ drivers/staging/rtl8723bs/hal/odm_HWConfig.h
@@ -31,16 +31,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*  structure and define */
 /*  */
 
-typedef struct _Phy_Rx_AGC_Info {
+struct PHY_RX_AGC_INFO_T {
 	#if (ODM_ENDIAN_TYPE == ODM_ENDIAN_LITTLE)
 		u8 gain:7, trsw:1;
 	#else
 		u8 trsw:1, gain:7;
 	#endif
-} PHY_RX_AGC_INFO_T, *pPHY_RX_AGC_INFO_T;
+};
 
-typedef struct _Phy_Status_Rpt_8192cd {
-	PHY_RX_AGC_INFO_T path_agc[2];
+struct PHY_STATUS_RPT_8192CD_T {
+	struct PHY_RX_AGC_INFO_T path_agc[2];
 	u8 ch_corr[2];
 	u8 cck_sig_qual_ofdm_pwdb_all;
 	u8 cck_agc_rpt_ofdm_cfosho_a;
@@ -75,10 +75,10 @@ typedef struct _Phy_Status_Rpt_8192cd {
 	u8 sgi_en:1;
 	u8 antsel_rx_keep_2:1;	/* ex_intf_flg:1; */
 #endif
-} PHY_STATUS_RPT_8192CD_T, *PPHY_STATUS_RPT_8192CD_T;
+};
 
 
-typedef struct _Phy_Status_Rpt_8812 {
+struct PHY_STATUS_RPT_8812_T {
 	/* 2012.05.24 LukeLee: This structure should take big/little endian in consideration later..... */
 
 	/* DWORD 0 */
@@ -119,7 +119,7 @@ typedef struct _Phy_Status_Rpt_8812 {
 	u8 antidx_anta:3;
 	u8 antidx_antb:3;
 	u8 resvd_1:2;
-} PHY_STATUS_RPT_8812_T, *PPHY_STATUS_RPT_8812_T;
+};
 
 
 void ODM_PhyStatusQuery(
