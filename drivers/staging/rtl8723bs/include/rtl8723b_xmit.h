542447813385e272aaf8280b2e7e69b38c11d410
Marco Cesati <marcocesati@gmail.com>
2021-03-12 09:26:25 +0100

    staging: rtl8723bs: remove typedefs in rtl8723b_xmit.h
    This commit fixes the following checkpatch.pl warning:
    
        WARNING: do not add new typedefs
        #62: FILE: include/rtl8723b_xmit.h:62:
        +typedef struct txdesc_8723b {
    
    Signed-off-by: Marco Cesati <marco.cesati@gmail.com>
    Link: https://lore.kernel.org/r/20210312082638.25512-21-marco.cesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/include/rtl8723b_xmit.h
+++ drivers/staging/rtl8723bs/include/rtl8723b_xmit.h
@@ -60,7 +60,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*  */
 /* defined for TX DESC Operation */
 /*  */
-typedef struct txdesc_8723b {
+struct TXDESC_8723B {
 	/*  Offset 0 */
 	u32 pktlen:16;
 	u32 offset:8;
@@ -176,7 +176,7 @@ typedef struct txdesc_8723b {
 	u32 txbf_path:1;
 	u32 seq:12;
 	u32 final_data_rate:8;
-} TXDESC_8723B, *PTXDESC_8723B;
+};
 
 #ifndef __INC_HAL8723BDESC_H
 #define __INC_HAL8723BDESC_H
