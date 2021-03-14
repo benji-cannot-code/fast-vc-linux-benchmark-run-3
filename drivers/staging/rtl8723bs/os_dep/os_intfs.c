d915082d318cbacdb47f54e8db6bfe02a75267b5
Fabio Aiuto <fabioaiuto83@gmail.com>
2021-03-14 19:12:23 +0100

    staging: rtl8723bs: remove unused code blocks conditioned by never set CONFIG_PNO_SUPPORT
    Remove conditional code blocks checked by unused CONFIG_PNO_SUPPORT
    
    Cleaning required in TODO file:
    
    find and remove code blocks guarded by never set CONFIG_FOO defines
    
    Signed-off-by: Fabio Aiuto <fabioaiuto83@gmail.com>
    Link: https://lore.kernel.org/r/a8ba6c7996ba3f08054c433860be2de66b25d942.1615744948.git.fabioaiuto83@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/os_dep/os_intfs.c
+++ drivers/staging/rtl8723bs/os_dep/os_intfs.c
@@ -1506,10 +1506,6 @@ int rtw_resume_process_wow(struct adapter *padapter)
 		goto exit;
 	}
 
-#ifdef CONFIG_PNO_SUPPORT
-	pwrpriv->pno_in_resume = true;
-#endif
-
 	if (pwrpriv->wowlan_mode) {
 		rtw_set_ps_mode(padapter, PS_MODE_ACTIVE, 0, 0, "WOWLAN");
 
@@ -1781,9 +1777,6 @@ int rtw_resume_common(struct adapter *padapter)
 
 	if (pwrpriv) {
 		pwrpriv->bInSuspend = false;
-	#ifdef CONFIG_PNO_SUPPORT
-		pwrpriv->pno_in_resume = false;
-	#endif
 	}
 	DBG_871X_LEVEL(_drv_always_, "%s:%d in %d ms\n", __func__, ret,
 		jiffies_to_msecs(jiffies - start_time));
