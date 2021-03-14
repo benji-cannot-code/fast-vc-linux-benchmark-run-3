a7033469adf4826bde5227a58048c1573ec8038b
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2021-03-14 16:46:53 +0000

    iio: adc: adi-axi-adc: Drop false marking for kernel-doc
    This comment block isn't in kernel-doc format so drop the /** marking.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Alexandru Ardelean <ardeleanalex@gmail.com>
    Reviewed-by: Lars-Peter Clausen <lars@metafoo.de>
    Cc: Nuno Sá <nuno.sa@analog.com>
    Link: https://lore.kernel.org/r/20210314164655.408461-7-jic23@kernel.org

--- drivers/iio/adc/adi-axi-adc.c
+++ drivers/iio/adc/adi-axi-adc.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fpga/adi-axi-common.h>
 #include <linux/iio/adc/adi-axi-adc.h>
 
-/**
+/*
  * Register definitions:
  *   https://wiki.analog.com/resources/fpga/docs/axi_adc_ip#register_map
  */
