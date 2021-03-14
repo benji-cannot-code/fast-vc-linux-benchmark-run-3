d506b97b1c731f74e91fa78300398e6ff756d346
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2021-03-14 16:46:51 +0000

    iio: dac: ad5504: fix wrong part number in kernel-doc structure name.
    Probably a bit of cut and paste where someone forgot to change the
    part number.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Lars-Peter Clausen <lars@metafoo.de>
    Link: https://lore.kernel.org/r/20210314164655.408461-5-jic23@kernel.org

--- drivers/iio/dac/ad5504.c
+++ drivers/iio/dac/ad5504.c
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AD5504_DAC_PWRDN_3STATE		1
 
 /**
- * struct ad5446_state - driver instance specific data
+ * struct ad5504_state - driver instance specific data
  * @spi:			spi_device
  * @reg:		supply regulator
  * @vref_mv:		actual reference voltage used
