0e0efbf1d39b5e6c090cc9a6d502103a2784c86c
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2021-03-14 18:15:09 +0000

    staging:iio:cdc:ad7150: Add copyright notice given substantial changes.
    It seems to me that the changes made to get this ready to move out of
    staging are substantial enough to warant a copyright notice addition.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    Link: https://lore.kernel.org/r/20210314181511.531414-23-jic23@kernel.org

--- drivers/staging/iio/cdc/ad7150.c
+++ drivers/staging/iio/cdc/ad7150.c
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * AD7150 capacitive sensor driver supporting AD7150/1/6
  *
  * Copyright 2010-2011 Analog Devices Inc.
+ * Copyright 2021 Jonathan Cameron <Jonathan.Cameron@huawei.com>
  */
 
 #include <linux/bitfield.h>
