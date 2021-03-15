da967d97ef10c52a147a4c8f6a0a78f05b9dd8f1
Marco Cesati <marcocesati@gmail.com>
2021-03-15 18:06:00 +0100

    Staging: rtl8723bs: fix spaces in basic_types.h
    This commit fixes the following checkpatch.pl error:
    
        ERROR:POINTER_LOCATION: "(foo*)" should be "(foo *)"
        #16: FILE: ./include/basic_types.h:16:
        +#define FIELD_OFFSET(s, field)	((__kernel_ssize_t)&((s*)(0))->field)
    
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Marco Cesati <marcocesati@gmail.com>
    Link: https://lore.kernel.org/r/20210315170618.2566-40-marcocesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/include/basic_types.h
+++ drivers/staging/rtl8723bs/include/basic_types.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
-#define FIELD_OFFSET(s, field)	((__kernel_ssize_t)&((s*)(0))->field)
+#define FIELD_OFFSET(s, field)	((__kernel_ssize_t)&((s *)(0))->field)
 
 #define SIZE_PTR __kernel_size_t
 #define SSIZE_PTR __kernel_ssize_t
