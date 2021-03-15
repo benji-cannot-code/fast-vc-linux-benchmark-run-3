0db850061e30125440ca37c2e2381f9d3cbecf46
Marco Cesati <marcocesati@gmail.com>
2021-03-15 18:05:54 +0100

    Staging: rtl8723bs: fix spaces in odm_debug.c
    This commit fixes the following checkpatch.pl error:
    
        ERROR:POINTER_LOCATION: "foo * bar" should be "foo *bar"
        #10: FILE: ./hal/odm_debug.c:10:
        +void ODM_InitDebugSetting(struct DM_ODM_T * pDM_Odm)
    
    Reviewed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Marco Cesati <marcocesati@gmail.com>
    Link: https://lore.kernel.org/r/20210315170618.2566-34-marcocesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/odm_debug.c
+++ drivers/staging/rtl8723bs/hal/odm_debug.c
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "odm_precomp.h"
 
-void ODM_InitDebugSetting(struct DM_ODM_T * pDM_Odm)
+void ODM_InitDebugSetting(struct DM_ODM_T *pDM_Odm)
 {
 	pDM_Odm->DebugLevel = ODM_DBG_LOUD;
 
