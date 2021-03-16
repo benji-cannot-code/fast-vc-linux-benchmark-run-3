634d5e76731b70a62cd8434059969cd86c62f0ad
Harshad Shirwadkar <harshadshirwadkar@gmail.com>
2021-03-16 15:19:21 -0700

    ext4: fix rename whiteout with fast commit
    This patch adds rename whiteout support in fast commits. Note that the
    whiteout object that gets created is actually char device. Which
    imples, the function ext4_inode_journal_mode(struct inode *inode)
    would return "JOURNAL_DATA" for this inode. This has a consequence in
    fast commit code that it will make creation of the whiteout object a
    fast-commit ineligible behavior and thus will fall back to full
    commits. With this patch, this can be observed by running fast commits
    with rename whiteout and seeing the stats generated by ext4_fc_stats
    tracepoint as follows:
    
    ext4_fc_stats: dev 254:32 fc ineligible reasons:
    XATTR:0, CROSS_RENAME:0, JOURNAL_FLAG_CHANGE:0, NO_MEM:0, SWAP_BOOT:0,
    RESIZE:0, RENAME_DIR:0, FALLOC_RANGE:0, INODE_JOURNAL_DATA:16;
    num_commits:6, ineligible: 6, numblks: 3
    
    So in short, this patch guarantees that in case of rename whiteout, we
    fall back to full commits.
    
    Amir mentioned that instead of creating a new whiteout object for
    every rename, we can create a static whiteout object with irrelevant
    nlink. That will make fast commits to not fall back to full
    commit. But until this happens, this patch will ensure correctness by
    falling back to full commits.
    
    Fixes: 8016e29f4362 ("ext4: fast commit recovery path")
    Cc: stable@kernel.org
    Signed-off-by: Harshad Shirwadkar <harshadshirwadkar@gmail.com>
    Link: https://lore.kernel.org/r/20210316221921.1124955-1-harshadshirwadkar@gmail.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -3874,6 +3874,7 @@ static int ext4_rename(struct user_namespace *mnt_userns, struct inode *old_dir,
 		retval = ext4_mark_inode_dirty(handle, whiteout);
 		if (unlikely(retval))
 			goto end_rename;
+
 	}
 	if (!new.bh) {
 		retval = ext4_add_entry(handle, new.dentry, old.inode);
@@ -3947,6 +3948,8 @@ static int ext4_rename(struct user_namespace *mnt_userns, struct inode *old_dir,
 			ext4_fc_track_unlink(handle, new.dentry);
 		__ext4_fc_track_link(handle, old.inode, new.dentry);
 		__ext4_fc_track_unlink(handle, old.inode, old.dentry);
+		if (whiteout)
+			__ext4_fc_track_create(handle, whiteout, old.dentry);
 	}
 
 	if (new.inode) {
