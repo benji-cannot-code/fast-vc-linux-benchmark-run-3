5f2878e94143702ae5d09bb047c83919b36a4d74
Marco Cesati <marcocesati@gmail.com>
2021-03-17 23:21:28 +0100

    Staging: rtl8723bs: fix in odm_DynamicBBPowerSaving.h
    This commit converts names of structs / enums
    in hal/odm_DynamicBBPowerSaving.h from ALL_CAPS format to lowercase
    
    Signed-off-by: Marco Cesati <marcocesati@gmail.com>
    Link: https://lore.kernel.org/r/20210317222130.29528-42-marcocesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/odm_DynamicBBPowerSaving.h
+++ drivers/staging/rtl8723bs/hal/odm_DynamicBBPowerSaving.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef	__ODMDYNAMICBBPOWERSAVING_H__
 #define    __ODMDYNAMICBBPOWERSAVING_H__
 
-struct PS_T { /* _Dynamic_Power_Saving_ */
+struct ps_t { /* _Dynamic_Power_Saving_ */
 	u8 PreCCAState;
 	u8 CurCCAState;
 
