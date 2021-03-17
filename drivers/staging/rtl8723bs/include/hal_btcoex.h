92fbe7a70cca6c7b3dafec7da495cbd65cab6752
Marco Cesati <marcocesati@gmail.com>
2021-03-17 23:21:16 +0100

    Staging: rtl8723bs: fix names in hal_btcoex.h
    This commit converts names of structs / enums
    in include/hal_btcoex.h from ALL_CAPS format to lowercase
    
    Signed-off-by: Marco Cesati <marcocesati@gmail.com>
    Link: https://lore.kernel.org/r/20210317222130.29528-30-marcocesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/include/hal_btcoex.h
+++ drivers/staging/rtl8723bs/include/hal_btcoex.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*  Some variables can't get from outsrc BT-Coex, */
 /*  so we need to save here */
-struct BT_COEXIST {
+struct bt_coexist {
 	u8 bBtExist;
 	u8 btTotalAntNum;
 	u8 btChipType;
