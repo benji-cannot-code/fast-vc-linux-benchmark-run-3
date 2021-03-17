34ae4725083ae2b0f3478beb9c18bbb5169fd712
Marco Cesati <marcocesati@gmail.com>
2021-03-17 23:21:02 +0100

    Staging: rtl8723bs: fix names in rtl8723b_xmit.h
    This commit converts names of structs / enums
    in include/rtl8723b_xmit.h from ALL_CAPS format to lowercase
    
    Signed-off-by: Marco Cesati <marcocesati@gmail.com>
    Link: https://lore.kernel.org/r/20210317222130.29528-16-marcocesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/include/rtl8723b_xmit.h
+++ drivers/staging/rtl8723bs/include/rtl8723b_xmit.h
@@ -60,7 +60,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*  */
 /* defined for TX DESC Operation */
 /*  */
-struct TXDESC_8723B {
+struct txdesc_8723b {
 	/*  Offset 0 */
 	u32 pktlen:16;
 	u32 offset:8;
