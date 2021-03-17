ced0209fe1074599fb5d939386db6a23e6a27cbf
Marco Cesati <marcocesati@gmail.com>
2021-03-17 23:21:19 +0100

    Staging: rtl8723bs: fix names in odm_EdcaTurboCheck.h
    This commit converts names of structs / enums
    in hal/odm_EdcaTurboCheck.h from ALL_CAPS format to lowercase
    
    Signed-off-by: Marco Cesati <marcocesati@gmail.com>
    Link: https://lore.kernel.org/r/20210317222130.29528-33-marcocesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/odm_EdcaTurboCheck.h
+++ drivers/staging/rtl8723bs/hal/odm_EdcaTurboCheck.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ODMEDCATURBOCHECK_H__
 #define __ODMEDCATURBOCHECK_H__
 
-struct EDCA_T { /* _EDCA_TURBO_ */
+struct edca_t { /* _EDCA_TURBO_ */
 	bool bCurrentTurboEDCA;
 	bool bIsCurRDLState;
 
