f05859eea92fd2eb37ada39db0c6c1cc083f2ed9
Lee Jones <lee.jones@linaro.org>
2021-03-18 12:44:15 +0000

    crypto: chelsio - Fix some kernel-doc issues
    Fixes the following W=1 kernel build warning(s):
    
     drivers/crypto/chelsio/chcr_core.c:2: warning: wrong kernel-doc identifier on line:
     drivers/crypto/chelsio/chcr_algo.c:806: warning: Function parameter or member 'wrparam' not described in 'create_cipher_wr'
     drivers/crypto/chelsio/chcr_algo.c:806: warning: Excess function parameter 'req' description in 'create_cipher_wr'
     drivers/crypto/chelsio/chcr_algo.c:806: warning: Excess function parameter 'ctx' description in 'create_cipher_wr'
     drivers/crypto/chelsio/chcr_algo.c:806: warning: Excess function parameter 'qid' description in 'create_cipher_wr'
     drivers/crypto/chelsio/chcr_algo.c:806: warning: Excess function parameter 'op_type' description in 'create_cipher_wr'
     drivers/crypto/chelsio/chcr_algo.c:1566: warning: Function parameter or member 'req' not described in 'create_hash_wr'
     drivers/crypto/chelsio/chcr_algo.c:1566: warning: Function parameter or member 'param' not described in 'create_hash_wr'
    
    Cc: Ayush Sawal <ayush.sawal@chelsio.com>
    Cc: Vinay Kumar Yadav <vinay.yadav@chelsio.com>
    Cc: Rohit Maheshwari <rohitm@chelsio.com>
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Manoj Malviya <manojmalviya@chelsio.com>
    Cc: Atul Gupta <atul.gupta@chelsio.com>
    Cc: Jitendra Lulla <jlulla@chelsio.com>
    Cc: M R Gowda <yeshaswi@chelsio.com>
    Cc: Harsh Jain <harsh@chelsio.com>
    Cc: linux-crypto@vger.kernel.org
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/chelsio/chcr_core.c
+++ drivers/crypto/chelsio/chcr_core.c
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/**
+/*
  * This file is part of the Chelsio T4/T5/T6 Ethernet driver for Linux.
  *
  * Copyright (C) 2011-2016 Chelsio Communications.  All rights reserved.
