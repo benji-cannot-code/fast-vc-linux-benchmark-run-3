faa987e4dd33731f98580c02ef1b4d730f95a22f
Kishon Vijay Abraham I <kishon@ti.com>
2021-03-19 18:11:26 +0530

    dt-bindings: phy: phy-cadence-sierra: Add binding to model Sierra as clock provider
    Add #clock-cells binding to model Sierra as clock provider and include
    clock IDs for PLL_CMNLC and PLL_CMNLC1.
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Link: https://lore.kernel.org/r/20210319124128.13308-12-kishon@ti.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>

--- include/dt-bindings/phy/phy-cadence.h
+++ include/dt-bindings/phy/phy-cadence.h
@@ -14,4 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define CDNS_TORRENT_REFCLK_DRIVER      0
 
+/* Sierra */
+#define CDNS_SIERRA_PLL_CMNLC		0
+#define CDNS_SIERRA_PLL_CMNLC1		1
+
 #endif /* _DT_BINDINGS_CADENCE_SERDES_H */
