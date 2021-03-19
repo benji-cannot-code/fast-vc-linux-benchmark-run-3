b1d821d5b1a8ab62b9d41f0d5fd6696008e4ece9
Stefano Stabellini <stefano.stabellini@xilinx.com>
2021-03-19 13:01:40 -0700

    xen/arm: introduce XENFEAT_direct_mapped and XENFEAT_not_direct_mapped
    Newer Xen versions expose two Xen feature flags to tell us if the domain
    is directly mapped or not. Only when a domain is directly mapped it
    makes sense to enable swiotlb-xen on ARM.
    
    Introduce a function on ARM to check the new Xen feature flags and also
    to deal with the legacy case. Call the function xen_swiotlb_detect.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>
    Reviewed-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Link: https://lore.kernel.org/r/20210319200140.12512-1-sstabellini@kernel.org
    Signed-off-by: Juergen Gross <jgross@suse.com>

---
+++ include/xen/arm/swiotlb-xen.h
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_ARM_SWIOTLB_XEN_H
+#define _ASM_ARM_SWIOTLB_XEN_H
+
+extern int xen_swiotlb_detect(void);
+
+#endif /* _ASM_ARM_SWIOTLB_XEN_H */
--- include/xen/swiotlb-xen.h
+++ include/xen/swiotlb-xen.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __LINUX_SWIOTLB_XEN_H
 
 #include <linux/swiotlb.h>
+#include <asm/xen/swiotlb-xen.h>
 
 void xen_dma_sync_for_cpu(struct device *dev, dma_addr_t handle,
 			  size_t size, enum dma_data_direction dir);
