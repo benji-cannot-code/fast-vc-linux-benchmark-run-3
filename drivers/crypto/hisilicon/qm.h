7c6c0fd0e148a4bcea61f8c1ceba32e6a2fbea5f
Weili Qian <qianweili@huawei.com>
2021-03-20 19:27:46 +0800

    crypto: hisilicon/qm - add queue isolation support for Kunpeng930
    Kunpeng930 supports doorbell isolation to ensure that each queue
    has an independent doorbell address space.
    
    Signed-off-by: Weili Qian <qianweili@huawei.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/hisilicon/qm.h
+++ drivers/crypto/hisilicon/qm.h
@@ -203,6 +203,7 @@ struct hisi_qm {
 	const char *dev_name;
 	struct pci_dev *pdev;
 	void __iomem *io_base;
+	void __iomem *db_io_base;
 	u32 sqe_size;
 	u32 qp_base;
 	u32 qp_num;
@@ -210,6 +211,7 @@ struct hisi_qm {
 	u32 ctrl_qp_num;
 	u32 max_qp_num;
 	u32 vfs_num;
+	u32 db_interval;
 	struct list_head list;
 	struct hisi_qm_list *qm_list;
 
@@ -251,7 +253,7 @@ struct hisi_qm {
 	/* doorbell isolation enable */
 	bool use_db_isolation;
 	resource_size_t phys_base;
-	resource_size_t phys_size;
+	resource_size_t db_phys_base;
 	struct uacce_device *uacce;
 	int mode;
 };
