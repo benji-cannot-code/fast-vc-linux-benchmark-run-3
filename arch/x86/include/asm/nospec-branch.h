67ca940f1a914c02b36ab52e00d63f11c47be871
Ingo Molnar <mingo@kernel.org>
2021-03-21 23:32:33 +0100

    x86: Remove unusual Unicode characters from comments
    We've accumulated a few unusual Unicode characters in arch/x86/
    over the years, substitute them with their proper ASCII equivalents.
    
    A few of them were a whitespace equivalent: ' ' - the use was harmless.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: linux-kernel@vger.kernel.org

--- arch/x86/include/asm/nospec-branch.h
+++ arch/x86/include/asm/nospec-branch.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*
  * Google experimented with loop-unrolling and this turned out to be
- * the optimal version — two calls, each with their own speculation
+ * the optimal version - two calls, each with their own speculation
  * trap should their return address end up getting used, in a loop.
  */
 #define __FILL_RETURN_BUFFER(reg, nr, sp)	\
