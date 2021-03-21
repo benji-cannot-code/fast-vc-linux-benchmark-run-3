0ff22a8a57499e9d8841bac355635e22b748540d
Aditya Srivastava <yashsri421@gmail.com>
2021-03-21 18:00:07 +0530

    crypto: nx - fix incorrect kernel-doc comment syntax in files
    The opening comment mark '/**' is used for highlighting the beginning of
    kernel-doc comments.
    There are certain files in drivers/crypto/nx, which follow this syntax,
    but the content inside does not comply with kernel-doc.
    Such lines were probably not meant for kernel-doc parsing, but are parsed
    due to the presence of kernel-doc like comment syntax(i.e, '/**'), which
    causes unexpected warnings from kernel-doc.
    
    E.g., presence of kernel-doc like comment in the header lines for
    drivers/crypto/nx/nx-sha256.c at header causes these warnings:
    "warning: Function parameter or member 'tfm' not described in 'nx_crypto_ctx_sha256_init'"
    "warning: expecting prototype for SHA(). Prototype was for nx_crypto_ctx_sha256_init() instead"
    
    Similarly for other files too.
    
    Provide a simple fix by replacing such occurrences with general comment
    format, i.e. '/*', to prevent kernel-doc from parsing it.
    
    Signed-off-by: Aditya Srivastava <yashsri421@gmail.com>
    Acked-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/nx/nx-aes-xcbc.c
+++ drivers/crypto/nx/nx-aes-xcbc.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0-only
-/**
+/*
  * AES XCBC routines supporting the Power 7+ Nest Accelerators driver
  *
  * Copyright (C) 2011-2012 International Business Machines Inc.
