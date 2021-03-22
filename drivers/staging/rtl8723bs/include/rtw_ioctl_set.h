0b743a2a9838403b4a3f2edc9d87419ff7e82236
Marco Cesati <marcocesati@gmail.com>
2021-03-22 14:37:03 +0100

    Staging: rtl8723bs: remove useless structs in rtw_ioctl_set.h
    Remove the following unreferenced struct in include/rtw_ioctl_set.h:
    
    	struct BSSIDInfo
    
    Signed-off-by: Marco Cesati <marcocesati@gmail.com>
    Link: https://lore.kernel.org/r/20210322133717.17797-6-marcocesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/include/rtw_ioctl_set.h
+++ drivers/staging/rtl8723bs/include/rtw_ioctl_set.h
@@ -11,12 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 typedef u8 NDIS_802_11_PMKID_VALUE[16];
 
-struct BSSIDInfo {
-	NDIS_802_11_MAC_ADDRESS  BSSID;
-	NDIS_802_11_PMKID_VALUE  PMKID;
-};
-
-
 u8 rtw_set_802_11_authentication_mode(struct adapter *pdapter, enum ndis_802_11_authentication_mode authmode);
 u8 rtw_set_802_11_bssid(struct adapter *padapter, u8 *bssid);
 u8 rtw_set_802_11_add_wep(struct adapter *padapter, struct ndis_802_11_wep *wep);
