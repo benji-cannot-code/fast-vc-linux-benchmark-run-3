272e29f9e2014f398adc9f3aaed6f60561ba2100
Marco Cesati <marcocesati@gmail.com>
2021-03-22 14:37:10 +0100

    Staging: rtl8723bs: remove useless structs in rtw_wifi_regd.h
    Remove the following unreferenced struct in include/rtw_wifi_regd.h:
    
    	struct country_code_to_enum_rd
    
    Signed-off-by: Marco Cesati <marcocesati@gmail.com>
    Link: https://lore.kernel.org/r/20210322133717.17797-13-marcocesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/include/rtw_wifi_regd.h
+++ drivers/staging/rtl8723bs/include/rtw_wifi_regd.h
@@ -9,11 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __RTW_WIFI_REGD_H__
 #define __RTW_WIFI_REGD_H__
 
-struct country_code_to_enum_rd {
-	u16 countrycode;
-	const char *iso_name;
-};
-
 enum country_code_type_t {
 	COUNTRY_CODE_USER = 0,
 
