9f5188abdadc79e6c8310539b2ad96d061e5bae3
Fenghua Yu <fenghua.yu@intel.com>
2021-03-22 13:53:23 +0000

    x86/cpufeatures: Enumerate #DB for bus lock detection
    A bus lock is acquired through either a split locked access to writeback
    (WB) memory or any locked access to non-WB memory. This is typically >1000
    cycles slower than an atomic operation within a cache line. It also
    disrupts performance on other cores.
    
    Some CPUs have the ability to notify the kernel by a #DB trap after a user
    instruction acquires a bus lock and is executed. This allows the kernel to
    enforce user application throttling or mitigation. Both breakpoint and bus
    lock can trigger the #DB trap in the same instruction and the ordering of
    handling them is the kernel #DB handler's choice.
    
    The CPU feature flag to be shown in /proc/cpuinfo will be "bus_lock_detect".
    
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Link: https://lore.kernel.org/r/20210322135325.682257-2-fenghua.yu@intel.com

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -355,6 +355,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define X86_FEATURE_AVX512_VPOPCNTDQ	(16*32+14) /* POPCNT for vectors of DW/QW */
 #define X86_FEATURE_LA57		(16*32+16) /* 5-level page tables */
 #define X86_FEATURE_RDPID		(16*32+22) /* RDPID instruction */
+#define X86_FEATURE_BUS_LOCK_DETECT	(16*32+24) /* Bus Lock detect */
 #define X86_FEATURE_CLDEMOTE		(16*32+25) /* CLDEMOTE instruction */
 #define X86_FEATURE_MOVDIRI		(16*32+27) /* MOVDIRI instruction */
 #define X86_FEATURE_MOVDIR64B		(16*32+28) /* MOVDIR64B instruction */
