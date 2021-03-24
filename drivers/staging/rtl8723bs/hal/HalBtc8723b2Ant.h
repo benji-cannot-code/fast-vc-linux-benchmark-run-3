cb0aacf8d227c28857d525dd8fe6f477c88d26b5
Marco Cesati <marcocesati@gmail.com>
2021-03-24 13:44:56 +0100

    Staging: rtl8723bs: remove named enums in HalBtc8723b2Ant.h
    Remove the following unnecessary enum names in
    hal/HalBtc8723b2Ant.h:
    
    	enum bt_info_src_8723b_2ant
    	enum bt_8723b_2ant_bt_status
    	enum bt_8723b_2ant_coex_algo
    
    Signed-off-by: Marco Cesati <marcocesati@gmail.com>
    Link: https://lore.kernel.org/r/20210324124456.25221-34-marcocesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/HalBtc8723b2Ant.h
+++ drivers/staging/rtl8723bs/hal/HalBtc8723b2Ant.h
@@ -17,14 +17,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define		BTC_RSSI_COEX_THRESH_TOL_8723B_2ANT		2
 
-enum bt_info_src_8723b_2ant {
+enum {
 	BT_INFO_SRC_8723B_2ANT_WIFI_FW        = 0x0,
 	BT_INFO_SRC_8723B_2ANT_BT_RSP         = 0x1,
 	BT_INFO_SRC_8723B_2ANT_BT_ACTIVE_SEND = 0x2,
 	BT_INFO_SRC_8723B_2ANT_MAX
 };
 
-enum bt_8723b_2ant_bt_status {
+enum {
 	BT_8723B_2ANT_BT_STATUS_NON_CONNECTED_IDLE = 0x0,
 	BT_8723B_2ANT_BT_STATUS_CONNECTED_IDLE     = 0x1,
 	BT_8723B_2ANT_BT_STATUS_INQ_PAGE           = 0x2,
@@ -34,7 +34,7 @@ enum bt_8723b_2ant_bt_status {
 	BT_8723B_2ANT_BT_STATUS_MAX
 };
 
-enum bt_8723b_2ant_coex_algo {
+enum {
 	BT_8723B_2ANT_COEX_ALGO_UNDEFINED       = 0x0,
 	BT_8723B_2ANT_COEX_ALGO_SCO             = 0x1,
 	BT_8723B_2ANT_COEX_ALGO_HID             = 0x2,
