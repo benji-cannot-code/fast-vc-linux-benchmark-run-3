2dee90bb60d6bed5616abdf18e951a2c421b39cc
Lecopzer Chen <lecopzer.chen@mediatek.com>
2021-03-24 12:05:18 +0800

    arm64: kasan: don't populate vmalloc area for CONFIG_KASAN_VMALLOC
    Linux support KAsan for VMALLOC since commit 3c5c3cfb9ef4da9
    ("kasan: support backing vmalloc space with real shadow memory")
    
    Like how the MODULES_VADDR does now, just not to early populate
    the VMALLOC_START between VMALLOC_END.
    
    Before:
    
    MODULE_VADDR: no mapping, no zero shadow at init
    VMALLOC_VADDR: backed with zero shadow at init
    
    After:
    
    MODULE_VADDR: no mapping, no zero shadow at init
    VMALLOC_VADDR: no mapping, no zero shadow at init
    
    Thus the mapping will get allocated on demand by the core function
    of KASAN_VMALLOC.
    
      -----------  vmalloc_shadow_start
     |           |
     |           |
     |           | <= non-mapping
     |           |
     |           |
     |-----------|
     |///////////|<- kimage shadow with page table mapping.
     |-----------|
     |           |
     |           | <= non-mapping
     |           |
     ------------- vmalloc_shadow_end
     |00000000000|
     |00000000000| <= Zero shadow
     |00000000000|
     ------------- KASAN_SHADOW_END
    
    Signed-off-by: Lecopzer Chen <lecopzer.chen@mediatek.com>
    Acked-by: Andrey Konovalov <andreyknvl@gmail.com>
    Tested-by: Andrey Konovalov <andreyknvl@gmail.com>
    Tested-by: Ard Biesheuvel <ardb@kernel.org>
    Link: https://lore.kernel.org/r/20210324040522.15548-2-lecopzer.chen@mediatek.com
    [catalin.marinas@arm.com: add a build check on VMALLOC_START != MODULES_END]
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/mm/kasan_init.c
+++ arch/arm64/mm/kasan_init.c
@@ -215,6 +215,7 @@ static void __init kasan_init_shadow(void)
 {
 	u64 kimg_shadow_start, kimg_shadow_end;
 	u64 mod_shadow_start, mod_shadow_end;
+	u64 vmalloc_shadow_end;
 	phys_addr_t pa_start, pa_end;
 	u64 i;
 
@@ -223,6 +224,8 @@ static void __init kasan_init_shadow(void)
 
 	mod_shadow_start = (u64)kasan_mem_to_shadow((void *)MODULES_VADDR);
 	mod_shadow_end = (u64)kasan_mem_to_shadow((void *)MODULES_END);
+
+	vmalloc_shadow_end = (u64)kasan_mem_to_shadow((void *)VMALLOC_END);
 
 	/*
 	 * We are going to perform proper setup of shadow memory.
@@ -242,12 +245,18 @@ static void __init kasan_init_shadow(void)
 
 	kasan_populate_early_shadow(kasan_mem_to_shadow((void *)PAGE_END),
 				   (void *)mod_shadow_start);
-	kasan_populate_early_shadow((void *)kimg_shadow_end,
-				   (void *)KASAN_SHADOW_END);
 
-	if (kimg_shadow_start > mod_shadow_end)
-		kasan_populate_early_shadow((void *)mod_shadow_end,
-					    (void *)kimg_shadow_start);
+	if (IS_ENABLED(CONFIG_KASAN_VMALLOC)) {
+		BUILD_BUG_ON(VMALLOC_START != MODULES_END);
+		kasan_populate_early_shadow((void *)vmalloc_shadow_end,
+					    (void *)KASAN_SHADOW_END);
+	} else {
+		kasan_populate_early_shadow((void *)kimg_shadow_end,
+					    (void *)KASAN_SHADOW_END);
+		if (kimg_shadow_start > mod_shadow_end)
+			kasan_populate_early_shadow((void *)mod_shadow_end,
+						    (void *)kimg_shadow_start);
+	}
 
 	for_each_mem_range(i, &pa_start, &pa_end) {
 		void *start = (void *)__phys_to_virt(pa_start);
