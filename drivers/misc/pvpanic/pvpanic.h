c93395e1beaffd253c999f4e618339f7ee8d3709
Mihai Carabas <mihai.carabas@oracle.com>
2021-03-24 16:49:15 +0200

    misc/pvpanic: probe multiple instances
    Create the mecahism that allows multiple pvpanic instances to call
    pvpanic_probe and receive panic events. A global list will retain all the
    mapped addresses where to write panic events.
    
    Signed-off-by: Mihai Carabas <mihai.carabas@oracle.com>
    Link: https://lore.kernel.org/r/1616597356-20696-3-git-send-email-mihai.carabas@oracle.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/misc/pvpanic/pvpanic.h
+++ drivers/misc/pvpanic/pvpanic.h
@@ -9,8 +9,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef PVPANIC_H_
 #define PVPANIC_H_
 
-void pvpanic_probe(void __iomem *base, unsigned int dev_cap);
-void pvpanic_remove(void);
-void pvpanic_set_events(unsigned int dev_events);
+struct pvpanic_instance {
+	void __iomem *base;
+	unsigned int capability;
+	unsigned int events;
+	struct list_head list;
+};
+
+int pvpanic_probe(struct pvpanic_instance *pi);
+void pvpanic_remove(struct pvpanic_instance *pi);
 
 #endif /* PVPANIC_H_ */
