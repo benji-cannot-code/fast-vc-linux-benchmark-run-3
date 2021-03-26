699e418f84abe27d56a777948347c2d69dd227b0
Marco Cesati <marcocesati@gmail.com>
2021-03-26 18:41:44 +0100

    Staging: rtl8723bs: remove obsolete comments
    Remove some orphan comments in the staging/rtl8723bs driver
    
    Signed-off-by: Marco Cesati <marcocesati@gmail.com>
    Link: https://lore.kernel.org/r/20210326174145.19427-1-marcocesati@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/odm_interface.h
+++ drivers/staging/rtl8723bs/hal/odm_interface.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 
-/*  =========== Constant/Structure/Enum/... Define */
-
 /*  =========== Macro Define */
 
 #define _reg_all(_name)			ODM_##_name
