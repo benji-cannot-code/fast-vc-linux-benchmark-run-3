46bd173c3dfbd09fa09fc6c385db6630cd96f21c
Colin Ian King <colin.king@canonical.com>
2021-03-26 19:52:51 +0000

    io_uring: remove unsued assignment to pointer io
    There is an assignment to io that is never read after the assignment,
    the assignment is redundant and can be removed.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -4799,7 +4799,6 @@ static int io_connect(struct io_kiocb *req, unsigned int issue_flags)
 			ret = -ENOMEM;
 			goto out;
 		}
-		io = req->async_data;
 		memcpy(req->async_data, &__io, sizeof(__io));
 		return -EAGAIN;
 	}
