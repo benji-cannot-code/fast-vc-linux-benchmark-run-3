be6bd115a3f8e01e3e13a210a78ddaca48d0bf55
Qiheng Lin <linqiheng@huawei.com>
2021-03-31 14:06:46 +0800

    Drivers: hv: vmbus: Remove unused linux/version.h header
    That file is not needed in hv.c.
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Signed-off-by: Qiheng Lin <linqiheng@huawei.com>
    Link: https://lore.kernel.org/r/20210331060646.2471-1-linqiheng@huawei.com
    Signed-off-by: Wei Liu <wei.liu@kernel.org>

--- drivers/hv/hv.c
+++ drivers/hv/hv.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
 #include <linux/hyperv.h>
-#include <linux/version.h>
 #include <linux/random.h>
 #include <linux/clockchips.h>
 #include <linux/interrupt.h>
