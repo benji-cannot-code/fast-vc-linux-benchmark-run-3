83a38fa9795d854c78e29820c0b81352aba91667
Colin Ian King <colin.king@canonical.com>
2021-04-01 16:28:39 +0100

    crypto: sa2ul - Fix memory leak of rxd
    There are two error return paths that are not freeing rxd and causing
    memory leaks.  Fix these.
    
    Addresses-Coverity: ("Resource leak")
    Fixes: 00c9211f60db ("crypto: sa2ul - Fix DMA mapping API usage")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/sa2ul.c
+++ drivers/crypto/sa2ul.c
@@ -1147,8 +1147,10 @@ static int sa_run(struct sa_req *req)
 		mapped_sg->sgt.sgl = src;
 		mapped_sg->sgt.orig_nents = src_nents;
 		ret = dma_map_sgtable(ddev, &mapped_sg->sgt, dir_src, 0);
-		if (ret)
+		if (ret) {
+			kfree(rxd);
 			return ret;
+		}
 
 		mapped_sg->dir = dir_src;
 		mapped_sg->mapped = true;
@@ -1156,8 +1158,10 @@ static int sa_run(struct sa_req *req)
 		mapped_sg->sgt.sgl = req->src;
 		mapped_sg->sgt.orig_nents = sg_nents;
 		ret = dma_map_sgtable(ddev, &mapped_sg->sgt, dir_src, 0);
-		if (ret)
+		if (ret) {
+			kfree(rxd);
 			return ret;
+		}
 
 		mapped_sg->dir = dir_src;
 		mapped_sg->mapped = true;
