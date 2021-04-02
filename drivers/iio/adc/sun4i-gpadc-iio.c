e179c50cbce299cb40a8e5a152b21e93fa779d66
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2021-04-02 19:45:41 +0100

    iio:adc:sun4i-gpadc: Use new IRQF_NO_AUTOEN flag instead of request then disable
    This new flag ensures a requested irq is not autoenabled, thus removing
    the need for the disable_irq() that follows and closing off any chance
    of spurious interrupts.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Cc: Maxime Ripard <maxime.ripard@bootlin.com>
    Reviewed-by: Barry Song <song.bao.hua@hisilicon.com>
    Reviewed-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Reviewed-by: Lars-Peter Clausen <lars@metafoo.de>
    Link: https://lore.kernel.org/r/20210402184544.488862-5-jic23@kernel.org

--- drivers/iio/adc/sun4i-gpadc-iio.c
+++ drivers/iio/adc/sun4i-gpadc-iio.c
@@ -471,7 +471,8 @@ static int sun4i_irq_init(struct platform_device *pdev, const char *name,
 	}
 
 	*irq = ret;
-	ret = devm_request_any_context_irq(&pdev->dev, *irq, handler, 0,
+	ret = devm_request_any_context_irq(&pdev->dev, *irq, handler,
+					   IRQF_NO_AUTOEN,
 					   devname, info);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "could not request %s interrupt: %d\n",
@@ -479,7 +480,6 @@ static int sun4i_irq_init(struct platform_device *pdev, const char *name,
 		return ret;
 	}
 
-	disable_irq(*irq);
 	atomic_set(atomic, 0);
 
 	return 0;
