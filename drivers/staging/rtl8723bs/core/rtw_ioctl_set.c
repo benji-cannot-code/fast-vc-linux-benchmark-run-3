05c5481df061b9a1ecd2a2e395b5af5d2b44d581
Fabio Aiuto <fabioaiuto83@gmail.com>
2021-04-04 16:09:42 +0200

    staging: rtl8723bs: place constant on the right side of the test in core/rtw_ioctl_set.c
    fix posst-commit hook checkpatch issues:
    
    WARNING: Comparisons should place the constant on the right
    side of the test
    40: FILE: drivers/staging/rtl8723bs/core/rtw_ioctl_set.c:71:
    +			if (_SUCCESS != ret)
    
    WARNING: Comparisons should place the constant on the right
    side of the test
    69: FILE: drivers/staging/rtl8723bs/core/rtw_ioctl_set.c:122:
    +					if (_SUCCESS != ret)
    
    Signed-off-by: Fabio Aiuto <fabioaiuto83@gmail.com>
    Link: https://lore.kernel.org/r/3c701cacd71a6bb0f59242fc3a987f72dabb93e2.1617545239.git.fabioaiuto83@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/core/rtw_ioctl_set.c
+++ drivers/staging/rtl8723bs/core/rtw_ioctl_set.c
@@ -69,7 +69,7 @@ u8 rtw_do_join(struct adapter *padapter)
 		) {
 			/*  submit site_survey_cmd */
 			ret = rtw_sitesurvey_cmd(padapter, &pmlmepriv->assoc_ssid, 1, NULL, 0);
-			if (_SUCCESS != ret)
+			if (ret != _SUCCESS)
 				pmlmepriv->to_join = false;
 
 		} else {
@@ -120,7 +120,7 @@ u8 rtw_do_join(struct adapter *padapter)
 				) {
 					/* DBG_871X("rtw_do_join() when   no desired bss in scanning queue\n"); */
 					ret = rtw_sitesurvey_cmd(padapter, &pmlmepriv->assoc_ssid, 1, NULL, 0);
-					if (_SUCCESS != ret)
+					if (ret != _SUCCESS)
 						pmlmepriv->to_join = false;
 
 				} else {
