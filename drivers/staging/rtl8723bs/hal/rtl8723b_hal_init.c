191037bb2019fffd7e9c697f991f5a6f413add4e
Fabio Aiuto <fabioaiuto83@gmail.com>
2021-04-05 18:49:56 +0200

    staging: rtl8723bs: remove empty for cycles
    remove for-cycles left empty after RT_TRACE deletion
    and unused index variables
    
    Suggested-by: Joe Perches <joe@perches.com>
    Signed-off-by: Fabio Aiuto <fabioaiuto83@gmail.com>
    Link: https://lore.kernel.org/r/09875f599705d8f50af3511fa6bfaa54b9446a86.1617640221.git.fabioaiuto83@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/rtl8723b_hal_init.c
+++ drivers/staging/rtl8723bs/hal/rtl8723b_hal_init.c
@@ -3467,7 +3467,6 @@ s32 c2h_handler_8723b(struct adapter *padapter, u8 *buf)
 {
 	struct c2h_evt_hdr_88xx *pC2hEvent = (struct c2h_evt_hdr_88xx *)buf;
 	s32 ret = _SUCCESS;
-	u8 index = 0;
 
 	if (!pC2hEvent) {
 		DBG_8192C("%s(): pC2hEventis NULL\n", __func__);
@@ -3492,8 +3491,6 @@ s32 c2h_handler_8723b(struct adapter *padapter, u8 *buf)
 		break;
 
 	case C2H_HW_INFO_EXCH:
-		for (index = 0; index < pC2hEvent->plen; index++) {
-		}
 		break;
 
 	case C2H_8723B_BT_INFO:
@@ -3514,8 +3511,6 @@ exit:
 
 static void process_c2h_event(struct adapter *padapter, struct c2h_evt_hdr_t *pC2hEvent, u8 *c2hBuf)
 {
-	u8 index = 0;
-
 	if (!c2hBuf) {
 		DBG_8192C("%s c2hbuff is NULL\n", __func__);
 		return;
@@ -3538,8 +3533,6 @@ static void process_c2h_event(struct adapter *padapter, struct c2h_evt_hdr_t *pC
 		break;
 
 	case C2H_HW_INFO_EXCH:
-		for (index = 0; index < pC2hEvent->CmdLen; index++) {
-		}
 		break;
 
 	case C2H_8723B_BT_INFO:
