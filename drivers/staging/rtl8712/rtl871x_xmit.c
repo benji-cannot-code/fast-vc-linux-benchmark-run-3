a66020b36c0807e5fce7782836a01e715ceb1f2e
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2021-04-06 22:41:32 +0200

    staging: rtl8712: Use constants from <linux/ieee80211.h>
    Some constants defined in wifi.h are already defined in <linux/ieee80211.h>
    with some other (but similar) names.
    Be consistent and use the ones from <linux/ieee80211.h>.
    
    The conversions made are:
    _SSID_IE_                -->  WLAN_EID_SSID
    _SUPPORTEDRATES_IE_      -->  WLAN_EID_SUPP_RATES
    _DSSET_IE_               -->  WLAN_EID_DS_PARAMS
    _IBSS_PARA_IE_           -->  WLAN_EID_IBSS_PARAMS
    _ERPINFO_IE_             -->  WLAN_EID_ERP_INFO
    _EXT_SUPPORTEDRATES_IE_  -->  WLAN_EID_EXT_SUPP_RATES
    
    _HT_CAPABILITY_IE_       -->  WLAN_EID_HT_CAPABILITY
    _HT_EXTRA_INFO_IE_       -->  WLAN_EID_HT_OPERATION    (not used)
    _HT_ADD_INFO_IE_         -->  WLAN_EID_HT_OPERATION
    
    _VENDOR_SPECIFIC_IE_     -->  WLAN_EID_VENDOR_SPECIFIC
    
    _RESERVED47_             --> (not used)
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Link: https://lore.kernel.org/r/fe35fb45323adc3a30f31b7280cec7700fd325d8.1617741313.git.christophe.jaillet@wanadoo.fr
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8712/rtl871x_xmit.c
+++ drivers/staging/rtl8712/rtl871x_xmit.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "osdep_intf.h"
 #include "usb_ops.h"
 
+#include <linux/ieee80211.h>
 
 static const u8 P802_1H_OUI[P80211_OUI_LEN] = {0x00, 0x00, 0xf8};
 static const u8 RFC1042_OUI[P80211_OUI_LEN] = {0x00, 0x00, 0x00};
@@ -710,7 +711,7 @@ void r8712_update_protection(struct _adapter *padapter, u8 *ie, uint ie_len)
 		break;
 	case AUTO_VCS:
 	default:
-		perp = r8712_get_ie(ie, _ERPINFO_IE_, &erp_len, ie_len);
+		perp = r8712_get_ie(ie, WLAN_EID_ERP_INFO, &erp_len, ie_len);
 		if (!perp) {
 			pxmitpriv->vcs = NONE_VCS;
 		} else {
