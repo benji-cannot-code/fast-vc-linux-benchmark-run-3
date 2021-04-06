47b1edcb73cfbe3a835d49e5bf630015d67eb350
Zhansaya Bagdauletkyzy <zhansayabagdaulet@gmail.com>
2021-04-06 17:43:46 +0600

    staging: rtl8712: Remove extra blank lines
    Remove extra blank lines after an open brace to adhere to Linux kernel
    coding style.
    Reported by checkpatch.
    
    Signed-off-by: Zhansaya Bagdauletkyzy <zhansayabagdaulet@gmail.com>
    Link: https://lore.kernel.org/r/f61f6d330bc3fa53d4d420754d1d461b6cfcb2de.1617708653.git.zhansayabagdaulet@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8712/rtl871x_mlme.h
+++ drivers/staging/rtl8712/rtl871x_mlme.h
@@ -78,7 +78,6 @@ struct sitesurvey_ctrl {
 };
 
 struct mlme_priv {
-
 	spinlock_t lock;
 	spinlock_t lock2;
 	sint	fw_state;	/*shall we protect this variable? */
--- drivers/staging/rtl8712/rtl871x_recv.h
+++ drivers/staging/rtl8712/rtl871x_recv.h
@@ -38,7 +38,6 @@ struct smooth_rssi_data {
 };
 
 struct rx_pkt_attrib {
-
 	u8	amsdu;
 	u8	order;
 	u8	qos;
