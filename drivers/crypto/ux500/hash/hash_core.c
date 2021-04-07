960619b917c4a1a95df7a9b55f570b28fc315410
YueHaibing <yuehaibing@huawei.com>
2021-04-07 22:18:17 +0800

    crypto: ux500 - Remove redundant dev_err calls
    There is a error message within devm_ioremap_resource
    already, so remove the dev_err call to avoid redundant
    error message.
    
    Signed-off-by: YueHaibing <yuehaibing@huawei.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/ux500/hash/hash_core.c
+++ drivers/crypto/ux500/hash/hash_core.c
@@ -1677,7 +1677,6 @@ static int ux500_hash_probe(struct platform_device *pdev)
 	device_data->phybase = res->start;
 	device_data->base = devm_ioremap_resource(dev, res);
 	if (IS_ERR(device_data->base)) {
-		dev_err(dev, "%s: ioremap() failed!\n", __func__);
 		ret = PTR_ERR(device_data->base);
 		goto out;
 	}
