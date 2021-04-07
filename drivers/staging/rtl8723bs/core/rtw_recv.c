800203461af8f44a8eb29eed5ea2263817b3c15c
Fabio Aiuto <fabioaiuto83@gmail.com>
2021-04-07 15:49:33 +0200

    staging: rtl8723bs: remove empty for cycles left by DBG_871X removal
    remove empty for cycles and unused counter variables to suppress
    compiler warnings.
    
    Signed-off-by: Fabio Aiuto <fabioaiuto83@gmail.com>
    Link: https://lore.kernel.org/r/fe758f88ef6d6eaad9762d16d78dcfa876fcf654.1617802415.git.fabioaiuto83@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/core/rtw_recv.c
+++ drivers/staging/rtl8723bs/core/rtw_recv.c
@@ -1455,10 +1455,6 @@ validate_80211w_fail:
 
 static inline void dump_rx_packet(u8 *ptr)
 {
-	int i;
-
-	for (i = 0; i < 64; i = i+8)
-		{}
 }
 
 static signed int validate_recv_frame(struct adapter *adapter, union recv_frame *precv_frame)
