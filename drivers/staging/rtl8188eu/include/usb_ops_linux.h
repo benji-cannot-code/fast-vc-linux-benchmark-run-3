91ff2e2fdaa916b7cfe85b57f9695d5d7045581d
Martin Kaiser <martin@kaiser.cx>
2021-04-08 21:55:52 +0200

    staging: rtl8188eu: make ffaddr2pipehdl static
    This function is used only inside usb_ops_linux.c.
    
    Signed-off-by: Martin Kaiser <martin@kaiser.cx>
    Link: https://lore.kernel.org/r/20210408195601.4762-3-martin@kaiser.cx
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8188eu/include/usb_ops_linux.h
+++ drivers/staging/rtl8188eu/include/usb_ops_linux.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define USB_HIGH_SPEED_BULK_SIZE	512
 #define USB_FULL_SPEED_BULK_SIZE	64
 
-unsigned int ffaddr2pipehdl(struct dvobj_priv *pdvobj, u32 addr);
-
 u8 usb_read8(struct adapter *adapter, u32 addr);
 u16 usb_read16(struct adapter *adapter, u32 addr);
 u32 usb_read32(struct adapter *adapter, u32 addr);
