2fff221c358390f8b3b01adfec12a4feecad5470
Fabio Aiuto <fabioaiuto83@gmail.com>
2021-04-09 12:01:39 +0200

    staging: rtl8723bs: remove decl of unused static const variable in os_dep/ioctl_linux.c
    fix following kernel test robot warning:
    
       At top level:
       drivers/staging/rtl8723bs/os_dep/ioctl_linux.c:36:27:
    warning: 'iw_operation_mode' defined but not used
    		[-Wunused-const-variable=]
          36 | static const char * const iw_operation_mode[] = {
             |                           ^~~~~~~~~~~~~~~~~
    
    Signed-off-by: Fabio Aiuto <fabioaiuto83@gmail.com>
    Link: https://lore.kernel.org/r/05cc97a114269ee55ac9a91c61493c885fcc0193.1617962215.git.fabioaiuto83@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/os_dep/ioctl_linux.c
+++ drivers/staging/rtl8723bs/os_dep/ioctl_linux.c
@@ -34,10 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static u32 rtw_rates[] = {1000000, 2000000, 5500000, 11000000,
 	6000000, 9000000, 12000000, 18000000, 24000000, 36000000, 48000000, 54000000};
 
-static const char * const iw_operation_mode[] = {
-	"Auto", "Ad-Hoc", "Managed",  "Master", "Repeater", "Secondary", "Monitor"
-};
-
 void indicate_wx_scan_complete_event(struct adapter *padapter)
 {
 	union iwreq_data wrqu;
