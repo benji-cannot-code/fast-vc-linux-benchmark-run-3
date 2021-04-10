d6aa5a78406744fddb28cce9b8ee7b1435f0c425
Fabio Aiuto <fabioaiuto83@gmail.com>
2021-04-10 16:20:21 +0200

    staging: rtl8723bs: remove code blocks guarded by DEBUG symbol
    remove code blocks guarded by DEBUG symbol
    remove undefinition of DEBUG
    
    Signed-off-by: Fabio Aiuto <fabioaiuto83@gmail.com>
    Link: https://lore.kernel.org/r/381da3f0c1ddf825b2aa7cbfd074ee2bf42aac4f.1618064274.git.fabioaiuto83@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/include/autoconf.h
+++ drivers/staging/rtl8723bs/include/autoconf.h
@@ -45,13 +45,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Debug Related Config
  */
-#undef DEBUG
 
-#ifdef DEBUG
-#define DBG	1	/*  for ODM & BTCOEX debug */
-#else /*  !DEBUG */
 #define DBG	0	/*  for ODM & BTCOEX debug */
-#endif /*  !DEBUG */
 
 /* define DBG_XMIT_BUF */
 /* define DBG_XMIT_BUF_EXT */
