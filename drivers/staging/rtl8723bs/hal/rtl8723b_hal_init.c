6e15df6c8fbb3150d8e6cbbf2ef39869c03b7eaa
Fabio Aiuto <fabioaiuto83@gmail.com>
2021-04-10 16:20:29 +0200

    staging: rtl8723bs: put spaces around operators
    fix following post-commit hook checkpatch issues:
    
    CHECK: spaces preferred around that '+' (ctx:VxV)
    87: FILE: drivers/staging/rtl8723bs/hal/rtl8723b_hal_init.c:746:
    +	if ((_offset+_size_byte) > EFUSE_MAX_MAP_LEN)
    --
    CHECK: spaces preferred around that '+' (ctx:VxV)
    105: FILE: drivers/staging/rtl8723bs/hal/rtl8723b_hal_init.c:847:
    +	if ((_offset+_size_byte) > EFUSE_BT_MAP_LEN)
    --
    CHECK: spaces preferred around that '-' (ctx:VxV)
    133: FILE: drivers/staging/rtl8723bs/hal/rtl8723b_hal_init.c:906:
    +		if ((eFuse_Addr-1) < total)
    --
    CHECK: spaces preferred around that '|' (ctx:VxV)
    392: FILE: drivers/staging/rtl8723bs/os_dep/ioctl_cfg80211.c:2485:
    +	if (check_fwstate(pmlmepriv, (_FW_LINKED|WIFI_AP_STATE)) != true)
    
    Signed-off-by: Fabio Aiuto <fabioaiuto83@gmail.com>
    Link: https://lore.kernel.org/r/65b7cf4b1ae3cea25a316f3c9f56c697ee7be8af.1618064274.git.fabioaiuto83@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/rtl8723bs/hal/rtl8723b_hal_init.c
+++ drivers/staging/rtl8723bs/hal/rtl8723b_hal_init.c
@@ -744,7 +744,7 @@ static void hal_ReadEFuse_WiFi(
 	/*  */
 	/*  Do NOT excess total size of EFuse table. Added by Roger, 2008.11.10. */
 	/*  */
-	if ((_offset+_size_byte) > EFUSE_MAX_MAP_LEN)
+	if ((_offset + _size_byte) > EFUSE_MAX_MAP_LEN)
 		return;
 
 	efuseTbl = rtw_malloc(EFUSE_MAX_MAP_LEN);
@@ -845,7 +845,7 @@ static void hal_ReadEFuse_BT(
 	/*  */
 	/*  Do NOT excess total size of EFuse table. Added by Roger, 2008.11.10. */
 	/*  */
-	if ((_offset+_size_byte) > EFUSE_BT_MAP_LEN)
+	if ((_offset + _size_byte) > EFUSE_BT_MAP_LEN)
 		return;
 
 	efuseTbl = rtw_malloc(EFUSE_BT_MAP_LEN);
@@ -904,7 +904,7 @@ static void hal_ReadEFuse_BT(
 			}
 		}
 
-		if ((eFuse_Addr-1) < total)
+		if ((eFuse_Addr - 1) < total)
 			break;
 
 	}
