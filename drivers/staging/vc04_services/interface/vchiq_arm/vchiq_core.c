d4c6a6bd5f3f92a4b424748ad9f29d82a90685b7
Stefan Wahren <stefan.wahren@i2se.com>
2021-04-11 20:11:33 +0200

    staging: vchiq_core: Get the rid off curly braces around cases
    Additional curly braces around cases are a bit harder to read. So
    change the scope of service quota to get the rid off those braces.
    
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>
    Link: https://lore.kernel.org/r/1618164700-21150-4-git-send-email-stefan.wahren@i2se.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/vc04_services/interface/vchiq_arm/vchiq_core.c
+++ drivers/staging/vc04_services/interface/vchiq_arm/vchiq_core.c
@@ -3358,6 +3358,7 @@ vchiq_set_service_option(unsigned int handle,
 {
 	struct vchiq_service *service = find_service_by_handle(handle);
 	enum vchiq_status status = VCHIQ_ERROR;
+	struct vchiq_service_quota *quota;
 
 	if (service) {
 		switch (option) {
@@ -3366,9 +3367,8 @@ vchiq_set_service_option(unsigned int handle,
 			status = VCHIQ_SUCCESS;
 			break;
 
-		case VCHIQ_SERVICE_OPTION_SLOT_QUOTA: {
-			struct vchiq_service_quota *quota =
-				&service->state->service_quotas[
+		case VCHIQ_SERVICE_OPTION_SLOT_QUOTA:
+			quota = &service->state->service_quotas[
 					service->localport];
 			if (value == 0)
 				value = service->state->default_slot_quota;
@@ -3386,11 +3386,10 @@ vchiq_set_service_option(unsigned int handle,
 				}
 				status = VCHIQ_SUCCESS;
 			}
-		} break;
+			break;
 
-		case VCHIQ_SERVICE_OPTION_MESSAGE_QUOTA: {
-			struct vchiq_service_quota *quota =
-				&service->state->service_quotas[
+		case VCHIQ_SERVICE_OPTION_MESSAGE_QUOTA:
+			quota = &service->state->service_quotas[
 					service->localport];
 			if (value == 0)
 				value = service->state->default_message_quota;
@@ -3408,7 +3407,7 @@ vchiq_set_service_option(unsigned int handle,
 					complete(&quota->quota_event);
 				status = VCHIQ_SUCCESS;
 			}
-		} break;
+			break;
 
 		case VCHIQ_SERVICE_OPTION_SYNCHRONOUS:
 			if ((service->srvstate == VCHIQ_SRVSTATE_HIDDEN) ||
