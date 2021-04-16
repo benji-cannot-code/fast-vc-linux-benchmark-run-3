a0e31a13f21d33cd4031158840b578d9fce8986d
Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
2021-04-16 14:23:11 +0200

    crypto: s5p-sss - consistently use local 'dev' variable in probe()
    For code readability, the probe() function uses 'dev' variable instead
    of '&pdev->dev', so update remaining places.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/crypto/s5p-sss.c
+++ drivers/crypto/s5p-sss.c
@@ -2187,14 +2187,14 @@ static int s5p_aes_probe(struct platform_device *pdev)
 	}
 
 	pdata->res = res;
-	pdata->ioaddr = devm_ioremap_resource(&pdev->dev, res);
+	pdata->ioaddr = devm_ioremap_resource(dev, res);
 	if (IS_ERR(pdata->ioaddr)) {
 		if (!pdata->use_hash)
 			return PTR_ERR(pdata->ioaddr);
 		/* try AES without HASH */
 		res->end -= 0x300;
 		pdata->use_hash = false;
-		pdata->ioaddr = devm_ioremap_resource(&pdev->dev, res);
+		pdata->ioaddr = devm_ioremap_resource(dev, res);
 		if (IS_ERR(pdata->ioaddr))
 			return PTR_ERR(pdata->ioaddr);
 	}
