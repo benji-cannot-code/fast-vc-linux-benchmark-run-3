a9ae9a66dadd8f8c8cff4a304a4f78121dc8f00a
Tony Lindgren <tony@atomide.com>
2021-04-17 11:38:39 +0300

    gpio: omap: Save and restore sysconfig
    As we are using cpu_pm to save and restore context, we must also save and
    restore the GPIO sysconfig register. This is needed because we are not
    calling PM runtime functions at all with cpu_pm.
    
    We need to save the sysconfig on idle as it's value can get reconfigured by
    PM runtime and can be different from the init time value. Device specific
    flags like "ti,no-idle-on-init" can affect the init value.
    
    Fixes: b764a5863fd8 ("gpio: omap: Remove custom PM calls and use cpu_pm instead")
    Cc: Aaro Koskinen <aaro.koskinen@iki.fi>
    Cc: Adam Ford <aford173@gmail.com>
    Cc: Andreas Kemnade <andreas@kemnade.info>
    Cc: Grygorii Strashko <grygorii.strashko@ti.com>
    Cc: Peter Ujfalusi <peter.ujfalusi@gmail.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Acked-by: Grygorii Strashko <grygorii.strashko@ti.com>
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

--- include/linux/platform_data/gpio-omap.h
+++ include/linux/platform_data/gpio-omap.h
@@ -86,6 +86,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * omap2+ specific GPIO registers
  */
 #define OMAP24XX_GPIO_REVISION		0x0000
+#define OMAP24XX_GPIO_SYSCONFIG		0x0010
 #define OMAP24XX_GPIO_IRQSTATUS1	0x0018
 #define OMAP24XX_GPIO_IRQSTATUS2	0x0028
 #define OMAP24XX_GPIO_IRQENABLE2	0x002c
@@ -109,6 +110,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define OMAP24XX_GPIO_SETDATAOUT	0x0094
 
 #define OMAP4_GPIO_REVISION		0x0000
+#define OMAP4_GPIO_SYSCONFIG		0x0010
 #define OMAP4_GPIO_EOI			0x0020
 #define OMAP4_GPIO_IRQSTATUSRAW0	0x0024
 #define OMAP4_GPIO_IRQSTATUSRAW1	0x0028
@@ -149,6 +151,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASSEMBLER__
 struct omap_gpio_reg_offs {
 	u16 revision;
+	u16 sysconfig;
 	u16 direction;
 	u16 datain;
 	u16 dataout;
