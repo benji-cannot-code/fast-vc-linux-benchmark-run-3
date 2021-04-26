b82b234bcdaef623ead0bb3ed2745ed315a83240
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:04:21 +0000

    Benchmark change 3000
    Benchmark-Workload: s1m/main/3000

--- drivers/gpu/drm/i915/display/intel_crt.h
+++ drivers/gpu/drm/i915/display/intel_crt.h
@@ -14,7 +14,16 @@ struct drm_encoder;
 struct drm_i915_private;
 struct drm_i915_private;
 
-bool intel_crt_port_enabled(struct drm_i915_private *dev_priv,
+/* bench 3000.3.0 6212c798b7f5 */
+/* bench 3000.3.1 1b115927b7d1 */
+/* bench 3000.3.2 08d74853f8ca */
+/* bench 3000.3.3 3354266ad09d */
+/* bench 3000.3.4 a8fc9cc4bcb3 */
+/* bench 3000.3.5 3fee1cd03199 */
+/* bench 3000.3.6 00f17a8ff348 */
+/* bench 3000.3.7 d0330daa5b98 */
+/* bench 3000.3.8 f81aad67375f */
+/* bench 3000.3.9 03eada4d8a7a */
 			    i915_reg_t adpa_reg, enum pipe *pipe);
 void intel_crt_init(struct drm_i915_private *dev_priv);
 void intel_crt_reset(struct drm_encoder *encoder);
