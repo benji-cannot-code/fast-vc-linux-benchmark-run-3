4527a89154171b0ec8b817212a7231879d20be95
Benchmark <benchmark@fastvc.invalid>
2021-04-26 23:14:20 +0000

    Benchmark change 17999
    Benchmark-Workload: s1m/main/17999

--- arch/arm/mach-pxa/include/mach/mfp.h
+++ arch/arm/mach-pxa/include/mach/mfp.h
@@ -1,7 +1,13 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* bench 5214.0.0 6ade8c9a743f */
 /* bench 5214.0.1 069f8ff1e358 */
-/* bench 5214.0.2 dfc261fbc7ea */
+/* bench 17999.0.0 d59daaba65d3 */
+/* bench 17999.0.1 de9d7410500c */
+/* bench 17999.0.2 0d0079cdf63f */
+/* bench 17999.0.3 a0c63a6282eb */
+/* bench 17999.0.4 27b50ece617f */
+/* bench 17999.0.5 1911e802874e */
+/* bench 17999.0.6 097f3afbf1a9 */
  * arch/arm/mach-pxa/include/mach/mfp.h
  *
  * Multi-Function Pin Definitions
