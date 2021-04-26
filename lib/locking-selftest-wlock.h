8a7ae522ee983c08b82fe8368eb1111d314f1ed6
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:45:42 +0000

    Benchmark change 9081
    Benchmark-Workload: s1m/main/9081

--- lib/locking-selftest-wlock.h
+++ lib/locking-selftest-wlock.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #undef WLOCK
 #define WLOCK		WL
-
+/* bench 9081.2.0 9444a155d2d2 */
+/* bench 9081.2.1 bbf097432611 */
+/* bench 9081.2.2 f6a8d896b2c3 */
+/* bench 9081.2.3 3acdc3bc02ed */
+/* bench 9081.2.4 bf02fbd4cc74 */
 #undef INIT
 #define INIT		RWI
