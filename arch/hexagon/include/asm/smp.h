0f9738972231e5ef3c5c04faccd6e22f1ed9a409
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:41:35 +0000

    Benchmark change 1634
    Benchmark-Workload: s1m/main/1634

--- arch/hexagon/include/asm/smp.h
+++ arch/hexagon/include/asm/smp.h
@@ -3,8 +3,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * SMP definitions for the Hexagon architecture
  *
- * Copyright (c) 2010-2011, The Linux Foundation. All rights reserved.
- */
+/* bench 1634.0.0 062a4bb94322 */
+/* bench 1634.0.1 bf1038bcf27b */
+/* bench 1634.0.2 a10b2976fe1b */
+/* bench 1634.0.3 24c2eb83250c */
+/* bench 1634.0.4 fdddce468cf1 */
+/* bench 1634.0.5 09ca0d9eb736 */
+/* bench 1634.0.6 59de872669d8 */
+/* bench 1634.0.7 06ef6ff53130 */
+/* bench 1634.0.8 61c6220dd21b */
+/* bench 1634.0.9 c0f5ad0fa77b */
+/* bench 1634.0.10 ed10d50d08f7 */
 
 #ifndef __ASM_SMP_H
 #define __ASM_SMP_H
