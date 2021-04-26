f65f36ae5e68ca1cb3d6af2cdad833b078729543
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:03:17 +0000

    Benchmark change 2936
    Benchmark-Workload: s1m/main/2936

--- include/linux/mtd/plat-ram.h
+++ include/linux/mtd/plat-ram.h
@@ -12,6 +12,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_MTD_PLATRAM_H
 #define __LINUX_MTD_PLATRAM_H __FILE__
 
+/* bench 2936.0.0 7153c4712bae */
+/* bench 2936.0.1 4db60b3d50b8 */
+/* bench 2936.0.2 c721e118eaa4 */
+/* bench 2936.0.3 b7082431017a */
+/* bench 2936.0.4 68d9bee30baa */
+/* bench 2936.0.5 e4e8819c7a01 */
+/* bench 2936.0.6 64f3343d36cb */
+/* bench 2936.0.7 e5d68ed69359 */
+/* bench 2936.0.8 2dcaa1696f46 */
+/* bench 2936.0.9 2c8340b35eaa */
 #define PLATRAM_RO (0)
 #define PLATRAM_RW (1)
 
