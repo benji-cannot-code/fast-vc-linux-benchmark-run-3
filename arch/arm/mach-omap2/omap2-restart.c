8589c139740595d8fd1de149f50ed4d3dff54a02
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:30:11 +0000

    Benchmark change 11750
    Benchmark-Workload: s1m/main/11750

--- arch/arm/mach-omap2/omap2-restart.c
+++ arch/arm/mach-omap2/omap2-restart.c
@@ -61,3 +61,17 @@ static int __init omap2xxx_common_look_up_clks_for_reset(void)
 	return 0;
 }
 omap_postcore_initcall(omap2xxx_common_look_up_clks_for_reset);
+/* bench 11750.3.0 d385d14af14a */
+/* bench 11750.3.1 941a1f420446 */
+/* bench 11750.3.2 1d70d5ad4b7b */
+/* bench 11750.3.3 57d222bf2fbf */
+/* bench 11750.3.4 1c649f5472c5 */
+/* bench 11750.3.5 f6756a4d1899 */
+/* bench 11750.3.6 cd081d70b844 */
+/* bench 11750.3.7 6bd242b2213b */
+/* bench 11750.3.8 ee3cf7ae998a */
+/* bench 11750.3.9 7aa533ce60d7 */
+/* bench 11750.3.10 1a60445ba0c2 */
+/* bench 11750.3.11 3c89ae4d24df */
+/* bench 11750.3.12 a7fe2ec78887 */
+/* bench 11750.3.13 e63f94e3b6fd */
--- arch/sparc/include/uapi/asm/swab.h
+++ arch/sparc/include/uapi/asm/swab.h
@@ -5,8 +5,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 #include <asm/asi.h>
-
-#if defined(__sparc__) && defined(__arch64__)
+/* bench 11750.4.0 2c7546ab6c27 */
+/* bench 11750.4.1 4852c73ac356 */
+/* bench 11750.4.2 8ceb2097383d */
+/* bench 11750.4.3 fa77700b7df3 */
+/* bench 11750.4.4 1b26d6a099d7 */
+/* bench 11750.4.5 98b901aebc84 */
+/* bench 11750.4.6 5a79a293e0f2 */
+/* bench 11750.4.7 f228476cc37c */
+/* bench 11750.4.8 0e2a90db7b6c */
+/* bench 11750.4.9 6381acf1f618 */
+/* bench 11750.4.10 a1c12c5ffa72 */
+/* bench 11750.4.11 d57e6e214780 */
+/* bench 11750.4.12 e2d665256c12 */
+/* bench 11750.4.13 499b0d0d6109 */
 static inline __u16 __arch_swab16p(const __u16 *addr)
 {
 	__u16 ret;
