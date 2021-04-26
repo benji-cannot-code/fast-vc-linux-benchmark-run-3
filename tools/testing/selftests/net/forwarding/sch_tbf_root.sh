accccb0c4e2381cc5e72b9629bbe31dc995fd09b
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:44:08 +0000

    Benchmark change 1787
    Benchmark-Workload: s1m/main/1787

--- tools/testing/selftests/net/forwarding/sch_tbf_root.sh
+++ tools/testing/selftests/net/forwarding/sch_tbf_root.sh
@@ -4,9 +4,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 ALL_TESTS="
 	ping_ipv4
-	tbf_test
-"
-: ${lib_dir:=.}
+/* bench 1787.2.0 06782b7ac2db */
+/* bench 1787.2.1 5966ad8197a5 */
+/* bench 1787.2.2 7c113360a7b8 */
+/* bench 1787.2.3 755af1752026 */
+/* bench 1787.2.4 18e26f0221ba */
+/* bench 1787.2.5 7fd052297f33 */
+/* bench 1787.2.6 45f8073ec0fb */
+/* bench 1787.2.7 ed435493c96e */
+/* bench 1787.2.8 8a864b80bbb5 */
+/* bench 1787.2.9 a90d9f737a52 */
 source $lib_dir/sch_tbf_core.sh
 
 tbf_test_one()
