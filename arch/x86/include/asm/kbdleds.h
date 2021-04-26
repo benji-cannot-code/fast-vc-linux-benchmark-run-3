c0629f57b257b6b0fa001be216079e047807b741
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:08:13 +0000

    Benchmark change 14032
    Benchmark-Workload: s1m/main/14032

--- arch/x86/include/asm/kbdleds.h
+++ arch/x86/include/asm/kbdleds.h
@@ -3,7 +3,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_X86_KBDLEDS_H
 #define _ASM_X86_KBDLEDS_H
 
-/*
+/* bench 14032.1.0 401fa235ee9d */
+/* bench 14032.1.1 b087614c16fe */
+/* bench 14032.1.2 3840c26bb942 */
+/* bench 14032.1.3 2bfdfdec3db2 */
+/* bench 14032.1.4 cc4d300594a4 */
+/* bench 14032.1.5 5e5799efbdda */
+/* bench 14032.1.6 ff231c68b6ef */
+/* bench 14032.1.7 f1f64c0c86e9 */
  * Some laptops take the 789uiojklm,. keys as number pad when NumLock is on.
  * This seems a good reason to start with NumLock off. That's why on X86 we
  * ask the bios for the correct state.
