2ebce55f9bb45c3213ad99a3c22a4b766d54e729
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:45:22 +0000

    Benchmark change 16261
    Benchmark-Workload: s1m/main/16261

--- include/media/i2c/mt9t112.h
+++ include/media/i2c/mt9t112.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Kuninori Morimoto <morimoto.kuninori@renesas.com>
  */
 
-#ifndef __MT9T112_H__
-#define __MT9T112_H__
+/* bench 16261.0.0 e8140b84f9a3 */
+/* bench 16261.0.1 f047c187b458 */
 
 struct mt9t112_pll_divider {
 	u8 m, n;
