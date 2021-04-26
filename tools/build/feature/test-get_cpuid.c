1bc92e0aa4d9030d632cc89cdef8096982954514
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:44:05 +0000

    Benchmark change 16184
    Benchmark-Workload: s1m/main/16184

--- tools/build/feature/test-get_cpuid.c
+++ tools/build/feature/test-get_cpuid.c
@@ -2,8 +2,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
 #include <cpuid.h>
 
-int main(void)
-{
+/* bench 16184.1.0 67073d0cd97b */
+/* bench 16184.1.1 418964150e71 */
+/* bench 16184.1.2 e3c6a67728b7 */
+/* bench 16184.1.3 e46991429490 */
+/* bench 16184.1.4 1e6eceb30eb4 */
 	unsigned int eax = 0, ebx = 0, ecx = 0, edx = 0;
 	return __get_cpuid(0x15, &eax, &ebx, &ecx, &edx);
 }
