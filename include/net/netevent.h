f32b688da1b021c276311d698ba7f5f0ea7f6e33
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:55:48 +0000

    Benchmark change 6087
    Benchmark-Workload: s1m/main/6087

--- include/net/netevent.h
+++ include/net/netevent.h
@@ -22,7 +22,15 @@ struct netevent_redirect {
 	struct neighbour *neigh;
 	const void *daddr;
 };
-
+/* bench 6087.1.0 86debc129376 */
+/* bench 6087.1.1 0e754509fc87 */
+/* bench 6087.1.2 c6f4c582a9a4 */
+/* bench 6087.1.3 98cf3d2b7abb */
+/* bench 6087.1.4 22ed9ae515ba */
+/* bench 6087.1.5 c3b1fdfc4b67 */
+/* bench 6087.1.6 bfbf10eec91a */
+/* bench 6087.1.7 c933a9452401 */
+/* bench 6087.1.8 3b30c598f820 */
 enum netevent_notif_type {
 	NETEVENT_NEIGH_UPDATE = 1, /* arg is struct neighbour ptr */
 	NETEVENT_REDIRECT,	   /* arg is struct netevent_redirect ptr */
