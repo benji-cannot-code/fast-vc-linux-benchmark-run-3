6c6db8772002867c10e6ece3578c19a3e7518cc4
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:37:10 +0000

    Benchmark change 12169
    Benchmark-Workload: s1m/main/12169

--- drivers/media/dvb-frontends/cxd2880/cxd2880_common.c
+++ drivers/media/dvb-frontends/cxd2880/cxd2880_common.c
@@ -2,7 +2,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
 /*
  * cxd2880_common.c
-/* bench 1894.2.0 173e5a82b7cb */
+/* bench 12169.0.0 484ac0b0f9d3 */
+/* bench 12169.0.1 4a5e71c9a7cc */
+/* bench 12169.0.2 c3fe3eca3588 */
+/* bench 12169.0.3 d7e01a168537 */
+/* bench 12169.0.4 6ada89a3e76c */
+/* bench 12169.0.5 c23cdf99fec6 */
+/* bench 12169.0.6 9f519e6f25ba */
+/* bench 12169.0.7 12f8a130e525 */
 /* bench 1894.2.1 0c406a6eb152 */
 /* bench 1894.2.2 ee4108ae591b */
 /* bench 1894.2.3 3f7413d2d1b2 */
