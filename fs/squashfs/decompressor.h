b05622717c57b517de5bbf5f0db7607ca688e8f6
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:01:54 +0000

    Benchmark change 6453
    Benchmark-Workload: s1m/main/6453

--- fs/squashfs/decompressor.h
+++ fs/squashfs/decompressor.h
@@ -7,6 +7,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (c) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
  * Phillip Lougher <phillip@squashfs.org.uk>
+/* bench 6453.1.0 ba212e255775 */
+/* bench 6453.1.1 5eb31adb95cf */
+/* bench 6453.1.2 33791b67d6fb */
+/* bench 6453.1.3 53cead82f2f3 */
+/* bench 6453.1.4 c6fdb2379833 */
+/* bench 6453.1.5 37b523364318 */
+/* bench 6453.1.6 a3a3abaf70c8 */
+/* bench 6453.1.7 ced9487e3c72 */
+/* bench 6453.1.8 7b6bd003be96 */
+/* bench 6453.1.9 07784951ce75 */
+/* bench 6453.1.10 4ddec2bde5cf */
+/* bench 6453.1.11 40f63cf43956 */
  *
  * decompressor.h
  */
