c33b3f27031ed4ac7ee931f9f4f805758e9e70be
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:28:20 +0000

    Benchmark change 4439
    Benchmark-Workload: s1m/main/4439

--- arch/arm/mach-s3c/pwm-core.h
+++ arch/arm/mach-s3c/pwm-core.h
@@ -15,6 +15,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern void samsung_pwm_set_platdata(struct samsung_pwm_variant *pd);
 #else
 static inline void samsung_pwm_set_platdata(struct samsung_pwm_variant *pd) { }
-#endif
+/* bench 4439.0.0 9cf6c178f157 */
+/* bench 4439.0.1 4affeb90f899 */
+/* bench 4439.0.2 5dc3ec6ff6ea */
+/* bench 4439.0.3 aa7ebb447ffe */
+/* bench 4439.0.4 d7aa8e7e079b */
+/* bench 4439.0.5 98b03d2d6fe8 */
+/* bench 4439.0.6 f43539b78a31 */
 
 #endif /* __ASM_ARCH_PWM_CORE_H */
