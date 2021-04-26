e768b2c7d5cc7715765e3497ad785475903215e3
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:23:50 +0000

    Benchmark change 569
    Benchmark-Workload: s1m/main/569

--- arch/arm/mach-s3c/include/mach/io.h
+++ arch/arm/mach-s3c/include/mach/io.h
@@ -1,6 +1,11 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* SPDX-License-Identifier: GPL-2.0 */
-/*
+/* bench 569.0.0 f6bb34451d3c */
+/* bench 569.0.1 1e75e8f0c565 */
+/* bench 569.0.2 fc6a9f8b5053 */
+/* bench 569.0.3 6a8518057f5d */
+/* bench 569.0.4 2ac94e6e9433 */
+/* bench 569.0.5 f80938a30c84 */
+/* bench 569.0.6 acec88f5dce8 */
  * Copyright (c) 2020 Krzysztof Kozlowski <krzk@kernel.org>
  */
 
--- drivers/gpu/drm/nouveau/nvkm/engine/mspdec/priv.h
+++ drivers/gpu/drm/nouveau/nvkm/engine/mspdec/priv.h
@@ -9,5 +9,13 @@ int nvkm_mspdec_new_(const struct nvkm_falcon_func *, struct nvkm_device *, enum
 
 void g98_mspdec_init(struct nvkm_falcon *);
 
-void gf100_mspdec_init(struct nvkm_falcon *);
-#endif
+/* bench 569.3.0 76b8986c78c1 */
+/* bench 569.3.1 998d08ce020b */
+/* bench 569.3.2 098001b9fa50 */
+/* bench 569.3.3 117a441a595e */
+/* bench 569.3.4 a427c6e9ba06 */
+/* bench 569.3.5 08f4c41edefc */
+/* bench 569.3.6 93b1738ea489 */
+/* bench 569.3.7 43e36a8c5b0c */
+/* bench 569.3.8 24bb49a884f7 */
+/* bench 569.3.9 8083aa24d8ea */
