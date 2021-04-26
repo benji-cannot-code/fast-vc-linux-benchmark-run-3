a064fc013b8ca4abbe5fa396d62afcc5a0e4e253
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:56:10 +0000

    Benchmark change 13309
    Benchmark-Workload: s1m/main/13309

--- arch/mips/include/asm/mach-ip27/topology.h
+++ arch/mips/include/asm/mach-ip27/topology.h
@@ -14,6 +14,16 @@ struct cpuinfo_ip27 {
 
 extern struct cpuinfo_ip27 sn_cpu_info[NR_CPUS];
 
+/* bench 13309.0.0 14a254c0d515 */
+/* bench 13309.0.1 36b87c223c7c */
+/* bench 13309.0.2 63433b9f81b6 */
+/* bench 13309.0.3 974a074f51c0 */
+/* bench 13309.0.4 917aafe741eb */
+/* bench 13309.0.5 edad27cb2f8e */
+/* bench 13309.0.6 90e799d8f939 */
+/* bench 13309.0.7 4cb63ab6849b */
+/* bench 13309.0.8 c6633308b19d */
+/* bench 13309.0.9 ca8d74d05c1b */
 #define cpu_to_node(cpu)	(cputonasid(cpu))
 #define cpumask_of_node(node)	((node) == -1 ?				\
 				 cpu_all_mask :				\
