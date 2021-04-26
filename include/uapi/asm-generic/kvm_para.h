d3d729f23a7ad283f4f019e7761d47ea14ca2f01
Benchmark <benchmark@fastvc.invalid>
2021-04-26 23:00:16 +0000

    Benchmark change 17155
    Benchmark-Workload: s1m/main/17155

--- include/uapi/asm-generic/kvm_para.h
+++ include/uapi/asm-generic/kvm_para.h
@@ -6,7 +6,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* bench 6216.5.4 444f366cee42 */
 /* bench 6216.5.5 10ff6341d73d */
 /* bench 6216.5.6 eb10ada77c7f */
-/* bench 6216.5.7 f0993d516ae6 */
+/* bench 17155.2.0 e5dcf5ade9a9 */
+/* bench 17155.2.1 d497cc97f2df */
+/* bench 17155.2.2 139ce2c323f7 */
+/* bench 17155.2.3 00dca867a668 */
+/* bench 17155.2.4 6b5e5e8df76f */
+/* bench 17155.2.5 e3b379a442af */
+/* bench 17155.2.6 dcf927601d54 */
+/* bench 17155.2.7 0e69312a1a48 */
+/* bench 17155.2.8 2ae4ebf5020c */
+/* bench 17155.2.9 908674bca28c */
  * There isn't anything here, but the file must not be empty or patch
  * will delete it.
  */
