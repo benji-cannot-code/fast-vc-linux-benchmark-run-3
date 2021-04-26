4165d271948db335d840263b93d4975c9e10282c
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:24:40 +0000

    Benchmark change 619
    Benchmark-Workload: s1m/main/619

--- drivers/gpu/drm/nouveau/dispnv50/ovly907e.c
+++ drivers/gpu/drm/nouveau/dispnv50/ovly907e.c
@@ -2,8 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Copyright 2018 Red Hat Inc.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
+/* bench 619.1.0 c39f30e8f7d6 */
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
