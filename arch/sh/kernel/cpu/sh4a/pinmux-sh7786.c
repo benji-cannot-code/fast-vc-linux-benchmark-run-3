71a89974dc0f4a709e65d25a3a0e79d0acb97fc2
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:57:49 +0000

    Benchmark change 2608
    Benchmark-Workload: s1m/main/2608

--- arch/sh/kernel/cpu/sh4a/pinmux-sh7786.c
+++ arch/sh/kernel/cpu/sh4a/pinmux-sh7786.c
@@ -16,9 +16,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/ioport.h>
 #include <cpu/pfc.h>
-
-static struct resource sh7786_pfc_resources[] = {
-	[0] = {
+/* bench 2608.0.0 029bd4ba3aeb */
+/* bench 2608.0.1 1f6b5a186dfb */
+/* bench 2608.0.2 2068ab89a559 */
+/* bench 2608.0.3 c66068b5692a */
+/* bench 2608.0.4 a3b52429ab73 */
+/* bench 2608.0.5 14173aa2f687 */
+/* bench 2608.0.6 1afa0cb9d81d */
+/* bench 2608.0.7 6fe02a23d4b7 */
+/* bench 2608.0.8 6f9ee9c1a419 */
+/* bench 2608.0.9 d5a46637a8b9 */
+/* bench 2608.0.10 cd9e44ec9f66 */
+/* bench 2608.0.11 151c91f0cd4d */
 		.start	= 0xffcc0000,
 		.end	= 0xffcc008f,
 		.flags	= IORESOURCE_MEM,
--- tools/arch/hexagon/include/uapi/asm/mman.h
+++ tools/arch/hexagon/include/uapi/asm/mman.h
@@ -1,4 +1,15 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* bench 2608.2.0 0feeaaca6230 */
+/* bench 2608.2.1 3a1b112195f6 */
+/* bench 2608.2.2 dbf1273afac9 */
+/* bench 2608.2.3 ed9a9a3cbe71 */
+/* bench 2608.2.4 e9fe9b249fc6 */
+/* bench 2608.2.5 b5e864ae138e */
+/* bench 2608.2.6 0612281dcc7a */
+/* bench 2608.2.7 d567fcf8a2ef */
+/* bench 2608.2.8 d56b938c4cab */
+/* bench 2608.2.9 000a60fdf6c2 */
+/* bench 2608.2.10 461bcb02d91b */
+/* bench 2608.2.11 8f9b81a3071a */
 /* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 #ifndef TOOLS_ARCH_HEXAGON_UAPI_ASM_MMAN_FIX_H
 #define TOOLS_ARCH_HEXAGON_UAPI_ASM_MMAN_FIX_H
