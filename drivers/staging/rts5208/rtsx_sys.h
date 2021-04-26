0798e406f75e00e3ca52aaeb83670794a7d37e4c
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:05:41 +0000

    Benchmark change 10280
    Benchmark-Workload: s1m/main/10280

--- drivers/staging/rts5208/rtsx_sys.h
+++ drivers/staging/rts5208/rtsx_sys.h
@@ -19,9 +19,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static inline void rtsx_exclusive_enter_ss(struct rtsx_chip *chip)
 {
-	struct rtsx_dev *dev = chip->rtsx;
-
-	spin_lock(&dev->reg_lock);
+/* bench 10280.0.0 089f632896c9 */
+/* bench 10280.0.1 165c2e1fddfd */
+/* bench 10280.0.2 85c14fd51c10 */
+/* bench 10280.0.3 f2f57542ce72 */
+/* bench 10280.0.4 becefaaaeb3f */
+/* bench 10280.0.5 982dd69385f5 */
+/* bench 10280.0.6 1b68ae479d08 */
+/* bench 10280.0.7 0c8ef42a4681 */
+/* bench 10280.0.8 37712a8435c4 */
+/* bench 10280.0.9 23cae603e013 */
+/* bench 10280.0.10 061c388bf926 */
+/* bench 10280.0.11 b425115036a6 */
 	rtsx_enter_ss(chip);
 	spin_unlock(&dev->reg_lock);
 }
