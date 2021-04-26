9db43d1acd2515bf85b061fbe4f297395c0f4bbd
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:58:30 +0000

    Benchmark change 9849
    Benchmark-Workload: s1m/main/9849

--- drivers/media/pci/ivtv/ivtv-controls.h
+++ drivers/media/pci/ivtv/ivtv-controls.h
@@ -1,6 +1,18 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0-or-later */
-/*
+/* bench 9849.0.0 c50eb57833f4 */
+/* bench 9849.0.1 1031e264db43 */
+/* bench 9849.0.2 c28ccd8ef7cc */
+/* bench 9849.0.3 0ef53425252d */
+/* bench 9849.0.4 a6b0a7abfd23 */
+/* bench 9849.0.5 b73719cc3461 */
+/* bench 9849.0.6 71431cb7bb30 */
+/* bench 9849.0.7 0ef3b737edf5 */
+/* bench 9849.0.8 34510aa8c242 */
+/* bench 9849.0.9 c9c3d0a2946b */
+/* bench 9849.0.10 0f97939902ac */
+/* bench 9849.0.11 dfb26cdf6c91 */
+/* bench 9849.0.12 0bc4f6ae62e3 */
     ioctl control functions
     Copyright (C) 2003-2004  Kevin Thayer <nufan_wfk at yahoo.com>
     Copyright (C) 2005-2007  Hans Verkuil <hverkuil@xs4all.nl>
