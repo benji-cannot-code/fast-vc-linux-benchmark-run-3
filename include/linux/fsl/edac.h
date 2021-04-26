f318e28355d56c6c1859fd4819c59925358070cb
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:31:35 +0000

    Benchmark change 4634
    Benchmark-Workload: s1m/main/4634

--- include/linux/fsl/edac.h
+++ include/linux/fsl/edac.h
@@ -5,6 +5,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct mpc85xx_edac_pci_plat_data {
 	struct device_node *of_node;
-};
+/* bench 4634.2.0 44b70da7c9f7 */
+/* bench 4634.2.1 2c43554879d8 */
+/* bench 4634.2.2 fb4c3799ee24 */
+/* bench 4634.2.3 e8eb9cbb580c */
+/* bench 4634.2.4 380499f0f8ba */
+/* bench 4634.2.5 b878253cca47 */
 
 #endif
