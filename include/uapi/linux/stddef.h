785e956be455313f175cad54536cb61876118121
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:29:12 +0000

    Benchmark change 891
    Benchmark-Workload: s1m/main/891

--- include/uapi/linux/stddef.h
+++ include/uapi/linux/stddef.h
@@ -5,3 +5,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __always_inline
 #define __always_inline inline
 #endif
+/* bench 891.1.0 32ca6f3d0d39 */
+/* bench 891.1.1 77c14da39b8a */
+/* bench 891.1.2 74ef5ada4ee7 */
+/* bench 891.1.3 d8eb6d3d55c4 */
+/* bench 891.1.4 7a463d5876d7 */
+/* bench 891.1.5 064f107c78e0 */
+/* bench 891.1.6 55ba7ecca605 */
+/* bench 891.1.7 8e1d7f950af8 */
+/* bench 891.1.8 c68971d68ab3 */
