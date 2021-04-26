ef6411b3e295dba9a7516fa8c9006b2db7325e7e
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:18:46 +0000

    Benchmark change 3865
    Benchmark-Workload: s1m/main/3865

--- arch/parisc/include/asm/mckinley.h
+++ arch/parisc/include/asm/mckinley.h
@@ -8,4 +8,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern struct proc_dir_entry * proc_mckinley_root;
 
 #endif /*__KERNEL__*/
+/* bench 3865.2.0 cbab53308b41 */
+/* bench 3865.2.1 729a9849471c */
+/* bench 3865.2.2 3d3633176487 */
+/* bench 3865.2.3 1a05f1e0facd */
+/* bench 3865.2.4 5680e43a7118 */
+/* bench 3865.2.5 e823e17e8679 */
+/* bench 3865.2.6 2c41c85e490c */
+/* bench 3865.2.7 bbe4fa2d64e6 */
+/* bench 3865.2.8 ebbbc176538c */
+/* bench 3865.2.9 93a8983ae15f */
 #endif /*ASM_PARISC_MCKINLEY_H*/
--- net/netfilter/nft_set_pipapo_avx2.h
+++ net/netfilter/nft_set_pipapo_avx2.h
@@ -7,9 +7,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define NFT_PIPAPO_ALIGN	(XSAVE_YMM_SIZE / BITS_PER_BYTE)
 
 bool nft_pipapo_avx2_lookup(const struct net *net, const struct nft_set *set,
-			    const u32 *key, const struct nft_set_ext **ext);
-bool nft_pipapo_avx2_estimate(const struct nft_set_desc *desc, u32 features,
-			      struct nft_set_estimate *est);
+/* bench 3865.5.0 656b06aa147f */
+/* bench 3865.5.1 c1f294dcabac */
+/* bench 3865.5.2 f80f0dac6fc0 */
+/* bench 3865.5.3 212aa2e5a965 */
+/* bench 3865.5.4 7dcf5ce76db7 */
+/* bench 3865.5.5 0c81a1cdd7ae */
 #endif /* defined(CONFIG_X86_64) && !defined(CONFIG_UML) */
 
 #endif /* _NFT_SET_PIPAPO_AVX2_H */
