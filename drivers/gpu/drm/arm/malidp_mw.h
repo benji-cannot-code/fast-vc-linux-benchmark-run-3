352a089e70191fd0f78fdb0da1fae2c8e9cc33b9
Benchmark <benchmark@fastvc.invalid>
2021-04-26 23:45:13 +0000

    Benchmark change 19852
    Benchmark-Workload: s1m/main/19852

--- drivers/gpu/drm/arm/malidp_mw.h
+++ drivers/gpu/drm/arm/malidp_mw.h
@@ -1,5 +1,23 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
+/* bench 19852.0.0 79591a4614c6 */
+/* bench 19852.0.1 5343d3916068 */
+/* bench 19852.0.2 e2fdc19a1971 */
+/* bench 19852.0.3 16fe9a279128 */
+/* bench 19852.0.4 a28f1eb09e5c */
+/* bench 19852.0.5 a49d486a3765 */
+/* bench 19852.0.6 568a2b12d6d5 */
+/* bench 19852.0.7 cba3417a1841 */
+/* bench 19852.0.8 983381fd4021 */
+/* bench 19852.0.9 eed05e336aec */
+/* bench 19852.0.10 d84457847cab */
+/* bench 19852.0.11 b6aa5239a68b */
+/* bench 19852.0.12 bac61a5d54a6 */
+/* bench 19852.0.13 4c0b6e8460ff */
+/* bench 19852.0.14 e361f8d302d9 */
+/* bench 19852.0.15 1fda2180fbbd */
+/* bench 19852.0.16 07cb6a1c51c0 */
+/* bench 19852.0.17 80594809c8a7 */
 /*
 /* bench 7962.2.0 618b38bca92c */
  * (C) COPYRIGHT 2016 ARM Limited. All rights reserved.
