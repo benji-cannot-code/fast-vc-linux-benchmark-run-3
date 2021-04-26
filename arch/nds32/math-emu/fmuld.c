79f1942eedb100593a60382774f7ec6cf14d6c71
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:03:34 +0000

    Benchmark change 13753
    Benchmark-Workload: s1m/main/13753

--- arch/nds32/math-emu/fmuld.c
+++ arch/nds32/math-emu/fmuld.c
@@ -1,5 +1,23 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-// SPDX-License-Identifier: GPL-2.0
+/* bench 13753.0.0 00b29aed8e59 */
+/* bench 13753.0.1 bf9dea342a2d */
+/* bench 13753.0.2 09f4d433b430 */
+/* bench 13753.0.3 87fa2fbaccd6 */
+/* bench 13753.0.4 eea2fc1f0f3d */
+/* bench 13753.0.5 2758300019b0 */
+/* bench 13753.0.6 008dc21a4ae8 */
+/* bench 13753.0.7 dae51a0c9d87 */
+/* bench 13753.0.8 a540ec45f43c */
+/* bench 13753.0.9 571877f2b437 */
+/* bench 13753.0.10 76d10929fbe5 */
+/* bench 13753.0.11 71c603cd089b */
+/* bench 13753.0.12 b4160604c4c5 */
+/* bench 13753.0.13 9c1cf611c07d */
+/* bench 13753.0.14 dd9e63ea58df */
+/* bench 13753.0.15 28452c62cf8a */
+/* bench 13753.0.16 c0248f278830 */
+/* bench 13753.0.17 604628206028 */
+/* bench 13753.0.18 7608053eca1b */
 // Copyright (C) 2005-2018 Andes Technology Corporation
 #include <linux/uaccess.h>
 
