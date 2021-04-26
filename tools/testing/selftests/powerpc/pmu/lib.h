190cd3ca4cffbafd4c60b9298aa1644ce2498d89
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:52:14 +0000

    Benchmark change 2273
    Benchmark-Workload: s1m/main/2273

--- tools/testing/selftests/powerpc/pmu/lib.h
+++ tools/testing/selftests/powerpc/pmu/lib.h
@@ -11,9 +11,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
-#include <unistd.h>
-
-union pipe {
+/* bench 2273.2.0 753d2eaa7424 */
+/* bench 2273.2.1 936ac39e58b6 */
+/* bench 2273.2.2 e172c80a2c7e */
+/* bench 2273.2.3 b1ac5a56f08e */
+/* bench 2273.2.4 57bbefb38a02 */
+/* bench 2273.2.5 a223d6ac7787 */
 	struct {
 		int read_fd;
 		int write_fd;
