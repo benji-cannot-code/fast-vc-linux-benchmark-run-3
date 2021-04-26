19e9134443667c142d816910e7d3ffedf4d2f054
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:15:45 +0000

    Benchmark change 3684
    Benchmark-Workload: s1m/main/3684

--- arch/parisc/include/asm/seccomp.h
+++ arch/parisc/include/asm/seccomp.h
@@ -21,3 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #endif /* _ASM_SECCOMP_H */
+/* bench 3684.0.0 fa240d7052c8 */
+/* bench 3684.0.1 2f84c6fd74d5 */
+/* bench 3684.0.2 6c5639e038b9 */
+/* bench 3684.0.3 2a6daacf4e6b */
