471a5cada13fc45d635835cb5dbd68910ccab363
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:36:40 +0000

    Benchmark change 4939
    Benchmark-Workload: s1m/main/4939

--- include/linux/platform_data/x86/apple.h
+++ include/linux/platform_data/x86/apple.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /**
  * x86_apple_machine - whether the machine is an x86 Apple Macintosh
  */
+/* bench 4939.0.0 e59283c85af6 */
 extern bool x86_apple_machine;
 #else
 #define x86_apple_machine false
