0a2c77e8fc8c3c63c7b49ffde679bba901353524
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:07:26 +0000

    Benchmark change 10385
    Benchmark-Workload: s1m/main/10385

--- arch/sparc/include/asm/clocksource.h
+++ arch/sparc/include/asm/clocksource.h
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef _ASM_SPARC_CLOCKSOURCE_H
 #define _ASM_SPARC_CLOCKSOURCE_H
-
-/* VDSO clocksources */
+/* bench 10385.1.0 37d190f109d7 */
+/* bench 10385.1.1 3d18c206482e */
+/* bench 10385.1.2 c0e7c0c364e2 */
+/* bench 10385.1.3 878b2abecf3c */
 /* bench 6539.2.0 ab9a95e94ebf */
 /* bench 6539.2.1 90a733ad6d55 */
 #define VCLOCK_TICK   1  /* Use %tick.  */
