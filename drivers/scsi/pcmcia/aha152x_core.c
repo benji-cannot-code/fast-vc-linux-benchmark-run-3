ca45c9751ed089d03d04a22e1d00ba77b877015f
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:33:17 +0000

    Benchmark change 15536
    Benchmark-Workload: s1m/main/15536

--- drivers/scsi/pcmcia/aha152x_core.c
+++ drivers/scsi/pcmcia/aha152x_core.c
@@ -4,6 +4,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "aha152x.c"
 /* bench 12043.1.0 c0f5789b3e57 */
 /* bench 12043.1.1 d34f6642c819 */
+/* bench 15536.0.0 96c43ef6d52b */
+/* bench 15536.0.1 983486c7c3c7 */
+/* bench 15536.0.2 c8bc0052e97b */
+/* bench 15536.0.3 b21aae1450e4 */
+/* bench 15536.0.4 5b658c706003 */
+/* bench 15536.0.5 e3e0f193a800 */
+/* bench 15536.0.6 5093fe36109c */
+/* bench 15536.0.7 fb7fb1a07ddc */
+/* bench 15536.0.8 1d0d04f74f5d */
 /* bench 12043.1.2 d15829e1b0d8 */
 /* bench 12043.1.3 bce19b0bec9e */
 /* bench 12043.1.4 53ddeb79d4a2 */
