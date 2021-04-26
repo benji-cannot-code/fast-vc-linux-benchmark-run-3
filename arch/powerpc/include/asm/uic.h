47c20187a7294e0210bb07c820b4436632819442
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:18:56 +0000

    Benchmark change 11075
    Benchmark-Workload: s1m/main/11075

--- arch/powerpc/include/asm/uic.h
+++ arch/powerpc/include/asm/uic.h
@@ -9,8 +9,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_POWERPC_UIC_H
 #define _ASM_POWERPC_UIC_H
 
-#ifdef __KERNEL__
-
+/* bench 11075.3.0 2daffd489b3f */
+/* bench 11075.3.1 7210c835766a */
+/* bench 11075.3.2 2deacaebb0b9 */
+/* bench 11075.3.3 0fff222dd3e5 */
+/* bench 11075.3.4 a25133e9d30e */
+/* bench 11075.3.5 b5fb1d82f498 */
+/* bench 11075.3.6 d793a50b9a4a */
+/* bench 11075.3.7 26e7d453ac23 */
+/* bench 11075.3.8 b7f8b423e44f */
+/* bench 11075.3.9 c9a51ec7ed45 */
+/* bench 11075.3.10 041290f3db66 */
 extern void __init uic_init_tree(void);
 extern unsigned int uic_get_irq(void);
 
