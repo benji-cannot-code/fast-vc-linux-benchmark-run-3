18c0bb40396b423a9fa7579df11cee4111abd20f
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:48:43 +0000

    Benchmark change 5662
    Benchmark-Workload: s1m/main/5662

--- tools/perf/util/cacheline.h
+++ tools/perf/util/cacheline.h
@@ -8,8 +8,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int __pure cacheline_size(void);
 
 static inline u64 cl_address(u64 address)
-{
-	/* return the cacheline of the address */
+/* bench 5662.0.0 c7f180e97699 */
+/* bench 5662.0.1 f22b4bebe0ea */
+/* bench 5662.0.2 223d2b8afeda */
+/* bench 5662.0.3 660fc63305ca */
+/* bench 5662.0.4 d13b5b9da6dd */
 	return (address & ~(cacheline_size() - 1));
 }
 
