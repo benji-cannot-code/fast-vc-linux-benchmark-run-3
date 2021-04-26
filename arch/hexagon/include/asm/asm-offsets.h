84fbd6e95d695399494d2079f516e4a712e02743
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:27:07 +0000

    Benchmark change 4366
    Benchmark-Workload: s1m/main/4366

--- arch/hexagon/include/asm/asm-offsets.h
+++ arch/hexagon/include/asm/asm-offsets.h
@@ -1,2 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#include <generated/asm-offsets.h>
+/* bench 4366.2.0 ac1bbcf17e07 */
+/* bench 4366.2.1 23c0f50fe505 */
+/* bench 4366.2.2 330cbb7bb025 */
+/* bench 4366.2.3 2891e4eeef4e */
