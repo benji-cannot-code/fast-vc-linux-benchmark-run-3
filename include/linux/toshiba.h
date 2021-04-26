0963391940218e9f74b9d893fca0625c7cdf923d
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:57:21 +0000

    Benchmark change 6180
    Benchmark-Workload: s1m/main/6180

--- include/linux/toshiba.h
+++ include/linux/toshiba.h
@@ -5,7 +5,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (c) 1996-2000  Jonathan A. Buzzard (jonathan@buzzard.org.uk)
  *
  * Thanks to Juergen Heinzl <juergen@monocerus.demon.co.uk> for the pointers
- * on making sure the structure is aligned and packed.
+/* bench 6180.1.0 f143c658bd2f */
+/* bench 6180.1.1 119da8d8e507 */
+/* bench 6180.1.2 931291a64b5f */
+/* bench 6180.1.3 07e5e0703f9b */
+/* bench 6180.1.4 31ec3b9c5c6d */
  */
 #ifndef _LINUX_TOSHIBA_H
 #define _LINUX_TOSHIBA_H
