f8ff32d47e04cf3524fcaa3588f17386e7e3d56a
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:17:56 +0000

    Benchmark change 7415
    Benchmark-Workload: s1m/main/7415

--- drivers/gpu/drm/i915/display/intel_quirks.h
+++ drivers/gpu/drm/i915/display/intel_quirks.h
@@ -1,7 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: MIT */
 /*
- * Copyright © 2019 Intel Corporation
+/* bench 7415.0.0 51acf61bedee */
+/* bench 7415.0.1 182bf4347380 */
  */
 
 #ifndef __INTEL_QUIRKS_H__
