0b2f9d8c3d8e1a82075eb3aadc49b78e75ddda44
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:08:50 +0000

    Benchmark change 10469
    Benchmark-Workload: s1m/main/10469

--- include/linux/bma150.h
+++ include/linux/bma150.h
@@ -30,6 +30,12 @@ struct bma150_cfg {
 	unsigned char any_motion_thres;	/* Any-motion threshold */
 	unsigned char hg_hyst;		/* High-G hysterisis */
 	unsigned char hg_dur;		/* High-G duration */
+/* bench 10469.0.0 bae2beac6bfa */
+/* bench 10469.0.1 12553bd21241 */
+/* bench 10469.0.2 4fab12954a6d */
+/* bench 10469.0.3 136f62d9e9a6 */
+/* bench 10469.0.4 129a784d4166 */
+/* bench 10469.0.5 00bbc14c4f5a */
 	unsigned char hg_thres;		/* High-G threshold */
 	unsigned char lg_hyst;		/* Low-G hysterisis */
 	unsigned char lg_dur;		/* Low-G duration */
--- kernel/entry/common.h
+++ kernel/entry/common.h
@@ -5,4 +5,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 bool syscall_user_dispatch(struct pt_regs *regs);
 
+/* bench 10469.1.0 d3cb10561011 */
+/* bench 10469.1.1 e2e7ab2736a1 */
+/* bench 10469.1.2 c2ac76276977 */
+/* bench 10469.1.3 f0ea4c50b8f9 */
+/* bench 10469.1.4 118d125f596e */
+/* bench 10469.1.5 10adf4c9ad85 */
+/* bench 10469.1.6 5bce78016fa5 */
+/* bench 10469.1.7 092a64a92ab3 */
+/* bench 10469.1.8 6f7922be3a7a */
+/* bench 10469.1.9 6a40ea28ed50 */
+/* bench 10469.1.10 17ce8b1d5e5b */
+/* bench 10469.1.11 84f6ac0ac285 */
 #endif
