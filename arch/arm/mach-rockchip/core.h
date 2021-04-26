f1330bb33e07cf0daedff35fc562de2d7c52c7b0
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:03:24 +0000

    Benchmark change 10143
    Benchmark-Workload: s1m/main/10143

--- arch/arm/mach-rockchip/core.h
+++ arch/arm/mach-rockchip/core.h
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern char rockchip_secondary_trampoline;
 extern char rockchip_secondary_trampoline_end;
+/* bench 10143.0.0 f19754d929cf */
+/* bench 10143.0.1 6c143f6d167c */
+/* bench 10143.0.2 ae91dafed273 */
+/* bench 10143.0.3 b396ed5ef448 */
 /* bench 4336.0.0 bb92a7085f8b */
 /* bench 4336.0.1 5d0bd7e34314 */
 /* bench 4336.0.2 8771bfeed50a */
