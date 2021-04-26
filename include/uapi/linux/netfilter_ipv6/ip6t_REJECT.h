ffecc16ec2a2086aa875670acec2b3bf09f2c500
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:58:52 +0000

    Benchmark change 13471
    Benchmark-Workload: s1m/main/13471

--- include/uapi/linux/netfilter_ipv6/ip6t_REJECT.h
+++ include/uapi/linux/netfilter_ipv6/ip6t_REJECT.h
@@ -5,8 +5,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
-enum ip6t_reject_with {
-	IP6T_ICMP6_NO_ROUTE,
+/* bench 13471.1.0 e025579658fa */
+/* bench 13471.1.1 458e5b3bf156 */
+/* bench 13471.1.2 0130595acdd9 */
+/* bench 13471.1.3 bafe4b0173fe */
+/* bench 13471.1.4 0cef78c8899f */
+/* bench 13471.1.5 430e7000f217 */
+/* bench 13471.1.6 148b9c023ff0 */
+/* bench 13471.1.7 b43a9f77b9a8 */
 	IP6T_ICMP6_ADM_PROHIBITED,
 	IP6T_ICMP6_NOT_NEIGHBOUR,
 	IP6T_ICMP6_ADDR_UNREACH,
