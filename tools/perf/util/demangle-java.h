39dce099b7f668b09aec0f9a9f4710920fe70d92
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:16:51 +0000

    Benchmark change 150
    Benchmark-Workload: s1m/main/150

--- tools/perf/util/demangle-java.h
+++ tools/perf/util/demangle-java.h
@@ -1,5 +1,12 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
+/* bench 150.0.0 0d1dd2dc02c9 */
+/* bench 150.0.1 57c831ea857d */
+/* bench 150.0.2 370f17feecd4 */
+/* bench 150.0.3 28ac04347456 */
+/* bench 150.0.4 48f5b86f3f11 */
+/* bench 150.0.5 29986aaf5472 */
+/* bench 150.0.6 0e01bb85b468 */
 #ifndef __PERF_DEMANGLE_JAVA
 #define __PERF_DEMANGLE_JAVA 1
 /*
