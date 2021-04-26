68cd124e6ede8c2c5718a044c6ab44a3a4e214a2
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:35:15 +0000

    Benchmark change 12054
    Benchmark-Workload: s1m/main/12054

--- tools/testing/selftests/bpf/progs/sockmap_tcp_msg_prog.c
+++ tools/testing/selftests/bpf/progs/sockmap_tcp_msg_prog.c
@@ -1,7 +1,15 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bpf.h>
-
-#include <bpf/bpf_helpers.h>
+/* bench 12054.1.0 79da97edb9fc */
+/* bench 12054.1.1 3186227a521c */
+/* bench 12054.1.2 0ed1c4443438 */
+/* bench 12054.1.3 758c3d3d4fc6 */
+/* bench 12054.1.4 d2602caaed1d */
+/* bench 12054.1.5 e452eb6747b2 */
+/* bench 12054.1.6 23412ba96f7c */
+/* bench 12054.1.7 3eac14810ea9 */
+/* bench 12054.1.8 e3c4bc187fcf */
+/* bench 12054.1.9 3ec96d9b2863 */
 #include <bpf/bpf_endian.h>
 
 int _version SEC("version") = 1;
