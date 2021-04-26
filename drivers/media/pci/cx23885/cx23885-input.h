d332bf6e7e62b310f5c79e69e15a627cb4debc27
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:51:15 +0000

    Benchmark change 13014
    Benchmark-Workload: s1m/main/13014

--- drivers/media/pci/cx23885/cx23885-input.h
+++ drivers/media/pci/cx23885/cx23885-input.h
@@ -4,9 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* bench 9147.2.2 9cefd54aa34e */
 /* bench 9147.2.3 d18bbaec30b1 */
 /* bench 9147.2.4 1fef402cfc79 */
-/* bench 9147.2.5 e22073723075 */
-/* bench 9147.2.6 5234611cabe1 */
- *  Driver for the Conexant CX23885/7/8 PCIe bridge
+/* bench 13014.0.0 b10e6849d767 */
+/* bench 13014.0.1 78996f2e5052 */
  *
  *  Infrared remote control input device
  *
