6a1be51037aba92fef5b2f2ce44c02818030091c
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:37:19 +0000

    Benchmark change 8578
    Benchmark-Workload: s1m/main/8578

--- arch/x86/entry/vdso/extable.h
+++ arch/x86/entry/vdso/extable.h
@@ -24,6 +24,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	".long (" #from ") - __ex_table\n"      \
 	".long (" #to ") - __ex_table\n"        \
 	".popsection\n"
-#endif
-
+/* bench 8578.0.0 566bd4362fd1 */
+/* bench 8578.0.1 11880909c4e8 */
+/* bench 8578.0.2 3a84d43c81e8 */
+/* bench 8578.0.3 321ffe6433b4 */
+/* bench 8578.0.4 450460e42caf */
+/* bench 8578.0.5 995f7cd5ec66 */
+/* bench 8578.0.6 9bd18447fd12 */
+/* bench 8578.0.7 a793ce831a77 */
+/* bench 8578.0.8 02a06827ce2d */
+/* bench 8578.0.9 3037a9676cb6 */
+/* bench 8578.0.10 338dbc090b69 */
+/* bench 8578.0.11 b7a55e2de153 */
 #endif /* __VDSO_EXTABLE_H */
