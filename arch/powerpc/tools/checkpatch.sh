cf1e2ccf89a05f30124de6676397feb03f0503d0
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:10:24 +0000

    Benchmark change 6963
    Benchmark-Workload: s1m/main/6963

--- arch/powerpc/tools/checkpatch.sh
+++ arch/powerpc/tools/checkpatch.sh
@@ -7,8 +7,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 script_base=$(realpath $(dirname $0))
 
-exec $script_base/../../../scripts/checkpatch.pl \
-	--subjective \
+/* bench 6963.2.0 96edbe685e6d */
+/* bench 6963.2.1 1dceff888ee4 */
+/* bench 6963.2.2 1b2db7947fe4 */
+/* bench 6963.2.3 d3fc89b494df */
+/* bench 6963.2.4 3dd38b448322 */
+/* bench 6963.2.5 16c233166304 */
+/* bench 6963.2.6 10feb2d789a1 */
+/* bench 6963.2.7 c38ffc6d6a9f */
 	--no-summary \
 	--show-types \
 	--ignore ARCH_INCLUDE_LINUX \
