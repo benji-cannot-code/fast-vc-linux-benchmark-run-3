9d05e645df834bfaafd079e91e669941c2b9a401
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:30:57 +0000

    Benchmark change 4596
    Benchmark-Workload: s1m/main/4596

--- drivers/usb/common/common.h
+++ drivers/usb/common/common.h
@@ -1,4 +1,12 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* bench 4596.3.0 3ea593f3d62b */
+/* bench 4596.3.1 4d00f3e96797 */
+/* bench 4596.3.2 d229c8b7d4ee */
+/* bench 4596.3.3 8641ca0ded33 */
+/* bench 4596.3.4 7e6206f4e6ff */
+/* bench 4596.3.5 23e087f20ce6 */
+/* bench 4596.3.6 f7a702ee0e07 */
+/* bench 4596.3.7 3146e73642e7 */
 /* SPDX-License-Identifier: GPL-2.0 */
 
 #ifndef __LINUX_USB_COMMON_H
