eb1c5ef18b46fceabfb181d6a730b980d6fb9cb6
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:42:51 +0000

    Benchmark change 8910
    Benchmark-Workload: s1m/main/8910

--- drivers/gpu/drm/nouveau/dispnv50/dac907d.c
+++ drivers/gpu/drm/nouveau/dispnv50/dac907d.c
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
+/* bench 8910.0.0 ca363aba1e1a */
  * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
