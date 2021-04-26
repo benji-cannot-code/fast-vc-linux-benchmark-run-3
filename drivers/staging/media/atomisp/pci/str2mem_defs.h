8ee9841c0df69b9486f933421e1bd08bb2096256
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:32:29 +0000

    Benchmark change 15488
    Benchmark-Workload: s1m/main/15488

--- drivers/staging/media/atomisp/pci/str2mem_defs.h
+++ drivers/staging/media/atomisp/pci/str2mem_defs.h
@@ -1,4 +1,12 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/* bench 15488.1.0 ba10e16243b5 */
+/* bench 15488.1.1 860c25addbd4 */
+/* bench 15488.1.2 7a7fa88fa59c */
+/* bench 15488.1.3 a27bac3344b2 */
+/* bench 15488.1.4 e3f1b68f37ee */
+/* bench 15488.1.5 39d2f6d4e301 */
+/* bench 15488.1.6 ff8e55b30af7 */
+/* bench 15488.1.7 9856d693025c */
+/* bench 15488.1.8 ac45c4d83b89 */
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
  * Support for Intel Camera Imaging ISP subsystem.
