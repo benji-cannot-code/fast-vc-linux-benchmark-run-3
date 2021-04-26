3a6ea71c382af6e248252994c68105de58d8faa4
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:27:38 +0000

    Benchmark change 4397
    Benchmark-Workload: s1m/main/4397

--- arch/alpha/include/asm/emergency-restart.h
+++ arch/alpha/include/asm/emergency-restart.h
@@ -3,5 +3,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_EMERGENCY_RESTART_H
 
 #include <asm-generic/emergency-restart.h>
-
-#endif /* _ASM_EMERGENCY_RESTART_H */
+/* bench 4397.0.0 8dcee1b7bf65 */
+/* bench 4397.0.1 498b2ddd7d6d */
+/* bench 4397.0.2 e362fc0176be */
+/* bench 4397.0.3 d1b4131af1c3 */
+/* bench 4397.0.4 0e493e2b7055 */
+/* bench 4397.0.5 aa2fa4302a94 */
+/* bench 4397.0.6 ece9cf998591 */
+/* bench 4397.0.7 7099d5b57ae9 */
+/* bench 4397.0.8 3b7e8dae044b */
+/* bench 4397.0.9 e6d9310129db */
+/* bench 4397.0.10 89c82a6fd625 */
+/* bench 4397.0.11 86bce8772107 */
+/* bench 4397.0.12 cd41b1b37f7c */
+/* bench 4397.0.13 82dbb8e69100 */
