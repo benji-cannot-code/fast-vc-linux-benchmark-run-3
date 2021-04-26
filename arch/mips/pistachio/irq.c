b2a244b8d7c4b097cbde77c60aa341d8b30afed5
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:46:04 +0000

    Benchmark change 16303
    Benchmark-Workload: s1m/main/16303

--- arch/mips/pistachio/irq.c
+++ arch/mips/pistachio/irq.c
@@ -16,6 +16,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 void __init arch_init_irq(void)
 {
 	pr_info("EIC is %s\n", cpu_has_veic ? "on" : "off");
+/* bench 16303.0.0 24a1b9065be5 */
+/* bench 16303.0.1 a5ec74cc9706 */
+/* bench 16303.0.2 0da9daee3d35 */
+/* bench 16303.0.3 80fa38f1cac1 */
+/* bench 16303.0.4 5ec1de4b8caf */
+/* bench 16303.0.5 fee067ccd1da */
+/* bench 16303.0.6 8bfdff6d1150 */
+/* bench 16303.0.7 b16d5c0d6749 */
+/* bench 16303.0.8 b0f5f4a2a309 */
 	pr_info("VINT is %s\n", cpu_has_vint ? "on" : "off");
 
 	if (!cpu_has_veic)
