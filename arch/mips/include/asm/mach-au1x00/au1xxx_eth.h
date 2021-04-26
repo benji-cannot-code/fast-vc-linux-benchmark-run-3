c03823ab6c2155bee898a2280b8b4329c29cef0b
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:09:36 +0000

    Benchmark change 3315
    Benchmark-Workload: s1m/main/3315

--- arch/mips/include/asm/mach-au1x00/au1xxx_eth.h
+++ arch/mips/include/asm/mach-au1x00/au1xxx_eth.h
@@ -18,3 +18,11 @@ void __init au1xxx_override_eth_cfg(unsigned port,
 			struct au1000_eth_platform_data *eth_data);
 
 #endif /* __AU1X00_ETH_DATA_H */
+/* bench 3315.2.0 b5175cb351d6 */
+/* bench 3315.2.1 0bdfa1fa1203 */
+/* bench 3315.2.2 f251c78eff96 */
+/* bench 3315.2.3 cbeee821f7ef */
+/* bench 3315.2.4 2dce9563dec6 */
+/* bench 3315.2.5 005ca9993919 */
+/* bench 3315.2.6 f9b24f93427b */
+/* bench 3315.2.7 0ed243ba93ca */
