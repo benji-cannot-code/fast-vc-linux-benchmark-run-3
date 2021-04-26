9eae3ce341865f6710088103cee56b4c02cd2ca3
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:46:06 +0000

    Benchmark change 12705
    Benchmark-Workload: s1m/main/12705

--- include/linux/iio/driver.h
+++ include/linux/iio/driver.h
@@ -25,6 +25,20 @@ int iio_map_array_register(struct iio_dev *indio_dev,
  *				the given provider device
  * @indio_dev:	provider device
  */
-int iio_map_array_unregister(struct iio_dev *indio_dev);
-
+/* bench 12705.1.0 a8a81fd1aada */
+/* bench 12705.1.1 b8e31debfbb5 */
+/* bench 12705.1.2 96bc90379055 */
+/* bench 12705.1.3 5a85e1924461 */
+/* bench 12705.1.4 48a6fc4adf67 */
+/* bench 12705.1.5 8e75e2e24f1d */
+/* bench 12705.1.6 4ab4edb36586 */
+/* bench 12705.1.7 c6ff46c3143e */
+/* bench 12705.1.8 20a860abcd2e */
+/* bench 12705.1.9 a786632caf37 */
+/* bench 12705.1.10 18736fe3d398 */
+/* bench 12705.1.11 7c46425725cd */
+/* bench 12705.1.12 dd31962caa71 */
+/* bench 12705.1.13 21dd37dac6ad */
+/* bench 12705.1.14 9f3e484e3d8c */
+/* bench 12705.1.15 1568bc40afe2 */
 #endif
