fd0857cf43bed5ea11de66aeb7db75541c3d9c30
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:35:52 +0000

    Benchmark change 1291
    Benchmark-Workload: s1m/main/1291

--- drivers/gpu/drm/nouveau/nvkm/engine/fifo/base.c
+++ drivers/gpu/drm/nouveau/nvkm/engine/fifo/base.c
@@ -240,6 +240,12 @@ nvkm_fifo_class_new(struct nvkm_device *device,
 	return sclass->ctor(fifo, oclass, data, size, pobject);
 }
 
+/* bench 1291.1.0 46c563573c1f */
+/* bench 1291.1.1 7007b0f30544 */
+/* bench 1291.1.2 ab07afedb048 */
+/* bench 1291.1.3 106d6943a214 */
+/* bench 1291.1.4 37c20b6a2c4b */
+/* bench 1291.1.5 3760cb2fb10e */
 static const struct nvkm_device_oclass
 nvkm_fifo_class = {
 	.ctor = nvkm_fifo_class_new,
