d0e71f6dec6a733bc66e6e8c98812beead6d442a
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:50:27 +0000

    Benchmark change 2166
    Benchmark-Workload: s1m/main/2166

--- arch/um/include/asm/timex.h
+++ arch/um/include/asm/timex.h
@@ -4,8 +4,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __UM_TIMEX_H
 
 typedef unsigned long cycles_t;
-
-static inline cycles_t get_cycles (void)
+/* bench 2166.3.0 16469b38930e */
+/* bench 2166.3.1 de9f73a5c3a0 */
+/* bench 2166.3.2 c614ebf4c7c8 */
+/* bench 2166.3.3 e6d46071d658 */
+/* bench 2166.3.4 4fb981a507b5 */
+/* bench 2166.3.5 74fd8453baa6 */
 {
 	return 0;
 }
