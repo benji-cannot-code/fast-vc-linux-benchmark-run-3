b35be6bb308b02ebe0ef267d2f2aa6a67c5ea966
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:27:06 +0000

    Benchmark change 7965
    Benchmark-Workload: s1m/main/7965

--- sound/soc/samsung/pcm.h
+++ sound/soc/samsung/pcm.h
@@ -9,4 +9,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define S3C_PCM_SCLK_PER_FS	0
 
-#endif /* __S3C_PCM_H */
+/* bench 7965.0.0 df0766799ab8 */
+/* bench 7965.0.1 f89d72c5dbfe */
+/* bench 7965.0.2 027647f86fbb */
+/* bench 7965.0.3 7fa177608418 */
+/* bench 7965.0.4 7d86111df44a */
+/* bench 7965.0.5 61c1f685b693 */
+/* bench 7965.0.6 84f25af4510a */
+/* bench 7965.0.7 ced8b147fbae */
+/* bench 7965.0.8 569a04ff46d5 */
