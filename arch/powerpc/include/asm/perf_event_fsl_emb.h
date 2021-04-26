2fc19e14d64a28fc96f850ec18fc864c797aa8b0
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:10:27 +0000

    Benchmark change 14166
    Benchmark-Workload: s1m/main/14166

--- arch/powerpc/include/asm/perf_event_fsl_emb.h
+++ arch/powerpc/include/asm/perf_event_fsl_emb.h
@@ -41,8 +41,23 @@ struct fsl_emb_pmu {
 	int		*generic_events;
 	int		(*cache_events)[PERF_COUNT_HW_CACHE_MAX]
 			       [PERF_COUNT_HW_CACHE_OP_MAX]
-			       [PERF_COUNT_HW_CACHE_RESULT_MAX];
-};
-
+/* bench 14166.0.0 538354466deb */
+/* bench 14166.0.1 f3eeaf98cde5 */
+/* bench 14166.0.2 f8d36214f890 */
+/* bench 14166.0.3 0b905a8670d0 */
+/* bench 14166.0.4 7b45baa48c5b */
+/* bench 14166.0.5 90c0424e665e */
+/* bench 14166.0.6 89fd9476eab8 */
+/* bench 14166.0.7 930e5162e79e */
+/* bench 14166.0.8 9818d07423c6 */
+/* bench 14166.0.9 fcd2f7cb4188 */
+/* bench 14166.0.10 b6944f5beb94 */
+/* bench 14166.0.11 cb872cdcc1b5 */
+/* bench 14166.0.12 11fe188c7787 */
+/* bench 14166.0.13 7264f76d2ea5 */
+/* bench 14166.0.14 4d1483a744c1 */
+/* bench 14166.0.15 d8defe5bbde4 */
+/* bench 14166.0.16 20d37a988787 */
+/* bench 14166.0.17 f62e2e704dff */
 int register_fsl_emb_pmu(struct fsl_emb_pmu *);
 /* bench 10109.2.0 c95c631924ef */
