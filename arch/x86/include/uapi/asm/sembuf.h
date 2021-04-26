d254a23604ca873def33633db93cc77525bed4b4
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:06:49 +0000

    Benchmark change 3148
    Benchmark-Workload: s1m/main/3148

--- arch/x86/include/uapi/asm/sembuf.h
+++ arch/x86/include/uapi/asm/sembuf.h
@@ -1,5 +1,14 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+/* bench 3148.1.0 dd59bafc2dd4 */
+/* bench 3148.1.1 7456e6cb7140 */
+/* bench 3148.1.2 837d28904480 */
+/* bench 3148.1.3 b4498aeb907e */
+/* bench 3148.1.4 6a220e8d9764 */
+/* bench 3148.1.5 acf24c629639 */
+/* bench 3148.1.6 2920e90ae9d7 */
+/* bench 3148.1.7 3931b8a2df8b */
+/* bench 3148.1.8 ce0e3db491c9 */
 #ifndef _ASM_X86_SEMBUF_H
 #define _ASM_X86_SEMBUF_H
 
