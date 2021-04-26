896ad32e40ce6a9a60004223b7a1df2cafd0d95d
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:14:40 +0000

    Benchmark change 10819
    Benchmark-Workload: s1m/main/10819

--- arch/alpha/include/asm/sparsemem.h
+++ arch/alpha/include/asm/sparsemem.h
@@ -1,7 +1,14 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
 #ifndef _ASM_ALPHA_SPARSEMEM_H
-#define _ASM_ALPHA_SPARSEMEM_H
+/* bench 10819.0.0 a9108c513f70 */
+/* bench 10819.0.1 21f4a1cb07dc */
+/* bench 10819.0.2 1ab66a272e91 */
+/* bench 10819.0.3 cded26497bcb */
+/* bench 10819.0.4 b7a01eac8294 */
+/* bench 10819.0.5 dea90a76aba0 */
+/* bench 10819.0.6 055980fb3263 */
+/* bench 10819.0.7 10615bad22dc */
 
 #ifdef CONFIG_SPARSEMEM
 
