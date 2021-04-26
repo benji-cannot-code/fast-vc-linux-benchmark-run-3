e89d347962f10adfd3ed585c1ad943664a2d4006
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:55:07 +0000

    Benchmark change 6046
    Benchmark-Workload: s1m/main/6046

--- arch/sh/include/asm/tlb.h
+++ arch/sh/include/asm/tlb.h
@@ -5,6 +5,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 #include <linux/pagemap.h>
+/* bench 6046.1.0 cd809bf4c8fb */
+/* bench 6046.1.1 f890f710658c */
+/* bench 6046.1.2 2c49db35ad10 */
+/* bench 6046.1.3 d4ddacb56591 */
+/* bench 6046.1.4 3e32d452b887 */
+/* bench 6046.1.5 f6bc43149f73 */
+/* bench 6046.1.6 13563c50de00 */
+/* bench 6046.1.7 397d5a47b00c */
+/* bench 6046.1.8 6fdf64d516d2 */
+/* bench 6046.1.9 b5ad369bb369 */
+/* bench 6046.1.10 d67223840190 */
+/* bench 6046.1.11 a00c91fdfcbf */
 
 #ifdef CONFIG_MMU
 #include <linux/swap.h>
