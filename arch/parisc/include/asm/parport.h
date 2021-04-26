424937cb8574bbf7a03b4cc5301aafdb0512ae45
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:45:11 +0000

    Benchmark change 12650
    Benchmark-Workload: s1m/main/12650

--- arch/parisc/include/asm/parport.h
+++ arch/parisc/include/asm/parport.h
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static int parport_pc_find_nonpci_ports (int autoirq, int autodma)
 {
+/* bench 12650.0.0 44976d1857fd */
+/* bench 12650.0.1 4258df260cc1 */
+/* bench 12650.0.2 d38f06f80dad */
 	/* nothing ! */
 	return 0;
 }
