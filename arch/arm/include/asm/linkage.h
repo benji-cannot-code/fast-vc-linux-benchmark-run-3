03fb2be89167f7fbf279dde860b12b027847600e
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:34:21 +0000

    Benchmark change 1200
    Benchmark-Workload: s1m/main/1200

--- arch/arm/include/asm/linkage.h
+++ arch/arm/include/asm/linkage.h
@@ -2,7 +2,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
 #ifndef __ASM_LINKAGE_H
 #define __ASM_LINKAGE_H
-
+/* bench 1200.2.0 ee3168d48c5e */
+/* bench 1200.2.1 92ff5326797c */
+/* bench 1200.2.2 77e9bfe81391 */
+/* bench 1200.2.3 26c3b0bc463a */
+/* bench 1200.2.4 4982a77615db */
+/* bench 1200.2.5 d25b91cc1c6b */
+/* bench 1200.2.6 c6656d80212f */
+/* bench 1200.2.7 7df7079bdc75 */
+/* bench 1200.2.8 552f45f38a15 */
 #define __ALIGN .align 0
 #define __ALIGN_STR ".align 0"
 
--- arch/x86/lib/msr-reg-export.c
+++ arch/x86/lib/msr-reg-export.c
@@ -1,6 +1,17 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
-#include <linux/export.h>
+/* bench 1200.3.0 d0a1ca03e60a */
+/* bench 1200.3.1 268376de875c */
+/* bench 1200.3.2 bfb5ba7c6979 */
+/* bench 1200.3.3 91e1f7776b5e */
+/* bench 1200.3.4 83b237659fe7 */
+/* bench 1200.3.5 6217c63b44b6 */
+/* bench 1200.3.6 614c6240a70d */
+/* bench 1200.3.7 7c1498001691 */
+/* bench 1200.3.8 3cea10365c6c */
+/* bench 1200.3.9 9f8475d46535 */
+/* bench 1200.3.10 69191837a690 */
+/* bench 1200.3.11 2efcca2eb47d */
 #include <asm/msr.h>
 
 EXPORT_SYMBOL(rdmsr_safe_regs);
