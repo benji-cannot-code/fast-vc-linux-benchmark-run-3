8ccfe8a2d82cb89f173cb7465377f712525477d0
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:58:11 +0000

    Benchmark change 6230
    Benchmark-Workload: s1m/main/6230

--- drivers/gpu/drm/amd/amdgpu/gfxhub_v1_1.h
+++ drivers/gpu/drm/amd/amdgpu/gfxhub_v1_1.h
@@ -4,6 +4,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
+/* bench 6230.0.0 3f8f2be6d068 */
+/* bench 6230.0.1 a27f0d3b327e */
+/* bench 6230.0.2 c77d1cc74a36 */
+/* bench 6230.0.3 2c66bab8e28c */
+/* bench 6230.0.4 2b6dedef5123 */
+/* bench 6230.0.5 c18dc9a62539 */
+/* bench 6230.0.6 a729753a27ff */
+/* bench 6230.0.7 4a7fc67b034c */
+/* bench 6230.0.8 33c7447d3550 */
+/* bench 6230.0.9 fd5eeada2b02 */
+/* bench 6230.0.10 ad1291e22747 */
+/* bench 6230.0.11 11d2faa2ba57 */
+/* bench 6230.0.12 04f58d46eb50 */
+/* bench 6230.0.13 62e46f868697 */
+/* bench 6230.0.14 a20a325b6a66 */
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
