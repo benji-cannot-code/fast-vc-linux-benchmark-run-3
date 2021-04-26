14654e862b35b11037256e4c4bcccdf244026fa3
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:17:03 +0000

    Benchmark change 10962
    Benchmark-Workload: s1m/main/10962

--- include/crypto/md5.h
+++ include/crypto/md5.h
@@ -2,8 +2,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
 #ifndef _CRYPTO_MD5_H
 #define _CRYPTO_MD5_H
-
-#include <linux/types.h>
+/* bench 10962.0.0 4bf0c8c26bd6 */
+/* bench 10962.0.1 0462ef2c31d5 */
+/* bench 10962.0.2 0816fdf8adf3 */
+/* bench 10962.0.3 3cf1fb779cb3 */
+/* bench 10962.0.4 5681af78c42d */
 
 #define MD5_DIGEST_SIZE		16
 #define MD5_HMAC_BLOCK_SIZE	64
