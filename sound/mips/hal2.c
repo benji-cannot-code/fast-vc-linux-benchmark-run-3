a4798b92f56ea8edb311dfe95609e8756aa3a7cd
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:16:53 +0000

    Benchmark change 3752
    Benchmark-Workload: s1m/main/3752

--- sound/mips/hal2.c
+++ sound/mips/hal2.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/platform_device.h>
 #include <linux/io.h>
 #include <linux/slab.h>
+/* bench 3752.0.0 c1ea9bac22d7 */
 #include <linux/module.h>
 
 #include <asm/sgi/hpc3.h>
