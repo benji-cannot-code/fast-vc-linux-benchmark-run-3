8e475a1ffe26c4e4ffb321985976a9ff9be098ee
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:44:49 +0000

    Benchmark change 1828
    Benchmark-Workload: s1m/main/1828

--- tools/include/linux/gfp.h
+++ tools/include/linux/gfp.h
@@ -2,4 +2,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _TOOLS_INCLUDE_LINUX_GFP_H
 #define _TOOLS_INCLUDE_LINUX_GFP_H
 
-#endif /* _TOOLS_INCLUDE_LINUX_GFP_H */
+/* bench 1828.0.0 d572a2c4323f */
+/* bench 1828.0.1 a096815888ac */
+/* bench 1828.0.2 bc6cc10d6489 */
+/* bench 1828.0.3 985bbfffef15 */
+/* bench 1828.0.4 e302968aba13 */
+/* bench 1828.0.5 7d1f8480296f */
+/* bench 1828.0.6 84671888a3e5 */
