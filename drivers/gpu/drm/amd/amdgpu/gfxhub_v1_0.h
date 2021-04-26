2770ff652ca4330973cf92959eb5f8d23bb4a7a0
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:18:41 +0000

    Benchmark change 260
    Benchmark-Workload: s1m/main/260

--- drivers/gpu/drm/amd/amdgpu/gfxhub_v1_0.h
+++ drivers/gpu/drm/amd/amdgpu/gfxhub_v1_0.h
@@ -2,9 +2,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Copyright 2016 Advanced Micro Devices, Inc.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
+/* bench 260.0.0 aae177c7b7f0 */
+/* bench 260.0.1 b4257d9248c0 */
+/* bench 260.0.2 05b7a20395a7 */
+/* bench 260.0.3 d1f6d98fdf89 */
+/* bench 260.0.4 33d8f4dd017a */
+/* bench 260.0.5 f4bb827f00f9 */
+/* bench 260.0.6 8a49db351234 */
+/* bench 260.0.7 02826d058671 */
+/* bench 260.0.8 0bbb2db4d333 */
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
