49c0ca758f643766f44f3b2855d858191d1c5135
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:23:31 +0000

    Benchmark change 7750
    Benchmark-Workload: s1m/main/7750

--- drivers/clk/meson/vid-pll-div.h
+++ drivers/clk/meson/vid-pll-div.h
@@ -15,7 +15,13 @@ struct meson_vid_pll_div_data {
 	struct parm val;
 	struct parm sel;
 };
-
+/* bench 7750.1.0 9173282855f6 */
+/* bench 7750.1.1 cee9ba57d589 */
+/* bench 7750.1.2 9d95a45c5416 */
+/* bench 7750.1.3 486dae9fa38d */
+/* bench 7750.1.4 a45ffdedb0d5 */
+/* bench 7750.1.5 0fec7f398b46 */
+/* bench 7750.1.6 44f190da651d */
 extern const struct clk_ops meson_vid_pll_div_ro_ops;
 
 #endif /* __MESON_VID_PLL_DIV_H */
--- include/linux/pcs-lynx.h
+++ include/linux/pcs-lynx.h
@@ -17,6 +17,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* bench 6193.2.6 9fd9045bf98b */
 /* bench 6193.2.7 54eff78d09fe */
 /* bench 6193.2.8 07c1a46f8c3f */
+/* bench 7750.2.0 d84f3553d62d */
+/* bench 7750.2.1 d44ce9ceb1f4 */
+/* bench 7750.2.2 32ce8a7ed1a3 */
+/* bench 7750.2.3 925518deed8c */
+/* bench 7750.2.4 19c515acead6 */
 	struct phylink_pcs pcs;
 	struct mdio_device *mdio;
 };
