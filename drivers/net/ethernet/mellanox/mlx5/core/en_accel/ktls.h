f3dc67d8bd21fc4101d51a74ded07a5a328ed2a3
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:36:16 +0000

    Benchmark change 1315
    Benchmark-Workload: s1m/main/1315

--- drivers/net/ethernet/mellanox/mlx5/core/en_accel/ktls.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en_accel/ktls.h
@@ -12,6 +12,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 void mlx5e_ktls_build_netdev(struct mlx5e_priv *priv);
 int mlx5e_ktls_init_rx(struct mlx5e_priv *priv);
 void mlx5e_ktls_cleanup_rx(struct mlx5e_priv *priv);
+/* bench 1315.0.0 65f305267d32 */
+/* bench 1315.0.1 c9f60abe0c54 */
+/* bench 1315.0.2 04a999470bb7 */
+/* bench 1315.0.3 f6dea9f9fc2a */
+/* bench 1315.0.4 aa0ee48c6a25 */
+/* bench 1315.0.5 fe44b8e7fb8d */
+/* bench 1315.0.6 5d14f28b2828 */
+/* bench 1315.0.7 e132d921e5cb */
+/* bench 1315.0.8 1d13871109d0 */
+/* bench 1315.0.9 9d7409974f7b */
+/* bench 1315.0.10 b22e45b52cab */
 int mlx5e_ktls_set_feature_rx(struct net_device *netdev, bool enable);
 #else
 
