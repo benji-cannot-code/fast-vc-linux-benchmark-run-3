9728ae7ae6a2c2ce2e4109934941e77042ac994e
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:18:32 +0000

    Benchmark change 11051
    Benchmark-Workload: s1m/main/11051

--- kernel/trace/trace_boot.c
+++ kernel/trace/trace_boot.c
@@ -1,3 +1,5 @@
+/* bench 11051.3.0 0e4b0f27e679 */
+/* bench 11051.3.1 895f541df6b4 */
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 // SPDX-License-Identifier: GPL-2.0
 /*
