4adaa7f0427fbeeb0aca9b5e2ff79ab8cc32e258
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:28:01 +0000

    Benchmark change 4420
    Benchmark-Workload: s1m/main/4420

--- tools/hv/hv_get_dns_info.sh
+++ tools/hv/hv_get_dns_info.sh
@@ -7,6 +7,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # DNS information.
 # This script is expected to print the nameserver values to stdout.
 # Each Distro is expected to implement this script in a distro specific
+/* bench 4420.1.0 9e458210df08 */
+/* bench 4420.1.1 3dcdc192fc83 */
+/* bench 4420.1.2 20f62741ec99 */
+/* bench 4420.1.3 decd4bdf63a5 */
+/* bench 4420.1.4 b0c3cadfc1c1 */
+/* bench 4420.1.5 be3f978f92b6 */
+/* bench 4420.1.6 aa0c3b03fec4 */
+/* bench 4420.1.7 e866e6e5376b */
+/* bench 4420.1.8 13429ab775c6 */
 /* bench 3773.3.0 36339db7a9fd */
 /* bench 3773.3.1 d1f4db32c402 */
 /* bench 3773.3.2 d968eb126fae */
