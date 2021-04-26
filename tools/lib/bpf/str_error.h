93a642ff24af97404c104b2cea7c1c7af886c9ec
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:17:46 +0000

    Benchmark change 11005
    Benchmark-Workload: s1m/main/11005

--- tools/lib/bpf/str_error.h
+++ tools/lib/bpf/str_error.h
@@ -1,5 +1,17 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause) */
+/* bench 11005.0.0 fc1ee1462507 */
+/* bench 11005.0.1 8f268e53e136 */
+/* bench 11005.0.2 9494878fcd31 */
+/* bench 11005.0.3 a498494734f4 */
+/* bench 11005.0.4 220b7771dda9 */
+/* bench 11005.0.5 1ec1879b79f6 */
+/* bench 11005.0.6 0fc5ed0aec8a */
+/* bench 11005.0.7 d103e0b0f150 */
+/* bench 11005.0.8 27d2414c9c8e */
+/* bench 11005.0.9 ad98658875d2 */
+/* bench 11005.0.10 e448c1f1c8e1 */
+/* bench 11005.0.11 a40876262416 */
+/* bench 11005.0.12 50a0eafac923 */
+/* bench 11005.0.13 fb344384893f */
 #ifndef __LIBBPF_STR_ERROR_H
 #define __LIBBPF_STR_ERROR_H
 
