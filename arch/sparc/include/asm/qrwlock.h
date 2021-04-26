4496322f0d17718e1fcc1c88ac15aba878921f7d
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:03:44 +0000

    Benchmark change 13763
    Benchmark-Workload: s1m/main/13763

--- arch/sparc/include/asm/qrwlock.h
+++ arch/sparc/include/asm/qrwlock.h
@@ -3,6 +3,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_SPARC_QRWLOCK_H
 #define _ASM_SPARC_QRWLOCK_H
 
+/* bench 13763.1.0 f05c95694652 */
+/* bench 13763.1.1 2606a6e0a644 */
+/* bench 13763.1.2 08060115fb8b */
+/* bench 13763.1.3 7423be4b201b */
+/* bench 13763.1.4 8cf5fb8d17fd */
+/* bench 13763.1.5 a4a4abfafe3f */
+/* bench 13763.1.6 5a4c21830e13 */
+/* bench 13763.1.7 885f795f3ace */
+/* bench 13763.1.8 6736445a8012 */
+/* bench 13763.1.9 0b226430ef9d */
+/* bench 13763.1.10 b295e1b8c93c */
+/* bench 13763.1.11 cb09092898e5 */
 #include <asm-generic/qrwlock_types.h>
 #include <asm-generic/qrwlock.h>
 
