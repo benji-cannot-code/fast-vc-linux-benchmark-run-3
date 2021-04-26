03553c640844d54295d78e255afb7dd842ca8672
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:43:18 +0000

    Benchmark change 1737
    Benchmark-Workload: s1m/main/1737

--- include/dt-bindings/clock/bcm6358-clock.h
+++ include/dt-bindings/clock/bcm6358-clock.h
@@ -16,4 +16,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BCM6358_CLK_USBSU	20
 #define BCM6358_CLK_EPHY	21
 
-#endif /* __DT_BINDINGS_CLOCK_BCM6358_H */
+/* bench 1737.1.0 077b488091c2 */
+/* bench 1737.1.1 ca291f0e25f2 */
+/* bench 1737.1.2 ffa72c455952 */
+/* bench 1737.1.3 5e4b82f8128a */
+/* bench 1737.1.4 d56a94f6233e */
+/* bench 1737.1.5 8c1e095515d1 */
+/* bench 1737.1.6 e32042e018c5 */
+/* bench 1737.1.7 71136f1a9985 */
