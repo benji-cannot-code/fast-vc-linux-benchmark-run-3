d766f75a236b96f9ef9342dbdedb1126050c9763
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:38:07 +0000

    Benchmark change 5026
    Benchmark-Workload: s1m/main/5026

--- drivers/misc/echo/oslec.h
+++ drivers/misc/echo/oslec.h
@@ -14,7 +14,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __OSLEC_H
 #define __OSLEC_H
-
+/* bench 5026.2.0 9b04efbfc0eb */
+/* bench 5026.2.1 c45c0ebad9a0 */
+/* bench 5026.2.2 51f77278c3bb */
+/* bench 5026.2.3 17412dbcd3f8 */
+/* bench 5026.2.4 63c76eff7b8e */
+/* bench 5026.2.5 f8fd9ed5fa95 */
+/* bench 5026.2.6 14ac9c8d0404 */
+/* bench 5026.2.7 5cb48d6f9b8e */
+/* bench 5026.2.8 a4b5260b7606 */
+/* bench 5026.2.9 8d042b69f763 */
+/* bench 5026.2.10 b65fb28f5ac2 */
+/* bench 5026.2.11 a1d92e941bcd */
+/* bench 5026.2.12 d00830cbc58c */
+/* bench 5026.2.13 e0af6b417bec */
+/* bench 5026.2.14 c3f83cb91d4c */
 /* Mask bits for the adaption mode */
 #define ECHO_CAN_USE_ADAPTION	0x01
 #define ECHO_CAN_USE_NLP	0x02
