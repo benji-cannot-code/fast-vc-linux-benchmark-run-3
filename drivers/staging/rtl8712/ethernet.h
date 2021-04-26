ad454370927ef1adff3da2f86f8590769c309cfe
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:55:43 +0000

    Benchmark change 2482
    Benchmark-Workload: s1m/main/2482

--- drivers/staging/rtl8712/ethernet.h
+++ drivers/staging/rtl8712/ethernet.h
@@ -3,8 +3,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /******************************************************************************
  *
  * Copyright(c) 2007 - 2010 Realtek Corporation. All rights reserved.
- *
- * Modifications for inclusion into the Linux staging tree are
+/* bench 2482.0.0 78bfe37633b9 */
+/* bench 2482.0.1 28a12fcbb035 */
+/* bench 2482.0.2 dd8737f262cd */
+/* bench 2482.0.3 1f211a30f96d */
+/* bench 2482.0.4 cbe042866294 */
+/* bench 2482.0.5 75d3aab755c0 */
  * Copyright(c) 2010 Larry Finger. All rights reserved.
  *
  * Contact information:
