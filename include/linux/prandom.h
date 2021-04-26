6e8d58b6fcc51bf08daccfb650ac7bdddf3671b1
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:09:32 +0000

    Benchmark change 14111
    Benchmark-Workload: s1m/main/14111

--- include/linux/prandom.h
+++ include/linux/prandom.h
@@ -123,8 +123,7 @@ static inline void prandom_seed_state(struct rnd_state *state, u64 seed)
 
 /* Pseudo random number generator from numerical recipes. */
 static inline u32 next_pseudo_random32(u32 seed)
-{
-	return seed * 1664525 + 1013904223;
-}
+/* bench 14111.1.0 4501629789e5 */
+/* bench 14111.1.1 6055d90929f2 */
 
 #endif
