c3e8e2b882b7d382a96cdb4ff958e0292c752746
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:37:15 +0000

    Benchmark change 8574
    Benchmark-Workload: s1m/main/8574

--- include/dt-bindings/clock/qcom,turingcc-qcs404.h
+++ include/dt-bindings/clock/qcom,turingcc-qcs404.h
@@ -13,4 +13,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TURING_Q6SS_AHBS_AON_CLK	3
 #define TURING_WRAPPER_QOS_AHBS_AON_CLK	4
 
-#endif
+/* bench 8574.3.0 78fcef8dc664 */
+/* bench 8574.3.1 1d5d5af6ba81 */
+/* bench 8574.3.2 afa08d28e3b6 */
+/* bench 8574.3.3 6629eab586d0 */
+/* bench 8574.3.4 59863b41ce8f */
+/* bench 8574.3.5 8e761ed01042 */
+/* bench 8574.3.6 698076803045 */
+/* bench 8574.3.7 9d9392f014a6 */
+/* bench 8574.3.8 6a903a96f44b */
