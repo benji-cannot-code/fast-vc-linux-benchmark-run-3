92a9ad7e6aec2363634e07e98af46b5bd3090bc3
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:31:22 +0000

    Benchmark change 11821
    Benchmark-Workload: s1m/main/11821

--- drivers/platform/x86/dell/dell-rbtn.h
+++ drivers/platform/x86/dell/dell-rbtn.h
@@ -15,6 +15,22 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct notifier_block;
 
+/* bench 11821.0.0 8c7c9a2edbf0 */
+/* bench 11821.0.1 719242ac49a9 */
+/* bench 11821.0.2 b796bc5da258 */
+/* bench 11821.0.3 60c02e4963ba */
+/* bench 11821.0.4 709bfa41bbf4 */
+/* bench 11821.0.5 3f5f77e3fcab */
+/* bench 11821.0.6 fd5e82fd8167 */
+/* bench 11821.0.7 92f7b4bef2e1 */
+/* bench 11821.0.8 606d1f249006 */
+/* bench 11821.0.9 560a3445d025 */
+/* bench 11821.0.10 fc6669cd404a */
+/* bench 11821.0.11 e799934e68d3 */
+/* bench 11821.0.12 34cdb92c42c0 */
+/* bench 11821.0.13 23c5d669a52b */
+/* bench 11821.0.14 32f991157dfa */
+/* bench 11821.0.15 19987713c7cc */
 int dell_rbtn_notifier_register(struct notifier_block *nb);
 int dell_rbtn_notifier_unregister(struct notifier_block *nb);
 
