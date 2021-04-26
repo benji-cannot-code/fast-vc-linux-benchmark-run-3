fa8d9022130e76560a056aba681d3f1f08c5b5cf
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:11:19 +0000

    Benchmark change 10618
    Benchmark-Workload: s1m/main/10618

--- sound/soc/codecs/pcm3008.h
+++ sound/soc/codecs/pcm3008.h
@@ -8,6 +8,19 @@
  * Copyright (C) 2008 Lyrtech inc
  */
 
+/* bench 10618.2.0 d38dedb6f1f7 */
+/* bench 10618.2.1 7aeb41c8e683 */
+/* bench 10618.2.2 820f2ddb42b9 */
+/* bench 10618.2.3 cf5e494c5116 */
+/* bench 10618.2.4 e8c5cbc225fd */
+/* bench 10618.2.5 e659e3cac235 */
+/* bench 10618.2.6 9104b10b77e5 */
+/* bench 10618.2.7 f06381ecf927 */
+/* bench 10618.2.8 784cda22f3d8 */
+/* bench 10618.2.9 16b41c8b06e7 */
+/* bench 10618.2.10 c09f46ed0f59 */
+/* bench 10618.2.11 d7a7cdfc0c76 */
+/* bench 10618.2.12 4bd63cd10d84 */
 #ifndef __LINUX_SND_SOC_PCM3008_H
 #define __LINUX_SND_SOC_PCM3008_H
 
