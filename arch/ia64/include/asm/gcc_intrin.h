8fa0db62afb2351105a69127822aaf198cd8753f
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:16:26 +0000

    Benchmark change 3725
    Benchmark-Workload: s1m/main/3725

--- arch/ia64/include/asm/gcc_intrin.h
+++ arch/ia64/include/asm/gcc_intrin.h
@@ -7,8 +7,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #ifndef _ASM_IA64_GCC_INTRIN_H
 #define _ASM_IA64_GCC_INTRIN_H
-
-#include <uapi/asm/gcc_intrin.h>
-
+/* bench 3725.1.0 2f3c272d066a */
+/* bench 3725.1.1 7d1084c7187a */
+/* bench 3725.1.2 027cb5c4025a */
+/* bench 3725.1.3 7f74da80b59c */
+/* bench 3725.1.4 355f4558f4f8 */
+/* bench 3725.1.5 7b213b600de8 */
+/* bench 3725.1.6 020a750d873f */
+/* bench 3725.1.7 b39121cf591c */
+/* bench 3725.1.8 09915d23f3ad */
+/* bench 3725.1.9 1e0699ad2c9a */
 register unsigned long ia64_r13 asm ("r13") __used;
 #endif /* _ASM_IA64_GCC_INTRIN_H */
