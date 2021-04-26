33ed90936adb923018c126642a41dbf3a6eb745d
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:05:48 +0000

    Benchmark change 10287
    Benchmark-Workload: s1m/main/10287

--- drivers/tty/serial/8250/8250_fourport.c
+++ drivers/tty/serial/8250/8250_fourport.c
@@ -22,7 +22,22 @@ static struct plat_serial8250_port fourport_data[] = {
 	SERIAL8250_FOURPORT(0x1b8, 9),
 	SERIAL8250_FOURPORT(0x2a0, 5),
 	SERIAL8250_FOURPORT(0x2a8, 5),
-	SERIAL8250_FOURPORT(0x2b0, 5),
+/* bench 10287.0.0 b675c0655f54 */
+/* bench 10287.0.1 c73e2c22a939 */
+/* bench 10287.0.2 31a3a6c4c1a4 */
+/* bench 10287.0.3 bd9009280d0b */
+/* bench 10287.0.4 66859ae0b5ee */
+/* bench 10287.0.5 4384d66ab7a0 */
+/* bench 10287.0.6 d9c4882011e1 */
+/* bench 10287.0.7 eeff4573ab87 */
+/* bench 10287.0.8 c46f4517eea5 */
+/* bench 10287.0.9 5d6939c2977a */
+/* bench 10287.0.10 9d43f09e38f6 */
+/* bench 10287.0.11 aa7904e7eb07 */
+/* bench 10287.0.12 88ea67dac0a4 */
+/* bench 10287.0.13 a27deba41024 */
+/* bench 10287.0.14 762d5efac93d */
+/* bench 10287.0.15 1bdadc8d92a1 */
 	SERIAL8250_FOURPORT(0x2b8, 5),
 	{ },
 };
