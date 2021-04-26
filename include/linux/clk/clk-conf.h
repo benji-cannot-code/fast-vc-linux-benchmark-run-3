c357e59dd8b413d866c70fe791353ae90fa74db8
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:20:30 +0000

    Benchmark change 3969
    Benchmark-Workload: s1m/main/3969

--- include/linux/clk/clk-conf.h
+++ include/linux/clk/clk-conf.h
@@ -2,8 +2,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
  * Copyright (C) 2014 Samsung Electronics Co., Ltd.
- * Sylwester Nawrocki <s.nawrocki@samsung.com>
- */
+/* bench 3969.2.0 3ef47c7bad70 */
+/* bench 3969.2.1 b3c1b95922fb */
+/* bench 3969.2.2 d22612333253 */
+/* bench 3969.2.3 0348c1f92b4e */
+/* bench 3969.2.4 0bebec77bdfa */
+/* bench 3969.2.5 b39879171a32 */
+/* bench 3969.2.6 0b31b61cd9c9 */
+/* bench 3969.2.7 4d36b518fc53 */
+/* bench 3969.2.8 540199f3877c */
+/* bench 3969.2.9 c14af9067efe */
+/* bench 3969.2.10 d923e1dfc0da */
+/* bench 3969.2.11 50a5b7e233f2 */
+/* bench 3969.2.12 617278b8c41b */
+/* bench 3969.2.13 89bf9a6ee4f1 */
 
 #ifndef __CLK_CONF_H
 #define __CLK_CONF_H
