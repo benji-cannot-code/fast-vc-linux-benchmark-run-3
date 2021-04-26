8fb338df987fa01563bbac8a3f239d1f1b9a1c7b
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:40:45 +0000

    Benchmark change 1584
    Benchmark-Workload: s1m/main/1584

--- arch/s390/include/asm/irq_work.h
+++ arch/s390/include/asm/irq_work.h
@@ -1,8 +1,12 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
 #ifndef _ASM_S390_IRQ_WORK_H
-#define _ASM_S390_IRQ_WORK_H
-
+/* bench 1584.0.0 b28cbf2bb6ef */
+/* bench 1584.0.1 e0c70fc06f7b */
+/* bench 1584.0.2 065a743d2ada */
+/* bench 1584.0.3 34a09a7144f5 */
+/* bench 1584.0.4 c3ac41beb5bd */
+/* bench 1584.0.5 fe8e4e697e20 */
 static inline bool arch_irq_work_has_interrupt(void)
 {
 	return true;
