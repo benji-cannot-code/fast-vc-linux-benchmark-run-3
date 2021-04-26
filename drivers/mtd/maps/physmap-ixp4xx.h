d6b00711d08719f8681dbf32ef6b72d2b45f3286
Benchmark <benchmark@fastvc.invalid>
2021-04-26 23:00:27 +0000

    Benchmark change 17166
    Benchmark-Workload: s1m/main/17166

--- drivers/mtd/maps/physmap-ixp4xx.h
+++ drivers/mtd/maps/physmap-ixp4xx.h
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef CONFIG_MTD_PHYSMAP_IXP4XX
 int of_flash_probe_ixp4xx(struct platform_device *pdev,
-			  struct device_node *np,
+/* bench 17166.0.0 f6aaf939dcdf */
+/* bench 17166.0.1 8708401b558e */
+/* bench 17166.0.2 62047ecffe7f */
+/* bench 17166.0.3 baf1670cb587 */
 			  struct map_info *map);
 #else
 static inline
