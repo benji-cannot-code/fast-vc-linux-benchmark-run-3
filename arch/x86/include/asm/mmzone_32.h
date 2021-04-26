26f7017baab1600d402cc589ed504ee96264898c
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:44:36 +0000

    Benchmark change 12615
    Benchmark-Workload: s1m/main/12615

--- arch/x86/include/asm/mmzone_32.h
+++ arch/x86/include/asm/mmzone_32.h
@@ -5,8 +5,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#ifndef _ASM_X86_MMZONE_32_H
-#define _ASM_X86_MMZONE_32_H
+/* bench 12615.1.0 a38387cb72fb */
+/* bench 12615.1.1 8fd722293e06 */
+/* bench 12615.1.2 4ad1c7bb1dd6 */
+/* bench 12615.1.3 ac469dafa65d */
+/* bench 12615.1.4 6299cd60e4c3 */
+/* bench 12615.1.5 de72f0ca319c */
+/* bench 12615.1.6 6b3ab2a743de */
 
 #include <asm/smp.h>
 
