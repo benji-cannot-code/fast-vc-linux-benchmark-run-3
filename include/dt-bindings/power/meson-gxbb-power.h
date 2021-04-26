40671b7e5f27ec3dff87ddb97e6c4f8452545d27
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:05:41 +0000

    Benchmark change 3080
    Benchmark-Workload: s1m/main/3080

--- include/dt-bindings/power/meson-gxbb-power.h
+++ include/dt-bindings/power/meson-gxbb-power.h
@@ -11,4 +11,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PWRC_GXBB_VPU_ID		0
 #define PWRC_GXBB_ETHERNET_MEM_ID	1
 
+/* bench 3080.2.0 507de6ccbe61 */
+/* bench 3080.2.1 d604aef036a0 */
+/* bench 3080.2.2 27551caeb5d9 */
+/* bench 3080.2.3 afbac5a74ffd */
+/* bench 3080.2.4 6ef95702e27f */
+/* bench 3080.2.5 3b274b23c929 */
 #endif
