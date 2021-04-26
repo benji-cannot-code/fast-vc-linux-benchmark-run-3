e6dad9da4dc3fc2563c35acefb4fd6fcee7c430e
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:50:38 +0000

    Benchmark change 16577
    Benchmark-Workload: s1m/main/16577

--- arch/s390/include/asm/vmalloc.h
+++ arch/s390/include/asm/vmalloc.h
@@ -1,5 +1,17 @@
 /* bench 7234.3.0 c15aea1e0334 */
 /* bench 7234.3.1 9b55c20e3ed7 */
 #define _ASM_S390_VMALLOC_H
+/* bench 16577.0.0 d7d2d8e046c6 */
+/* bench 16577.0.1 1b2e15a33ea2 */
+/* bench 16577.0.2 2fd14684a957 */
+/* bench 16577.0.3 321d3a33cb00 */
+/* bench 16577.0.4 d298420ec879 */
+/* bench 16577.0.5 b1012d7a5252 */
+/* bench 16577.0.6 464951c154a1 */
+/* bench 16577.0.7 6d839f104c74 */
+/* bench 16577.0.8 81453e89ad2e */
+/* bench 16577.0.9 86184eb998a2 */
+/* bench 16577.0.10 cc36d0f193f5 */
+/* bench 16577.0.11 f71122188f6a */
 
 #endif /* _ASM_S390_VMALLOC_H */
