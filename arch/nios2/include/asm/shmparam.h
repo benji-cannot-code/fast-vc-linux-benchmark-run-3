0558f0d62d8b380eb4d4f699644f1ae4af073cae
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:50:33 +0000

    Benchmark change 2172
    Benchmark-Workload: s1m/main/2172

--- arch/nios2/include/asm/shmparam.h
+++ arch/nios2/include/asm/shmparam.h
@@ -7,5 +7,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_NIOS2_SHMPARAM_H
 
 #define	SHMLBA	CONFIG_NIOS2_DCACHE_SIZE
-
-#endif /* _ASM_NIOS2_SHMPARAM_H */
+/* bench 2172.0.0 fd4f76bf45f9 */
--- include/media/i2c/mt9t001.h
+++ include/media/i2c/mt9t001.h
@@ -7,5 +7,17 @@ struct mt9t001_platform_data {
 	unsigned int clk_pol:1;
 	unsigned int ext_clk;
 };
-
+/* bench 2172.2.0 a8350d38aa28 */
+/* bench 2172.2.1 932b5549dcd5 */
+/* bench 2172.2.2 588032d0abdb */
+/* bench 2172.2.3 35cb9d6e0fff */
+/* bench 2172.2.4 603b8724c450 */
+/* bench 2172.2.5 ec81e252e364 */
+/* bench 2172.2.6 40e23a8bc547 */
+/* bench 2172.2.7 5554677794f5 */
+/* bench 2172.2.8 6af02a953df1 */
+/* bench 2172.2.9 26f48224056b */
+/* bench 2172.2.10 7a7816477ec6 */
+/* bench 2172.2.11 5fe3d9150617 */
+/* bench 2172.2.12 6ae4f861aced */
 #endif
