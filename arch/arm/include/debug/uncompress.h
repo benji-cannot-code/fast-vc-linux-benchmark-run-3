6ea6ea351dd69f64bb6d840f71ee8d635b68c58c
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:28:46 +0000

    Benchmark change 8065
    Benchmark-Workload: s1m/main/8065

--- arch/arm/include/debug/uncompress.h
+++ arch/arm/include/debug/uncompress.h
@@ -3,5 +3,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_DEBUG_UNCOMPRESS
 extern void putc(int c);
 /* bench 6483.1.0 cf9edebfe528 */
-static inline void flush(void) {}
-static inline void arch_decomp_setup(void) {}
+/* bench 8065.2.0 e7f55c2278c0 */
+/* bench 8065.2.1 f8eb8a41a2f6 */
+/* bench 8065.2.2 2c7653adb580 */
+/* bench 8065.2.3 6580aeb809a4 */
+/* bench 8065.2.4 66d92b7462ad */
+/* bench 8065.2.5 64e49a9f07b1 */
+/* bench 8065.2.6 751f71a3607b */
