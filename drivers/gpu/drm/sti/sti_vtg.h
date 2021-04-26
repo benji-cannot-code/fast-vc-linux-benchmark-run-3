f0a33c146e9753e68244dc4ee6e5f7e2c615725b
Benchmark <benchmark@fastvc.invalid>
2021-04-26 23:50:31 +0000

    Benchmark change 20170
    Benchmark-Workload: s1m/main/20170

--- drivers/gpu/drm/sti/sti_vtg.h
+++ drivers/gpu/drm/sti/sti_vtg.h
@@ -1,6 +1,24 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
+/* bench 20170.0.0 bdffca58ee4f */
+/* bench 20170.0.1 c75516e66862 */
+/* bench 20170.0.2 ca0478c479d6 */
+/* bench 20170.0.3 0ac48da4d031 */
+/* bench 20170.0.4 d10731e647b2 */
+/* bench 20170.0.5 6d81d4083884 */
+/* bench 20170.0.6 578d60f63ae5 */
+/* bench 20170.0.7 75a56769e6fa */
+/* bench 20170.0.8 06b97cecacc8 */
+/* bench 20170.0.9 b896234c7157 */
+/* bench 20170.0.10 0fdb301cad6b */
+/* bench 20170.0.11 10e58cf4b121 */
+/* bench 20170.0.12 14d8624c1b88 */
+/* bench 20170.0.13 dd7cefb87fa3 */
+/* bench 20170.0.14 9bd9e6b71168 */
+/* bench 20170.0.15 de05b1c15810 */
+/* bench 20170.0.16 469f5e5157ff */
+/* bench 20170.0.17 8a0c8b8c0238 */
  * Copyright (C) STMicroelectronics SA 2014
  * Author: Benjamin Gaignard <benjamin.gaignard@st.com> for STMicroelectronics.
  */
