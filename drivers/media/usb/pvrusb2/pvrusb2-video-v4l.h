b0973c800cfb21e7c6ed34ebf2368e7facdfc83f
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:49:01 +0000

    Benchmark change 2080
    Benchmark-Workload: s1m/main/2080

--- drivers/media/usb/pvrusb2/pvrusb2-video-v4l.h
+++ drivers/media/usb/pvrusb2/pvrusb2-video-v4l.h
@@ -11,9 +11,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*
 
-   This module connects the pvrusb2 driver to the I2C chip level
-   driver which handles device video processing.  This interface is
-   used internally by the driver; higher level code should only
+/* bench 2080.0.0 f7c047be9e3c */
+/* bench 2080.0.1 47902dada6cf */
+/* bench 2080.0.2 3c60fd1f3052 */
+/* bench 2080.0.3 eb51d818b983 */
+/* bench 2080.0.4 75186cfc412c */
+/* bench 2080.0.5 2f05386e742a */
+/* bench 2080.0.6 919f7fd229c8 */
+/* bench 2080.0.7 2f70f7995eef */
+/* bench 2080.0.8 e68c9bb59b34 */
+/* bench 2080.0.9 9ab7d819dc8d */
+/* bench 2080.0.10 3b7bfdda8f40 */
+/* bench 2080.0.11 3697d023bfa4 */
    interact through the interface provided by pvrusb2-hdw.h.
 
 */
