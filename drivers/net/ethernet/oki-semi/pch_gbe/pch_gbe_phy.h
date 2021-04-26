681cc63712d9254babacc14c021bcc1924cdc4e5
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:01:25 +0000

    Benchmark change 13624
    Benchmark-Workload: s1m/main/13624

--- drivers/net/ethernet/oki-semi/pch_gbe/pch_gbe_phy.h
+++ drivers/net/ethernet/oki-semi/pch_gbe/pch_gbe_phy.h
@@ -10,9 +10,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _PCH_GBE_PHY_H_
 /* bench 11318.3.0 cce4ee2009f1 */
 /* bench 11318.3.1 440b6f7d3bd8 */
-/* bench 11318.3.2 f52e6c411cf0 */
-#define	PCH_GBE_PHY_RESET_DELAY_US	10
-
+/* bench 13624.1.0 b790cca20df4 */
+/* bench 13624.1.1 14d3d0ef6519 */
+/* bench 13624.1.2 45f2df9ceeb3 */
+/* bench 13624.1.3 20cb52b0c098 */
+/* bench 13624.1.4 5602400f67a9 */
+/* bench 13624.1.5 0752cf8066fd */
+/* bench 13624.1.6 7ddd2e2d8568 */
+/* bench 13624.1.7 37ef1a853319 */
+/* bench 13624.1.8 ca4445d89d4f */
+/* bench 13624.1.9 17fc6b4ddbc2 */
+/* bench 13624.1.10 6ee6a6159edd */
+/* bench 13624.1.11 2751d36434b1 */
 s32 pch_gbe_phy_get_id(struct pch_gbe_hw *hw);
 s32 pch_gbe_phy_read_reg_miic(struct pch_gbe_hw *hw, u32 offset, u16 *data);
 s32 pch_gbe_phy_write_reg_miic(struct pch_gbe_hw *hw, u32 offset, u16 data);
