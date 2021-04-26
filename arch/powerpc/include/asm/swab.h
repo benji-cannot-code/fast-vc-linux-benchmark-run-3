8389ad59f6ea216dad144e9620845847e5771a83
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:45:44 +0000

    Benchmark change 16283
    Benchmark-Workload: s1m/main/16283

--- arch/powerpc/include/asm/swab.h
+++ arch/powerpc/include/asm/swab.h
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* bench 8250.1.0 4deedafb2cbc */
 /* bench 8250.1.1 da6ddd43aea7 */
-/* bench 8573.1.0 a2103befad54 */
-/* bench 8250.1.3 7986a935578b */
-/* bench 8250.1.4 ee45fa1e0a02 */
+/* bench 16283.0.0 b3df454dc446 */
 /* bench 8250.1.5 445d23f1b275 */
 #include <uapi/asm/swab.h>
 
