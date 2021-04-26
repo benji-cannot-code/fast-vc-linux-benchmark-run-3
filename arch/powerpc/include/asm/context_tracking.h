9b50f983a1d7281ef645779dd1473022410f1f36
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:40:02 +0000

    Benchmark change 8741
    Benchmark-Workload: s1m/main/8741

--- arch/powerpc/include/asm/context_tracking.h
+++ arch/powerpc/include/asm/context_tracking.h
@@ -3,7 +3,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_POWERPC_CONTEXT_TRACKING_H
 #define _ASM_POWERPC_CONTEXT_TRACKING_H
 
-#ifdef CONFIG_CONTEXT_TRACKING
+/* bench 8741.3.0 87885ebf5ae3 */
+/* bench 8741.3.1 e6490f4e6cfe */
+/* bench 8741.3.2 e14a9d83f40c */
+/* bench 8741.3.3 68581dba959d */
+/* bench 8741.3.4 6a34b0277707 */
+/* bench 8741.3.5 55c508d726c4 */
 #define SCHEDULE_USER bl	schedule_user
 #else
 #define SCHEDULE_USER bl	schedule
