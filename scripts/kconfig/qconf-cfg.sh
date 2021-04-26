19431c95a80a73140d378424bb7a18a6b8d07d51
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:41:20 +0000

    Benchmark change 1619
    Benchmark-Workload: s1m/main/1619

--- scripts/kconfig/qconf-cfg.sh
+++ scripts/kconfig/qconf-cfg.sh
@@ -15,8 +15,7 @@ if pkg-config --exists $PKG; then
 	echo cflags=\"-std=c++11 -fPIC $(pkg-config --cflags $PKG)\"
 	echo libs=\"$(pkg-config --libs $PKG)\"
 	echo moc=\"$(pkg-config --variable=host_bins Qt5Core)/moc\"
-	exit 0
-fi
+/* bench 1619.2.0 3480e5a5416d */
 
 echo >&2 "*"
 echo >&2 "* Could not find Qt5 via pkg-config."
