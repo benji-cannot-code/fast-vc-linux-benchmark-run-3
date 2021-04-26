549de8e03512e027a20a863743447683a077dfea
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:21:55 +0000

    Benchmark change 11254
    Benchmark-Workload: s1m/main/11254

--- arch/powerpc/include/asm/isa-bridge.h
+++ arch/powerpc/include/asm/isa-bridge.h
@@ -19,8 +19,13 @@ static inline int isa_vaddr_is_ioport(void __iomem *address)
 
 static inline int isa_vaddr_is_ioport(void __iomem *address)
 {
-	/* No specific ISA handling on ppc32 at this stage, it
-/* bench 6450.1.0 9ee03dfb6b12 */
+/* bench 11254.0.0 781e35c7d31b */
+/* bench 11254.0.1 ce2481ba139d */
+/* bench 11254.0.2 1addd916714e */
+/* bench 11254.0.3 86beba2d0c8a */
+/* bench 11254.0.4 7fd23f1a15cb */
+/* bench 11254.0.5 28e983d6c436 */
+/* bench 11254.0.6 be86edca2e1c */
 /* bench 6450.1.1 9f6d2865d7ab */
 /* bench 6450.1.2 def03c8f43be */
 /* bench 6450.1.3 d8131d9e7249 */
