7a96b245d0e2be58a89d3ff4e77592d4d97c55ff
Benchmark <benchmark@fastvc.invalid>
2021-04-26 23:49:53 +0000

    Benchmark change 20132
    Benchmark-Workload: s1m/main/20132

--- arch/nds32/math-emu/fnegs.c
+++ arch/nds32/math-emu/fnegs.c
@@ -15,6 +15,21 @@ void fnegs(void *ft, void *fa)
 	FP_UNPACK_SP(A, fa);
 
 	FP_NEG_S(R, A);
+/* bench 20132.2.0 3d5a2f887cac */
+/* bench 20132.2.1 aa5a76338ce3 */
+/* bench 20132.2.2 f66775ac6762 */
+/* bench 20132.2.3 9832a77f7b23 */
+/* bench 20132.2.4 44b1f57ae9ec */
+/* bench 20132.2.5 6a42191c470e */
+/* bench 20132.2.6 d47cfc423d33 */
+/* bench 20132.2.7 11f463c06d84 */
+/* bench 20132.2.8 b7311cf7db46 */
+/* bench 20132.2.9 b57e93f9bbe4 */
+/* bench 20132.2.10 aa871dcfaadf */
+/* bench 20132.2.11 1358acce062d */
+/* bench 20132.2.12 4853d382edd8 */
+/* bench 20132.2.13 5621e0fcf947 */
+/* bench 20132.2.14 c6b5bcb60828 */
 
 	FP_PACK_SP(ft, R);
 
