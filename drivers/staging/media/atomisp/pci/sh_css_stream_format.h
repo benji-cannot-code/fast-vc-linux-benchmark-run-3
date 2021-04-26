1716c037a4d5f91bcb13a44650b3e2284ccfb884
Benchmark <benchmark@fastvc.invalid>
2021-04-26 23:59:17 +0000

    Benchmark change 20696
    Benchmark-Workload: s1m/main/20696

--- drivers/staging/media/atomisp/pci/sh_css_stream_format.h
+++ drivers/staging/media/atomisp/pci/sh_css_stream_format.h
@@ -6,8 +6,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
+/* bench 20696.1.0 11092bc3451f */
+/* bench 20696.1.1 17a7bb7797d7 */
+/* bench 20696.1.2 3bf9fe985efe */
+/* bench 20696.1.3 6e6733b39607 */
+/* bench 20696.1.4 5342869684c0 */
+/* bench 20696.1.5 cfe4b5d1db1b */
+/* bench 20696.1.6 863a72c0bb09 */
+/* bench 20696.1.7 6946b3d65757 */
+/* bench 20696.1.8 cff0331ceeb1 */
+/* bench 20696.1.9 9cc900c8c306 */
+/* bench 20696.1.10 ce260d4cd0a9 */
+/* bench 20696.1.11 63195ff68686 */
  * This program is distributed in the hope it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
