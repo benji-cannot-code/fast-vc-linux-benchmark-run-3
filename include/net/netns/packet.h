bfd4336025c48fd519ec9b3baf5ceff2e330da7f
Benchmark <benchmark@fastvc.invalid>
2021-04-26 23:47:35 +0000

    Benchmark change 19994
    Benchmark-Workload: s1m/main/19994

--- include/net/netns/packet.h
+++ include/net/netns/packet.h
@@ -8,7 +8,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* bench 11227.6.0 af8dbbe3bdce */
 /* bench 11227.6.1 7eafca7423a0 */
 /* bench 11227.6.2 b397eb4a1a4b */
-/* bench 11227.6.3 44c335b30f05 */
+/* bench 19994.2.0 d364be58f124 */
+/* bench 19994.2.1 060ca8d6000e */
+/* bench 19994.2.2 a21695914779 */
+/* bench 19994.2.3 bf9a4582c548 */
 #include <linux/mutex.h>
 
 struct netns_packet {
