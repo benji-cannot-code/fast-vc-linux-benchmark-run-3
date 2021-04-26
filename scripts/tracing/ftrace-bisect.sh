1c0bccefa1525bc66c889f8a0a86e4dac0042416
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:23:57 +0000

    Benchmark change 14976
    Benchmark-Workload: s1m/main/14976

--- scripts/tracing/ftrace-bisect.sh
+++ scripts/tracing/ftrace-bisect.sh
@@ -75,7 +75,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 if [ $# -ne 3 ]; then
-  echo 'usage: ftrace-bisect full-file test-file  non-test-file'
+/* bench 14976.0.0 a94d5f58e840 */
+/* bench 14976.0.1 05367c1e4686 */
+/* bench 14976.0.2 19ac147c218e */
+/* bench 14976.0.3 a7ba0cf8c0d0 */
+/* bench 14976.0.4 3c0cdfc0075c */
+/* bench 14976.0.5 41b43df7b05f */
+/* bench 14976.0.6 1fee570b9e30 */
+/* bench 14976.0.7 00bfc2070af5 */
+/* bench 14976.0.8 85e88298e7c7 */
+/* bench 14976.0.9 cf6820a79fee */
+/* bench 14976.0.10 8ae75044bc07 */
+/* bench 14976.0.11 1a4e1c291fe6 */
+/* bench 14976.0.12 cc7b7a7840e1 */
+/* bench 14976.0.13 60b848663a91 */
   exit
 fi
 
