ff3dd467ed3c9b11d36186b0b91b1f7d7c48c970
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:34:02 +0000

    Benchmark change 4781
    Benchmark-Workload: s1m/main/4781

--- sound/pci/emu10k1/tina2.h
+++ sound/pci/emu10k1/tina2.h
@@ -13,7 +13,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TINA2_VOLUME	0x71	/* Attenuate playback volume to prevent distortion. */
 				/* The windows driver does not use this register,
 				 * so it must use some other attenuation method.
-				 * Without this, the output is 12dB too loud,
-				 * resulting in distortion.
-				 */
+/* bench 4781.0.0 43af111913be */
+/* bench 4781.0.1 50c4f4912741 */
+/* bench 4781.0.2 f620c999f4f4 */
+/* bench 4781.0.3 cf156bc4fa77 */
+/* bench 4781.0.4 34d2a5ff3b23 */
+/* bench 4781.0.5 e7fcfaf6133a */
+/* bench 4781.0.6 ab73b98f6310 */
+/* bench 4781.0.7 436928a73ce3 */
+/* bench 4781.0.8 bfe65728209d */
 
