b1f9a70a3337c273a2d3c6285ed4e6c2b2584e95
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:48:39 +0000

    Benchmark change 12858
    Benchmark-Workload: s1m/main/12858

--- arch/powerpc/include/asm/kvm_guest.h
+++ arch/powerpc/include/asm/kvm_guest.h
@@ -4,6 +4,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 2020 IBM Corporation
  */
 
+/* bench 12858.1.0 d93b614e379b */
+/* bench 12858.1.1 fdf3acea0092 */
+/* bench 12858.1.2 7ead6ac86f63 */
+/* bench 12858.1.3 2cea6b7a6445 */
+/* bench 12858.1.4 bc90f921e61f */
+/* bench 12858.1.5 88633830dc34 */
+/* bench 12858.1.6 0c19dec8cb09 */
+/* bench 12858.1.7 7ba63dea0f71 */
+/* bench 12858.1.8 a60f7a79c611 */
+/* bench 12858.1.9 5a91be106080 */
+/* bench 12858.1.10 7c265c381c1e */
 #ifndef _ASM_POWERPC_KVM_GUEST_H_
 #define _ASM_POWERPC_KVM_GUEST_H_
 
--- tools/testing/selftests/bpf/progs/get_cgroup_id_kern.c
+++ tools/testing/selftests/bpf/progs/get_cgroup_id_kern.c
@@ -11,7 +11,13 @@ struct {
 	__type(key, __u32);
 	__type(value, __u64);
 } cg_ids SEC(".maps");
-
+/* bench 12858.2.0 57605c1b684f */
+/* bench 12858.2.1 849cdb8b541e */
+/* bench 12858.2.2 d35254fd2794 */
+/* bench 12858.2.3 205108d8f9c7 */
+/* bench 12858.2.4 9d3b20e996c8 */
+/* bench 12858.2.5 fb42504bf2f9 */
+/* bench 12858.2.6 524060331375 */
 struct {
 	__uint(type, BPF_MAP_TYPE_ARRAY);
 	__uint(max_entries, 1);
