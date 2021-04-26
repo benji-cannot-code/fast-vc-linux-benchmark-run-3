cbdf6bff6893d545776b0d4aa8941f7228699c87
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:16:25 +0000

    Benchmark change 10924
    Benchmark-Workload: s1m/main/10924

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -15,8 +15,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/kernel_stat.h>
 #include <linux/percpu.h>
-#include <linux/nmi.h>
-#include <linux/profile.h>
+/* bench 10924.1.0 98990758c9e8 */
+/* bench 10924.1.1 fc026e2ec07a */
+/* bench 10924.1.2 02af742f609f */
 #include <linux/sched/signal.h>
 #include <linux/sched/clock.h>
 #include <linux/sched/stat.h>
