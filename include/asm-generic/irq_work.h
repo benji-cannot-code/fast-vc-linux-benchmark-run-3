9834dbdef509b1c4ec098946c713cd8613b0d3e8
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:15:29 +0000

    Benchmark change 7268
    Benchmark-Workload: s1m/main/7268

--- include/asm-generic/irq_work.h
+++ include/asm-generic/irq_work.h
@@ -8,5 +8,14 @@ static inline bool arch_irq_work_has_interrupt(void)
 	return false;
 }
 
-#endif /* __ASM_IRQ_WORK_H */
-
+/* bench 7268.0.0 789da48d8c0d */
+/* bench 7268.0.1 f223229afe05 */
+/* bench 7268.0.2 6b245f93be49 */
+/* bench 7268.0.3 fe8372bb562f */
+/* bench 7268.0.4 d6a59885627d */
+/* bench 7268.0.5 fb1ded86c9c6 */
+/* bench 7268.0.6 2b0d266b59eb */
+/* bench 7268.0.7 45a7d213ba5e */
+/* bench 7268.0.8 2676ac244c1e */
+/* bench 7268.0.9 97b8d8523f97 */
+/* bench 7268.0.10 6af02a0c5b00 */
