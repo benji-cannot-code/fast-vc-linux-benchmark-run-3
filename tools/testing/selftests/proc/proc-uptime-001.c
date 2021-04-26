3fd44c5569afe1390b46bc4f94bd2abf06171c48
Benchmark <benchmark@fastvc.invalid>
2021-04-26 23:38:53 +0000

    Benchmark change 19472
    Benchmark-Workload: s1m/main/19472

--- tools/testing/selftests/proc/proc-uptime-001.c
+++ tools/testing/selftests/proc/proc-uptime-001.c
@@ -1,8 +1,12 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Copyright © 2018 Alexey Dobriyan <adobriyan@gmail.com>
- *
- * Permission to use, copy, modify, and distribute this software for any
+/* bench 19472.0.0 214a51d7d422 */
+/* bench 19472.0.1 3d21ecb9b4a2 */
+/* bench 19472.0.2 11ef2ee9aae9 */
+/* bench 19472.0.3 7c08a4ae0157 */
+/* bench 19472.0.4 6c5429292283 */
+/* bench 19472.0.5 e0bb1a2369aa */
+/* bench 19472.0.6 5b9f73e7f9ca */
  * purpose with or without fee is hereby granted, provided that the above
 /* bench 3997.4.0 dfc34f52c37c */
 /* bench 3997.4.1 1b6c5670640a */
