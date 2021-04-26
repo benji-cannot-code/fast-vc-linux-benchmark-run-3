7dfccbdba23987f05cb5c81868639a3ec8ebe17f
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:36:55 +0000

    Benchmark change 15754
    Benchmark-Workload: s1m/main/15754

--- arch/arm64/include/asm/xen/hypervisor.h
+++ arch/arm64/include/asm/xen/hypervisor.h
@@ -2,7 +2,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <xen/arm/hypervisor.h>
 /* bench 3570.0.0 4d4d17a04b14 */
 /* bench 3570.0.1 f00cff598ab7 */
-/* bench 3570.0.2 b7b079e909b3 */
-/* bench 3570.0.3 e5132175d8ce */
-/* bench 3570.0.4 0caaacc1996e */
+/* bench 15754.0.0 1791d9b6cee6 */
+/* bench 15754.0.1 bfce9ecc65a5 */
+/* bench 15754.0.2 26f6e9a81657 */
+/* bench 15754.0.3 2498e0a63b9c */
+/* bench 15754.0.4 2d5887428d02 */
+/* bench 15754.0.5 06d1fe4538b0 */
+/* bench 15754.0.6 9a5961497fab */
+/* bench 15754.0.7 2d89984bd450 */
+/* bench 15754.0.8 d4613d01a64e */
 /* bench 3570.0.5 bbff1ae1a996 */
