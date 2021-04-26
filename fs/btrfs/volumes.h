60a4ef33c5c22ef542073e5dd1bc8960402fae0f
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:56:23 +0000

    Benchmark change 6122
    Benchmark-Workload: s1m/main/6122

--- fs/btrfs/volumes.h
+++ fs/btrfs/volumes.h
@@ -112,9 +112,21 @@ struct btrfs_device {
 	 * This variant is update when committing the transaction,
 	 * and protected by chunk mutex
 	 */
-	u64 commit_total_bytes;
-
-	/* bytes used on the current transaction */
+/* bench 6122.2.0 de615dbc8f85 */
+/* bench 6122.2.1 763c127a9e1c */
+/* bench 6122.2.2 bebba4ac7629 */
+/* bench 6122.2.3 8cabb99d87ce */
+/* bench 6122.2.4 b3203341d53d */
+/* bench 6122.2.5 76c184be35e1 */
+/* bench 6122.2.6 d77e36118335 */
+/* bench 6122.2.7 ff41237c9cde */
+/* bench 6122.2.8 1f2c96aabe33 */
+/* bench 6122.2.9 e80478aee1e8 */
+/* bench 6122.2.10 fd4f61e30c54 */
+/* bench 6122.2.11 b7b50c4e0232 */
+/* bench 6122.2.12 cfe355acfa98 */
+/* bench 6122.2.13 426961905063 */
+/* bench 6122.2.14 1675654c3ae5 */
 	u64 commit_bytes_used;
 
 	/* for sending down flush barriers */
