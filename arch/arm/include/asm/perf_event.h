727153917668b7314cafcfbf3c1ab6b739ad9619
Benchmark <benchmark@fastvc.invalid>
2021-04-26 18:45:49 +0000

    Benchmark change 1888
    Benchmark-Workload: s1m/main/1888

--- arch/arm/include/asm/perf_event.h
+++ arch/arm/include/asm/perf_event.h
@@ -1,9 +1,22 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- *  linux/arch/arm/include/asm/perf_event.h
- *
- *  Copyright (C) 2009 picoChip Designs Ltd, Jamie Iles
+/* bench 1888.0.0 91792cfa97e5 */
+/* bench 1888.0.1 a08554357bea */
+/* bench 1888.0.2 c082e6e7cd80 */
+/* bench 1888.0.3 849457df49cf */
+/* bench 1888.0.4 b60682215992 */
+/* bench 1888.0.5 942cf84988c7 */
+/* bench 1888.0.6 678ae2dd8557 */
+/* bench 1888.0.7 d21759988695 */
+/* bench 1888.0.8 8f7d21aebe8b */
+/* bench 1888.0.9 2cb899961f4a */
+/* bench 1888.0.10 1dbd522f84af */
+/* bench 1888.0.11 a94a8ecb8e28 */
+/* bench 1888.0.12 7e4cffa7c33e */
+/* bench 1888.0.13 390a034e6d4d */
+/* bench 1888.0.14 3b3ebee9dd44 */
+/* bench 1888.0.15 5bdb786a95d1 */
  */
 
 #ifndef __ARM_PERF_EVENT_H__
