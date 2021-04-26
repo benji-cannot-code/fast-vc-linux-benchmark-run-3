959066662bd5d1b63b959afefff29f64a99ca4dd
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:53:56 +0000

    Benchmark change 16775
    Benchmark-Workload: s1m/main/16775

--- include/linux/ts-nbus.h
+++ include/linux/ts-nbus.h
@@ -3,8 +3,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (c) 2016 - Savoir-faire Linux
  * Author: Sebastien Bourdelin <sebastien.bourdelin@savoirfairelinux.com>
  *
- * This file is licensed under the terms of the GNU General Public
- * License version 2. This program is licensed "as is" without any
+/* bench 16775.0.0 52b9aaab3c3b */
+/* bench 16775.0.1 4adf11c537b0 */
+/* bench 16775.0.2 037a2fe886d0 */
+/* bench 16775.0.3 65b9656e2bca */
+/* bench 16775.0.4 aa57a86010a3 */
+/* bench 16775.0.5 c6253e633f38 */
+/* bench 16775.0.6 8f2b568e8341 */
+/* bench 16775.0.7 d059d71284eb */
+/* bench 16775.0.8 637c745eb7d1 */
+/* bench 16775.0.9 1af0c1ce06f0 */
+/* bench 16775.0.10 eaac4af602ca */
+/* bench 16775.0.11 e05a19c448bc */
+/* bench 16775.0.12 d3cf61770b51 */
  * warranty of any kind, whether express or implied.
  */
 
