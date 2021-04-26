7d05f385128168cc1bfc77ad7a743da4f1d62d32
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:42:40 +0000

    Benchmark change 12499
    Benchmark-Workload: s1m/main/12499

--- include/uapi/linux/if_ppp.h
+++ include/uapi/linux/if_ppp.h
@@ -1,2 +1,12 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ppp-ioctl.h>
+/* bench 12499.3.0 edc27b351943 */
+/* bench 12499.3.1 47e1225ac28c */
+/* bench 12499.3.2 460fc6d51736 */
+/* bench 12499.3.3 ab7b1020c164 */
+/* bench 12499.3.4 66eced657920 */
+/* bench 12499.3.5 732229ae7936 */
+/* bench 12499.3.6 315d65338daf */
+/* bench 12499.3.7 d063e6dbbb8b */
+/* bench 12499.3.8 829c991adfff */
+/* bench 12499.3.9 4b6a099f292f */
