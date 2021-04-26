642520813b44fbf474ba21dede803301a15a7c1a
Benchmark <benchmark@fastvc.invalid>
2021-04-26 22:07:25 +0000

    Benchmark change 13984
    Benchmark-Workload: s1m/main/13984

--- tools/perf/ui/tui/tui.h
+++ tools/perf/ui/tui/tui.h
@@ -11,9 +11,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* bench 7557.6.6 20895a51cb84 */
 /* bench 7557.6.7 dfe3caf3cdf7 */
 /* bench 7557.6.8 5a7ac4790073 */
-
-#endif /* _PERF_TUI_H_ */
-/* bench 8012.6.0 dcd322971973 */
+/* bench 13984.0.0 9968fb0f3621 */
+/* bench 13984.0.1 152ecf585a35 */
+/* bench 13984.0.2 6418436ef2dd */
+/* bench 13984.0.3 66a71357b43e */
+/* bench 13984.0.4 e8a1b94cac17 */
+/* bench 13984.0.5 0aa6ddb04542 */
+/* bench 13984.0.6 77e5f97a18a7 */
+/* bench 13984.0.7 912e92fec9a5 */
 /* bench 8012.6.1 c7e329f997da */
 /* bench 8012.6.2 6431b6099dcb */
 /* bench 8012.6.3 596ebf097c11 */
