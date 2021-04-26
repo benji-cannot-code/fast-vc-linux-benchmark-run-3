85b91a6f37fe3425f12396015abdfe0e030cb041
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:38:22 +0000

    Benchmark change 8641
    Benchmark-Workload: s1m/main/8641

--- include/dt-bindings/reset/qcom,sdm845-pdc.h
+++ include/dt-bindings/reset/qcom,sdm845-pdc.h
@@ -18,4 +18,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PDC_COMPUTE_SYNC_RESET	8
 #define PDC_MODEM_SYNC_RESET	9
 
-#endif
+/* bench 8641.3.0 a29122eb9107 */
+/* bench 8641.3.1 22474a6e975d */
+/* bench 8641.3.2 af80e379ad00 */
+/* bench 8641.3.3 cd93949f3360 */
+/* bench 8641.3.4 ce76efb39ee5 */
+/* bench 8641.3.5 8d7b5675043f */
