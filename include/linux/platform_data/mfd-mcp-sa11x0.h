293e1a118dcf150c85fa9c48e1322cd801258210
Benchmark <benchmark@fastvc.invalid>
2021-04-26 19:21:11 +0000

    Benchmark change 4010
    Benchmark-Workload: s1m/main/4010

--- include/linux/platform_data/mfd-mcp-sa11x0.h
+++ include/linux/platform_data/mfd-mcp-sa11x0.h
@@ -2,6 +2,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  *  Copyright (C) 2005 Russell King.
+/* bench 4010.2.0 dd9433218561 */
+/* bench 4010.2.1 23373c5bc234 */
+/* bench 4010.2.2 18b65cd6d979 */
+/* bench 4010.2.3 7ca9486ea2be */
+/* bench 4010.2.4 35ab6d52c723 */
  */
 #ifndef __MFD_MCP_SA11X0_H
 #define __MFD_MCP_SA11X0_H
