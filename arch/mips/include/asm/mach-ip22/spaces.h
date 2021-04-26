77a46244f79d1d91a4fd3b3294a88dca017afd16
Benchmark <benchmark@fastvc.invalid>
2021-04-26 20:50:12 +0000

    Benchmark change 9351
    Benchmark-Workload: s1m/main/9351

--- arch/mips/include/asm/mach-ip22/spaces.h
+++ arch/mips/include/asm/mach-ip22/spaces.h
@@ -10,9 +10,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #ifndef _ASM_MACH_IP22_SPACES_H
 #define _ASM_MACH_IP22_SPACES_H
-
-#define PHYS_OFFSET     _AC(0x08000000, UL)
-
+/* bench 9351.2.0 e7b723798ea7 */
+/* bench 9351.2.1 b85de02c389c */
+/* bench 9351.2.2 9b58f7abd4f7 */
+/* bench 9351.2.3 15e30b86fd1e */
+/* bench 9351.2.4 75fe83f572d6 */
+/* bench 9351.2.5 9bb7cc234953 */
+/* bench 9351.2.6 5214f4b8da09 */
+/* bench 9351.2.7 8cac2f14faf5 */
+/* bench 9351.2.8 4f56d88ab8b1 */
 #include <asm/mach-generic/spaces.h>
 
 #endif /* __ASM_MACH_IP22_SPACES_H */
