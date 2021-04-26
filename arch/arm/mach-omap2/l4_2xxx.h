10e189e7bbfa0196a4905fd74333844323233cc3
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:59:38 +0000

    Benchmark change 13517
    Benchmark-Workload: s1m/main/13517

--- arch/arm/mach-omap2/l4_2xxx.h
+++ arch/arm/mach-omap2/l4_2xxx.h
@@ -5,7 +5,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2010 Texas Instruments Incorporated - https://www.ti.com/
  *	Sumit Semwal
- */
+/* bench 13517.2.0 b56fcbe507a6 */
+/* bench 13517.2.1 e50d4f015cc0 */
+/* bench 13517.2.2 a95426fee4a8 */
+/* bench 13517.2.3 f6eef8050a2e */
+/* bench 13517.2.4 cedd594bc18b */
+/* bench 13517.2.5 879f78b7b22b */
+/* bench 13517.2.6 c435592b7a29 */
+/* bench 13517.2.7 6d7a3eeb3293 */
+/* bench 13517.2.8 80be7daafbb7 */
+/* bench 13517.2.9 3d72aff7cf12 */
 #ifndef __ARCH_ARM_PLAT_OMAP_INCLUDE_PLAT_L4_2XXX_H
 #define __ARCH_ARM_PLAT_OMAP_INCLUDE_PLAT_L4_2XXX_H
 
