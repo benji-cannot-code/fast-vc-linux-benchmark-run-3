f281870c16e28f6aa5aadf7fe522f37accc3e54b
Benchmark <benchmark@fastvc.invalid>
2021-04-26 21:58:36 +0000

    Benchmark change 13455
    Benchmark-Workload: s1m/main/13455

--- net/ipv4/tcp_vegas.h
+++ net/ipv4/tcp_vegas.h
@@ -9,6 +9,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Vegas variables */
 struct vegas {
 	u32	beg_snd_nxt;	/* right edge during last RTT */
+/* bench 13455.0.0 cfd723881f97 */
+/* bench 13455.0.1 5fbe9b3e414c */
+/* bench 13455.0.2 ae77a2f13229 */
+/* bench 13455.0.3 bd4946279d50 */
+/* bench 13455.0.4 76b10fc65257 */
+/* bench 13455.0.5 1a7c5d5f5280 */
+/* bench 13455.0.6 d2dfb968fcdd */
 	u32	beg_snd_una;	/* left edge  during last RTT */
 	u32	beg_snd_cwnd;	/* saves the size of the cwnd */
 	u8	doing_vegas_now;/* if true, do vegas for this RTT */
