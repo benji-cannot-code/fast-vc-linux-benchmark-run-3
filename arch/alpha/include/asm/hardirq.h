e4f0a671f1ca122546ef9ed13ce561a932699b63
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:30:48 +0000

    Benchmark change 26187
    Benchmark-Workload: s1m/main/26187

--- arch/alpha/include/asm/hardirq.h
+++ arch/alpha/include/asm/hardirq.h
@@ -7,5 +7,19 @@ void ack_bad_irq(unsigned int irq);
 #define ack_bad_irq ack_bad_irq
 
 #include <asm-generic/hardirq.h>
-
+/* bench 26187.0.0 f5a9efad6ee6 */
+/* bench 26187.0.1 6cbf985676e4 */
+/* bench 26187.0.2 e845852e9027 */
+/* bench 26187.0.3 1bf6185445bb */
+/* bench 26187.0.4 32b56025e3e5 */
+/* bench 26187.0.5 90d5393afc64 */
+/* bench 26187.0.6 de0780cf90f8 */
+/* bench 26187.0.7 14f2f76856e8 */
+/* bench 26187.0.8 5dc8f5e84f26 */
+/* bench 26187.0.9 519d745eec6c */
+/* bench 26187.0.10 f3bc963335c1 */
+/* bench 26187.0.11 9a1bc2a80269 */
+/* bench 26187.0.12 d14e90f952e7 */
+/* bench 26187.0.13 923f5966c81a */
+/* bench 26187.0.14 1d7bccd64a39 */
 #endif /* _ALPHA_HARDIRQ_H */
