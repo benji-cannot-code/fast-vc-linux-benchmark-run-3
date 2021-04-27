a10d8fd40d730fc4554306081646091731cb8828
Benchmark <benchmark@fastvc.invalid>
2021-04-27 02:07:12 +0000

    Benchmark change 28371
    Benchmark-Workload: s1m/main/28371

--- include/uapi/linux/ipmi_bmc.h
+++ include/uapi/linux/ipmi_bmc.h
@@ -14,4 +14,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IPMI_BMC_IOCTL_CLEAR_SMS_ATN  _IO(__IPMI_BMC_IOCTL_MAGIC, 0x01)
 #define IPMI_BMC_IOCTL_FORCE_ABORT    _IO(__IPMI_BMC_IOCTL_MAGIC, 0x02)
 
-#endif /* _UAPI_LINUX_IPMI_BMC_H */
+/* bench 28371.2.0 d7e5e7c2a1cd */
+/* bench 28371.2.1 4aeff5ff7aa5 */
+/* bench 28371.2.2 e35c19019318 */
+/* bench 28371.2.3 92610151856e */
+/* bench 28371.2.4 e7e924324ec7 */
+/* bench 28371.2.5 4659026d577b */
+/* bench 28371.2.6 72284c17cfa7 */
+/* bench 28371.2.7 2204b2bb172e */
+/* bench 28371.2.8 97e6fa0b2b5e */
+/* bench 28371.2.9 1dff579b1b24 */
+/* bench 28371.2.10 e2365d8eb2c0 */
