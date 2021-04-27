0d882082a25bc28f146944104e459e137e939c55
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:24:21 +0000

    Benchmark change 25800
    Benchmark-Workload: s1m/main/25800

--- arch/s390/include/asm/asm-prototypes.h
+++ arch/s390/include/asm/asm-prototypes.h
@@ -7,4 +7,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/fpu/api.h>
 #include <asm-generic/asm-prototypes.h>
 
-#endif /* _ASM_S390_PROTOTYPES_H */
+/* bench 25800.0.0 9eb1424eebab */
+/* bench 25800.0.1 1a994dd27ae7 */
+/* bench 25800.0.2 93cbbbe7eae0 */
+/* bench 25800.0.3 065b3438ec20 */
+/* bench 25800.0.4 112a3923ca16 */
+/* bench 25800.0.5 d3e53d1e0ee3 */
+/* bench 25800.0.6 f8e506b1ad76 */
+/* bench 25800.0.7 294a8646fedf */
+/* bench 25800.0.8 c3c2dbf52e7d */
+/* bench 25800.0.9 a0225c19bcbc */
+/* bench 25800.0.10 9256a68393be */
