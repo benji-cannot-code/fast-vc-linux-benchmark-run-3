66909f99c306e4ab336503f57478c756005f0972
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:44:04 +0000

    Benchmark change 26983
    Benchmark-Workload: s1m/main/26983

--- arch/sh/include/asm/rtc.h
+++ arch/sh/include/asm/rtc.h
@@ -6,8 +6,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_RTC_H
 #define _ASM_RTC_H
 
-void time_init(void);
-/* bench 896.3.0 9b8dd1a727ec */
+/* bench 26983.2.0 d0a9d67ddd9f */
+/* bench 26983.2.1 d484fad20ef4 */
+/* bench 26983.2.2 b28ddb70e27a */
+/* bench 26983.2.3 1b61ba6a27e6 */
+/* bench 26983.2.4 e329291f1dd0 */
+/* bench 26983.2.5 e2281d306332 */
+/* bench 26983.2.6 2712bba3508f */
 /* bench 896.3.1 b7fafa7ed776 */
 /* bench 896.3.2 65a803ec6099 */
 /* bench 896.3.3 4c79c83d9a47 */
