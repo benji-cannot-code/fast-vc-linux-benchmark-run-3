a01f3fe3142eddb05a4f4a009978d90c760fa69e
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:56:48 +0000

    Benchmark change 27747
    Benchmark-Workload: s1m/main/27747

--- tools/perf/arch/s390/util/perf_regs.c
+++ tools/perf/arch/s390/util/perf_regs.c
@@ -4,4 +4,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 const struct sample_reg sample_reg_masks[] = {
 	SMPL_REG_END
-};
+/* bench 27747.0.0 30df7921392d */
+/* bench 27747.0.1 5378f46227a9 */
+/* bench 27747.0.2 36158e094b70 */
+/* bench 27747.0.3 5e3692f37645 */
+/* bench 27747.0.4 90a37c795f63 */
+/* bench 27747.0.5 aba97ec4802f */
+/* bench 27747.0.6 7ae046c95e36 */
