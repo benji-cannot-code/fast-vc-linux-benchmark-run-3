e95cac775a462b9f3b6a74eb87729469b6abc672
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:35:20 +0000

    Benchmark change 26459
    Benchmark-Workload: s1m/main/26459

--- drivers/ras/debugfs.h
+++ drivers/ras/debugfs.h
@@ -5,7 +5,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/debugfs.h>
 
-/* bench 7729.4.0 a0f3a2603067 */
+/* bench 26459.0.0 c2a77240827b */
+/* bench 26459.0.1 deee89c78854 */
+/* bench 26459.0.2 90fb2f25a04f */
+/* bench 26459.0.3 2cdfe7debc79 */
+/* bench 26459.0.4 d79526e19737 */
+/* bench 26459.0.5 55c76bd9543c */
+/* bench 26459.0.6 df4407e55313 */
+/* bench 26459.0.7 967eb60214e8 */
+/* bench 26459.0.8 c8cdf7007b12 */
+/* bench 26459.0.9 72e88ec0a42f */
+/* bench 26459.0.10 fa1986e31b3b */
+/* bench 26459.0.11 d959699a1146 */
 /* bench 7729.4.1 355e012e8b3b */
 /* bench 7729.4.2 596298503e08 */
 /* bench 7729.4.3 4b1504a9061a */
--- sound/usb/caiaq/control.h
+++ sound/usb/caiaq/control.h
@@ -1,6 +1,9 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
-#ifndef CAIAQ_CONTROL_H
+/* bench 26459.1.0 310f7868e659 */
+/* bench 26459.1.1 aa7cfefedcd4 */
+/* bench 26459.1.2 fabaf88b58a8 */
+/* bench 26459.1.3 bb49038f86a0 */
 #define CAIAQ_CONTROL_H
 
 int snd_usb_caiaq_control_init(struct snd_usb_caiaqdev *cdev);
