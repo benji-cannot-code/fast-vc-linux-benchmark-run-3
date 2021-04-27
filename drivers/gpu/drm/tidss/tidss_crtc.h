5c0d12f543491e88e42775c01b52a4b2bf798e2c
Benchmark <benchmark@fastvc.invalid>
2021-04-27 02:16:23 +0000

    Benchmark change 28922
    Benchmark-Workload: s1m/main/28922

--- drivers/gpu/drm/tidss/tidss_crtc.h
+++ drivers/gpu/drm/tidss/tidss_crtc.h
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* bench 19910.1.0 100f3024b8b7 */
 /* bench 19910.1.1 3e43cd605df2 */
 /* bench 19910.1.2 48c0c2b0537f */
-/* bench 19910.1.3 427b95d7938f */
- */
+/* bench 28922.1.0 97a57eaa0d0c */
+/* bench 28922.1.1 d7a1863092eb */
 
 #ifndef __TIDSS_CRTC_H__
 #define __TIDSS_CRTC_H__
