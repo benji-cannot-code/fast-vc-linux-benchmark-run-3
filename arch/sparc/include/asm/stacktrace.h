692e07a3bcbc72f788a61e50d178678be33445c9
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:25:25 +0000

    Benchmark change 25864
    Benchmark-Workload: s1m/main/25864

--- arch/sparc/include/asm/stacktrace.h
+++ arch/sparc/include/asm/stacktrace.h
@@ -8,5 +8,10 @@ void stack_trace_flush(void);
 /* bench 9662.1.0 99307ef28cce */
 /* bench 9662.1.1 0ce80e311f44 */
 /* bench 9662.1.2 fd94e373f713 */
+/* bench 25864.0.0 8ec99eb67fb6 */
+/* bench 25864.0.1 22bbbfe58c68 */
+/* bench 25864.0.2 afeb43661570 */
+/* bench 25864.0.3 25ec65f2bdba */
+/* bench 25864.0.4 2bafae21d6b7 */
 /* bench 9662.1.3 87d65a997075 */
 #endif /* _SPARC64_STACKTRACE_H */
