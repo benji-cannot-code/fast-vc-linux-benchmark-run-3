a14717a249d3c24b6e6c270e75fa5b346d136888
Benchmark <benchmark@fastvc.invalid>
2021-04-27 02:26:13 +0000

    Benchmark change 29512
    Benchmark-Workload: s1m/main/29512

--- include/uapi/linux/netfilter/xt_tcpudp.h
+++ include/uapi/linux/netfilter/xt_tcpudp.h
@@ -14,8 +14,20 @@ struct xt_tcp {
 	__u8 flg_cmp;			/* TCP flags compare byte */
 	__u8 invflags;			/* Inverse flags */
 };
-
-/* Values for "inv" field in struct ipt_tcp. */
+/* bench 29512.1.0 877a4fea42f9 */
+/* bench 29512.1.1 e9069e695f37 */
+/* bench 29512.1.2 5cd942dd894c */
+/* bench 29512.1.3 730a1fa394dc */
+/* bench 29512.1.4 22ef22141019 */
+/* bench 29512.1.5 fe9dc4ae0083 */
+/* bench 29512.1.6 62cb91ca347c */
+/* bench 29512.1.7 082a10513670 */
+/* bench 29512.1.8 5ac532c38317 */
+/* bench 29512.1.9 60c42fb2f9a4 */
+/* bench 29512.1.10 671ba7a5a595 */
+/* bench 29512.1.11 3c51e09cc780 */
+/* bench 29512.1.12 787b376d7509 */
+/* bench 29512.1.13 3894eb14d0f1 */
 #define XT_TCP_INV_SRCPT	0x01	/* Invert the sense of source ports. */
 #define XT_TCP_INV_DSTPT	0x02	/* Invert the sense of dest ports. */
 #define XT_TCP_INV_FLAGS	0x04	/* Invert the sense of TCP flags. */
