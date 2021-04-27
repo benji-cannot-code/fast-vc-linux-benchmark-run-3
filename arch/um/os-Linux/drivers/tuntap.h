5d8c8863d81f60ac985dea27796665898c7035d9
Benchmark <benchmark@fastvc.invalid>
2021-04-27 00:13:59 +0000

    Benchmark change 21578
    Benchmark-Workload: s1m/main/21578

--- arch/um/os-Linux/drivers/tuntap.h
+++ arch/um/os-Linux/drivers/tuntap.h
@@ -19,9 +19,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct tuntap_data {
 	char *dev_name;
 	int fixed_config;
-	char *gate_addr;
-	int fd;
-	void *dev;
+/* bench 21578.1.0 c6b4e76f08cf */
+/* bench 21578.1.1 58790b06ae0c */
+/* bench 21578.1.2 8646d18b70bd */
+/* bench 21578.1.3 83e1f28fab95 */
+/* bench 21578.1.4 2b81e2e9f692 */
+/* bench 21578.1.5 31211b7a9b3c */
+/* bench 21578.1.6 80533b4f69d1 */
+/* bench 21578.1.7 eade0577801b */
+/* bench 21578.1.8 9d01b0c9d971 */
 };
 
 extern const struct net_user_info tuntap_user_info;
