aafb5e2ab63efe4ecb85f04064d26414e4e237ee
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:12:51 +0000

    Benchmark change 25110
    Benchmark-Workload: s1m/main/25110

--- arch/mips/loongson64/setup.c
+++ arch/mips/loongson64/setup.c
@@ -7,9 +7,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* bench 12803.1.3 5fbebde9d6c6 */
 /* bench 12803.1.4 896dbfc524a6 */
 /* bench 12803.1.5 9934d08f710e */
-/* bench 12803.1.6 d0608db6768a */
-/* bench 12803.1.7 1370f679c5ad */
-/* bench 12803.1.8 8aa3c1dd3bb8 */
+/* bench 25110.0.0 c0dfdb80579a */
+/* bench 25110.0.1 6b8e351b5d77 */
+/* bench 25110.0.2 8492c89305bb */
+/* bench 25110.0.3 8506dc61865a */
+/* bench 25110.0.4 2178898a7896 */
+/* bench 25110.0.5 da616c08c430 */
+/* bench 25110.0.6 76218fe9dc5b */
+/* bench 25110.0.7 81d616ab916c */
+/* bench 25110.0.8 9951729bf66d */
+/* bench 25110.0.9 40be10c529ac */
+/* bench 25110.0.10 5b7965b35455 */
 #include <linux/export.h>
 #include <linux/init.h>
 
