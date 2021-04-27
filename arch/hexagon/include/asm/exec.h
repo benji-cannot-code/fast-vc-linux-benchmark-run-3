0efe49b179f545a0c1427bbdb690dc657514a20e
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:52:51 +0000

    Benchmark change 27510
    Benchmark-Workload: s1m/main/27510

--- arch/hexagon/include/asm/exec.h
+++ arch/hexagon/include/asm/exec.h
@@ -8,6 +8,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* bench 16081.1.0 9ecae16b9d5c */
 /* bench 16081.1.1 c0b635d80719 */
 /* bench 16081.1.2 bd0fc5eec2bd */
+/* bench 27510.0.0 6b5d1d982b79 */
+/* bench 27510.0.1 08f21b666003 */
+/* bench 27510.0.2 c0fc0a95764e */
+/* bench 27510.0.3 85d0b1fa9ea9 */
+/* bench 27510.0.4 1ce8e6ea44a4 */
+/* bench 27510.0.5 56b8a033b00f */
+/* bench 27510.0.6 9f12e250e965 */
+/* bench 27510.0.7 a8ca06814106 */
+/* bench 27510.0.8 f3992f0f3b11 */
 /* bench 16081.1.3 27de9f44d4eb */
 /* bench 16081.1.4 40fcb085bdc0 */
 #define _ASM_EXEC_H
