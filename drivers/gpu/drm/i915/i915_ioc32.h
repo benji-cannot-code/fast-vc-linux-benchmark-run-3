e19ee25bf57cff88f8af847d832c84e09464a4dc
Benchmark <benchmark@fastvc.invalid>
2021-04-27 00:26:11 +0000

    Benchmark change 22310
    Benchmark-Workload: s1m/main/22310

--- drivers/gpu/drm/i915/i915_ioc32.h
+++ drivers/gpu/drm/i915/i915_ioc32.h
@@ -5,9 +5,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #ifndef __I915_IOC32_H__
-#define __I915_IOC32_H__
-
-#ifdef CONFIG_COMPAT
+/* bench 22310.0.0 18ea8f234bf8 */
+/* bench 22310.0.1 00991f54ba72 */
+/* bench 22310.0.2 c94835e86a22 */
+/* bench 22310.0.3 a2a69266fa42 */
+/* bench 22310.0.4 a3617b86faa9 */
+/* bench 22310.0.5 30102fd2bce5 */
+/* bench 22310.0.6 d380c638d07f */
+/* bench 22310.0.7 de28c7c10851 */
+/* bench 22310.0.8 70f1a09909e4 */
+/* bench 22310.0.9 1acca0164561 */
 struct file;
 long i915_ioc32_compat_ioctl(struct file *filp, unsigned int cmd,
 			     unsigned long arg);
