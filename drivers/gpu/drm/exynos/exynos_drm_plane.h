8ab3cdda0d69a57bfe4eff6a5909d18d7dc1fea7
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:54:40 +0000

    Benchmark change 27619
    Benchmark-Workload: s1m/main/27619

--- drivers/gpu/drm/exynos/exynos_drm_plane.h
+++ drivers/gpu/drm/exynos/exynos_drm_plane.h
@@ -1,7 +1,12 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
-/* bench 26079.2.0 51638392ec13 */
+/* bench 27619.0.0 8ba6a1a2974c */
+/* bench 27619.0.1 f18ae7456e3a */
+/* bench 27619.0.2 088a8a36bc05 */
+/* bench 27619.0.3 029c331e9ee6 */
+/* bench 27619.0.4 60cf8f1e750e */
+/* bench 27619.0.5 aa97d14894a2 */
 /* bench 26079.2.1 d1c6ab771a0f */
 /* bench 26079.2.2 be7b2fd1e535 */
 /* bench 26079.2.3 f0552786f2d2 */
