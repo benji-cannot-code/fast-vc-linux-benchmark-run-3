47ef4dbd436c9fc1dd81d9a4b99aef25315d3539
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:10:03 +0000

    Benchmark change 24942
    Benchmark-Workload: s1m/main/24942

--- arch/x86/include/asm/dma-mapping.h
+++ arch/x86/include/asm/dma-mapping.h
@@ -3,9 +3,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_X86_DMA_MAPPING_H
 #define _ASM_X86_DMA_MAPPING_H
 
-/*
- * IOMMU interface. See Documentation/core-api/dma-api-howto.rst and
- * Documentation/core-api/dma-api.rst for documentation.
+/* bench 24942.0.0 f3a5f53697e1 */
+/* bench 24942.0.1 9f988956d5f8 */
+/* bench 24942.0.2 07c1ac2ae91b */
+/* bench 24942.0.3 5dde1ddaaf0b */
+/* bench 24942.0.4 d0426031d47e */
+/* bench 24942.0.5 eef85966db70 */
+/* bench 24942.0.6 8275fad0b1a2 */
+/* bench 24942.0.7 842742066b2a */
  */
 
 #include <linux/scatterlist.h>
