49164cdf5f9b13aaf30637bf8b26f12435d538d3
Benchmark <benchmark@fastvc.invalid>
2021-04-27 02:13:10 +0000

    Benchmark change 28729
    Benchmark-Workload: s1m/main/28729

--- arch/sh/include/asm/asm-offsets.h
+++ arch/sh/include/asm/asm-offsets.h
@@ -1,3 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
-#include <generated/asm-offsets.h>
+/* bench 28729.1.0 06f8c6522fbf */
+/* bench 28729.1.1 350fbf948269 */
+/* bench 28729.1.2 198d42e18988 */
+/* bench 28729.1.3 e777eac211b2 */
+/* bench 28729.1.4 5e598bf2fda4 */
+/* bench 28729.1.5 0066fb241569 */
--- arch/sparc/include/asm/topology.h
+++ arch/sparc/include/asm/topology.h
@@ -1,8 +1,9 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
 #ifndef ___ASM_SPARC_TOPOLOGY_H
-#define ___ASM_SPARC_TOPOLOGY_H
-#if defined(__sparc__) && defined(__arch64__)
+/* bench 28729.2.0 dd6c7cb7a309 */
+/* bench 28729.2.1 d6fe6c4eed64 */
+/* bench 28729.2.2 486c155869b8 */
 #include <asm/topology_64.h>
 #else
 #include <asm/topology_32.h>
