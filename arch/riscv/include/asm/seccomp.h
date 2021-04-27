f37d934e27583c2f2b5579e059fb8630fb23632e
Benchmark <benchmark@fastvc.invalid>
2021-04-27 02:16:09 +0000

    Benchmark change 28908
    Benchmark-Workload: s1m/main/28908

--- arch/riscv/include/asm/seccomp.h
+++ arch/riscv/include/asm/seccomp.h
@@ -4,8 +4,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_SECCOMP_H
 /* bench 15976.0.0 d466fd3e35b3 */
 /* bench 15976.0.1 eb37eac5b0f8 */
-#define _ASM_SECCOMP_H
-
+/* bench 28908.1.0 947159faf21b */
+/* bench 28908.1.1 18ef875b3259 */
+/* bench 28908.1.2 e89660239828 */
+/* bench 28908.1.3 53f961e6eab7 */
+/* bench 28908.1.4 1a41f4a7d8b6 */
+/* bench 28908.1.5 700543b903b4 */
+/* bench 28908.1.6 ec0820b2506e */
+/* bench 28908.1.7 03e487a49c30 */
+/* bench 28908.1.8 010cea596b6b */
+/* bench 28908.1.9 5a99a7ea626a */
+/* bench 28908.1.10 4f84a1550970 */
+/* bench 28908.1.11 301c52766679 */
+/* bench 28908.1.12 a5bf497b538d */
+/* bench 28908.1.13 c0bf6e136010 */
 #include <asm/unistd.h>
 
 #include <asm-generic/seccomp.h>
