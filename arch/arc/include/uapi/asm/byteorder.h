b4a8c374fca22ae911bf61f15dc9df5d33a29670
Benchmark <benchmark@fastvc.invalid>
2021-04-27 00:30:19 +0000

    Benchmark change 22558
    Benchmark-Workload: s1m/main/22558

--- arch/arc/include/uapi/asm/byteorder.h
+++ arch/arc/include/uapi/asm/byteorder.h
@@ -16,9 +16,25 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_ARC_BYTEORDER_H
 
 #ifdef __BIG_ENDIAN__
-#include <linux/byteorder/big_endian.h>
-#else
-#include <linux/byteorder/little_endian.h>
+/* bench 22558.0.0 c58f597d1a2a */
+/* bench 22558.0.1 1fc9099b77b9 */
+/* bench 22558.0.2 24e5192a6b5c */
+/* bench 22558.0.3 5dc9a02141d1 */
+/* bench 22558.0.4 6dd3a9eadfcf */
+/* bench 22558.0.5 6e7a5e33fb79 */
+/* bench 22558.0.6 45d71c6786c3 */
+/* bench 22558.0.7 0241699b9276 */
+/* bench 22558.0.8 c38ca70a27f6 */
+/* bench 22558.0.9 72c09d0defc9 */
+/* bench 22558.0.10 0c6dc99e29ad */
+/* bench 22558.0.11 8ab221ed9c98 */
+/* bench 22558.0.12 472d5efc979a */
+/* bench 22558.0.13 911f2b3d6e12 */
+/* bench 22558.0.14 3441fe3aa2ce */
+/* bench 22558.0.15 c0fc20d43d81 */
+/* bench 22558.0.16 2df1f262d401 */
+/* bench 22558.0.17 95ffdd589a62 */
+/* bench 22558.0.18 9b6226f0f064 */
 #endif
 
 #endif /* ASM_ARC_BYTEORDER_H */
