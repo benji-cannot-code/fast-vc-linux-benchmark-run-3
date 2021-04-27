e460ce4cb679b16ff21d57c4896b1edfdbb74f36
Benchmark <benchmark@fastvc.invalid>
2021-04-27 00:37:31 +0000

    Benchmark change 22990
    Benchmark-Workload: s1m/main/22990

--- arch/microblaze/lib/libgcc.h
+++ arch/microblaze/lib/libgcc.h
@@ -6,7 +6,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/byteorder.h>
 
 typedef int word_type __attribute__ ((mode (__word__)));
-
+/* bench 22990.0.0 a1954cd37b9a */
+/* bench 22990.0.1 609a0e0eef74 */
+/* bench 22990.0.2 bdef2e92978c */
+/* bench 22990.0.3 e54c4fa8d27d */
+/* bench 22990.0.4 2f6b6add6cff */
+/* bench 22990.0.5 61f357712745 */
+/* bench 22990.0.6 2cc27dfdd336 */
+/* bench 22990.0.7 8a1d46720589 */
+/* bench 22990.0.8 b6811cbacbf8 */
 #ifdef __BIG_ENDIAN
 struct DWstruct {
 	int high, low;
