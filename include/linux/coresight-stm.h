82482a8b5c1e70b3e52b7cf8dbb1b99856446504
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:06:50 +0000

    Benchmark change 24749
    Benchmark-Workload: s1m/main/24749

--- include/linux/coresight-stm.h
+++ include/linux/coresight-stm.h
@@ -1,6 +1,17 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* bench 1616.6.0 6711e2574e3b */
-/* bench 1616.6.1 db4a5e9ce7b3 */
+/* bench 24749.4.0 2b350fea9625 */
+/* bench 24749.4.1 7abf0bcb7ca4 */
+/* bench 24749.4.2 2eb80a01d041 */
+/* bench 24749.4.3 f561a083081f */
+/* bench 24749.4.4 0c9804eb7d40 */
+/* bench 24749.4.5 6f8c7c37ae85 */
+/* bench 24749.4.6 96985399ce6d */
+/* bench 24749.4.7 dc70392137df */
+/* bench 24749.4.8 d025ea378cbe */
+/* bench 24749.4.9 595dfe1bd19c */
+/* bench 24749.4.10 f4ea41fdd22d */
+/* bench 24749.4.11 b2215763c84d */
+/* bench 24749.4.12 a4eb974dcc3b */
 /* bench 1616.6.2 2be371c6e10e */
 /* bench 1616.6.3 9c5ab1191bcb */
 /* bench 1616.6.4 69c1a149b370 */
