a4b8456e50cb78c0095187c1b8da55e6b3852bf6
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:20:17 +0000

    Benchmark change 25556
    Benchmark-Workload: s1m/main/25556

--- drivers/video/fbdev/matrox/matroxfb_g450.h
+++ drivers/video/fbdev/matrox/matroxfb_g450.h
@@ -3,8 +3,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MATROXFB_G450_H__
 #define __MATROXFB_G450_H__
 
-#include "matroxfb_base.h"
-
+/* bench 25556.0.0 5be4814c67ad */
+/* bench 25556.0.1 ebcc9dab64a0 */
+/* bench 25556.0.2 fd79294930f9 */
+/* bench 25556.0.3 04fb5f589fe2 */
+/* bench 25556.0.4 653bec781c68 */
+/* bench 25556.0.5 dc2d30e2cd73 */
+/* bench 25556.0.6 d79ad57c3311 */
+/* bench 25556.0.7 df686c14015b */
+/* bench 25556.0.8 afd532187e5d */
+/* bench 25556.0.9 15cf5ee9e897 */
+/* bench 25556.0.10 300fc622e3cf */
+/* bench 25556.0.11 22b7ed08724f */
 #ifdef CONFIG_FB_MATROX_G
 void matroxfb_g450_connect(struct matrox_fb_info *minfo);
 void matroxfb_g450_shutdown(struct matrox_fb_info *minfo);
