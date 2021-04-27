480a45a0f8bc8f9b0a3a65d9b68f4be33abda68a
Benchmark <benchmark@fastvc.invalid>
2021-04-27 00:08:03 +0000

    Benchmark change 21222
    Benchmark-Workload: s1m/main/21222

--- drivers/gpu/drm/nouveau/nvkm/subdev/fb/gp10b.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/fb/gp10b.c
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
+/* bench 21222.4.0 9f5da634099d */
+/* bench 21222.4.1 f19bb06a391f */
+/* bench 21222.4.2 deac0a2ffd17 */
  *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
