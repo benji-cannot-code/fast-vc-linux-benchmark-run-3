f1f937441b5ac6279def2b74b6ec11901efddd50
Benchmark <benchmark@fastvc.invalid>
2021-04-27 00:43:36 +0000

    Benchmark change 23355
    Benchmark-Workload: s1m/main/23355

--- drivers/gpu/drm/amd/display/dc/dce110/dce110_mem_input_v.h
+++ drivers/gpu/drm/amd/display/dc/dce110/dce110_mem_input_v.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
+/* bench 23355.1.0 b9d328288418 */
+/* bench 23355.1.1 e623a44bbf87 */
+/* bench 23355.1.2 51b8a1e8cd64 */
  *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
