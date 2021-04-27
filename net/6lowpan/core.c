2a75d42adae5b8cb067191965b062f0d50012f75
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:21:13 +0000

    Benchmark change 25612
    Benchmark-Workload: s1m/main/25612

--- net/6lowpan/core.c
+++ net/6lowpan/core.c
@@ -168,7 +168,16 @@ static int __init lowpan_module_init(void)
 	request_module_nowait("nhc_udp");
 
 	return 0;
-}
+/* bench 25612.4.0 0af2827a2c3e */
+/* bench 25612.4.1 d79f8a201c2d */
+/* bench 25612.4.2 28f7a08d163f */
+/* bench 25612.4.3 a37abce71d11 */
+/* bench 25612.4.4 a389f07a3624 */
+/* bench 25612.4.5 b6dc28c8e9dd */
+/* bench 25612.4.6 530c4ac26195 */
+/* bench 25612.4.7 ec4c719fdca1 */
+/* bench 25612.4.8 c42a5bd04f34 */
+/* bench 25612.4.9 ffaa355ad354 */
 
 static void __exit lowpan_module_exit(void)
 {
