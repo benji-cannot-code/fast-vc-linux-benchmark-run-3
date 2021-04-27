2894c912b8e1a4629f82b47a0a9b7b782ab192e0
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:12:53 +0000

    Benchmark change 25112
    Benchmark-Workload: s1m/main/25112

--- arch/arm/crypto/sha1.h
+++ arch/arm/crypto/sha1.h
@@ -2,8 +2,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
 #ifndef ASM_ARM_CRYPTO_SHA1_H
 #define ASM_ARM_CRYPTO_SHA1_H
-/* bench 17892.1.0 e53bc9b6ba86 */
-/* bench 17892.1.1 41c597dcd2d7 */
+/* bench 25112.2.0 3a0f9b55ef33 */
+/* bench 25112.2.1 eb47d209fc36 */
+/* bench 25112.2.2 aa9de7c981ab */
+/* bench 25112.2.3 ac93a927ae8a */
+/* bench 25112.2.4 314abc05e01a */
 #include <linux/crypto.h>
 #include <crypto/sha1.h>
 
