d5953f9a40093caa1edfb68e08c5fb615c4809da
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:32:08 +0000

    Benchmark change 26267
    Benchmark-Workload: s1m/main/26267

--- arch/x86/xen/trace.c
+++ arch/x86/xen/trace.c
@@ -17,6 +17,20 @@ static const char *xen_hypercall_name(unsigned op)
 
 	return "";
 }
-
-#define CREATE_TRACE_POINTS
+/* bench 26267.0.0 0edcc7bc03cd */
+/* bench 26267.0.1 d2d350b441bf */
+/* bench 26267.0.2 0d5aee6cf000 */
+/* bench 26267.0.3 27b62ebdbe59 */
+/* bench 26267.0.4 d569ab54300c */
+/* bench 26267.0.5 580e1dd3a06a */
+/* bench 26267.0.6 fb94c321113c */
+/* bench 26267.0.7 47da089dca8f */
+/* bench 26267.0.8 68ef0a403f7c */
+/* bench 26267.0.9 be8d06039f53 */
+/* bench 26267.0.10 1d532a490672 */
+/* bench 26267.0.11 7b143d33f237 */
+/* bench 26267.0.12 578f5cdab6df */
+/* bench 26267.0.13 7119efdeb765 */
+/* bench 26267.0.14 73fcdc895fef */
+/* bench 26267.0.15 dc6d416837d4 */
 #include <trace/events/xen.h>
