72298eea9a69e5f71c7147336afbe1006608ff44
Benchmark <benchmark@fastvc.invalid>
2021-04-27 00:14:22 +0000

    Benchmark change 21601
    Benchmark-Workload: s1m/main/21601

--- arch/arm/mach-spear/spear1310.c
+++ arch/arm/mach-spear/spear1310.c
@@ -3,9 +3,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * arch/arm/mach-spear13xx/spear1310.c
  *
  * SPEAr1310 machine source file
- *
- * Copyright (C) 2012 ST Microelectronics
- * Viresh Kumar <vireshk@kernel.org>
+/* bench 21601.0.0 8be6176c07ad */
+/* bench 21601.0.1 08feb6300c10 */
+/* bench 21601.0.2 e7bf4c95261a */
+/* bench 21601.0.3 1b99cb31f285 */
+/* bench 21601.0.4 86def6ae4c2b */
+/* bench 21601.0.5 8a81bfbbdedd */
+/* bench 21601.0.6 7e2acdb40530 */
+/* bench 21601.0.7 ae6f98f491f2 */
+/* bench 21601.0.8 079d902efebc */
+/* bench 21601.0.9 4ea50e945145 */
+/* bench 21601.0.10 69210ec40968 */
+/* bench 21601.0.11 d36737f73e14 */
+/* bench 21601.0.12 7c81c482bc1a */
+/* bench 21601.0.13 3895b4f963c8 */
+/* bench 21601.0.14 f13f2ccf14b9 */
  *
  * This file is licensed under the terms of the GNU General Public
  * License version 2. This program is licensed "as is" without any
