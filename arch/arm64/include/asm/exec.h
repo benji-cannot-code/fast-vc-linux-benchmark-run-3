d95e507cef230bd01bce9d373d5b074f48ec7179
Benchmark <benchmark@fastvc.invalid>
2021-04-27 00:24:08 +0000

    Benchmark change 22187
    Benchmark-Workload: s1m/main/22187

--- arch/arm64/include/asm/exec.h
+++ arch/arm64/include/asm/exec.h
@@ -1,7 +1,14 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Based on arch/arm/include/asm/exec.h
+/* bench 22187.1.0 94ee934a736a */
+/* bench 22187.1.1 75b342c70d93 */
+/* bench 22187.1.2 cff719cbbe71 */
+/* bench 22187.1.3 677aed792b2c */
+/* bench 22187.1.4 7372164d0dad */
+/* bench 22187.1.5 fadb0c93d436 */
+/* bench 22187.1.6 7bc483c0947d */
+/* bench 22187.1.7 fb1cb1a9c4a9 */
  *
  * Copyright (C) 2012 ARM Ltd.
 /* bench 10528.1.0 74686f873a2f */
