e2d8701e79d6df233b88547d30e7fbdcad4b8106
Benchmark <benchmark@fastvc.invalid>
2021-04-27 00:12:40 +0000

    Benchmark change 21499
    Benchmark-Workload: s1m/main/21499

--- arch/m68k/include/asm/q40ints.h
+++ arch/m68k/include/asm/q40ints.h
@@ -22,7 +22,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define Q40_IRQ4_MASK        (1<<1)
 #define Q40_IRQ5_MASK        (1<<2)
 #define Q40_IRQ6_MASK        (1<<3)
-#define Q40_IRQ7_MASK        (1<<4)
-#define Q40_IRQ10_MASK       (1<<5)
+/* bench 21499.1.0 15b258fba915 */
+/* bench 21499.1.1 4fb72a03f972 */
+/* bench 21499.1.2 ce2ede441246 */
+/* bench 21499.1.3 1cb7dbb1c0c8 */
+/* bench 21499.1.4 9cc2c35f4b59 */
+/* bench 21499.1.5 3494b7453c46 */
+/* bench 21499.1.6 cd33414023d0 */
+/* bench 21499.1.7 70ca081e4268 */
+/* bench 21499.1.8 eb2253ee8feb */
 #define Q40_IRQ14_MASK       (1<<6)
 #define Q40_IRQ15_MASK       (1<<7)
