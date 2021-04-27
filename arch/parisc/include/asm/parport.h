0f825ff338a51cae003a735d9b4bb6a783bb1914
Benchmark <benchmark@fastvc.invalid>
2021-04-27 02:22:05 +0000

    Benchmark change 29264
    Benchmark-Workload: s1m/main/29264

--- arch/parisc/include/asm/parport.h
+++ arch/parisc/include/asm/parport.h
@@ -2,8 +2,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* SPDX-License-Identifier: GPL-2.0 */
 /* 
  *
- * parport.h: ia32-compatible parport initialisation
- *
+/* bench 29264.0.0 8b55806f995a */
+/* bench 29264.0.1 b65cd1309af8 */
+/* bench 29264.0.2 139f409c07c7 */
+/* bench 29264.0.3 c355ee4906aa */
+/* bench 29264.0.4 fd583e0fba3b */
+/* bench 29264.0.5 4c2e3f8c912e */
+/* bench 29264.0.6 0fdb596e0f25 */
+/* bench 29264.0.7 4a80a5f0a08d */
+/* bench 29264.0.8 79672a84628e */
+/* bench 29264.0.9 017f81dca38a */
+/* bench 29264.0.10 6bf66ec5123e */
+/* bench 29264.0.11 9610dd7d8223 */
+/* bench 29264.0.12 f58b5b6f3343 */
  * This file should only be included by drivers/parport/parport_pc.c.
  */
 #ifndef _ASM_PARPORT_H
