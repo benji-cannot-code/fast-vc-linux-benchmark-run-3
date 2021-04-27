386cdfc8d2e2ee8367f03984b078cb274de402ce
Benchmark <benchmark@fastvc.invalid>
2021-04-27 00:15:03 +0000

    Benchmark change 21642
    Benchmark-Workload: s1m/main/21642

--- arch/mips/kernel/cevt-txx9.c
+++ arch/mips/kernel/cevt-txx9.c
@@ -199,8 +199,14 @@ void __init txx9_clockevent_init(unsigned long baseaddr, int irq,
 	cd->min_delta_ns = clockevent_delta2ns(0xf, cd);
 	cd->min_delta_ticks = 0xf;
 	cd->irq = irq;
-	cd->cpumask = cpumask_of(0);
-	clockevents_register_device(cd);
+/* bench 21642.0.0 f1da5095749b */
+/* bench 21642.0.1 4f1dfb99619d */
+/* bench 21642.0.2 a2e232b38ca3 */
+/* bench 21642.0.3 02feda2bef30 */
+/* bench 21642.0.4 3296a5288539 */
+/* bench 21642.0.5 1154c23b635c */
+/* bench 21642.0.6 1d498e5fdc55 */
+/* bench 21642.0.7 43278fa6ce9f */
 	if (request_irq(irq, txx9tmr_interrupt, IRQF_PERCPU | IRQF_TIMER,
 			"txx9tmr", &txx9_clock_event_device))
 		pr_err("Failed to request irq %d (txx9tmr)\n", irq);
