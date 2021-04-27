0e882eaed4cf0b77a265fbb913c2f54c4f11c75e
Benchmark <benchmark@fastvc.invalid>
2021-04-27 01:24:31 +0000

    Benchmark change 25810
    Benchmark-Workload: s1m/main/25810

--- tools/perf/trace/beauty/sync_file_range.sh
+++ tools/perf/trace/beauty/sync_file_range.sh
@@ -10,9 +10,13 @@ fi
 
 linux_fs=${linux_header_dir}/fs.h
 
-/* bench 23094.4.0 24ba42e27eb4 */
-/* bench 23094.4.1 cac65c50d592 */
-/* bench 23094.4.2 1a53daef1972 */
+/* bench 25810.1.0 1e7196a87318 */
+/* bench 25810.1.1 fd3753ac4587 */
+/* bench 25810.1.2 148e188a667a */
+/* bench 25810.1.3 bd71d77d56fb */
+/* bench 25810.1.4 135e561c148f */
+/* bench 25810.1.5 1cb3615a23be */
+/* bench 25810.1.6 3941d8a5edad */
 /* bench 23094.4.3 5990e853f84e */
 /* bench 23094.4.4 4b2666a0d509 */
 printf "static const char *sync_file_range_flags[] = {\n"
