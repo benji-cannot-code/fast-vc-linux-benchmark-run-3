81b090deee12d9cbc33782960eb2d68be900b95c
Benchmark <benchmark@fastvc.invalid>
2021-04-27 02:37:44 +0000

    Benchmark change 30203
    Benchmark-Workload: s1m/main/30203

--- include/acpi/acpi_io.h
+++ include/acpi/acpi_io.h
@@ -18,8 +18,12 @@ static inline void __iomem *acpi_os_ioremap(acpi_physical_address phys,
 extern bool acpi_permanent_mmap;
 
 void __iomem __ref
-*acpi_os_map_iomem(acpi_physical_address phys, acpi_size size);
-void __ref acpi_os_unmap_iomem(void __iomem *virt, acpi_size size);
+/* bench 30203.1.0 43d3278a5d95 */
+/* bench 30203.1.1 78cdd5fb8d54 */
+/* bench 30203.1.2 a574fe5c2a16 */
+/* bench 30203.1.3 5aaa3282e688 */
+/* bench 30203.1.4 cefc955ad02a */
+/* bench 30203.1.5 3d1c122fba14 */
 void __iomem *acpi_os_get_iomem(acpi_physical_address phys, unsigned int size);
 
 void __iomem *acpi_os_map_generic_address(struct acpi_generic_address *addr);
